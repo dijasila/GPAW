2b37efc2fc3f96d24683937b947ffca8bd2a88cf
getri <getri@6419b465-d902-0410-997c-be2266baf723>
2008-10-15 14:49:56 +0000

    generalized finite difference approximation

--- gpaw/operators.py
+++ gpaw/operators.py
@@ -112,6 +112,15 @@ laplace = [[0],
            [-5269/1800, 5/3, -5/21, 5/126, -5/1008, 1/3150],
            [-5369/1800, 12/7, -15/56, 10/189, -1/112, 2/1925, -1/16632]]
 
+# Cross terms
+cross = [[0],
+         [1/4],
+         [4/9  ,-1/18  ,1/144], #1,1 / 1,2 / 2,2
+         [9/16, -9/80,  1/80, 9/400, -1/400, 1/3600], #1,1 / 1,2 / 1,3 / 2,2 / 2,3 / 3,3
+         [16/25,-4/25,16/525,-1/350,  1/25 ,-4/525 ,1/1400, 16/11025, -1/7350, 1/78400]]
+         #[....., 1/ 1587600]
+         #[....., 1/30735936]
+
 # Check numbers:
 if debug:
     for coefs in laplace:
@@ -121,23 +130,58 @@ if debug:
 def Laplace(gd, scale=1.0, n=1, dtype=float):
     """Central finite diference Laplacian.
 
-    Uses 6*n neighbors."""
+    Uses (max) 12*n**2 + 6*n neighbors."""
 
     n = int(n)
     h = gd.h_c
     h2 = h**2
+
+    iucell_cv = npy.linalg.inv((gd.domain.cell_cv/((gd.domain.cell_cv**2).sum(1)**0.5)).T)
+    d2 = (iucell_cv**2).sum(1)
+
     offsets = [(0, 0, 0)]
-    coefs = [scale * npy.sum(npy.divide(laplace[n][0], h2))]
+    coefs = [scale * npy.sum(d2 * npy.divide(laplace[n][0],h2))]
+
     for d in range(1, n + 1):
         offsets.extend([(-d, 0, 0), (d, 0, 0),
                         (0, -d, 0), (0, d, 0),
                         (0, 0, -d), (0, 0, d)])
-        c = scale * npy.divide(laplace[n][d], h2)
+        c = scale * d2 * npy.divide(laplace[n][d], h2)
+
         coefs.extend([c[0], c[0],
                       c[1], c[1],
                       c[2], c[2]])
-    return Operator(coefs, offsets, gd, dtype)
 
+    #cross terms
+    n = min(n,4)
+    ci=0
+
+    for d1 in range(n):
+        for d2 in range(d1,n):
+
+            offset=[[( d1+1, d2+1, 0   ),(-d1-1 , d2+1 , 0   ),( d1+1 ,-d2-1, 0   ),(-d1-1,-d2-1,0    )],
+                    [( 0   , d1+1, d2+1),( 0    ,-d1-1 , d2+1),( 0    , d1+1,-d2-1),( 0   ,-d1-1,-d2-1)],
+                    [( d2+1, 0   , d1+1),( d2+1 , 0    ,-d1-1),(-d2-1 , 0   , d1+1),(-d2-1,0    ,-d1-1)]]
+
+            for i in range(3):
+                c=2.*cross[n][ci]*npy.dot(iucell_cv[i],iucell_cv[(i+1)%3])/(h[i]*h[(i+1)%3])
+
+                if abs(c)>1E-10:
+                    offsets.extend(offset[i])
+                    coefs.extend([c,-c,-c,c])
+
+                    if (d1<>d2): #ugly XXX
+                        if (i==0):
+                            offsets.extend([( d2+1, d1+1, 0   ),( d2+1,-d1-1,0    ),(-d2-1, d1+1, 0   ),(-d2-1,-d1-1,0    )])
+                        elif (i==1):
+                            offsets.extend([( 0   , d2+1, d1+1),( 0   , d2+1,-d1-1),( 0   ,-d2-1, d1+1),( 0   ,-d2-1,-d1-1)])
+                        elif (i==2):
+                            offsets.extend([( d1+1, 0   , d2+1),(-d1-1, 0   , d2+1),( d1+1, 0   ,-d2-1),(-d1-1, 0   ,-d2-1)])
+                        coefs.extend([c,-c,-c,c])
+
+            ci+=1
+
+    return Operator(coefs, offsets, gd, dtype)
 
 def LaplaceA(gd, scale, dtype=float):
     c = npy.divide(-1/12, gd.h_c**2) * scale  # Why divide? XXX
