2a455c8119dbc63476ebef74e052b19a5b95e8a6
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2008-12-05 22:03:34 +0000

    Add second type of mapfile for BlueGene/P.

--- tools/mapfile.py
+++ tools/mapfile.py
@@ -2,6 +2,9 @@ from sys import argv
 import numpy as np
 nodes = int(argv[1])
 shape = [int(x) for x in argv[2].split(',')]
+mode = 'normal'
+if len(argv) == 4:
+    mode = argv[3]
 layout = {  512: (8,  8,  8 ),
            1024: (8,  8,  16),
            2048: (8,  8,  32),
@@ -18,18 +21,36 @@ A = layout[0] // shape[0]
 B = layout[1] // shape[1]
 check = np.zeros(layout + (4,), int)
 rank = 0
-for n in range(blocks):
-    x0 = (n % A) * shape[0]
-    y0 = ((n // A) % B) * shape[1]
-    z0 = (n / (A * B)) * shape[2] // 4
-    for a in range(shape[0]):
-        for b in range(shape[1]):
-            for c in range(shape[2]):
-                x = x0 + a
-                y = y0 + b
-                z = z0 + c // 4
-                t = c % 4
-                check[x, y, z, t] = 1
-                print x, y, z, t
+if mode == 'normal':
+    for n in range(blocks):
+        x0 = (n % A) * shape[0]
+        y0 = ((n // A) % B) * shape[1]
+        z0 = (n // (A * B)) * shape[2] // 4
+        for a in range(shape[0]):
+            for b in range(shape[1]):
+                for c in range(shape[2]):
+                    x = x0 + a
+                    y = y0 + b
+                    z = z0 + c // 4
+                    t = c % 4
+                    check[x, y, z, t] = 1
+                    print x, y, z, t
+                rank += 1
+else:
+    assert mode == 'shared'
+    for n in range(blocks):
+        t = n % 4
+        m = n // 4
+        x0 = (m % A) * shape[0]
+        y0 = ((m // A) % B) * shape[1]
+        z0 = (m // (A * B)) * shape[2]
+        for a in range(shape[0]):
+            for b in range(shape[1]):
+                for c in range(shape[2]):
+                    x = x0 + a
+                    y = y0 + b
+                    z = z0 + c
+                    check[x, y, z, t] = 1
+                    print x, y, z, t
                 rank += 1
 assert check.sum() == nodes * 4
