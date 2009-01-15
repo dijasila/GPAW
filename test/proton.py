5c51ecad71bd8d29d97e9ad536fa2979643fce53
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2009-01-15 15:10:36 +0000

    A proton should not have magmom=1.

--- test/proton.py
+++ test/proton.py
@@ -3,9 +3,9 @@ from ase import *
 from gpaw import GPAW
 
 a = 4.0
-H = Atoms([Atom('H', (a/2, a/2, a/2), magmom=1)],
-                pbc=0,
-                cell=(a, a, a))
+H = Atoms('H', [(a / 2, a / 2, a / 2)],
+          pbc=0,
+          cell=(a, a, a))
 calc = GPAW(nbands=1, h=0.2, charge=1)
 H.set_calculator(calc)
 print H.get_potential_energy() + calc.get_reference_energy()
