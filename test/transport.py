97b95692b24864132260d48c83908bbcce8864cf
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2009-01-16 09:23:16 +0000

    add a restart_flag for lead

--- test/transport.py
+++ test/transport.py
@@ -1,44 +1,38 @@
 from ase import *
 from gpaw import *
 from gpaw.lcao.gpawtransport import GPAWTransport 
-from gpaw.atom.basis import BasisMaker
 import pickle
 import numpy as npy
 
 a = 3.6
-L = 7.00
+L = 7.00 
 
 atoms = Atoms('Na4', pbc=(1, 1, 1), cell=[4 * a, L, L])
 atoms.positions[:4, 0] = [i * a for i in range(4)]
 atoms.positions[:, 1:] = L / 2.
-
-bm = BasisMaker('Na')#, run=False)
-#bm.generator.run(write_xml=False)
-basis = bm.generate(1, 1)
-
 atoms.center()
 atoms.set_calculator(GPAW(h=0.3,
                           xc='PBE',
-                          basis={'Na': basis},
+                          basis='szp',
                           kpts=(3,1,1),
                           width=0.01,
                           mode='lcao',
                           txt='Na_lcao.txt',
                           usesymm=None,
                           mixer=Mixer(0.1, 5, metric='new', weight=100.0)))
-
-# Setup the GPAWTransport calculator
-pl_atoms1 = range(4)     # Atomic indices of the left principal layer
-pl_atoms2 = range(-4, 0) # Atomic indices of the right principal layer
-pl_cell1 = (4 * a, L, L) # Cell for the left principal layer
-pl_cell2 = pl_cell1      # Cell for the right principal layer
+pl_atoms1 = range(4)     
+pl_atoms2 = range(-4, 0) 
+pl_cell1 = (4 * a, L, L) 
+pl_cell2 = pl_cell1      
 
 gpawtran = GPAWTransport(atoms=atoms,
                          pl_atoms=(pl_atoms1, pl_atoms2),
                          pl_cells=(pl_cell1, pl_cell2),
-                         d=0) #transport direction (0 := x)
-
-gpawtran.negf_prepare('test1', 0)
-gpawtran.get_selfconsistent_hamiltonian(bias=0, gate=0, verbose=1)
-
+                         d=0) 
+gpawtran.negf_prepare('test1', scat_restart=False, lead_restart=False)
+gpawtran.get_selfconsistent_hamiltonian(bias=0, gate=0,extend_layer=0,verbose=1)
+filename = 'Na4_eq'
+fd = file(filename,'wb')
+pickle.dump((gpawtran.h_syzkmm, gpawtran.d_syzkmm, gpawtran.atoms.calc.hamiltonian.vt_sG), fd, 2)
+fd.close()
 
