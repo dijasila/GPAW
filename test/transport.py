991e342098224b2c98e13c21cda763e9a1da2214
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2009-01-30 09:07:52 +0000

    change the transport direction to z for parallel calculation

--- test/transport.py
+++ test/transport.py
@@ -9,14 +9,14 @@ L = 7.00
 
 basis = BasisMaker('Na').generate(1, 1)
 
-atoms = Atoms('Na4', pbc=(1, 1, 1), cell=[4 * a, L, L])
-atoms.positions[:4, 0] = [i * a for i in range(4)]
-atoms.positions[:, 1:] = L / 2.
+atoms = Atoms('Na4', pbc=(1, 1, 1), cell=[L, L, 4 * a])
+atoms.positions[:4, 2] = [i * a for i in range(4)]
+atoms.positions[:, :2] = L / 2.
 atoms.center()
 atoms.set_calculator(GPAW(h=0.3,
                           xc='PBE',
                           basis={'Na': basis},
-                          kpts=(3,1,1),
+                          kpts=(1,1,13),
                           width=0.01,
                           mode='lcao',
                           txt='Na_lcao.txt',
@@ -24,17 +24,14 @@ atoms.set_calculator(GPAW(h=0.3,
                           mixer=Mixer(0.1, 5, metric='new', weight=100.0)))
 pl_atoms1 = range(4)     
 pl_atoms2 = range(-4, 0) 
-pl_cell1 = (4 * a, L, L) 
+pl_cell1 = (L, L, 4 * a) 
 pl_cell2 = pl_cell1      
 
 gpawtran = GPAWTransport(atoms=atoms,
                          pl_atoms=(pl_atoms1, pl_atoms2),
                          pl_cells=(pl_cell1, pl_cell2),
-                         d=0) 
+                         d=2) 
 gpawtran.negf_prepare(scat_restart=True, lead_restart=True)
 gpawtran.get_selfconsistent_hamiltonian(bias=0, gate=0,verbose=1)
 filename = 'Na4_eq'
-fd = file(filename,'wb')
-pickle.dump((gpawtran.h_syzkmm, gpawtran.d_syzkmm, gpawtran.atoms.calc.hamiltonian.vt_sG), fd, 2)
-fd.close()
-
+gpawtran.output(filename)
