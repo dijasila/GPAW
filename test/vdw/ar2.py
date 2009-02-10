9595f3b273013442893933252fd0bcc3714f3bbf
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2009-02-10 08:41:01 +0000

    Only run vdW-DF test if the vdW-kernel file is available - generating the file requires scipy.

--- test/vdw/ar2.py
+++ test/vdw/ar2.py
@@ -1,3 +1,4 @@
+import os
 from ase import *
 from gpaw import GPAW, setup_paths
 from gpaw.vdw import FFTVDWFunctional
@@ -10,26 +11,30 @@ if rank == 0:
 barrier()
 setup_paths.insert(0, '.')
 
-vdw = FFTVDWFunctional(verbose=1)
-d = 3.9
-x = d / sqrt(3)
-L = 3.0 + 2 * 4.0
-dimer = Atoms('Ar2', [(0, 0, 0), (x, x, x)], cell=(L, L, L))
-dimer.center()
-calc = GPAW(h=0.2, xc='revPBE')
-dimer.set_calculator(calc)
-e2 = dimer.get_potential_energy()
-calc.write('Ar2.gpw')
-e2vdw = calc.get_xc_difference(vdw)
-e2vdwb = GPAW('Ar2.gpw').get_xc_difference(vdw)
-print e2vdw - e2vdw
-assert abs(e2vdw - e2vdw) < 1e-9
-del dimer[1]
-e = dimer.get_potential_energy()
-evdw = calc.get_xc_difference(vdw)
+def test():
+    vdw = FFTVDWFunctional(verbose=1)
+    d = 3.9
+    x = d / sqrt(3)
+    L = 3.0 + 2 * 4.0
+    dimer = Atoms('Ar2', [(0, 0, 0), (x, x, x)], cell=(L, L, L))
+    dimer.center()
+    calc = GPAW(h=0.2, xc='revPBE')
+    dimer.set_calculator(calc)
+    e2 = dimer.get_potential_energy()
+    calc.write('Ar2.gpw')
+    e2vdw = calc.get_xc_difference(vdw)
+    e2vdwb = GPAW('Ar2.gpw').get_xc_difference(vdw)
+    print e2vdw - e2vdw
+    assert abs(e2vdw - e2vdw) < 1e-9
+    del dimer[1]
+    e = dimer.get_potential_energy()
+    evdw = calc.get_xc_difference(vdw)
 
-E = 2 * e - e2
-Evdw = E + 2 * evdw - e2vdw
-print E, Evdw
-assert abs(E - -0.0048) < 1e-4
-assert abs(Evdw - +0.0223) < 1e-4
+    E = 2 * e - e2
+    Evdw = E + 2 * evdw - e2vdw
+    print E, Evdw
+    assert abs(E - -0.0048) < 1e-4
+    assert abs(Evdw - +0.0223) < 1e-4
+
+if 'GPAW_VDW' in os.environ:
+    test()
