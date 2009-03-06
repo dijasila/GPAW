aa8235903f2d5df6679cd51aa62a1e30626d75e4
naromero <naromero@6419b465-d902-0410-997c-be2266baf723>
2009-03-06 16:43:05 +0000

    Updates to blacs_redist. Added GPAW scalapack symmetric diagonalize using divide and conquer algorithm (untested).

--- c/blacs.c
+++ c/blacs.c
@@ -26,11 +26,12 @@ void Cblacs_pinfo_(int *mypnum, int *nprocs);
 int Csys2blacs_handle_(MPI_Comm SysCtxt);
 // End of BLACS
 
-// ScaLapack
+// ScaLAPACK
 #ifdef GPAW_AIX
 #define   descinit_  descinit
-#define   numroc_  numroc
-#define   Cpdgemr2d_  Cpdgemr2d
+#define   numroc_    numroc
+#define   Cpdgemr2d_ Cpdgemr2d
+#define   pdlamch_   pdlamch
 
 #define   pdpotrf_  pdpotrf
 #define   pdpotri_  pdpotri
@@ -40,10 +41,10 @@ int Csys2blacs_handle_(MPI_Comm SysCtxt);
 #define   pztrtri_  pztrtri
 
 #define   pdsyevd_  pdsyevd
-#define   pdsyev_  pdsyev
+#define   pdsyev_   pdsyev
 #define   pdsyevx_  pdsyevx
 #define   pdsygvx_  pdsygvx
-#define   pzheev_  pzheev
+#define   pzheev_   pzheev
 #endif
 
 // tools
@@ -55,6 +56,8 @@ int numroc_(int* n, int* nb, int* iproc, int* isrcproc, int* nprocs);
 
 void Cpdgemr2d_(int m, int n, double *A, int IA, int JA, int *descA,
                double *B, int IB, int JB, int *descB, int gcontext);
+
+double pdlamch_(int* ictxt, char* cmach);
 
 // cholesky
 void pdpotrf_(char *uplo, int *n, double* a, int *ia, int* ja, int* desca,
@@ -128,7 +131,7 @@ PyObject* blacs_array(PyObject *self, PyObject *args)
 
   if (comm_obj == Py_None)
     {
-      desc[0] = 1;
+      desc[0] = 1;  // BLOCK_CYCLIC_2D
       desc[1] = -1; // Tells BLACS to ignore me.
       desc[2] = m;
       desc[3] = n;
@@ -156,7 +159,7 @@ PyObject* blacs_array(PyObject *self, PyObject *args)
 
       lld = numroc_(&m, &mb, &myrow, &rsrc, &nprow);
 
-      desc[0] = 1;
+      desc[0] = 1; // BLOCK_CYCLIC_2D
       desc[1] = ConTxt;
       desc[2] = m;
       desc[3] = n;
@@ -173,15 +176,37 @@ PyObject* blacs_array(PyObject *self, PyObject *args)
 
 PyObject* blacs_redist(PyObject *self, PyObject *args)
 {
-    PyArrayObject* a_obj;     //source matrix
-    PyArrayObject* adesc; //description vector
-    PyArrayObject* bdesc; //description vector
-    int m, n, ConTxt; 
-    if (!PyArg_ParseTuple(args, "OOOiii", &a_obj, &adesc, &bdesc, &m, &n, &ConTxt))
-      return NULL;
- 
+    PyArrayObject* a_obj; //source matrix
+    PyArrayObject* adesc; //source descriptor
+    PyArrayObject* bdesc; //destination descriptor
+    int m = 0;
+    int n = 0;
+    int ConTxt;
     static int one = 1;
 
+    if (!PyArg_ParseTuple(args, "OOO|ii", &a_obj, &adesc, &bdesc, &m, &n))
+      return NULL;
+
+    // adesc
+    int a_mycol = -1;
+    int a_myrow = -1;
+    int a_nprow, a_npcol; 
+    int a_ConTxt = INTP(adesc)[1];
+    int a_m = INTP(adesc)[2];
+    int a_n = INTP(adesc)[3];
+    int a_mb = INTP(adesc)[4];
+    int a_nb = INTP(adesc)[5];
+    int a_rsrc = INTP(adesc)[6];
+    int a_csrc = INTP(adesc)[7];
+
+    // If m and n not specified, redistribute all rows and columns of a.
+    if ((m == 0) | (n == 0))
+      {
+        m = a_m;
+        n = a_n;	  
+      }
+
+    // bdesc
     int b_mycol = -1;
     int b_myrow = -1;
     int b_nprow, b_npcol; 
@@ -193,8 +218,11 @@ PyObject* blacs_redist(PyObject *self, PyObject *args)
     int b_rsrc = INTP(bdesc)[6];
     int b_csrc = INTP(bdesc)[7];
 
+    // Get adesc and bdesc grid info
+    Cblacs_gridinfo_(a_ConTxt, &a_nprow, &a_npcol,&a_myrow, &a_mycol);
+
     Cblacs_gridinfo_(b_ConTxt, &b_nprow, &b_npcol,&b_myrow, &b_mycol);
-    // printf("b_ConTxt=%d,b_nprow=%d,b_npcol=%d,b_myrow=%d,b_mycol=%d",b_ConTxt,b_nprow,b_npcol,b_myrow,b_mycol);
+    // printf("b_ConTxt=%d,b_nprow=%d,b_npcol=%d,b_myrow=%d,b_mycol=%d\n",b_ConTxt,b_nprow,b_npcol,b_myrow,b_mycol);
 
     int b_locM = numroc_(&b_m, &b_mb, &b_myrow, &b_rsrc, &b_nprow);
     int b_locN = numroc_(&b_n, &b_nb, &b_mycol, &b_csrc, &b_npcol);
@@ -205,14 +233,144 @@ PyObject* blacs_redist(PyObject *self, PyObject *args)
     npy_intp b_dims[2] = {b_locM, b_locN};
     PyArrayObject* b_obj = (PyArrayObject*)PyArray_SimpleNew(2, b_dims, NPY_DOUBLE);
 
+
+    // Determine the largest grid because the ConTxt that is passed
+    // to Cpdgemr2d must encompass both grids (I think). The SCALAPACK
+    // documentation is not clear on this point. 
+    printf("a_nprow=%d,a_npcol=%d,b_nprow=%d,b_npcol=%d\n",a_nprow,a_npcol,b_nprow,b_npcol);
+    if ((a_nprow*a_npcol) > (b_nprow*b_npcol))
+      {
+        ConTxt = a_ConTxt;
+      }
+    else
+      {
+	ConTxt = b_ConTxt;
+      }
+
+
+    // It appears that the memory requirements for Cpdgemr2d are non-trivial.
+    // Consideer A_loc, B_loc to be the local piece of the global array. Then
+    // to perform this operation you will need an extra A_loc, B_loc worth of
+    // memory. Hence, for --state-parallelization=4 the memory savings are
+    // about nbands-by-nbands is exactly 1/4*(nbands-by-nbands). For --state-
+    // parallelization=8 it is about 3/4*(nbands-by-nbands). For --state-
+    // parallelization=B it is about (B-2)/B*(nbands-by-nbands).
     if (ConTxt != -1)
-    {
+      {
 	Cpdgemr2d_(m, n, DOUBLEP(a_obj), one, one, INTP(adesc), DOUBLEP(b_obj), one, one, INTP(bdesc), ConTxt);
 	return Py_BuildValue("O",b_obj);
-    }
+      }
     else
-    {
+      {
 	Py_RETURN_NONE;
-    }
+      }
+}
+PyObject* scalapack_diagonalize_dc(PyObject *self, PyObject *args)
+{
+    // Standard Driver for Divide and Conquer Algorithm
+    // Computes all eigenvalues and eigenvectors
+ 
+    PyArrayObject* a_obj; // symmetric matrix
+    PyArrayObject* adesc; // symmetric matrix description vector
+    int z_mycol = -1;
+    int z_myrow = -1;
+    int z_nprow, z_npcol;
+    int z_type, z_ConTxt, z_m, z_n, z_mb, z_nb, z_rsrc, z_csrc;
+    int zdesc[9];
+    char jobz = 'V'; // eigenvectors also
+    char uplo = 'U'; // work with upper
+    static int one = 1;
+
+    if (!PyArg_ParseTuple(args, "OO", &a_obj, &adesc))
+      return NULL;
+
+    // adesc
+    int a_type  = INTP(adesc)[0];
+    int a_ConTxt = INTP(adesc)[1];
+    int a_m = INTP(adesc)[2];
+    int a_n = INTP(adesc)[3];
+    int a_mb = INTP(adesc)[4];
+    int a_nb = INTP(adesc)[5];
+    int a_rsrc = INTP(adesc)[6];
+    int a_csrc = INTP(adesc)[7];
+
+    // Note that A is symmetric, so n = a_m = a_n;
+    // We do not test for that here.
+    int n = a_n;
+    if (a_ConTxt == -1) n = 0; // Eigenvalues end up on same tasks
+                               // as eigenvectors
+
+
+    // zdesc = adesc
+    // This is generally not required, as long as the 
+    // alignment properties are satisfied, see pdsyevd.f
+    // In the context of GPAW, don't see why zdesc would
+    // not be equal to adesc so I am just hard-coding it in.
+    z_type   = a_type;
+    z_ConTxt = a_ConTxt;
+    z_m      = a_m;
+    z_n      = a_n;
+    z_mb     = a_mb;
+    z_nb     = a_nb;
+    z_rsrc   = a_rsrc;
+    z_csrc   = a_csrc;
+    zdesc[0] = z_type;
+    zdesc[1] = z_ConTxt;
+    zdesc[2] = z_m;
+    zdesc[3] = z_n;
+    zdesc[4] = z_mb;
+    zdesc[5] = z_nb;
+    zdesc[6] = z_rsrc;
+    zdesc[7] = z_csrc;
+
+
+    Cblacs_gridinfo_(z_ConTxt, &z_nprow, &z_npcol,&z_myrow, &z_mycol);
+
+    int z_locM = numroc_(&z_m, &z_mb, &z_myrow, &z_rsrc, &z_nprow);
+    int z_locN = numroc_(&z_n, &z_nb, &z_mycol, &z_csrc, &z_npcol);
+    
+    if (z_locM < 0) z_locM = 0;
+    if (z_locN < 0) z_locN = 0;
+
+    // Eigenvectors
+    npy_intp z_dims[2] = {z_locM, z_locN};
+    PyArrayObject* z_obj = (PyArrayObject*)PyArray_SimpleNew(2, z_dims, NPY_DOUBLE);
+    
+    // Eigenvalues
+    npy_intp w_dims[1] = {n};
+    PyArrayObject* w_obj = (PyArrayObject*)PyArray_SimpleNew(1, w_dims, NPY_DOUBLE);
+
+    if (z_ConTxt != -1)
+      {
+        // Query part, need to find the optimal size of a number of work arrays
+	int info;
+        double* work;
+        work = GPAW_MALLOC(double, 3);
+        int querylwork = -1;
+        int* iwork;
+        iwork = GPAW_MALLOC(int, 1);
+        int queryliwork = 1;
+        pdsyevd_(&jobz, &uplo, &n, DOUBLEP(a_obj), &one, &one, INTP(adesc), 
+                 DOUBLEP(w_obj), DOUBLEP(z_obj), &one, &one, zdesc, 
+                 work, &querylwork, iwork, &queryliwork, &info);
+	// Computation part
+        int lwork = (int)work[0];
+        free(work);
+        int liwork = (int)iwork[0];
+        free(iwork);
+        work = GPAW_MALLOC(double, lwork);
+        iwork = GPAW_MALLOC(int, liwork);
+        pdsyevd_(&jobz, &uplo, &n, DOUBLEP(a_obj), &one, &one, INTP(adesc), 
+                 DOUBLEP(w_obj), DOUBLEP(z_obj), &one, &one, zdesc, 
+                 work, &lwork, iwork, &liwork, &info);
+
+        free(work);
+	free(iwork);
+        return Py_BuildValue("(OO)", w_obj, z_obj);
+      }
+    else
+      {
+	Py_RETURN_NONE;
+      }
 }
 #endif
