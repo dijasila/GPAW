f380633083ed9fa4f37bec20954411735964f3d7
naromero <naromero@6419b465-d902-0410-997c-be2266baf723>
2009-03-16 03:30:09 +0000

    Working blacs test.

--- test/parallel/bandpar3.py
+++ test/parallel/bandpar3.py
@@ -13,7 +13,7 @@ import _gpaw
 B = parsize_bands   # number of blocks
     
 G = 120  # number of grid points (G x G x G)
-N = 1000  # number of bands
+N = 16  # number of bands
 
 h = 0.2        # grid spacing
 a = h * G      # side length of box
@@ -105,56 +105,81 @@ def overlap(psit_mG, send_mG, recv_mG):
     domain_comm.sum(S_imm)
 
     # Blocks of matrix on each processor become one matrix.
-    # We can think of this as a 1D block matrix 
-    S_nm = np.zeros((0,0)); # Otherwise, blacs_redist will complain of UnboundLocalError
+    # We can think of this as a 1D block matrix
     if (scalapack0_comm):
         S_nm = S_imm.reshape(N,M)
+    else:
+        S_nm = None
     del S_imm
 
     # Test
+    # Step 0 - Initialize distributed Matrix to None;
+    # Otherwise, scalapack_redist will complain of UnboundLocalError
     if world.rank == 0: print "before redist"
+    H_nm = None 
+    S_nm = None 
 
-    # Create a simple matrix, diagonal elements 
-    # will equal ranks
+    # Step 1 - Fill matrices with values and convert to Fortran order 
+    # Create a simple matrix H: diagonal elements which equal ranks
+    # Create a simple overlap matrix: unity
     if (scalapack0_comm):
-        S_nm = scalapack0_comm.rank*np.eye(N,M,-M*scalapack0_comm.rank)
+        H_nm = scalapack0_comm.rank*np.eye(N,M,-M*scalapack0_comm.rank)
+        H_nm = H_nm.copy("Fortran") # Fortran order required for ScaLAPACK
+        S_nm = np.eye(N,M,-M*scalapack0_comm.rank)
         S_nm = S_nm.copy("Fortran") # Fortran order required for ScaLAPACK
-        print scalapack0_comm.rank, S_nm
+        print scalapack0_comm.rank, H_nm
 
-    # Desc for S_nm
-    desc0 = _gpaw.blacs_array(scalapack0_comm,N,N,1,B,N,M)
+    # Step 2 - Create descriptor for distributed arrays.
+    # Desc for H_nm : 1D grid
+    desc0 = _gpaw.blacs_create(scalapack0_comm,N,N,1,B,N,M)
+    # Desc for H_mm : 2D grid
+    desc1 = _gpaw.blacs_create(scalapack1_comm,N,N,B,B,4,4)
 
-    # Desc for S_mm
-    desc1 = _gpaw.blacs_array(scalapack1_comm,N,N,B,B,64,64)
+    # Step 3 - Redistribute from 1D -> 2D grid
+    # This is mostly for performance reasons
+    H_mm = _gpaw.scalapack_redist(H_nm,desc0,desc1)
+    S_mm = _gpaw.scalapack_redist(S_nm,desc0,desc1)
 
-    #  Debugging only
-    #  if (scalapack0_comm):
-    #     print 'desc0 =', desc0
-    #     print 'desc1 =', desc1
+    # Debug
+    # if (scalapack1_comm):
+    #             print "H_mm Fortran order =", H_mm.flags.f_contiguous
+                
+    # if world.rank == 0: print 'redistributed array'
+    # if (scalapack1_comm):
+    #     print scalapack1_comm.rank, H_mm, S_mm
+
+    # Step 4 - Call ScaLAPACK diagonalize
+    # Call scalapack diagonalize D&C
+    W, Z_mm  = _gpaw.scalapack_diagonalize_dc(H_mm, desc1)
+
+    # Call ScaLAPACK general diagonalize
+    # W, Z_mm = _gpaw.scalapack_general_diagonalize(H_mm, S_mm, desc1)
+
+    # Debug
+    # Eigenvectors
+    # if (scalapack1_comm):
+        #     print "Z_mm Fortran order =", Z_mm.flags.f_contiguous
+
+    # Step 5 - Redistribute from 2D -> 1D grid
+    # Copy from Z_mm -> H_nm, we use arrays whenever possible
+    # Note that W is still only on scalapack1_comm, i.e. desc1, and
+    # contains *all* eigenvalues
+    H_nm = _gpaw.scalapack_redist(Z_mm,desc1,desc0)
+
+    # Debug
+    # if (scalapack0_comm):
+    #     print "H_nm Fortran order =", H_nm.flags.f_contiguous
         
-    # Redistribute from S_nm -> S_mm
-    S_mm = _gpaw.blacs_redist(S_nm,desc0,desc1)
+    if world.rank == 0: print 'result in original distribution'
 
-    if world.rank == 0: print 'redistributed array'
-    if (scalapack1_comm):
-        print scalapack1_comm.rank, S_mm
-        
-    # Call new scalapack diagonalize
-    W, Z_mm  = _gpaw.scalapack_diagonalize_dc(S_mm, desc1)
-
-    if (scalapack1_comm):
-        print scalapack1_comm.rank, Z_mm
-
-    # Get the eigevenctors from Z_mm and redistribute to S_nm
-    # Copy from Z_mm -> Z_nm 
-    S_nm = _gpaw.blacs_redist(Z_mm,desc1,desc0)
-
-    # if world.rank == 0: print 'restore original array'
+    # Step 6 - Convert to C array for general use in GPAW.
     if (scalapack0_comm):
-        S_nm = S_nm.copy("C") # Convert to C array, which is default for NumPy
-        print scalapack0_comm.rank, W
-        print scalapack0_comm.rank, S_nm
+        H_nm = H_nm.copy("C")
+        print scalapack0_comm.rank, W 
+        print scalapack0_comm.rank, H_nm
 
+    _gpaw.blacs_destroy(desc0)
+    _gpaw.blacs_destroy(desc1)
 
 def matrix_multiply(C_nn, psit_mG, send_mG, recv_mG):
     """Calculate new linear compination of wave functions."""
