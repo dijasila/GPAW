5c8c1c9948311edf9cb35f4a92354841b14e522d
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2011-04-28 10:23:32 +0000

    Run 7x7x7 calculation on only 4 cores.

--- doc/tutorials/pbe0/submit.agts.py
+++ doc/tutorials/pbe0/submit.agts.py
@@ -4,13 +4,13 @@ def agts(queue):
     queue.add('bs_plot.py', deps=bs, creates='bs-PBE.png')
 
     w = queue.add('wannier.py', deps=si, ncpus=1, walltime=60)
-    bs = queue.add('bs_pbe0.py', deps=w, ncpus=1, walltime=60)
+    bs = queue.add('bs_pbe0.py', deps=w, ncpus=1, walltime=10 * 60)
     queue.add('bs_plot2.py', deps=bs, creates='bs-PBE0.png')
 
     deps = []
-    for k in range(3, 7):
+    for k in range(3, 8):
         deps.append(queue.add('eos.py %d' % k, ncpus=4, walltime=10 * 60))
-    for k in range(7, 13):
+    for k in range(8, 13):
         deps.append(queue.add('eos.py %d' % k, ncpus=16, walltime=10 * 60))
     queue.add('results.py', deps=deps, creates=['a.png', 'B.png'])
 
