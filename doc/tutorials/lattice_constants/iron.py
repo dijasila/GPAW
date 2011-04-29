8738587b16293e8bf7da44ede2ad327f09bd94e3
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2011-04-29 13:49:47 +0000

    Test Methfessel-Paxton also.

--- doc/tutorials/lattice_constants/iron.py
+++ doc/tutorials/lattice_constants/iron.py
@@ -6,8 +6,8 @@ from gpaw.utilities.bulk2 import GPAWRunner
 
 strains = np.linspace(0.98, 1.02, 9)
 a0 = 2.84
-atoms = bulk('Fe', 'bcc', a0)
-atoms.set_initial_magnetic_moments([2.3])
+atoms = bulk('Fe', 'bcc', a0, cubic=True)
+atoms.set_initial_magnetic_moments([2.3, 2.3])
 def f(name, dist, k, g):
     tag = '%s-%02d-%2d' % (name, k, g)
     r = GPAWRunner('Fe', atoms, strains, tag=tag)
@@ -29,5 +29,6 @@ for width in [0.05, 0.1, 0.15, 0.2]:
     for k in [4, 6, 8, 10, 12]:
         f('FD-%.2f' % width, FermiDirac(width), k, 12)
         f('MP-%.2f' % width, MethfesselPaxton(width), k, 12)
-for g in range(8, 32, 4):
+        f('MP1-%.2f' % width, MethfesselPaxton(width, 1), k, 12)
+for g in range(16, 32, 4):
     f('FD-%.2f' % 0.1, FermiDirac(0.1), 8, g)
