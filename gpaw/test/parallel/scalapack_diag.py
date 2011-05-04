1ef1ea5aeb39ec4c12f04e30fea5ca4ae23740d9
juya <juya@6419b465-d902-0410-997c-be2266baf723>
2011-05-04 08:18:23 +0000

    update scalapack test

--- gpaw/test/parallel/scalapack_diag.py
+++ gpaw/test/parallel/scalapack_diag.py
@@ -5,59 +5,74 @@ from gpaw.utilities.lapack import diagonalize
 from gpaw.utilities.scalapack import scalapack_diagonalize_dc
 from gpaw.blacs import Redistributor
 
-# This test performs diagonalization of a matrix, originally distributed to all the processors, by scalapack
 
+def scal_diagonalize(A, nodes='master'):
+    # Diagonalize matrix A (size N*N) with scalapack
+    # Usage: eps, B = scal_diagonalize(A)
+    # eps and B and the eigenvalues and eigenvectors
+    # nodes = 'master': eigenvectors only available on master node
+    # nodes = 'all': eigenvectors broadcast to all nodes
 
+    # make sure A is N*N, and hermitian
+    N = A.shape[0]
+    assert A.shape[0] == A.shape[1]
+    for i in range(N):
+        for j in range(i, N):
+            assert A[i,j] == A[j,i].conj()
+
+    # create blacs descriptor
+    mb = 64
+    g = BlacsGrid(world, 2, size//2)
+    nndesc1 = g.new_descriptor(N, N, N,  N) 
+    nndesc2 = g.new_descriptor(N, N, mb, mb)
+
+    # distribute A to blacs grid A_
+    if rank != 0:
+        A = nndesc1.zeros()
+    A_ = nndesc2.empty(dtype=float)
+    redistributor = Redistributor(world, nndesc1, nndesc2)
+    redistributor.redistribute(A, A_)
+
+    # diagonalize
+    B_ = nndesc2.zeros()
+    eps = np.zeros(N)
+    nndesc2.diagonalize_dc(A_, B_, eps, 'L')
+
+    # distribute the eigenvectors to master
+    B = np.zeros_like(A)
+    redistributor = Redistributor(world, nndesc2, nndesc1)
+    redistributor.redistribute(B_, B)
+
+    if nodes == 'master':
+        return eps, B
+    elif nodes == 'all':
+        if rank != 0:
+            B = np.zeros((N, N))
+        world.broadcast(B, 0)
+        return eps, B
+    
+
+# generate a matrix
 N = 512
-assert N % size == 0
-# Create a matrix, hermitian
 A = np.arange(N**2,dtype=float).reshape(N,N)
 for i in range(N):
     for j in range(i,N):
         A[i,j] = A[j,i]
 
-# Distribute the matrix to all the processors
-# In reality, A_local is the matrix that is stored and need to be diagonalized.
-N_start = rank * N // size
-N_end = (rank + 1) * N // size
-A_local = A[N_start:N_end, :]
-
-# Create Blacs grid, g1 has the same shape and structure for A_local matrix
-# g2 is a new Blacs grid that used for diagonalizing the A matrix
-mb = 16
-g1 = BlacsGrid(world, size, 1)
-g2 = BlacsGrid(world, 2, size//2)
-nndesc1 = g1.new_descriptor(N, N, N//size,N)
-nndesc2 = g2.new_descriptor(N, N, mb, mb)
-
-# Distribute A to accomodate blacs grid g2
-B = nndesc2.empty(dtype=float)
-redistributor = Redistributor(world, nndesc1, nndesc2)
-redistributor.redistribute(A_local, B)
-
-# Diagonalize, C is the eigenvectors and eps is the eigenvalues
-C = nndesc2.zeros()
-eps = np.zeros(N)
-nndesc2.diagonalize_dc(B, C, eps, 'L')
-
-# After the diagonalization, the eigenvectors C needs to redistribute to the shape of the original A_local matrix
-C_local = np.zeros_like(A_local)
-redistributor = Redistributor(world, nndesc2, nndesc1)
-redistributor.redistribute(C, C_local)
-
-# make sure that the scalapack gives the same result as the lapack
-eps1 = np.zeros(N)
-diagonalize(A,eps1)
-A_local =  A[N_start:N_end, :]
-assert np.abs(eps - eps1).sum() < 1e-6
-for i in range(N//size):
-    # the eigenvectors are row of the matrix, it can be differ by a minus sign.
-    if np.abs(A_local[i,:] - C_local[i,:]).sum() > 1e-6:
-        if np.abs(A_local[i,:] + C_local[i,:]).sum() > 1e-6:
-            raise ValueError('Check !')
-        
+# diagonalize
+eps, B = scal_diagonalize(A)
 
 
-
-
+check = 1
+if check and rank == 0:
+# check whether it gives the same result with lapack
+    eps1 = np.zeros(N)
+    diagonalize(A, eps1)
+    assert np.abs(eps-eps1).sum() < 1e-6
+    
+    for i in range(N//size):
+        # the eigenvectors are row of the matrix, it can be differ by a minus sign.
+        if np.abs(A[i,:] - B[i,:]).sum() > 1e-6:
+            if np.abs(A[i,:] + B[i,:]).sum() > 1e-6:
+                raise ValueError('Check !')
 
