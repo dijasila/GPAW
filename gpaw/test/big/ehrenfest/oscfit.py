f359b861b8b8f0953332d57c87132cb2d53648df
s032082 <s032082@6419b465-d902-0410-997c-be2266baf723>
2011-11-15 18:12:31 +0000

    Another Ehrenfest AGTS test update. Implemented quick check for energy conservation and internal consistency of positions, velocities and forces within the Velocity Verlet method.

--- gpaw/test/big/ehrenfest/oscfit.py
+++ gpaw/test/big/ehrenfest/oscfit.py
@@ -12,6 +12,7 @@ except (ImportError, RuntimeError):
     mpl = None
 
 from scipy.optimize import leastsq
+from ase.units import second
 from ase.io.trajectory import PickleTrajectory
 
 # Dimer oscillation model used for least-squares fit 
@@ -38,22 +39,44 @@ for name in ['h2_osc', 'n2_osc', 'na2_osc']:
 
     # Read dimer bond length time series from trajectory file
     traj = PickleTrajectory(name + '_td.traj', 'r')
-    symbol = traj[0].get_name()
     nframes = len(traj)
-    t = np.empty(nframes)
-    d = np.empty(nframes)
+    natoms = len(traj[0])
+    symbol = traj[0].get_name()
+    t_i = m.timestep * m.ndiv * np.arange(nframes)
+    Ekin_i, Epot_i = np.empty(nframes), np.empty(nframes)
+    R_iav = np.empty((nframes, natoms, 3))
+    V_iav = np.empty((nframes, natoms, 3))
+    A_iav = np.empty((nframes, natoms, 3))
     for i in range(nframes):
-        pos_av = traj[i].get_positions()
-        t[i] = m.timestep * m.ndiv * i
-        d[i] = np.sum((pos_av[1] - pos_av[0])**2)**0.5
+        Ekin_i[i] = traj[i].get_kinetic_energy()
+        Epot_i[i] = traj[i].get_potential_energy()
+        R_iav[i] = traj[i].get_positions()
+        V_iav[i] = traj[i].get_velocities()
+        A_iav[i] = traj[i].get_forces() / traj[i].get_masses()[:,np.newaxis]
     print 'Read %d frames from trajectory...' % nframes
-    #assert nframes * ndiv == niter, (nframes, ndiv, niter) #TODO uncomment
+    assert nframes * m.ndiv == m.niter, (nframes, m.ndiv, m.niter)
     traj.close()
 
+    # Verify that energy was conserved
+    dEstd = np.std(Ekin_i + Epot_i)
+    dEmax = np.max(Ekin_i.ptp(), Epot_i.ptp())
+    assert dEstd < 1e-2 * dEmax + 1e-6, (dEstd, dEmax)
+
+    # Compare position, velocity and force time series using Velocity Verlet
+    dt = m.timestep * m.ndiv * 1e-18 * second
+    Rn_iav = R_iav[:-1] + V_iav[:-1] * dt + 0.5 * A_iav[:-1] * dt**2
+    Vn_iav = V_iav[:-1] + 0.5 * (A_iav[:-1] + A_iav[1:]) * dt
+    dRstd_av = (np.sum((Rn_iav - R_iav[1:])**2, axis=0) / len(Rn_iav))**0.5
+    dVstd_av = (np.sum((Vn_iav - V_iav[1:])**2, axis=0) / len(Vn_iav))**0.5
+    dRmax_av, dVmax_av = R_iav.ptp(axis=0), V_iav.ptp(axis=0)
+    assert np.all(dRstd_av < 1e-4 * dRmax_av + 1e-12), (dRstd_av, dRmax_av)
+    assert np.all(dVstd_av < 1e-4 * dVmax_av + 1e-12), (dVstd_av, dVmax_av)
+
     # Fit model to time series using imported parameters as an initial guess
+    d_i = np.sum((R_iav[:,1] - R_iav[:,0])**2, axis=-1)**0.5
     p0 = (m.d_disp, 2 * np.pi / m.period, -m.timestep * m.ndiv, m.d_bond)
-    p, cov, info, msg, status = leastsq(lambda p: f(p, t) - d, p0, \
-        Dfun=lambda p: Df(p,t), col_deriv=True, full_output=True)
+    p, cov, info, msg, status = leastsq(lambda p: f(p, t_i) - d_i, p0, \
+        Dfun=lambda p: Df(p, t_i), col_deriv=True, full_output=True)
     print 'leastsq returned %d: %s' % (status, msg.replace('\n ',''))
     print 'p0=', np.asarray(p0)
     print 'p =', p
@@ -66,7 +89,7 @@ for name in ['h2_osc', 'n2_osc', 'na2_osc']:
 
     if mpl:
         fig = Figure()
-        ax = fig.add_axes([0.1, 0.1, 0.87, 0.83])
+        ax = fig.add_axes([0.11, 0.1, 0.86, 0.83])
         raw = r',\;'.join([r'T=%.2f\mathrm{\,fs}',
                            r'T_\mathrm{ref}=%.2f\mathrm{\,fs}',
                            r'\eta=%.2f\,\%%'])
@@ -74,7 +97,7 @@ for name in ['h2_osc', 'n2_osc', 'na2_osc']:
         ax.set_title(symbol + ' ($' + mathmode + '$)')
         ax.set_xlabel('Time [fs]')
         ax.set_ylabel('Dimer bond length [Ang]')
-        ax.plot(t * 1e-3, d, '-b', t * 1e-3, f(p,t), '--k')
+        ax.plot(t_i * 1e-3, d_i, '-b', t_i * 1e-3, f(p, t_i), '--k')
         ax.legend(('Ehrenfest data', r'$A\,\mathrm{cos}(\omega(t-t_0))+B$'))
         FigureCanvasAgg(fig).print_figure(name + '.png', dpi=90)
 
