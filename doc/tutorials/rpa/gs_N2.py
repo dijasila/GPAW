7e166bf9d3ba2fa8320824272abe4936085e3572
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2012-02-29 14:58:20 +0000

    optimize no longer in ase namespace.

--- doc/tutorials/rpa/gs_N2.py
+++ doc/tutorials/rpa/gs_N2.py
@@ -1,15 +1,14 @@
-from ase import *
+from ase.optimize import BFGS
 from ase.structure import molecule
 from ase.parallel import paropen
-from gpaw import *
-from gpaw.wavefunctions.pw import PW
+from gpaw import GPAW, PW
 from gpaw.xc.hybridk import HybridXC
 
 # N -------------------------------------------
 
 N = molecule('N')
-N.set_pbc(True)
-N.set_cell((6., 6., 7.))
+N.pbc = True
+N.cell = (6.0, 6.0, 7.0)
 N.center()
 calc = GPAW(mode=PW(500),
             dtype=complex,
@@ -20,8 +19,7 @@ calc = GPAW(mode=PW(500),
             txt='N.txt',
             convergence={'density': 1.e-6})
 
-N.set_calculator(calc)
-N.set_calculator(calc)
+N.calc = calc
 E1_pbe = N.get_potential_energy()
 E1_hf = E1_pbe + calc.get_xc_difference(HybridXC('EXX'))
 
@@ -31,8 +29,8 @@ calc.write('N.gpw', mode='all')
 # N2 ------------------------------------------
 
 N2 = molecule('N2')
-N2.set_pbc(True)
-N2.set_cell((6., 6., 7.))
+N2.pbc = True
+N2.cell = (6.0, 6.0, 7.0)
 N2.center()
 calc = GPAW(mode=PW(500),
             dtype=complex,
@@ -41,14 +39,14 @@ calc = GPAW(mode=PW(500),
             txt='N2.txt',
             convergence={'density': 1.e-6})
 
-N2.set_calculator(calc)
-dyn = optimize.BFGS(N2)
+N2.calc = calc
+dyn = BFGS(N2)
 dyn.run(fmax=0.05)
 E2_pbe = N2.get_potential_energy()
 E2_hf = E2_pbe + calc.get_xc_difference(HybridXC('EXX'))
 f = paropen('PBE_HF.dat', 'w')
-print >> f, 'PBE: ', E2_pbe - 2*E1_pbe
-print >> f, 'HF: ', E2_hf - 2*E1_hf
+print >> f, 'PBE: ', E2_pbe - 2 * E1_pbe
+print >> f, 'HF: ', E2_hf - 2 * E1_hf
 f.close()
 
 calc.diagonalize_full_hamiltonian(nbands=4800)
