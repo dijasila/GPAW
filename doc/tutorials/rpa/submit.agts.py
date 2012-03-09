d031ffc984a2ac6b71c3a85c7751f82f32326f07
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2012-03-09 07:24:32 +0000

    con_freq.py needs more time.

--- doc/tutorials/rpa/submit.agts.py
+++ doc/tutorials/rpa/submit.agts.py
@@ -1,7 +1,7 @@
 def agts(queue):
     gs_N2 = queue.add('gs_N2.py', ncpus=1, walltime=200)
     w = queue.add('frequency.py', deps=gs_N2, walltime=200)
-    f = queue.add('con_freq.py', deps=gs_N2, walltime=200)
+    f = queue.add('con_freq.py', deps=gs_N2, walltime=400)
     rpa_N2 = queue.add('rpa_N2.py', deps=gs_N2, ncpus=16, walltime=600)
     queue.add('plot_w.py', deps=w, creates='E_w.png')
     queue.add('plot_con_freq.py', deps=f, creates='con_freq.png')
