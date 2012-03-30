a4cb154bbe83faad317b70045b4a92ea8a7af80a
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2012-03-30 13:40:30 +0000

    Undo part of last commit.

--- gpaw/test/big/exx/bulk.py
+++ gpaw/test/big/exx/bulk.py
@@ -6,8 +6,8 @@ from gpaw import PW
 from gpaw.factory import GPAWFactory
 
 
-for ecut in [340]:
-    for k in [6]:
+for ecut in [400, 500, 600]:
+    for k in [10, 12, 14]:
         kpts = monkhorst_pack((k, k, k)) + 0.5 / k
         task = EXXBulkTask(
             calcfactory=GPAWFactory(xc='PBE', 
