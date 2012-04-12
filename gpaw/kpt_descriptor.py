7fd66bf01a3dfab324789af8043cea66c0542cab
juya <juya@6419b465-d902-0410-997c-be2266baf723>
2012-04-12 01:13:00 +0000

    force kpoint -0.5 to be 0.5

--- gpaw/kpt_descriptor.py
+++ gpaw/kpt_descriptor.py
@@ -357,6 +357,9 @@ class KPointDescriptor:
         shift_c = 0.5 * ((self.N_c + 1) % 2) / self.N_c
         bzq_qc = monkhorst_pack(self.N_c) + shift_c
         to1bz(bzq_qc, self.cell_cv)
+        bzq_qc[np.where(bzq_qc>0.501)] -= 1
+        bzq_qc[np.where(bzq_qc<-0.499)] += 1
+        
         return bzq_qc
 
     def get_ibz_q_points(self, bzq_qc, op_scc):
