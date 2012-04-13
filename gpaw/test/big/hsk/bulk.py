16059763f0ddcb237a0d6103ae007faf8e064a1e
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2012-04-13 10:56:06 +0000

    More work on bulk test.

--- gpaw/test/big/hsk/bulk.py
+++ gpaw/test/big/hsk/bulk.py
@@ -1,18 +1,22 @@
-from ase.dft.kpoints import monkhorst_pack
+import sys
 
-from gpaw.test.big.exx.hsk import HarlSchimkaKresseEXXBulkTask as EXXBulkTask
+from ase.dft.kpoints import monkhorst_pack
 
 from gpaw import PW
 from gpaw.factory import GPAWFactory
+from gpaw.test.big.hsk.hsk import HarlSchimkaKressePBEBulkTask as PBEBulkTask
 
+
+setups = sys.argv[1]
 
 for ecut in [400, 500, 600]:
     for k in [10, 12, 14]:
         kpts = monkhorst_pack((k, k, k)) + 0.5 / k
-        task = EXXBulkTask(
+        task = PBEBulkTask(
             calcfactory=GPAWFactory(xc='PBE', 
                                     mode=PW(ecut),
+                                    setups=setups,
                                     kpts=kpts),
-            tag='%d-%d' % (ecut, k),
+            tag='%s-%d-%d' % (setups, ecut, k),
             use_lock_files=True)
         task.run()
