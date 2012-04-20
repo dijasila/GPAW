d4858fd37f325043812ae30b0603cfd025b6b0c1
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2012-04-20 07:18:04 +0000

    more time needed

--- gpaw/test/big/setups/g2_1/fix/pbe_gpaw_nrel08.agts.py
+++ gpaw/test/big/setups/g2_1/fix/pbe_gpaw_nrel08.agts.py
@@ -3,21 +3,24 @@ def agts(queue):
     run_generate = queue.add(tag + '_generate.py',
                              queueopts='-l nodes=1:ppn=4:ethernet',
                              ncpus=1,walltime=20, deps=[])
-    run_special = queue.add(tag + '_run_special.py',
-                            queueopts='-l nodes=1:ppn=4:ethernet',
-                            ncpus=1,walltime=10*60, deps=[run_generate])
+    run_special1 = queue.add(tag + '_run_special.py',
+                             queueopts='-l nodes=1:ppn=4:ethernet',
+                             ncpus=1,walltime=10*60, deps=[run_generate])
+    run_special2 = queue.add(tag + '_run_special.py',
+                             queueopts='-l nodes=1:ppn=4:ethernet',
+                             ncpus=1,walltime=10*60, deps=[run_generate])
     run1 = queue.add(tag +  '_run.py',
                      queueopts='-l nodes=1:ppn=4:opteron:ethernet',
-                     ncpus=1,walltime=20*60, deps=[run_special])
+                     ncpus=1,walltime=20*60, deps=[run_special1, run_special2])
     run2 = queue.add(tag + '_run.py',
                      queueopts='-l nodes=1:ppn=4:opteron:ethernet',
-                     ncpus=1,walltime=20*60, deps=[run_special])
+                     ncpus=1,walltime=20*60, deps=[run_special1, run_special2])
     run3 = queue.add(tag + '_run.py',
                      queueopts='-l nodes=1:ppn=4:opteron:ethernet',
-                     ncpus=1,walltime=20*60, deps=[run_special])
+                     ncpus=1,walltime=20*60, deps=[run_special1, run_special2])
     run4 = queue.add(tag + '_run.py',
                      queueopts='-l nodes=1:ppn=4:opteron:ethernet',
-                     ncpus=1,walltime=20*60, deps=[run_special])
+                     ncpus=1,walltime=20*60, deps=[run_special1, run_special2])
     analyse = queue.add(tag + '_analyse.py',
                         queueopts='-l nodes=1:ppn=1',
                         ncpus=1, walltime=5, deps=[run1, run2, run3, run4],
