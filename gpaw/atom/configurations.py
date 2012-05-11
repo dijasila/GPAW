2a0aa61f352796d18bf9ad836704fc4b76048e7f
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2012-05-11 10:11:58 +0000

    Rn cutoffs by Marcin added. At least a setup is produced now.

--- gpaw/atom/configurations.py
+++ gpaw/atom/configurations.py
@@ -1086,11 +1086,11 @@ parameters = {
 #'Hg': Missing
 #'Tl': Missing
  'Pb': {'core': '[Xe]4f', 'rcut': [2.4, 2.6, 2.4]},
- 'Bi': {'core': '[Xe]4f', 'rcut': [2.2, 2.4, 2.2]}
+ 'Bi': {'core': '[Xe]4f', 'rcut': [2.2, 2.4, 2.2]},
 #'Bi': Missing
 #'Po': Missing
 #'At': Missing
-#'Rn': Missing
+ 'Rn': {'core': '[Xe]4f5d', 'rcut': [2.13, 2.29, 2.31]}
  }
 
 # Extra setups
