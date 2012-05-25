5ae37a1e47de32906776106fd1264a6ddf9929d3
jesswe <jesswe@6419b465-d902-0410-997c-be2266baf723>
2012-05-25 13:15:17 +0000

    funcs_mgga.c util.h xc_funcs.h libxc_funcs.f90 mgga.c xc.h Makefile.am mgga_x_mbeef.c

--- c/libxc/src/xc.h
+++ c/libxc/src/xc.h
@@ -162,6 +162,8 @@ FLOAT XC(hyb_gga_exx_coef)   (XC(hyb_gga_type) *p);
 #define XC_MGGA_X_REVTPSS        205
 #define XC_MGGA_C_REVTPSS        206 
 
+#define XC_MGGA_X_MBEEF       207
+
 typedef struct{
   const XC(func_info_type) *info;  /* which functional did we chose   */
   int nspin;                       /* XC_UNPOLARIZED or XC_POLARIZED  */
