d5fd0df0ab4e14f7c4ca372fd4ad905e75a91679
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2012-06-11 12:30:21 +0000

    Test PWLFC.stress_tensor_contribution() function some more.

--- gpaw/test/pw/stresstest.py
+++ gpaw/test/pw/stresstest.py
@@ -12,11 +12,12 @@ r = np.linspace(0, rc, 100)
 
 n = 40
 a = 8.0
-gd = GridDescriptor((n, n, n), (a, a, a), comm=mpi.serial_comm)
+cell_cv = np.array([[a, 0.5, -1], [0, a, 2], [-1, 0, a + 1]])
+gd = GridDescriptor((n, n, n), cell_cv, comm=mpi.serial_comm)
 
 a_R = gd.empty()
-z = np.linspace(0, a, n, endpoint=False)
-a_R[:] = 2 + np.sin(2 * np.pi * z / a)
+z = np.linspace(0, n, n, endpoint=False)
+a_R[:] = 2 + np.sin(2 * np.pi * z / n)
 
 spos_ac = np.array([(0.15, 0.45, 0.95)])
 
@@ -25,29 +26,40 @@ a_G = pd.fft(a_R)
 
 s = Spline(0, rc, 2 * x**1.5 / np.pi * np.exp(-x * r**2))
 p = Spline(1, rc, 2 * x**1.5 / np.pi * np.exp(-x * r**2))
+d = Spline(2, rc, 2 * x**1.5 / np.pi * np.exp(-x * r**2))
 
-lfc = PWLFC([[s, p]], pd)
+lfc = PWLFC([[s, p, d]], pd)
 lfc.set_positions(spos_ac)
-b_LG = pd.zeros(4)
-lfc.add(b_LG, {0: np.eye(4)})
+b_LG = pd.zeros(9)
+lfc.add(b_LG, {0: np.eye(9)})
 e1 = pd.integrate(a_G, b_LG)
 assert abs(lfc.integrate(a_G)[0] - e1).max() < 1e-11
 
 s1 = []
-for i in range(4):
-    x = [0, 0, 0, 0]
+for i in range(9):
+    x = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     x[i] = 1
-    s1.append(-3 * e1[i] + lfc.stress_tensor_contribution(a_G, {0: x}).trace())
+    s1.append(lfc.stress_tensor_contribution(a_G, {0: x}) -
+              np.eye(3) * e1[i])
 
-eps = 1e-6
-a *= 1 + eps
-gd = GridDescriptor((n, n, n), (a, a, a), comm=mpi.serial_comm)
-
-pd = PWDescriptor(45, gd)
-a_G /= (1 + eps)**3
-lfc = PWLFC([[s, p]], pd)
-lfc.set_positions(spos_ac)
-b_LG = pd.zeros(4)
-lfc.add(b_LG, {0: np.eye(4)})
-e2 = pd.integrate(a_G, b_LG)
-assert abs((e2 - e1) / eps - s1).max() < 2e-5
+x = 1e-6
+for dist in [[[x, 0, 0], [0, 0, 0], [0, 0, 0]],
+             [[0, 0, 0], [0, x, 0], [0, 0, 0]],
+             [[0, 0, 0], [0, 0, 0], [0, 0, x]],
+             [[0, x, 0], [x, 0, 0], [0, 0, 0]],
+             [[0, 0, x], [0, 0, 0], [x, 0, 0]],
+             [[0, 0, 0], [0, 0, x], [0, x, 0]]]:
+    e = dist + np.eye(3)
+    c_cv = np.dot(cell_cv, e)
+    gd = GridDescriptor((n, n, n), c_cv, comm=mpi.serial_comm)
+    pd = PWDescriptor(45, gd)
+    aa_G = a_G / np.linalg.det(e)
+    lfc = PWLFC([[s, p, d]], pd)
+    lfc.set_positions(spos_ac)
+    b_LG = pd.zeros(9)
+    lfc.add(b_LG, {0: np.eye(9)})
+    e2 = pd.integrate(aa_G, b_LG)
+    s2 = (e2 - e1) / x
+    error = (np.array(s1) * dist).sum(1).sum(1) / x - s2
+    print s2, abs(error).max()
+    assert abs(error).max() < 2e-6
