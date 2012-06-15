1e4f25724ca319949d22056643970d26c6b665e6
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2012-06-15 13:15:46 +0000

    changing niflheim and agts tests

--- tools/niflheim-agts.py
+++ tools/niflheim-agts.py
@@ -1,22 +1,58 @@
 import os
+import sys
 import glob
+import shutil
 import subprocess
-
-from gpaw.test.big.agts import AGTSQueue
-from gpaw.test.big.niflheim import NiflheimCluster
-
 
 def cmd(c):
     x = os.system(c)
     assert x == 0, c
 
+def fail(subject, email=None, filename='/dev/null'):
+    import os
+    if email is not None:
+        assert os.system('uuencode %s %s | mail -s "%s" %s' %
+                         (filename, os.path.basename(filename),
+                          subject, email)) == 0
+    raise SystemExit
 
-os.chdir('agts')
+if '--dir' in sys.argv:
+    i = sys.argv.index('--dir')
+    dir = sys.argv[i+1]
+else:
+    dir = 'agts'
+
+if '--email' in sys.argv:
+    i = sys.argv.index('--email')
+    email = sys.argv[i+1]
+else:
+    email = None
+
+assert os.path.isdir(dir)
+
+gpawdir = os.path.join(dir, 'gpaw')
+
+# remove the old run directory
+if os.path.isdir(dir):
+    shutil.rmtree(dir)
+
+os.mkdir(dir)
+os.chdir(dir)
 
 cmd('svn checkout https://svn.fysik.dtu.dk/projects/gpaw/trunk gpaw')
 
-cmd("""echo "\
-cd agts/gpaw&& \
+# a version of gpaw is needed for imports from within this script!
+cmd("\
+cd " + gpawdir + "&& \
+source /home/camp/modulefiles.sh&& \
+module load NUMPY&& \
+python setup.py build_ext 2>&1 | tee build_ext.log")
+
+# import gpaw from where it was installed
+sys.path.insert(0, gpawdir)
+
+cmd("echo '\
+cd '" + gpawdir + "'&& \
 source /home/camp/modulefiles.sh&& \
 module load NUMPY&& \
 module load open64/4.2.3-0 && \
@@ -24,16 +60,16 @@ module load openmpi/1.3.3-1.el5.fys.open64.4.2.3 && \
 module load hdf5/1.8.6-5.el5.fys.open64.4.2.3.openmpi.1.3.3 && \
 python setup.py --remove-default-flags --customize=\
 doc/install/Linux/Niflheim/el5-xeon-open64-acml-4.4.0-acml-4.4.0-hdf-SL-2.0.1.py \
-build_ext" | ssh thul bash""")
+build_ext' | ssh thul bash")
 
-cmd("""echo "\
-cd agts/gpaw&& \
+cmd("echo '\
+cd '" + gpawdir + "'&& \
 source /home/camp/modulefiles.sh&& \
 module load NUMPY&& \
 module load open64/4.2.3-0 && \
 python setup.py --remove-default-flags --customize=\
 doc/install/Linux/Niflheim/el5-opteron-open64-acml-4.4.0-acml-4.4.0-hdf-SL-2.0.1.py \
-build_ext" | ssh fjorm bash""")
+build_ext' | ssh fjorm bash")
 
 cmd("""wget --no-check-certificate --quiet \
 http://wiki.fysik.dtu.dk/gpaw-files/gpaw-setups-latest.tar.gz && \
@@ -41,15 +77,41 @@ tar xzf gpaw-setups-latest.tar.gz && \
 rm gpaw-setups-latest.tar.gz && \
 mv gpaw-setups-[0-9]* gpaw/gpaw-setups""")
 
-cmd('svn checkout https://svn.fysik.dtu.dk/projects/ase/trunk ase')
+cmd('svn export https://svn.fysik.dtu.dk/projects/ase/trunk ase')
+
+# ase needed
+sys.path.insert(0, '%s/ase' % dir)
+
+from gpaw.test.big.agts import AGTSQueue
+from gpaw.test.big.niflheim import NiflheimCluster
 
 queue = AGTSQueue()
 queue.collect()
-cluster = NiflheimCluster('~/agts/ase', '~/agts/gpaw/gpaw-setups')
+cluster = NiflheimCluster(asepath=os.path.join(dir, 'ase'),
+                          setuppath=os.path.join(gpawdir, 'gpaw-setups'))
+# Example below is confusing: job.script must NOT be the *.agts.py script,
+# but the actual python script to be run!
+# testsuite.agts.py does both: see gpaw/test/big/miscellaneous/testsuite.agts.py
 #queue.jobs = [job for job in queue.jobs if job.script == 'testsuite.agts.py']
+
 nfailed = queue.run(cluster)
 
-queue.copy_created_files('/home/camp2/jensj/WWW/gpaw-files')
+gfiles = os.path.join(dir, 'gpaw-files')
+if not os.path.isdir(gfiles):
+    os.mkdir(gfiles)
+
+queue.copy_created_files(gfiles)
+
+from gpaw.version import version
+
+subject = 'AGTS GPAW %s: ' % str(version)
+# Send mail:
+if nfailed:
+    subject += ' failed'
+    fail(subject, email, os.path.join(dir, 'status.log'))
+else:
+    subject += ' succeeded'
+    fail(subject, email, os.path.join(dir, 'status.log'))
 
 if 0:
     # Analysis:
@@ -62,6 +124,5 @@ if 0:
             '../analysis',                 # Where to dump figures
             rev=niflheim.revision,
             #mailto='gpaw-developers@listserv.fysik.dtu.dk',
-            mailto='jensj@fysik.dtu.dk',
             mailserver='servfys.fysik.dtu.dk',
             attachment='status.log')
--- tools/nightly_test_niflheim.py
+++ tools/nightly_test_niflheim.py
@@ -5,24 +5,42 @@ import time
 import glob
 import tempfile
 
-def fail(subject, filename='/dev/null'):
-    assert os.system(
-        'mail -s "%s" gpaw-developers@listserv.fysik.dtu.dk < %s' %
-        (subject, filename)) == 0
+def fail(subject, email=None, filename='/dev/null'):
+    if email is not None:
+        assert os.system('mail -s "%s" %s < %s' %
+                         (subject, email, filename)) == 0
     raise SystemExit
+
+day = time.localtime()[6]
+if '--debug' in sys.argv[1:]:
+    args = '--debug'
+    np = 2 ** (1 + (day+1) % 3)
+else:
+    args = ''
+    np = 2 ** (1 + day % 3)
 
 if '--dir' in sys.argv:
     i = sys.argv.index('--dir')
-    sys.argv.pop(i)
-    dir = sys.argv.pop(i)
+    dir = sys.argv[i+1]
 else:
     dir = None
+
+if '--email' in sys.argv:
+    i = sys.argv.index('--email')
+    email = sys.argv[i+1]
+else:
+    email = None
+
+# allow overwrite of np
+if '--np' in sys.argv:
+    i = sys.argv.index('--np')
+    np = int(sys.argv[i+1])
 
 tmpdir = tempfile.mkdtemp(prefix='gpaw-parallel-', dir=dir)
 os.chdir(tmpdir)
 
 # Checkout a fresh version and install:
-if os.system('svn export ' +
+if os.system('svn checkout ' +
              'https://svn.fysik.dtu.dk/projects/gpaw/trunk gpaw') != 0:
     fail('Checkout of gpaw failed!')
 if os.system('svn export ' +
@@ -44,18 +62,15 @@ if os.system('source /home/camp/modulefiles.sh&& ' +
 
 os.system('mv ../ase/ase ../lib64/python')
 
+# import gpaw from where it was installed
+sys.path.insert(0, '%s/%s/python' % (tmpdir, 'lib64'))
+
+from gpaw.version import version
+
 os.system('wget --no-check-certificate --quiet ' +
           'http://wiki.fysik.dtu.dk/gpaw-files/gpaw-setups-latest.tar.gz')
 os.system('tar xvzf gpaw-setups-latest.tar.gz')
 setups = tmpdir + '/gpaw/' + glob.glob('gpaw-setups-[0-9]*')[0]
-
-day = time.localtime()[6]
-if '--debug' in sys.argv[1:]:
-    args = '--debug'
-    cpus = 2 ** (1 + (day+1) % 3)
-else:
-    args = ''
-    cpus = 2 ** (1 + day % 3)
     
 # Run test-suite:
 print 'Run'
@@ -67,10 +82,10 @@ if os.system('source /home/camp/modulefiles.sh; ' +
              'export PYTHONPATH=%s/lib64/python:$PYTHONPATH; ' % tmpdir +
              'export GPAW_SETUP_PATH=%s; ' % setups +
              'export OMP_NUM_THREADS=1; ' +
-             'mpiexec -np %d ' % cpus +
+             'mpiexec -np %d ' % np +
              tmpdir + '/bin/gpaw-python ' +
              'tools/gpaw-test --directory=. %s >& test.out' % args) != 0:
-    fail('Testsuite crashed!', 'test.out')
+    fail('GPAW %s:  Testsuite crashed!' % str(version), email, 'test.out')
 
 try:
     failed = open('failed-tests.txt').readlines()
@@ -86,7 +101,8 @@ else:
                                                failed[0][:-1], failed[1][:-1])
         if n > 2:
             subject += ', ...'
-    fail(subject, 'test.out')
+    subject = 'GPAW %s: ' % str(version) + subject
+    fail(subject, email, 'test.out')
 
 print 'Done'
-os.system('cd; rm -r ' + tmpdir)
+os.system('cd; rm -rf ' + tmpdir)
