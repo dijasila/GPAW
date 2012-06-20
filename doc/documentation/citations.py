d988fe6871ad67a7760bcbda4d6ed0fb2e6ef87e
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2012-06-20 09:18:49 +0000

    Update list of papers citing GPAW method papers.

--- doc/documentation/citations.py
+++ doc/documentation/citations.py
@@ -5,7 +5,7 @@ import datetime
 import matplotlib
 matplotlib.use('Agg')
 import pylab as plt
-import numpy as np
+
 
 months = [datetime.date(2000, m, 1).strftime('%B')[:3].upper()
           for m in range(1, 13)]
@@ -13,39 +13,26 @@ months = [datetime.date(2000, m, 1).strftime('%B')[:3].upper()
 
 def f(filename):
     papers = []
-    nodoi = 0
     lines = open(filename).readlines()
     n = 0
     while n < len(lines):
         line = lines[n]
-        if line[0] == '@':
-            title = day = month = year = doi = None
-        elif line.startswith('Title'):
-            title = line.split('{{')[1]
-            while '}}' not in title:
-                title += lines[n + 1]
-                n += 1
-            title = ' '.join(title.split('}}')[0].split())
-        elif line.startswith('Year'):
-            year = int(line.split('{{')[1].split('}}')[0])
-        elif line.startswith('Month'):
-            month = line.split('{{')[1].split('}}')[0]
-            if ' ' in month:
-                month, day = month.split()
-                day = int(day)
+        tag = line[:2]
+        if tag == 'TI':
+            ntitle = n
+        elif tag == 'SO':
+            title = ' '.join(lines[i][3:] for i in range(ntitle, n))
+        elif tag == 'DI':
+            doi = line[3:]
+        elif tag == 'PD':
+            w = line.split()[1:]
+            if len(w) == 1:
+                date = datetime.date(int(w[0]), 6, 15)
+            elif len(w) == 2:
+                date = datetime.date(int(w[1]), 1 + months.index(w[0]), 15)
             else:
-                day = 15
-            month = months.index(month) + 1
-        elif line.startswith('DOI'):
-            doi = line.split('{{')[1].split('}}')[0]
-        elif line[0] == '}':
-            if month is None:
-                month, day = 6, 15
-            date = datetime.date(year, month, day)
-            if doi is None:
-                doi = filename + str(nodoi)
-                nodoi += 1
-                print title
+                date = datetime.date(int(w[2]), 1 + months.index(w[0]),
+                                     int(w[1]))
             papers.append((date, doi, title))
         n += 1
 
@@ -55,13 +42,13 @@ def f(filename):
 plt.figure(figsize=(10, 5))
 total = {}
 for bib in ['gpaw1', 'tddft', 'gpaw2', 'response']:
-    papers = f(bib + '.bib')
+    papers = f(bib + '.txt')
     plt.plot([paper[0] for paper in papers], range(1, len(papers) + 1),
              '-o', label=bib)
     for date, doi, title in papers:
         total[doi] = (date, title)
-    x=dict([(p[1],0) for p in papers])
-    print bib, len(papers), len(x), len(total)
+    x = dict([(p[1], 0) for p in papers])
+    print(bib, len(papers), len(x), len(total))
 
 allpapers = [(paper[0], doi, paper[1]) for doi, paper in total.items()]
 allpapers.sort()
