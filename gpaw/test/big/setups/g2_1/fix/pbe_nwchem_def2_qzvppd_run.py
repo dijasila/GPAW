e2ac5ed4e07fe72011d1257673152f4818d00090
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2012-07-11 12:05:50 +0000

    drop asserts. leave it to the analysis part

--- gpaw/test/big/setups/g2_1/fix/pbe_nwchem_def2_qzvppd_run.py
+++ gpaw/test/big/setups/g2_1/fix/pbe_nwchem_def2_qzvppd_run.py
@@ -1,3 +1,3 @@
 import os
 from gpaw.test.big.setups.g2_1.fix.pbe_nwchem_def2_qzvppd_analyse import tag
-assert os.system("NWCHEM_COMMAND='mpiexec -np 4 nwchem' ase nwchem molecule g2-1 -t " + tag + " --atomize -l -p geometry='noautosym nocenter',task='gradient',xc='PBE',smear=0.0,grid='nodisk',tolerances='tight',basis='def2-qzvppd',basispar='spherical',dftcontrol='direct\nnoio'") == 0
+os.system("NWCHEM_COMMAND='mpiexec -np 4 nwchem' ase nwchem molecule g2-1 -t " + tag + " --atomize -l -p geometry='noautosym nocenter',task='gradient',xc='PBE',smear=0.0,grid='nodisk',tolerances='tight',basis='def2-qzvppd',basispar='spherical',dftcontrol='direct\nnoio'")
