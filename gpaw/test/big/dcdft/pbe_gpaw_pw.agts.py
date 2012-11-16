fa73167666b7dc4d91d087d629e94c14525bc7ae
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2012-11-16 13:41:35 +0000

    don't run all costly tests now; run next time when setups change

--- gpaw/test/big/dcdft/pbe_gpaw_pw.agts.py
+++ gpaw/test/big/dcdft/pbe_gpaw_pw.agts.py
@@ -1,4 +1,7 @@
-from gpaw.test.big.dcdft.pbe_gpaw_pw import elements_slow
+if 0:
+    from gpaw.test.big.dcdft.pbe_gpaw_pw import elements_slow
+else:
+    elements_slow = ['Mn']  # just test single element
 def agts(queue):
     run16 = [queue.add('pbe_gpaw_pw.py %s' % s,
                        ncpus=16,
@@ -7,8 +10,8 @@ def agts(queue):
     run4 = [queue.add('pbe_gpaw_pw.py %s' % s,
                       ncpus=4,
                       walltime=40*60)
-            for s in range(20)]
-    if 1:  # run when new setups ready
+            for s in range(0)]  # use range(20)
+    if 0:  # run when new setups ready
         analyse = queue.add('analyse.py dcdft_pbe_gpaw_pw',
                             ncpus=1, walltime=10, deps=run16 + run4,
                             creates=['dcdft_pbe_gpaw_pw.csv',
