5a11d6344f2b53e459f35672b3c3395c9c9d5b67
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2013-04-11 09:11:40 +0000

    Fix bad ReST - part two

--- gpaw/elph/electronphonon.py
+++ gpaw/elph/electronphonon.py
@@ -230,11 +230,15 @@ class ElectronPhononCoupling(Displacement):
         ----------
         dump: int
             Dump supercell matrix to pickle file (default: 0).
+
             0: Supercell matrix not saved
+
             1: Supercell matrix saved in a single pickle file.
+
             2: Dump matrix for different gradients in separate files. Useful
                for large systems where the total array gets too large for a
                single pickle file.
+
         name: string
             User specified name of the generated pickle file(s). If not
             provided, the string in the ``name`` attribute is used.
