4d9b6e8caf3f7152ff647606b8c4cbbacf2f958c
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2013-06-03 10:42:37 +0000

    Test suite has grown bigger.

--- gpaw/test/big/miscellaneous/testsuite.agts.py
+++ gpaw/test/big/miscellaneous/testsuite.agts.py
@@ -1,5 +1,5 @@
 def agts(queue):
-    queue.add('testsuite.agts.py', ncpus=8, walltime=30)
+    queue.add('testsuite.agts.py', ncpus=8, walltime=40)
 
 if __name__ == '__main__':
     # Run test suite
