f5a09b9d05f8e83a227e82b32977165b9d6f8a4b
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2013-06-11 07:31:20 +0000

    convergence.py prints ~15h more needed

--- doc/tutorials/gw_tutorial/submit.agts.py
+++ doc/tutorials/gw_tutorial/submit.agts.py
@@ -1,7 +1,7 @@
 def agts(queue):
     groundstate = queue.add('Si_groundstate.py')
     queue.add('Si_gw.py', deps=groundstate, ncpus=1, walltime=20)
-    conv = queue.add('convergence.py', ncpus=4, walltime=10*60)
+    conv = queue.add('convergence.py', ncpus=4, walltime=30*60)
     queue.add('plot_convergence.py', deps=conv, creates=['Si_EXX.png', 'Si_GW.png'])
-    freq = queue.add('frequency.py', ncpus=4, walltime=10*60)
+    freq = queue.add('frequency.py', ncpus=4, walltime=12*60)
     queue.add('plot_frequency.py', deps=freq, creates='Si_w.png')
