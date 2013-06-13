df978323ad627ab58566e9cafbb9481c56286104
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2013-06-13 13:05:46 +0000

    do not set globally numpy print precision; verbose numpy and scipy tests

--- gpaw/test/numpy_test.py
+++ gpaw/test/numpy_test.py
@@ -4,6 +4,6 @@ _stdout = sys.stdout
 _stderr = sys.stderr
 # numpy tests write to stderr
 sys.stderr = sys.stdout
-assert test().wasSuccessful()
+assert test(verbose=10).wasSuccessful()
 sys.stdout = _stdout
 sys.stderr = _stderr
