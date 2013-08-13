2462fe7da0a1039d9b309e685fbf0bdffb81b6bd
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2013-08-13 10:32:57 +0000

    Setup names have been changed

--- doc/exercises/lrtddft/ground_state.py
+++ doc/exercises/lrtddft/ground_state.py
@@ -12,7 +12,6 @@ atoms.center(vacuum=4.0) # For real calculations use larger vacuum (e.g. 6)
 
 calc = GPAW(nbands=1,
             h=0.35,
-            setups={'Na': '1'},
             txt='Na2_gs.txt')
 
 atoms.set_calculator(calc)
