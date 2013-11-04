0008062b3362186c2a3a4c4185ad2ea9f7db8397
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2013-11-04 11:49:27 +0000

    Should give correct numbers now.

--- gpaw/response/chi0.py
+++ gpaw/response/chi0.py
@@ -1,10 +1,10 @@
 """Todo:
 
 * metals
-* parallelize over spin and bands
 * optical limit
 """
 
+import sys
 from math import pi
 
 import numpy as np
@@ -19,45 +19,81 @@ from gpaw.response.math_func import two_phi_planewave_integrals
 
 class Chi0:
     def __init__(self, calc, omega_w, ecut=50 / Hartree, hilbert=False,
-                 eta=0.2 / Hartree, blocksize=50):
+                 eta=0.2 / Hartree, blocksize=50, spin=0, ftol=1e-6,
+                 comm=world, fd=None):
         self.calc = calc
         self.omega_w = omega_w
         self.ecut = ecut
         self.hilbert = hilbert
         self.eta = eta
         self.blocksize = blocksize
-
+        self.spin = spin
+        self.ftol = ftol
+        self.comm = comm
+        self.fd = fd or sys.stdout
+            
         self.spos_ac = calc.atoms.get_scaled_positions()
         
-        self.mynk = None
-        self.eps_nk = None
-        self.ut_knR = None
-        self.P_kani = None
+        self.nocc1 = None  # number of completely filled bands
+        self.nocc2 = None  # number of non-empty bands
+        self.count_occupied_bands()
         
+        self.Kn1n2 = None  # my occupied k-points and bands
+        self.distribute_k_points_and_bands()
+        
+        self.ut_knR = None  # periodic part of wave functions in real-space
+        self.eps_kn = None  # eigenvalues
+        self.f_kn = None  # occupation numbers
+        self.P_kani = None  # PAW projections
+        self.shift_k = None  # long story - see get_symmetry_operators()
         self.initialize_occupied_states()
         
+        vol = abs(np.linalg.det(calc.wfs.gd.cell_cv))
+        self.prefactor = 2 / vol / calc.wfs.kd.nbzkpts
+
+    def count_occupied_bands(self):
+        self.nocc1 = 9999999
+        self.nocc2 = 0
+        for kpt in self.calc.wfs.kpt_u:
+            f_n = kpt.f_n / kpt.weight
+            self.nocc1 = min((f_n > 1 - self.ftol).sum(), self.nocc1)
+            self.nocc2 = max((f_n > self.ftol).sum(), self.nocc2)
+        self.fd.write('Number of completely filled bands: %d\n' % self.nocc1)
+        self.fd.write('Number of non-empty bands: %d\n' % self.nocc2)
+        self.fd.write('Total number of bands: %d\n' % self.calc.wfs.bd.nbands)
+        
+    def distribute_k_points_and_bands(self):
+        comm = self.comm
+        nk = self.calc.wfs.kd.nbzkpts
+        n = (nk * self.nocc2 + comm.size - 1) // comm.size
+        i1 = comm.rank * n
+        i2 = min((comm.rank + 1) * n, nk * self.nocc2)
+        K1, n1 = divmod(i1, self.nocc2)
+        K2, n2 = divmod(i2, self.nocc2)
+        self.Kn1n2_k = []
+        for K in range(K1, K2):
+            self.Kn1n2_k.append((K, n1, self.nocc2))
+            n1 = 0
+        if n2 > 0:
+            self.Kn1n2_k.append((K2, n1, n2))
+        self.fd.write('k-points: %s\n' % self.calc.wfs.kd.description)
+        self.fd.write('Distributing %d x %d bands over %d process%s' %
+                      (nk, self.nocc2, comm.size, ['es', ''][comm.size == 1]))
+            
     def initialize_occupied_states(self):
-        wfs = self.calc.wfs
-        kd = wfs.kd
-        
-        vol = abs(np.linalg.det(wfs.gd.cell_cv))
-        self.prefactor = 2 / vol / kd.nbzkpts
-        
-        nocc = wfs.nvalence // 2
-        self.mynk = kd.nbzkpts // world.size
-        
-        self.ut_knR = wfs.gd.empty((self.mynk, nocc), complex)
-        self.eps_kn = np.empty((self.mynk, nocc))
+        self.ut_knR = []
+        self.eps_kn = []
+        self.f_kn = []
         self.P_kani = []
         self.shift_kc = []
-        for k in range(self.mynk):
-            K = k + world.rank * self.mynk
-            ut_nG, eps_n, P_ani, shift_c = self.get_k_point(K, 0, nocc)
-            self.ut_knR[k] = ut_nG
-            self.eps_kn[k] = eps_n
+        for K, n1, n2 in self.Kn1n2_k:
+            ut_nG, eps_n, f_n, P_ani, shift_c = self.get_k_point(K, n1, n2)
+            self.ut_knR.append(ut_nG)
+            self.eps_kn.append(eps_n)
+            self.f_kn.append(f_n)
             self.P_kani.append(P_ani)
             self.shift_kc.append(shift_c)
-            
+
     def get_k_point(self, K, n1, n2):
         wfs = self.calc.wfs
         
@@ -71,6 +107,7 @@ class Chi0:
             ut_nR[n - n1] = T(wfs.pd.ifft(psit_nG[n], ik))
 
         eps_n = kpt.eps_n[n1:n2]
+        f_n = kpt.f_n[n1:n2] / kpt.weight
         
         P_ani = []
         for (b, T_ii, time_reversal) in T_a:
@@ -79,52 +116,55 @@ class Chi0:
                 P_ni = P_ni.conj()
             P_ani.append(P_ni)
         
-        return ut_nR, eps_n, P_ani, shift_c
+        return ut_nR, eps_n, f_n, P_ani, shift_c
     
-    def calculate(self, q_c):
+    def calculate(self, q_c, chi0_wGG=None, nocc1=None, nbands=None):
         wfs = self.calc.wfs
 
         if self.eta == 0.0:
-            update = self.update_chi0_hermetian
+            update = self.update_hermetian
         elif self.hilbert:
-            update = self.update_chi0_hilbert
+            update = self.update_hilbert
         else:
-            update = self.update_chi0
+            update = self.update
             
         qd = KPointDescriptor([q_c])
         pd = PWDescriptor(self.ecut, wfs.gd, complex, qd)
         nG = pd.ngmax
-        chi0_wGG = np.zeros((len(self.omega_w), nG, nG), complex)
         
-        self.initialize_paw_corrections(pd)
+        if chi0_wGG is None:
+            # Start from scratch and do all empty bands:
+            chi0_wGG = np.zeros((len(self.omega_w), nG, nG), complex)
+            nocc1 = self.nocc1
+            nbands = wfs.bd.nbands
+            
+        Q_aGii = self.calculate_paw_corrections(pd)
         
-        nocc = wfs.nvalence // 2
-        nunocc = wfs.bd.nbands - nocc
-
-        for k in range(self.mynk):
+        for k, (K, n1, n2) in enumerate(self.Kn1n2_k):
             P_ani = self.P_kani[k]
-            K = k + world.rank * self.mynk
             K2 = wfs.kd.find_k_plus_q(q_c, [K])[0]
-            ut_mR, eps_m, P_ami, shift_c = self.get_k_point(K2, nocc,
-                                                            nocc + nunocc)
+            ut_mR, eps_m, f_m, P_ami, shift_c = self.get_k_point(
+                K2, nocc1, nbands)
             Q_G = self.get_fft_indices(K, K2, q_c, pd,
                                        self.shift_kc[k] - shift_c)
-            for n in range(nocc):
-                eps = self.eps_kn[k, n]
-                utcc_R = self.ut_knR[k, n].conj()
+            for n in range(n2 - n1):
+                eps = self.eps_kn[k][n]
+                f = self.f_kn[k][n]
+                utcc_R = self.ut_knR[k][n].conj()
                 C_aGi = [np.dot(Q_Gii, P_ni[n].conj())
-                         for Q_Gii, P_ni in zip(self.Q_aGii, P_ani)]
-                for m1 in range(0, nunocc, self.blocksize):
-                    m2 = min(m1 + self.blocksize, nunocc)
+                         for Q_Gii, P_ni in zip(Q_aGii, P_ani)]
+                for m1 in range(0, nbands - nocc1, self.blocksize):
+                    m2 = min(m1 + self.blocksize, nbands - nocc1)
                     n_mG = self.calculate_pair_densities(utcc_R, C_aGi,
                                                          ut_mR, P_ami,
                                                          m1, m2, pd, Q_G)
-                    omega_m = eps - eps_m[m1:m2]
-                    update(n_mG, omega_m, chi0_wGG)
+                    deps_m = eps - eps_m[m1:m2]
+                    update(n_mG, deps_m, f - f_m, chi0_wGG)
                     
         world.sum(chi0_wGG)
         
         if self.eta == 0.0:
+            # Set lower triangle also:
             il = np.tril_indices(nG, -1)
             iu = il[::-1]
             for chi0_GG in chi0_wGG:
@@ -136,26 +176,26 @@ class Chi0:
         
         return chi0_wGG, pd
         
-    def update_chi0(self, n_mG, omega_m, chi0_wGG):
+    def update(self, n_mG, deps_m, df_m, chi0_wGG):
         for w, omega in enumerate(self.omega_w):
-            x_m = (1.0 / (omega + omega_m + 1j * self.eta) -
-                   1.0 / (omega - omega_m + 1j * self.eta))
+            x_m = df_m * (1.0 / (omega + deps_m + 1j * self.eta) -
+                          1.0 / (omega - deps_m + 1j * self.eta))
             x_mG = n_mG * x_m[:, np.newaxis]
             gemm(self.prefactor, n_mG.conj(), np.ascontiguousarray(x_mG.T),
                  1.0, chi0_wGG[w])
 
-    def update_chi0_hermitian(self, n_mG, omega_m, chi0_wGG):
+    def update_hermitian(self, n_mG, deps_m, df_m, chi0_wGG):
         for w, omega in enumerate(self.omega_w):
-            x_m = (2 * omega_m / (omega**2 + omega_m**2))**0.5
+            x_m = (2 * df_m * deps_m / (omega.imag**2 + deps_m**2))**0.5
             x_mG = n_mG * x_m[:, np.newaxis]
             rk(self.prefactor, x_mG, 1.0, chi0_wGG[w])
 
-    def update_chi0_hilbert(self, n_mG, omega_m, chi0_wGG):
+    def update_hilbert(self, n_mG, deps_m, df_m, chi0_wGG):
         domega = self.omega_w[1]
-        for omega, n_G in zip(omega_m, n_mG):
+        for omega, df, n_G in zip(deps_m, df_m, n_mG):
             w = omega / domega
             iw = int(w)
-            weights = np.array([[1 - w + iw], [w - iw]])
+            weights = df * np.array([[1 - w + iw], [w - iw]])
             x_2G = n_G * weights**0.5
             rk(self.prefactor, x_2G, 1.0, chi0_wGG[iw:iw + 2])
 
@@ -224,7 +264,7 @@ class Chi0:
 
         return T, T_a, shift_c
 
-    def initialize_paw_corrections(self, pd):
+    def calculate_paw_corrections(self, pd):
         wfs = self.calc.wfs
         q_v = pd.K_qv[0]
         G_Gv = pd.G_Qv[pd.Q_qG[0]] + q_v
@@ -237,9 +277,10 @@ class Chi0:
             ni = atomdata.ni
             Q_xGii[id] = Q_Gii.reshape((-1, ni, ni))
         
-        self.Q_aGii = []
+        Q_aGii = []
         for a, atomdata in enumerate(wfs.setups):
             id = wfs.setups.id_a[a]
             Q_Gii = Q_xGii[id]
             x_G = np.exp(-1j * np.dot(G_Gv, pos_av[a]))
-            self.Q_aGii.append(x_G[:, np.newaxis, np.newaxis] * Q_Gii)
+            Q_aGii.append(x_G[:, np.newaxis, np.newaxis] * Q_Gii)
+        return Q_aGii
