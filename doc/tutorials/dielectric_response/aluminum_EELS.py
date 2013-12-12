f3605b3e5da6c211dd9233281ddfd4a524b9e44d
fras <fras@6419b465-d902-0410-997c-be2266baf723>
2013-12-12 17:52:37 +0000

    Changed the example to use PW mode and the new DielectricFunction.

--- doc/tutorials/dielectric_response/aluminum_EELS.py
+++ doc/tutorials/dielectric_response/aluminum_EELS.py
@@ -1,19 +1,20 @@
 import numpy as np
 from ase.lattice import bulk
 from gpaw import GPAW
-from gpaw.response.df import DF
+from gpaw.response.df2 import DielectricFunction
 
 # Part 1: Ground state calculation
-atoms = bulk('Al', 'fcc', a=4.043)  # Generate fcc crystal structure for aluminum
-calc = GPAW(h=0.2, kpts=(4,4,4))    # GPAW calculator initialization
+atoms = bulk('Al', 'fcc', a=4.043)      # Generate fcc crystal structure for aluminum
+calc = GPAW(mode='pw', kpts=(4,4,4))    # GPAW calculator initialization
 
 atoms.set_calculator(calc)
-atoms.get_potential_energy()        # Ground state calculation is performed
-calc.write('Al.gpw','all')          # Use 'all' option to write wavefunctions
+atoms.get_potential_energy()            # Ground state calculation is performed
+calc.write('Al.gpw','all')              # Use 'all' option to write wavefunctions
 
-# Part 2: Spectrum calculation      # DF: dielectric function object
-df = DF(calc='Al.gpw',              # Ground state gpw file as input
-        q=np.array([1./4., 0, 0]),  # Momentum transfer, must be the difference between two kpoints !
-        w=np.linspace(0, 24, 241))  # The Energies (eV) for spectrum: from 0-24 eV with 0.1 eV spacing
+# Part 2: Spectrum calculation
+w = np.linspace(0, 24, 241)             # The energies (eV) for spectrum: from 0-24 eV with 0.1 eV spacing
+df = DielectricFunction(calc='Al.gpw' , # Ground state gpw file as input
+                         frequencies=w) # Energies as input
 
-df.get_EELS_spectrum()              # By default, a file called 'EELS.dat' is generated
+q = np.array([1./4., 0, 0])             # Momentum transfer, must be the difference between two kpoints !
+df.get_eels_spectrum(q_c=q)             # By default, a file called 'eels.csv' is generated
