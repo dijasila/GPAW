b2cb15e6d178a60dc39ec12b0182bd66f24ea313
fras <fras@6419b465-d902-0410-997c-be2266baf723>
2013-12-12 18:03:59 +0000

    Added a input parameter comment for consistency

--- doc/tutorials/dielectric_response/silicon_ABS1.py
+++ doc/tutorials/dielectric_response/silicon_ABS1.py
@@ -5,7 +5,7 @@ from gpaw.response.df2 import DielectricFunction
 w = np.linspace(0, 24., 481)    # 0-24 eV with 0.05 eV spacing
 
 df = DielectricFunction(calc='si.gpw',
-                        frequencies=w,
+                        frequencies=w,     # Frequencies
                         eta=0.1,           # Broadening parameter 
                         ecut=150,          # Energy cutoff for planewaves
                         txt='df_2.out')    # Output text
