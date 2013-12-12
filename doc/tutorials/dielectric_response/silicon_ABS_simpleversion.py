68e1f1e77e351107a795720b4d93bcbe746cb385
fras <fras@6419b465-d902-0410-997c-be2266baf723>
2013-12-12 18:06:18 +0000

    Changed some comments.

--- doc/tutorials/dielectric_response/silicon_ABS_simpleversion.py
+++ doc/tutorials/dielectric_response/silicon_ABS_simpleversion.py
@@ -5,15 +5,15 @@ from gpaw.response.df2 import DielectricFunction
 
 # Part 1: Ground state calculation
 atoms = bulk('Si', 'diamond', a=5.431)   # Generate diamond crystal structure for silicon
-calc = GPAW(mode='pw', kpts=(4,4,4))        # GPAW calculator initialization
+calc = GPAW(mode='pw', kpts=(4,4,4))     # GPAW calculator initialization
  
 atoms.set_calculator(calc)               
 atoms.get_potential_energy()             # Ground state calculation is performed
 calc.write('si.gpw','all')               # Use 'all' option to write wavefunction
 
 # Part 2 : Spectrum calculation          # DF: dielectric function object
-df = DielectricFunction(calc='si.gpw',                   # Ground state gpw file (with wavefunction) as input
-         frequencies=np.linspace(0,14,141))         # The Energies (eV) for spectrum: from 0-14 eV with 0.1 eV spacing
-#        optical_limit=True)              # Indicates that its a optical spectrum calculation
+w = np.linspace(0,14,141)                # The Energies (eV) for spectrum: from 0-14 eV with 0.1 eV spacing
+df = DielectricFunction(calc='si.gpw',   # Ground state gpw file (with wavefunction) as input
+                        frequencies=w)   # Energies as input
 
-df.get_polarizability()             # By default, a file called 'Absorption.dat' is generated
+df.get_polarizability()                  # By default, a file called 'absorption.csv' is generated
