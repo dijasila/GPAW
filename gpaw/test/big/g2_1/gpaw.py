37ba267e1981ea5b109b6d1e49476748b069fd55
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2014-01-08 10:53:34 +0000

    Update G2-1 test

--- gpaw/test/big/g2_1/gpaw.py
+++ gpaw/test/big/g2_1/gpaw.py
@@ -1,8 +1,9 @@
 import ase.db
 from ase.structure import molecule
 from ase.optimize.bfgs import BFGS
-from ase.calculators.nwchem import NWChem
 from ase.data.g2_1 import molecule_names, atom_names
+
+from gpaw import GPAW, PW, Mixer
 
 
 c = ase.db.connect('g2-1.db')
@@ -12,16 +13,15 @@ for name in molecule_names + atom_names:
     if id is None:
         continue
     atoms = molecule(name)
-    atoms.calc = NWChem(command='mpiexec -np 4 nwchem PREFIX.nw > PREFIX.out',
-                        geometry='noautosym nocenter noautoz',
-                        task='gradient',
-                        xc='PBE',
-                        grid='nodisk',
-                        tolerances='tight',
-                        basis='def2-qzvppd',
-                        basispar='spherical',
-                        direct='noio',
-                        label=name)
+    atoms.cell = [12, 12.01, 12.02]
+    atoms.center()
+    atoms.calc = GPAW(mode=PW(800),
+                      xc='PBE',
+                      fixmom=True,
+                      txt=name + '.txt')
+    if name in ['Na2', 'NaCl', 'NO', 'ClO']:
+        atoms.calc.set(eigensolver='cg',
+                       mixer=Mixer(0.05, 2))
     atoms.get_forces()
     c.write(atoms, name=name, relaxed=False)
     if len(atoms) > 1:
