690f8b35ebc962f430fe3a763364645619ea3179
tolsen <tolsen@6419b465-d902-0410-997c-be2266baf723>
2014-01-12 23:33:10 +0000

    rpa doc

--- doc/tutorials/rpa/gs_graph_Co.py
+++ doc/tutorials/rpa/gs_graph_Co.py
@@ -1,22 +1,19 @@
 import numpy as np
-from ase import *
 from ase.visualize import view
 from ase.dft.kpoints import monkhorst_pack
 from ase.parallel import paropen
 from ase.lattice.surface import *
-from gpaw import *
-from gpaw import PW
+from gpaw import GPAW, PW, FermiDirac, MixerSum
 from gpaw.xc.exx import EXX
 
-kpts = monkhorst_pack((16,16,1))
+kpts = monkhorst_pack((16, 16, 1))
 kpts += np.array([1/32., 1/32., 0])
 
 a = 2.51 # Lattice parameter of Co
 slab = hcp0001('Co', a=a, c=4.07, size=(1,1,4))
-slab.set_pbc([1,1,1])
 pos = slab.get_positions()
 cell = slab.get_cell()
-cell[2,2] = 20. + pos[-1,2]
+cell[2,2] = 20. + pos[-1, 2]
 slab.set_cell(cell)
 slab.set_initial_magnetic_moments([0.7, 0.7, 0.7, 0.7])
 
@@ -25,8 +22,8 @@ ds = [1.75, 2.0, 2.25, 2.5, 2.75, 3.0, 3.25, 3.5, 3.75, 4.0, 5.0, 6.0, 10.0]
 for d in ds:
     pos = slab.get_positions()
     add_adsorbate(slab, 'C', d, position=(pos[3,0], pos[3,1]))
-    add_adsorbate(slab, 'C', d, position=(cell[0,0]/3+cell[1,0]/3,
-                                          cell[0,1]/3+cell[1,1]/3))
+    add_adsorbate(slab, 'C', d, position=(cell[0,0]/3 + cell[1,0]/3,
+                                          cell[0,1]/3 + cell[1,1]/3))
     #view(slab)
     calc = GPAW(xc='PBE',
                 eigensolver='cg',
@@ -45,7 +42,7 @@ for d in ds:
     exx.calculate()
     E_hf = exx.get_total_energy()
 
-    #calc.diagonalize_full_hamiltonian()
+    calc.diagonalize_full_hamiltonian()
     calc.write('gs_%s.gpw' % d, mode='all')
 
     f = paropen('hf_acdf.dat', 'a')
