766a67ff70fe5d37773be88b6c3e1b78efb09595
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2014-03-29 08:51:16 +0000

    adjust more tests

--- gpaw/test/big/scf/trouble/tio2v2o5.py
+++ gpaw/test/big/scf/trouble/tio2v2o5.py
@@ -1,6 +1,6 @@
 from ase.io import read
 from gpaw import GPAW
-atoms = read('tio2v2o5.traj')
+atoms = read('tio2v2o5.txt')
 atoms.calc = GPAW(h=0.20,
                   kpts = (2,2,1),
                   xc='RPBE')
