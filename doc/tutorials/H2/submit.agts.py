e5727df2db0eaf9d10e17b2c432094163744199c
mogje <mogje@6419b465-d902-0410-997c-be2266baf723>
2014-04-10 13:17:54 +0000

    Added ensemble.py to agts. Not working. Disabled. Ticket 250.

--- doc/tutorials/H2/submit.agts.py
+++ doc/tutorials/H2/submit.agts.py
@@ -2,3 +2,6 @@ def agts(queue):
     a = queue.add('atomization.py', ncpus=1, walltime=30, 
                   creates=['atomization.txt', 'H.gpw', 'H2.gpw'])
     queue.add('relax.py', deps=a, ncpus=1, walltime=30, creates=['optimization.txt'])
+    if 0:  # https://trac.fysik.dtu.dk/projects/gpaw/ticket/250
+        queue.add('ensembles.py', deps=a, ncpus=1, 
+                  walltime=30, creates=['ensemble_energies.txt', 'ensemble.dat'])
