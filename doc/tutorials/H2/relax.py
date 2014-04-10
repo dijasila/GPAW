cdd27ad7e03096959f486886f1672df0e5ef5204
simola <simola@6419b465-d902-0410-997c-be2266baf723>
2014-04-10 12:43:20 +0000

    optimization added to H2

---
+++ doc/tutorials/H2/relax.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+
+from gpaw import restart
+from ase.parallel import paropen as open
+from ase.optimize import QuasiNewton
+
+
+molecule, calc = restart('H2.gpw', txt='H2-relaxed.txt')
+
+e2 = molecule.get_potential_energy()
+d0 = molecule.get_distance(0, 1)
+
+fd = open('optimization.txt', 'w')
+print('experimental bond length:')
+print('hydrogen molecule energy: %5.2f eV' % e2)
+print('bondlength              : %5.2f Ang' % d0)
+
+# Find the theoretical bond length:
+relax = QuasiNewton(molecule, logfile='qn.log')
+relax.run(fmax=0.05)
+
+e2 = molecule.get_potential_energy()
+d0 = molecule.get_distance(0, 1)
+
+print()
+print('PBE energy minimum:')
+print('hydrogen molecule energy: %5.2f eV' % e2)
+print('bondlength              : %5.2f Ang' % d0)
+fd.close()
--- doc/tutorials/H2/submit.agts.py
+++ doc/tutorials/H2/submit.agts.py
@@ -1,3 +1,4 @@
 def agts(queue):
     a = queue.add('atomization.py', ncpus=1, walltime=30, 
                   creates=['atomization.txt', 'H.gpw', 'H2.gpw'])
+    queue.add('relax.py', deps=a, ncpus=1, walltime=30, creates=['optimization.txt'])
