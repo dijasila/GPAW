c6543141aa6f217e4e1269a68c0a6df391137a98
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2014-04-14 08:21:05 +0000

    correcting files - the tutorial seems not producing results making sense?

--- doc/tutorials/neb/submit.agts.py
+++ doc/tutorials/neb/submit.agts.py
@@ -1,4 +1,4 @@
 def agts(queue):
     d = queue.add('diffusion1.py', ncpus=1, walltime=25)
-    queue.add('neb.py', deps=d, ncpus=12, walltime=40*60)
+    queue.add('neb.py', deps=d, ncpus=12, walltime=60)
     
