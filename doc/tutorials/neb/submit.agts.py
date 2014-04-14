0aa3ed36262f1dd72065a8104d7ade91a0e2ee94
kiran <kiran@6419b465-d902-0410-997c-be2266baf723>
2014-04-14 08:47:40 +0000

    neb added to agts

--- doc/tutorials/neb/submit.agts.py
+++ doc/tutorials/neb/submit.agts.py
@@ -1,4 +1,4 @@
 def agts(queue):
-    d = queue.add('diffusion1.py', ncpus=1, walltime=25)
+    d = queue.add('diffusion1.py', ncpus=1, walltime=10)
     queue.add('neb.py', deps=d, ncpus=12, walltime=60)
     
