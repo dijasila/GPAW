b13af7cd0361c454ec18279011b97fa8efcfbaeb
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2014-04-14 08:21:24 +0000

    fix filenames

--- doc/tutorials/H2/submit.agts.py
+++ doc/tutorials/H2/submit.agts.py
@@ -1,7 +1,8 @@
 def agts(queue):
-    a = queue.add('atomization.py', ncpus=1, walltime=30, 
-                  creates=['atomization.txt', 'H.gpw', 'H2.gpw'])
-    queue.add('relax.py', deps=a, ncpus=1, walltime=30, creates=['optimization.txt'])
+    a = queue.add('atomize.py', ncpus=1, walltime=30, 
+                  creates=['atomization.txt'])
+    queue.add('relax.py', deps=a, ncpus=1, walltime=30,
+              creates=['optimization.txt'])
     if 0:  # https://trac.fysik.dtu.dk/projects/gpaw/ticket/250
-        queue.add('ensembles.py', deps=a, ncpus=1, 
-                  walltime=30, creates=['ensemble_energies.txt', 'ensemble.dat'])
+        queue.add('ensembles.py', deps=a, ncpus=1, walltime=30,
+                  creates=['ensemble_energies.txt', 'ensemble.dat'])
