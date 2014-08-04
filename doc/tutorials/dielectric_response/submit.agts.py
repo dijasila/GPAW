32ba1cb5499107ae54c273d81f8106a5d5d8b97d
mogje <mogje@6419b465-d902-0410-997c-be2266baf723>
2014-08-04 11:07:34 +0000

    New agts

--- doc/tutorials/dielectric_response/submit.agts.py
+++ doc/tutorials/dielectric_response/submit.agts.py
@@ -1,21 +1,17 @@
 def agts(queue):
     queue.add('plot_freq.py', creates='nl_freq_grid.png')
     
-    simple_si = queue.add('silicon_ABS_simpleversion.py', creates='df.csv')
+    simple_si = queue.add('silicon_ABS_simpleversion.py')
     queue.add('plot_silicon_ABS_simple.py', creates='si_abs.png',
               deps=simple_si)
     
-    si = queue.add('silicon_ABS.py', creates=['si_abs.csv', 'mac_eps.csv'],
+    si = queue.add('silicon_ABS.py', creates='mac_eps.csv',
                    ncpus=16, walltime=100)
     queue.add('plot_ABS.py', deps=si, creates='silicon_ABS.png')
     
-    al = queue.add('aluminum_EELS.py', creates=['eels.csv'],
-                   ncpus=8, walltime=100)
+    al = queue.add('aluminum_EELS.py', ncpus=8, walltime=100)
     queue.add('plot_aluminum_EELS_simple.py', deps=al,
               creates=['aluminum_EELS.png'])
     
-    GR = queue.add('graphite_EELS.py',
-                   creates=(['graphite_q_list'] +
-                            ['graphite_EELS_%d.csv' % i for i in range(1, 8)]),
-                   ncpus=8, walltime=100)
+    GR = queue.add('graphite_EELS.py', ncpus=8, walltime=100)
     queue.add('plot_EELS.py', deps=GR, creates='graphite_EELS.png')
