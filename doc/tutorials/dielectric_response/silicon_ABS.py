78776915b549def4c892e38775038df337f1a7bc
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2014-08-07 06:26:30 +0000

    Don't use basis

--- doc/tutorials/dielectric_response/silicon_ABS.py
+++ doc/tutorials/dielectric_response/silicon_ABS.py
@@ -1,67 +1,56 @@
-### Refer to G. Kresse, Phys. Rev. B 73, 045112 (2006)
-### for comparison of macroscopic and microscopic dielectric constant 
-### and absorption peaks. 
+# Refer to G. Kresse, Phys. Rev. B 73, 045112 (2006)
+# for comparison of macroscopic and microscopic dielectric constant
+# and absorption peaks.
 from __future__ import print_function
 
-import os
-import sys
-import numpy as np
-
-from ase.units import Bohr
 from ase.lattice import bulk
 from ase.parallel import paropen
 from gpaw import GPAW, FermiDirac
-from gpaw.mpi import serial_comm, rank, size
-from gpaw.utilities import devnull
 from gpaw.response.df import DielectricFunction
 
 # Ground state calculation
-a = 5.431 #10.16 * Bohr 
+a = 5.431
 atoms = bulk('Si', 'diamond', a=a)
 
 calc = GPAW(mode='pw',
             kpts={'density': 5.0, 'gamma': True},
             xc='LDA',
-            basis='dzp',
-            occupations=FermiDirac(0.001), # Use smaller FD smearing 
-            )
+            occupations=FermiDirac(0.001))  # Use small FD smearing
 
-atoms.set_calculator(calc) 
-atoms.get_potential_energy() # Get ground state density
-calc.write('si_small.gpw')
+atoms.set_calculator(calc)
+atoms.get_potential_energy()  # Get ground state density
 
 # Restart Calculation with fixed density and dense kpoint sampling
-calc = GPAW('si_small.gpw', fixdensity=True,
-            kpts={'density': 15.0, 'gamma': True},  # Dense kpoint sampling
-            )
-
-calc.diagonalize_full_hamiltonian(nbands=70) # Diagonalize Hamiltonian 
-calc.write('si_large.gpw', 'all') # Write wavefunctions
+calc.set(kpts={'density': 15.0, 'gamma': False},  # Dense kpoint sampling
+         fixdensity=True)
+atoms.get_potential_energy()
+calc.diagonalize_full_hamiltonian(nbands=70)  # Diagonalize Hamiltonian
+calc.write('si_large.gpw', 'all')  # Write wavefunctions
 
 # Getting absorption spectrum
-df = DielectricFunction(calc='si_large.gpw', 
-                        domega0=0.01,
-                        eta=0.1,
+df = DielectricFunction(calc='si_large.gpw',
+                        eta=0.05,
+                        domega0=0.02,
+                        alpha=5,
                         ecut=150)
 df.get_dielectric_function(filename='si_abs.csv')
 
 # Getting macroscopic constant
-df = DielectricFunction(calc='si_large.gpw', 
-                        frequencies=np.array([.0]), 
+df = DielectricFunction(calc='si_large.gpw',
+                        frequencies=[0.0],
                         eta=0.0001,
-                        ecut=150, 
+                        ecut=150,
                         )
 
 epsNLF, epsLF = df.get_macroscopic_dielectric_constant()
 
 # Make table
-from ase.parallel import paropen as open
 epsrefNLF = 14.08  # From [1] in top
 epsrefLF = 12.66  # From [1] in top
 
-f = open('mac_eps.csv', 'w')
+f = paropen('mac_eps.csv', 'w')
 print(' , Without LFE, With LFE', file=f)
-print('%s, %.6f, %.6f' %('GPAW-linear response', epsNLF, epsLF), file=f)
-print('%s, %.6f, %.6f' %('[1]', epsrefNLF, epsrefLF), file=f)
-print('%s, %.6f, %.6f' %('Exp.', 11.90, 11.90), file=f)
+print('%s, %.6f, %.6f' % ('GPAW-linear response', epsNLF, epsLF), file=f)
+print('%s, %.6f, %.6f' % ('[1]', epsrefNLF, epsrefLF), file=f)
+print('%s, %.6f, %.6f' % ('Exp.', 11.90, 11.90), file=f)
 f.close()
