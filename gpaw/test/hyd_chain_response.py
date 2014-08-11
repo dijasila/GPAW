3f55a1c428002afb82a304ea301146e85cc98267
mogje <mogje@6419b465-d902-0410-997c-be2266baf723>
2014-08-11 14:51:44 +0000

    Added some assertions. Note that the test fails. Something is going on.

--- gpaw/test/hyd_chain_response.py
+++ gpaw/test/hyd_chain_response.py
@@ -1,7 +1,7 @@
 from ase import Atoms
 from gpaw import GPAW, PW
 from gpaw.response.df import DielectricFunction
-
+from gpaw.test import findpeak, equal
 
 def get_hydrogen_chain_dielectric_function(NH, NK):
     a = Atoms('H', cell=[2, 2, 0.5], pbc=True)
@@ -21,12 +21,24 @@ def get_hydrogen_chain_dielectric_function(NH, NK):
 omega1_w, eps1_w = get_hydrogen_chain_dielectric_function(10, 8)
 omega2_w, eps2_w = get_hydrogen_chain_dielectric_function(20, 4)
 omega3_w, eps3_w = get_hydrogen_chain_dielectric_function(40, 2)
+
+eels1_w = -(1. / eps1_w).imag
+eels2_w = -(1. / eps2_w).imag
+eels3_w = -(1. / eps3_w).imag
+
+opeak1, peak1 = findpeak(omega1_w, eels1_w)
+opeak2, peak2 = findpeak(omega2_w, eels2_w)
+opeak3, peak3 = findpeak(omega3_w, eels3_w)
+
+equal(opeak1, opeak2, tolerance=1e-3)
+equal(opeak2, opeak3, tolerance=1e-3)
+
 from gpaw.mpi import world
 from matplotlib import pyplot as plt
 if not world.rank:
-    plt.plot(omega1_w, -(1. / eps1_w).imag, label='1')
-    plt.plot(omega2_w, -(1. / eps2_w).imag, label='2')
-    plt.plot(omega3_w, -(1. / eps3_w).imag, label='3')
+    plt.plot(omega1_w, -(1. / eels1_w).imag, label='1')
+    plt.plot(omega2_w, -(1. / eels2_w).imag, label='2')
+    plt.plot(omega3_w, -(1. / eels3_w).imag, label='3')
     plt.xlim(xmin=0.0, xmax=10.0)
     plt.legend()
     plt.show()
