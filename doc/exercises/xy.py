f6426b7cca4e03a9d193ec60789e062c3365865c
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2014-08-12 14:16:37 +0000

    Slightly more advanced script

--- doc/exercises/xy.py
+++ doc/exercises/xy.py
@@ -1,8 +1,9 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-a = np.loadtxt(sys.argv[1], delimiter=',').T
-x = a[0]
-for y in a[1:]:
-    plt.plot(x, y, '-')
+for filename in sys.argv[1:]:
+    a = np.loadtxt(filename, delimiter=',').T
+    x = a[0]
+    for y in a[1:]:
+        plt.plot(x, y, '-')
 plt.show()
