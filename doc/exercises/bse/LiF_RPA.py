038fdb5b5f7d0fc554d0e2257461f7cc928b5892
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2014-08-12 07:02:05 +0000

    Use new DF-code

--- doc/exercises/bse/LiF_RPA.py
+++ doc/exercises/bse/LiF_RPA.py
@@ -1,24 +1,12 @@
-import numpy as np
-#from gpaw.response.df import DielectricFunction as DF
-from gpaw.response.df0 import DF
-from ase.parallel import paropen
+from gpaw.response.df import DielectricFunction as DF
 
-w_grid = np.linspace(5.0, 15.0, 1001)
-
-df = DF(calc='LiF_fulldiag.gpw',
-        xc='RPA',
-        optical_limit=True,
-        q=np.array([0.0001, 0., 0.]),
-        w=w_grid,
-        eta=0.1,  # Broadening parameter
-        nbands=60,  # Number of bands to consider for building chi
-        ecut=30,   # Energy cutoff for planewaves
-        txt='LiF_RPA_out.txt')  # Output text
+df = DF('LiF_fulldiag.gpw',
+        domega0=0.01,  # grid-spacing at 0 eV
+        omega2=10.0,   # frequency where grid-spacing has doubled to 0.02 eV
+        eta=0.1,       # broadening parameter
+        nbands=60,     # number of bands to consider for building chi
+        ecut=30,       # energy cutoff for planewaves
+        txt='LiF_RPA_out2.txt')
 
 # Calculate the dielectric function without and with local field effects:
-df1_w, df2_w = df.get_dielectric_function()
-
-# Txt files for saving the spectrum:
-Im_df_RPA_data = paropen('Im_df_RPA_data.txt', 'w')
-for iw in range(len(w_grid) - 1):
-   print >> Im_df_RPA_data, w_grid[iw], df2_w[iw].imag
+df.get_dielectric_function()
