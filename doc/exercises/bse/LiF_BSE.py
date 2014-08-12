67ed35fae742747c9d4a8aa2ad550cfb9b28aaad
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2014-08-12 06:05:19 +0000

    Simplify and clean up

--- doc/exercises/bse/LiF_BSE.py
+++ doc/exercises/bse/LiF_BSE.py
@@ -1,22 +1,19 @@
 import numpy as np
 from gpaw.response.bse import BSE
-from ase.parallel import paropen
-import sys
 
-####################################################################################################
-# BSE CALCULATION AND BSE SPECTRUM
-####################################################################################################
+w_grid = np.linspace(0, 15, 1001)
 
-w_grid = np.linspace(0.,15.,1001)
-
+# It stores the four-points kernel used for building the two-particles
+# Hamiltonian in LiF_W_qGG.
 bse = BSE('LiF_fulldiag.gpw',
-w=w_grid,
-q=np.array([0.0001, 0., 0.]),
-optical_limit=True,
-ecut=30,
-nbands=60,
-eta=0.1,
-kernel_file = 'LiF_W_qGG',   # It stores the four-points kernel used for building the two-particles Hamiltonian
-txt='LiF_BSE_out.txt')
+          w=w_grid,
+          q=np.array([0.0001, 0., 0.]),
+          optical_limit=True,
+          ecut=30,
+          nbands=60,
+          eta=0.1,
+          kernel_file='LiF_W_qGG',
+          txt='LiF_BSE_out.txt')
 
-df_BSE = bse.get_dielectric_function()  # It returns the dielectric function calculated at the BSE level
+# Calculate the dielectric function calculated at the BSE level:
+df_BSE = bse.get_dielectric_function()
--- doc/exercises/bse/LiF_gs.py
+++ doc/exercises/bse/LiF_gs.py
@@ -1,45 +1,25 @@
-import numpy as np
 from ase.lattice.spacegroup import crystal
-from gpaw import GPAW, FermiDirac, PW
-from ase.dft.kpoints import monkhorst_pack
-from ase.visualize import view
+from gpaw import GPAW, PW
 
+a = 4.0351  # Experimental lattice constant in Angstrom
+Ecut = 250  # Energy cut off for PW calculation
+k = 5  # Number of kpoints per each direction
 
-####################################################################################################
-# GROUND STATE DFT
-####################################################################################################
-
-#-------------------------------------
-# Structure
-#-------------------------------------
-a = 4.0351     # Experimental lattice constant in \AA
-
-Ecut = 250       # Energy cut off for PW calculation
-kgrid=5          # Number of kpoints per each direction
-
-
-LiF = crystal(['Li', 'F'], [(0, 0, 0), (0.5, 0.5, 0.5)], spacegroup=225,
-              cellpar=[a, a, a, 90, 90, 90])                                #This gives the typical NaCl structure
-
-kpts = monkhorst_pack((kgrid,kgrid,kgrid))
-
-
-#-------------------------------------
-# Self consistent calculation
-#-------------------------------------
+# This gives the typical NaCl structure:
+LiF = crystal(['Li', 'F'],
+              [(0, 0, 0), (0.5, 0.5, 0.5)],
+              spacegroup=225,
+              cellpar=[a, a, a, 90, 90, 90])
 
 calc = GPAW(mode=PW(Ecut),
-           xc='LDA',
-           kpts=kpts,
-           txt='LiF_out_gs.txt')
+            xc='LDA',
+            kpts=(k, k, k),
+            txt='LiF_out_gs.txt')
 
-LiF.set_calculator(calc)       
-LiF.get_potential_energy() 
+LiF.set_calculator(calc)
+LiF.get_potential_energy()
 
-#-------------------------------------
-# Full diagonalization
-#-------------------------------------
-# With the full diagonalization we calculate all the single particle states needed for the response calculation
-
-calc.diagonalize_full_hamiltonian(nbands=100)  
-calc.write('LiF_fulldiag.gpw','all')
+# With the full diagonalization we calculate all the single particle states
+# needed for the response calculation:
+calc.diagonalize_full_hamiltonian(nbands=100)
+calc.write('LiF_fulldiag.gpw', 'all')
