0af7c3c6a18401306b598a47d61025c3feae34fd
krbt <krbt@6419b465-d902-0410-997c-be2266baf723>
2014-08-14 08:16:55 +0000

    Use fd and no optimization

--- doc/exercises/lrtddft/Na2TDDFT.py
+++ doc/exercises/lrtddft/Na2TDDFT.py
@@ -1,19 +1,16 @@
 from gpaw import GPAW
 from ase import Atoms
-from ase.optimize import QuasiNewton
 from gpaw.lrtddft import LrTDDFT
 
-molecule = Atoms('Na2', positions=((0.0, 0.0, 0.0), (2.5, 0.0, 0.0)))
+molecule = Atoms('Na2', positions=((0.0, 0.0, 0.0), (3.12, 0.0, 0.0)))
 
 molecule.center(vacuum=6.0)
 
-calc = GPAW(mode='lcao', xc='PBE', basis='dzp')
+calc = GPAW(xc='PBE')
 
 molecule.set_calculator(calc)
 
-# Find the theoretical bond length:
-relax = QuasiNewton(molecule, logfile='qn.log')
-relax.run(fmax=0.05)
+molecule.get_potential_energy()
 
 lr = LrTDDFT(calc, xc='LDA', istart=0, jend=10, nspins=2)
 lr.write('Omega_Na2.gz')
