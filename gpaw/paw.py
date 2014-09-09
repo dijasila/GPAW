39d24388e99713b74163a707b1f74786b0c09fd3
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2014-09-09 08:02:09 +0000

    Patch from Tristan Maxson: allow nbands='150%' syntax

--- gpaw/paw.py
+++ gpaw/paw.py
@@ -455,13 +455,21 @@ class PAW(PAWTextOutput):
         nvalence = setups.nvalence - par.charge
         M_v = magmom_av.sum(0)
         M = np.dot(M_v, M_v)**0.5
-        
+ 
         nbands = par.nbands
+        if isinstance(nbands, basestring):
+            if nbands[-1] == '%':
+                basebands = int(nvalence + M + 0.5) // 2
+                nbands = int((float(nbands[:-1]) / 100) * basebands)
+            else:
+                raise ValueError('Integer Expected: Only use a string '
+                                 'if giving a percentage of occupied bands')
+
         if nbands is None:
             nbands = 0
             for setup in setups:
                 nbands_from_atom = setup.get_default_nbands()
-                
+
                 # Any obscure setup errors?
                 if nbands_from_atom < -(-setup.Nv // 2):
                     raise ValueError('Bad setup: This setup requests %d'
--- gpaw/test/8Si.py
+++ gpaw/test/8Si.py
@@ -17,7 +17,7 @@ bulk = Atoms(symbols='Si8',
 bulk.set_cell((a, a, a), scale_atoms=True)
 n = 20
 calc = GPAW(gpts=(n, n, n),
-            nbands=8*3,
+            nbands='150%',
             occupations=FermiDirac(width=0.01),
             poissonsolver=PoissonSolver(nn='M', relax='J'),
             kpts=(2, 2, 2),
@@ -29,18 +29,18 @@ e1 = bulk.get_potential_energy()
 niter1 = calc.get_number_of_iterations()
 
 f2 = numeric_force(bulk, 0, 2)
-print f1,f2,f1-f2
+print(f1, f2, f1 - f2)
 equal(f1, f2, 0.005)
 
 # Volume per atom:
 vol = a**3 / 8
 de = calc.get_electrostatic_corrections() / vol
-print de
+print(de)
 assert abs(de[0] - -2.190) < 0.001
 
-print e1, f1, niter1
+print(e1, f1, niter1)
 energy_tolerance = 0.00025
 force_tolerance = 0.0001
 niter_tolerance = 0
-equal(e1, -46.6596470348, energy_tolerance) # svnversion 5252
-equal(f1, -1.38242356123, force_tolerance) # svnversion 5252
+equal(e1, -46.6596470348, energy_tolerance)  # svnversion 5252
+equal(f1, -1.38242356123, force_tolerance)  # svnversion 5252
