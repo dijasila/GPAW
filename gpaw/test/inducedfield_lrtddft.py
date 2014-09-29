3d054af8b43d99e8b927359d4ad22b58d722ba9e
sakko <sakko@6419b465-d902-0410-997c-be2266baf723>
2014-09-29 08:16:37 +0000

    Improve test

--- gpaw/test/inducedfield_lrtddft.py
+++ gpaw/test/inducedfield_lrtddft.py
@@ -24,10 +24,10 @@ lr.diagonalize()
 lr.write('na2_lr.dat.gz')
 
 # 3) Calculate induced field
-frequencies = [1.0]     # Frequencies of interest in eV
-folding = 'Gauss'       # Folding function
-width = 0.1             # Line width for folding in eV
-kickdir = 0             # Kick field direction 0, 1, 2 for x, y, z
+frequencies = [1.0, 2.08] # Frequencies of interest in eV
+folding = 'Gauss'         # Folding function
+width = 0.1               # Line width for folding in eV
+kickdir = 0               # Kick field direction 0, 1, 2 for x, y, z
 ind = LrTDDFTInducedField(paw=calc, lr=lr, frequencies=frequencies, folding=folding, width=width, kickdir=kickdir)
 ind.calculate_induced_field(gridrefinement=2, from_density='comp')
 
@@ -38,7 +38,16 @@ val1 = ind.fieldgd.integrate(ind.Ffe_wg[0])
 val2 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[0][0]))
 val3 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[0][1]))
 val4 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[0][2]))
+val5 = ind.fieldgd.integrate(ind.Ffe_wg[1])
+val6 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[1][0]))
+val7 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[1][1]))
+val8 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[1][2]))
 equal(val1, 3175.76177761, tol)
 equal(val2, 1700.43442519, tol)
 equal(val3, 1187.26249225, tol)
 equal(val4, 1187.26249225, tol)
+equal(val5, 10956.9813196, tol)
+equal(val6, 6574.58868754, tol)
+equal(val7, 4589.74440108, tol)
+equal(val8, 4589.74440108, tol)
+
