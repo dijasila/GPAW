80b555493804ac98bbcb99aa484b34d12ac75a26
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2014-09-30 09:56:06 +0000

    typo

--- doc/exercises/bse/submit.agts.py
+++ doc/exercises/bse/submit.agts.py
@@ -1,5 +1,4 @@
 def agts(queue):
     a = queue.add('LiF_gs.py', ncpus=1, walltime=10)
-    b = queue.add('LiF_RPA.py', ncpus=8, walltime=10, deps=a)
-    cqueue.add('LiF_BSE.py', ncpus=24, walltime=20, deps=a)
-
+    queue.add('LiF_RPA.py', ncpus=8, walltime=10, deps=a)
+    queue.add('LiF_BSE.py', ncpus=24, walltime=20, deps=a)
