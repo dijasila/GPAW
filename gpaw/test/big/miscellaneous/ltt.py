97b710ebf2ab475a24a97cb7cc482ca2f81f1c68
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2014-10-07 13:12:02 +0000

    Turn off symmetry

--- gpaw/test/big/miscellaneous/ltt.py
+++ gpaw/test/big/miscellaneous/ltt.py
@@ -8,7 +8,7 @@ n = 24
 a = n * h
 b = a / 2
 H = Atoms('H', [(b - 0.1, b, b)], pbc=True, cell=(a, a, a))
-calc = GPAW(nbands=1, gpts=(n, n, n), txt='ltt.txt')
+calc = GPAW(nbands=1, gpts=(n, n, n), symmetry='off', txt='ltt.txt')
 H.set_calculator(calc)
 e0 = H.get_potential_energy()
 for i in range(50):
