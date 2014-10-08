f130699485a04ed5329a0f1bf1bbd7dfa5869db4
jesswe <jesswe@6419b465-d902-0410-997c-be2266baf723>
2014-10-08 15:25:57 +0000

    updated and simplified mBEEF test

--- gpaw/test/mbeef.py
+++ gpaw/test/mbeef.py
@@ -1,51 +1,54 @@
-from ase import *
+from ase.structure import molecule
 from ase.dft.bee import BEEF_Ensemble
+from ase.parallel import rank, barrier
 from gpaw import GPAW
 from gpaw.cluster import Cluster
 from gpaw.test import equal
 import numpy as np
 
 xc = 'mBEEF'
-conv = {'eigenstates':1.e-6, 'density':1.e-6, 'energy':1.e-6}
 h = 0.18
-tol1 = 1.e-3
+conv  = {'eigenstates':1.e2, 'density':1.e-2, 'energy':1.e-4}
+list  = ['N2', 'N']
+tol1 = 1.e-4
 tol2 = 1.e-1
 
-# N2 molecule
-n2 = Cluster(Atoms('N2',[[0.,0.,0.],[0.,0.,1.09]]))
-n2.minimal_box(3.0, h=h)
-cell = n2.get_cell()
-calc = GPAW(h=h, xc='PBE', convergence=conv)
-n2.set_calculator(calc)
-n2.get_potential_energy()
-n2.calc.set(xc=xc)
-e_n2 = n2.get_potential_energy()
-f = n2.get_forces()
-ens = BEEF_Ensemble(n2)
-de_n2 = ens.get_ensemble_energies()
-del n2, calc, ens
+for i,a in enumerate(list):
+    # ASE
+    atoms = molecule(a)
+    atoms.center(vacuum=3.0)
 
-# N atom
-n = Atoms('N')
-n.set_cell(cell)
-n.center()
-calc = GPAW(h=h, xc='PBE', convergence=conv, hund=True)
-n.set_calculator(calc)
-n.get_potential_energy()
-n.calc.set(xc=xc)
-e_n = n.get_potential_energy()
-ens = BEEF_Ensemble(n)
-de_n = ens.get_ensemble_energies()
-del n, calc, ens
+    # scf
+    calc = GPAW(xc='PBE',
+                h=h,
+                convergence=conv,
+                nbands=-2)
+    atoms.set_calculator(calc)
+    atoms.get_potential_energy()
+    atoms.calc.set(xc=xc)
+    atoms.get_potential_energy()
 
-# forces
-f0 = f[0].sum()
-f1 = f[1].sum()
-equal(f0, -f1, tol1)
+    # forces
+    if i == 0:
+        f = atoms.get_forces()
+        f0 = f[0].sum()
+        f1 = f[1].sum()
+        equal(f0, -f1, tol1)
 
-# binding energy
-E_bind = 2*e_n - e_n2
-dE_bind = 2*de_n[:] - de_n2[:]
-dE_bind = np.std(dE_bind)
-equal(E_bind, 9.7, tol2)
-equal(dE_bind, 0.4, tol2)
+    # BEE ensemble
+    ens = BEEF_Ensemble(calc)
+    ens.get_ensemble_energies()
+    ens.write(a)
+
+    del atoms, calc, ens
+
+# evaluate binding energy and ensemble error estimate
+barrier
+if rank == 0:
+    ens = BEEF_Ensemble()
+    e1, de1 = ens.read(list[0])
+    e2, de2 = ens.read(list[1])
+    e = 2*e2 - e1
+    de = np.std(2*de2 - de1)
+    equal(e, 9.8, tol2)
+    equal(de, 0.4, tol2)
