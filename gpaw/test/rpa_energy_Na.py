f83ae19fd3aa8499c012b93f97921d16257066da
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2014-10-15 10:22:03 +0000

    Revert part of last commit and clean up

--- gpaw/test/rpa_energy_Na.py
+++ gpaw/test/rpa_energy_Na.py
@@ -1,21 +1,14 @@
-import numpy as np
-from ase.units import Ha
-from ase.dft.kpoints import monkhorst_pack
-from ase.parallel import paropen
 from ase.lattice import bulk
-from gpaw import GPAW, FermiDirac
-from gpaw.wavefunctions.pw import PW
-from gpaw.mpi import size, serial_comm
+from gpaw import GPAW, FermiDirac, PW
+from gpaw.mpi import serial_comm
 from gpaw.xc.rpa import RPACorrelation
 from gpaw.test import equal
-
-kpts = monkhorst_pack((4,4,4))
-kpts += np.array([1/8., 1/8., 1/8.])
 
 bulk = bulk('Na', 'bcc', a=4.23)
 
 ecut = 350
-calc = GPAW(mode=PW(ecut),dtype=complex, basis='dzp', kpts=kpts,
+calc = GPAW(mode=PW(ecut), dtype=complex, basis='dzp',
+            kpts={'size': (4, 4, 4), 'gamma': True},
             parallel={'domain': 1}, txt='gs_occ_pw.txt', nbands=4,
             occupations=FermiDirac(0.01),
             setups={'Na': '1'},
@@ -24,11 +17,12 @@ bulk.set_calculator(calc)
 bulk.get_potential_energy()
 calc.write('gs_occ_pw.gpw')
 
-calc = GPAW('gs_occ_pw.gpw',txt='gs_pw.txt', parallel={'band': 1})
+calc = GPAW('gs_occ_pw.gpw', txt='gs_pw.txt', parallel={'band': 1})
 calc.diagonalize_full_hamiltonian(nbands=520)
 calc.write('gs_pw.gpw', 'all')
 
 ecut = 120
 calc = GPAW('gs_pw.gpw', communicator=serial_comm, txt=None)
-rpa = RPACorrelation(calc, txt='rpa_%s.txt' %(ecut))
-E = rpa.calculate(ecut=[50, 60])
+rpa = RPACorrelation(calc, txt='rpa_%s.txt' % ecut)
+E = rpa.calculate(ecut=[ecut])
+equal(E, -1.106, 0.005)
