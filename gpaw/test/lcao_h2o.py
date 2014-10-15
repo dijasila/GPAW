9235430b6678ed5033a2de790d973a517b8ca70b
kuismam <kuismam@6419b465-d902-0410-997c-be2266baf723>
2014-10-15 12:44:02 +0000

    Check LCAO gamma point calculations for complex wave functions.

--- gpaw/test/lcao_h2o.py
+++ gpaw/test/lcao_h2o.py
@@ -24,3 +24,9 @@ print('Err', err)
 assert err < 1e-4
 
 equal(niter, 8, 1)
+
+# Check that complex wave functions are allowed with gamma point calculations
+calc = GPAW(gpts=(32, 32, 32), nbands=4, mode='lcao', dtype=complex)
+mol.set_calculator(calc)
+ec = mol.get_potential_energy()
+equal(e, ec, 1e-5)
