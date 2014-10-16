86f8b5c1a3a2ae93ea6ba71377f33af4d6e8f25c
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2014-10-16 09:33:03 +0000

    Sometimes double quoted strings are needed.

--- gpaw/lrtddft/__init__.py
+++ gpaw/lrtddft/__init__.py
@@ -90,7 +90,7 @@ class LrTDDFT(ExcitationList):
                     'Linear response TDDFT supported only in real space mode')
             if calculator.wfs.kd.comm.size > 1:
                 err_txt = 'Spin parallelization with Linear response '
-                err_txt += 'TDDFT. Use parallel = {'domain' : 'domain_only'} '
+                err_txt += "TDDFT. Use parallel = {'domain' : 'domain_only'} "
                 err_txt += 'calculator parameter.'
                 raise NotImplementedError(err_txt)
             if self.xc == 'GS':
