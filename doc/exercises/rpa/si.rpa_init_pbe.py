e1dc1ea37b75e68d9930a88d14e4ee47bacbffdc
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2014-10-21 14:58:22 +0000

    Use different name for gpw-file so that it doesn't collide with gpw-file for EXX-calculation

--- doc/exercises/rpa/si.rpa_init_pbe.py
+++ doc/exercises/rpa/si.rpa_init_pbe.py
@@ -6,23 +6,19 @@ from gpaw.wavefunctions.pw import PW
 # Plane wave cutoff
 pwcutoff = 400.0
 
-# NxNxN k-point sampling, gamma-centred grid
+# NxNxN k-point sampling
 k = 4
 
 # Si lattice constant
-
 alat = 5.421
 
 # bulk calculation
-
 bulk_crystal = bulk('Si', 'diamond', a=alat)
 bulk_calc = GPAW(mode=PW(pwcutoff),
-                 kpts={'size': (k, k, k), 'gamma': True},
-                 dtype=complex,
+                 kpts={'size': (k, k, k), 'gamma': True},  # gamma-centred grid
                  xc='PBE',
                  txt='si.rpa.pbe_output.txt',
-                 parallel={'band': 1}
-                 )
+                 parallel={'band': 1})
 
 bulk_crystal.set_calculator(bulk_calc)
 e0_bulk_pbe = bulk_crystal.get_potential_energy()
@@ -32,6 +28,5 @@ e0_bulk_pbe = bulk_crystal.get_potential_energy()
 # the empty states as well (expensive)
 bulk_calc.diagonalize_full_hamiltonian(nbands=200)
 
-
 # the 'all' ensures we write wavefunctions too
-bulk_calc.write('bulk.gpw', mode='all')
+bulk_calc.write('bulk.all.gpw', mode='all')
