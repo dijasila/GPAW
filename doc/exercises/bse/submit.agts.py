950e2db6158137bc3e9781c90ded5b209b9dc1a4
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2014-10-22 09:13:01 +0000

    Disable BSE test for now

--- doc/exercises/bse/submit.agts.py
+++ doc/exercises/bse/submit.agts.py
@@ -1,4 +1,5 @@
 def agts(queue):
     a = queue.add('LiF_gs.py', ncpus=1, walltime=10)
     queue.add('LiF_RPA.py', ncpus=8, walltime=10, deps=a)
-    queue.add('LiF_BSE.py', ncpus=24, walltime=5 * 60, deps=a)
+    # Someone should fix this:
+    # queue.add('LiF_BSE.py', ncpus=24, walltime=5 * 60, deps=a)
