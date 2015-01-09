c271dfb117266fa67bcc868c04c9c8778fbdfdd5
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2015-01-09 09:29:10 +0000

    Test also text output.

--- gpaw/test/ts09.py
+++ gpaw/test/ts09.py
@@ -13,20 +13,29 @@ h = 0.4
 s = Cluster(molecule('Na2'))
 s.minimal_box(3., h=h)
 
+out_traj = 'H2.traj'
+out_txt = 'H2.txt'
                        
-cc = GPAW(h=h, xc='PBE')
+cc = GPAW(h=h, xc='PBE', txt=out_txt)
+
+# this is needed to initialize txt output
+cc.initialize(s)
+
 c = vdWTkatchenko09prl(HirshfeldPartitioning(cc),
                        vdWradii(s.get_chemical_symbols(), 'PBE'))
 s.set_calculator(c)
 E = s.get_potential_energy()
 F_ac = s.get_forces()
-s.write('H2.traj')
+s.write(out_traj)
 
 barrier()
 
-s_out = io.read('H2.traj')
-##print s_out.get_potential_energy(), E
-##print s_out.get_forces()
-equal(s_out.get_potential_energy(), E)
-for fi, fo in zip(F_ac, s_out.get_forces()):
-    equal(fi, fo)
+# test I/O, accuracy due to text output
+accuracy = 1.e-5
+for fname in [out_traj, out_txt]:
+    s_out = io.read(fname)
+    ##print s_out.get_potential_energy(), E
+    ##print s_out.get_forces()
+    equal(s_out.get_potential_energy(), E, accuracy)
+    for fi, fo in zip(F_ac, s_out.get_forces()):
+        equal(fi, fo, accuracy)
