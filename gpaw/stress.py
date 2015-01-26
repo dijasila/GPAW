Fix stress calculation for large molecules

gpaw/stress.py
@@ -4,6 +4,7 @@ import ase.units as units
 from gpaw.utilities import unpack
 from gpaw.wavefunctions.pw import PWWaveFunctions
 
+
 def stress(calc):
     wfs = calc.wfs
     dens = calc.density
@@ -15,7 +16,7 @@ def stress(calc):
     if ham.xc.orbital_dependent:
         raise NotImplementedError('Calculation of stress tensor is not ' +
                                   'implemented for orbital-dependent ' +
-                                  'XC functionals such as '+ham.xc.name)
+                                  'XC functionals such as ' + ham.xc.name)
 
     calc.timer.start('Stress tensor')
 
@@ -28,10 +29,10 @@ def stress(calc):
     p_G[1:] /= pd.G2_qG[0][1:]**2
     G_Gv = pd.get_reciprocal_vectors()
     for v1 in range(3):
-        s_vv[v1,v1] -= ham.epot
+        s_vv[v1, v1] -= ham.epot
         for v2 in range(3):
-            s_vv[v1,v2] += pd.integrate(p_G, dens.rhot_q *
-                                        G_Gv[:, v1] * G_Gv[:, v2])
+            s_vv[v1, v2] += pd.integrate(p_G, dens.rhot_q *
+                                         G_Gv[:, v1] * G_Gv[:, v2])
     s_vv += dens.ghat.stress_tensor_contribution(ham.vHt_q, dens.Q_aL)
 
     s_vv -= np.eye(3) * ham.ebar
@@ -52,7 +53,7 @@ def stress(calc):
             s0 += np.vdot(P_ni, a_ni)
             a_ani[a] = 2 * a_ni.conj()
         s0_vv += wfs.pt.stress_tensor_contribution(kpt.psit_nG, a_ani,
-                                               q=kpt.q)
+                                                   q=kpt.q)
     s0_vv -= s0.real * np.eye(3)
     wfs.bd.comm.sum(s0_vv)
     wfs.kd.comm.sum(s0_vv)