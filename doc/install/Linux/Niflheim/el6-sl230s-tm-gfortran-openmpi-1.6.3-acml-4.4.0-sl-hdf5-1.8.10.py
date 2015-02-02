b6c4ab6e9d6325e9a1b600fd6f10b48a41056114
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2015-02-02 14:05:39 +0000

    go back to acml. openblas turned out to result in scf convergence problems for some systems - see revision 12393

--- doc/install/Linux/Niflheim/el6-sl230s-tm-gfortran-openmpi-1.6.3-acml-4.4.0-sl-hdf5-1.8.10.py
+++ doc/install/Linux/Niflheim/el6-sl230s-tm-gfortran-openmpi-1.6.3-acml-4.4.0-sl-hdf5-1.8.10.py
@@ -19,12 +19,12 @@ library_dirs =[
     '/home/opt/el6/' + nodetype + '/scalapack-2.0.2-' + nodetype + '-tm-gfortran-openmpi-1.6.3-acml-4.4.0-1/lib',
     '/home/opt/common/acml-gfortran-64bit-4.4.0/lib',
     '/home/opt/el6/' + nodetype + '/hdf5-1.8.10-' + nodetype + '-tm-gfortran-openmpi-1.6.3-1/lib',
-    '/home/opt/el6/' + nodetype + '/libxc-2.2.0-' + nodetype + '-gfortran-1/lib',
+    '/home/opt/el6/' + nodetype + '/libxc-2.2.1-' + nodetype + '-gfortran-1/lib',
     ]
 include_dirs +=[
     '/home/opt/el6/' + nodetype + '/openmpi-1.6.3-' + nodetype + '-tm-gfortran-1/include',
     '/home/opt/el6/' + nodetype + '/hdf5-1.8.10-' + nodetype + '-tm-gfortran-openmpi-1.6.3-1/include',
-    '/home/opt/el6/' + nodetype + '/libxc-2.2.0-' + nodetype + '-gfortran-1/include',
+    '/home/opt/el6/' + nodetype + '/libxc-2.2.1-' + nodetype + '-gfortran-1/include',
     ]
 extra_link_args =[
     '-Wl,-rpath=/home/opt/el6/' + nodetype + '/openmpi-1.6.3-' + nodetype + '-tm-gfortran-1/lib'
