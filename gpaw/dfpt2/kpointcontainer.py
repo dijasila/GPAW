eb48f334c3d927d2db0b94dff654d4b2842ea670
rbw <rbw@6419b465-d902-0410-997c-be2266baf723>
2015-03-19 11:42:32 +0000

    Copy of folder dfpt to dfpt2. Avoids breaking stuff. Hacked to be able to run single q instead of band-structure. DIRTY.

---
+++ gpaw/dfpt2/kpointcontainer.py
@@ -0,0 +1,14 @@
+class KPointContainer:
+    """Simple container for storing k-dependent quantities."""
+    def __init__(self, **kwargs):
+        """Init attributes."""
+ 
+        self.set(**kwargs)
+
+    def set(self, **kwargs):
+        """Set attributes from the provided keyword arguments."""
+
+        for key, value in kwargs.iteritems():
+            
+            assert type(key) == str
+            setattr(self, key, value)
