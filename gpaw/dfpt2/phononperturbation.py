dfpt2: Hack to allow time_reversal in GS calculation. Just call with ibz index instead of the bz one. Probably wrong for PAW.

gpaw/dfpt2/phononperturbation.py
@@ -244,26 +244,6 @@ class PhononPerturbation(Perturbation):
 
         return self.phase_cd
 
-
-
-
-
-    def has_q(self):
-        """Overwrite base class member function."""
-
-        return (not self.gamma)
-
-    def get_q(self):
-        """Return q-vector."""
-
-        assert not self.gamma, "Gamma-point calculation."
-        
-        return self.kd.ibzk_qc[self.q]
-    
-
-
-
-        
     def apply(self, psi_nG, y_nG, wfs, k, kplusq):
         """Apply perturbation to unperturbed wave-functions.
 
@@ -279,7 +259,7 @@ class PhononPerturbation(Perturbation):
             Index of the k-point for the vectors.
         kplusq: int
             Index of the k+q vector.
-            
+
         """
 
         assert self.a is not None
@@ -304,34 +284,34 @@ class PhononPerturbation(Perturbation):
             Index of the k-point being operated on.
         kplusq: int
             Index of the k+q vector.
-            
+
         """
 
         assert self.a is not None
         assert self.v is not None
         assert psi_nG.ndim in (3, 4)
         assert tuple(self.gd.n_c) == psi_nG.shape[-3:]
-        
+
         if psi_nG.ndim == 3:
             n = 1
         else:
-            n = psi_nG.shape[0] 
-            
+            n = psi_nG.shape[0]
+
         a = self.a
         v = self.v
-        
+
         P_ani = wfs.kpt_u[k].P_ani
         dP_aniv = wfs.kpt_u[k].dP_aniv
         pt = wfs.pt
-        
+
         # < p_a^i | Psi_nk >
         P_ni = P_ani[a]
         # < dp_av^i | Psi_nk > - remember the sign convention of the derivative
         dP_ni = -1 * dP_aniv[a][...,v]
-        
+
         # Expansion coefficients for the projectors on atom a
         dH_ii = unpack(self.dH_asp[a][0])
-       
+
         # The derivative of the non-local PAW potential has two contributions
         # 1) Sum over projectors
         c_ni = np.dot(dP_ni, dH_ii)
@@ -347,3 +327,32 @@ class PhononPerturbation(Perturbation):
         dc_ani[a] = -1 * dc_ni
         # k+q !!
         pt.add_derivative(a, v, y_nG, dc_ani, q=kplusq)
+
+
+
+
+
+
+
+
+
+
+    def has_q(self):
+        """Overwrite base class member function."""
+
+        return (not self.gamma)
+
+    def get_q(self):
+        """Return q-vector."""
+
+        assert not self.gamma, "Gamma-point calculation."
+        
+        return self.kd.ibzk_qc[self.q]
+    
+
+
+
+        
+
+
+

gpaw/dfpt2/preconditioner.py
@@ -15,17 +15,17 @@ class ScipyPreconditioner:
         ----------
         gd: GridDescriptor
             Coarse grid
-            
+
         """
 
         self.project = project
         self.gd = gd
         # K-point for the preconditioner
         self.kpt = None
-        
+
         kin = Laplace(gd, scale=-0.5, n=3, dtype=dtype)
         self.pc = Preconditioner(gd, kin, dtype=dtype)
-        
+
         # For scipy's linear solver
         N = np.prod(gd.n_c)
         self.shape = (N,N)
@@ -42,7 +42,7 @@ class ScipyPreconditioner:
         """
 
         self.kpt = kpt
-        
+
     def matvec(self, x):
         """Matrix vector multiplication for ``scipy.sparse.linalg`` solvers.
 
@@ -59,7 +59,7 @@ class ScipyPreconditioner:
 
         size = x.size
         assert size == np.prod(shape)
-        
+
         x_G = x.reshape(shape)
 
         # Call gpaw preconditioner
@@ -67,7 +67,7 @@ class ScipyPreconditioner:
 
         # Project out undesired (numerical) components
         self.project(y_G)
-        
+
         y = y_G.ravel()
-        
+
         return y

gpaw/dfpt2/sternheimeroperator.py
@@ -5,7 +5,7 @@ Sternheimer equation::
        /           \             q
        | H - eps   | P      |dPsi  > = - P      dV  |Psi  > ,
        \        nk /  c,k+q      nk       c,k+q   q     nk
-   
+
 where P_c is the projection operator onto the unoccupied states. For a
 perturbation with a specific q-vector, only the projections onto states at
 k+q are needed.
@@ -14,13 +14,13 @@ k+q are needed.
 
 import numpy as np
 
+from gpaw.fd_operators import Laplace
 from gpaw.utilities import unpack
 from gpaw.utilities.blas import gemm
-from gpaw.fd_operators import Laplace
 
 class SternheimerOperator:
     """Class implementing the linear operator in the Sternheimer equation.
-    
+
     The main purpose of this class is to implement the multiplication of the
     linear operator (H - eps_nk) with a vector in the ``apply`` member
     function. An additional ``matvec`` member function has been defined so that
@@ -28,7 +28,7 @@ class SternheimerOperator:
     iterative Krylov solvers in ``scipy.sparse.linalg``.
 
     """
-    
+
     def __init__(self, hamiltonian, wfs, gd, dtype=float):
         """Save useful objects for the Sternheimer operator.
 
@@ -47,6 +47,7 @@ class SternheimerOperator:
 
         self.hamiltonian = hamiltonian
         self.kin = Laplace(gd, scale=-0.5, n=3, dtype=dtype)
+
         self.kpt_u = wfs.kpt_u
         self.pt = wfs.pt
         self.gd = gd
@@ -55,7 +56,7 @@ class SternheimerOperator:
         self.k = None
         self.n = None
         self.kplusq = None
-        
+
         # For scipy's linear solver
         N = np.prod(gd.n_c)
         self.shape = (N, N)
@@ -65,7 +66,7 @@ class SternheimerOperator:
         """Set k-vector for the Bloch-state in consideration."""
 
         self.k = k
-        
+
     def set_band(self, n):
         """Set band index for the Bloch-state in consideration.
 
@@ -94,14 +95,14 @@ class SternheimerOperator:
         For the eigenvalue term the k-point is the one of the state.
         For the other terms the k-point to be used is the one given by the k+q
         phase of the first-order of the state. Only for q=0 do the two coincide.
-        
+
         Parameters
         ----------
         x_nG: ndarray
             Vector(s) to which the Sternheimer operator is applied.
         y_nG: ndarray
             Resulting vector(s).
-            
+
         """
 
         assert x_nG.ndim in (3, 4)
@@ -126,13 +127,14 @@ class SternheimerOperator:
         P_ani = self.pt.dict(shape=shape)
 
         # k+q 
-        self.pt.integrate(x_nG, P_ani, q=kplusqpt.k)
+        # BUG ??? q in reducible or irreducible
+        self.pt.integrate(x_nG, P_ani, q=kplusqpt.ik)
 
         for a, P_ni in P_ani.items():
             dH_ii = unpack(self.hamiltonian.dH_asp[a][kpt.s])
             P_ani[a] = np.dot(P_ni, dH_ii)
         # k+q
-        self.pt.add(y_nG, P_ani, q=kplusqpt.k)
+        self.pt.add(y_nG, P_ani, q=kplusqpt.ik)
 
         # XXX Eigenvalue term
         if self.n is not None:
@@ -152,11 +154,11 @@ class SternheimerOperator:
 
         The projection operator is defined as follows::
 
-                      --                    --             
+                      --                    --
              P      = >  |Psi ><Psi | = 1 - >  |Psi ><Psi |
-              c,k+q   --     c     c        --     v     v 
+              c,k+q   --     c     c        --     v     v
                        c    k+q   k+q        v    k+q   k+q
-        
+
         """
 
         # It might be a good idea to move this functionality to its own class
@@ -180,7 +182,7 @@ class SternheimerOperator:
         ## 
         ##     proj = self.gd.integrate(psit_G.conjugate() * x_nG)
         ##     x_nG -= proj * psit_G
-        
+
     def matvec(self, x):
         """Matrix-vector multiplication for scipy's Krylov solvers.
 

gpaw/dfpt2/wavefunctions.py
@@ -8,7 +8,7 @@ from gpaw.dfpt2.kpointcontainer import KPointContainer
 
 class WaveFunctions:
     """Class for wave-function related stuff (e.g. projectors)."""
-    def __init__(self, nbands, kpt_u, setups, kd, gd, dtype=float):
+    def __init__(self, nbands, kpt_u, setups, kd, gd, spos_ac, dtype=float):
         """Store and initialize required attributes.
 
         Parameters
@@ -35,8 +35,12 @@ class WaveFunctions:
         # Number of occupied bands
         self.nbands = nbands
         # Projectors
+        # BUG The lfc is initialized with the ground-state kd aka with the
+        # irreducible information, while it is later called from kpt_u, which
+        # contains only the unfolded full brilluoin zone information.
         self.pt = LFC(gd, [setup.pt_j for setup in setups], kd,
                       dtype=self.dtype)
+        self.pt.set_positions(spos_ac)
         # Store grid
         self.gd = gd
 
@@ -58,24 +62,31 @@ class WaveFunctions:
                 for n, psit_G in enumerate(psit_nG):
                     psit_G[:] = kpt_.psit_nG[n]
 
+                P_ani = []
+                for a in range(len(kpt_.P_ani)):
+                    P_ni = kpt_.P_ani[a][:nbands]
+                    P_ani.append(P_ni)
+
+                dP_aniv = self.calculate_projector_coef(n=self.nbands, q=k,
+                                                        psit_nG=psit_nG)
+
                 # Strip off k-point attributes and store in the KPointContainer
                 # Note, only the occupied GS wave-functions are retained here!
                 kpt = KPointContainer(weight=kpt_.weight,
                                       k=kpt_.k,
+                                      ik=kpt_.k,
                                       s=kpt_.s,
                                       phase_cd=kpt_.phase_cd,
                                       eps_n=kpt_.eps_n[:nbands],
                                       psit_nG=psit_nG,
                                       psit1_nG=None,
-                                      P_ani=None,
-                                      dP_aniv=None)
+                                      P_ani=P_ani,
+                                      dP_aniv=dP_aniv)
 
                 self.kpt_u.append(kpt)
         else:
             # If the ground-state calculation used symmetries, map now every-
             # thing back to the full BZ.
-            # XXX Actually one should rather make the rest work with the IBZ.
-            #     Later....
             assert len(kpt_u) == kd.nibzkpts
 
             for k, k_c in enumerate(kd.bzk_kc):
@@ -99,21 +110,38 @@ class WaveFunctions:
                 phase_cd = np.exp(2j * pi * gd.sdisp_cd * k_c[:, np.newaxis])
 
                 psit_nG = gd.empty(nbands, dtype=self.dtype)
+
+                # NOTE When we introduce point group symmetry, we actually have
+                # to rotate P_ani
+                P_ani = []
+                for a in range(len(kpt_.P_ani)):
+                    P_ni = kpt_.P_ani[a][:nbands]
+                    if time_reversal:
+                        P_ni = P_ni.conj()
+                    P_ani.append(P_ni)
+
                 for n, psit_G in enumerate(psit_nG):
                     # Rotate wave function of the irreducible point to the
                     # full k-point
-                    psit_G[:] = kd.symmetry.symmetrize_wavefunction(
-                        kpt_.psit_nG[n], ik_c, k_c, op_cc, time_reversal)
+                    #psit_G[:] = kd.symmetry.symmetrize_wavefunction(
+                    #    kpt_.psit_nG[n], ik_c, k_c, op_cc, time_reversal)
+                    psit_G[:] = kd.transform_wave_function(kpt_.psit_nG[n], k)
+
+                # BUG no clue what symmetry adaption dP_aniv needs. PAW not
+                # working yet
+                dP_aniv = self.calculate_projector_coef(n=self.nbands, q=ik,
+                                                        psit_nG=kpt_.psit_nG[:nbands])
 
                 kpt = KPointContainer(weight=weight,
                                       k=k,
+                                      ik=ik,
                                       s=kpt_.s,
                                       phase_cd=phase_cd,
                                       eps_n=kpt_.eps_n[:nbands],
                                       psit_nG=psit_nG,
                                       psit1_nG=None,
-                                      P_ani=None,
-                                      dP_aniv=None)
+                                      P_ani=P_ani,
+                                      dP_aniv=dP_aniv)
 
                 self.kpt_u.append(kpt)
 
@@ -121,10 +149,10 @@ class WaveFunctions:
         """Initialize projectors according to the ``gamma`` attribute."""
 
         # Set positions on LFC's
-        self.pt.set_positions(spos_ac)
+        #self.pt.set_positions(spos_ac)
         
         # Calculate projector coefficients for the GS wave-functions
-        self.calculate_projector_coef()
+        ####self.calculate_projector_coef()
 
     def reset(self):
         """Make fresh arrays for wave-function derivatives."""
@@ -132,7 +160,7 @@ class WaveFunctions:
         for kpt in self.kpt_u:
             kpt.psit1_nG = self.gd.zeros(n=self.nbands, dtype=self.dtype)
         
-    def calculate_projector_coef(self):
+    def calculate_projector_coef(self, n=None, q=None, psit_nG=None):
         """Coefficients for the derivative of the non-local part of the PP.
 
         Parameters
@@ -165,22 +193,42 @@ class WaveFunctions:
 
         """
 
-        n = self.nbands
+        if n is None:
+            n = self.nbands
 
-        for kpt in self.kpt_u:
+        if q is None:
+            assert kpt is None
+            for kpt in self.kpt_u:
+                # K-point index and wave-functions
+                ik = kpt.ik
+                psit_nG = kpt.psit_nG
 
-            # K-point index and wave-functions
-            k = kpt.k
-            psit_nG = kpt.psit_nG
-            
+                # Integration dicts
+                P_ani   = self.pt.dict(shape=n)
+                dP_aniv = self.pt.dict(shape=n, derivative=True)
+
+                # 1) Integrate with projectors
+                self.pt.integrate(psit_nG, P_ani, q=ik)
+                kpt.P_ani = P_ani
+
+                # 2) Integrate with derivative of projectors
+                self.pt.derivative(psit_nG, dP_aniv, q=ik)
+                kpt.dP_aniv = dP_aniv
+
+        else:
+            assert psit_nG is not None
+
             # Integration dicts
-            P_ani   = self.pt.dict(shape=n)
+            #P_ani   = self.pt.dict(shape=n)
             dP_aniv = self.pt.dict(shape=n, derivative=True)
-    
+
+            # XXX Skip this, should have been done in init
             # 1) Integrate with projectors
-            self.pt.integrate(psit_nG, P_ani, q=k)
-            kpt.P_ani = P_ani
-            
+            #self.pt.integrate(psit_nG, P_ani, q=k)
+            #kpt.P_ani = P_ani
+
             # 2) Integrate with derivative of projectors
-            self.pt.derivative(psit_nG, dP_aniv, q=k)
-            kpt.dP_aniv = dP_aniv
+            self.pt.derivative(psit_nG, dP_aniv, q=q)
+
+            return dP_aniv
+