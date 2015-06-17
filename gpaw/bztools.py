55b581d75645985a4199865f6508bd3cdc6c9c87
mogje <mogje@6419b465-d902-0410-997c-be2266baf723>
2015-06-17 06:43:52 +0000

    Some optimizations. Changed the tesselation method.

--- gpaw/bztools.py
+++ gpaw/bztools.py
@@ -1,103 +1,95 @@
+from __future__ import division, print_function
+
 import numpy as np
 
 from itertools import product
 
 from scipy.spatial import Delaunay, Voronoi
 
+from ase.units import Bohr
+
 from gpaw import GPAW
 from gpaw.symmetry import Symmetry
 
-
-def generate_convex_coeff(NK, N=1):
-    a_n = np.linspace(0, 1.0, N + 2)
-    gen = product(*([a_n] * NK))
-
-    for a_k in gen:
-        a_k = np.array(a_k)
-        if np.abs(a_k.sum() - 1) < 1e-10:
-            yield a_k
+def get_lattice_symmetry(cell_cv):
+    latsym = Symmetry([0], cell_cv)
+    latsym.find_lattice_symmetry()
+    return latsym
 
 
-def refine_simplex(simplex_pv, N=1):
-    # Refine simplex
-    assert len(simplex_pv) < 5
-    
-    newpoints_pv = []
-    n = len(simplex_pv)
-    for a_p in generate_convex_coeff(n, N):
-        p_v = np.dot(a_p, simplex_pv)
-        newpoints_pv.append(p_v.tolist())
-    
-    return newpoints_pv
-
-
-def refine_grid(input_pv, N):
-    tri = Delaunay(input_pv)
-    points_pv = tri.points
-    newpoints_pv = []
-
-    for simplex in tri.simplices:
-        simplex_pv = points_pv[simplex]
-        rpoints_pv = refine_simplex(simplex_pv, N)
-        if len(newpoints_pv):
-            newpoints_pv.extend(rpoints_pv)
-        else:
-            newpoints_pv = rpoints_pv
-
-    if not len(newpoints_pv):
-        return input_pv
-
-    rpoints_pv = np.append(points_pv,
-                           np.array(newpoints_pv), axis=0)
-
-    return unique_rows(rpoints_pv, tol=1e-7)
-
-
-def tesselate_brillouin_zone(calc, N=5):
+def tesselate_brillouin_zone(calc, density=3.5):
     """Refine kpoint grid of previously calculated ground state."""
     if isinstance(calc, str):
         calc = GPAW(calc, txt=None)
-    bzk_kc, ibzk_kc = get_BZ(calc)
+
     cell_cv = calc.wfs.gd.cell_cv
     B_cv = np.linalg.inv(cell_cv).T * 2 * np.pi
     A_cv = cell_cv / (2 * np.pi)
+
+    # Tesselate lattice IBZ
+    latsym = get_lattice_symmetry(cell_cv)
+    lU_scc = get_symmetry_operations(latsym.op_scc,
+                                     latsym.time_reversal)
+    ibzk_kc = get_IBZ_vertices(cell_cv,
+                               U_scc=latsym.op_scc,
+                               time_reversal=latsym.time_reversal)
     ibzk_kv = np.dot(ibzk_kc, B_cv)
 
-    ibzk_kv = refine_grid(ibzk_kv, N)
+    # This could probably be optimized
+    density = density / Bohr
+    refining = True
+    while refining:
+        refining = False
+        tess = Delaunay(ibzk_kv)
+        newpoints_pv = []
+        for simplex in tess.simplices:
+            k_kv = tess.points[simplex]
+            for k1_v in k_kv:
+                for k2_v in k_kv:
+                    dist = ((k1_v - k2_v)**2).sum()**0.5 / (2 * np.pi)
+                    if dist > 1 / density:
+                        p_v = (k1_v + k2_v) / 2
+                        newpoints_pv.append(p_v)
+                        refining = True
+
+        if len(newpoints_pv):
+            ibzk_kv = np.append(ibzk_kv, newpoints_pv, axis=0)
+        ibzk_kv = unique_rows(ibzk_kv, tol=1e-6)
+
     ibzk_kc = np.dot(ibzk_kv, A_cv.T)
 
     symmetry = calc.wfs.kd.symmetry
     U_scc = get_symmetry_operations(symmetry.op_scc,
                                     symmetry.time_reversal)
+
+    # Fold out to crystal IBZ
+    ibzk_kc = expand_ibz(lU_scc, U_scc, ibzk_kc)
+
+    # Find full BZ
     bzk_kc = unique_rows(np.concatenate(np.dot(ibzk_kc,
                                                U_scc.transpose(0, 2, 1))))
 
-    # Remove points which are equivalent
+    # Remove BZ points which are equivalent
     i = 0
     while i < len(bzk_kc):
         k1_c = bzk_kc[i]
-        j = i + 1
-        while j < len(bzk_kc):
-            k2_c = bzk_kc[j]
-            diff_c = np.abs((k2_c - k1_c) - (k2_c - k1_c).round())
-            if (diff_c < 1e-7).all():
-                bzk_kc = np.delete(bzk_kc, j, axis=0)
-                j -= 1
-            j += 1
+        dk_kc = bzk_kc[i + 1:] - k1_c
+        diff_k = np.abs(dk_kc - dk_kc.round()).sum(1)
+        inds_k = np.nonzero(diff_k < 1e-7)[0]
+        bzk_kc = np.delete(bzk_kc, inds_k + i + 1, axis=0)
         i += 1
 
     return bzk_kc
 
 
-def unique_rows(ain, tol=1e-10):
-    # Round
-    a = ain.copy().round((-np.log10(tol).astype(int)))
-    order = np.lexsort(a.T)
-    a = a[order].copy()
-    diff = np.diff(a, axis=0)
+def unique_rows(a, tol=1e-10):
+    order = np.lexsort(a.T)  # Lexical ordering
+    a = a[order]
+    # Round off difference betwwen points
+    diff = np.diff(a, axis=0).round((-np.log10(tol).astype(int)))
     ui = np.ones(len(a), 'bool')
     ui[1:] = (diff != 0).any(1)
-    return ain[order][ui]
+    return a[ui]
 
 
 def get_smallest_Gvecs(cell_cv, n=5):
@@ -128,7 +120,6 @@ def get_symmetry_operations(U_scc, time_reversal):
 
 def get_IBZ_vertices(cell_cv, U_scc=None,
                      time_reversal=None, tol=1e-7):
-    
     # Choose an origin
     origin_c = np.array([0.12, 0.22, 0.21], float)
 
@@ -175,26 +166,38 @@ def get_IBZ_vertices(cell_cv, U_scc=None,
 def get_BZ(calc):
     if isinstance(calc, str):
         calc = GPAW(calc, txt=None)
-    symmetry = calc.wfs.kd.symmetry
     cell_cv = calc.wfs.gd.cell_cv
     
-    # The BZ is just the IBZ
-    # without symmetries
-    bzk_kc = get_IBZ_vertices(cell_cv, tol=1e-7)
-
-    # Use the symmetries to find
-    # the IBZ
-    latsym = Symmetry([0], cell_cv)
-    latsym.find_lattice_symmetry()
+    # Crystal symmetries
+    symmetry = calc.wfs.kd.symmetry
     cU_scc = get_symmetry_operations(symmetry.op_scc,
                                      symmetry.time_reversal)
+
+    return get_reduced_BZ(cell_cv, cU_scc, False)
+
+
+def get_reduced_BZ(cell_cv, cU_scc, time_reversal):
+    # Lattice symmetries
+    latsym = get_lattice_symmetry(cell_cv)
     lU_scc = get_symmetry_operations(latsym.op_scc,
                                      latsym.time_reversal)
     
+    # Find Lattice IBZ
     ibzk_kc = get_IBZ_vertices(cell_cv,
                                U_scc=latsym.op_scc,
                                time_reversal=latsym.time_reversal)
 
+    # Expand lattice IBZ to crystal IBZ
+    ibzk_kc = expand_ibz(lU_scc, cU_scc, ibzk_kc)
+
+    # Fold out to full BZ
+    bzk_kc = unique_rows(np.concatenate(np.dot(ibzk_kc,
+                                               cU_scc.transpose(0, 2, 1))))
+
+    return bzk_kc, ibzk_kc
+
+
+def expand_ibz(lU_scc, cU_scc, ibzk_kc):
     # Find right cosets
     cosets = []
     Utmp_scc = lU_scc.copy()
@@ -225,60 +228,4 @@ def get_BZ(calc):
 
     ibzk_kc = unique_rows(np.concatenate(nibzk_ikc))
 
-    bzk_kc = unique_rows(np.concatenate(np.dot(ibzk_kc,
-                                               cU_scc.transpose(0, 2, 1))))
-
-    return bzk_kc, ibzk_kc
-
-
-def get_reduced_BZ(cell_cv, U_scc, time_reversal):
-    # The BZ is just the IBZ
-    # without symmetries
-    bzk_kc = get_IBZ_vertices(cell_cv, tol=1e-7)
-
-    # Use the symmetries to find
-    # the IBZ
-    latsym = Symmetry([0], cell_cv)
-    latsym.find_lattice_symmetry()
-    cU_scc = get_symmetry_operations(U_scc, time_reversal)
-    lU_scc = get_symmetry_operations(latsym.op_scc,
-                                     latsym.time_reversal)
-    
-    ibzk_kc = get_IBZ_vertices(cell_cv,
-                               U_scc=latsym.op_scc,
-                               time_reversal=latsym.time_reversal)
-
-    # Find right cosets
-    cosets = []
-    Utmp_scc = lU_scc.copy()
-    while len(Utmp_scc):
-        U1_cc = Utmp_scc[0]
-        Utmp_scc = np.delete(Utmp_scc, 0, axis=0)
-        j = 0
-        new_coset = [U1_cc]
-        while j < len(Utmp_scc):
-            U2_cc = Utmp_scc[j]
-            U3_cc = np.dot(U1_cc, np.linalg.inv(U2_cc))
-            if (U3_cc == cU_scc).all(2).all(1).any():
-                new_coset.append(U2_cc)
-                Utmp_scc = np.delete(Utmp_scc, j, axis=0)
-                j -= 1
-            j += 1
-        cosets.append(new_coset)
-
-    nibzk_ikc = []
-    for U_scc in cosets:
-        ibzk_ksc = np.dot(ibzk_kc, np.array(U_scc).transpose((0, 2, 1)))
-        dist_ks = ((ibzk_ksc -
-                    ibzk_kc[:, np.newaxis])**2).sum(-1)**0.5
-        dist_s = dist_ks.sum(0)
-        s = np.argmin(dist_s)
-        U_cc = U_scc[s]
-        nibzk_ikc.append(np.dot(ibzk_kc, U_cc.T))
-
-    ibzk_kc = unique_rows(np.concatenate(nibzk_ikc))
-
-    bzk_kc = unique_rows(np.concatenate(np.dot(ibzk_kc,
-                                               cU_scc.transpose(0, 2, 1))))
-
-    return bzk_kc, ibzk_kc
+    return ibzk_kc
