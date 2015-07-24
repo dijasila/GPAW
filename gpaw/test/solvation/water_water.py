de7ef123634b06475709c83687bb17c2a697ec8d
aheld <aheld@6419b465-d902-0410-997c-be2266baf723>
2015-07-24 13:45:22 +0000

    Adds convenient helper function for water solvation parameters.
    * adds helper function
    * modifies solvation/water_water.py test to use helper function
    * adds information about helper function to CSM tutorial

--- gpaw/test/solvation/water_water.py
+++ gpaw/test/solvation/water_water.py
@@ -2,28 +2,16 @@ from gpaw import GPAW
 from gpaw.cluster import Cluster
 from gpaw.test import equal
 from ase.structure import molecule
-from ase.units import mol, kcal, Pascal, m
-from ase.data.vdw import vdw_radii
+from ase.units import mol, kcal
 from gpaw.solvation import (
     SolvationGPAW,
-    EffectivePotentialCavity,
-    Power12Potential,
-    LinearDielectric,
-    GradientSurface,
-    SurfaceInteraction,
+    make_HW14_water_kwargs,
 )
 
 SKIP_VAC_CALC = True
 
 h = 0.24
 vac = 4.0
-u0 = 0.180
-epsinf = 78.36
-st = 18.4 * 1e-3 * Pascal * m
-T = 298.15
-vdw_radii = vdw_radii.copy()
-vdw_radii[1] = 1.09
-atomic_radii = lambda atoms: [vdw_radii[n] for n in atoms.numbers]
 convergence = {
     'energy': 0.05 / 8.,
     'density': 10.,
@@ -43,12 +31,8 @@ else:
 
 atoms.calc = SolvationGPAW(
     xc='PBE', h=h, convergence=convergence,
-    cavity=EffectivePotentialCavity(
-        effective_potential=Power12Potential(atomic_radii, u0),
-        temperature=T,
-        surface_calculator=GradientSurface()),
-    dielectric=LinearDielectric(epsinf=epsinf),
-    interactions=[SurfaceInteraction(surface_tension=st)])
+    **make_HW14_water_kwargs()
+)
 Ewater = atoms.get_potential_energy()
 Eelwater = atoms.calc.get_electrostatic_energy()
 Esurfwater = atoms.calc.get_solvation_interaction_energy('surf')
