ec9841a7d2ab8120abb929548a29111a6c1e32f8
tgmaxson <tgmaxson@6419b465-d902-0410-997c-be2266baf723>
2015-08-20 19:25:26 +0000

    revert customize

--- customize.py
+++ customize.py
@@ -1,73 +1,86 @@
-#User provided customizations for the gpaw setup
- 
+er provided customizations for the gpaw setup
+
+#Here, one can override the default arguments, or append own
+#arguments to default ones
+#To override use the form
+#     libraries = ['somelib','otherlib']
+#To append use the form
+#     libraries += ['somelib','otherlib']
+
+import os
+if 'GPAW_MPI' in os.environ:
+    # Build MPI-interface into _gpaw.so:
+    compiler = 'mpicc'
+    define_macros += [('PARALLEL', '1')]
+    mpicompiler = None
+    
+#compiler = 'mpcc'
+#libraries = []
+#libraries += []
+
+#library_dirs = []
+#library_dirs += []
+
+#include_dirs = []
+#include_dirs += []
+
+#extra_link_args = []
+#extra_link_args += []
+
+#extra_compile_args = []
+#extra_compile_args += []
+
+#runtime_library_dirs = []
+#runtime_library_dirs += []
+
+#extra_objects = []
+#extra_objects += []
+
+#define_macros = []
+#define_macros += []
+
+#mpicompiler = None
+#mpilinker = None
+#mpi_libraries = []
+#mpi_libraries += []
+
+#mpi_library_dirs = []
+#mpi_library_dirs += []
+
+#mpi_include_dirs = []
+#mpi_include_dirs += []
+
+#mpi_runtime_library_dirs = []
+#mpi_runtime_library_dirs += []
+
+#mpi_define_macros = []
+#mpi_define_macros += []
+
+#platform_id = ''
+
 #hdf5 = True
-scalapack = True
- 
+
+# Valid values for scalapack are False, or True:
+# False (the default) - no ScaLapack compiled in
+# True - ScaLapack compiled in
+# Warning! At least scalapack 2.0.1 is required!
+# See https://trac.fysik.dtu.dk/projects/gpaw/ticket/230
+scalapack = False
+
 if scalapack:
-    define_macros = [('GPAW_NO_UNDERSCORE_CBLACS', '1')]
+    libraries += ['scalapack']
+    library_dirs += []
+    define_macros += [('GPAW_NO_UNDERSCORE_CBLACS', '1')]
     define_macros += [('GPAW_NO_UNDERSCORE_CSCALAPACK', '1')]
- 
-#opt_string = '-prof-use'
- 
-compiler = 'icc'
-linker = 'icc ' #+ opt_string
- 
-extra_compile_args = [
-    '-w',
-    '-xhost',
-    #'-no-prec-div',
-    '-O3',
-    '-ipo',
-    '-funroll-all-loops',
-    '-fPIC',
-    #'-static',
-    '-std=c99',
-    #opt_string,
-    ]
- 
-libraries = [
-    'xc',
-    'mpi',
-    ]
- 
-mkl_lib_path = '/apps/rhel6/intel/composer_xe_2015.1.133/mkl/lib/intel64/'
- 
-extra_link_args = [
-    '/apps/rhel6/libxc-2.2.0/lib/libxc.a',
-    mkl_lib_path + 'libmkl_scalapack_lp64.a',
-    '-Wl,--start-group',
-    mkl_lib_path + 'libmkl_intel_lp64.a',
-    mkl_lib_path + 'libmkl_sequential.a',
-    mkl_lib_path + 'libmkl_core.a',
-    mkl_lib_path + 'libmkl_blacs_intelmpi_lp64.a',
-    '-Wl,--end-group',
-    '-lpthread',
-    '-lm',
-    ]
- 
-library_dirs = [
-    '/apps/rhel6/intel/composer_xe_2015.1.133/mkl/lib/intel64',
-    '/apps/rhel6/intel/composer_xe_2015.1.133/mkl/compiler/intel64',
-    '/apps/rhel6/intel/impi/5.0.2.044/lib64',
-    '/apps/rhel6/libxc-2.2.0/include',
-    ]
- 
-include_dirs = [
-    '/apps/rhel6/intel/composer_xe_2015.1.133/mkl/lib/intel64',
-    '/apps/rhel6/intel/composer_xe_2015.1.133/mkl/compiler/intel64',
-    '/apps/rhel6/intel/impi/5.0.2.044/include64'
-    ]
- 
-mpicompiler = 'mpiicc'
-mpilinker = mpicompiler + ' ' #+ opt_string
- 
-mpi_libraries = [
-#'hdf5',
-#'z',
-]
- 
-mpi_library_dirs = ['/apps/rhel6/intel/impi/5.0.2.044/lib64']
- 
-mpi_include_dirs = ['/apps/rhel6/intel/impi/5.0.2.044/include64']
- 
-define_macros += [('GPAW_MKL','1')] 
+
+# In order to link libxc installed in a non-standard location
+# (e.g.: configure --prefix=/home/user/libxc-2.0.1-1), use:
+# - static linking:
+#include_dirs += ['/home/user/libxc-2.0.1-1/include']
+#extra_link_args += ['/home/user/libxc-2.0.1-1/lib/libxc.a']
+#if 'xc' in libraries: libraries.remove('xc')
+# - dynamic linking (requires also setting LD_LIBRARY_PATH at runtime):
+#include_dirs += ['/home/user/libxc-2.0.1-1/include']
+#library_dirs += ['/home/user/libxc-2.0.1-1/lib']
+#if 'xc' not in libraries: libraries.append('xc')
+
