cf9f92e41470730758ceed6be88ac9164ca0a645
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2015-08-24 10:20:55 +0000

    Use write_plt()

--- doc/tutorials/plotting/CO2plt.py
+++ doc/tutorials/plotting/CO2plt.py
@@ -1,14 +1,11 @@
 from __future__ import print_function
-from ase.io import write
+from ase.io.plt import write_plt
 from gpaw import restart
 
 basename = 'CO'
 
 # load binary file and get calculator
 atoms, calc = restart(basename + '.gpw')
-
-# write atomic positions to xyz-file
-write(basename + '.xyz', atoms)
 
 # loop over all wfs and write their cube files
 nbands = calc.get_number_of_bands()
@@ -17,4 +14,4 @@ for band in range(nbands):
     wf = calc.get_pseudo_wave_function(band=band)
     fname = '{0}_{1}.plt'.format(basename, band)
     print('writing wf', band, 'to file', fname)
-    write(fname, atoms, data=wf)
+    write_plt(fname, atoms, data=wf)
