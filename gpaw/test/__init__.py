1b0097f4c84e8830b036c0350dd93af8d50d9e4b
askhl <askhl@6419b465-d902-0410-997c-be2266baf723>
2015-08-24 12:54:56 +0000

    add small test of libvdwxc.  As of now it does not yet test anything sensible, just that it runs.

--- gpaw/test/__init__.py
+++ gpaw/test/__init__.py
@@ -11,7 +11,7 @@ from ase.utils import devnull
 
 from gpaw.atom.generator import Generator
 from gpaw.atom.configurations import parameters, tf_parameters
-from gpaw.utilities import compiled_with_sl
+from gpaw.utilities import compiled_with_sl, compiled_with_libvdwxc
 from gpaw import setup_paths
 from gpaw import mpi
 import gpaw
@@ -171,6 +171,7 @@ tests = [
     'pseudopotential/ah.py',                # ~2s
     'lcao_restart.py',                      # ~2s
     'lcao_tddft.py',                        # ~2s
+    'vdw/libvdwxc_h2o',                     # ~2s
     'wfs_io.py',                            # ~3s
     'lrtddft2.py',                          # ~3s
     'fileio/file_reference.py',             # ~3s
@@ -447,6 +448,9 @@ if mpi.size != 1 and not compiled_with_sl():
 
 if not compiled_with_sl():
     exclude.append('lcao_atomic_corrections.py')
+
+if not compiled_with_libvdwxc():
+    exclude.append('vdw/libvdwxc_h2o.py')
 
 if np.__version__ < '1.6.0':
     exclude.append('chi0.py')
