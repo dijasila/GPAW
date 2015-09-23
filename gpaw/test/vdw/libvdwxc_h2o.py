e013a240b675abf8e9868a2c624b278cd628206a
askhl <askhl@6419b465-d902-0410-997c-be2266baf723>
2015-09-23 17:20:33 +0000

    update selfconsistent libvdwxc test so it will run

--- gpaw/test/vdw/libvdwxc_h2o.py
+++ gpaw/test/vdw/libvdwxc_h2o.py
@@ -1,6 +1,6 @@
 from ase.structure import molecule
 from gpaw import GPAW, Mixer
-from gpaw.xc.libvdwxc import LibVDWXC
+from gpaw.xc.libvdwxc import VDWDF
 
 system = molecule('H2O')
 system.center(vacuum=1.5)
@@ -8,7 +8,7 @@ system.pbc = 1
 
 calc = GPAW(eigensolver='rmm-diis',
             mixer=Mixer(0.3, 5, 10.),
-            xc=LibVDWXC())
+            xc=VDWDF())
 
 system.set_calculator(calc)
 system.get_potential_energy()
