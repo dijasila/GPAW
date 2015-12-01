bc3118b0f89d2d40785d650e2b12b89699a568f4
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2015-12-01 13:53:47 +0000

    Test result of parallel NEB calculation

--- doc/tutorials/neb/neb.py
+++ doc/tutorials/neb/neb.py
@@ -36,7 +36,7 @@ for i in range(3):
     
 images.append(final)
 
-neb = NEB(images, parallel=True)
+neb = NEB(images, parallel=True, climb=True)
 neb.interpolate()
 
 qn = BFGS(neb, logfile='qn.log')
