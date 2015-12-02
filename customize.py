2de64ce6a05b95b73961b335dfa954658c4590ba
kuismam <kuismam@6419b465-d902-0410-997c-be2266baf723>
2015-12-02 17:24:10 +0000

    Add libvdwxc bindings to customise.py

--- customize.py
+++ customize.py
@@ -45,23 +45,39 @@ if scalapack:
     define_macros += [('GPAW_NO_UNDERSCORE_CBLACS', '1')]
     define_macros += [('GPAW_NO_UNDERSCORE_CSCALAPACK', '1')]
 
-# LibXC:
+
+username = 'username'
+
+# -------------------------------------------------------------- #
+# LibXC                                                          #
+# -------------------------------------------------------------- #
 # In order to link libxc installed in a non-standard location
 # (e.g.: configure --prefix=/home/user/libxc-2.0.1-1), use:
 
 # - static linking:
 if 0:
-    include_dirs += ['/home/user/libxc-2.0.1-1/include']
-    extra_link_args += ['/home/user/libxc-2.0.1-1/lib/libxc.a']
+    include_dirs += ['/home/%s/libxc-2.0.1-1/include' % username]
+    extra_link_args += ['/home/%s/libxc-2.0.1-1/lib/libxc.a' % username]
     if 'xc' in libraries:
         libraries.remove('xc')
         
 # - dynamic linking (requires also setting LD_LIBRARY_PATH at runtime):
 if 0:
-    include_dirs += ['/home/user/libxc-2.0.1-1/include']
-    library_dirs += ['/home/user/libxc-2.0.1-1/lib']
+    include_dirs += ['/home/%s/libxc-2.0.1-1/include' % username]
+    library_dirs += ['/home/%s/libxc-2.0.1-1/lib' % username]
     if 'xc' not in libraries:
         libraries.append('xc')
+
+# -------------------------------------------------------------- #
+# libvdwxc                                                       #
+# -------------------------------------------------------------- #
+# - dynamic linking (requires also setting LD_LIBRARY_PATH at runtime):
+if 0:
+    include_dirs += ['/home/%s/libvdwxc/include' % username]
+    library_dirs += ['/home/%s/libvdwxc/lib' % username]
+    libraries += ['vdwxc']
+    define_macros += [('GPAW_WITH_LIBVDWXC', '1')]
+
 
 # Build MPI-interface into _gpaw.so:
 if 0:
