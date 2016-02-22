b515d2111fc297ea496c5e7a65c62f4f38ae28b8
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-02-22 13:27:22 +0100

    Wave function interpolator now works

--- gpaw/utilities/wavefunctions.py
+++ gpaw/utilities/wavefunctions.py
@@ -1,5 +1,5 @@
 import numpy as np
-from ase.units import Hartree, Bohr
+from ase.units import Bohr
 
 from gpaw.fftw import get_efficient_fft_size
 from gpaw.grid_descriptor import GridDescriptor
@@ -8,21 +8,34 @@ from gpaw.utilities import h2gpts
 from gpaw.wavefunctions.pw import PWDescriptor
 
 
-class WaveFunctions:
-    def __init__(self, calc, ecut=800):
+class WaveFunctionInterpolator:
+    def __init__(self, calc, h=0.05, n=2):
+        """Create interpolation object for PAW wave functions.
+        
+        calc: GPAW calculator object
+            The calcalator that has the wave functions.
+        h: float
+            Desired grid-spacing in Angstrom.
+        n: int
+            Force number of points to be a mulitiple of n.
+        """
         self.calc = calc
-        self.pd0 = calc.wfs.pd
-        gd0 = self.pd0.gd
-        ecut /= Hartree
-        h = np.pi / (4 * ecut)**0.5
-        N_c = h2gpts(h, gd0.cell_cv, 1)
+
+        # Create plane-wave descriptor for starting grid:
+        gd0 = calc.wfs.gd
+        self.pd0 = PWDescriptor(ecut=None, gd=gd0)
+        
+        # ... and a descriptor for the final gris:
+        N_c = h2gpts(h / Bohr, gd0.cell_cv, n)
         N_c = np.array([get_efficient_fft_size(N) for N in N_c])
         self.gd = GridDescriptor(N_c, gd0.cell_cv)
-        self.pd = PWDescriptor(ecut, self.gd)
-        print(gd0.N_c, N_c, self.pd0.ecut, self.pd.ecut)
-        self.dphi = None
+        self.pd = PWDescriptor(ecut=None, gd=self.gd)
+        
+        self.dphi = None  # PAW correction (will be initialize when needed)
 
-    def calculate_corrections(self):
+    def initialize_corrections(self):
+        if self.dphi is not None:
+            return
         splines = {}
         dphi_aj = []
         for setup in self.calc.wfs.setups:
@@ -39,31 +52,30 @@ class WaveFunctions:
                                                    points=200))
             dphi_aj.append(dphi_j)
             
-        dphi = LFC(self.gd, dphi_aj)
-        dphi.set_positions(self.calc.atoms.get_scaled_positions())
-        return dphi
+        self.dphi = LFC(self.gd, dphi_aj)
+        self.dphi.set_positions(self.calc.atoms.get_scaled_positions())
         
     def get_wave_function(self, n, k=0, s=0, ae=True):
-        wfs = self.calc.wfs
-        psi_r = wfs.get_wave_function_array(n, k, s)
+        """Interpolate wave function.
+        
+        n: int
+            Band index.
+        k: int
+            K-point index.
+        s: int
+            Spin index.
+        ae: bool
+            Add PAW correction to get an all-electron wave function.
+        """
+        psi_r = self.calc.get_pseudo_wave_function(n, k, s, pad=True)
+        psi_r *= Bohr**1.5
         psi_R, _ = self.pd0.interpolate(psi_r, self.pd)
-        P_ai = dict((a, P_ni[n]) for a, P_ni in wfs.kpt_u[k].P_ani.items())
         if ae:
-            if self.dphi is None:
-                self.dphi = self.calculate_corrections()
+            self.initialize_corrections()
+            wfs = self.calc.wfs
+            kpt_rank, u = wfs.kd.get_rank_and_index(s, k)
+            band_rank, n = wfs.bd.who_has(n)
+            assert kpt_rank == 0 and band_rank == 0
+            P_ai = dict((a, P_ni[n]) for a, P_ni in wfs.kpt_u[u].P_ani.items())
             self.dphi.add(psi_R, P_ai)
         return psi_R
-
-        
-from gpaw import GPAW
-c = GPAW('H2', txt=None)
-wfs = WaveFunctions(c)
-w0 = wfs.get_wave_function(0, ae=False)
-w = wfs.get_wave_function(0)
-print(wfs.gd.integrate(w0**2))
-print(wfs.gd.integrate(w**2))
-import matplotlib.pyplot as plt
-plt.plot(w0[7,7])
-plt.plot(w[7,7])
-plt.show()
-
