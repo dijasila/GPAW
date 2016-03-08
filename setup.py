b0ba70e04404d6a8fd5c9b23875438daaaa3cff7
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-03-08 15:53:04 +0100

    Call setup() only once plu clean-up

--- setup.py
+++ setup.py
@@ -6,18 +6,21 @@
 import distutils
 import distutils.util
 import os
+import os.path as op
 import re
 import sys
-
+from distutils.command.build_ext import build_ext as _build_ext
 from distutils.command.build_scripts import build_scripts as _build_scripts
 from distutils.command.sdist import sdist as _sdist
 from distutils.core import setup, Extension
 from glob import glob
-from os.path import join
 
-from config import (check_packages, get_system_config, get_parallel_config,
+from config import (get_system_config, get_parallel_config,
                     check_dependencies,
                     write_configuration, build_interpreter, get_config_vars)
+
+
+assert sys.version_info >= (2, 6)
 
 # Get the current version number:
 with open('gpaw/__init__.py') as fd:
@@ -27,8 +30,6 @@ long_description = """\
 A grid-based real-space Projector Augmented Wave (PAW) method Density
 Functional Theory (DFT) code featuring: Flexible boundary conditions,
 k-points and gradient corrected exchange-correlation functionals."""
-
-msg = [' ']
 
 libraries = []
 library_dirs = []
@@ -75,12 +76,10 @@ for i, arg in enumerate(sys.argv):
         customize = sys.argv.pop(i).split('=')[1]
         break
 
-check_packages(packages, msg, include_ase, import_numpy)
-
 get_system_config(define_macros, undef_macros,
                   include_dirs, libraries, library_dirs,
                   extra_link_args, extra_compile_args,
-                  runtime_library_dirs, extra_objects, msg,
+                  runtime_library_dirs, extra_objects,
                   import_numpy)
 
 mpicompiler = get_parallel_config(mpi_libraries,
@@ -107,7 +106,6 @@ if platform_id != '':
     distutils.util.get_platform = my_get_platform
 
 if compiler is not None:
-    msg += ['* Compiling gpaw with %s' % compiler]
     # A hack to change the used compiler and linker:
     vars = get_config_vars()
     if remove_default_flags:
@@ -139,18 +137,15 @@ if mpicompiler is not None:
 
 if scalapack:
     define_macros.append(('GPAW_WITH_SL', '1'))
-    msg.append('* Compiling with ScaLapack')
 
 
 if libvdwxc:
     define_macros.append(('GPAW_WITH_LIBVDWXC', '1'))
-    msg.append('* Compiling with libvdwxc')
 
 
 # distutils clean does not remove the _gpaw.so library and gpaw-python
 # binary so do it here:
 plat = distutils.util.get_platform()
-msg += ['* Architecture: ' + plat]
 plat = plat + '-' + sys.version[0:3]
 gpawso = 'build/lib.%s/' % plat + '_gpaw.so'
 gpawbin = 'build/bin.%s/' % plat + 'gpaw-python'
@@ -186,7 +181,6 @@ extensions = [extension]
 if hdf5:
     hdf5_sources = ['c/hdf5.c']
     define_macros.append(('GPAW_WITH_HDF5', '1'))
-    msg.append('* Compiling with HDF5')
 
     hdf5_extension = Extension('_gpaw_hdf5',
                                hdf5_sources,
@@ -203,8 +197,8 @@ if hdf5:
 
 files = ['gpaw-analyse-basis', 'gpaw-basis', 'gpaw-install-setups',
          'gpaw-mpisim', 'gpaw-plot-parallel-timings', 'gpaw-runscript',
-         'gpaw-setup', 'gpaw-test', 'gpaw-upfplot', 'gpaw']
-scripts = [join('tools', script) for script in files]
+         'gpaw-setup', 'gpaw-upfplot', 'gpaw']
+scripts = [op.join('tools', script) for script in files]
 
 write_configuration(define_macros, include_dirs, libraries, library_dirs,
                     extra_link_args, extra_compile_args,
@@ -231,7 +225,8 @@ class sdist(_sdist):
     """Fix distutils.
     
     Distutils insists that there should be a README or README.txt,
-    but GitLab.com needs README.rst in order to parse it as reStructureText."""
+    but GitLab.com needs README.rst in order to parse it as
+    reStructuredText."""
     
     def warn(self, msg):
         if msg.startswith('standard file not found: should have one of'):
@@ -240,12 +235,21 @@ class sdist(_sdist):
             _sdist.warn(self, msg)
 
             
-setup(ext_modules=extensions,
-      scripts=scripts,
-      cmdclass={'sdist': sdist},
-      **kwargs)
-      
+class build_ext(_build_ext):
+    def run(self):
+        _build_ext.run(self)
+        if custom_interpreter:
+            error = build_interpreter(
+                define_macros, include_dirs, libraries,
+                library_dirs, extra_link_args, extra_compile_args,
+                runtime_library_dirs, extra_objects,
+                mpicompiler, mpilinker, mpi_libraries,
+                mpi_library_dirs,
+                mpi_include_dirs,
+                mpi_runtime_library_dirs, mpi_define_macros)
+            assert error == 0
 
+        
 class build_scripts(_build_scripts):
     # Python 3's version will try to read the first line of gpaw-python
     # because it thinks it is a Python script that need an adjustment of
@@ -254,36 +258,20 @@ class build_scripts(_build_scripts):
         self.mkpath(self.build_dir)
         outfiles = []
         updated_files = []
-        script = self.scripts[0]
-        outfile = os.path.join(self.build_dir, os.path.basename(script))
-        outfiles.append(outfile)
-        updated_files.append(outfile)
-        self.copy_file(script, outfile)
+        for script in self.scripts:
+            outfile = op.join(self.build_dir, os.path.basename(script))
+            outfiles.append(outfile)
+            updated_files.append(outfile)
+            self.copy_file(script, outfile)
         return outfiles, updated_files
         
         
 if custom_interpreter:
-    scripts = ['build/bin.%s/' % plat + 'gpaw-python']
-    error, par_msg = build_interpreter(
-        define_macros, include_dirs, libraries,
-        library_dirs, extra_link_args, extra_compile_args,
-        runtime_library_dirs, extra_objects,
-        mpicompiler, mpilinker, mpi_libraries,
-        mpi_library_dirs,
-        mpi_include_dirs,
-        mpi_runtime_library_dirs, mpi_define_macros)
-    msg += par_msg
-    # install also gpaw-python
-    if 'install' in sys.argv and error == 0:
-        setup(cmdclass={'build_scripts': build_scripts},
-              ext_modules=[extension],
-              scripts=scripts,
-              **kwargs)
+    scripts.append('build/bin.%s/' % plat + 'gpaw-python')
 
-else:
-    msg += ['* Only a serial version of gpaw was built!']
-
-# Messages make sense only when building
-if 'build' in sys.argv or 'build_ext' in sys.argv or 'install' in sys.argv:
-    for line in msg:
-        print(line)
+setup(cmdclass={'sdist': sdist,
+                'build_ext': build_ext,
+                'build_scripts': build_scripts},
+      ext_modules=extensions,
+      scripts=scripts,
+      **kwargs)
