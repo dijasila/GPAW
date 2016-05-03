7adea524e3f2ff4fc876347e009b14544807e8ef
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-05-03 11:25:11 +0200

    Fix paths

--- doc/platforms/Linux/Niflheim/gpaw.sh
+++ doc/platforms/Linux/Niflheim/gpaw.sh
@@ -6,8 +6,9 @@ module load GPAW
 module load NUMPY/1.7.1-1
 module load SCIPY/0.12.0-1
 module load fftw
-export PATH=$GPAW/tools:$GPAW/build/bin.linux-x86_64-$FYS_PLATFORM:$PATH
-export PYTHONPATH=$GPAW:$GPAW/build/lib.linux-x86_64-$FYS_PLATFORM:$PYTHONPATH
+PLATFORM=linux-x86_64-$FYS_PLATFORM-2.6
+export PATH=$GPAW/tools:$GPAW/build/bin.$PLATFORM:$PATH
+export PYTHONPATH=$GPAW:$GPAW/build/lib.$PLATFORM:$PYTHONPATH
 export PATH=$ASE/tools:$PATH
 export PYTHONPATH=$ASE:$PYTHONPATH
 alias gpaw-qsub=$GPAW/doc/platforms/Linux/Niflheim/gpaw-qsub.py
