564bb125b67cda74f5ce69d9dddb8dbce1af4154
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-05-03 10:48:30 +0200

    Simplify further

--- doc/platforms/Linux/Niflheim/compile.sh
+++ doc/platforms/Linux/Niflheim/compile.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 nh=doc/platforms/Linux/Niflheim
-c1=$nh/el6-x3455-tm-gfortran-openmpi-1.6.3-acml-4.4.0-sl-hdf5-1.8.10.py
-c2=$nh/el6-sl230s-tm-gfortran-openmpi-1.6.3-acml-4.4.0-sl-hdf5-1.8.10.py
-c3=$nh/el6-dl160g6-tm-gfortran-openmpi-1.6.3-acml-4.4.0-sl-hdf5-1.8.10.py
 rm -rf build
 cmd="cd $PWD && python setup.py --remove-default-flags build_ext"
-ssh slid "$cmd --customize=$c1 > x3455.log 2>&1"
-ssh surt "$cmd --customize=$c2 > sl230s.log 2>&1"
-ssh muspel "$cmd --customize=$c3 > dl160g6.log 2>&1"
+ssh slid "$cmd --customize=$nh/x3455.py > x3455.log 2>&1"
+ssh surt "$cmd --customize=$nh/sl230s.py > sl230s.log 2>&1"
+ssh muspel "$cmd --customize=$nh/dl160g6.py > dl160g6.log 2>&1"
---
+++ doc/platforms/Linux/Niflheim/gpaw.sh
@@ -0,0 +1,13 @@
+if [ -n $PBS_ENVIRONMENT ]; then cd $PBS_O_WORKDIR; fi
+GPAW=~/gpaw
+ASE=~/ase
+source /home/opt/modulefiles/modulefiles_el6.sh
+module load GPAW
+module load NUMPY/1.7.1-1
+module load SCIPY/0.12.0-1
+module load fftw
+export PATH=$GPAW/tools:$GPAW/build/bin.linux-x86_64-$FYS_PLATFORM:$PATH
+export PYTHONPATH=$GPAW:$GPAW/build/lib.linux-x86_64-$FYS_PLATFORM:$PYTHONPATH
+export PATH=$ASE/tools:$PATH
+export PYTHONPATH=$ASE:$PYTHONPATH
+alias gpaw-qsub=$GPAW/doc/platforms/Linux/niflheim/gpaw-qsub.py
