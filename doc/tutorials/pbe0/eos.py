d6308b5b757ee97780a3beecd552044a8f68019f
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-05-25 11:11:24 +0200

    Finding gaps works ok now

--- doc/tutorials/pbe0/eos.py
+++ doc/tutorials/pbe0/eos.py
@@ -15,28 +15,18 @@ for k in range(2, 9):
         if id is None:
             continue
         si = bulk('Si', 'diamond', a)
-        name = 'Si-{0:.2f}-{1}'.format(a, k)
-        si.calc = GPAW(kpts={'size': (k, k, k), 'gamma': True},
+        si.calc = GPAW(kpts=(k, k, k),
                        mode=PW(400),
                        xc='PBE',
-                       occupations=FermiDirac(0.01),
                        parallel={'domain': 1, 'band': 1},
-                       convergence={'bands': 5},
-                       txt=name + '.txt')
+                       txt=None)
         si.get_potential_energy()
+        name = 'Si-{0:.2f}-{1}'.format(a, k)
         si.calc.write(name, mode='all')
         
-        eig_pbe_n = si.calc.get_eigenvalues(0)[:5]  # Gamma point
-        deig_pbe_n = vxc(si.calc, 'PBE')[0, 0, :5]
-        
-        pbe0 = EXX(name, 'PBE0', bands=[0, 5], txt=name + '.pbe0.txt')
+        pbe0 = EXX(name, 'PBE0', txt=name + '.pbe0.txt')
         pbe0.calculate()
         epbe0 = pbe0.get_total_energy()
-        deig_pbe0_n = pbe0.get_eigenvalue_contributions()[0, 0]
-        eig_pbe0_n = eig_pbe_n - deig_pbe_n + deig_pbe0_n
         
-        gap = eig_pbe_n[4] - eig_pbe_n[3]
-        gap0 = eig_pbe0_n[4] - eig_pbe0_n[3]
-        
-        con.write(si, a=a, k=k, epbe0=epbe0, gap=gap, gap0=gap0)
+        con.write(si, a=a, k=k, epbe0=epbe0)
         del con[id]
--- doc/tutorials/pbe0/gap.py
+++ doc/tutorials/pbe0/gap.py
@@ -1,44 +1,57 @@
-import ase.db
-from ase.build import bulk
 import numpy as np
+from ase.build import bulk
+from ase.parallel import paropen
 from gpaw.xc.exx import EXX
 from gpaw.xc.tools import vxc
-from gpaw import GPAW, PW, FermiDirac
+from gpaw import GPAW, PW
 
-a0 = 5.43
+a = 5.43
 si = bulk('Si', 'diamond', a)
+
+fd = paropen('si-gaps.csv', 'w')
 
 for k in range(2, 9, 2):
     name = 'Si-{0}'.format(k)
     si.calc = GPAW(kpts={'size': (k, k, k), 'gamma': True},
-                   mode=PW(300),
+                   mode=PW(200),
                    xc='PBE',
                    convergence={'bands': 5},
                    txt=name + '.txt')
     si.get_potential_energy()
     si.calc.write(name, mode='all')
     
-    ibzkpts = si.calc.get_ibz_k_points()
+    # Range of eigenvalues:
     n1 = 3
     n2 = 5
+    
+    ibzkpts = si.calc.get_ibz_k_points()
     kpt_indices = []
+    pbeeigs = []
     for kpt in [(0, 0, 0), (0.5, 0.5, 0)]:
         # Find k-point index:
         i = abs(ibzkpts - kpt).sum(1).argmin()
         kpt_indices.append(i)
+        pbeeigs.append(si.calc.get_eigenvalues(i)[n1:n2])
+
+    # DFT eigenvalues:
+    pbeeigs = np.array(pbeeigs)
+    
+    # PBE contribution:
+    dpbeeigs = vxc(si.calc, 'PBE')[0, kpt_indices, n1:n2]
         
-        eps_kn.append(atoms.calc.get_eigenvalues(k)[n - 1:n + 1])
-        eig_pbe_n = si.calc.get_eigenvalues(0)[:5]  # Gamma point
-        deig_pbe_n = vxc(si.calc, 'PBE')[0, 0, :5]
-        
-        pbe0 = EXX(name, 'PBE0', bands=[0, 5], txt=name + '.pbe0.txt')
-        pbe0.calculate()
-        epbe0 = pbe0.get_total_energy()
-        deig_pbe0_n = pbe0.get_eigenvalue_contributions()[0, 0]
-        eig_pbe0_n = eig_pbe_n - deig_pbe_n + deig_pbe0_n
-        
-        gap = eig_pbe_n[4] - eig_pbe_n[3]
-        gap0 = eig_pbe0_n[4] - eig_pbe0_n[3]
-        
-        con.write(si, a=a, k=k, epbe0=epbe0, gap=gap, gap0=gap0)
-        del con[id]
+    # Do PBE0 calculation:
+    pbe0 = EXX(name, 'PBE0', kpts=kpt_indices, bands=[n1, n2],
+               txt=name + '.pbe0.txt')
+    pbe0.calculate()
+    
+    dpbe0eigs = pbe0.get_eigenvalue_contributions()[0]
+    pbe0eigs = pbeeigs - dpbeeigs + dpbe0eigs
+
+    print('{0}, {1:.3f}, {2:.3f}, {3:.3f}, {4:.3f}'
+          .format(k,
+                  pbeeigs[0, 1] - pbeeigs[0, 0],
+                  pbeeigs[1, 1] - pbeeigs[0, 0],
+                  pbe0eigs[0, 1] - pbe0eigs[0, 0],
+                  pbe0eigs[1, 1] - pbe0eigs[0, 0]),
+          file=fd)
+    fd.flush()
