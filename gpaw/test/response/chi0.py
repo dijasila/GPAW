020e0eb2410e1732815cdf0c30c277c9e38b98f6
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-05-25 14:41:47 +0200

    Remove last trace of old chi-0 code

--- gpaw/test/response/chi0.py
+++ gpaw/test/response/chi0.py
@@ -5,7 +5,6 @@ from ase.dft.kpoints import monkhorst_pack
 from ase.units import Bohr
 
 from gpaw import GPAW
-from gpaw.response.chi import CHI
 from gpaw.response.chi0 import Chi0
 from gpaw.mpi import serial_comm
 
@@ -35,54 +34,30 @@ for k in [2, 3]:
                         width=0.001,
                         txt=name + '.txt')
                     e = a.get_potential_energy()
-                    #calc.diagonalize_full_hamiltonian(nbands=100)
                     calc.write(name, 'all')
                     
                 calc = GPAW(name, txt=None, communicator=serial_comm)
-
-                chiold = CHI(calc, w=omega, q=q_c, ecut=100,
-                             hilbert_trans=False, xc='RPA',
-                             G_plus_q=True, txt=name + '.logold')
-                chiold.initialize()
-                chiold.calculate()
-                chi0old_wGG = chiold.chi0_wGG
 
                 chi = Chi0(calc, omega, hilbert=False,
                            ecut=100, txt=name + '.log')
                 pd, chi0_wGG, _, _ = chi.calculate(q_c)
                 
-                assert abs(chi0_wGG - chi0old_wGG).max() < 1e-8
-                
                 if not sym and not center:
                     chi00_w = chi0_wGG[:, 0, 0]
                 elif -1 not in calc.wfs.kd.bz2bz_ks:
                     assert abs(chi0_wGG[:, 0, 0] - chi00_w).max() < 3e-5
-                    #print abs(chi0_wGG[:, 0, 0] - chi00_w).max()
                     
                 if not sym:
                     chi00_wGG = chi0_wGG
                 elif -1 not in calc.wfs.kd.bz2bz_ks:
                     assert abs(chi0_wGG - chi00_wGG).max() < 2e-5
-                    #print abs(chi0_wGG - chi00_wGG).max()
 
                 q0_c = [0, 1e-7, 1e-7]
                 q0_v = np.dot(q0_c, a.get_reciprocal_cell() * 2 * np.pi) * Bohr
                 q0 = (q0_v**2).sum()**0.5
                 
-                chiold = CHI(calc, w=omega, q=q0_c, ecut=100,
-                             hilbert_trans=False, xc='RPA', optical_limit=True,
-                             G_plus_q=True, txt=name + '.logold0')
-                chiold.initialize()
-                chiold.calculate()
-                chi0old_wGG = chiold.chi0_wGG
-                chi0old_wGG[:, 0] /= q0
-                chi0old_wGG[:, :, 0] /= q0
-                
                 pd, chi0_wGG, _, _ = chi.calculate([0, 0, 0])
                 
-                assert abs(chi0_wGG - chi0old_wGG).max() < 0.003
-                assert abs(chi0_wGG - chi0old_wGG)[:, 1:, 1:].max() < 1e-8
-
                 if not sym and not center:
                     chi000_w = chi0_wGG[:, 0, 0]
                 elif -1 not in calc.wfs.kd.bz2bz_ks:
