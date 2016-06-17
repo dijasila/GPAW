7e9bb435b26c163d4c8163b51ab9fa28bcea9255
Mikkel Strange <mikst@fysik.dtu.dk>
2016-06-17 11:47:10 +0200

    update the test

--- gpaw/test/pw/direct.py
+++ gpaw/test/pw/direct.py
@@ -2,13 +2,13 @@ from ase import Atoms
 from gpaw import GPAW, PW
 from gpaw.mpi import world
 
-if world.size == -1:
+if world.size == 1:
     atoms = Atoms('H', cell=(2, 2, 2), pbc=True)
-    atoms.calc = GPAW(mode=PW(200), eigensolver='direct', dtype=complex)
+    atoms.calc = GPAW(mode=PW(300), eigensolver='direct', dtype=complex)
     atoms.get_potential_energy()
 
-if world.size == 1:
-    atoms = Atoms('H', cell=(1, 1, 1), pbc=True)
+if world.size == 2:
+    atoms = Atoms('H', cell=(2, 2, 2), pbc=True)
     atoms.calc = GPAW(mode=PW(300),
                       eigensolver='direct',
                       kpts=(3, 2, 2))
