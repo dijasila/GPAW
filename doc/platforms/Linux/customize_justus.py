c509c81496d6a492385079b93c1178fbd66ee1e4
Michael Walter <Michael.Walter@fmf.uni-freiburg.de>
2016-06-21 12:18:15 +0200

    Correct customize.py

--- doc/platforms/Linux/customize_justus.py
+++ doc/platforms/Linux/customize_justus.py
@@ -1,17 +1,11 @@
+compiler='mpicc'
 libraries = ['xc']
-
 extra_link_args += [
      '-Wl,--no-as-needed',
+    '-L/opt/bwhpc/common/compiler/intel/compxe.2013.sp1.4.211/mkl/lib/intel64',
     '-lmkl_scalapack_lp64',
     '-lmkl_intel_lp64',
     '-lmkl_core',
     '-lmkl_sequential',
     '-lmkl_blacs_intelmpi_lp64',
-    '-lpthread',
-    '-lm',
-]
-
-#extra_compile_args = []
-extra_compile_args += [
-    '-m64',
 ]
