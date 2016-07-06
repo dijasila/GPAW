9ad065790cdbd66e6a585ed3b1ecc7c5dde90f6b
Tuomas Rossi <tuomas.rossi@aalto.fi>
2016-07-06 14:42:50 +0300

    Use relative tolerances in inducedfield_* tests

--- gpaw/test/inducedfield_td.py
+++ gpaw/test/inducedfield_td.py
@@ -63,7 +63,7 @@ ind.calculate_induced_field(gridrefinement=2, from_density='comp',
                             poisson_eps=1e-20)
 
 # Test
-tol = 1e-6
+tol = 1e-4
 val1 = ind.fieldgd.integrate(ind.Ffe_wg[0])
 val2 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[0][0]))
 val3 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[0][1]))
@@ -73,13 +73,16 @@ val6 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[1][0]))
 val7 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[1][1]))
 val8 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[1][2]))
 
-equal(val1, 3314.4687247954053, tol)
-equal(val2, 2.2544610801998815, tol)
-equal(val3, 2.2089702543970193, tol)
-equal(val4, 1.6975670957094118, tol)
-equal(val5, 3311.7608134815737, tol)
-equal(val6, 2.102508154206625, tol)
-equal(val7, 2.1038712082637567, tol)
-equal(val8, 1.602254592413304, tol)
+def relative_equal(x, y, tol):
+    return equal(x/y, 1.0, tol)
+
+relative_equal(val1, 3314.4687247954053, tol)
+relative_equal(val2, 2.2544610801998815, tol)
+relative_equal(val3, 2.2089702543970193, tol)
+relative_equal(val4, 1.6975670957094118, tol)
+relative_equal(val5, 3311.7608134815737, tol)
+relative_equal(val6, 2.102508154206625, tol)
+relative_equal(val7, 2.1038712082637567, tol)
+relative_equal(val8, 1.602254592413304, tol)
 
 ind.paw = None
