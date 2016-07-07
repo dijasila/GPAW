6c6bcbf22fe90d221d48efff2b985d1660ff105a
Tuomas Rossi <tuomas.rossi@aalto.fi>
2016-07-07 16:06:13 +0300

    Use better converged ground state in tests

--- gpaw/test/inducedfield_lrtddft.py
+++ gpaw/test/inducedfield_lrtddft.py
@@ -6,8 +6,12 @@ from gpaw.inducedfield.inducedfield_lrtddft import LrTDDFTInducedField
 from gpaw.poisson import PoissonSolver
 from gpaw.test import equal
 
+do_print_values = False  # Use this for printing the reference values
+poisson_eps = 1e-20
+density_eps = 1e-8
+
 # 0) PoissonSolver
-poissonsolver = PoissonSolver(eps=1e-20)
+poissonsolver = PoissonSolver(eps=poisson_eps)
 
 # 1) Ground state calculation with empty states
 atoms = Atoms(symbols='Na2',
@@ -15,7 +19,8 @@ atoms = Atoms(symbols='Na2',
               pbc=False)
 atoms.center(vacuum=3.0)
 
-calc = GPAW(nbands=20, h=0.6, setups={'Na': '1'}, poissonsolver=poissonsolver)
+calc = GPAW(nbands=20, h=0.6, setups={'Na': '1'}, poissonsolver=poissonsolver,
+        convergence={'density': density_eps})
 atoms.set_calculator(calc)
 energy = atoms.get_potential_energy()
 calc.write('na2_gs_casida.gpw', mode='all')
@@ -36,10 +41,16 @@ kickdir = 0                # Kick field direction 0, 1, 2 for x, y, z
 ind = LrTDDFTInducedField(paw=calc, lr=lr, frequencies=frequencies,
                           folding=folding, width=width, kickdir=kickdir)
 ind.calculate_induced_field(gridrefinement=2, from_density='comp',
-                            poisson_eps=1e-20)
+                            poisson_eps=poisson_eps)
+
+# Estimate tolerance (worst case error accumulation)
+tol = len(lr) * ind.fieldgd.integrate(ind.fieldgd.zeros() + 1.0) * \
+        max(density_eps, np.sqrt(poisson_eps))
+# tol = 0.000369606940
+if do_print_values:
+    print('tol = %.12f' % tol)
 
 # Test
-tol = 1e-4
 val1 = ind.fieldgd.integrate(ind.Ffe_wg[0])
 val2 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[0][0]))
 val3 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[0][1]))
@@ -49,14 +60,18 @@ val6 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[1][0]))
 val7 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[1][1]))
 val8 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[1][2]))
 
-def relative_equal(x, y, tol):
-    return equal(x/y, 1.0, tol)
+if do_print_values:
+    i = 1
+    def equal(x, y, tol):
+        global i
+        print("equal(val%d, %20.12f, tol)" % (i, x))
+        i += 1
 
-relative_equal(val1, 3175.7735779847876, tol)
-relative_equal(val2, 1700.4802379802722, tol)
-relative_equal(val3, 1187.2584619412758, tol)
-relative_equal(val4, 1187.2584619407216, tol)
-relative_equal(val5, 10957.19614721598, tol)
-relative_equal(val6, 6574.846859393127, tol)
-relative_equal(val7, 4589.770191548621, tol)
-relative_equal(val8, 4589.770191545197, tol)
+equal(val1,    3175.710769842502, tol)
+equal(val2,    1700.915873926834, tol)
+equal(val3,    1187.042280386919, tol)
+equal(val4,    1187.042280388817, tol)
+equal(val5,   10957.186908354506, tol)
+equal(val6,    6576.600305045955, tol)
+equal(val7,    4588.724567703681, tol)
+equal(val8,    4588.724567714311, tol)
--- gpaw/test/inducedfield_td.py
+++ gpaw/test/inducedfield_td.py
@@ -6,8 +6,12 @@ from gpaw.inducedfield.inducedfield_tddft import TDDFTInducedField
 from gpaw.poisson import PoissonSolver
 from gpaw.test import equal
 
+do_print_values = False  # Use this for printing the reference values
+poisson_eps = 1e-20
+density_eps = 1e-8
+
 # PoissonSolver
-poissonsolver = PoissonSolver(eps=1e-20)
+poissonsolver = PoissonSolver(eps=poisson_eps)
 
 # Na2 cluster
 atoms = Atoms(symbols='Na2',
@@ -16,7 +20,8 @@ atoms = Atoms(symbols='Na2',
 atoms.center(vacuum=3.0)
 
 # Standard ground state calculation
-calc = GPAW(nbands=2, h=0.6, setups={'Na': '1'}, poissonsolver=poissonsolver)
+calc = GPAW(nbands=2, h=0.6, setups={'Na': '1'}, poissonsolver=poissonsolver,
+        convergence={'density': density_eps})
 atoms.set_calculator(calc)
 energy = atoms.get_potential_energy()
 calc.write('na2_gs.gpw', mode='all')
@@ -60,10 +65,16 @@ td_calc.propagate(time_step, iterations // 2, 'na2_td_dm.dat', 'na2_td.gpw')
 
 # Calculate induced electric field
 ind.calculate_induced_field(gridrefinement=2, from_density='comp',
-                            poisson_eps=1e-20)
+                            poisson_eps=poisson_eps)
+
+# Estimate tolerance (worst case error accumulation)
+tol = iterations * ind.fieldgd.integrate(ind.fieldgd.zeros() + 1.0) * \
+        max(density_eps, np.sqrt(poisson_eps))
+#tol = 0.001167179811
+if do_print_values:
+    print('tol = %.12f' % tol)
 
 # Test
-tol = 1e-4
 val1 = ind.fieldgd.integrate(ind.Ffe_wg[0])
 val2 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[0][0]))
 val3 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[0][1]))
@@ -73,16 +84,20 @@ val6 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[1][0]))
 val7 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[1][1]))
 val8 = ind.fieldgd.integrate(np.abs(ind.Fef_wvg[1][2]))
 
-def relative_equal(x, y, tol):
-    return equal(x/y, 1.0, tol)
+if do_print_values:
+    i = 1
+    def equal(x, y, tol):
+        global i
+        print("equal(val%d, %20.12f, tol)" % (i, x))
+        i += 1
 
-relative_equal(val1, 3314.4687247954053, tol)
-relative_equal(val2, 2.2544610801998815, tol)
-relative_equal(val3, 2.2089702543970193, tol)
-relative_equal(val4, 1.6975670957094118, tol)
-relative_equal(val5, 3311.7608134815737, tol)
-relative_equal(val6, 2.102508154206625, tol)
-relative_equal(val7, 2.1038712082637567, tol)
-relative_equal(val8, 1.602254592413304, tol)
+equal(val1,    3314.463566436767, tol)
+equal(val2,       2.254452178599, tol)
+equal(val3,       2.208970141543, tol)
+equal(val4,       1.697564042167, tol)
+equal(val5,    3311.757129629823, tol)
+equal(val6,       2.102499949779, tol)
+equal(val7,       2.103871112604, tol)
+equal(val8,       1.602251056016, tol)
 
 ind.paw = None
