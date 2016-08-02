55d32d3effd20d33310395a507e33421bc4f203a
Tuomas Rossi <tuomas.rossi@aalto.fi>
2016-08-02 15:28:12 +0300

    Estimate test tolerances from convergence params

--- gpaw/test/complex.py
+++ gpaw/test/complex.py
@@ -3,12 +3,13 @@ from gpaw import GPAW, restart
 from ase.build import molecule
 from gpaw.test import equal
 Eini0 = -17.8037610364
+energy_eps = 5e-4
 esolvers = ['cg', 'rmm-diis', 'dav']
 
 calc = GPAW(xc='LDA',
             h=0.21,
             eigensolver='cg',
-            convergence={'eigenstates': 3.5e-5},
+            convergence={'eigenstates': 3.5e-5, 'energy': energy_eps},
             txt=None,
             dtype=complex)
 
@@ -20,7 +21,8 @@ Eini = mol.get_potential_energy()
 Iini = calc.get_number_of_iterations()
 print(('%10s: %12.6f eV in %3d iterations' %
        ('init(cg)', Eini, Iini)))
-equal(Eini, Eini0, 1E-5)
+print(energy_eps * calc.get_number_of_electrons())
+equal(Eini, Eini0, energy_eps * calc.get_number_of_electrons())
 
 calc.write('N2_complex.gpw', mode='all')
 del calc, mol
@@ -32,7 +34,7 @@ for esolver in esolvers:
     assert calc.wfs.kpt_u[0].psit_nG.dtype == complex
     
     calc.scf.reset()
-    calc.set(convergence={'eigenstates': 3.5e-9})
+    calc.set(convergence={'eigenstates': 3.5e-9, 'energy': energy_eps})
     calc.set(eigensolver=esolver)
     E = mol.get_potential_energy()
-    equal(E, Eini, 0.0005)
+    equal(E, Eini, energy_eps * calc.get_number_of_electrons())
