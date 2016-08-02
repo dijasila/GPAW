3747f294f97f35b094c63d065170d0145b6b518e
Tuomas Rossi <tuomas.rossi@aalto.fi>
2016-08-02 15:15:03 +0300

    Speed up tests

--- gpaw/test/fdtd/ed.py
+++ gpaw/test/fdtd/ed.py
@@ -53,14 +53,14 @@ gs_calc = GPAW(gpts          = gpts,
                eigensolver   = 'cg',
                nbands        = -1,
                poissonsolver = poissonsolver,
-               convergence   = {'density': 1e-8, 'energy': energy_eps})
+               convergence   = {'energy': energy_eps})
 atoms.set_calculator(gs_calc)
 
 # Ground state
 energy = atoms.get_potential_energy()
 
 # Test ground state
-equal(energy, -0.631876, energy_eps * gs_calc.get_number_of_electrons())
+equal(energy, -0.631881, energy_eps * gs_calc.get_number_of_electrons())
 
 # Test floating point arithmetic errors
 equal(gs_calc.hamiltonian.poisson.shift_indices_1, [4, 4, 10], 0)
@@ -87,8 +87,8 @@ td_calc2 = TDDFT('td.gpw', poissonsolver=poissonsolver)
 td_calc2.propagate(time_step,  max_time/time_step/2, 'dm.dat', 'td.gpw')
 
 # Test
-ref_cl_dipole_moment = [  5.50005140e-14,  2.07063307e-13,  3.08381524e-02]
-ref_qm_dipole_moment = [  5.26105795e-11,  3.11558604e-11,  5.21411099e-01]
+ref_cl_dipole_moment = [  5.25374117e-14,  5.75811267e-14,  3.08349334e-02]
+ref_qm_dipole_moment = [  1.78620337e-11, -1.57782578e-11,  5.21368300e-01]
 #print("ref_cl_dipole_moment = %s" % td_calc2.hamiltonian.poisson.get_classical_dipole_moment())
 #print("ref_qm_dipole_moment = %s" % td_calc2.hamiltonian.poisson.get_quantum_dipole_moment())
 
