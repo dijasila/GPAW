b22fdef02ed94c724e533467ea76e8c9e5f02ab2
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-10-11 22:00:01 +0200

    debugging

--- gpaw/test/matrix/matrix.py
+++ gpaw/test/matrix/matrix.py
@@ -3,7 +3,7 @@ import numpy as np
 from gpaw.mpi import world
 from gpaw.fd_operators import Laplace
 from gpaw.grid_descriptor import GridDescriptor
-from gpaw.matrix import Matrix, UniformGridMatrix
+from gpaw.matrix import Matrix, UniformGridMatrix, ProjectionMatrix
 
 gd = GridDescriptor([2, 3, 4], [2, 3, 4])
 dt = complex
@@ -12,9 +12,18 @@ T = functools.partial(Laplace(gd, -0.5, 1, dt).apply, phase_cd=ph)
 N = 2
 a = UniformGridMatrix(N, gd, dt, dist=(world, world.size))
 a.a[:] = 1
-a.A[0, 0, world.rank] = -1j
+a.array[0, 0, world.rank] = -1j
 c = Matrix(N, N, dt, dist=(world, world.size))
-
+p = {0: np.arange(10).reshape((2, 5)) * 0.1,
+     1: np.arange(10).reshape((2, 5)) * 0.2}
+P = ProjectionMatrix(N, world, float, p, ())
+P2 = P.new()
+P2.a[:]=1
+m = Matrix(N, N, float, dist=(world, world.size))
+m.a[:]=0
+m += P.C * P2.T
+print(m.a)
+adflkjh
 c[:] = (a | a)
 print(c.a)
 c.cholesky()
