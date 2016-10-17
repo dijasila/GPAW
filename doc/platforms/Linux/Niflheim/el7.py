f67e7b174c1886aadcc05951797c5e2c819c3910
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-10-17 16:09:13 +0200

    Link to readline also

--- doc/platforms/Linux/Niflheim/el7.py
+++ doc/platforms/Linux/Niflheim/el7.py
@@ -2,6 +2,7 @@ scalapack = True
 libraries = ['gfortran',
              'scalapack',
              'openblas',
+             'readline',
              'xc']
 extra_compile_args = ['-O3', '-std=c99', '-fPIC', '-Wall']
 define_macros += [('GPAW_NO_UNDERSCORE_CBLACS', '1'),
--- doc/platforms/Linux/Niflheim/gpaw.sh
+++ doc/platforms/Linux/Niflheim/gpaw.sh
@@ -7,15 +7,17 @@ then
     module load SCIPY/0.12.0-1
     module load fftw
     PLATFORM=linux-x86_64-$FYS_PLATFORM-2.6
+    GPAW=~/gpaw
+    ASE=~/ase
     alias gpaw-qsub=$GPAW/doc/platforms/Linux/Niflheim/gpaw-qsub.py
 else
     module load GPAW
-    PLATFORM=el7
+    PLATFORM=linux-x86_64-el7-3.5
+    GPAW=~/gpaw7
+    ASE=~/ase7
     alias gpaw-sbatch=$GPAW/doc/platforms/Linux/Niflheim/gpaw-sbatch.py
 fi
 
-GPAW=~/gpaw
-ASE=~/ase
 export PATH=$GPAW/tools:$GPAW/build/bin.$PLATFORM:$PATH
 export PYTHONPATH=$GPAW:$GPAW/build/lib.$PLATFORM:$PYTHONPATH
 export PATH=$ASE/tools:$PATH
