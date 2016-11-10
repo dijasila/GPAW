0d34c518f93278c0435bfe55002a47b62df6a3ec
Ask Hjorth Larsen <asklarsen@gmail.com>
2016-11-10 19:46:18 +0100

    define a function to dynamically construct libvdwxc class as subclass of either GGA or MGGA.  This is a temporary solution to the problem if subclassing both GGA and MGGA.  Define vdw_mbeef().  Update libvdwxc_h2o test to run mbeef calculation.  Results are as of yet untested

--- gpaw/test/vdw/libvdwxc_h2o.py
+++ gpaw/test/vdw/libvdwxc_h2o.py
@@ -1,20 +1,38 @@
 from ase.build import molecule
 from gpaw import GPAW, Mixer
-from gpaw.xc.libvdwxc import vdw_df
+from gpaw.atom.generator import Generator
+from gpaw.xc.libvdwxc import vdw_df, vdw_mbeef
 
 system = molecule('H2O')
 system.center(vacuum=1.5)
 system.pbc = 1
 
+sol_setups = {}
+for sym in 'H', 'O':
+    s = Generator(sym, xcname='PBEsol')
+    setup = s.run(write_xml=False)
+    sol_setups[sym] = setup
+
 for mode in ['lcao', 'fd', 'pw']:
-    calc = GPAW(mode=mode,
-                basis='szp(dzp)',
-                mixer=Mixer(0.3, 5, 10.),
-                xc=vdw_df())
-    def stopcalc():
-        calc.scf.converged = True
+    for vdw in 'df', 'mbeef':
+        if mode == 'lcao' and vdw == 'mbeef':
+            continue
+        print(mode, vdw)
+        kwargs = dict(mode=mode,
+                      basis='szp(dzp)',
+                      mixer=Mixer(0.3, 5, 10.))
+        if vdw == 'df':
+            kwargs['xc'] = vdw_df()
+        elif vdw == 'mbeef':
+            kwargs['xc'] = vdw_mbeef()
+            kwargs['setups'] = sol_setups
+        else:
+            assert 0
+        calc = GPAW(**kwargs)
+        def stopcalc():
+            calc.scf.converged = True
 
-    calc.attach(stopcalc, 6)
+        calc.attach(stopcalc, 6)
 
-    system.set_calculator(calc)
-    system.get_potential_energy()
+        system.set_calculator(calc)
+        system.get_potential_energy()
