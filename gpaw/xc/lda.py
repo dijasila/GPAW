d91cb97ee966f0157467839f5d6b02bc4f689374
Ask Hjorth Larsen <asklarsen@gmail.com>
2016-11-11 01:13:04 +0100

    move calculation methods from LDA class to separate methods

--- gpaw/xc/lda.py
+++ gpaw/xc/lda.py
@@ -6,6 +6,74 @@ from gpaw.xc.functional import XCFunctional
 from gpaw.sphere.lebedev import Y_nL, weight_n
 
 
+
+def calculate_paw_correction(radial_expansion, setup, D_sp, dEdD_sp=None,
+                             addcoredensity=True, a=None):
+    c = setup.xc_correction
+    if c is None:
+        return 0.0
+
+    rgd = c.rgd
+    nspins = len(D_sp)
+
+    if addcoredensity:
+        nc0_sg = rgd.empty(nspins)
+        nct0_sg = rgd.empty(nspins)
+        nc0_sg[:] = sqrt(4 * pi) / nspins * c.nc_g
+        nct0_sg[:] = sqrt(4 * pi) / nspins * c.nct_g
+        if c.nc_corehole_g is not None and nspins == 2:
+            nc0_sg[0] -= 0.5 * sqrt(4 * pi) * c.nc_corehole_g
+            nc0_sg[1] += 0.5 * sqrt(4 * pi) * c.nc_corehole_g
+    else:
+        nc0_sg = 0
+        nct0_sg = 0
+
+    D_sLq = np.inner(D_sp, c.B_pqL.T)
+
+    e, dEdD_sqL = radial_expansion(rgd, D_sLq, c.n_qg, nc0_sg)
+    et, dEtdD_sqL = radial_expansion(rgd, D_sLq, c.nt_qg, nct0_sg)
+
+    if dEdD_sp is not None:
+        dEdD_sp += np.inner((dEdD_sqL - dEtdD_sqL).reshape((nspins, -1)),
+                            c.B_pqL.reshape((len(c.B_pqL), -1)))
+
+    if addcoredensity:
+        return e - et - c.e_xc0
+    else:
+        return e - et
+
+
+def calculate_radial(kernel, rgd, n_sLg, Y_L):
+    nspins = len(n_sLg)
+    n_sg = np.dot(Y_L, n_sLg)
+    e_g = rgd.empty()
+    dedn_sg = rgd.zeros(nspins)
+
+    kernel.calculate(e_g, n_sg, dedn_sg)
+    return e_g, dedn_sg
+
+
+def calculate_radial_expansion(kernel, rgd, D_sLq, n_qg, nc0_sg):
+    n_sLg = np.dot(D_sLq, n_qg)
+    n_sLg[:, 0] += nc0_sg
+
+    dEdD_sqL = np.zeros_like(np.transpose(D_sLq, (0, 2, 1)))
+
+    Lmax = n_sLg.shape[1]
+
+    E = 0.0
+    for n, Y_L in enumerate(Y_nL[:, :Lmax]):
+        w = weight_n[n]
+
+        e_g, dedn_sg = calculate_radial(kernel, rgd, n_sLg, Y_L)
+        dEdD_sqL += np.dot(rgd.dv_g * dedn_sg,
+                           n_qg.T)[:, :, np.newaxis] * (w * Y_L)
+        E += w * rgd.integrate(e_g)
+
+    return E, dEdD_sqL
+
+
+
 class LDA(XCFunctional):
     def __init__(self, kernel):
         self.kernel = kernel
@@ -27,70 +95,15 @@ class LDA(XCFunctional):
 
     def calculate_paw_correction(self, setup, D_sp, dEdD_sp=None,
                                  addcoredensity=True, a=None):
-        c = setup.xc_correction
-        if c is None:
-            return 0.0
-
-        rgd = c.rgd
-        nspins = len(D_sp)
-
-        if addcoredensity:
-            nc0_sg = rgd.empty(nspins)
-            nct0_sg = rgd.empty(nspins)
-            nc0_sg[:] = sqrt(4 * pi) / nspins * c.nc_g
-            nct0_sg[:] = sqrt(4 * pi) / nspins * c.nct_g
-            if c.nc_corehole_g is not None and nspins == 2:
-                nc0_sg[0] -= 0.5 * sqrt(4 * pi) * c.nc_corehole_g
-                nc0_sg[1] += 0.5 * sqrt(4 * pi) * c.nc_corehole_g
-        else:
-            nc0_sg = 0
-            nct0_sg = 0
-
-        D_sLq = np.inner(D_sp, c.B_pqL.T)
-
-        e, dEdD_sqL = self.calculate_radial_expansion(rgd, D_sLq, c.n_qg,
-                                                      nc0_sg)
-        et, dEtdD_sqL = self.calculate_radial_expansion(rgd, D_sLq, c.nt_qg,
-                                                        nct0_sg)
-
-        if dEdD_sp is not None:
-            dEdD_sp += np.inner((dEdD_sqL - dEtdD_sqL).reshape((nspins, -1)),
-                                c.B_pqL.reshape((len(c.B_pqL), -1)))
-
-        if addcoredensity:
-            return e - et - c.e_xc0
-        else:
-            return e - et
+        return calculate_paw_correction(self.calculate_radial_expansion,
+                                        setup, D_sp, dEdD_sp,
+                                        addcoredensity, a)
 
     def calculate_radial_expansion(self, rgd, D_sLq, n_qg, nc0_sg):
-        n_sLg = np.dot(D_sLq, n_qg)
-        n_sLg[:, 0] += nc0_sg
-
-        dEdD_sqL = np.zeros_like(np.transpose(D_sLq, (0, 2, 1)))
-
-        Lmax = n_sLg.shape[1]
-
-        E = 0.0
-        for n, Y_L in enumerate(Y_nL[:, :Lmax]):
-            w = weight_n[n]
-
-            e_g, dedn_sg = self.calculate_radial(rgd, n_sLg, Y_L)
-            dEdD_sqL += np.dot(rgd.dv_g * dedn_sg,
-                               n_qg.T)[:, :, np.newaxis] * (w * Y_L)
-            E += w * rgd.integrate(e_g)
-
-        return E, dEdD_sqL
+        return calculate_radial_expansion(self.kernel, rgd, D_sLq, n_qg, nc0_sg)
 
     def calculate_radial(self, rgd, n_sLg, Y_L):
-        nspins = len(n_sLg)
-
-        n_sg = np.dot(Y_L, n_sLg)
-        e_g = rgd.empty()
-        dedn_sg = rgd.zeros(nspins)
-
-        self.kernel.calculate(e_g, n_sg, dedn_sg)
-
-        return e_g, dedn_sg
+        return calculate_radial(self.kernel, rgd, n_sLg, Y_L)
 
     def calculate_spherical(self, rgd, n_sg, v_sg, e_g=None):
         if e_g is None:
