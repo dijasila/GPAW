3b330fa54ab493fb3bfb9a499661cb6268bdf4b5
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-11-15 09:34:12 +0100

    Python 2.6 compatibility

--- gpaw/lrtddft/finite_differences.py
+++ gpaw/lrtddft/finite_differences.py
@@ -117,7 +117,7 @@ class FiniteDifference:
     def displacements(self):
         for a in self.indices:
             for i in range(3):
-                filename = ('{}_{}_{}'.format(self.name, a, 'xyz'[i]))
+                filename = ('{0}_{1}_{2}'.format(self.name, a, 'xyz'[i]))
                 yield filename, a, i
 
     def restart(self, restartfunction, **kwargs):
