cf6df2cd20f7c2dac8a718fca0cfe30b3be5e9fb
Per S. Schmidt <psisc@fysik.dtu.dk>
2016-12-12 10:31:17 +0100

    removed files

--- doc/tutorials/gw_tutorial/C_07_12/submit.agts.py
+++
@@ -1,6 +0,0 @@
-def agts(queue):
-    conv = queue.add('C_ecut_k_conv_GW.py', ncpus=8, walltime=10 * 60)
-    queue.add('C_ecut_k_conv_plot_GW.py', deps=conv, creates=['C_GW.png'])
-    freq = queue.add('C_frequency_conv.py', walltime=30)
-    queue.add('C_frequency_conv_plot.py', deps=freq, creates=['C_freq.png'])
-    queue.add('C_equal_test.py', deps=[conv, freq])
