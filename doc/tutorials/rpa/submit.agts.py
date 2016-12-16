ddb400496d2d0161ca7806ec4f9900f7a45a022e
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-12-16 10:07:42 +0100

    Use 8 cores to make it run faster

--- doc/tutorials/rpa/submit.agts.py
+++ doc/tutorials/rpa/submit.agts.py
@@ -1,5 +1,5 @@
 def agts(queue):
-    gs_N2 = queue.add('gs_N2.py', ncpus=1, walltime=2000)
+    gs_N2 = queue.add('gs_N2.py', ncpus=8, walltime=2000)
     w = queue.add('frequency.py', deps=gs_N2, walltime=200)
     f = queue.add('con_freq.py', ncpus=2, deps=gs_N2, walltime=1000)
     rpa_N2 = queue.add('rpa_N2.py', deps=gs_N2,
