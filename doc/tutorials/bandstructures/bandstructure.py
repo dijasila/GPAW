b44c195e669d2411becb4e5f32762d7a25c5c5d1
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-12-20 11:28:23 +0100

    Fix bandstructure tutorial

--- doc/tutorials/bandstructures/bandstructure.py
+++ doc/tutorials/bandstructures/bandstructure.py
@@ -4,7 +4,6 @@ Calculate the band structure of Si along high symmetry directions
 Brillouin zone
 """
 
-import numpy as np
 from ase.build import bulk
 from gpaw import GPAW, PW, FermiDirac
 
@@ -13,15 +12,12 @@ si = bulk('Si', 'diamond', 5.43)
 calc = GPAW(mode=PW(200),
             xc='PBE',
             kpts=(8, 8, 8),
-            random=True,  # random guess (needed if many onoccupied bands required)
+            random=True,  # random guess (needed if many empty bands required)
             occupations=FermiDirac(0.01),
             txt='Si_gs.txt')
-si.set_calculator(calc)
+si.calc = calc
 si.get_potential_energy()
 calc.write('Si_gs.gpw')
-ef = calc.get_fermi_level()
-
-nbands = 8
 
 # Restart from ground state and fix potential:
 calc = GPAW('Si_gs.gpw',
@@ -29,12 +25,10 @@ calc = GPAW('Si_gs.gpw',
             fixdensity=True,
             symmetry='off',
             kpts={'path': 'GXWKL', 'npoints': 60},
-            convergence={'bands': nbands})
+            convergence={'bands': 8})
 
-# Use ase.dft module for obtaining k-points along high symmetry directions
 calc.get_potential_energy()
+
 band_structure = calc.band_structure()
 band_structure.plot(filename='bandstructure.png',
                     show=True)
-# plt.title('Bandstructure of Silicon')
-# plt.savefig('bandstructure.png')
