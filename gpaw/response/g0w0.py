1c48f2611a8f5a4f30dd145c7941708222b5b97f
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-01-11 07:44:45 +0100

    PEP8 and simplify a bit

--- gpaw/response/g0w0.py
+++ gpaw/response/g0w0.py
@@ -96,11 +96,16 @@ class G0W0(PairDensity):
             Shift Fermi level of ground state calculation by the
             specified amount.
         anisotropy_correction: bool
-            Analytic correction to the q=0 contribution applicable to 2D systems.
+            Analytic correction to the q=0 contribution applicable to 2D
+            systems.
         nblocks: int
-            Number of blocks chi0 should be distributed in so each core does not have to store the entire matrix. This is to reduce memory requirement. nblocks must be less than or equal to the number of processors.
+            Number of blocks chi0 should be distributed in so each core
+            does not have to store the entire matrix. This is to reduce
+            memory requirement. nblocks must be less than or equal to the
+            number of processors.
         nblocksmax: bool
-            Cuts chi0 into as many blocks as possible to reduce memory requirements as much as possible.
+            Cuts chi0 into as many blocks as possible to reduce memory
+            requirements as much as possible.
         savew: bool
             Save W to a file.
         savepckl: bool
@@ -110,7 +115,8 @@ class G0W0(PairDensity):
         maxiter: int
             Number of iterations in a GW0 calculation.
         mixing: float
-            Number between 0 and 1 determining how much of previous iteration's eigenvalues to mix with.
+            Number between 0 and 1 determining how much of previous
+            iteration's eigenvalues to mix with.
         ecut_extrapolation: bool
             Carries out the extrapolation to infinite cutoff automatically.
         """
@@ -149,10 +155,11 @@ class G0W0(PairDensity):
         if nblocksmax:
             nblocks = world.size
             if isinstance(calc, GPAW):
-              raise Exception('Using a calulator is not implemented at the moment, load from file!')
-              nblocks_calc = calc
+                raise Exception('Using a calulator is not implemented at '
+                                'the moment, load from file!')
+                # nblocks_calc = calc
             else:
-              nblocks_calc = GPAW(calc, txt=None)
+                nblocks_calc = GPAW(calc, txt=None)
             ngmax = []
             for q_c in nblocks_calc.wfs.kd.bzk_kc:
                 qd = KPointDescriptor([q_c])
@@ -263,9 +270,7 @@ class G0W0(PairDensity):
 
         # assert self.calc.wfs.nspins == 1
 
-        # print at least at the end...
-        if world.rank != 0:
-            txt.flush()
+        txt.flush()
 
     @timer('G0W0')
     def calculate(self):
