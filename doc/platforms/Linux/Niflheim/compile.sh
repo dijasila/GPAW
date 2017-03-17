34ccef78f6de9aefb4e97ac475a655a75c21b09f
Mikkel Strange <mikst@sylg.fysik.dtu.dk>
2017-03-17 15:21:36 +0100

    fix cpu_arch name

--- doc/platforms/Linux/Niflheim/compile.sh
+++ doc/platforms/Linux/Niflheim/compile.sh
@@ -4,5 +4,5 @@ rm -rf build
 cmd="cd $PWD && python setup.py --remove-default-flags build_ext"
 ssh surt "$cmd --customize=$nh/sl230s.py > sl230s.log 2>&1"
 ssh muspel "$cmd --customize=$nh/dl160g6.py > dl160g6.log 2>&1"
-ssh sylg "$cmd --customize=$nh/el7.py > $CPU_ARCH-el7.log 2>&1"
-ssh fjorm "$cmd --customize=$nh/el7.py > $CPU_ARCH-el7.log 2>&1"
+ssh sylg "$cmd --customize=$nh/el7.py > broadwell-el7.log 2>&1"
+ssh fjorm "$cmd --customize=$nh/el7.py > nehalem-el7.log 2>&1"
