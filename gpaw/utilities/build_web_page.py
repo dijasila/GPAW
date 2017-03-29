74d53f96902a548174bd4866ec2ff296d2b3a5e1
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-03-29 19:16:00 +0200

    Build two versions of docs

--- gpaw/utilities/build_web_page.py
+++ gpaw/utilities/build_web_page.py
@@ -1,25 +1,61 @@
-import glob
+"""Build GPAW's web-page.
+
+Initial setup::
+
+    cd ~
+    python3 -m venv web-page
+    cd web-page
+    . bin/activate
+    pip install sphinx-rtd-theme
+    pip install Sphinx
+    pip install matplotlib scipy
+    git clone git@gitlab.com:gpaw/gpaw
+    cd gpaw
+    pip install -e .
+
+Crontab::
+
+    build="python -m gpaw.utilities.build_web_page"
+    10 * * * * cd ~/web-page; . bin/activate; cd gpaw; $build > ../gpaw.log
+
+"""
+
 import os
 import subprocess
+import sys
 
-from ase.utils.build_web_page import build, git_pull, main
+from gpaw import __version__
 
 
-def build_gpaw_web_page(force_build):
-    changes = git_pull('gpaw')
-    if force_build or changes:
-        os.chdir('ase')
-        subprocess.check_call('python setup.py install --home=..', shell=True)
-        os.chdir('..')
-        subprocess.check_call(
-            'wget --no-check-certificate --quiet --timestamping '
-            'http://wiki.fysik.dtu.dk/gpaw-files/'
-            'gpaw-setups-latest.tar.gz', shell=True)
-        subprocess.check_call(
-            'tar xf gpaw-setups-latest.tar.gz', shell=True)
-        path = os.path.abspath(glob.glob('gpaw-setups-[0-9]*')[0])
-        build(True, 'gpaw', 'GPAW_SETUP_PATH=' + path)
-        
-        
+cmds = """\
+touch ../gpaw-web-page.lock
+git clean -fdx
+git checkout web-page
+git pull
+cd doc; sphinx-build -b html -d build/doctrees . build/html
+mv doc/build/html web-page
+git clean -fdx doc
+git checkout master
+git pull
+cd doc; sphinx-build -b html -d build/doctrees . build/html
+mv doc/build/html web-page/dev
+python setup.py sdist
+cp dist/gpaw-*.tar.gz web-page/
+cp dist/gpaw-*.tar.gz web-page/dev/
+find web-page -name install.html | xargs sed -i s/snapshot.tar.gz/{}/g
+tar -cf web-page.tar.gz web-page""".format('gpaw-' + __version__ + '.tar.gz')
+
+
+def build():
+    if os.path.isfile('../gpaw-web-page.lock'):
+        print('Locked', file=sys.stderr)
+        return
+    try:
+        for cmd in cmds.splitlines():
+            subprocess.check_call(cmd, shell=True)
+    finally:
+        os.remove('../gpaw-web-page.lock')
+
+
 if __name__ == '__main__':
-    main(build_gpaw_web_page)
+    build()
