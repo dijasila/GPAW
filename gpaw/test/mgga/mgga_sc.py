bb8ff951f36c1759ab8f239bf294d12f35828dbd
Ask Hjorth Larsen <asklarsen@gmail.com>
2017-04-06 20:18:53 +0200

    update mgga test.  Its tolerance was more strict than its computational parameters could guarantee, so it only passed by (small) random chance

--- gpaw/test/mgga/mgga_sc.py
+++ gpaw/test/mgga/mgga_sc.py
@@ -1,15 +1,18 @@
 """Compare TPSS from scratch and from PBE"""
 from ase import Atoms
-from gpaw import GPAW
+from gpaw import GPAW, Davidson
 
 n = Atoms('N', magmoms=[3])
-n.center(vacuum=2.5)
-n.calc = GPAW(xc='TPSS')
+n.center(vacuum=2.0)
+n.calc = GPAW(xc='TPSS', h=0.25, eigensolver=Davidson(5),
+              convergence=dict(energy=5e-6))
 e1 = n.get_potential_energy()
 
-n.calc = GPAW(xc='PBE')
+n.calc = GPAW(xc='PBE', h=0.25, eigensolver=Davidson(5),
+              convergence=dict(energy=5e-6))
 n.get_potential_energy()
 n.calc.set(xc='TPSS')
 e2 = n.get_potential_energy()
 print('Energy difference', e1 - e2)
-assert abs(e1 - e2) < 1e-5
+err = abs(e1 - e2)
+assert err < 2e-5, err
