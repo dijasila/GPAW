28ea78079b8cb43edecd78c2205cf965857542d5
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-04-19 10:56:27 +0200

    Add --nodes=... option to sbatch

--- gpaw/test/big/niflheim.py
+++ gpaw/test/big/niflheim.py
@@ -7,8 +7,8 @@ from gpaw.test.big.agts import Cluster
 class NiflheimCluster(Cluster):
     def __init__(self, asepath='', setuppath='$GPAW_SETUP_PATH',
                  partition=None):
-        if partition is None:
-            partition = 'xeon8'
+        # if partition is None:
+        #     partition = 'xeon8'
         self.partition = partition
         self.asepath = asepath
         self.setuppath = setuppath
@@ -20,14 +20,19 @@ class NiflheimCluster(Cluster):
         if self.partition is None:
             if job.ncpus % 24 == 0:
                 self.partition = 'xeon24'
+                size = 24
             else:
                 self.partition = 'xeon8'
+                size = 8
 
         cmd = ['sbatch',
                '--partition={}'.format(self.partition),
                '--job-name={}'.format(job.name),
                '--time={}'.format(job.walltime // 60),
                '--ntasks={}'.format(job.ncpus)]
+
+        if job.ncpus % size == 0:
+            cmd.append('--nodes={}'.format(job.ncpus // size))
 
         mpi_cmd = 'mpirun '
         if self.partition == 'xeon24':
