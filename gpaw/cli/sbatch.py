955a7f4225884096fd77733963a99691d2296742
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-04-21 09:22:53 +0200

    Finish gpaw sbatch command

--- gpaw/cli/sbatch.py
+++ gpaw/cli/sbatch.py
@@ -1,30 +1,29 @@
+from __future__ import print_function
 import subprocess
 
 
 class CLICommand:
     short_description = 'Submit a GPAW Python script via sbatch.'
-    description = ('Usage: gpaw sbatch [sbatch options] script.py '
-                   '[script arguments]')
 
     @staticmethod
     def add_arguments(parser):
-        parser.add_argument('arguments', nargs='+')
+        parser.usage = 'Usage: gpaw sbatch [-0] script.py -- [sbatch options]'
+        parser.add_argument('script')
+        parser.add_argument('-0', '--dry-run', action='store_true')
+        parser.add_argument('arguments', nargs='*')
 
     @staticmethod
     def run(args):
-        for i, arg in enumerate(args.arguments):
-            if arg.endswith('.py'):
-                break
-        else:
-            return
-
-        script = '#!/bin/sh\n'
-        for line in open(arg):
+        script = '#!/bin/bash -l\n'
+        for line in open(args.script):
             if line.startswith('#SBATCH'):
                 script += line
         script += ('OMP_NUM_THREADS=1 '
-                   'mpiexec gpaw-python ' +
-                   ' '.join(args.arguments[i:]) + '\n')
-        cmd = ['sbatch'] + args.argument[:i]
-        p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
-        p.communicate(script.encode())
+                   'mpiexec `echo $GPAW_MPI_OPTIONS` gpaw-python {}\n'
+                   .format(args.script))
+        cmd = ['sbatch'] + args.arguments
+        if args.dry_run:
+            print(script, end='')
+        else:
+            p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+            p.communicate(script.encode())
