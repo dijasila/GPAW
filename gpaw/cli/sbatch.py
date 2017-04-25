bdd560361bb4f64d6f65a76339f2b10ffee8e6b2
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-04-25 14:10:53 +0200

    Use new gpaw sbatch command on Niflheim

--- gpaw/cli/sbatch.py
+++ gpaw/cli/sbatch.py
@@ -7,10 +7,12 @@ class CLICommand:
 
     @staticmethod
     def add_arguments(parser):
-        parser.usage = 'Usage: gpaw sbatch [-0] script.py -- [sbatch options]'
+        parser.usage = (
+            'Usage: gpaw sbatch [-0] [-a args] script.py -- [sbatch options]')
         parser.add_argument('script')
         parser.add_argument('-0', '--dry-run', action='store_true')
-        parser.add_argument('arguments', nargs='*')
+        parser.add_argument('-a', '--script-options', default='')
+        parser.add_argument('sbatch_options', nargs='*')
 
     @staticmethod
     def run(args):
@@ -19,10 +21,13 @@ class CLICommand:
             if line.startswith('#SBATCH'):
                 script += line
         script += ('OMP_NUM_THREADS=1 '
-                   'mpiexec `echo $GPAW_MPI_OPTIONS` gpaw-python {}\n'
-                   .format(args.script))
-        cmd = ['sbatch'] + args.arguments
+                   'mpiexec `echo $GPAW_MPI_OPTIONS` gpaw-python {} {}\n'
+                   .format(args.script, args.script_options))
+        cmd = ['sbatch'] + args.sbatch_options
         if args.dry_run:
+            print('sbatch command:')
+            print(' '.join(cmd))
+            print('\nscript:')
             print(script, end='')
         else:
             p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
