6d77897d3e28706307ec2b8fbdef7a1ee7706837
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-06-15 13:50:28 +0200

    Move things

--- gpaw/matrix/__init__.py
+++
@@ -1,6 +0,0 @@
-from gpaw.matrix.matrix import Matrix
-from gpaw.matrix.spatial import (UniformGridWaveFunctions,
-                                 PlaneWaveExpansionWaveFunctions,
-                                 AtomBlockMatrix, UniformGridDensity,
-                                 AtomCenteredFunctions, ProjectionMatrix)
-__all__ = ['Matrix']
