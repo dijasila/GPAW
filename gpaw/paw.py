409039b994786ea3a066bb2c78687101dba9c806
Thomas Olsen <tolsen@fysik.dtu.dk>
2017-08-13 23:29:20 +0200

    Added PDOS with spinorbit

--- gpaw/paw.py
+++ gpaw/paw.py
@@ -411,7 +411,7 @@ class PAW:
 
     def get_orbital_ldos(self, a,
                          spin=0, angular='spdf', npts=201, width=None,
-                         nbands=None):
+                         nbands=None, spinorbit=False):
         """The Local Density of States, using atomic orbital basis functions.
 
         Project wave functions onto an atom orbital at atom ``a``, and
@@ -430,8 +430,12 @@ class PAW:
         if width is None:
             width = 0.1
 
-        from gpaw.utilities.dos import raw_orbital_LDOS, fold
-        energies, weights = raw_orbital_LDOS(self, a, spin, angular, nbands)
+        if not spinorbit:
+            from gpaw.utilities.dos import raw_orbital_LDOS, fold
+            energies, weights = raw_orbital_LDOS(self, a, spin, angular, nbands)
+        else:
+            from gpaw.utilities.dos import raw_spinorbit_orbital_LDOS, fold
+            energies, weights = raw_spinorbit_orbital_LDOS(self, a, spin, angular)
         return fold(energies * Ha, weights, npts, width)
 
     def get_lcao_dos(self, atom_indices=None, basis_indices=None,
