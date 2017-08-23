8e6aa68b9d5463885fbbe63f414b6f7051f1b734
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-08-23 16:17:48 +0200

    PDOS tool

--- gpaw/cli/dos.py
+++ gpaw/cli/dos.py
@@ -7,7 +7,8 @@ from ase.dft.dos import DOS
 from gpaw import GPAW
 
 
-def dos(filename, plot=False, output='dos.csv', width=0.1, integrated=False):
+def dos(filename, plot=False, output='dos.csv', width=0.1, integrated=False
+        projection=None):
     """Calculate density of states.
 
     filename: str
@@ -19,11 +20,31 @@ def dos(filename, plot=False, output='dos.csv', width=0.1, integrated=False):
     width: float
         Width of Gaussians.
     integrated: bool
-        Calculate integratede DOS.
+        Calculate integrated DOS.
+
     """
     calc = GPAW(filename, txt=None)
     dos = DOS(calc, width)
-    D = [dos.get_dos(spin) for spin in range(calc.get_number_of_spins())]
+    if projection is None:
+        D = [dos.get_dos(spin) for spin in range(calc.get_number_of_spins())]
+    else:
+        for p in projection.split(','):
+            a, l = p.split('-')
+            if a.isnumber():
+                A = [int(a)]
+            else:
+                A = [a for a, symbol in cals.atoms.symbols if symbol == a]
+            for a in A:
+                energies, weights = ldos(calc, a, s, l)
+                energies.shape = (kd.nibzkpts, -1)
+                energies = energies[kd.bz2ibz_k]
+                energies.shape = tuple(kd.N_c) + (-1,)
+                weights.shape = (kd.nibzkpts, -1)
+                weights /= kd.weight_k[:, np.newaxis]
+                w = weights[kd.bz2ibz_k]
+                w.shape = tuple(kd.N_c) + (-1,)
+                p = ltidos(calc.atoms.cell, energies * Ha, e, w)
+
     if integrated:
         de = dos.energies[1] - dos.energies[0]
         dos.energies += de / 2
