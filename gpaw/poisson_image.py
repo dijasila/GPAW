bb8a2e6ff825a947d20ac1e925dc1f3f94664141
Robert Warmbier <robert.warmbier@wits.ac.za>
2017-09-11 11:49:20 +0200

    Use grid extension to one side only, which makes things faster. Also added class description. Note: I'm still troubled with how to mirror the charge exactly, because we have N-1 grid points for the non-periodic direction.

--- gpaw/poisson_image.py
+++ gpaw/poisson_image.py
@@ -1,22 +1,50 @@
 import numpy as np
 from gpaw.poisson_extended import ExtendedPoissonSolver as EPS
+from gpaw.poisson import FDPoissonSolver
 from gpaw.dipole_correction import dipole_correction
-
-
-# XXX add class description
+from gpaw.utilities.extend_grid import extended_grid_descriptor
 
 # TODO
 # Documentation
-# Working examples
+# Working examples (there is a test already)
 # 'right' gives very wrong results, why?
 # allow arbitrary directions?
 # Automatically assign gpts2, users shouldn't be bothered with this
-# Extend grid only on required side, not both. Makes grid 2* size, instead of 3*
 
-class EPS2(EPS):
+class ImagePoissonSolver(EPS):
+    """ Extended Poisson solver with image/mirror charges
+    and dipole corrections
+
+    This class is intended for calculations of charged unit cells with mixed
+    boundary conditions. A typical application would be capacitor half cells or
+    surface slabs with absorbed ions.
+
+    Workings: First the dipole correction is applied to remove dipole
+    components. Then the cell is extended in a given directions (currently
+    fixed to z). Then the image charge is appiled in one of the extended
+    regions, either 'left' or 'right' of the actual cell. Currently only 'left'
+    is giving correct results though. The corresponding face of the cell is
+    acting like a perfect absorber. This system is then given to the Poisson
+    solver. The results is de-extended and the dipole component are added back
+    after.
+
+    Usage:
+    gpts = h2gpts(0.2, atoms.cell, idiv=16)
+    extgpts = (gpts[0], gpts[1], gpts[2] * 3)
+
+    calc = GPAW(mode='lcao',
+            gpts=gpts,
+            charge=1,
+            basis='dzp',
+            poissonsolver=ImagePoissonSolver(direction=2, side='left',
+                               extended={'gpts': extgpts, 'useprev': True}),
+               )
+
+    """
+
     def __init__(self, direction, side, *args, **kwargs):
-        super(EPS2, self).__init__(*args, **kwargs)
-        # Currently z-direction only. Fix this
+        super(ImagePoissonSolver, self).__init__(*args, **kwargs)
+        # XXX Currently z-direction only. Fix this
         assert direction == 2
         self.direction = direction
         self.c = direction
@@ -26,17 +54,30 @@ class EPS2(EPS):
         self.correction = None
 
     def set_grid_descriptor(self, gd):
-        super(EPS2, self).set_grid_descriptor(gd)
-        cell_cv = self.gd.cell_cv
+        # super(ImagePoissonSolver, self).set_grid_descriptor(gd)
+        if self.is_extended:
+            self.gd_original = gd
+            assert np.all(self.gd_original.N_c <= self.extended['finegpts']), \
+                'extended grid has to be larger than the original one'
+
+            extra_points = self.extended['finegpts'][self.c] // 2
+            extend_N_cd = np.zeros((3,2), dtype=int)
+            if  self.side == 'left':
+                extend_N_cd[self.c,0] = extra_points
+            else:
+                extend_N_cd[self.c,1] = extra_points                    
+            gd, _, _ = extended_grid_descriptor(gd, extend_N_cd=extend_N_cd,
+                                            extcomm=self.extended.get('comm'))
+
+        FDPoissonSolver.set_grid_descriptor(self, gd)
+
         # Allow only orthogonal cells for sanities sake
-        for i in range(2):
-            dir2 = (self.direction + 1 + i) % 3
-            assert np.dot(cell_cv[self.direction], cell_cv[dir2]) < 1e-8
+        assert gd.orthogonal
         # This is not supposed to be used with all periodic boundary conditions...
         assert not self.gd.pbc_c[self.direction]
 
     def get_description(self):
-        description = super(EPS2, self).get_description()
+        description = super(ImagePoissonSolver, self).get_description()
 
         lines = [description]
         lines.extend(['    Adding mirror charges to neutralize the system'])
@@ -48,33 +89,24 @@ class EPS2(EPS):
                                                              self.gd_original,
                                                              rhot_g)
 
-        #print("drhot_g[z]: {}".format(drhot_g.mean(0).mean(0)))
-        #print("dvHt_g[z]: {}".format(dvHt_g.mean(0).mean(0)*Ha))
-        #print("pot shift: {}".format(self.correction*Ha))
-
         # shift dipole potential to be zero at "electrode"
         
-	# XXX Use self.correction to shift the potential. The question is 
-	# is just, how do you figure out, if it's + or - 
-	if self.side == 'right':
+        # XXX Use self.correction to shift the potential. The question is 
+        # is just, how do you figure out, if it's + or - 
+        if self.side == 'right':
             # XXX "right" doesn't work. This ugly fix helps, but it's still wrong.
             drhot_g *= -1.
             dvHt_g *= -1.
             #self.correction *= -1. 
 
         dvHt_g -= self.correction
-	# XXX hamiltonian.get_electrostatic_potential() uses this one. We don't like that.
-	self.correction = 0.0
+        # XXX hamiltonian.get_electrostatic_potential() uses this one. We don't like that.
+        self.correction = 0.0
         vHt_g -= dvHt_g
 
-        iters = super(EPS2, self).solve(vHt_g, rhot_g + drhot_g, **kwargs)
+        iters = super(ImagePoissonSolver, self).solve(vHt_g, rhot_g + drhot_g,
+                                                      **kwargs)
         
-        #from matplotlib import pyplot as pl
-        #pl.plot(vHt_g.mean(0).mean(0)*Ha)
-        #pl.plot((vHt_g+dvHt_g).mean(0).mean(0)*Ha, '-.')
-        #pl.plot((dvHt_g).mean(0).mean(0)*Ha, '.')
-        #pl.show()
-
         vHt_g += dvHt_g
         return iters
 
@@ -82,47 +114,35 @@ class EPS2(EPS):
                zero_initial_phi=False):
 
         from gpaw.utilities.grid_redistribute import AlignedGridRedistributor
-        #print('System charge: {}\nIntegrated charge: {}'.
-              #format(charge, self.gd.integrate(rho))
         
         # Redistribute grid distriptor to reduce communication later.
         # Aka each rank has whole z-domain.
         gd = self.gd
-        redist = AlignedGridRedistributor(gd, 0, 2)
+        redist = AlignedGridRedistributor(gd, 0, 2) # XXX direction
         rho_xy = redist.forth(rho)
 
-        # XXX ugly
-        extend2_Nz_d = np.zeros(3, dtype=int)
-        extend2_Nz_d[0] = gd.extend_N_cd[2, 0]
-        extend2_Nz_d[1] = self.gd_original.N_c[2]
-        extend2_Nz_d[2] = gd.extend_N_cd[2, 1]
-
-        assert extend2_Nz_d.sum() == gd.N_c[2]
-        
-        oldcellstart = extend2_Nz_d[0]
-        oldcellstop = extend2_Nz_d[0] + extend2_Nz_d[1]
-        #print('start/stop: {} {}'.format(oldcellstart, oldcellstop))
-
-        # XXX there is some problem with the fact, that the last element in rho
-        # is always zero and acutally not part of the original array????????
-        rho_central = rho_xy[:, :, oldcellstart:oldcellstop]
+        # Our direction is non-periodic, so we have one element less in array.
+        # First element of original array is implicit zero.
         if self.side == 'left':
-            rho_xy[:, :, :oldcellstart] -= rho_central[:, :, ::-1]
-        elif self.side == 'right':
-            # Have to cut first element...
-            rho_central = rho_central[:, :, 1:]
-            rho_xy[:, :, oldcellstop:] -= rho_central[:, :, ::-1]
-                
-        mirror_rho = redist.back(rho_xy)
-        rho = mirror_rho
+            # N-1 points: Have two artifical zeros in center. Last point
+            # get cut off. Shift by 1 element
+            startpoint = gd.extend_N_cd[self.c, 0] - 1 
+        else:
+            startpoint = 0
+        stoppoint = startpoint + self.gd_original.N_c[self.c] - 1
 
-        #from matplotlib import pyplot as pl
-        #pl.plot(rho.sum(0).sum(0))
-        #pl.show()
-        
-        x = super(EPS2, self)._solve(phi, rho, charge=0, eps=None,
-                                     maxcharge=1e-6,
-                                     zero_initial_phi=False)
+        rho_central = rho_xy[:, :, startpoint:stoppoint] # XXX direction
+        if self.side == 'left':
+            rho_xy[:, :, 0:startpoint] -= rho_central[:, :, ::-1]
+        else:
+            rho_xy[:, :, stoppoint:-1] -= rho_central[:, :, ::-1]
+            rho_xy[:, :, -1] = 0.0
+
+        rho = redist.back(rho_xy)
+
+        x = super(ImagePoissonSolver, self)._solve(phi, rho, charge=0,
+                                                   eps=None,
+                                                   maxcharge=1e-6,
+                                                   zero_initial_phi=False)
 
         return x
-
--- gpaw/test/poisson_image_h3o+.py
+++ gpaw/test/poisson_image_h3o+.py
@@ -1,7 +1,7 @@
 from ase.build import molecule
 from gpaw import GPAW
 from gpaw.utilities import h2gpts
-from gpaw.poisson_image import EPS2
+from gpaw.poisson_image import ImagePoissonSolver as EPS2
 from ase.parallel import parprint
 
 atoms = molecule('NH3')
@@ -10,7 +10,7 @@ atoms.center(vacuum=5.0)
 
 atoms.pbc = (1,1,0)
 gpts = h2gpts(0.2, atoms.cell, idiv=16)
-extgpts = (gpts[0], gpts[1], gpts[2] * 3)
+extgpts = (gpts[0], gpts[1], gpts[2] * 2)
 
 calc = GPAW(mode='lcao',
             gpts=gpts,
@@ -27,6 +27,8 @@ pot = calc.get_electrostatic_potential().mean(0).mean(0)
 parprint('Potential[1]', pot[1])
 parprint('Potential[-1]', pot[-1])
 
-assert abs(pot[1] - -0.04969663) < 1e-3
-assert abs(pot[-1] - -6.56813735) < 1e-3
+assert abs(pot[1] - -0.220660556026) < 1e-3
+assert abs(pot[-1] - -6.61986899999) < 1e-3
+# assert abs(pot[1] - -0.04969663) < 1e-3
+# assert abs(pot[-1] - -6.56813735) < 1e-3
 
