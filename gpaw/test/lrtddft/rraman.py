3aa39c179371864a4461da227f0715e96e589acc
Michael Walter <Michael.Walter@fmf.uni-freiburg.de>
2017-10-04 12:02:31 +0200

    Explicit test for Placzek

--- gpaw/test/lrtddft/rraman.py
+++ gpaw/test/lrtddft/rraman.py
@@ -1,8 +1,9 @@
 from ase import Atoms, Atom
-from ase.vibrations.resonant_raman import ResonantRaman
+from ase.vibrations.placzek import Placzek
 
 from gpaw import GPAW
 from gpaw.lrtddft.kssingle import KSSingles
+from gpaw.test import equal
 
 txt = '-'
 txt = None
@@ -22,15 +23,19 @@ H2.set_calculator(calc)
 H2.get_potential_energy()
 
 gsname = exname = 'rraman'
-rr = ResonantRaman(H2, KSSingles, gsname=gsname, exname=exname,
-                   exkwargs={'eps':0.0, 'jend':1}
-               )
-rr.run()
+pz = Placzek(H2, KSSingles, gsname=gsname, exname=exname,
+             exkwargs={'eps':0.0, 'jend':1}
+)
+pz.run()
 
 # check size
 kss = KSSingles('rraman-d0.010.eq.ex.gz')
 assert(len(kss) == 1)
 
-rr = ResonantRaman(H2, KSSingles, gsname=gsname, exname=exname, 
+pz = Placzek(H2, KSSingles, gsname=gsname, exname=exname, 
                    verbose=True,)
-rr.summary(omega=5, method='frederiksen')
+ai = pz.absolute_intensity(omega=5)[-1]
+equal(ai, 299.955946107, 1e-3) # earlier obtained value
+i = pz.intensity(omega=5)[-1]
+equal(i, 7.82174195159e-05, 1e-11) # earlier obtained value
+pz.summary(omega=5, method='frederiksen')
