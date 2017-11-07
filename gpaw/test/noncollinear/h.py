1c2e2d870b8652fc4cf086f0b6683e6c5afd14b6
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-11-07 16:54:27 +0100

    Make the test fail again

--- gpaw/test/noncollinear/h.py
+++ gpaw/test/noncollinear/h.py
@@ -2,8 +2,9 @@ from ase import Atoms
 from gpaw import GPAW
 a = Atoms('H', cell=[2, 2, 2], magmoms=[1], pbc=(1, 0, 0))
 a.calc = GPAW(mode='pw',
-              kpts=(2, 1, 1),
+              kpts=(4, 1, 1),
+              symmetry={'do_not_symmetrize_the_density': True},
               #experimental={'magmoms': [[0.5, 0.5, 0]]}
-              experimental={'magmoms': [[1, 0, 0]]}
+              #experimental={'magmoms': [[1, 0, 0]]}
               )
 a.get_potential_energy()
