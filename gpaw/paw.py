ab0704e0537aecada6253883c0a39b5bbe899c90
Tuomas Rossi <tuomas.rossi@aalto.fi>
2017-11-16 10:25:02 +0200

    Avoid circular references with observers

--- gpaw/paw.py
+++ gpaw/paw.py
@@ -77,6 +77,13 @@ class PAW:
                 # of the method and avoid circular reference:
                 function = function.__func__.__name__
 
+        # Replace self in args with another unique reference
+        # to avoid circular reference
+        if not hasattr(self, 'self_magicref'):
+            self.self_magicref = object()
+        magic = self.self_magicref
+        args = tuple([magic if arg is self else arg for arg in args])
+
         self.observers.append((function, n, args, kwargs))
 
     def call_observers(self, iter, final=False):
@@ -97,6 +104,9 @@ class PAW:
             if call:
                 if isinstance(function, str):
                     function = getattr(self, function)
+                # Replace self reference with self
+                magic = self.self_magicref
+                args = tuple([self if arg is magic else arg for arg in args])
                 function(*args, **kwargs)
 
     def get_reference_energy(self):
