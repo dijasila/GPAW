f2ee0718acc31d9708778dfe02ab0bee24a40284
Michael Walter <Michael.Walter@fmf.uni-freiburg.de>
2017-12-01 09:53:55 +0100

    There was still too much symmetry in the peridic direction.

--- gpaw/test/fd_ops/gd.py
+++ gpaw/test/fd_ops/gd.py
@@ -8,7 +8,7 @@ assert gd.integrate(a.real, a.real) == 1.0
 
 gd = GridDescriptor([10] * 3, cell_cv=[1.] * 3, pbc_c=[1, 0, 0])
 # Point outside box in non-periodic direction should stay
-r_v = np.array([0, 2.5, 0])
+r_v = np.array([0.01, 2.5, 0])
 dr_cG = gd.get_grid_point_distance_vectors(r_v)
 assert (dr_cG[:, 0, 0, 0] == np.dot(gd.h_cv, gd.beg_c) - r_v).all()
 # Point outside box in periodic direction should be folded inside
