d287b785d19c856f814a77d997a1c1f614b918ca
hpleva <hpleva@utu.fi>
2017-12-05 14:11:22 +0200

    Fix force component comparison in qna_force test.

--- gpaw/test/xc/qna_force.py
+++ gpaw/test/xc/qna_force.py
@@ -42,4 +42,4 @@ parprint('Analytical force = ',F)
 parprint('Numerical  force = ',F_num)
 parprint('Difference       = ',F_err)
 assert abs(F_err) < 1.5e-3
-assert abs(E[-1]-270.179014) < 2e-6
+assert abs(E[-1]-270.17901094) < 2e-6
