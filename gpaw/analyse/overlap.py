e3be6a25d40f5f14f169ce97da9f48052a72c1d1
Michael Walter <Michael.Walter@fmf.uni-freiburg.de>
2017-12-12 17:44:41 +0100

    Work on full overlap - still wrong

--- gpaw/analyse/overlap.py
+++ gpaw/analyse/overlap.py
@@ -24,13 +24,15 @@ class Overlap:
             u_ij =  \int dx mypsitilde_i^*(x) otherpsitilde_j(x)
         """
         no = other.get_number_of_bands() * other.density.nspins
-        assert(self.calc.density.nspins == 1)
-        assert(other.density.nspins == 1)
+        assert(len(self.calc.wfs.kpt_u) == 1)
+        assert(len(other.wfs.kpt_u) == 1)
 
         overlap_nn = np.zeros((self.n, no), dtype=self.calc.wfs.dtype)
-        psit_nG = self.calc.wfs.kpt_u[0].psit_nG
+        mkpt = self.calc.wfs.kpt_u[0]
+        okpt = other.wfs.kpt_u[0]
+        psit_nG = mkpt.psit_nG
         norm_n = self.gd.integrate(psit_nG.conj() * psit_nG)
-        psito_nG = other.wfs.kpt_u[0].psit_nG
+        psito_nG = okpt.psit_nG
         normo_n = other.wfs.gd.integrate(psito_nG.conj() * psito_nG)
         for i in range(self.n):
             p_nG = np.repeat(psit_nG[i].conj()[np.newaxis], no, axis=0)
@@ -39,3 +41,28 @@ class Overlap:
                 overlap_nn[i] /= np.sqrt(np.repeat(norm_n[i], no) * normo_n)
         return overlap_nn
 
+    def full(self, other):
+        """Overlap with pseudo wave functions only
+
+        Parameter
+        ---------
+        other: gpaw
+            gpaw-object containing pseudo wave functions
+        normalize: bool
+            normalize pseudo wave functions in the overlap integral
+
+        Returns
+        -------
+        out: array
+            u_ij =  \int dx mypsi_i^*(x) otherpsi_j(x)
+        """
+        ov_nn = self.pseudo(other, normalize=False)
+        mkpt = self.calc.wfs.kpt_u[0]
+        okpt = other.wfs.kpt_u[0]
+        
+        for a, mP_ni in mkpt.P_ani.items():
+            oP_ni = okpt.P_ani[a]
+            Delta_i = self.calc.wfs.setups[a].Delta_pL[:,0]
+            for i, P_i in enumerate(mP_ni):
+                ov_nn[i] += np.dot(oP_ni, P_i)
+        return ov_nn
