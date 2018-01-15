be2b47c8ad5a4ffc7d8c018e729c16f3d0489022
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2018-01-15 14:31:08 +0100

    Test also spectrum

--- gpaw/test/lcaotddft/simple.py
+++ gpaw/test/lcaotddft/simple.py
@@ -5,6 +5,7 @@ from gpaw import GPAW
 from gpaw.lcaotddft import LCAOTDDFT
 from gpaw.poisson import PoissonSolver
 from gpaw.lcaotddft.dipolemomentwriter import DipoleMomentWriter
+from gpaw.tddft.spectrum import photoabsorption_spectrum
 
 from gpaw.test import equal
 
@@ -30,6 +31,7 @@ td_calc = LCAOTDDFT('%s_gs.gpw' % name,
 DipoleMomentWriter(td_calc, '%s_dm.dat' % name)
 td_calc.absorption_kick(np.ones(3) * 1e-5)
 td_calc.propagate(20, 3)
+photoabsorption_spectrum('%s_dm.dat' % name, '%s_spec.dat' % name, delta_e=5)
 
 # Test dipole moment
 data_i = np.loadtxt('%s_dm.dat' % name)[:, 2:].ravel()
@@ -54,4 +56,42 @@ ref_i = [4.786589735249e-15,
          5.385043148270e-05]
 
 tol = 1e-12
+equal(data_i, ref_i, tol)
+
+# Test spectrum
+data_i = np.loadtxt('%s_spec.dat' % name).ravel()
+if 0:
+    from gpaw.test import print_reference
+    print_reference(data_i, 'ref_i', '%.12le')
+
+ref_i = [0.000000000000e+00,
+         0.000000000000e+00,
+         0.000000000000e+00,
+         0.000000000000e+00,
+         5.000000000000e+00,
+         4.500226506100e-03,
+         4.500226505900e-03,
+         4.408376617800e-03,
+         1.000000000000e+01,
+         1.659425994100e-02,
+         1.659425994100e-02,
+         1.623811179300e-02,
+         1.500000000000e+01,
+         3.244686580500e-02,
+         3.244686580400e-02,
+         3.168680387500e-02,
+         2.000000000000e+01,
+         4.684883362500e-02,
+         4.684883362300e-02,
+         4.559686832900e-02,
+         2.500000000000e+01,
+         5.466780758300e-02,
+         5.466780758100e-02,
+         5.290167692700e-02,
+         3.000000000000e+01,
+         5.231585754700e-02,
+         5.231585754600e-02,
+         5.008658428800e-02]
+
+tol = 1e-10
 equal(data_i, ref_i, tol)
