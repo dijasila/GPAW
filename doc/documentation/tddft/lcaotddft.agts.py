518f43bc144df6c89694d14ffacf0c2f223c29dd
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2018-01-19 15:46:12 +0100

    Run the example script for testing

--- doc/documentation/tddft/lcaotddft.agts.py
+++ doc/documentation/tddft/lcaotddft.agts.py
@@ -2,3 +2,4 @@ def agts(queue):
     basis = queue.add('lcaotddft_basis.py', ncpus=1, walltime=10)
     ag55 = queue.add('lcaotddft_ag55.py', deps=[basis], ncpus=48, walltime=100)
     queue.add('lcaotddft_fig1.py', deps=[ag55], creates='fig1.png')
+    queue.add('lcaotddft.py', ncpus=4, walltime=40)
