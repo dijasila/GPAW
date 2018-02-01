0798bf37d7fd62d2e1da8c6c973313b32a260333
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-02-01 10:18:30 +0100

    Sort of works

--- gpaw/utilities/ibz2bz.py
+++ gpaw/utilities/ibz2bz.py
@@ -1,32 +1,52 @@
-import sys
-
 import numpy as np
-from ase.io.ulm import copy
+import ase.io.ulm as ulm
 
 from gpaw import GPAW
 
 
 def ibz2bz(name):
+    """Unfold IBZ to full BZ and write new gpw-file.
+
+    Example::
+
+        ibz2bz('abc.gpw')
+
+    This wil create a 'abc-bz.gpw' file.
+    """
+
     calc = GPAW(name, txt=None)
     spos_ac = calc.atoms.get_scaled_positions()
     kd = calc.wfs.kd
-    print(kd.N_c)
+
+    nK = kd.N_c.prod()
+    nbands = calc.wfs.bd.nbands
+    ns = calc.wfs.nspins
+
+    I_a = None
 
     for K, k in enumerate(kd.bz2ibz_k):
-        a_a, U_aii, time_rev = construct_symmetry_operators(wfs, spos_ac, K)
+        a_a, U_aii, time_rev = construct_symmetry_operators(calc.wfs,
+                                                            spos_ac, K)
 
         if I_a is None:
+            # First time:
             I1 = 0
             I_a = [0]
             for U_ii in U_aii:
                 I2 = I1 + len(U_ii)
-                I_a.eppend(I2)
+                I_a.append(I2)
                 I1 = I2
-            P_sknI = np
 
-        for s in range(len(calc.wfs.nspins)):
-            P_nI = calc.collect_projections(k, s)
-            P2_nI = np.empty_like(P_nI)
+            # Allocate arrays:
+            P_sKnI = np.empty((ns, nK, nbands, I2), calc.wfs.dtype)
+            e_sKn = np.empty((ns, nK, nbands))
+            f_sKn = np.empty((ns, nK, nbands))
+
+        for s in range(ns):
+            e_sKn[s, K] = calc.get_eigenvalues(k, s)
+            f_sKn[s, K] = calc.get_occupation_numbers(k, s)
+            P_nI = calc.wfs.collect_projections(k, s)
+            P2_nI = P_sKnI[s, K]
             a = 0
             for b, U_ii in zip(a_a, U_aii):
                 P_ni = np.dot(P_nI[:, I_a[b]:I_a[b + 1]], U_ii)
@@ -35,16 +55,25 @@ def ibz2bz(name):
                 P2_nI[:, I_a[a]:I_a[a + 1]] = P_ni
                 a += 1
 
-    copy(name, name[:-4] + '-bz.gpw', exclude={'.wave_functions'},
-         extra={'wave_functions': {
-                    'kpts': {
-                        'bz2ibz': ,
-                        'bzkpts': ,
-                        'ibzkpts': ,
-                        'weights': ,},
-                    'eigenvalues': e_skn,
-                    'occupations': f_skn,
-                    'projections': P_sknI}})
+    # Write new gpw-file:
+    parameters = calc.todict()
+    parameters['symmetry'] = 'off'
+
+    bzkpts_Kc = kd.ibzk_kc[kd.bz2ibz_k]
+
+    out = ulm.open(name[:-4] + '-bz.gpw', 'w')
+    ulm.copy(name, out, exclude={'.wave_functions', '.parameters'})
+    out.write(parameters=parameters)
+    wfs = out.child('wave_functions')
+    wfs.write(eigenvalues=e_sKn,
+              occupations=f_sKn,
+              projections=P_sKnI)
+    kpts = wfs.child('kpts')
+    kpts.write(bz2ibz=np.arange(nK),
+               bzkpts=bzkpts_Kc,
+               ibzkpts=bzkpts_Kc,
+               weights=np.ones(nK, int))
+    out.close()
 
 
 def construct_symmetry_operators(wfs, spos_ac, K):
@@ -83,6 +112,7 @@ def construct_symmetry_operators(wfs, spos_ac, K):
 
 
 if __name__ == '__main__':
+    import sys
     ibz2bz(sys.argv[1])
 
 
