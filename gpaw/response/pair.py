702cc5ad1d58153954062bc3bb076bcf2b02c3b8
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-02-01 13:24:08 +0100

    Python3 fix

--- gpaw/response/pair.py
+++ gpaw/response/pair.py
@@ -1,8 +1,8 @@
 from __future__ import print_function, division
 
-import sys
 import functools
-
+import numbers
+import sys
 from math import pi
 
 import numpy as np
@@ -791,7 +791,7 @@ class PairDensity:
         kd = wfs.kd
 
         # Parse kpoint: is k_c an index or a vector
-        if not isinstance(k_c, int):
+        if not isinstance(k_c, numbers.Integral):
             K = self.find_kpoint(k_c)
             shift0_c = (kd.bzk_kc[K] - k_c).round().astype(int)
         else:
