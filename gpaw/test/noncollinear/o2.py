7d93b7ac4fd858140659a10cfc3ec6c64537bd2f
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-02-06 16:48:21 +0100

    Write in parallel

--- gpaw/test/noncollinear/o2.py
+++ gpaw/test/noncollinear/o2.py
@@ -1,7 +1,12 @@
 from ase import Atoms
 from gpaw import GPAW
-a = Atoms('OO', [[0, 0, 0], [0, 0, 1.1]], magmoms=[1, 1])
+a = Atoms('OO', [[0, 0, 0], [0, 0, 1.1]], magmoms=[1, 1], pbc=(1, 0, 0))
 a.center(vacuum=2.5)
 a.calc = GPAW(mode='pw',
+              kpts=(2, 1, 1),
+              symmetry='off',
               experimental={'magmoms': [[0, 0.5, 0.5], [0, 0, 1]]})
 a.get_forces()
+a.calc.write('o2.gpw')
+a.calc.write('o2b.gpw', 'all')
+GPAW('o2.gpw')
