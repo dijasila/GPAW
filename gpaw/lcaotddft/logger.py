27c5f3196ddf31f0fe53a48d0d31f899c6fad5e6
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2018-03-08 10:07:07 +0100

    Flush log periodically

--- gpaw/lcaotddft/logger.py
+++ gpaw/lcaotddft/logger.py
@@ -7,8 +7,11 @@ from gpaw.tddft.units import autime_to_attosec
 
 class TDDFTLogger(TDDFTObserver):
 
-    def __init__(self, paw, interval=1):
+    def __init__(self, paw, flush_interval=10, interval=1):
         TDDFTObserver.__init__(self, paw, interval)
+        assert flush_interval > 0
+        self.flush_interval = flush_interval
+        self.flush_next = paw.niter + self.flush_interval - 1
 
     def _update(self, paw):
         if paw.action == 'init':
@@ -21,6 +24,7 @@ class TDDFTLogger(TDDFTObserver):
         line = ('      %4s %9s %11s %9s' %
                 ('iter', 'realtime', 'calctime', 'log(norm)'))
         paw.log(line)
+        paw.log.flush()
 
     def _write_data(self, paw):
         density = paw.density
@@ -30,3 +34,8 @@ class TDDFTLogger(TDDFTObserver):
                 (paw.niter, T[3], T[4], T[5],
                  paw.time * autime_to_attosec,
                  ln(abs(norm) + 1e-16) / ln(10)))
+        if paw.niter > self.flush_next:
+            paw.log.flush()
+            while paw.niter > self.flush_next:
+                self.flush_next += self.flush_interval
+
