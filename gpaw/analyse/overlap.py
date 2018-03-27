4b199c842e1a19689274402bd2e6b1cf9c51a8bb
Michael Walter <mcoywalter@gmail.com>
2018-03-27 11:08:08 +0200

    Do not ultiply independent other k-points

--- gpaw/analyse/overlap.py
+++ gpaw/analyse/overlap.py
@@ -6,11 +6,11 @@ class Overlap:
     """Wave funcion overlap of two GPAW objects"""
     def __init__(self, calc):
         self.calc = calc
-        self.n = self.number_of_states(calc)
+        self.nb, self.nk = self.number_of_states(calc)
         self.gd = self.calc.wfs.gd
 
     def number_of_states(self, calc):
-        return calc.get_number_of_bands() * len(calc.wfs.kd.ibzk_kc)
+        return calc.get_number_of_bands(), len(calc.wfs.kd.ibzk_kc)
 
     def pseudo(self, other, normalize=True):
         """Overlap with pseudo wave functions only
@@ -27,22 +27,22 @@ class Overlap:
         out: array
             u_ij =  \int dx mypsitilde_i^*(x) otherpsitilde_j(x)
         """
-        no = self.number_of_states(other)
-        assert(len(self.calc.wfs.kpt_u) == 1)
-        assert(len(other.wfs.kpt_u) == 1)
+        nbo, nko = self.number_of_states(other)
+        assert(self.nk == 1)
+        assert(nko == 1)
 
-        overlap_nn = np.zeros((self.n, no), dtype=self.calc.wfs.dtype)
+        overlap_nn = np.zeros((self.nb, nbo), dtype=self.calc.wfs.dtype)
         mkpt = self.calc.wfs.kpt_u[0]
         okpt = other.wfs.kpt_u[0]
         psit_nG = mkpt.psit_nG
         norm_n = self.gd.integrate(psit_nG.conj() * psit_nG)
         psito_nG = okpt.psit_nG
         normo_n = other.wfs.gd.integrate(psito_nG.conj() * psito_nG)
-        for i in range(self.n):
-            p_nG = np.repeat(psit_nG[i].conj()[np.newaxis], no, axis=0)
+        for i in range(self.nb):
+            p_nG = np.repeat(psit_nG[i].conj()[np.newaxis], nbo, axis=0)
             overlap_nn[i] = self.gd.integrate(p_nG * psito_nG)
             if normalize:
-                overlap_nn[i] /= np.sqrt(np.repeat(norm_n[i], no) * normo_n)
+                overlap_nn[i] /= np.sqrt(np.repeat(norm_n[i], nbo) * normo_n)
         return overlap_nn
 
     def full(self, other):
