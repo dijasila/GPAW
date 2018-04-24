c24cab1f04807fd3a7f8e79e4504d0a2ca6481b5
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-04-24 12:35:03 +0200

    Fix NEB tests

--- doc/tutorials/neb/submit.agts.py
+++ doc/tutorials/neb/submit.agts.py
@@ -4,5 +4,5 @@ from myqueue.job import Job
 def workflow():
     return [
         Job('diffusion1.py@4x10s'),
-        Job('neb.py@12x1m', deps=['diffusion1.py']),
+        Job('neb.py@6x1m', deps=['diffusion1.py']),
         Job('check.py', deps=['neb.py'])]
