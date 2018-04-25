8032580c77a04a199273d813f939456207ce718e
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-04-25 11:36:00 +0200

    Adjust tmax

--- doc/documentation/pdos/agts.py
+++ doc/documentation/pdos/agts.py
@@ -3,7 +3,7 @@ from myqueue.job import Job
 
 def workflow():
     return [
-        Job('top.py@8x15s'),
+        Job('top.py@8x15m'),
         Job('pdos.py', deps=['top.py']),
-        Job('lcaodos_gs.py@8x15s'),
+        Job('lcaodos_gs.py@8x15m'),
         Job('lcaodos_plt.py', deps=['lcaodos_gs.py'])]
