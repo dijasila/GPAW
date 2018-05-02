471e7b01fd1b4c8df933b8d81a20df7b3ee3a1c0
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-05-02 08:04:20 +0200

    More time

--- gpaw/test/big/g2_1/submit.agts.py
+++ gpaw/test/big/g2_1/submit.agts.py
@@ -2,5 +2,5 @@ from myqueue.job import Job
 
 
 def workflow():
-    jobs = [Job('g21gpaw.py+{}@1x40m'.format(i)) for i in range(4)]
+    jobs = [Job('g21gpaw.py+{}@1x40h'.format(i)) for i in range(4)]
     return jobs + [Job('analyse.py', deps=jobs)]
