7083ee3e6d8cde1e97e33831df7b634aef47de31
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-05-04 07:23:41 +0200

    Add AGTS jobs

---
+++ doc/devel/ase_optimize/agts.py
@@ -0,0 +1,13 @@
+def workflow():
+    from myqueue.job import Job
+    return [Job('agts.py'),
+            Job('run_tests_emt.py', deps='agts.py'),
+            Job('run_tests.py+1@8x1d', deps='agts.py'),
+            Job('run_tests.py+2@8x1d', deps='agts.py'),
+            Job('analyze.py',
+                deps=['run_tests_emt.py', 'run_tests.py+1', 'run_tests.py+2'])]
+
+
+if __name__ == '__main__':
+    from ase.optimize.test.systems import create_database
+    create_database()  # creates systems.db
