afd7d43d07819e910379adff7fe6258b96b99c73
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-05-29 13:59:48 +0200

    Move non-agts files out of agts folder

--- doc/images.py
+++ doc/images.py
@@ -82,7 +82,8 @@ get('bgp', ['bgp_mapping_intranode.png',
 get('workshop13', ['workshop13_01_33-1.jpg'], 'static')
 get('workshop16', ['gpaw2016-photo.jpg'], 'static')
 
-# files from agtspath
+
+# files from http://wiki.fysik.dtu.dk/gpaw-files/things/
 
 scf_conv_eval_stuff = """
 scf_g2_1_pbe0_fd_calculator_steps.png
@@ -91,8 +92,7 @@ scf_dcdft_pbe_pw_calculator_steps.png
 scf_dcdft_pbe_pw_energy.csv
 """.split()
 
-get('agts-files', scf_conv_eval_stuff, target='documentation/scf_conv_eval',
-    source=agtspath)
+get('things', scf_conv_eval_stuff, target='documentation/scf_conv_eval')
 
 # Warning: for the moment dcdft runs are not run (files are static)!
 dcdft_pbe_aims_stuff = """
@@ -101,7 +101,7 @@ dcdft_aims.tight.01.16.db_raw.csv
 dcdft_aims.tight.01.16.db_Delta.txt
 """.split()
 
-get('agts-files', dcdft_pbe_aims_stuff, target='setups', source=agtspath)
+get('things', dcdft_pbe_aims_stuff, target='setups')
 
 # Warning: for the moment dcdft runs are not run (files are static)!
 dcdft_pbe_gpaw_pw_stuff = """
@@ -110,7 +110,7 @@ dcdft_pbe_gpaw_pw_raw.csv
 dcdft_pbe_gpaw_pw_Delta.txt
 """.split()
 
-get('agts-files', dcdft_pbe_gpaw_pw_stuff, target='setups', source=agtspath)
+get('things', dcdft_pbe_gpaw_pw_stuff, target='setups')
 
 # Warning: for the moment dcdft runs are not run (files are static)!
 dcdft_pbe_jacapo_stuff = """
@@ -119,7 +119,7 @@ dcdft_pbe_jacapo_raw.csv
 dcdft_pbe_jacapo_Delta.txt
 """.split()
 
-get('agts-files', dcdft_pbe_jacapo_stuff, target='setups', source=agtspath)
+get('things', dcdft_pbe_jacapo_stuff, target='setups')
 
 # Warning: for the moment dcdft runs are not run (files are static)!
 dcdft_pbe_abinit_fhi_stuff = """
@@ -128,7 +128,7 @@ dcdft_pbe_abinit_fhi_raw.csv
 dcdft_pbe_abinit_fhi_Delta.txt
 """.split()
 
-get('agts-files', dcdft_pbe_abinit_fhi_stuff, target='setups', source=agtspath)
+get('things', dcdft_pbe_abinit_fhi_stuff, target='setups')
 
 g2_1_stuff = """
 pbe_gpaw_nrel_ea_vs.csv pbe_gpaw_nrel_ea_vs.png
@@ -136,11 +136,11 @@ pbe_gpaw_nrel_opt_ea_vs.csv pbe_gpaw_nrel_opt_distance_vs.csv
 pbe_nwchem_def2_qzvppd_opt_ea_vs.csv pbe_nwchem_def2_qzvppd_opt_distance_vs.csv
 """.split()
 
-get('agts-files', g2_1_stuff, target='setups', source=agtspath)
+get('things', g2_1_stuff, target='setups')
 
 get('tutorials/wannier90', ['GaAs.png', 'Cu.png', 'Fe.png'])
 
-get('agts-files', ['datasets.json'], 'setups', source=agtspath)
+get('things', ['datasets.json'], 'setups')
 
 # Carlsberg foundation figure:
 get('.', ['carlsberg.png'])
