08e3578136e70da0a1c6044a0427eda8b0325ebf
Ask Hjorth Larsen <asklarsen@gmail.com>
2018-06-25 20:35:08 +0200

    synchronize atoms/spos_ac more rigorously

--- gpaw/calculator.py
+++ gpaw/calculator.py
@@ -182,11 +182,14 @@ class GPAW(PAW, Calculator):
 
     def _set_atoms(self, atoms):
         self.atoms = atoms
+        mpi.synchronize_atoms(self.atoms, self.world)
+
         # GPAW works in terms of the scaled positions.  We want to
         # extract the scaled positions in only one place, and that is
         # here.  No other place may recalculate them, or we might end up
         # with rounding errors and inconsistencies.
-        self.spos_ac = atoms.get_scaled_positions() % 1.0
+        self.spos_ac = np.ascontiguousarray(atoms.get_scaled_positions() % 1.0)
+        self.world.broadcast(self.spos_ac, 0)
 
     def read(self, filename):
         from ase.io.trajectory import read_atoms
@@ -451,8 +454,6 @@ class GPAW(PAW, Calculator):
         else:
             atoms = atoms.copy()
             self._set_atoms(atoms)
-
-        mpi.synchronize_atoms(atoms, self.world)
 
         rank_a = self.wfs.gd.get_ranks_from_positions(self.spos_ac)
         atom_partition = AtomPartition(self.wfs.gd.comm, rank_a, name='gd')
