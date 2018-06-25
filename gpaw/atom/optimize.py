16af254b7872eb195f70b901498930a3deae0875
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-06-25 12:53:29 +0200

    It works again

--- gpaw/atom/optimize.py
+++ gpaw/atom/optimize.py
@@ -1,15 +1,11 @@
-#!/bin/env python
 from __future__ import print_function, division
 import os
 import re
-import traceback
 
 import numpy as np
 from scipy.optimize import differential_evolution as DE
 from ase import Atoms
 from ase.data import covalent_radii, atomic_numbers, chemical_symbols
-from ase.build import bulk
-from ase.build import fcc111
 from ase.units import Bohr
 
 from gpaw import GPAW, PW, setup_paths, Mixer, ConvergenceError, Davidson
@@ -21,15 +17,16 @@ for e in ['Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr']:  # missing radii
     my_covalent_radii[atomic_numbers[e]] = 1.7
 
 
-NN = 5
+class PAWDataError(Exception):
+    """Error in PAW data generation."""
 
 
 class DatasetOptimizer:
-    tolerances = np.array([0.3,
+    tolerances = np.array([0.2,
+                           0.3,
                            40,
                            2 / 3 * 300 * 0.1**0.25,  # convergence
-                           0.0005,  # eggbox error
-                           0.2])
+                           0.0005])  # eggbox error
 
     def __init__(self, symbol='H', nc=False, processes=None):
         self.old = False
@@ -63,7 +60,8 @@ class DatasetOptimizer:
         # Round to integers:
         self.x = energies + radii + [r0]
         self.bounds = ([(-1.0, 4.0)] * self.nenergies +
-                       [(0.2, 1.7 * self.rc / Bohr)] * (len(radii) + 1))
+                       [(r * 0.6, r * 1.5) for r in radii] +
+                       [(0.3, max(radii) * 1.4)])
 
         self.ecut1 = 450.0
         self.ecut2 = 800.0
@@ -71,17 +69,13 @@ class DatasetOptimizer:
         setup_paths[:0] = ['.']
 
     def run(self):
-        print('Running:', self.symbol, self.rc)
-        # self.x = [3.96311677, 3.01924399, 0.98966047, 0.99035728, 0.84078698]
-        self.x = [-1.7, 1.9, 1.6, 1.2, 1.2]
-        self(self.x)
-        #DE(self, self.bounds)
+        print(self.symbol, self.rc / Bohr)
+        print(self.x)
+        print(self.bounds)
+        DE(self, self.bounds)
 
-    def generate(self, fd, xc, projectors, radii, r0,
-                 scalar_relativistic=False, tag=None, logderivs=True):
-        if self.old:
-            self.generate_old(fd, xc, scalar_relativistic, tag)
-            return 0.0
+    def generate(self, fd, projectors, radii, r0, xc,
+                 scalar_relativistic=True, tag=None, logderivs=True):
 
         if projectors[-1].isupper():
             nderiv0 = 5
@@ -97,12 +91,11 @@ class DatasetOptimizer:
                             scalar_relativistic, None, r0, nderiv0,
                             (type, 4), None, None, fd)
         except np.linalg.linalg.LinAlgError:
-            return np.inf
+            raise PAWDataError('LinAlgError')
 
         if not scalar_relativistic:
             if not gen.check_all():
-                print('dataset check failed')
-                return np.inf
+                raise PAWDataError('dataset check failed')
 
         if tag is not None:
             gen.make_paw_setup(tag or None).write_xml()
@@ -138,93 +131,54 @@ class DatasetOptimizer:
         return energies, radii, r0, projectors
 
     def __call__(self, x):
-        print(x)
-        fd = open('out.txt', 'w')
         energies, radii, r0, projectors = self.parameters(x)
 
-        if any(r < r0 for r in radii):  # or any(e <= 0.0 for e in energies):
-            # print(x, 'radii too small')
-            return np.inf
+        print('({}, {}, {:.2f}):'
+              .format(', '.join('{:+.2f}'.format(e) for e in energies),
+                      ', '.join('{:.2f}'.format(r) for r in radii),
+                      r0),
+              end='')
 
-        errors = self.test(fd, projectors, radii, r0)
-
+        fd = open('out.txt', 'w')
+        errors, msg = self.test(fd, projectors, radii, r0)
         error = ((errors / self.tolerances)**2).sum()
-        print(errors)
-        print(error)
+
+        print('{:9.1f} ({:.2f}, {:.3f}, {:.1f}, {}, {:.5f}) {}'
+              .format(error, *errors, msg),
+              flush=True)
+
         return error
 
     def test(self, fd, projectors, radii, r0):
-        error = self.generate(fd, 'PBE', projectors, radii, r0, tag='de')
-        for xc in ['PBE', 'LDA', 'PBEsol', 'RPBE', 'PW91']:
-            error += self.generate(fd, xc, projectors, radii, r0,
-                                   scalar_relativistic=True)
-
-        if not np.isfinite(error):
-            return [np.inf] * NN
-
-        results = {'dataset': error}
+        errors = [np.inf] * 5
 
         try:
-            results['convergence'], results['iters'] = self.convergence(fd)
-            results['eggbox'] = self.eggbox(fd)
-        except ConvergenceError:
-            return np.inf
-        except Exception as ex:
-            print(ex)
-            traceback.print_exc()
-            return np.inf
+            if any(r < r0 for r in radii):
+                raise PAWDataError('Core radius too large')
 
-        rc = self.rc / Bohr
-        results['radii'] = sum(r - rc for r in radii if r > rc)
+            rc = self.rc / Bohr
+            errors[0] = sum(r - rc for r in radii if r > rc)
 
-        errors = self.calculate_total_error(fd, results)
+            error = 0.0
+            for kwargs in [dict(xc='PBE', tag='de'),
+                           dict(xc='PBE', scalar_relativistic=False),
+                           dict(xc='LDA'),
+                           dict(xc='PBEsol'),
+                           dict(xc='RPBE'),
+                           dict(xc='PW91')]:
+                error += self.generate(fd, projectors, radii, r0, **kwargs)
+            errors[1] = error
 
-        return errors
+            errors[2:4] = self.convergence(fd)
 
-    def calculate_total_error(self, fd, results):
-        errors = [results['dataset']]
-        iters = results['iters']
+            errors[4] = self.eggbox(fd)
 
-        for name in []:  # 'fcc', 'rocksalt']:
-            result = results[name]
-            if isinstance(result, dict):
-                maxiter = max(maxiter, result['maxiter'])
-                errors.append(result['a'] - result['a0'])
-                errors.append(result['c90'] - result['c90ref'])
-                errors.append(result['c80'] - result['c80ref'])
-            else:
-                maxiter = np.inf
-                errors.extend([np.inf, np.inf, np.inf])
-
-        errors.append(iters)
-        errors.append(results['convergence'])
-
-        errors.append(results['eggbox'])
-
-        errors.append(results['radii'])
-
-        return errors
-
-    def slab(self, fd):
-        a0 = self.reference['fcc']['a']
-        atoms = fcc111(self.symbol, (1, 1, 7), a0, vacuum=3.5)
-        assert not atoms.pbc[2]
-        M = 333
-        if 58 <= self.Z <= 70 or 90 <= self.Z <= 102:
-            M = 1333
-            mixer = {'mixer': Mixer(0.001, 3, 100)}
+        except (ConvergenceError, PAWDataError) as e:
+            msg = str(e)
         else:
-            mixer = {}
-        atoms.calc = GPAW(mode=PW(self.ecut1),
-                          kpts={'density': 2.0, 'even': True},
-                          xc='PBE',
-                          setups='de',
-                          maxiter=M,
-                          txt=fd,
-                          **mixer)
-        atoms.get_potential_energy()
-        itrs = atoms.calc.get_number_of_iterations()
-        return itrs
+            msg = ''
+
+        return errors, msg
 
     def eggbox(self, fd):
         energies = []
@@ -254,7 +208,7 @@ class DatasetOptimizer:
             atoms.positions -= h / 6
             e3 = atoms.get_potential_energy()
             energies.append(np.ptp([e0, e1, e2, e3]))
-        print(energies)
+        # print(energies)
         return max(energies)
 
     def convergence(self, fd):
@@ -285,7 +239,7 @@ class DatasetOptimizer:
             atoms.calc.set(mode=PW(ec))
             atoms.calc.set(eigensolver='rmm-diis')
             de = atoms.get_potential_energy() - e0
-            print(ec, de)
+            # print(ec, de)
             fde = f(abs(de))
             if fde > f(0.1):
                 if oldfde is None:
@@ -299,85 +253,6 @@ class DatasetOptimizer:
             oldfde = fde
 
         return area, iters
-
-    def fcc(self, n, fd):
-        ref = self.reference['fcc']
-        a0r = ref['a']  # scalar-relativistic minimum
-        sc = min(0.8, 2 * self.rc * 2**0.5 / a0r)
-        sc = min((abs(s - sc), s) for s in ref if s != 'a')[1]
-        maxiter = 0
-        energies = []
-        M = 200
-        mixer = {}
-        if 58 <= self.Z <= 70 or 90 <= self.Z <= 102:
-            M = 999
-            mixer = {'mixer': Mixer(0.01, 5)}
-        for s in [sc, 0.9, 0.95, 1.0, 1.05]:
-            atoms = bulk(self.symbol, 'fcc', a0r * s)
-            atoms.calc = GPAW(mode=PW(self.ecut2),
-                              kpts={'density': 4.0, 'even': True},
-                              xc='PBE',
-                              setups='ga' + str(n),
-                              maxiter=M,
-                              txt=fd,
-                              **mixer)
-            e = atoms.get_potential_energy()
-            maxiter = max(maxiter, atoms.calc.get_number_of_iterations())
-            energies.append(e)
-
-        return {'c90': energies[1] - energies[3],
-                'c80': energies[0] - energies[3],
-                'c90ref': ref[0.9] - ref[1.0],
-                'c80ref': ref[sc] - ref[1.0],
-                'a0': fit([ref[s] for s in [0.95, 1.0, 1.05]]) * 0.05 * a0r,
-                'a': fit(energies[2:]) * 0.05 * a0r,
-                'maxiter': maxiter}
-
-    def rocksalt(self, n, fd):
-        ref = self.reference['rocksalt']
-        a0r = ref['a']
-        sc = min(0.8, 2 * (self.rc + self.rco) / a0r)
-        sc = min((abs(s - sc), s) for s in ref if s != 'a')[1]
-        maxiter = 0
-        energies = []
-        M = 200
-        mixer = {}
-        if 58 <= self.Z <= 70 or 90 <= self.Z <= 102:
-            M = 999
-            mixer = {'mixer': Mixer(0.01, 5)}
-        for s in [sc, 0.9, 0.95, 1.0, 1.05]:
-            atoms = bulk(self.symbol + 'O', 'rocksalt', a0r * s)
-            atoms.calc = GPAW(mode=PW(self.ecut2),
-                              kpts={'density': 4.0, 'even': True},
-                              xc='PBE',
-                              setups={self.symbol: 'ga' + str(n)},
-                              maxiter=M,
-                              txt=fd,
-                              **mixer)
-            e = atoms.get_potential_energy()
-            maxiter = max(maxiter, atoms.calc.get_number_of_iterations())
-            energies.append(e)
-
-        return {'c90': energies[1] - energies[3],
-                'c80': energies[0] - energies[3],
-                'c90ref': ref[0.9] - ref[1.0],
-                'c80ref': ref[sc] - ref[1.0],
-                'a0': fit([ref[s] for s in [0.95, 1.0, 1.05]]) * 0.05 * a0r,
-                'a': fit(energies[2:]) * 0.05 * a0r,
-                'maxiter': maxiter}
-
-    def best(self, N=None):
-        ga = GA(self.x)
-        best = sorted((error, id, x)
-                      for x, (error, id) in ga.individuals.items())
-        if 0:
-            import pickle
-            pickle.dump(sorted(ga.individuals.values()), open('Zn.pckl', 'wb'))
-        if N is None:
-            return len(best), best[0] + (ga.errors[best[0][1]],)
-        else:
-            return [(error, id, x, ga.errors[id])
-                    for error, id, x in best[:N]]
 
     def summary(self, N=10):
         # print('dFffRrrICEer:')
