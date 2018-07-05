8446121ef018e8c06720b51e6978e57e3586826f
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-07-05 14:09:21 +0200

    Test notebooks in AGTS

--- doc/summerschools/summerschool18/catalysis/agts.py
+++ doc/summerschools/summerschool18/catalysis/agts.py
@@ -1,3 +1,11 @@
+from pathlib import Path
 from myqueue.task import task
+
+
 def create_tasks():
-    return [task('run.py', tmax='5h')]
+    return [task('run.py', tmax='5h'),
+            task('nbrun.py', args='convergence', tmax='3h', deps='run.py'),
+            task('nbrun.py', args='n2_on_metal', tmax='3h'),
+            task('nbrun.py', args='neb', tmax='3h'),
+            task('ts.py'),
+            task('nbrun.py', args='vibrations', tmax='3h', deps='ts.py')]
