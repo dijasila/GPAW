b7b54193919b97499dcb231890e4893f6f6754e8
Esko Makkonen <makkonen@taito-login4.csc.fi>
2018-07-13 16:10:22 +0300

    cdwriter fixed 13.7

--- gpaw/lcaotddft/cdwriter.py
+++ gpaw/lcaotddft/cdwriter.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from gpaw.mpi import world
+from ase.units import Hartree, alpha, Bohr
 from gpaw.utilities.tools import coordinates
 from gpaw.fd_operators import Gradient
 from gpaw.lcaotddft.observer import TDDFTObserver
@@ -93,13 +95,18 @@ class CDWriter(TDDFTObserver):
 
         grad_psit_G = gd.empty(3, dtype=complex)
         psit_G=gd.empty(dtype=complex)
+        rxnabla_a = np.zeros(3,dtype=complex)
+        # Ra x <psi1| nabla |psi2>
+        Rxnabla_a = np.zeros(3, dtype=complex)
+        Rxnabla_a2 = np.zeros(3, dtype=complex)
 
         rxnabla_g = np.zeros(3, dtype=complex)
         R0 = 0.5 * np.diag(paw.wfs.gd.cell_cv)
         r_cG, r2_G = coordinates(paw.wfs.gd, origin=R0)
-
         for kpt in paw.wfs.kpt_u:
-            for f, C_M in zip(kpt.f_n, kpt.C_nM):
+            paw.wfs.atomic_correction.calculate_projections(paw.wfs, kpt)
+
+            for n, (f, C_M) in enumerate(zip(kpt.f_n, kpt.C_nM)):
                 psit_G[:] = 0.0
                 paw.wfs.basis_functions.lcao_to_grid(C_M, psit_G, kpt.q)
                 for c in range(3): 
@@ -113,14 +120,58 @@ class CDWriter(TDDFTObserver):
                 rxnabla_g[2] += -1j*f*paw.wfs.gd.integrate(psit_G.conjugate() *
                                                         (r_cG[0] * grad_psit_G[1] -
                                                          r_cG[1] * grad_psit_G[0]))
-        return rxnabla_g.real.copy()
+
+                # augmentation contributions to magnetic moment
+                # <psi1| r x nabla |psi2> = <psi1| (r-Ra+Ra) x nabla |psi2>
+                #                         = <psi1| (r-Ra) x nabla |psi2> + Ra x <psi1| nabla |psi2>
+
+
+                # <psi1| (r-Ra) x nabla |psi2>
+                for a, P_ni in kpt.P_ani.items():
+                    P_i = P_ni[n]
+                    def skew(a):
+                        return (a-a.T)/2
+
+                   
+                    rxnabla_iiv = paw.wfs.setups[a].rxnabla_iiv.copy()
+                    nabla_iiv = paw.wfs.setups[a].nabla_iiv.copy()
+
+                    for c in range(3):
+                        rxnabla_iiv[:,:,c]=skew(rxnabla_iiv[:,:,c])
+                        nabla_iiv[:,:,c]=skew(nabla_iiv[:,:,c])     
+
+
+
+                    Rxnabla_a2[0] += np.dot(P_i,np.dot(nabla_iiv[:,:,0],P_i.T.conjugate()))
+                    Ra = (paw.atoms[a].position /Bohr) - R0
+                    for i1, P1 in enumerate(P_i):
+                        for i2, P2 in enumerate(P_i):
+                            for c in range(3):
+                                rxnabla_a[c] += -1j*f*P1.conjugate() * P2 * rxnabla_iiv[i1, i2, c]
+
+                            # (y pz - z py)i + (z px - x pz)j + (x py - y px)k
+                            Rxnabla_a[0] +=-1j*f* P1.conjugate() * P2 * ( Ra[1] * nabla_iiv[i1, i2, 2] - Ra[2] * nabla_iiv[i1, i2, 1] )
+                            Rxnabla_a[1] +=-1j*f* P1.conjugate() * P2 * ( Ra[2] * nabla_iiv[i1, i2, 0] - Ra[0] * nabla_iiv[i1, i2, 2] )
+                            Rxnabla_a[2] +=-1j*f* P1.conjugate() * P2 * ( Ra[0] * nabla_iiv[i1, i2, 1] - Ra[1] * nabla_iiv[i1, i2, 0] )
+                               
+        paw.wfs.gd.comm.sum(rxnabla_a) # sum up from different procs  
+
+        paw.wfs.gd.comm.sum(Rxnabla_a) # sum up from different procs
+
+        return rxnabla_g+ Rxnabla_a+ rxnabla_a
 
     def _write_cd(self, paw):
         time = paw.time
 
         cd = self.calculate_cd_moment(paw,center=self.do_center)
+        #line = ('%20.8lf %22.12le %22.12le %22.12le %22.12le %22.12le %22.12le\n' %
+        # (time, rxnabla_g[0].real, Rxnabla_a[0].real, rxnabla_a[0].real, rxnabla_g[0].imag, Rxnabla_a[0].imag, rxnabla_a[0].imag))
+        
+
         line = ('%20.8lf %22.12le %22.12le %22.12le\n' %
-                (time, cd[0], cd[1], cd[2]))
+          (time, cd[0], cd[1], cd[2]))
+
+
         self._write(line)
 
     def _update(self, paw):
@@ -134,3 +185,13 @@ class CDWriter(TDDFTObserver):
         if self.master:
             self.fd.close()
         TDDFTObserver.__del__(self)
+
+
+
+
+
+
+
+
+
+
