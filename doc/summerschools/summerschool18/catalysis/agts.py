6d1aff951d61f2a3974b2ecc4bc656e0286f74ac
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-07-23 17:04:58 +0200

    More time for conv-test

--- doc/summerschools/summerschool18/catalysis/agts.py
+++ doc/summerschools/summerschool18/catalysis/agts.py
@@ -1,10 +1,9 @@
-from pathlib import Path
 from myqueue.task import task
 
 
 def create_tasks():
     return [
-        task('run.py', tmax='5h'),
+        task('run.py', tmax='10h'),
         task('nbrun.py', args=['convergence.ipynb'], tmax='3h',
              deps='run.py'),
         task('nbrun.py', args=['n2_on_metal.master.ipynb'], tmax='3h'),
