87d67fba7c122f757c133ba6e07ed83022239424
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-07-25 09:06:47 +0200

    Change version numbers back to before merge of master

--- gpaw/__init__.py
+++ gpaw/__init__.py
@@ -22,8 +22,8 @@ with broadcast_imports:
 
 assert not np.version.version.startswith('1.6.0')
 
-__version__ = '1.4.1b1'
-__ase_version_required__ = '3.16.1b1'
+__version__ = '1.4.0'
+__ase_version_required__ = '3.16.0'
 
 __all__ = ['GPAW',
            'Mixer', 'MixerSum', 'MixerDif', 'MixerSum2',
