96e1a39a173c85a47c0e10c8bbff1964f468f31e
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-07-30 14:41:05 +0200

    Test also virations notebook

--- doc/summerschools/summerschool18/catalysis/agts.py
+++ doc/summerschools/summerschool18/catalysis/agts.py
@@ -1,4 +1,4 @@
-# Creates: N2Ru_hollow.png, 2NadsRu.png
+# Creates: N2Ru_hollow.png, 2NadsRu.png, ts.traj
 from myqueue.task import task
 
 
@@ -10,7 +10,5 @@ def create_tasks():
         task(nbrun, args=['n2_on_metal.master.ipynb'], tmax='3h'),
         task(nbrun, args=['neb.master.ipynb'], tmax='3h', cores=8,
              deps=nbrun + '+n2_on_metal.master.ipynb'),
-        task('ts.py'),
-        # task(nbrun, args=['vibrations.master.ipynb'], tmax='3h',
-        #      deps='ts.py')
-    ]
+        task(nbrun, args=['vibrations.master.ipynb'], tmax='9h', cores=8,
+             deps=nbrun + '+neb.master.ipynb')]
