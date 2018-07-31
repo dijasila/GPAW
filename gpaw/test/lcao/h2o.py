4c3245a0e8adc177646b26434423496b8c74e95a
Mikael Kuisma <mikael.j.kuisma@jyu.fi>
2018-07-31 21:39:37 +0300

    Remove prints.

--- gpaw/test/lcao/h2o.py
+++ gpaw/test/lcao/h2o.py
@@ -19,7 +19,7 @@ equal(e, -10.266984, 1e-4)
 equal(niter, 8, 1)
 
 # Check that complex wave functions are allowed with gamma point calculations
-calc = GPAW(gpts=(32, 32, 32), nbands=4, mode=LCAO(force_complex_dtype=True))
+calc = GPAW(gpts=(32, 32, 32), nbands=4, mode=LCAO(force_complex_dtype=True), poissonsolver=FDPoissonSolver())
 mol.set_calculator(calc)
 ec = mol.get_potential_energy()
 equal(e, ec, 1e-5)
