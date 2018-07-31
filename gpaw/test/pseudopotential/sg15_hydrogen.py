5ac71f7081bea8e55e6908b435119ccc7bd57ab5
Mikael Kuisma <mikael.j.kuisma@jyu.fi>
2018-07-31 16:55:54 +0300

    Fix few more tests.

--- gpaw/test/pseudopotential/sg15_hydrogen.py
+++ gpaw/test/pseudopotential/sg15_hydrogen.py
@@ -3,7 +3,7 @@
 
 from __future__ import print_function
 from ase.build import molecule
-from gpaw import GPAW, FDPoissonSolver as PoissonSoler, Davidson, Mixer
+from gpaw import GPAW, FDPoissonSolver as PoissonSolver, Davidson, Mixer
 from gpaw.utilities import h2gpts
 from gpaw.test.pseudopotential.H_sg15 import pp_text
 from gpaw import setup_paths
