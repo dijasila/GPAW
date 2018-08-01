37986db688cce8f4603f323b32eee5f37410799d
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-08-01 08:30:07 +0200

    Run convergence test in parallel

--- doc/summerschools/summerschool18/catalysis/agts.py
+++ doc/summerschools/summerschool18/catalysis/agts.py
@@ -5,7 +5,7 @@ from myqueue.task import task
 def create_tasks():
     nbrun = 'gpaw.utilities.nbrun'
     return [
-        task('run.py', tmax='10h'),
+        task('run.py', tmax='5h', cores=8),
         task(nbrun, args=['convergence.ipynb'], deps='run.py'),
         task(nbrun, args=['n2_on_metal.master.ipynb'], tmax='3h'),
         task(nbrun, args=['neb.master.ipynb'], tmax='3h', cores=8,
