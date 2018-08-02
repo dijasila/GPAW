a2e5d5d2266433a9a5f9e5520511196e1c406cd0
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-08-02 17:25:32 +0200

    Run pv2 notebook on 8 cores

--- doc/summerschools/summerschool18/photovoltaics/agts.py
+++ doc/summerschools/summerschool18/photovoltaics/agts.py
@@ -5,7 +5,7 @@ def create_tasks():
     nbrun = 'gpaw.utilities.nbrun'
     return [
         task(nbrun, args=['pv1.master.ipynb'], tmax='13h'),
-        task(nbrun, args=['pv2.master.ipynb'], tmax='13h',
+        task(nbrun, args=['pv2.master.ipynb'], tmax='13h', cores=8,
              deps=nbrun + '+pv1.master.ipynb'),
         task(nbrun, args=['pv3.master.ipynb'], tmax='13h',
              deps=nbrun + '+pv2.master.ipynb')]
