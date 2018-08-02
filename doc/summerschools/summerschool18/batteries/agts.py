9d58e8d1a14151259bcfc9f244df51723c99b98c
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-08-02 13:50:33 +0200

    Enable tests of photovoltaics and batteries projects

--- doc/summerschools/summerschool18/batteries/agts.py
+++ doc/summerschools/summerschool18/batteries/agts.py
@@ -4,7 +4,8 @@ from myqueue.task import task
 def create_tasks():
     nbrun = 'gpaw.utilities.nbrun'
     return [
-        # task(nbrun, args=['batteries1.master.ipynb'], tmax='13h'),
-        # task(nbrun, args=['batteries2.master.ipynb'], tmax='13h'),
-        # task(nbrun, args=['batteries3.master.ipynb'], tmax='13h')
-    ]
+        task(nbrun, args=['batteries1.master.ipynb'], tmax='13h'),
+        task(nbrun, args=['batteries2.master.ipynb'], tmax='13h',
+             deps=nbrun + '+batteries1.master.ipynb')
+        task(nbrun, args=['batteries3.master.ipynb'], tmax='13h',
+             deps=nbrun + '+batteries2.master.ipynb')]
