75676dd3c30bb4878d211553c1cb7c40d07dbb5a
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-08-03 11:25:18 +0200

    Need more time on old GBAR computers

--- doc/summerschools/summerschool18/catalysis/agts.py
+++ doc/summerschools/summerschool18/catalysis/agts.py
@@ -7,7 +7,7 @@ def create_tasks():
     return [
         task('check_convergence.py', tmax='5h', cores=8),
         task(nbrun, args=['convergence.ipynb'], deps='check_convergence.py'),
-        task(nbrun, args=['n2_on_metal.master.ipynb'], tmax='3h'),
+        task(nbrun, args=['n2_on_metal.master.ipynb'], tmax='6h'),
         task(nbrun, args=['neb.master.ipynb'], tmax='3h', cores=8,
              deps=nbrun + '+n2_on_metal.master.ipynb'),
         task(nbrun, args=['vibrations.master.ipynb'], tmax='9h', cores=8,
