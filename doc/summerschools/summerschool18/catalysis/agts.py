c7f21cb4324ed3f348c2744262bb890d5fa280fb
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-08-10 12:43:42 +0200

    Revert to symmetric TS

--- doc/summerschools/summerschool18/catalysis/agts.py
+++ doc/summerschools/summerschool18/catalysis/agts.py
@@ -8,7 +8,7 @@ def create_tasks():
         task('check_convergence.py', tmax='5h', cores=8),
         task(nbrun, args=['convergence.ipynb'], deps='check_convergence.py'),
         task(nbrun, args=['n2_on_metal.master.ipynb'], tmax='6h'),
-        task(nbrun, args=['neb.master.ipynb'], tmax='3h', cores=24,
+        task(nbrun, args=['neb.master.ipynb'], tmax='3h', cores=8,
              deps=nbrun + '+n2_on_metal.master.ipynb'),
         task(nbrun, args=['vibrations.master.ipynb'], tmax='9h', cores=8,
              deps=nbrun + '+neb.master.ipynb')]
