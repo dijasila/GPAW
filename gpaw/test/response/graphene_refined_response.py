280b2c9c6f36a700fe9022d8022292ab8c88c26b
Morten Gjerding <mogje@fysik.dtu.dk>
2018-09-05 15:03:19 +0200

    Make small silly test of graphene response.

--- gpaw/test/response/graphene_refined_response.py
+++ gpaw/test/response/graphene_refined_response.py
@@ -1,10 +1,9 @@
-import numpy as np
-
 from ase.lattice.hexagonal import Graphene
 
 from gpaw import GPAW, PW, FermiDirac
-from gpaw.mpi import world
 from gpaw.response.df import DielectricFunction
+from ase.units import Hartree
+from gpaw.test import equal
 
 import os
 
@@ -14,41 +13,45 @@ system = Graphene(symbol='C',
 system.pbc = (1, 1, 0)
 system.center(axis=2, vacuum=4.0)
 
+nk = 6
+nkrefine = 5
+
 kpt_refine = {"center": [1. / 3, 1. / 3, 0.],
-              "size": [[5, 5, 1],
-                       [5, 5, 1],
-                       [5, 5, 1]],
+              "size": [nkrefine, nkrefine, 1],
               "reduce_symmetry": False,
-              'q': [1. / (15 * 5), 0, 0]}
+              'q': [1. / (nk * nkrefine), 0, 0]}
 
 if not os.path.exists('graphene.gpw'):
     calc = GPAW(mode=PW(ecut=400),
                 xc='PBE',
-                kpts={"size": [15, 15, 1], "gamma": True},
+                kpts={"size": [nk, nk, 1], "gamma": True},
                 experimental={'kpt_refine': kpt_refine},
                 occupations=FermiDirac(0.026))
     system.set_calculator(calc)
     system.get_potential_energy()
     calc.write('graphene.gpw', 'all')
 
-df = DielectricFunction('graphene.gpw')
+pbc = system.pbc
 
-alpha0x_qw = []
-alphax_qw = []
+df = DielectricFunction('graphene.gpw', eta=25e-3, domega0=0.01)
+alpha0x_w, alphax_w = df.get_polarizability(q_c=[1 / (nk * nkrefine), 0, 0])
+omega_w = df.get_frequencies()
+analyticalalpha_w = 1j / (8 * omega_w[1:] / Hartree)
 
-q_q = np.array(range(1, 2)) / (15 * 5)
+# Just some hardcoded test for alpha at omega=0
+equal(alphax_w[0].real, 6.7314, tolerance=1e-2,
+      msg='Polarizability at omega=0 is wrong')
 
-for q in q_q:
-    alpha0x_w, alphax_w = df.get_polarizability(q_c=[q, 0, 0])
-    alpha0x_qw.append(alpha0x_w)
-    alphax_qw.append(alphax_w)
+if 0:
+    from matplotlib import pyplot as plt
+    plt.plot(omega_w, alphax_w.real, label='GPAW real part')
+    plt.plot(omega_w, alphax_w.imag, '--', label='GPAW imag part')
+    plt.plot(omega_w[1:], analyticalalpha_w.imag,
+             '--', label='Analytical imag part')
+    plt.xlabel(r'$\hbar\omega$ (eV)')
+    plt.ylabel(r'Polarizability $\mathrm{\AA}$')
+    plt.legend()
+    plt.xlim(0, 1)
+    plt.ylim(0, 100)
+    plt.show()
 
-frequencies = df.get_frequencies()
-data = {'alpha0x_qw': np.array(alpha0x_qw),
-        'alphax_qw': np.array(alphax_qw),
-        'frequencies': frequencies}
-
-filename = 'graphene_pol_data.npz'
-
-if world.rank == 0:
-    np.savez_compressed(filename, **data)
