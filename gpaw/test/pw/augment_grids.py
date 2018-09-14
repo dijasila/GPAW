04ade125af1a043c73d213fe00bf6a762d1e75e4
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-09-14 11:00:51 +0200

    Expand test

--- gpaw/test/pw/augment_grids.py
+++ gpaw/test/pw/augment_grids.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ase import Atoms
 from gpaw import GPAW, PW, FermiDirac
+from gpaw.mpi import world
 
 ecut = 200
 kpoints = [1, 1, 4]
@@ -8,11 +9,14 @@ atoms = Atoms('HLi', cell=[6, 6, 3.4], pbc=True,
               positions=[[3, 3, 0], [3, 3, 1.6]])
 
 for xc in ['LDA', 'PBE']:
-    def calculate(aug):
+    def calculate(d, k):
         atoms.calc = GPAW(mode=PW(ecut),
                           xc=xc,
-                          txt='gpaw.{}.aug{}.txt'.format(xc, aug),
-                          parallel={'augment_grids': aug},
+                          # txt='gpaw.{}.aug{}.txt'.format(xc, aug),
+                          txt='gpaw.{xc}.domain{d}.kpt{k}txt'
+                          .format(xc=xc, d=d, k=k),
+                          # parallel={'augment_grids': aug},
+                          parallel={'domain': d, 'kpt': k},
                           kpts={'size': kpoints},
                           occupations=FermiDirac(width=0.1))
 
@@ -25,14 +29,19 @@ for xc in ['LDA', 'PBE']:
         s = atoms.get_stress()
         return e, f, s
 
-    e1, f1, s1 = calculate(False)
-    e2, f2, s2 = calculate(True)
-
-    eerr = abs(e2 - e1)
-    ferr = np.abs(f2 - f1).max()
-    serr = np.abs(s2 -s1).max()
-    if atoms.calc.wfs.world.rank == 0:
-        print('errs', eerr, ferr, serr)
-    assert eerr < 1e-12, 'bad {} energy: err={}'.format(xc, eerr)
-    assert ferr < 1e-12, 'bad {} forces: err={}'.format(xc, ferr)
-    assert serr < 1e-12, 'bad {} stress: err={}'.format(xc, serr)
+    for d in [1, 2, 4, 8]:
+        for k in [1, 2]:
+            if d * k > world.size:
+                continue
+            e2, f2, s2 = calculate(d, k)
+            if d + k == 2:
+                e1, f1, s1 = e2, f2, s2
+            else:
+                eerr = abs(e2 - e1)
+                ferr = np.abs(f2 - f1).max()
+                serr = np.abs(s2 - s1).max()
+                if world.rank == 0:
+                    print('errs', d, k, eerr, ferr, serr)
+                assert eerr < 1e-12, 'bad {} energy: err={}'.format(xc, eerr)
+                assert ferr < 1e-12, 'bad {} forces: err={}'.format(xc, ferr)
+                assert serr < 1e-12, 'bad {} stress: err={}'.format(xc, serr)
