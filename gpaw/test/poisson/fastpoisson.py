92a2dc93f2bb2992a4ac2c729b8ac29a457b928d
Ask Hjorth Larsen <asklarsen@gmail.com>
2018-09-18 19:50:00 +0200

    WIP: thorough test of new poisson solver for different cells, pbcs

---
+++ gpaw/test/poisson/fastpoisson.py
@@ -0,0 +1,80 @@
+import itertools
+import numpy as np
+from ase.build import bulk
+from gpaw.poisson import FastPoissonSolver
+from gpaw.grid_descriptor import GridDescriptor
+from gpaw.fd_operators import Laplace
+from gpaw.mpi import world
+from gpaw.utilities.gpts import get_number_of_grid_points
+
+# Test: different pbcs
+# For pbc=000, test charged system
+# Different cells (orthorhombic/general)
+# use_cholesky keyword
+
+
+cell_cv = np.array(bulk('Au').cell)
+rng = np.random.RandomState(42)
+
+tf = range(2)
+comm = world
+
+def icells():
+    # cells: orthorhombic fcc bcc hcp
+    yield np.diag([3., 4., 5.])
+
+
+    return  # XXXXXXX test the other cells, too
+
+    for sym in ['Au', 'Fe', 'Sc']:
+        cell = bulk(sym).cell.round(1)  # Round for nice printing
+        yield cell.copy()
+        cell += 2.0 * rng.rand(3, 3)
+        cell = cell.round(1)
+        yield cell
+
+
+#import matplotlib.pyplot as plt
+
+
+nn = 1
+
+for cellno, cell_cv in enumerate(icells()):
+    N_c = get_number_of_grid_points(cell_cv, 0.12, 'fd', True, None)
+    for pbc in itertools.product(tf, tf, tf):
+        gd = GridDescriptor(N_c, cell_cv, pbc_c=pbc)
+        rho_g = gd.zeros()
+        phi_g = gd.zeros()
+        rho_g[:] = -0.3 + rng.rand(*rho_g.shape)
+
+        # Neutralize charge:
+        charge = gd.integrate(rho_g)
+        magic = gd.get_size_of_global_array().prod()
+        rho_g -= charge / gd.dv / magic
+        charge = gd.integrate(rho_g)
+        assert abs(charge) < 1e-12
+
+        # Check use_cholesky=True/False ?
+        from gpaw.poisson import FDPoissonSolver
+        ps = FastPoissonSolver(nn=nn)
+        ps.set_grid_descriptor(gd)
+        ps.solve(phi_g, rho_g)
+
+        laplace = Laplace(gd, scale=-1.0 / (4.0 * np.pi), n=nn)
+
+        rhotest_g = gd.zeros()
+        laplace.apply(phi_g, rhotest_g)
+        maxerr = np.abs(rhotest_g - rho_g).max()
+        ps2 = FDPoissonSolver(relax='J', nn=nn, eps=1e-18)
+        ps2.set_grid_descriptor(gd)
+        phi2_g = gd.zeros()
+        ps2.solve(phi2_g, rho_g)
+
+        phimaxerr = np.abs(phi2_g - phi_g).max()
+        rhotest2_g = gd.zeros()
+        laplace.apply(phi2_g, rhotest2_g)
+        maxerr2 = np.abs(rho_g - rhotest2_g).max()
+
+        pbcstring = '{}{}{}'.format(*pbc)
+        print('{:2d} pbc={} err[fast]={:8.5e} err[J]={:8.5e} err[phi]={:8.5e}'
+              .format(cellno, pbcstring, maxerr, maxerr2, phimaxerr))
