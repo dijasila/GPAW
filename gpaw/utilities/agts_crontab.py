a1d151d4c3302d1897cdfa014cc3f957bc8a9562
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-09-24 10:27:55 +0200

    Set PYTHONPATH

--- gpaw/utilities/agts_crontab.py
+++ gpaw/utilities/agts_crontab.py
@@ -42,15 +42,20 @@ def agts(cmd):
         if tasks:
             raise ValueError('Not ready!')
 
-        shell('cd ase; git pull')
-        shell('cd gpaw; git clean -fdx; git pull;'
-              '. doc/platforms/Linux/Niflheim/compile.sh;'
-              'cd doc; make')
         arch = 'linux-x86_64-broadwell-el7-3.6'
         path = '/home/niflheim2/jensj/agts'
-        pp = ('{path}/ase:{path}/gpaw:{path}/gpaw/build/lib.{arch}'
+        pp = ('{path}/ase:{path}/gpaw:{path}/gpaw/build/lib.{arch}:$PYTHONPATH'
               .format(path=path, arch=arch))
-        shell('PYTHONPATH={pp}:$PYTHONPATH mq workflow -p agts.py gpaw -T'
+
+        shell('cd ase; git pull')
+        shell('cd gpaw;'
+              'git clean -fdx;'
+              'git pull;'
+              '. doc/platforms/Linux/Niflheim/compile.sh;'
+              'cd doc;'
+              'PYTHONPATH={pp} make'
+              .format(pp=pp))
+        shell('PYTHONPATH={pp} mq workflow -p agts.py gpaw -T'
               .format(pp=pp))
 
     elif cmd == 'summary':
