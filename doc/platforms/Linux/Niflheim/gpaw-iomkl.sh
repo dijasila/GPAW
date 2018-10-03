3f4cfb480b9b5de40d1d0f3598a907d39e97f81d
Mikkel Strange <mikst@fysik.dtu.dk>
2018-10-03 09:12:51 +0200

    iomkl bashrc setup

---
+++ doc/platforms/Linux/Niflheim/gpaw-iomkl.sh
@@ -0,0 +1,29 @@
+if [ -f /etc/bashrc ]; then
+    . /etc/bashrc
+fi
+
+if [ -z $GPAW ]; then
+    GPAW=~/gpaw
+fi
+if [ -z $ASE ]; then
+    ASE=~/ase
+fi
+
+module load Python/Python-3.6.4-iomkl-2018a
+module load libxc/4.2.3-iomkl-2018a
+module load matplotlib-2.1.2-iomkl-2018a-Python-3.6.4.eb
+PLATFORM=linux-x86_64-$CPU_ARCH-el7-3.6
+if [ $CPU_ARCH = broadwell ]; then
+    export GPAW_MPI_OPTIONS="-mca pml cm -mca mtl psm2"
+elif [ $CPU_ARCH = sandybridge ]; then
+    export GPAW_MPI_OPTIONS=""
+elif [ $CPU_ARCH = ivybridge ]; then
+    export GPAW_MPI_OPTIONS=""
+elif [ $CPU_ARCH = nehalem ]; then
+    export GPAW_MPI_OPTIONS=""
+fi
+
+export PATH=$GPAW/tools:$GPAW/build/bin.$PLATFORM:$PATH
+export PYTHONPATH=$GPAW:$GPAW/build/lib.$PLATFORM:$PYTHONPATH
+export PATH=$ASE/bin:$PATH
+export PYTHONPATH=$ASE:$PYTHONPATH
