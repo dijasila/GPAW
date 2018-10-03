d77a1a06b6cdc7e17f8c47f58e8fdbb94df5c474
Mikkel Strange <mikst@fysik.dtu.dk>
2018-10-03 09:14:25 +0200

    compile script

--- doc/platforms/Linux/Niflheim/compile.sh
+++ doc/platforms/Linux/Niflheim/compile.sh
@@ -2,8 +2,8 @@
 nh=doc/platforms/Linux/Niflheim
 rm -rf build
 cmd="cd $PWD && python setup.py --remove-default-flags build_ext"
-ssh sylg "$cmd --customize=$nh/el7.py > broadwell-el7.log 2>&1"
-ssh thul "$cmd --customize=$nh/el7.py > sandybridge-el7.log 2>&1"
-ssh fjorm "$cmd --customize=$nh/el7.py > nehalem-el7.log 2>&1"
+ssh sylg "$cmd --customize=$nh/el7-iomkl.py > broadwell-el7-iomkl.log 2>&1"
+ssh thul "$cmd --customize=$nh/el7-iomkl.py > sandybridge-el7-iomkl.log 2>&1"
+ssh fjorm "$cmd --customize=$nh/el7-iomkl.py > nehalem-el7-iomkl.log 2>&1"
 (cd build && ln -sf bin.linux-x86_64-{sandybridge,ivybridge}-el7-3.6)
 (cd build && ln -sf lib.linux-x86_64-{sandybridge,ivybridge}-el7-3.6)
