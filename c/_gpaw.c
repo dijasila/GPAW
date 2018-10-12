4d80886444ba7fd5806b4d6e0e79c740b7caf9c2
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-10-12 07:26:54 +0200

    Add pw_insert() C-func

--- c/_gpaw.c
+++ c/_gpaw.c
@@ -77,6 +77,7 @@ PyObject* exterior_electron_density_region(PyObject *self, PyObject *args);
 PyObject* plane_wave_grid(PyObject *self, PyObject *args);
 PyObject* tci_overlap(PyObject *self, PyObject *args);
 PyObject *pwlfc_expand(PyObject *self, PyObject *args);
+PyObject *pw_insert(PyObject *self, PyObject *args);
 PyObject* overlap(PyObject *self, PyObject *args);
 PyObject* vdw(PyObject *self, PyObject *args);
 PyObject* vdw2(PyObject *self, PyObject *args);
@@ -184,6 +185,7 @@ static PyMethodDef functions[] = {
     {"eed_region", exterior_electron_density_region, METH_VARARGS, 0},
     {"plane_wave_grid", plane_wave_grid, METH_VARARGS, 0},
     {"pwlfc_expand", pwlfc_expand, METH_VARARGS, 0},
+    {"pw_insert", pw_insert, METH_VARARGS, 0},
     {"erf", errorfunction, METH_VARARGS, 0},
     {"cerf", cerf, METH_VARARGS, 0},
     {"pack", pack, METH_VARARGS, 0},
--- c/plane_wave.c
+++ c/plane_wave.c
@@ -1,44 +1,48 @@
 #include "extensions.h"
 #include <stdlib.h>
+#include <numpy/arrayobject.h>
 
-PyObject *plane_wave_grid(PyObject *self, PyObject *args)
+
+void _pw_insert(int nG,
+                int nQ,
+                double complex* c_G,
+                npy_int32* Q_G,
+                double scale,
+                double complex* tmp_Q)
+// Does the same as these two lines of Python:
+//
+//     tmp_Q[:] = 0.0
+//     tmp_Q.ravel()[Q_G] = c_G * scale
 {
-  PyArrayObject* beg_c;
-  PyArrayObject* end_c;
-  PyArrayObject* h_c;
-  PyArrayObject* k_c;
-  PyArrayObject* r0_c;
-  PyArrayObject* pw_g;
-  if (!PyArg_ParseTuple(args, "OOOOOO", &beg_c, &end_c, &h_c, 
-			&k_c, &r0_c, &pw_g))
-    return NULL;
-
-  long *beg = LONGP(beg_c);
-  long *end = LONGP(end_c);
-  double *h = DOUBLEP(h_c);
-  double *vk = DOUBLEP(k_c);
-  double *vr0 = DOUBLEP(r0_c);
-  double_complex *pw = COMPLEXP(pw_g);
-
-  double kr[3], kr0[3];
-  int n[3], ij;
-  for (int c = 0; c < 3; c++) { 
-    n[c] = end[c] - beg[c];
-    kr0[c] = vk[c] * vr0[c];
-  }
-  for (int i = 0; i < n[0]; i++) {
-    kr[0] = vk[0] * h[0] * (beg[0] + i) - kr0[0];
-    for (int j = 0; j < n[1]; j++) {
-      kr[1] = kr[0] + vk[1] * h[1] * (beg[1] + j) - kr0[1];
-      ij = (i*n[1] + j)*n[2];
-      for (int k = 0; k < n[2]; k++) {
-	kr[2] = kr[1] + vk[2] * h[2] * (beg[2] + k) - kr0[2];
-	pw[ij + k] = cos(kr[2]) + I * sin(kr[2]);
-      }
-    }
-  }
-  Py_RETURN_NONE;
+    int Q1 = 0;
+    for (int G = 0; G < nG; G++) {
+        int Q2 = Q_G[G];
+        for (; Q1 < Q2; Q1++)
+            tmp_Q[Q1] = 0.0;
+        tmp_Q[Q1++] = c_G[G] * scale;
+        }
+    for (; Q1 < nQ; Q1++)
+        tmp_Q[Q1] = 0.0;
 }
+
+
+PyObject *pw_insert(PyObject *self, PyObject *args)
+// Python wrapper
+{
+    PyArrayObject *c_G_obj, *Q_G_obj, *tmp_Q_obj;
+    double scale;
+    if (!PyArg_ParseTuple(args, "OOdO",
+                          &c_G_obj, &Q_G_obj, &scale, &tmp_Q_obj))
+        return NULL;
+    double complex *c_G = PyArray_DATA(c_G_obj);
+    npy_int32 *Q_G = PyArray_DATA(Q_G_obj);
+    double complex *tmp_Q = PyArray_DATA(tmp_Q_obj);
+    int nG = PyArray_SIZE(c_G_obj);
+    int nQ = PyArray_SIZE(tmp_Q_obj);
+    _pw_insert(nG, nQ, c_G, Q_G, scale, tmp_Q);
+    Py_RETURN_NONE;
+}
+
 
 PyObject *pwlfc_expand(PyObject *self, PyObject *args)
 {
@@ -109,7 +113,8 @@ PyObject *pwlfc_expand(PyObject *self, PyObject *args)
                 double complex emiGR_f = emiGR_G[G] * f_G[G1 + G] * ilpow;
                 for(L=0; L < nL; L++) {
                     // Terrible memory locality!
-                    f_IG[(I1 + L) * nG + G] = emiGR_f * Y_LG[(L2 + L) * nGtotal + G1 + G];
+                    f_IG[(I1 + L) * nG + G] = (emiGR_f *
+                                               Y_LG[(L2 + L) * nGtotal + G1 + G];
                 }
             }
             I1 += nL;
@@ -119,3 +124,41 @@ PyObject *pwlfc_expand(PyObject *self, PyObject *args)
 }
 
 
+PyObject *plane_wave_grid(PyObject *self, PyObject *args)
+{
+  PyArrayObject* beg_c;
+  PyArrayObject* end_c;
+  PyArrayObject* h_c;
+  PyArrayObject* k_c;
+  PyArrayObject* r0_c;
+  PyArrayObject* pw_g;
+  if (!PyArg_ParseTuple(args, "OOOOOO", &beg_c, &end_c, &h_c,
+                        &k_c, &r0_c, &pw_g))
+    return NULL;
+
+  long *beg = LONGP(beg_c);
+  long *end = LONGP(end_c);
+  double *h = DOUBLEP(h_c);
+  double *vk = DOUBLEP(k_c);
+  double *vr0 = DOUBLEP(r0_c);
+  double_complex *pw = COMPLEXP(pw_g);
+
+  double kr[3], kr0[3];
+  int n[3], ij;
+  for (int c = 0; c < 3; c++) {
+    n[c] = end[c] - beg[c];
+    kr0[c] = vk[c] * vr0[c];
+  }
+  for (int i = 0; i < n[0]; i++) {
+    kr[0] = vk[0] * h[0] * (beg[0] + i) - kr0[0];
+    for (int j = 0; j < n[1]; j++) {
+      kr[1] = kr[0] + vk[1] * h[1] * (beg[1] + j) - kr0[1];
+      ij = (i*n[1] + j)*n[2];
+      for (int k = 0; k < n[2]; k++) {
+        kr[2] = kr[1] + vk[2] * h[2] * (beg[2] + k) - kr0[2];
+        pw[ij + k] = cos(kr[2]) + I * sin(kr[2]);
+      }
+    }
+  }
+  Py_RETURN_NONE;
+}
