131e879ee3188ec5f41c3a821882e240a599dd0c
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-10-26 09:01:17 +0200

    Do not write large pickle restart files

--- doc/tutorials/qeh/bb_MoS2.py
+++ doc/tutorials/qeh/bb_MoS2.py
@@ -7,9 +7,8 @@ df = DielectricFunction(calc='MoS2_gs_fulldiag.gpw',
                         omega2=10.0,
                         nblocks=8,
                         ecut=150,
-                        name='MoS2_response_',
                         truncation='2D')
 
-buildingblock = BuildingBlock('MoS2', df, qmax=3.)
+buildingblock = BuildingBlock('MoS2', df, qmax=3.0)
 
 buildingblock.calculate_building_block()
--- doc/tutorials/qeh/bb_WSe2.py
+++ doc/tutorials/qeh/bb_WSe2.py
@@ -7,9 +7,8 @@ df = DielectricFunction(calc='WSe2_gs_fulldiag.gpw',
                         omega2=10.0,
                         nblocks=8,
                         ecut=150,
-                        name='WSe2_response_',
                         truncation='2D')
 
-buildingblock = BuildingBlock('WSe2', df, qmax=3.)
+buildingblock = BuildingBlock('WSe2', df, qmax=3.0)
 
 buildingblock.calculate_building_block()
