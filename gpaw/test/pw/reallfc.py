Fix derivative()

gpaw/test/pw/reallfc.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import numpy as np
 
 from gpaw.test import equal
+from gpaw.fftw import FFTPlan
 from gpaw.grid_descriptor import GridDescriptor
 from gpaw.spline import Spline
 import gpaw.mpi as mpi
@@ -22,7 +23,6 @@ spos_ac = np.array([(0.15, 0.45, 0.95)])
 pd = PWDescriptor(45, gd, complex)
 pdr = PWDescriptor(45, gd)
 
-from gpaw.fftw import FFTPlan
 print(FFTPlan)
 
 for l in range(4):
@@ -31,7 +31,7 @@ for l in range(4):
 
     lfc = PWLFC([[s]], pd)
     lfcr = PWLFC([[s]], pdr)
-    
+
     c_axi = {0: np.zeros((1, 2 * l + 1), complex)}
     c_axi[0][0, 0] = 1.9
     cr_axi = {0: np.zeros((1, 2 * l + 1))}
@@ -47,7 +47,7 @@ for l in range(4):
 
     a = pd.ifft(b)
     ar = pdr.ifft(br)
-    equal(abs(a-ar).max(), 0, 1e-14)
+    equal(abs(a - ar).max(), 0, 1e-14)
 
     if l == 0:
         a = a[:, ::-1].copy()
@@ -56,10 +56,10 @@ for l in range(4):
 
     lfc.integrate(b0, c_axi)
     lfcr.integrate(br0, cr_axi)
-    assert abs(c_axi[0][0]-cr_axi[0][0]).max() < 1e-14
+    assert abs(c_axi[0][0] - cr_axi[0][0]).max() < 1e-14
 
     c_axiv = {0: np.zeros((1, 2 * l + 1, 3), complex)}
     cr_axiv = {0: np.zeros((1, 2 * l + 1, 3))}
     lfc.derivative(b0, c_axiv)
     lfcr.derivative(br0, cr_axiv)
-    assert abs(c_axiv[0][0]-cr_axiv[0][0]).max() < 1e-14
+    assert abs(c_axiv[0][0] - cr_axiv[0][0]).max() < 1e-14

gpaw/wavefunctions/pw.py
@@ -1580,7 +1580,7 @@ class PWLFC(BaseLFC):
 
         x = 0.0
         for G1, G2 in self.block(q):
-            f_IG = self.expand(q, G1, G2)
+            f_GI = self.expand(q, G1, G2)
             G_Gv = self.pd.G_Qv[self.pd.myQ_qG[q][G1:G2]]
             if self.pd.dtype == float:
                 for v in range(3):
@@ -1590,10 +1590,10 @@ class PWLFC(BaseLFC):
                          x, b_vxI[v], 'c')
             else:
                 for v in range(3):
-                    gemm(-alpha,
-                         f_IG * (G_Gv[:, v] + K_v[v]),
-                         a_xG[:, G1:G2],
-                         x, b_vxI[v], 'c')
+                    mmm(-alpha,
+                        a_xG[:, G1:G2], 'N',
+                        f_GI * (G_Gv[:, v] + K_v[v])[:, np.newaxis], 'N',
+                        x, b_vxI[v])
             x = 1.0
 
         self.comm.sum(c_vxI)