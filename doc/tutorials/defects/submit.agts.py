1b5c8883bb655e75d811a923c73ed9c2fedad2d6
Sten Haastrup <sthaa@mott.fysik.dtu.dk>
2018-11-19 14:14:54 +0100

    Update submit agts to new style

--- doc/tutorials/defects/submit.agts.py
+++ doc/tutorials/defects/submit.agts.py
@@ -1,8 +1,16 @@
-def agts(queue):
-    queue.add('gaas.py 1', ncpus=8, walltime=1)
-    queue.add('gaas.py 2', ncpus=16, walltime=2)
-    queue.add('gaas.py 3', ncpus=24, walltime=3)
-    gaas = queue.add('gaas.py 4', ncpus=24, walltime=6)
-    elc = queue.add('electrostatics.py', deps=gaas)
-    queue.add('plot_potentials.py', deps=elc, creates='planaraverages.png')
-    queue.add('plot_energies.py', deps=elc, creates='energies.png')
+# Creates: planaraverages.png,energies.png
+from myqueue.task import task
+
+
+def create_tasks():
+    queue = [task('gaas.py+1@8:1h'),
+             task('gaas.py+2@8:1h'),
+             task('gaas.py+31@24:2h'),
+             task('gaas.py+4@24:4h'),
+             task('electrostatics.py@1:15m', deps=['gaas.py+1',
+                                                   'gaas.py+2',
+                                                   'gaas.py+3',
+                                                   'gaas.py+4']),
+             task('plot_potentials.py', deps='electrostatics'),
+             task('plot_energies.py', deps='electrostatics')]
+    return queue
