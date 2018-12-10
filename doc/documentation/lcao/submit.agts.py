b999ac23359f7378b7b36e05b44fa452c34ca148
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-12-10 09:13:47 +0100

    Disable ELPA for now

--- doc/documentation/lcao/submit.agts.py
+++ doc/documentation/lcao/submit.agts.py
@@ -4,5 +4,5 @@ from myqueue.task import task
 def create_tasks():
     return [
         task('basisgeneration.py@1:10m'),
-        task('lcao_h2o.py@1:10m'),
-        task('lcao_opt.py@8:10m')]
+        # task('lcao_opt.py@8:10m'),  # needs ELPA
+        task('lcao_h2o.py@1:10m')]
