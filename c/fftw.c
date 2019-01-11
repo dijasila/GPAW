ba19fefe32f59fe4bcc5d001851d9edc72dd6479
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-01-11 15:18:46 +0000

    update old fftw c interface.  Numpy has changed a lot apparently

--- c/fftw.c
+++ c/fftw.c
@@ -5,7 +5,6 @@
 #include <numpy/arrayobject.h>
 #include <fftw3.h>
 
-
 /* Create plan and return pointer to plan as a string */
 PyObject * FFTWPlan(PyObject *self, PyObject *args)
 {
@@ -18,24 +17,38 @@ PyObject * FFTWPlan(PyObject *self, PyObject *args)
         return NULL;
 
     fftw_plan* plan = (fftw_plan*)malloc(sizeof(fftw_plan));
-    
-    if (in->descr->type_num == PyArray_DOUBLE)
-      *plan = fftw_plan_dft_r2c(in->nd, in->dimensions,
-				(double*)in->data,
-				(double (*)[2])out->data,
-				flags);
-    else if (out->descr->type_num == PyArray_DOUBLE)
-        *plan = fftw_plan_dft_c2r(in->nd, out->dimensions,
-				  (double (*)[2])in->data,
-				  (double*)out->data,
-				  flags);
-    else
-        *plan = fftw_plan_dft(in->nd, out->dimensions,
-			      (double (*)[2])in->data,
-			      (double (*)[2])out->data,
-			      sign, flags);
-    
-    return Py_BuildValue("s#", plan, sizeof(fftw_plan*));
+
+    int ndim = PyArray_NDIM(in);
+    int dims_in[ndim];
+    int dims_out[ndim];
+    int i;
+
+    void *indata = PyArray_DATA(in);
+    void *outdata = PyArray_DATA(out);
+
+    for(i=0; i < ndim; i++) {
+        dims_in[i] = (int)PyArray_DIMS(in)[i];
+        dims_out[i] = (int)PyArray_DIMS(out)[i];
+    }
+
+    if (PyArray_DESCR(in)->type_num == NPY_DOUBLE) {
+        *plan = fftw_plan_dft_r2c(ndim, dims_in,
+                                  (double *)indata,
+                                  (fftw_complex *)outdata,
+                                  flags);
+    } else if (PyArray_DESCR(out)->type_num == NPY_DOUBLE) {
+        *plan = fftw_plan_dft_c2r(ndim, dims_out,
+                                  (fftw_complex *)indata,
+                                  (double *)outdata,
+                                  flags);
+    } else {
+        *plan = fftw_plan_dft(ndim, dims_out,
+                              (fftw_complex *)indata,
+                              (fftw_complex *)outdata,
+                              sign, flags);
+    }
+
+    return Py_BuildValue("y#", plan, sizeof(fftw_plan*));
 }
 
 
@@ -43,7 +56,7 @@ PyObject * FFTWExecute(PyObject *self, PyObject *args)
 {
     fftw_plan* plan;
     int n;
-    if (!PyArg_ParseTuple(args, "s#", &plan, &n))
+    if (!PyArg_ParseTuple(args, "y#", &plan, &n))
         return NULL;
     fftw_execute(*plan);
     Py_RETURN_NONE;
@@ -54,10 +67,10 @@ PyObject * FFTWDestroy(PyObject *self, PyObject *args)
 {
     fftw_plan* plan;
     int n;
-    if (!PyArg_ParseTuple(args, "s#", &plan, &n))
+    if (!PyArg_ParseTuple(args, "y#", &plan, &n))
         return NULL;
     fftw_destroy_plan(*plan);
-    free(plan);
+    //free(plan);
     Py_RETURN_NONE;
 }
 
