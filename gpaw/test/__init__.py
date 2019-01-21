5fd70e1d46031cc961ba30e341df5bbb231ac6e8
Thorbjørn Skovhus <s134264@student.dtu.dk>
2019-01-21 10:11:47 +0100

    BUG: missing comma

--- gpaw/test/__init__.py
+++ gpaw/test/__init__.py
@@ -197,7 +197,7 @@ tests = [
     'solvation/vacuum.py',                  # ~3s
     'vdw/libvdwxc_mbeef.py',                # ~3s
     'response/graphene_refined_response.py',  # ~3s
-    'response/pdens_tool.py'                # ~4s
+    'response/pdens_tool.py',               # ~4s
     'pw/par_strategies.py',                  # ~4s
     'pseudopotential/sg15_hydrogen.py',     # ~4s
     'generic/move_across_cell.py',          # ~4s
