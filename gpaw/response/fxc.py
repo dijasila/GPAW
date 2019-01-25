BUG: corrected index bug in vectorization of spherical bessel function calculation and updated test values

gpaw/response/fxc.py
@@ -386,16 +386,14 @@ class AdiabaticKernelCalculator:
                 
                 # Expand plane wave
                 nM = len(L_M)
-                Y_MGG = np.zeros((nM, *dG_GG.shape))
                 (r_gMGG, l_gMGG,
-                 dG_gMGG) = [a.reshape(ng, nM,
-                                       *dG_GG.shape)
-                             for a in np.meshgrid(r_g, l_M,
-                                                  dG_GG)]
-                j_gMGG = spherical_jn(l_gMGG, dG_gMGG * r_gMGG)
+                 dG_gMGG) = [a.reshape(ng, nM, *dG_GG.shape)
+                             for a in np.meshgrid(r_g, l_M, dG_GG.flatten(),
+                                                  indexing='ij')]
+                j_gMGG = spherical_jn(l_gMGG, dG_gMGG * r_gMGG)  # Slow step
                 Y_MGG = Yarr(L_M, dGn_GGv)
-                ii_MK = (-1.j) ** np.repeat(l_M,
-                                            np.prod(dG_GG.shape))
+                ii_MK = (-1j) ** np.repeat(l_M,
+                                           np.prod(dG_GG.shape))
                 ii_MGG = ii_MK.reshape((nM, *dG_GG.shape))
                 
                 # Perform integration
@@ -411,7 +409,7 @@ class AdiabaticKernelCalculator:
             self.world.sum(KxcPAW_GG)
             Kxc_GG += KxcPAW_GG
 
-        if self.RSrep == 'GPAW':  # XXX old stuff, to be deleted
+        if self.RSrep == 'GPAW':  # XXX old stuff, to be removed
             print("\tCalculating PAW corrections to the kernel", file=self.fd)
             
             G_Gv = pd.get_reciprocal_vectors()
@@ -565,7 +563,7 @@ class AdiabaticKernelCalculator:
 
         self.rsheconvmin = np.min(rsheconv_g)
         
-    def distribute_correction(self, setups, size):  # XXX old stuff, tbd
+    def distribute_correction(self, setups, size):  # XXX old stuff, tbr
         """ Make every process work an equal amount """
         # Figure out the total number of grid points
         tp = 0

gpaw/test/response/iron_sf_ALDA.py
@@ -33,6 +33,7 @@ frq_w = np.linspace(0.150, 0.350, 26)
 Kxc = 'ALDA'
 ecut = 300
 eta = 0.01
+rshecc_c = [0.99, 0.999]  # test different levels of expansion
 
 # ------------------- Script ------------------- #
 
@@ -61,10 +62,12 @@ tms = TransverseMagneticSusceptibility(calc='Fe',
                                        eta=eta,
                                        ecut=ecut)
 
-chiM0_w, chiM_w = tms.get_dynamic_susceptibility(q_c=q_c, xc=Kxc,
-                                                 RSrep='gpaw-rshe0.999',
-                                                 filename='iron_dsus'
-                                                 + '_G.csv')
+for c, rshecc in enumerate(rshecc_c):
+    RSrep = 'gpaw-rshe%.5f' % rshecc
+    chiM0_w, chiM_w = tms.get_dynamic_susceptibility(q_c=q_c, xc=Kxc,
+                                                     RSrep=RSrep,
+                                                     filename='iron_dsus'
+                                                     + '_G%d.csv' % (c + 1))
 
 t3 = time.time()
 
@@ -73,19 +76,26 @@ parprint('Excited state calculation took', (t3 - t2) / 60, 'minutes')
 
 world.barrier()
 
-# Part 3: identify magnon peak in scattering function
-d = np.loadtxt('iron_dsus_G.csv', delimiter=', ')
+# Part 3: identify magnon peak in scattering functions
+d1 = np.loadtxt('iron_dsus_G1.csv', delimiter=', ')
+d2 = np.loadtxt('iron_dsus_G2.csv', delimiter=', ')
 
-wpeak, Ipeak = findpeak(d[:, 0], - d[:, 4])
+wpeak1, Ipeak1 = findpeak(d1[:, 0], - d1[:, 4])
+wpeak2, Ipeak2 = findpeak(d2[:, 0], - d2[:, 4])
 
-mw = (wpeak + d[0, 0]) * 1000
+mw1 = (wpeak1 + d1[0, 0]) * 1000
+mw2 = (wpeak2 + d2[0, 0]) * 1000
 
 # Part 4: compare new results to test values
-test_mw = 248.822795658  # meV
-test_Ipeak = 66.5595189306  # a.u.
+test_mw1 = 247.45900031014196  # meV
+test_Ipeak1 = 69.03459765802732  # a.u.
+test_mw2 = 249.07170917738293  # meV
+test_Ipeak2 = 66.2462879760728  # a.u.
 
 # Magnon peak:
-equal(test_mw, mw, eta * 100)
+equal(test_mw1, mw1, eta * 100)
+equal(test_mw2, mw2, eta * 100)
 
 # Scattering function intensity:
-equal(test_Ipeak, Ipeak, 1.5)
+equal(test_Ipeak1, Ipeak1, 1.5)
+equal(test_Ipeak2, Ipeak2, 1.5)