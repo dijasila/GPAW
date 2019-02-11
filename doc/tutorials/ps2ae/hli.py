f8564eaed7e08c4023ea3f088e6359d905f0c9e0
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-02-11 07:36:08 +0100

    Figure out why it doesn't work ...

--- doc/tutorials/ps2ae/hli.py
+++ doc/tutorials/ps2ae/hli.py
@@ -1,5 +1,10 @@
 # creates: hli-wfs.png, hli-pot.png
 import sys
 sys.path.insert(0, '.')
-import hli_wfs  # noqa
-import hli_pot  # noqa
+try:
+    import hli_wfs  # noqa
+    import hli_pot  # noqa
+except ImportError:
+    from pathlib import Path
+    print(Path.cwd(), sys.path)
+    raise
