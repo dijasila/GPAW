e3a1b10187d1040e51203d6eebe86ddc274e7d65
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-02-25 07:33:23 +0100

    Use empty bound states last when calculating initial occ numbers + better test

--- gpaw/test/initial_occs.py
+++ gpaw/test/initial_occs.py
@@ -1,19 +1,16 @@
 from gpaw.setup import create_setup
 
-s = create_setup('Pd')
-f_si = s.calculate_initial_occupation_numbers(magmom=1,
-                                              hund=False,
-                                              charge=0,
-                                              nspins=2)
-print(f_si)
-magmom = (f_si[0] - f_si[1]).sum()
-assert abs(magmom - 1.0) < 1e-10, f_si
-
-s = create_setup('Fe')
-f_si = s.calculate_initial_occupation_numbers(magmom=3,
-                                              hund=False,
-                                              charge=0,
-                                              nspins=2)
-print(f_si)
-magmom = (f_si[0] - f_si[1]).sum()
-assert abs(magmom - 3.0) < 1e-10, f_si
+for symbol, M, n in [('Pd', 1, 6),  # 4d -> 5s
+                     ('Fe', 3, 5),
+                     ('V', 3, 5),
+                     ('Ti', 2, 5)]:
+    s = create_setup(symbol)
+    f_si = s.calculate_initial_occupation_numbers(magmom=M,
+                                                  hund=False,
+                                                  charge=0,
+                                                  nspins=2)
+    print(f_si)
+    magmom = (f_si[0] - f_si[1]).sum()
+    assert abs(magmom - M) < 1e-10
+    N = ((f_si[0] - f_si[1]) != 0).sum()
+    assert n == N, 'Wrong # of values have changed'
