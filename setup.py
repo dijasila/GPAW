cbd90a3646b61b9a61fdb012068e858d014db07e
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-03-18 16:36:20 +0100

    more more Python 2.7.  Closes: #204

--- setup.py
+++ setup.py
@@ -258,8 +258,6 @@ setup(name='gpaw',
           'License :: OSI Approved :: '
           'GNU General Public License v3 or later (GPLv3+)',
           'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
