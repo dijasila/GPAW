948e6178131bbe37c608a3d84230d882566eb4a5
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-03-25 21:13:25 +0100

    It runs

--- gpaw/xc/hf.py
+++ gpaw/xc/hf.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 import numpy as np
 
 from gpaw.kpt_descriptor import KPointDescriptor
+from gpaw.wavefunctions.pw import PWDescriptor, PWLFC
 
 
 def create_symmetry_map(kd: KPointDescriptor):  # -> List[List[int]]
@@ -62,15 +63,17 @@ class BlockOfStates:
     def __init__(self, pd, kpt):
         self.pd = pd
         self.projections = kpt.projections
-        self.psit_nR = [pd.ifft(psit_G)
-                        for psit_G in kpt.psit.array]
+        self.u_nR = [pd.ifft(psit_G)
+                     for psit_G in kpt.psit.array]
+
+    def __len__(self):
+        return len(self.u_nR)
 
 
 def exx(b1: BlockOfStates,
         b2: BlockOfStates,
         Delta_aiiL,
         ghat,
-        pd,
         v_G):  # -> float
     Q_anni = [np.einsum('mi,ijL,nj->mnL',
                         b1.projections[a].conj(),
@@ -83,21 +86,16 @@ def exx(b1: BlockOfStates,
         n0 = n1 if b1 is b2 else 0
         u2_nR = b2.u_nR[n0:]
         rho_nR = u1_R.conj() * u2_nR
-        ghat.add({a: Q_nni[n1, n0:]
-                  for a, Q_nni in enumerate(Q_anni)}, rho_nR)
+        ghat.add(rho_nR,
+                 {a: Q_nni[n1, n0:]
+                  for a, Q_nni in enumerate(Q_anni)})
         for n2, rho_R in enumerate(rho_nR, n0):
-            rho_G = pd.fft(rho_R)
-            e = pd.integrate(rho_G, v_G * rho_G)
+            rho_G = ghat.pd.fft(rho_R)
+            e = ghat.pd.integrate(rho_G, v_G * rho_G).real
             exx_nn[n1, n2] = e
             if b1 is b2:
                 exx_nn[n2, n1] = e
     return exx_nn
-
-
-def run(calc):
-    print('Using Wigner-Seitz truncated Coulomb interaction.')
-    wstc = WignerSeitzTruncatedCoulomb(calc.wfs.gd.cell_cv,
-                                       calc.wfs.kd.N_c)
 
 
 def hf(calc, coulomb):
@@ -105,6 +103,7 @@ def hf(calc, coulomb):
     kd = wfs.kd
     pairs = find_kpt_pairs(kd)
     kpts = calc.wfs.mykpts
+    Delta_aiiL = [pawdata.Delta_iiL for pawdata in wfs.setups]
     i0 = -1
     for i1, i2, s, weight in sorted(pairs):
         kpt1 = kpts[i1]
@@ -120,14 +119,16 @@ def hf(calc, coulomb):
             b2 = b2.apply_symmetry(s)
 
         k1_c = kd.ibzk_kc[i1]
-        k2 = kd.ibz2_bz_k[i2]
+        k2 = kd.ibz2bz_k[i2]
         k2 = kd.bz2bz_ks[k2, s]
         k2_c = kd.bzk_kc[k2]
         q_c = k1_c - k2_c
         qd = KPointDescriptor([q_c])
         pd = PWDescriptor(wfs.ecut, wfs.gd, wfs.dtype, kd=qd)
         ghat = PWLFC([pawdata.ghat_l for pawdata in wfs.setups], pd)
-        exx(b1, b2)
+        ghat.set_positions(calc.spos_ac)
+        v_G = coulomb.get_potential(pd)
+        exx(b1, b2, Delta_aiiL, ghat, v_G)
 
 
 def all2all():
@@ -135,6 +136,7 @@ def all2all():
 
 
 def run(c1, c2):
+    import threading
     thread = None
     while True:
         array1 = c1.next()
@@ -156,6 +158,11 @@ if __name__ == '__main__':
                   kpts=(4, 4, 1),
                   txt=None)
     h.get_potential_energy()
+    # print('Using Wigner-Seitz truncated Coulomb interaction.')
+    from gpaw.response.wstc import WignerSeitzTruncatedCoulomb
+    wstc = WignerSeitzTruncatedCoulomb(h.calc.wfs.gd.cell_cv,
+                                       h.calc.wfs.kd.N_c)
+    hf(h.calc, wstc)
     kd = h.calc.wfs.kd
     print(kd.ibz2bz_k)
     print(kd.bz2ibz_k)
