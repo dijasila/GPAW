7ae66d6c93221e1ec8d27942dbd85174f7312c55
Asbjorn Rasmussen <asbra@fysik.dtu.dk>
2019-04-16 09:06:51 +0200

    Started weighting implemenetation

--- gpaw/xc/wlda.py
+++ gpaw/xc/wlda.py
@@ -8,11 +8,13 @@ class WLDA(XCFunctional):
         if kernel is None:
             kernel = PurePythonLDAKernel()
         self.kernel = kernel
-
+        self.weight = self._theta_filter
 
 
     def calculate_impl(self, gd, n_sg, v_sg, e_g):
         #Fermi wavelength is given by k_f^3 = 3*pi^2 n
+        self.apply_weighting(gd, n_sg)
+
 
         from gpaw.xc.lda import lda_c
         C0I, C1, CC1, CC2, IF2 = lda_constants()
@@ -40,6 +42,49 @@ class WLDA(XCFunctional):
             zeta = 0
             #lda_x(0, e_g, n, v_sg[0])
             lda_c(0, e_g, n, v_sg[0], zeta)
+
+    def apply_weighting(self, gd, n_sg):
+        if n_sg.shape[0] > 1:
+
+            raise NotImplementedError
+        K_G = self._get_K_G(gd, n_sg)
+
+        fn_G = np.fft.fftn(n_sg[0])
+
+
+        #assert (Nx, Ny, Nz) == fn_G.shape This is not true
+        k_F = K_G[5,5,5]
+        
+        self.weight(k_F, n_sg, K_G, fn_G)
+        
+        q = np.fft.ifftn(fn_G)
+        b = np.allclose(q, q.real)
+        #if not b:
+        #    print(q.imag)
+        #assert b
+        n_sg[0,:] = q.real
+
+
+
+
+    def _get_K_G(self, gd, n_sg):
+        dKx, dKy, dKz = 2*np.pi/np.diag(gd.cell_cv)
+        dx, dy, dz = gd.coords(0)[1] - gd.coords(0)[0], gd.coords(1)[1] - gd.coords(1)[0], gd.coords(2)[1] - gd.coords(2)[0]
+        Nx, Ny, Nz = n_sg[0].shape #len(gd.coords(0)), len(gd.coords(1)), len(gd.coords(2)) doesnt work, not same shape as density
+        ##But then dx,dy,dz is inconsistent with Nx,Ny,Nz
+        
+        reciprocal_vecs = gd.icell_cv.T #get dKs from this
+        
+        K_G = np.indices((Nx, Ny, Nz)).T.dot(2*np.pi/np.array([dx*Nx, dy*Ny, dz*Nz])).T
+
+        return K_G
+
+    def _theta_filter(self, k_F, K_G, n_G):
+        
+        Theta_G = (K_G**2 < 2*k_F**2).astype(np.complex128)
+
+        return n_G*Theta_G
+
 
     def calculate_paw_correction(self, setup, D_sp, dEdD_sp=None, a=None):
         from gpaw.xc.lda import calculate_paw_correction
