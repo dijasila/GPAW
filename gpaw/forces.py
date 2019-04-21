bd1459e574c36f656d58b397d53fc23610f9461c
alxvov <alxvov@gmail.com>
2019-04-21 20:58:18 +0000

    add forces

--- gpaw/forces.py
+++ gpaw/forces.py
@@ -3,6 +3,8 @@ import numpy as np
 from ase.units import Hartree, Bohr
 
 from gpaw.xc.hybrid import HybridXCBase
+from gpaw.utilities import unpack
+from gpaw.directmin.tools import get_n_occ
 
 
 def calculate_forces(wfs, dens, ham, log=None):
@@ -10,6 +12,11 @@ def calculate_forces(wfs, dens, ham, log=None):
 
     assert not isinstance(ham.xc, HybridXCBase)
     assert not ham.xc.name.startswith('GLLB')
+
+    egs_name = getattr(wfs.eigensolver, "name", None)
+    if egs_name == 'direct_min':
+        return calculate_forces_using_non_diag_lagr_matrix(
+            wfs, dens, ham, log)
 
     natoms = len(wfs.setups)
 
@@ -44,3 +51,74 @@ def calculate_forces(wfs, dens, ham, log=None):
         log()
 
     return F_av
+
+
+def calculate_forces_using_non_diag_lagr_matrix(wfs, dens, ham, log=None):
+
+    natoms = len(wfs.setups)
+    F_wfs_av = np.zeros((natoms, 3))
+    esolv = wfs.eigensolver
+
+    for kpt in wfs.kpt_u:
+        wfs.pt.integrate(kpt.psit_nG, kpt.P_ani, kpt.q)
+
+    grad_knG = esolv.get_gradients_2(ham, wfs)
+
+    if esolv.odd_parameters['name'] == 'PZ_SIC':
+        for kpt in wfs.kpt_u:
+            esolv.odd.get_energy_and_gradients_kpt(wfs, kpt, grad_knG)
+
+    for kpt in wfs.kpt_u:
+        k = esolv.n_kps * kpt.s + kpt.q
+        n_occ = get_n_occ(kpt)
+
+        lamb = wfs.gd.integrate(kpt.psit_nG[:n_occ],
+            grad_knG[k][:n_occ], False)
+        lamb = np.ascontiguousarray(lamb)
+        wfs.gd.comm.sum(lamb)
+
+        P_ani = kpt.P_ani
+        dP_aniv = wfs.pt.dict(n_occ, derivative=True)
+        wfs.pt.derivative(kpt.psit_nG[:n_occ], dP_aniv, kpt.q)
+        dH_asp = ham.dH_asp
+
+        for a, dP_niv in dP_aniv.items():
+            dP_niv = dP_niv.conj()
+            dO_ii = wfs.setups[a].dO_ii
+            P_ni = P_ani[a][:n_occ]
+            dS_nkv = np.einsum('niv,ij,kj->nkv', dP_niv, dO_ii, P_ni)
+            dS_nkv = \
+                (dS_nkv + np.transpose(dS_nkv, (1, 0, 2)).conj())
+            # '-' becuase there is extra minus in 'pt derivative'
+            F_wfs_av[a] -= np.einsum('kn,nkv->v', lamb, dS_nkv).real
+            dH_ii = unpack(dH_asp[a][kpt.s])
+            dh_nv = np.einsum('niv,ij,nj->nv', dP_niv, dH_ii, P_ni)
+            # '+' becuase there is extra minus in 'pt derivative'
+            F_wfs_av[a] += np.einsum('n,nv->v', kpt.f_n[:n_occ],
+                                     2.0 * dh_nv.real)
+
+        if esolv.odd_parameters['name'] == 'PZ_SIC':
+            esolv.odd.get_odd_corrections_to_forces(F_wfs_av,
+                                                    wfs,
+                                                    kpt)
+
+    wfs.kd.comm.sum(F_wfs_av, 0)
+    wfs.gd.comm.sum(F_wfs_av, 0)
+
+    F_ham_av = np.zeros((natoms, 3))
+    ham.calculate_forces(dens, F_ham_av)
+    F_av = F_ham_av + F_wfs_av
+    wfs.world.broadcast(F_av, 0)
+
+    F_av = wfs.kd.symmetry.symmetrize_forces(F_av)
+
+    if log:
+        log('\nForces in eV/Ang:')
+        c = Hartree / Bohr
+        for a, setup in enumerate(wfs.setups):
+            log('%3d %-2s %10.5f %10.5f %10.5f' %
+                ((a, setup.symbol) + tuple(F_av[a] * c)))
+        log()
+
+    return F_av
+
