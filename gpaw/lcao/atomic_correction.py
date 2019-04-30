1d3aa898681bf9f52d1c90c8dd9d2f2044beecfe
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-04-30 17:44:52 +0200

    block-sparse implementation is not as efficient as true sparse.  Remove

--- gpaw/lcao/atomic_correction.py
+++ gpaw/lcao/atomic_correction.py
@@ -101,19 +101,18 @@ class DenseAtomicCorrection(BaseAtomicCorrection):
         self.nops = nops
 
 
-class DistributedAtomicCorrection(BaseAtomicCorrection):
-    name = 'distributed'
-    description = 'distributed and block-sparse'
+class SparseAtomicCorrection(BaseAtomicCorrection):
+    name = 'sparse'
+    description = 'sparse using scipy'
 
-    def gobble_data(self, wfs):
-        self.orig_partition = wfs.atom_partition
-        evenpart = EvenPartitioning(self.orig_partition.comm,
-                                    self.orig_partition.natoms)
-        self.even_partition = evenpart.as_atom_partition()
+    def __init__(self, tolerance=1e-12):
+        BaseAtomicCorrection.__init__(self)
+        from scipy import sparse
+        self.sparse = sparse
+        self.tolerance = tolerance
 
     def get_a_values(self):
         return self.orig_partition.my_indices  # XXXXXXXXXX
-        # return self.even_partition.my_indices
 
     def redistribute(self, wfs, dX_asp, type='asp', op='forth'):
         if type not in ['asp', 'aii']:
@@ -140,125 +139,11 @@ class DistributedAtomicCorrection(BaseAtomicCorrection):
             dX_asp.redistribute(self.orig_partition)
         return dX_asp
 
-    def calculate(self, wfs, q, dX_aii, X_MM):
-        # XXX reduce according to kpt.q
-        dtype = wfs.dtype
-        M_a = wfs.setups.M_a
-        nM_a = np.array([setup.nao for setup in wfs.setups])
-        Mstart = wfs.ksl.Mstart
-        Mstop = wfs.ksl.Mstop
-
-        # Now calculate basis-projector-basis overlap: a1 -> a3 -> a2
-        #
-        # specifically:
-        #   < phi[a1] | p[a3] > * dX[a3] * < p[a3] | phi[a2] >
-        #
-        # This matrix multiplication is semi-sparse.  It works by blocks
-        # of atoms, looping only over pairs that do have nonzero
-        # overlaps.  But it might be even nicer with scipy sparse.
-        # This we will have to check at some point.
-        #
-        # The projection arrays P_aaim are distributed over the grid,
-        # whereas the X_MM is distributed over the band comm.
-        # One could choose a set of a3 to optimize the load balance.
-        # Right now the load balance will be "random" and probably
-        # not very good.
-        # innerloops = 0
-
-        setups = wfs.setups
-
-        outer = 0
-        inner = 0
-        nops = 0
-
-        nao_a = [setup.nao for setup in setups]
-
-        for (a3, a1), P1_qim in wfs.P_aaqim.items():
-            P1_im = P1_qim[q]
-            a1M1 = M_a[a1]
-            nM1 = nM_a[a1]
-            a1M2 = a1M1 + nM1
-
-            if a1M1 > Mstop or a1M2 < Mstart:
-                continue
-
-            stickout1 = max(0, Mstart - a1M1)
-            stickout2 = max(0, a1M2 - Mstop)
-            P1_mi = np.conj(P1_im.T[stickout1:nM1 - stickout2])
-            dX_ii = dX_aii[a3]
-            X_mM = X_MM[a1M1 + stickout1 - Mstart:a1M2 - stickout2 - Mstart]
-
-            P1dX_mi = np.dot(P1_mi, dX_ii)
-            nops += P1dX_mi.size * dX_ii.shape[0]
-            outer += 1
-
-            assert len(wfs.P_neighbors_a[a3]) > 0
-            if 0:
-                for a2 in wfs.P_neighbors_a[a3]:
-                    inner += 1
-                    # We can use symmetry somehow.  Since the entire matrix
-                    # is symmetrized after the X_MM is constructed,
-                    # at least in the non-Gamma-point case, we should do
-                    # so conditionally somehow.  Right now let's stay out
-                    # of trouble.
-
-                    # Humm.  The following works with gamma point
-                    # but not with kpts.  XXX take a look at this.
-                    # also, it doesn't work with a2 < a1 for some reason.
-                    # if a2 > a1:
-                    #     continue
-                    a2M1 = M_a[a2]
-                    a2M2 = a2M1 + wfs.setups[a2].nao
-                    P2_im = wfs.P_aaqim[(a3, a2)][q]
-                    P1dXP2_mm = np.dot(P1dX_mi, P2_im)
-                    X_mM[:, a2M1:a2M2] += P1dXP2_mm
-                    # innerloops += 1
-                    # if wfs.world.rank == 0:
-                    #     print 'y', y
-                    # if a1 != a2:
-                    #     X_MM[a2M1:a2M2, a1M1:a1M2] += P1dXP2_mm.T.conj()
-
-            a2_a = wfs.P_neighbors_a[a3]
-
-            a2nao = sum(nao_a[a2] for a2 in a2_a)
-
-            P2_iam = np.empty((P1_mi.shape[1], a2nao), dtype)
-            m2 = 0
-            for a2 in a2_a:
-                P2_im = wfs.P_aaqim[(a3, a2)][q]
-                nao = nao_a[a2]
-                P2_iam[:, m2:m2 + nao] = P2_im
-                m2 += nao
-
-            P1dXP2_mam = np.zeros((P1dX_mi.shape[0],
-                                   P2_iam.shape[1]), dtype)
-            gemm(1.0, P2_iam, P1dX_mi, 0.0, P1dXP2_mam)
-            nops += P1dXP2_mam.size * P2_iam.shape[0]
-
-            m2 = 0
-            for a2 in a2_a:
-                nao = nao_a[a2]
-                X_mM[:, M_a[a2]:M_a[a2] + setups[a2].nao] += \
-                    P1dXP2_mam[:, m2:m2 + nao]
-                m2 += nao
-
-        self.nops = nops
-        self.inner = inner
-        self.outer = outer
-
-
-class SparseAtomicCorrection(DistributedAtomicCorrection):
-    name = 'sparse'
-    description = 'sparse using scipy'
-
-    def __init__(self, tolerance=1e-12):
-        DistributedAtomicCorrection.__init__(self)
-        from scipy import sparse
-        self.sparse = sparse
-        self.tolerance = tolerance
-
     def gobble_data(self, wfs):
-        DistributedAtomicCorrection.gobble_data(self, wfs)
+        self.orig_partition = wfs.atom_partition
+        evenpart = EvenPartitioning(self.orig_partition.comm,
+                                    self.orig_partition.natoms)
+        self.even_partition = evenpart.as_atom_partition()
         nq = len(wfs.kd.ibzk_qc)
 
         I_a = [0]
