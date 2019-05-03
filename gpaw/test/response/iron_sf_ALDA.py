652534daf0211f9cbafae33349d72c95023ce75a
Thorbjørn Skovhus <s134264@student.dtu.dk>
2019-05-03 11:05:26 +0200

    deleted band summation and added precise test changes

--- gpaw/test/response/iron_sf_ALDA.py
+++ gpaw/test/response/iron_sf_ALDA.py
@@ -1,5 +1,10 @@
 """
 Calculate the magnetic response in iron using ALDA.
+
+Tests whether the magnon energies and scattering intensities
+have changed for:
+ * Different kernel calculation strategies
+ * Different chi0 transitions summation strategies
 """
 
 # Workflow modules
@@ -24,16 +29,26 @@ xc = 'LDA'
 kpts = 4
 nb = 6
 pw = 300
+conv = {'density': 1.e-8,
+        'forces': 1.e-8}
 a = 2.867
 mm = 2.21
 
 # Part 2: magnetic response calculation
-q_c = [0.0, 0.0, 0.0]  # Gamma-point
-frq_w = np.linspace(0.150, 0.350, 26)
+q_c = [0.0, 0.0, 1 / 4.]
 Kxc = 'ALDA'
 ecut = 300
 eta = 0.01
-rshecc_c = [0.99, 0.999]  # test different levels of expansion
+
+# Test different kernel and summation strategies
+strat_sd = [(None, False),  # rshe, dsctrs
+            (0.99, False),
+            (0.999, False),
+            (0.999, True)]
+frq_sw = [np.linspace(0.160, 0.320, 21),
+          np.linspace(0.320, 0.480, 21),
+          np.linspace(0.320, 0.480, 21),
+          np.linspace(0.320, 0.480, 21)]
 
 # ------------------- Script ------------------- #
 
@@ -48,6 +63,8 @@ calc = GPAW(xc=xc,
             mode=PW(pw),
             kpts=monkhorst_pack((kpts, kpts, kpts)),
             nbands=nb,
+            convergence=conv,
+            symmetry={'point_group': False},
             idiotproof=False,
             parallel={'band': 1})
 
@@ -57,44 +74,60 @@ calc.write('Fe', 'all')
 t2 = time.time()
 
 # Part 2: magnetic response calculation
-tms = TransverseMagneticSusceptibility(calc='Fe',
-                                       frequencies=frq_w,
-                                       eta=eta,
-                                       ecut=ecut)
 
-for c, rshecc in enumerate(rshecc_c):
+for s, ((rshe, dsctrs), frq_w) in enumerate(zip(strat_sd, frq_sw)):
+    tms = TransverseMagneticSusceptibility(calc='Fe',
+                                           frequencies=frq_w,
+                                           eta=eta,
+                                           ecut=ecut,
+                                           disable_spincons_time_reversal=dsctrs)
     chiM0_w, chiM_w = tms.get_dynamic_susceptibility(q_c=q_c, xc=Kxc,
-                                                     rshe=rshecc,
+                                                     rshe=rshe,
                                                      filename='iron_dsus'
-                                                     + '_G%d.csv' % (c + 1))
+                                                     + '_G%d.csv' % (s + 1))
 
 t3 = time.time()
 
 parprint('Ground state calculation took', (t2 - t1) / 60, 'minutes')
-parprint('Excited state calculation took', (t3 - t2) / 60, 'minutes')
+parprint('Excited state calculations took', (t3 - t2) / 60, 'minutes')
 
 world.barrier()
 
 # Part 3: identify magnon peak in scattering functions
 d1 = np.loadtxt('iron_dsus_G1.csv', delimiter=', ')
 d2 = np.loadtxt('iron_dsus_G2.csv', delimiter=', ')
+d3 = np.loadtxt('iron_dsus_G3.csv', delimiter=', ')
+d4 = np.loadtxt('iron_dsus_G4.csv', delimiter=', ')
 
 wpeak1, Ipeak1 = findpeak(d1[:, 0], - d1[:, 4])
 wpeak2, Ipeak2 = findpeak(d2[:, 0], - d2[:, 4])
+wpeak3, Ipeak3 = findpeak(d3[:, 0], - d3[:, 4])
+wpeak4, Ipeak4 = findpeak(d4[:, 0], - d4[:, 4])
 
 mw1 = (wpeak1 + d1[0, 0]) * 1000
 mw2 = (wpeak2 + d2[0, 0]) * 1000
+mw3 = (wpeak3 + d3[0, 0]) * 1000
+mw4 = (wpeak4 + d4[0, 0]) * 1000
 
 # Part 4: compare new results to test values
-test_mw1 = 247.46  # meV
-test_mw2 = 249.07  # meV
-test_Ipeak1 = 69.03  # a.u.
-test_Ipeak2 = 66.25  # a.u.
+test_mw1 = 242.53  # meV
+test_mw2 = 399.83  # meV
+test_mw3 = 400.62  # meV
+test_Ipeak1 = 57.19  # a.u.
+test_Ipeak2 = 59.50  # a.u.
+test_Ipeak3 = 59.10  # a.u.
 
+# Different kernel strategies should remain the same
 # Magnon peak:
 equal(mw1, test_mw1, eta * 100)
 equal(mw2, test_mw2, eta * 100)
+equal(mw3, test_mw3, eta * 100)
 
 # Scattering function intensity:
 equal(Ipeak1, test_Ipeak1, 1.5)
 equal(Ipeak2, test_Ipeak2, 1.5)
+equal(Ipeak3, test_Ipeak3, 1.5)
+
+# The two transitions summation strategies should give identical results
+equal(mw3, mw4, eta * 100)
+equal(Ipeak3, Ipeak4, 1.5)
