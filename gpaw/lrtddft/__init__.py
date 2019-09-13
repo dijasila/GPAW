309a3232a40ed5c7fc9bbe1fecc8d365fa4009ea
Michael Walter <mcoywalter@gmail.com>
2019-09-13 11:21:10 +0200

    Make energyunit changeable

--- gpaw/lrtddft/__init__.py
+++ gpaw/lrtddft/__init__.py
@@ -579,6 +579,7 @@ class LrTDDFTExcitation(Excitation):
 
 def photoabsorption_spectrum(excitation_list, spectrum_file=None,
                              e_min=None, e_max=None, delta_e=None,
+                             energyunit='eV',
                              folding='Gauss', width=0.1, comment=None):
     """Uniform absorption spectrum interface
 
@@ -598,6 +599,6 @@ def photoabsorption_spectrum(excitation_list, spectrum_file=None,
 
     spectrum(exlist=excitation_list, filename=spectrum_file,
              emin=e_min, emax=e_max,
-             de=delta_e, energyunit='eV',
+             de=delta_e, energyunit,
              folding=folding, width=width,
              comment=comment)
