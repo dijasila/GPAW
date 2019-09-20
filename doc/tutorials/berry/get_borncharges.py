9ff9ef45be77ee08a08ecd34bc99ce050d6515ad
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-09-20 11:12:13 +0200

    Nicer output

--- doc/tutorials/berry/get_borncharges.py
+++ doc/tutorials/berry/get_borncharges.py
@@ -4,4 +4,5 @@ import numpy as np
 with open('borncharges-0.01.json') as fd:
     Z_avv = json.load(fd)['Z_avv']
 for a, Z_vv in enumerate(Z_avv):
-    print(a, np.round(Z_vv, 2))
+    print(a)
+    print(np.round(Z_vv, 2))
