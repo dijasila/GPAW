fb5011c593e274b0ad53a48d126a53241b4db80a
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-09-20 10:26:47 +0200

    Make FOSS toolchain work

--- doc/platforms/Linux/Niflheim/el7-foss.py
+++ doc/platforms/Linux/Niflheim/el7-foss.py
@@ -1,6 +1,5 @@
 scalapack = True
 fftw = True
-platform_id = os.environ['CPU_ARCH'] + '-el7'
 
 # Clean out any autodetected things, we only want the EasyBuild
 # definitions to be used.
@@ -9,9 +8,7 @@ mpi_libraries = []
 include_dirs = []
 
 # Use EasyBuild scalapack from the active toolchain
-mpi_libraries += ['scalapack']
-mpi_define_macros += [('GPAW_NO_UNDERSCORE_CBLACS', '1')]
-mpi_define_macros += [('GPAW_NO_UNDERSCORE_CSCALAPACK', '1')]
+libraries += ['scalapack']
 
 # Use EasyBuild libxc
 libxc = os.getenv('EBROOTLIBXC')
@@ -29,7 +26,3 @@ if libvdwxc:
 
 # Now add a EasyBuild "cover-all-bases" library_dirs
 library_dirs = os.getenv('LD_LIBRARY_PATH').split(':')
-
-# Build separate gpaw-python
-mpicompiler = 'mpicc'
-mpilinker = mpicompiler
