e4a8dbf3c494f4fbee3606f5281002a9fb584db9
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-10-29 13:35:36 +0100

    Fix MANIFEST.in

--- gpaw/utilities/build_web_page.py
+++ gpaw/utilities/build_web_page.py
@@ -47,7 +47,7 @@ git clean -fdx doc
 rm -r build
 git checkout master -q > /dev/null 2>&1
 git pull -q > /dev/null 2>&1
-GPAW_CONFIG=doc/platforms/Linux/Niflheim/el7-intel.sh python setup.py install --remove-default-flags
+GPAW_CONFIG=doc/platforms/Linux/Niflheim/el7-intel.sh python setup.py install
 cd doc; sphinx-build -b html -d build/doctrees . build/html
 mv doc/build/html gpaw-web-page/dev
 python setup.py sdist
