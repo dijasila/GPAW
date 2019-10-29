47db4900d3f2602c5318565d589bc4624a5215d5
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-10-29 09:31:59 +0100

    Use RMMDIIS

--- gpaw/test/big/pbe0/molecules.py
+++ gpaw/test/big/pbe0/molecules.py
@@ -1,6 +1,6 @@
 import numpy as np
 from ase import Atoms
-from gpaw import GPAW, PW
+from gpaw import GPAW, PW, RMMDIIS
 from gpaw.hybrids import HybridXC
 
 
@@ -10,7 +10,7 @@ def run(symbol, d0, M, ecut, L):
     a.center()
     D = np.linspace(d0 * 0.98, d0 * 1.02, 5)
     E = []
-    for d in D:
+    if 0:  # for d in D:
         a.set_distance(0, 1, d)
         a.calc = GPAW(
             mode=PW(ecut, force_complex_dtype=True),
@@ -20,23 +20,25 @@ def run(symbol, d0, M, ecut, L):
             txt=f'{symbol}2-{d / d0:.2f}.txt')
         e = a.get_potential_energy()
         E.append(e)
+    """
     p0 = np.polyfit(D, E, 3)
     p1 = np.polyder(p0)
     p2 = np.polyder(p1)
     d = sorted([(np.polyval(p2, d), d) for d in np.roots(p1)])[1][1]
     e2 = np.polyval(p0, d)
+    """
 
     a = Atoms(symbol, cell=[L, L, L], magmoms=[M])
     a.center()
     a.calc = GPAW(
         mode=PW(ecut, force_complex_dtype=True),
         xc=HybridXC('PBE0', mix_all=False),
-        eigensolver='rmm-diis',
+        eigensolver=RMMDIIS(niter=1),  # 'rmm-diis',
         parallel={'band': 1, 'kpt': 1},
         txt=f'{symbol}.txt')
     e1 = a.get_potential_energy()
-    print(symbol, 2 * e1 - e2, d)
-    return 2 * e1 - e2, d
+    # print(symbol, 2 * e1 - e2, d)
+    # return 2 * e1 - e2, d
 
 
 if __name__ == '__main__......':
--- gpaw/test/exx/grr.py
+++ gpaw/test/exx/grr.py
@@ -1,7 +1,7 @@
 import sys
 # import numpy as np
 from ase import Atoms
-from gpaw import GPAW, PW, Davidson
+from gpaw import GPAW, PW, RMMDIIS, Davidson
 from gpaw.hybrids import HybridXC
 
 k = int(sys.argv[1])
@@ -9,16 +9,18 @@ L = 4.0
 a = Atoms('H',
           cell=[L, L, 1],
           pbc=1)
-a *= (1, 1, 2 // k)
+a *= (1, 1, 4 // k)
 a.center()
 es = Davidson(1)
 es.keep_htpsit = True
-xc = HybridXC('HSE06')
+xc = HybridXC('EXX')  # HSE06')
 a.calc = GPAW(
     mode=PW(400, force_complex_dtype=True),
     kpts={'size': (1, 1, k), 'gamma': True},
+    setups='ae',
     symmetry='off',
     nbands=1,
     eigensolver=es,
+    # eigensolver=RMMDIIS(0, niter=1),
     xc=xc)
 e = a.get_potential_energy()
