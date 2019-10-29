b4e24d4178df9e121232d7efa055a1e3162dab78
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-10-29 17:02:10 +0100

    Run test with HSE06

--- gpaw/test/exx/grr.py
+++ gpaw/test/exx/grr.py
@@ -4,15 +4,28 @@ from ase import Atoms
 from gpaw import GPAW, PW, RMMDIIS, Davidson
 from gpaw.hybrids import HybridXC
 
-k = int(sys.argv[1])
 L = 4.0
 a = Atoms('H',
           cell=[L, L, 1],
           pbc=1)
-a *= (1, 1, 4 // k)
 a.center()
+a.calc = GPAW(
+    mode=PW(400, force_complex_dtype=True),
+    eigensolver=Davidson(1),
+    kpts={'size': (1, 1, 2), 'gamma': True},
+    xc='HSE06')
+e1 = a.get_potential_energy()
+a *= (1, 1, 2)
 es = Davidson(1)
 es.keep_htpsit = True
+a.calc = GPAW(
+    mode=PW(400, force_complex_dtype=True),
+    #
+    # eigensolver=RMMDIIS(1, niter=1),
+    xc='HSE06')
+e2 = a.get_potential_energy()
+
+
 xc = HybridXC('EXX')  # HSE06')
 a.calc = GPAW(
     mode=PW(400, force_complex_dtype=True),
@@ -21,6 +34,5 @@ a.calc = GPAW(
     symmetry='off',
     nbands=1,
     eigensolver=es,
-    # eigensolver=RMMDIIS(0, niter=1),
     xc=xc)
 e = a.get_potential_energy()
