774a23bca771f824ba728a91605c7bdd31731a98
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-11-01 09:40:34 +0100

    Simpler crontab script for testing GPAW

--- gpaw/test/crontab.py
+++ gpaw/test/crontab.py
@@ -1,53 +1,44 @@
-"""Test GPAW.
+"""Test GPAW in a venv."""
 
-Initial setup::
-
-    cd ~
-    python3 -m venv gpaw-tests
-    cd gpaw-tests
-    . bin/activate
-    pip install scipy
-    git clone http://gitlab.com/ase/ase.git
-    cd ase
-    pip install -U .
-    cd ..
-    git clone http://gitlab.com/gpaw/gpaw.git
-    cd gpaw
-    python setup.py install ...
-
-Crontab::
-
-    GPAW_COMPILE_OPTIONS="..."
-    CMD="python3 -m gpaw.test.crontab"
-    10 20 * * * cd ~/gpaw-tests; . bin/activate; $CMD > test.out
-
-"""
-from __future__ import print_function
 import os
+import shutil
 import subprocess
 import sys
+from pathlib import Path
 
 
 cmds = """\
-pip install --upgrade pip
-touch gpaw-tests.lock
-cd ase; git pull -q; pip install -U .
-cd gpaw; git clean -fdx; git pull -q
-cd gpaw; python setup.py install {} 2> ../test.err
+python3 -m venv venv
+. venv/bin/activate
+pip install wheel
+pip install git+https://gitlab.com/ase/ase.git@master
+pip install git+https://gitlab.com/gpaw/gpaw.git@master
 gpaw test > test-1.out
 gpaw -P 2 test > test-2.out
 gpaw -P 4 test > test-4.out
-mpiexec -np 8 --oversubscribe gpaw test > test-8.out
-grep -i fail test-?.out >&2 || echo OK"""
-# grep return error code 1 if nothing found.  So we end with "echo OK" to get
-# a zero error code.
+gpaw -P 8 test > test-8.out"""
 
-cmds = cmds.format(os.environ.get('GPAW_COMPILE_OPTIONS', ''))
 
-if os.path.isfile('gpaw-tests.lock'):
-    sys.exit('Locked')
-try:
-    for cmd in cmds.splitlines():
-        subprocess.check_call(cmd, shell=True)
-finally:
-    os.remove('gpaw-tests.lock')
+def run_tests():
+    root = Path('/tmp/gpaw-tests')
+    if root.is_dir():
+        sys.exit('Locked')
+    root.mkdir()
+    os.chdir(root)
+    cmds2 = ' && '.join(cmd for cmd in cmds.splitlines()
+                        if not cmd.startswith('#'))
+    p = subprocess.run(cmds2, shell=True)
+    if p.returncode == 0:
+        status = 'ok'
+    else:
+        print('FAILED!', file=sys.stdout)
+        status = 'error'
+    f = root.with_name('gpaw-test-' + status)
+    if f.is_dir():
+        shutil.rmtree(f)
+    root.rename(f)
+    return status
+
+
+if __name__ == '__main__':
+    run_tests()
