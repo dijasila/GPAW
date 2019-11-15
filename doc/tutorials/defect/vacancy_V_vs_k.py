339ee93a7d460970d467d9b228c5ad6bb0dc053d
Kristen Kaasbjerg <kkaa@dtu.dk>
2019-11-15 08:53:15 +0100

    Added new files

--- doc/tutorials/defect/vacancy_V_vs_k.py
+++ doc/tutorials/defect/vacancy_V_vs_k.py
@@ -7,7 +7,7 @@ import numpy.linalg as la
 import pylab as plt
 
 from ase.io import read
-from ase.dft.kpoints import ibz_points
+from ase.dft.kpoints import ibz_points, kpoint_convert
 
 from gpaw import GPAW
 
@@ -18,20 +18,16 @@ save = False
 
 # Parameters
 nkpts = 100           # k-point grid
-ebands = slice(3, 5)  # Electronic bands (valence + conduction)
-nbands = ebands.stop - ebands.start
+bands = slice(3, 5)  # Electronic bands (valence + conduction)
+nbands = bands.stop - bands.start
 
 # Atoms
 atoms = read('graphene.traj')
-
 # Unit cell (Ang)
 cell_cv = atoms.cell
-# Cell of reciprocal lattice
-reci_vc = 2 * pi * la.inv(cell_cv)
 
 # Square k-point grid that covers the BZ
-a_ = la.norm(reci_vc[:, 0])
-b = a_ * 0.66
+b = 0.75 * 2 * pi / cell_cv[0,0]
 k_v = np.linspace(-b, b, nkpts)
 kx_vv, ky_vv = np.meshgrid(k_v, k_v)
 
@@ -39,7 +35,7 @@ kx_vv, ky_vv = np.meshgrid(k_v, k_v)
 kpts_kv = np.array([kx_vv.ravel(), ky_vv.ravel(),
                     np.zeros_like(kx_vv.ravel())]).transpose()
 # Convert to units of reciprocal lattice vectors
-kpts_kc = np.dot(kpts_kv, la.inv(reci_vc).T)
+kpts_kc = kpoint_convert(cell_cv, ckpts_kv=kpts_kv)
 
 # High-symmetry points in the Brillouin zone
 points = ibz_points['hexagonal']
@@ -58,7 +54,7 @@ calc_ft = Fourier(calc)
 e_nk, c_knM = calc_ft.diagonalize(kpts_kc, return_wfs=True)
 
 # Slice out LCAO coefficient for specified bands
-c_kn = np.ascontiguousarray(c_knM[:, ebands])
+c_kn = np.ascontiguousarray(c_knM[:, bands])
 
 # Defect calculation
 basis = 'DZP'
@@ -72,12 +68,14 @@ calc_def = Defect(atoms, calc=None, supercell=(N, N, 1), defect=None,
 # Load supercell matrix
 calc_def.load_supercell_matrix('%s(dzp)' % basis.lower(), cutoff=cutoff,
                                pos0=atoms.positions[0])
+
 # Calculate matrix elements
 V_kknn = calc_def.bloch_matrix(kpts_kc, c_kn, kpts_from=[0])
 # Extract elements and reshape
 V0_vvnn = V_kknn[0, 1:].reshape(len(kx_vv), len(ky_vv), nbands, nbands)
 
-# Band element for plotting
+# Plotting
+# Matrix element to be plotted
 ind1 = 1 ; ind2 = 1
 Vnn = np.abs(V0_vvnn[:, :, ind1, ind2])
 
@@ -97,10 +95,10 @@ K_kc = [[-1./3, 1./3, 0],
         [-1./3, 1./3, 0],
         ]
 # Convert to cartesian coordinates
-K_kv = np.dot(K_kc, reci_vc.T)
-k0_v = np.dot(k0_c, reci_vc.T)
+K_kv = kpoint_convert(cell_cv, skpts_kc=K_kc)
+k0_v = kpoint_convert(cell_cv, skpts_kc=k0_c)
 
-# Plot - convert to (2pi/a) coordinates
+# Convert to units of (2pi/a)
 a = la.norm(cell_cv[0])
 kx_vv /= (2*pi / a)
 ky_vv /= (2*pi / a)
@@ -110,26 +108,23 @@ k0_v /= (2*pi / a)
 
 plt.figure(1, (8, 6))
 plt.axes([.18, .18, .76, .76])
+
 # BZ
 plt.plot(K_kv[:, 0], K_kv[:, 1], 'k', lw=2)
-
 # Mark initial state
 plt.plot([k0_v[0],], [k0_v[1],], 'o', markerfacecolor='w',
          markersize=8, zorder=11)
 plt.text(0.975*k0_v[0], 1.025*k0_v[1], r'$\mathbf{k}$', fontsize=30, color='w',
          va='bottom', ha='left')
-
-
 # Matrix element
 p1 = plt.imshow(Vnn, origin='lower', interpolation='nearest',
                 extent=(kx_vv[0, 0], kx_vv[0, -1],
                         ky_vv[0, 0], ky_vv[-1, 0]), cmap='jet')
-
 # Color limits
 V_max = np.max(Vnn)
 V_min = np.min(Vnn)
 p1.set_clim([0, V_max])
-    
+
 # Colorbar
 tick_pos = np.linspace(0, V_max, 4)
 c = plt.colorbar(ticks=tick_pos)
@@ -138,6 +133,7 @@ c.ax.set_yticklabels(['%2i' % t for t in tick_pos])
 # Add contour plot
 plt.contour(kx_vv, ky_vv, Vnn, 10, colors='k')
 
+# Limits, ticks, and labels
 plt.xlim([kx_vv.min(), kx_vv.max()])
 plt.ylim([ky_vv.min(), ky_vv.max()])
 
