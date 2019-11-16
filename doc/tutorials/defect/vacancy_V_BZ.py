3774c3b0857ed6133136da8bafee782784533f4d
Kristen Kaasbjerg <kkaa@dtu.dk>
2019-11-16 10:23:49 +0100

    small change

--- doc/tutorials/defect/vacancy_V_BZ.py
+++ doc/tutorials/defect/vacancy_V_BZ.py
@@ -12,8 +12,8 @@ from gpaw1.lcao.fourier import Fourier
 from gpaw1.defects.defect import Defect
 
 
-# Use a number divicible by 3 in order to include K,K' points
-nkpts = 15
+# Use an odd number divicible by 3 in order to include K,K' points
+nkpts = 45
 bands = slice(0, 8)
 nbands = bands.stop - bands.start
 
@@ -23,17 +23,30 @@ atoms = read('graphene.traj')
 # k-point grid
 kpts_kc = monkhorst_pack((nkpts, nkpts, 1))
 
-# Calculate wfs
+############################################
+#              Calculate wfs
+############################################
 # Use lcao/fourier module to interpolate onto arbitrary k-points
 calc = GPAW('calc_lcao.gpw')
 calc_ft = Fourier(calc)
-e_nk, c_knM = calc_ft.diagonalize(kpts_kc, return_wfs=True)
+eps_kn, coef_knM = calc_ft.diagonalize(kpts_kc, return_wfs=True)
 
-# Slice out LCAO coefficient for specified bands
-c_kn = np.ascontiguousarray(c_knM[:, bands])
+# Slice out bands
+c_knM = coef_knM[:, bands]
+e_kn = eps_kn[:, bands]
+
+# Dump bands and states to file for later use
+c_nkM = c_knM.transpose(1, 0, 2)
+e_nk = e_kn.T
+base = 'graphene_lcao_BZ_%ux%u_bands_%u-%u' % \
+       (nkpts, nkpts, bands.start, bands.stop)
+fd = open('%s.pckl' % base, 'wb')
+pickle.dump([kpts_kc, e_nk, c_nkM], fd)
+fd.close()
+
 
 ############################################
-#            Defect calculation
+#       Calculate matrix elements
 ############################################
 basis_lcao = 'DZP'
 N = 5
@@ -46,29 +59,22 @@ calc_def = Defect(atoms, calc=None, supercell=(N, N, 1), defect=None,
 calc_def.load_supercell_matrix('%s(dzp)' % basis_lcao.lower(), cutoff=cutoff,
                                pos0=atoms.positions[0])
 
-# Calculate matrix elements: hermitian=True -> only upper triangle wrt the
-# k-point index is calculated
-V_kknn = calc_def.bloch_matrix(kpts_kc, c_kn, hermitian=True)
+# Calculate matrix elements: hermitian=True: take advantage of the fact that
+# the matrix is hermitian -> only upper triangle is calculated
+V_kknn = calc_def.bloch_matrix(kpts_kc, c_knM, hermitian=True)
 
-# Reorder matrix elements
+# Rearrange matrix elements
 V_nknk = np.ascontiguousarray(V_kknn.transpose(2, 0, 3, 1))
  
 # Check for hermitian
-hermitian = np.allclose(V_nknk, V_nknk.conj().transpose(2,3,0,1))
-assert hermitian, "Matrix not hermitian"
+# hermitian = np.allclose(V_nknk, V_nknk.conj().transpose(2,3,0,1))
+# assert hermitian, "Matrix not hermitian"
 assert V_nknk.flags['C_CONTIGUOUS']
 
-# Dump matrix elements to file -- use numpy format as matrices can be very
-# large 
+# Dump matrix elements to file -- use numpy format as matrices can get very
+# large
 base = 'vacancy_V_BZ_%ux%u_bands_%u-%u' % \
        (nkpts, nkpts, bands.start, bands.stop)
 fd = open('./pickle/%s.npy' % base, 'wb')
 np.save(fd, V_nknk, allow_pickle=False)
-fd.close()
-
-# Dump to unperturbed bands to file
-base = 'graphene_bands_BZ_%ux%u_bands_%u-%u' % \
-       (nkpts, nkpts, bands.start, bands.stop)
-fd = open('%s.pckl' % base, 'wb')
-pickle.dump([kpts_kc, e_nk], fd)
 fd.close()
