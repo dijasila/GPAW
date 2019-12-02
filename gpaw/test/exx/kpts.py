015a7deebe35260cc541db45329815fd1c2fba37
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-12-02 12:09:18 +0100

    Clean up tests

--- gpaw/test/exx/kpts.py
+++ gpaw/test/exx/kpts.py
@@ -12,7 +12,7 @@ def test(kpts, setup, spinpol, symmetry):
                   setups=setup,
                   kpts=kpts,
                   spinpol=spinpol,
-                  #nbands=1,
+                  # nbands=1,
                   symmetry=symmetry,
                   txt=None,
                   xc='PBE')
@@ -26,51 +26,35 @@ def check(atoms, xc):
     xc1.initialize(c.density, c.hamiltonian, c.wfs, c.occupations)
     xc1.set_positions(c.spos_ac)
     e = xc1.calculate_energy()
-    # print(e)
     xc1.calculate_eigenvalues(0, 2, None)
-    # print('A', xc1.e_skn * Ha)
 
     xc2 = EXX(c, xc=xc, bands=(0, 2), txt=None)
     xc2.calculate()
     e0 = xc2.get_exx_energy()
     eps0 = xc2.get_eigenvalue_contributions()
-    # print('B', eps0)
     assert np.allclose(eps0, xc1.e_skn * Ha)
-    # print(e0, e[0] + e[1])
     assert np.allclose(e0, e[0] + e[1])
-    # print(xc1.description)
     ecv, evv, v_skn = xc1.test()
-    # print('C', v_skn)
     assert np.allclose(e0, ecv + evv)
-    #print(v_skn / eps0)
     assert np.allclose(v_skn, eps0)
 
 
 def main():
-    for spinpol in [False,
-                    True
-                    ]:
-        for setup in ['ae',
-                      'paw'
-                      ]:
-            for symmetry in ['off',
-                             {}]:
-                for kpts in [
-                    (1, 1, 1),
-                    (1, 1, 2),
-                    (1, 1, 3),
-                    (1, 1, 4),
-                    (2, 2, 1),
-                    [(0, 0, 0.5)],
-                    [(0, 0, 0), (0, 0, 0.5)]]:
+    for spinpol in [False, True]:
+        for setup in ['ae', 'paw']:
+            for symmetry in ['off', {}]:
+                for kpts in [(1, 1, 1),
+                             (1, 1, 2),
+                             (1, 1, 3),
+                             (1, 1, 4),
+                             (2, 2, 1),
+                             [(0, 0, 0.5)],
+                             [(0, 0, 0), (0, 0, 0.5)]]:
                     atoms = test(kpts, setup, spinpol, symmetry)
-                    for xc in ['EXX',
-                               'PBE0', 'HSE06'
-                               ]:
+                    for xc in ['EXX', 'PBE0', 'HSE06']:
                         print(spinpol, setup, symmetry, kpts, xc,
                               len(atoms.calc.wfs.mykpts))
                         check(atoms, xc)
-                    #break
 
 
 main()
