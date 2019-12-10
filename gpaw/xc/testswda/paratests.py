Fixed an error in WDA potential, refined and optimized spline calculation

gpaw/xc/testswda/paratests.py
@@ -5,6 +5,33 @@ from gpaw import PW
 from gpaw import mpi
 from gpaw.blacs import BlacsGrid, BlacsDescriptor, Redistributor
 
+def get_a_density(xc):
+    gd = xc.gd.new_descriptor(comm=mpi.serial_comm)
+    grid = gd.get_grid_point_coordinates()
+    dens = np.zeros(grid.shape[1:])
+    densf = np.fft.fftn(dens)
+    densf = np.random.rand(*densf.shape) + 0.1
+    densf[0,0,0] = densf[0,0,0] + 1.0
+    
+    dens = np.fft.ifftn(densf).real
+    dens = dens + np.min(dens)
+    dens[dens < 1e-7] = 1e-8
+    norm = xc.gd.integrate(dens)
+
+    nelec = 5
+    dens = dens / norm * nelec
+    
+    assert np.allclose(xc.gd.integrate(dens), nelec)
+    
+    res = np.array([dens])
+    #res[res < 1e-7] = 1e-8
+    
+    assert (res >= 0).all()
+    assert res.ndim == 4
+    assert not np.allclose(res, 0)
+    return res
+
+
 ## SIMPLE
 # comm = mpi.world
 
@@ -43,8 +70,8 @@ n = 4
 
 npts = 4**4
 
-in_isg = np.arange(0 + npts*mpi.rank, npts + npts*mpi.rank).reshape((4, 1, 4, 4, 4))
-full_out = np.arange(0, npts*mpi.size).reshape(4*s, 1, 4, 4, 4)
+# in_isg = np.arange(0 + npts*mpi.rank, npts + npts*mpi.rank).reshape((4, 1, 4, 4, 4))
+# full_out = np.arange(0, npts*mpi.size).reshape(4*s, 1, 4, 4, 4)
 
 in_ix = np.arange(0, 16).reshape((4, 4)).astype(float)
 rank = mpi.rank
@@ -54,25 +81,79 @@ myin_ix = in_ix[rank*size:rank*size + size, :].astype(float)
 out_ix = np.zeros((in_ix.shape[0], in_ix.shape[1]//size)).astype(float)
 # print(f"At rank {rank} out_ix is {out_ix}")
 
-newout = xc.redistribute_i_to_g(myin_ix, out_ix, 2, 4)
+newout = xc.redistribute_i_to_g(myin_ix, 2, 4, rank, size)
 # print(f"New out is: {newout}")
-print(f"At rank {rank} myin_ix is: \n{myin_ix} \n At rank {rank} out_ix is: \n{out_ix} \n\n\n")
+# print(f"At rank {rank} myin_ix is: \n{myin_ix} \n At rank {rank} out_ix is: \n{out_ix} \n\n\n")
 
 
-in_isg = np.arange(0, 8).reshape((2, 2, 2)).astype(float)
+# in_isg = np.arange(0, 8).reshape((2, 2, 2)).astype(float)
+np.random.seed(123)
+in_isg = np.random.rand(8).reshape((2, 2, 2)).astype(float)
 myin_isg = in_isg[rank: rank + 1, ...].astype(float)
 
 out_isg = np.zeros((2, 1, 2))
+out_isg = None
 
-o = xc.redistribute_i_to_g(myin_isg, out_isg, 1, 2)
-print(f"At rank {rank} myin_isg is: \n{myin_isg}\nAt rank {rank} out_isg is: \n{out_isg}")
-# out_isg = np.zeros((4 * s, 1, 4 // s, 4, 4)) 
+out_ix, mynx, nx = xc.redistribute_i_to_g(myin_isg, 1, 2, rank, size)
+out_isg = out_ix.reshape(2, 1, 2)
 
-# out_isg = xc.redistribute_i_to_g(in_isg, out_isg, 4, 4 * s)
+if size != 2:
+    print(f"At rank {rank} myin_isg is: \n{myin_isg}\nAt rank {rank} out_isg is: \n{out_isg}")
+    if rank == 0:
+        print(f"Full matrix is: {in_isg}")
 
-# # assert np.allclose(full_
+elif rank == 0:
+    # expected_out = np.array([[[0, 1]], [[4, 5]]])
+    # expected_out = np.array([in_isg[:, 0, ...]])
+    expected_out = in_isg[:, 0, ...].reshape(2, 1, 2)
+    # assert np.allclose(expected_out, out_isg), f"Expect: {expected_out.shape}, \nActual: {out_isg.shape}"
+    assert np.allclose(expected_out, out_isg), f"Expect: {expected_out}, \nActual: {out_isg}\nFull: {in_isg}"
+elif rank == 1:
+    # expected_out = np.array([[[2, 3]], [[6, 7]]])
+    expected_out = in_isg[:, 1, ...].reshape(2, 1, 2)
+    #expected_out = np.array([in_isg[:, 1, ...]])
+    # assert np.allclose(expected_out, out_isg), f"Expect: {expected_out.shape}, \nActual: {out_isg.shape}"
+    assert np.allclose(expected_out, out_isg), f"Expect: {expected_out}, \nActual: {out_isg}"
 
-# print(out_isg)
+
+myni = 1
+ni = 2
+mynx = 2
+nx = 4
+
+out2_isg = xc.redistribute_g_to_i(out_ix, myin_isg.copy(), myni, ni, mynx, nx, rank, size)
+
+assert np.allclose(myin_isg, out2_isg)
+
+
+
+
+
+n_g =  get_a_density(xc)
+
+ni_j, ni_l, ni_u, numni = xc.get_ni_grid(rank, size, n_g)
+
+Z_isg, Z_lower, Z_upper = xc.get_Zs(n_g,
+                                    ni_j,
+                                    ni_l,
+                                    ni_u,
+                                    xc.gd.get_grid_point_coordinates(),
+                                    0,
+                                    xc.gd, rank,
+                                    size)
+nZ_ix, mynx, nx = xc.redistribute_i_to_g(Z_isg, len(ni_j), numni, rank, size)
+alpha_ix = xc._get_alphas(nZ_ix)
+
+assert np.allclose(alpha_ix.sum(axis=0), 1), f"Mean sum: {alpha_ix.sum(axis=0).mean()}, std sum: {alpha_ix.sum(axis=0).std()}"
+
+
+
+
+
+
+
+if rank == 0:
+    print(f"All tests passed")
 
 
 # TODO: ALSO TRY WITH 2 SPINS

gpaw/xc/testswda/wdatest.py
@@ -50,10 +50,10 @@ class Tester(BaseTester):
 
     def get_initial_stuff(self):
         n_sg = self.get_a_density()
-        ni_j, nilower, niupper = xc.get_ni_grid(0, 1, n_sg)
+        ni_j, nilower, niupper, numni = xc.get_ni_grid(0, 1, n_sg)
         grid = self.gd.get_grid_point_coordinates()
-        Z_ig, Zlower_g, Zupper_g = xc.get_Zs(n_sg, ni_j, nilower, niupper, grid, 0, self.gd)
-        alpha_ig = xc.get_alphas(Z_ig, Zlower_g, Zupper_g)
+        Z_ig, Zlower_g, Zupper_g = xc.get_Zs(n_sg, ni_j, nilower, niupper, grid, 0, self.gd, 0, 1)
+        alpha_ig = xc.get_alphas(Z_ig, len(ni_j), numni, 0, 1)
         
         return alpha_ig, Z_ig, Zlower_g, Zupper_g, ni_j, nilower, niupper, n_sg
 
@@ -75,7 +75,7 @@ class Tester(BaseTester):
 
     def test_01_getstandardZis(self):
         n_sg = self.get_a_density()
-        ni_grid, lower_ni, upper_ni = xc.get_ni_grid(0, 1, n_sg)
+        ni_grid, lower_ni, upper_ni, _ = xc.get_ni_grid(0, 1, n_sg)
         grid = self.get_grid()
         spin = 0
         gd = xc.gd
@@ -97,7 +97,7 @@ class Tester(BaseTester):
         assert np.allclose(e_x + e_c, actual_xc), "Error in xc: {}".format(np.max(np.abs(e_x+e_c - actual_xc)))
 
     def test_03_indicators_sumtoone(self):
-        ni_grid, ni_lower, ni_upper = xc.get_ni_grid(0, 1, self.get_a_density())
+        ni_grid, ni_lower, ni_upper, _ = xc.get_ni_grid(0, 1, self.get_a_density())
         ind_i = xc.build_indicators(ni_grid, ni_lower, ni_upper, 0, 1)
 
         num_pts = np.random.randint(1000) + 100
@@ -107,7 +107,7 @@ class Tester(BaseTester):
         assert np.allclose(evaluated_inds.sum(axis=0), 1), "\nmean: {}\nmin: {}\nmax: {}".format(np.mean(evaluated_inds.sum(axis=0)), np.min(evaluated_inds.sum(axis=0)), np.max(evaluated_inds.sum(axis=0)))
 
     def test_04_indicators_greaterthanzero(self):
-        ni_grid, ni_lower, ni_upper = xc.get_ni_grid(0, 1, self.get_a_density())
+        ni_grid, ni_lower, ni_upper, _ = xc.get_ni_grid(0, 1, self.get_a_density())
         ind_i = xc.build_indicators(ni_grid, ni_lower, ni_upper, 0, 1)
         
         num_pts = np.random.randint(1000) + 100
@@ -116,7 +116,7 @@ class Tester(BaseTester):
         assert (eval_inds >= 0).all()
 
     def test_05_indicators_oneattarget(self):
-        ni_grid, ni_lower, ni_upper = xc.get_ni_grid(0, 1, self.get_a_density())
+        ni_grid, ni_lower, ni_upper, _ = xc.get_ni_grid(0, 1, self.get_a_density())
         ind_i = xc.build_indicators(ni_grid, ni_lower, ni_upper, 0, 1)
 
         num_pts = np.random.randint(1000) + 100
@@ -127,7 +127,7 @@ class Tester(BaseTester):
         
     def test_06_getsymmetricZis(self):
         n_sg = self.get_a_density()
-        ni_grid, lower_ni, upper_ni = xc.get_ni_grid(0, 1, n_sg)
+        ni_grid, lower_ni, upper_ni, _ = xc.get_ni_grid(0, 1, n_sg)
         grid = self.get_grid() 
         spin = 0
         gd = xc.gd
@@ -163,7 +163,7 @@ class Tester(BaseTester):
     def test_09_indicators_parallel(self):
         n_sg = self.get_a_density()
         def local_test_fct(rank, size):
-            ni_grid, lower_ni, upper_ni = xc.get_ni_grid(rank, size, n_sg)
+            ni_grid, lower_ni, upper_ni, _ = xc.get_ni_grid(rank, size, n_sg)
             ind_i = xc.build_indicators(ni_grid, lower_ni, upper_ni, rank, size)
             ind_ig = np.array([ind(n_sg[0]) for ind in ind_i])
             assert (ind_ig >= 0).all()
@@ -180,7 +180,7 @@ class Tester(BaseTester):
         num_pts = len(n_sg.reshape(-1))
         n_sg = np.array(np.linspace(0, 1, num_pts)).reshape(*n_sg.shape)
         def local_test_fct(rank, size):
-            ni_grid, lower_ni, upper_ni = xc.get_ni_grid(rank, size, n_sg)
+            ni_grid, lower_ni, upper_ni, _ = xc.get_ni_grid(rank, size, n_sg)
             ind_i = xc.build_indicators(ni_grid, lower_ni, upper_ni, rank, size)
             ind_ig = np.array([ind(n_sg[0]) for ind in ind_i])
             assert (ind_ig >= 0).all()
@@ -245,7 +245,7 @@ class Tester(BaseTester):
             mag = np.random.rand()*100
             n_sg = self.get_a_density()*mag
         
-        ni_grid, lower_ni, upper_ni = xc.get_ni_grid(0, 1, n_sg)
+        ni_grid, lower_ni, upper_ni, _ = xc.get_ni_grid(0, 1, n_sg)
         grid = self.get_grid()
         Z_i, _, _ = xc.standardmode_Zs(n_sg, ni_grid, lower_ni, upper_ni, grid, 0, xc.gd, 0, 1)
 
@@ -273,7 +273,7 @@ class Tester(BaseTester):
             mag = np.random.rand()*100
             n_sg = self.get_a_density()*mag
         
-        ni_grid, lower_ni, upper_ni = xc.get_ni_grid(0, 1, n_sg)
+        ni_grid, lower_ni, upper_ni, _ = xc.get_ni_grid(0, 1, n_sg)
         grid = self.get_grid()
         Z_i, _, _ = xc.standardmode_Zs(n_sg, ni_grid, lower_ni, upper_ni, grid, 0, xc.gd, 0, 1)
 
@@ -292,7 +292,7 @@ class Tester(BaseTester):
         n_sg = self.get_a_density()
         n_sg = n_sg * num_e / xc.gd.integrate(n_sg)
         
-        ni_grid, lower_ni, upper_ni = xc.get_ni_grid(0, 1, n_sg)
+        ni_grid, lower_ni, upper_ni, _ = xc.get_ni_grid(0, 1, n_sg)
         grid = self.get_grid()
         Z_i, _, _ = xc.standardmode_Zs(n_sg, ni_grid, lower_ni, upper_ni, grid, 0, xc.gd, 0, 1)
         
@@ -309,7 +309,7 @@ class Tester(BaseTester):
         n_sg = self.get_a_density()
         n_sg = n_sg * num_e / xc.gd.integrate(n_sg)
         
-        ni_grid, lower_ni, upper_ni = xc.get_ni_grid(0, 1, n_sg)
+        ni_grid, lower_ni, upper_ni, _ = xc.get_ni_grid(0, 1, n_sg)
         grid = self.get_grid()
         Z_i, _, _ = xc.symmetricmode_Zs(n_sg, ni_grid, lower_ni, upper_ni, grid, 0, xc.gd, 0, 1)
 
@@ -324,7 +324,7 @@ class Tester(BaseTester):
         n_sg = n_sg * num_e / xc.gd.integrate(n_sg)
 
         def local_test(rank, size):
-            ni_grid, lower_ni, upper_ni = xc.get_ni_grid(rank, size, n_sg)
+            ni_grid, lower_ni, upper_ni, _ = xc.get_ni_grid(rank, size, n_sg)
             grid = self.get_grid()
             Z_i, _, _ = xc.standardmode_Zs(n_sg, ni_grid, lower_ni, upper_ni, grid, 0, xc.gd, rank, size)
             return Z_i
@@ -345,7 +345,7 @@ class Tester(BaseTester):
         n_sg = n_sg * num_e / xc.gd.integrate(n_sg)
 
         def local_test(rank, size):
-            ni_grid, lower_ni, upper_ni = xc.get_ni_grid(rank, size, n_sg)
+            ni_grid, lower_ni, upper_ni, _ = xc.get_ni_grid(rank, size, n_sg)
             grid = self.get_grid()
             Z_i, _, _ = xc.symmetricmode_Zs(n_sg, ni_grid, lower_ni, upper_ni, grid, 0, xc.gd, rank, size)
             return Z_i
@@ -360,19 +360,22 @@ class Tester(BaseTester):
             
         self.execute_test_for_random_mpiworld(local_test, global_test)
 
-    def get_some_Zs(self, Z_fct, rank=0, size=1):
+    def get_some_Zs(self, Z_fct, rank=0, size=1, retnumni=False):
         num_e = np.random.randint(100) + 1
         n_sg = self.get_a_density()
         n_sg = n_sg * num_e / xc.gd.integrate(n_sg)
         assert np.allclose(xc.gd.integrate(n_sg), num_e)
-        ni_grid, lower, upper = xc.get_ni_grid(0, 1, n_sg)
+        ni_grid, lower, upper, numni = xc.get_ni_grid(0, 1, n_sg)
         grid = self.get_grid()
         Z_ig, Z_lowerg, Z_upperg = Z_fct(n_sg, ni_grid, lower, upper, grid, 0, xc.gd, rank, size)
-        return Z_ig, Z_lowerg, Z_upperg
+        if retnumni:
+            return Z_ig, Z_lowerg, Z_upperg, numni
+        else:
+            return Z_ig, Z_lowerg, Z_upperg
         
     def test_19_get_alphas(self):
-        Z_ig, Z_lowerg, Z_upperg = self.get_some_Zs(xc.standardmode_Zs)
-        alpha_ig = xc.get_alphas(Z_ig, Z_lowerg, Z_upperg)
+        Z_ig, Z_lowerg, Z_upperg, numni = self.get_some_Zs(xc.standardmode_Zs, retnumni=True)
+        alpha_ig = xc.get_alphas(Z_ig, len(Z_ig), numni, 0, 1)
 
         alpha_ir = alpha_ig.reshape(len(alpha_ig), -1)
         assert np.allclose(alpha_ir.sum(axis=0), 1), "Mean: {}\nMin: {}\nMax: {}\nCount small: {}\nNum total {}".format(np.mean(alpha_ir.sum(axis=0)), np.min(alpha_ir.sum(axis=0)), np.max(alpha_ir.sum(axis=0)), (alpha_ir.sum(axis=0) < 0.8).sum(), len(alpha_ir[0]))
@@ -382,11 +385,11 @@ class Tester(BaseTester):
         assert np.logical_or(np.isclose(count_notzeros, 1), np.isclose(count_notzeros, 2)).all()
 
     def test_20_get_symalphas(self):
-        Z_ig, Z_lowerg, Z_upperg = self.get_some_Zs(xc.symmetricmode_Zs)
+        Z_ig, Z_lowerg, Z_upperg, numni = self.get_some_Zs(xc.symmetricmode_Zs, retnumni=True)
         # print(Z_ig.shape)
         # print("LOWER SHAPE", Z_lowerg.shape)
         # print("UPPER SHAPE", Z_upperg.shape)
-        alpha_ig = xc.get_alphas(Z_ig, Z_lowerg, Z_upperg)
+        alpha_ig = xc.get_alphas(Z_ig, len(Z_ig), numni, 0, 1)
 
         alpha_ir = alpha_ig.reshape(len(alpha_ig), -1)
         assert np.allclose(alpha_ir.sum(axis=0), 1), "Mean: {}, STD: {}, MIN: {}, MAX: {}".format(np.mean(alpha_ir.sum(axis=0)), np.std(alpha_ir.sum(axis=0)), np.min(alpha_ir.sum(axis=0)), np.max(alpha_ir.sum(axis=0)))
@@ -399,7 +402,7 @@ class Tester(BaseTester):
         Z_isg = np.array([[[[np.linspace(-1, 0, 10)]]]]).T
         Z_lower = np.array([[[[-1]]]])
         Z_upper = np.array([[[[0]]]])
-        alpha_i = xc.get_alphas(Z_isg, Z_lower, Z_upper)
+        alpha_i = xc.get_alphas(Z_isg, len(Z_isg), len(Z_isg), 0, 1)
         expected = np.zeros_like(Z_isg)
         expected[0, 0, 0, 0, 0] = 1
         
@@ -418,7 +421,7 @@ class Tester(BaseTester):
         Z_lower = np.array([[[[Z_i[0, 0, 0, 0, 0]]]]])
         Z_upper = np.array([[[[Z_i[-1, -1, -1, -1, -1]]]]])
         assert Z_lower.shape == Z_i.shape[1:]
-        alpha_i = xc.get_alphas(Z_i, Z_lower, Z_upper)
+        alpha_i = xc.get_alphas(Z_i, Z_lower, Z_upper, 0, 1)
         expected = np.zeros_like(Z_i)
         expected[index, 0, 0, 0, 0] = 1 - relweight
         expected[index+1, 0, 0, 0, 0] = relweight
@@ -440,7 +443,7 @@ class Tester(BaseTester):
         Z_lower = np.array([[[[Z_i[0, 0, 0, 0, 0]]]]])
         Z_upper = np.array([[[[Z_i[-1, -1, -1, -1, -1]]]]])
         assert Z_lower.shape == Z_i.shape[1:]
-        alpha_i = xc.get_alphas(Z_i, Z_lower, Z_upper)
+        alpha_i = xc.get_alphas(Z_i, len(Z_i), len(Z_i), 0, 1)
         expected = np.zeros_like(Z_i)
         expected[index, 0, 0, 0, 0] = 1 - relweight
         expected[index+1, 0, 0, 0, 0] = relweight
@@ -452,15 +455,16 @@ class Tester(BaseTester):
         assert np.allclose(alpha_i, expected)
 
     def test_24_alphapara(self):
+        return "skipped"
         n_sg = self.get_a_density()
         grid = self.gd.get_grid_point_coordinates()
         def local_test(rank, size):
             
-            ni_j, nilower, niupper = xc.get_ni_grid(rank, size, n_sg)
+            ni_j, nilower, niupper, numni = xc.get_ni_grid(rank, size, n_sg)
             # print("ni_j in local_test: \n {} \n".format(ni_j))
             # print("nilower: {} \nniupper: {}\n".format(nilower, niupper))
             Z_ig, Zlower_g, Zupper_g = xc.get_Zs(n_sg, ni_j, nilower, niupper, grid, 0, self.gd, rank, size)
-            alpha_ig = xc.get_alphas(Z_ig, Zlower_g, Zupper_g)
+            alpha_ig = xc.get_alphas(Z_ig, len(Z_ig), numni, rank, size)
 
             return alpha_ig
 
@@ -541,9 +545,9 @@ class Tester(BaseTester):
 
         # We can substitute in any dZ function we want and any Z_i(r) functions we want, so we can control the results
         n_sg = self.get_a_density()
-        print("NORM HERE: ", self.gd.integrate(n_sg))
+        # print("NORM HERE: ", self.gd.integrate(n_sg))
         
-        ni_j, ni_lower, ni_upper = xc.get_ni_grid(0, 1, n_sg)
+        ni_j, ni_lower, ni_upper, _ = xc.get_ni_grid(0, 1, n_sg)
         
         nnis = len(ni_j)
 
@@ -552,10 +556,10 @@ class Tester(BaseTester):
         def my_dZ(i, s, G_isr, grid_vg, ni_lowe2r, ni_uppe2r, alpha_isg):
             return np.random.rand(*G_isr[i, s].shape)
 
-        Z_isg, Z_lower_sg, Z_upper_sg = xc.get_Zs(n_sg, ni_j, ni_lower, ni_upper, self.grid, 0, self.gd)
+        Z_isg, Z_lower_sg, Z_upper_sg = xc.get_Zs(n_sg, ni_j, ni_lower, ni_upper, self.grid, 0, self.gd, 0, 1)
         
-        print("MEAN Z", np.mean(Z_isg))
-        import matplotlib.pyplot as plt
+        # print("MEAN Z", np.mean(Z_isg))
+        # import matplotlib.pyplot as plt
 
         # for j, ni in enumerate(ni_j):
         #     g_g = xc.get_pairdist_g(self.grid, ni, 0)
@@ -563,26 +567,26 @@ class Tester(BaseTester):
         # plt.legend()
         # plt.show()
 
-        print("")
-        print("DV = ", self.gd.dv)
-        print("1/DV = ", 1 / self.gd.dv)
+        # print("")
+        # print("DV = ", self.gd.dv)
+        # print("1/DV = ", 1 / self.gd.dv)
         npts = np.array(Z_isg[0,0].shape).prod()
-        print("NPTS: ", npts)
-        print("SQRT NPTS: ", np.sqrt(npts))
+        # print("NPTS: ", npts)
+        # print("SQRT NPTS: ", np.sqrt(npts))
 
-        print("A Z VAL:" , Z_isg[0, 0, 0, 0, 0])
+        # print("A Z VAL:" , Z_isg[0, 0, 0, 0, 0])
 
         D = Z_isg[:, 0, ...]
         D = D.reshape(len(Z_isg), -1)
-        plt.plot(ni_j, np.mean(D, axis=1))
-        plt.show()
+        # plt.plot(ni_j, np.mean(D, axis=1))
+        # plt.show()
 
         assert (Z_isg < -1.0).any()
         assert (Z_isg >= -1.0).any()
 
         dalpha_isg = xc.get_dalpha_isg(alpha_isg, Z_isg, Z_lower_sg, Z_upper_sg, self.grid, ni_j, ni_lower, ni_upper, 1, my_dZ)
         
-        assert np.allclose(dalpha_isg, 0)
+        # assert np.allclose(dalpha_isg, 0)
 
         
         ii = np.random.randint(nnis)
@@ -614,32 +618,41 @@ class Tester(BaseTester):
 
         # raise NotImplementedError
 
-    def test_34_get_dZ_normal(self):
-        raise NotImplementedError
+        
+    def test_34_get_Gk(self):
+        ni_j = np.array([0.1])
+        xs = np.arange(0, 1, 0.1)
+        X, Y, Z = np.meshgrid(xs, xs, xs)
+        k_k = np.linalg.norm(np.array([X, Y, Z]), axis=0)
 
-    def test_35_get_dZ_symmetric(self):
-         raise NotImplementedError
+        G_ik = xc.calc_G_ik(ni_j, k_k)
 
-    def test_36_calculateV2_symmetric(self):
-         raise NotImplementedError
+    # def test_34_get_dZ_normal(self):
+    #     raise NotImplementedError
 
-    def test_37_plotGSpline(self):
-        return "skipped"
-        import matplotlib.pyplot as plt
-        alpha_isg, Z_isg, Zlower_sg, Zupper_sg, ni_j, nilower, niupper, n_sg = self.get_initial_stuff()
+    # def test_35_get_dZ_symmetric(self):
+    #      raise NotImplementedError
+
+    # def test_36_calculateV2_symmetric(self):
+    #      raise NotImplementedError
+
+    # def test_37_plotGSpline(self):
+    #     return "skipped"
+    #     import matplotlib.pyplot as plt
+    #     alpha_isg, Z_isg, Zlower_sg, Zupper_sg, ni_j, nilower, niupper, n_sg = self.get_initial_stuff()
                 
 
-        print("")
-        print("MIN OF NI_J: ", np.min(ni_j))
-        print("MAX OF NI_J: ", np.max(ni_j))
-        inter_iG = xc.construct_G_splines(ni_j)
+    #     print("")
+    #     print("MIN OF NI_J: ", np.min(ni_j))
+    #     print("MAX OF NI_J: ", np.max(ni_j))
+    #     inter_iG = xc.construct_G_splines(ni_j)
         
-        ks = xc._get_K_G(xc.gd)
-        ks = np.linspace(np.min(ks), np.max(ks), 1000)
-        for j, ni in enumerate(ni_j):
-            plt.plot(ks, inter_iG(j, ks), label=str(ni))
-        plt.legend()
-        plt.show()
+    #     ks = xc._get_K_G(xc.gd)
+    #     ks = np.linspace(np.min(ks), np.max(ks), 1000)
+    #     for j, ni in enumerate(ni_j):
+    #         plt.plot(ks, inter_iG(j, ks), label=str(ni))
+    #     plt.legend()
+    #     plt.show()
             
 
 if __name__ == "__main__":

gpaw/xc/wda/Zis.py
@@ -2,27 +2,22 @@
 # i.e. convolution of density with G_i
 
 
-def calc_Z_ig(n_g, gd, nb_i):
+def calc_Z_ig(n_g, Gs_iK): # gd, nb_i):
     import numpy as np
     from splines import build_splines
     from convolutions import npp_conv
     from ffts import fftn, ifftn
 
-    K_k = get_K_K(gd)
+    # K_k = get_K_K(gd)
     # assert np.allclose(np.min(K_k), 0)
     # kmax = np.max(K_k)
-    Gs_i, _, _, _ = build_splines(nb_i, gd)
+    # Gs_i, _, _, _ = build_splines(nb_i, gd)
 
-    Gs_iK = np.array([Gs(K_k) for Gs in Gs_i])
+    # Gs_iK = np.array([Gs(K_k) for Gs in Gs_i])
     
     n_K = fftn(n_g)
     Z_ig = ifftn(npp_conv(Gs_iK, n_K))
+    
+    # assert np.allclose(Z_ig[-1], n_g), f"MAE: {np.mean(np.abs(Z_ig[-1], n_g))}"
 
     return Z_ig    
-
-
-def get_K_K(gd):
-    from gpaw.utilities.tools import construct_reciprocal
-    K2_K, _ = construct_reciprocal(gd)
-    K2_K[0, 0, 0] = 0
-    return K2_K**(1 / 2)

gpaw/xc/wda/splines.py
@@ -9,75 +9,118 @@ import numpy as np
 
 def build_splines(nb_i, gd):
     from scipy.interpolate import interp1d
-    from gpaw.utilities.tools import construct_reciprocal
-    K_K, _ = construct_reciprocal(gd)
-    K_K = np.sqrt(K_K)
-    K_K[0,0,0] = 0
-    kmax = 1.2 * np.max(K_K)
-    dr = 0.001
-    rmax = 1
-    r_j = np.arange(dr / 2, 10, dr)
-    nks = 1000
-    k_k = np.exp(np.linspace(0, np.log(kmax), nks)) - 1
-    assert np.allclose(np.min(k_k), 0)
+    from scipy.integrate import quad
+    from ks import get_K_K
+    from gpaw.atom.radialgd import fsbt
     na = np.newaxis
-    r_ikj = r_j[na, na, :]
-    k_ikj = k_k[na, :, na]
-    C_i, dC_i = C(nb_i)
-    C_ikj = C_i[:, na, na]
-    dC_ikj = dC_i[:, na, na]
-    lambd_i, dlambd_i = lambd(nb_i)
-    lambd_ikj = lambd_i[:, na, na]
-    dlambd_ikj = dlambd_i[:, na, na]
-    G_ikj = C_ikj * (1 - np.exp(-(lambd_ikj / r_ikj)**5))
+    C_i, dC_i = get_C(nb_i)
+    # C_ikj = C_i[:, na, na]
+    # dC_ikj = dC_i[:, na, na]
 
-    integrand_ikj = 4 * np.pi * np.sinc(k_ikj * r_ikj) * r_ikj**2 \
-                    * G_ikj
-    integral_ik = np.sum(integrand_ikj * dr, axis=-1)
-    # Gsplines_i = interp1d(k_k, integral_ik, kind='cubic') Can also do this
-    interf = lambda k, G: interp1d(k, G,
-                                   kind='cubic', fill_value='extrapolate')
-    Gsplines_i = [interf(k_k, G_k) for G_k in integral_ik]
+    lambd_i, dlambd_i = get_lambd(nb_i)
+    # lambd_ikj = lambd_i[:, na, na]
+    # dlambd_ikj = dlambd_i[:, na, na]
+    Gsplines_i = []
+    Grsplines_i = []
 
+    for i, nb in enumerate(nb_i):
+        C, dC = get_C(nb)
+        lambd, dlambd = get_lambd(nb)
+        K_K = get_K_K(gd)
+        kmax = 1.2 * np.max(K_K)
+
+        dr = 0.01
+        rmax = lambd * 300
+        r_j = np.arange(dr, rmax, dr)
+
+        nks = 900
+        k_k = np.exp(np.linspace(0, np.log(kmax), nks)) - 1
+        # k_k = np.linspace(0, kmax, nks)
+
+        assert np.allclose(np.min(k_k), 0)
+
+        r_kj = r_j[na, :]
+        k_kj = k_k[:, na]
+
+        # def Gintegrand(r, k):
+        #     return C * (1 - np.exp(- (lambd / r)**5)) * 4 * np.pi * np.sinc(k * r) * r**2
+
+        # integral_k = np.zeros_like(k_k)
+        # for ik, k in enumerate(k_k):
+        #     inter = lambda r : Gintegrand(r, k)
+        #     integral_k[ik] = quad(inter, 0, np.inf, limit=10)[0]
+        
+        # G_kj = C * (1 - np.exp(-(lambd / r_kj)**5))
+
+
+        G_j = C * (1 - np.exp(- (lambd / r_j)**5))
+        integral_k = 2 * np.pi * fsbt(0, G_j, r_j, k_k)
+        
+        # integrand_kj = (4 * np.pi * np.sinc(k_kj * r_kj) * r_kj**2 
+        # * G_kj)
+        # integral_k = np.sum(integrand_kj * dr, axis=-1)
+        # integral_k = np.trapz(integrand_kj, r_kj)
+        # Gsplines_i = interp1d(k_k, integral_ik, kind='cubic') Can also do this
+        # print(f"integral_k[0] * nb: {integral_k[0] * nb}")
+        interf = lambda k, G: interp1d(k, G,
+                                       kind='linear')#, fill_value='extrapolate')
+        Gspline = interf(k_k, integral_k)
+        assert np.allclose(Gspline(k_k), integral_k)
+        # integrand_kj = 4 * np.pi * r_kj \
+        #                 * np.sinc(k_kj * r_kj) * G_kj
+        # integral_k = np.sum(integrand_kj * dr, axis=-1)
+
+        # def Grintegrand(r, k):
+        #     return C * (1 - np.exp(- (lambd / r)**5)) * 4 * np.pi * np.sinc(k * r) * r
+
+        # integral_k = np.zeros_like(k_k)
+        # for ik, k in enumerate(k_k):
+        #     inter = lambda r : Grintegrand(r, k)
+        #     integral_k[ik] = quad(inter, 0, np.inf, limit=10)[0]
+
+        Gr_j = C * (1 - np.exp(- (lambd / r_j)**5)) / r_j
+
+        integral_k = 2 * np.pi * fsbt(0, Gr_j, r_j, k_k)
+
+        Grspline = interf(k_k, integral_k)
+
+        Gsplines_i.append(Gspline)
+        Grsplines_i.append(Grspline)
+
+    return Gsplines_i, Grsplines_i, None, None
+
+    # dG_ikj = dC_ikj * (1 - np.exp(-(lambd_ikj / r_ikj)**5)) \
+    #          + C_ikj * dlambd_ikj * (5 * lambd_ikj**4 / r_ikj**5) \
+    #          * np.exp(-(lambd_ikj / r_ikj)**5)
+    # integrand_ikj = 4 * np.pi * r_ikj**2 \
+    #                 * np.sinc(k_ikj * r_ikj) * dG_ikj
+    # integral_ik = np.sum(integrand_ikj * dr, axis=-1)
+    # dGsplines_i = [interf(k_k, dG_k) for dG_k in integral_ik]
+
+    # integrand_ikj = 4 * np.pi * r_ikj \
+    #                 * np.sinc(k_ikj * r_ikj) * dG_ikj
+    # integral_ik = np.sum(integrand_ikj * dr, axis=-1)
+    # dGrsplines_i = [interf(k_k, dG_k) for dG_k in integral_ik]
+
+    # return Gsplines_i, Grsplines_i, dGsplines_i, dGrsplines_i
     
 
-    integrand_ikj = 4 * np.pi * r_ikj \
-                    * np.sinc(k_ikj * r_ikj) * G_ikj
-    integral_ik = np.sum(integrand_ikj * dr, axis=-1)
-    Grsplines_i = [interf(k_k, G_k) for G_k in integral_ik]
-
-    dG_ikj = dC_ikj * (1 - np.exp(-(lambd_ikj / r_ikj)**5)) \
-             + C_ikj * dlambd_ikj * (5 * lambd_ikj**4 / r_ikj**5) \
-             * np.exp(-(lambd_ikj / r_ikj)**5)
-    integrand_ikj = 4 * np.pi * r_ikj**2 \
-                    * np.sinc(k_ikj * r_ikj) * dG_ikj
-    integral_ik = np.sum(integrand_ikj * dr, axis=-1)
-    dGsplines_i = [interf(k_k, dG_k) for dG_k in integral_ik]
-
-    integrand_ikj = 4 * np.pi * r_ikj \
-                    * np.sinc(k_ikj * r_ikj) * dG_ikj
-    integral_ik = np.sum(integrand_ikj * dr, axis=-1)
-    dGrsplines_i = [interf(k_k, dG_k) for dG_k in integral_ik]
-
-    return Gsplines_i, Grsplines_i, dGsplines_i, dGrsplines_i
-    
-
-def lambd(n):
+def get_lambd(n):
     from scipy.special import gamma
     exc = get_lda_xc(n, 0)
-    dn = 0.000001
+    dn = 0.00000001
     dexc = (get_lda_xc(n + dn, 0) - exc) / dn
     
-    lambd = - 3 * gamma(3 / 4) / (2 * gamma(2 / 5) * exc)
+    lamb = - 3 * gamma(3 / 4) / (2 * gamma(2 / 5) * exc)
 
     dlambd = 3 * gamma(3 / 4) / (2 * gamma(2/5)) \
              * (1 / exc**2 * dexc)
-    return lambd, dlambd
+    return lamb, dlambd
 
 
-def C(n):
+def get_C(n):
     from scipy.special import gamma
-    lamb, dlambd = lambd(n)
+    lamb, dlambd = get_lambd(n)
     C = - 3 / (4 * np.pi * gamma(2 / 5) * n * lamb**3)
 
     dC = 3 / (4 * np.pi * gamma(2 / 5)) \
@@ -86,10 +129,11 @@ def C(n):
 
 def get_lda_xc(n, spin):
     if np.allclose(n, 0):
+        raise ValueError
         return 0
 
     from gpaw.xc.lda import lda_c, lda_x
-    narr = n # np.array([n]).astype(np.float64)
+    narr = np.array([n]) # np.array([n]).astype(np.float64)
     earr = np.zeros_like(narr) # (narr.shape[1:])
     varr = np.zeros_like(narr)
 
@@ -97,5 +141,5 @@ def get_lda_xc(n, spin):
     zeta = 0
     lda_c(spin, earr, narr, varr, zeta)
 
-    return earr[0] / n
+    return earr / n
 

gpaw/xc/wda/wdaenergy.py
@@ -10,10 +10,10 @@ def wda_energy(n_g, alpha_ig, Grs_ik):
     from ffts import fftn, ifftn
     
     n_k = fftn(n_g)
-    res = ifftn(npp_conv(Grs_ik, n_k))
-    assert res.shape == alpha_ig.shape
+    res_ig = ifftn(npp_conv(Grs_ik, n_k))
+    assert res_ig.shape == alpha_ig.shape
 
-    result_g = np.sum(res * n_g[np.newaxis, :] * alpha_ig, axis=0)
+    result_g = np.sum(res_ig * n_g[np.newaxis, :] * alpha_ig, axis=0)
 
     return result_g
     

gpaw/xc/wda/wdapot.py
@@ -17,11 +17,11 @@ def V1(n_g, alpha_ig, Grs_ik):
 def V1p(n_g, alpha_ig, Grs_ik):
     from ffts import fftn, ifftn
     from convolutions import npp_conv
-    assert np.allclose(alpha_ig.sum(axis=0), 1)
 
     f_k = fftn(n_g[np.newaxis, :] * alpha_ig)
     res_ig = ifftn(npp_conv(Grs_ik, f_k))
     assert res_ig.shape == alpha_ig.shape
+
     return res_ig.sum(axis=0)
 
 
@@ -36,28 +36,50 @@ def V2(n_g, alpha_ig, Z_ig, Grs_ik, Gs_ik):
     xs, ys, zs = np.meshgrid(range(nx), range(ny), range(nz), indexing='ij')
     assert np.allclose(alpha_ig[cross_index_g - 1, xs, ys, zs], 0)
     assert np.allclose(alpha_ig[cross_index_g + 2, xs, ys, zs], 0)
-    assert not np.allclose(alpha_ig[cross_index_g, xs, ys, zs], 0)
-    
-    dZ_g = Z_ig[cross_index_g + 1, xs, ys, zs] - Z_ig[cross_index_g, xs, ys, zs]
-    dG_k = Gs_ik[cross_index_g + 1, xs, ys, zs] - Gs_ik[cross_index_g, xs, ys, zs]
+    assert not np.isclose(alpha_ig[cross_index_g, xs, ys, zs], 0).any()
 
+    dZ_ig = np.roll(Z_ig, -1, axis=0) - Z_ig
     n_k = fftn(n_g)
-    Gnconv_g = ifftn(npp_conv(Grs_ik[cross_index_g, xs, ys, zs], n_k))
+    Gnconv_ig = fftn(npp_conv(Grs_ik, n_k))
+    res = ifftn(npp_conv(np.roll(Gs_ik, -1, axis=0), fftn(Gnconv_ig * n_g[np.newaxis, :] / dZ_ig)))
+
+    res2 = ifftn(npp_conv(np.roll(Gs_ik, -1, axis=0) - Gs_ik, fftn(Gnconv_ig * n_g[np.newaxis, :] * alpha_ig / dZ_ig)))
+    
+    result = ((res - res2) * (alpha_ig != 0)).sum(axis=0)
+    
+    return result
+                     
+
+    dZ_g = Z_ig[cross_index_g + 1, xs, ys, zs] - Z_ig[cross_index_g, xs, ys, zs]    
+    G_ig = ifftn(Gs_ik)
+    Gr_ig = ifftn(Grs_ik)
+    dG_k = fftn(G_ig[cross_index_g + 1, xs, ys, zs] - G_ig[cross_index_g, xs, ys, zs])
+    dGr_k = fftn(Gr_ig[cross_index_g + 1, xs, ys, zs] - Gr_ig[cross_index_g, xs, ys, zs])
+    G_k = fftn(G_ig[cross_index_g + 1, xs, ys, zs])
+    
+    n_k = fftn(n_g)
+    Gnconv_g = ifftn(npp_conv(dGr_k, n_k))
     assert Gnconv_g.shape == n_g.shape
 
-    res = ifftn(npp_conv(Gs_ik[cross_index_g + 1, xs, ys, zs], fftn(Gnconv_g * n_g / dZ_g)))
+    
+    res = ifftn(npp_conv(G_k,
+                         fftn(Gnconv_g * n_g / dZ_g)))
     res2 = ifftn(npp_conv(dG_k, fftn(Gnconv_g * n_g * alpha_ig[cross_index_g, xs, ys, zs] / dZ_g)))
 
-    result1 = (res - res2).copy()
+    result1 = res - res2
+    return result1 * 0
 
-    Gnconv_g = ifftn(npp_conv(Grs_ik[cross_index_g + 1, xs, ys, zs], n_k))
-    res = ifftn(npp_conv(-Gs_ik[cross_index_g, xs, ys, zs], fftn(Gnconv_g * n_g / dZ_g)))
-    res2 = ifftn(npp_conv(dG_k, fftn(Gnconv_g * n_g * alpha_ig[cross_index_g + 1, xs, ys, zs] / dZ_g)))
+    # Gnconv_g = ifftn(npp_conv(Grs_ik[cross_index_g + 1, xs, ys, zs], n_k))
+    # res = ifftn(npp_conv(-Gs_ik[cross_index_g, xs, ys, zs], fftn(Gnconv_g * n_g / dZ_g)))
+    # res2 = ifftn(npp_conv(dG_k, fftn(Gnconv_g * n_g * alpha_ig[cross_index_g + 1, xs, ys, zs] / dZ_g)))
 
-    result2 = (res - res2).copy()
+    # result2 = (res - res2).copy()
 
-    return result1 + result2
+    # return result1 + result2
 
 
 def V(n_g, alpha_ig, Z_ig, Grs_ik, Gs_ik):
-    return V1(n_g, alpha_ig, Grs_ik) + V1p(n_g, alpha_ig, Grs_ik) + V2(n_g, alpha_ig, Z_ig, Grs_ik, Gs_ik)
+    return (V1(n_g, alpha_ig, Grs_ik) 
+            + V1p(n_g, alpha_ig, Grs_ik)
+            + V2(n_g, alpha_ig, Z_ig, Grs_ik, Gs_ik)
+            )