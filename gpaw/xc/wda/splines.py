42acd122d75f00c10cf9e878888d531f6c291c6b
Asbjorn Rasmussen <asbra@fysik.dtu.dk>
2020-01-07 15:47:44 +0100

    WDA

--- gpaw/xc/wda/splines.py
+++ gpaw/xc/wda/splines.py
@@ -1,12 +1,5 @@
 import numpy as np
 
-
-
-# Write function that returns
-# [spline(G_i)], [spline(G_i/r)]
-# [spline(dG_i)], [spline(dG_i/r)]
-
-
 def build_splines(nb_i, gd):
     from scipy.interpolate import interp1d
     from scipy.integrate import quad
@@ -14,12 +7,8 @@ def build_splines(nb_i, gd):
     from gpaw.atom.radialgd import fsbt
     na = np.newaxis
     C_i, dC_i = get_C(nb_i)
-    # C_ikj = C_i[:, na, na]
-    # dC_ikj = dC_i[:, na, na]
 
     lambd_i, dlambd_i = get_lambd(nb_i)
-    # lambd_ikj = lambd_i[:, na, na]
-    # dlambd_ikj = dlambd_i[:, na, na]
     Gsplines_i = []
     Grsplines_i = []
 
@@ -35,48 +24,16 @@ def build_splines(nb_i, gd):
 
         nks = 900
         k_k = np.exp(np.linspace(0, np.log(kmax), nks)) - 1
-        # k_k = np.linspace(0, kmax, nks)
 
         assert np.allclose(np.min(k_k), 0)
-
-        # r_kj = r_j[na, :]
-        # k_kj = k_k[:, na]
-
-        # def Gintegrand(r, k):
-        #     return C * (1 - np.exp(- (lambd / r)**5)) * 4 * np.pi * np.sinc(k * r) * r**2
-
-        # integral_k = np.zeros_like(k_k)
-        # for ik, k in enumerate(k_k):
-        #     inter = lambda r : Gintegrand(r, k)
-        #     integral_k[ik] = quad(inter, 0, np.inf, limit=10)[0]
-        
-        # G_kj = C * (1 - np.exp(-(lambd / r_kj)**5))
-
 
         G_j = C * (1 - np.exp(- (lambd / r_j)**5))
         integral_k = 2 * np.pi * fsbt(0, G_j, r_j, k_k)
         
-        # integrand_kj = (4 * np.pi * np.sinc(k_kj * r_kj) * r_kj**2 
-        # * G_kj)
-        # integral_k = np.sum(integrand_kj * dr, axis=-1)
-        # integral_k = np.trapz(integrand_kj, r_kj)
-        # Gsplines_i = interp1d(k_k, integral_ik, kind='cubic') Can also do this
-        # print(f"integral_k[0] * nb: {integral_k[0] * nb}")
         interf = lambda k, G: interp1d(k, G,
-                                       kind='cubic')#, fill_value='extrapolate')
+                                       kind='cubic', fill_value='extrapolate')
         Gspline = interf(k_k, integral_k)
         assert np.allclose(Gspline(k_k), integral_k)
-        # integrand_kj = 4 * np.pi * r_kj \
-        #                 * np.sinc(k_kj * r_kj) * G_kj
-        # integral_k = np.sum(integrand_kj * dr, axis=-1)
-
-        # def Grintegrand(r, k):
-        #     return C * (1 - np.exp(- (lambd / r)**5)) * 4 * np.pi * np.sinc(k * r) * r
-
-        # integral_k = np.zeros_like(k_k)
-        # for ik, k in enumerate(k_k):
-        #     inter = lambda r : Grintegrand(r, k)
-        #     integral_k[ik] = quad(inter, 0, np.inf, limit=10)[0]
 
         Gr_j = C * (1 - np.exp(- (lambd / r_j)**5)) / r_j
 
@@ -87,23 +44,7 @@ def build_splines(nb_i, gd):
         Gsplines_i.append(Gspline)
         Grsplines_i.append(Grspline)
 
-    return Gsplines_i, Grsplines_i, None, None
-
-    # dG_ikj = dC_ikj * (1 - np.exp(-(lambd_ikj / r_ikj)**5)) \
-    #          + C_ikj * dlambd_ikj * (5 * lambd_ikj**4 / r_ikj**5) \
-    #          * np.exp(-(lambd_ikj / r_ikj)**5)
-    # integrand_ikj = 4 * np.pi * r_ikj**2 \
-    #                 * np.sinc(k_ikj * r_ikj) * dG_ikj
-    # integral_ik = np.sum(integrand_ikj * dr, axis=-1)
-    # dGsplines_i = [interf(k_k, dG_k) for dG_k in integral_ik]
-
-    # integrand_ikj = 4 * np.pi * r_ikj \
-    #                 * np.sinc(k_ikj * r_ikj) * dG_ikj
-    # integral_ik = np.sum(integrand_ikj * dr, axis=-1)
-    # dGrsplines_i = [interf(k_k, dG_k) for dG_k in integral_ik]
-
-    # return Gsplines_i, Grsplines_i, dGsplines_i, dGrsplines_i
-    
+    return Gsplines_i, Grsplines_i    
 
 def get_lambd(n):
     from scipy.special import gamma
--- gpaw/xc/wda/wdapot.py
+++ gpaw/xc/wda/wdapot.py
@@ -30,14 +30,6 @@ def V2(n_g, alpha_ig, Z_ig, Grs_ik, Gs_ik):
     from ffts import fftn, ifftn
     from convolutions import npp_conv
 
-    # cross_index_g = (alpha_ig != 0).argmax(axis=0)
-    
-    # ni, nx, ny, nz = Z_ig.shape
-    # xs, ys, zs = np.meshgrid(range(nx), range(ny), range(nz), indexing='ij')
-    # assert np.allclose(alpha_ig[cross_index_g - 1, xs, ys, zs], 0)
-    # assert np.allclose(alpha_ig[cross_index_g + 2, xs, ys, zs], 0)
-    # assert not np.isclose(alpha_ig[cross_index_g, xs, ys, zs], 0).any()
-
     dZ_ig = np.roll(Z_ig, -1, axis=0) - Z_ig
     n_k = fftn(n_g)
     Gnconv_ig = fftn(npp_conv(Grs_ik, n_k))
@@ -48,34 +40,6 @@ def V2(n_g, alpha_ig, Z_ig, Grs_ik, Gs_ik):
     result = ((res - res2) * (alpha_ig != 0)).sum(axis=0)
     
     return result
-                     
-
-    # dZ_g = Z_ig[cross_index_g + 1, xs, ys, zs] - Z_ig[cross_index_g, xs, ys, zs]    
-    # G_ig = ifftn(Gs_ik)
-    # Gr_ig = ifftn(Grs_ik)
-    # dG_k = fftn(G_ig[cross_index_g + 1, xs, ys, zs] - G_ig[cross_index_g, xs, ys, zs])
-    # dGr_k = fftn(Gr_ig[cross_index_g + 1, xs, ys, zs] - Gr_ig[cross_index_g, xs, ys, zs])
-    # G_k = fftn(G_ig[cross_index_g + 1, xs, ys, zs])
-    
-    # n_k = fftn(n_g)
-    # Gnconv_g = ifftn(npp_conv(dGr_k, n_k))
-    # assert Gnconv_g.shape == n_g.shape
-
-    
-    # res = ifftn(npp_conv(G_k,
-    #                      fftn(Gnconv_g * n_g / dZ_g)))
-    # res2 = ifftn(npp_conv(dG_k, fftn(Gnconv_g * n_g * alpha_ig[cross_index_g, xs, ys, zs] / dZ_g)))
-
-    # result1 = res - res2
-    # return result1 * 0
-
-    # Gnconv_g = ifftn(npp_conv(Grs_ik[cross_index_g + 1, xs, ys, zs], n_k))
-    # res = ifftn(npp_conv(-Gs_ik[cross_index_g, xs, ys, zs], fftn(Gnconv_g * n_g / dZ_g)))
-    # res2 = ifftn(npp_conv(dG_k, fftn(Gnconv_g * n_g * alpha_ig[cross_index_g + 1, xs, ys, zs] / dZ_g)))
-
-    # result2 = (res - res2).copy()
-
-    # return result1 + result2
 
 
 def V(n_g, alpha_ig, Z_ig, Grs_ik, Gs_ik):
