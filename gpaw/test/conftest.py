9ddc833783aee6797e0ab949c5d089ce1abb493f
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-01-09 15:19:36 +0100

    Add GPAW plugin for pytest

--- gpaw/test/conftest.py
+++ gpaw/test/conftest.py
@@ -8,6 +8,18 @@ def in_tmp_dir(tmpdir):
     cwd = os.getcwd()
     os.chdir(str(tmpdir))
     try:
-        yield
+        yield tmpdir
     finally:
         os.chdir(cwd)
+
+
+class GPAWPlugin:
+    def pytest_terminal_summary(self, terminalreporter, exitstatus, config):
+        from gpaw.mpi import rank, size
+        terminalreporter.section('GPAW-MPI stuff')
+
+        terminalreporter.write(f'rank, size: {rank}, {size}')
+
+
+def pytest_configure(config):
+    config.pluginmanager.register(GPAWPlugin(), 'pytest_gpaw')
