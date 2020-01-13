d1e9feee6a793ad9163b39bc234b899d9e668b01
Asbjorn Rasmussen <asbra@fysik.dtu.dk>
2020-01-13 12:10:14 +0100

    optimization of get alphas

--- gpaw/xc/wda.py
+++ gpaw/xc/wda.py
@@ -2,6 +2,7 @@ from gpaw.xc.functional import XCFunctional
 from gpaw import mpi
 import numpy as np
 
+
 def timer(f):
     from functools import wraps
     from time import time
@@ -14,6 +15,7 @@ def timer(f):
             print("Running {} took {} seconds".format(f.__name__, t2 - t1))
         return res
     return wrapped
+
 
 class WDA(XCFunctional):
     def __init__(self):
@@ -88,7 +90,7 @@ class WDA(XCFunctional):
         return Gs_ik, Grs_ik
 
     @timer
-    def calc_Z_isg(self, n_sg, Gs_iK):        
+    def calc_Z_isg(self, n_sg, Gs_iK):
         assert len(n_sg.shape) == 4
         n_sK = self.fftn(n_sg)
         na = np.newaxis
@@ -100,9 +102,9 @@ class WDA(XCFunctional):
         return Z_isg
         
     def npp_conv(self, np_xK, p_xK):
-        assert np_xK.shape[-3:] == p_xK.shape[-3:], '{} - {} - {}'.format(np_xK.shape, p_xK.shape, self.gd.n_c)
+        b = np_xK.shape[-3:] == p_xK.shape[-3:]
+        assert b, '{} - {} - {}'.format(np_xK.shape, p_xK.shape, self.gd.n_c)
         return np_xK * p_xK
-
 
     def pp_conv(self, p1_xK, p2_xK, volume):
         assert p1_xK.shape[-3:] == p2_xK.shape[-3:]
@@ -112,7 +114,7 @@ class WDA(XCFunctional):
     def fftn(self, n_g):
         N = np.prod(n_g.shape[-3:])
 
-        return np.fft.fftn(n_g, axes=(-3, -2, -1))  / N
+        return np.fft.fftn(n_g, axes=(-3, -2, -1)) / N
 
     def ifftn(self, n_g):
         N = np.prod(n_g.shape[-3:])
@@ -123,22 +125,28 @@ class WDA(XCFunctional):
         return res.real
 
     @timer
-    def get_alphas(self, Z_ig):
-        Z_ri = Z_ig.reshape(len(Z_ig), -1).T
+    def get_alphas(self, Z_isg):
+        # Assumptions: Z_isg is monotonically increasing
+        # at all s, g
+        # Z_isg crosses -1 at all s, g
 
-        alpha_ri = np.zeros_like(Z_ri)
-        for ir, Z_i in enumerate(Z_ri):
-            for ii, Z in enumerate(Z_i[:-1]):
-                did_cross = (Z_i[ii] <= -1 and Z_i[ii + 1] > -1) \
-                            or (Z_i[ii] > -1 and Z_i[ii + 1] <= -1)
-                if did_cross:
-                    alpha_ri[ir, ii] = (Z_i[ii + 1] - (-1)) / (Z_i[ii + 1] - Z_i[ii])
-                    alpha_ri[ir, ii + 1] = ((-1) - Z_i[ii]) / (Z_i[ii + 1] - Z_i[ii])
-                    break
+        fg_g = np.argmax(Z_isg > -1, axis=0)
+        ll_g = fg_g - 1
 
+        lengths = Z_isg.shape[-4:]
+        arrs = [range(x) for x in lengths]
 
-        res = alpha_ri.T.reshape(Z_ig.shape)    
-        return res
+        S, X, Y, Z = np.meshgrid(*arrs, indexing='ij')
+
+        alpha_isg = np.zeros_like(Z_isg)
+        alpha_isg[ll_g, S, X, Y, Z] = ((Z_isg[fg_g, S, X, Y, Z] - (-1))
+                                       / (Z_isg[fg_g, S, X, Y, Z]
+                                          - Z_isg[ll_g, S, X, Y, Z]))
+        alpha_isg[fg_g, S, X, Y, Z] = (((-1) - Z_isg[ll_g, S, X, Y, Z])
+                                       / (Z_isg[fg_g, S, X, Y, Z]
+                                          - Z_isg[ll_g, S, X, Y, Z]))
+
+        return alpha_isg
 
     @timer
     def wda_energy(self, n_g, alpha_ig, Grs_ik):
@@ -148,7 +156,9 @@ class WDA(XCFunctional):
         n_k = self.fftn(n_g)
         na = np.newaxis
         res_ig = self.ifftn(self.npp_conv(Grs_ik[:, na, :, :, :], n_k))
-        assert res_ig.shape == alpha_ig.shape, "{}-{}-{}-{}".format(res_ig.shape, alpha_ig.shape, n_g.shape, Grs_ik.shape)
+        b = res_ig.shape == alpha_ig.shape
+        assert b, "{}-{}-{}-{}".format(res_ig.shape,
+                                       alpha_ig.shape, n_g.shape, Grs_ik.shape)
 
         result_g = np.sum(res_ig * n_g[np.newaxis, :] * alpha_ig, axis=0)
 
@@ -159,7 +169,8 @@ class WDA(XCFunctional):
         n_k = self.fftn(n_g)
     
         res_ig = self.ifftn(self.npp_conv(Grs_ik, n_k))
-        assert res_ig.shape == alpha_ig.shape, "{} - {}".format(res_ig.shape, alpha_ig.shape)
+        b = res_ig.shape == alpha_ig.shape
+        assert b, "{} - {}".format(res_ig.shape, alpha_ig.shape)
 
         result_g = np.sum(res_ig * alpha_ig, axis=0)
         return result_g
@@ -177,9 +188,15 @@ class WDA(XCFunctional):
         dZ_ig = np.roll(Z_ig, -1, axis=0) - Z_ig
         n_k = self.fftn(n_g)
         Gnconv_ig = self.fftn(self.npp_conv(Grs_ik, n_k))
-        res = self.ifftn(self.npp_conv(np.roll(Gs_ik, -1, axis=0), self.fftn(Gnconv_ig * n_g[np.newaxis, :] / dZ_ig)))
+        res = self.ifftn(
+            self.npp_conv(
+                np.roll(Gs_ik, -1, axis=0),
+                self.fftn(Gnconv_ig * n_g[np.newaxis, :] / dZ_ig)))
         
-        res2 = self.ifftn(self.npp_conv(np.roll(Gs_ik, -1, axis=0) - Gs_ik, self.fftn(Gnconv_ig * n_g[np.newaxis, :] * alpha_ig / dZ_ig)))
+        res2 = self.ifftn(
+            self.npp_conv(
+                np.roll(Gs_ik, -1, axis=0) - Gs_ik,
+                self.fftn(Gnconv_ig * n_g[np.newaxis, :] * alpha_ig / dZ_ig)))
         
         result = ((res - res2) * (alpha_ig != 0)).sum(axis=0)
     
@@ -189,7 +206,7 @@ class WDA(XCFunctional):
     def V(self, n_g, alpha_ig, Z_ig, Grs_ik, Gs_ik):
         Gs_ik = Gs_ik[:, np.newaxis, :, :, :]
         Grs_ik = Grs_ik[:, np.newaxis, :, :, :]
-        return (self.V1(n_g, alpha_ig, Grs_ik) 
+        return (self.V1(n_g, alpha_ig, Grs_ik)
                 + self.V1p(n_g, alpha_ig, Grs_ik)
                 + self.V2(n_g, alpha_ig, Z_ig, Grs_ik, Gs_ik))
 
