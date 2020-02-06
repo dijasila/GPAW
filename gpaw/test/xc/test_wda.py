d6f7d9f2af6ed5e95c278b0b9f96b156ba8b5113
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-02-06 10:43:14 +0100

    Small changes I forgot to commit

--- gpaw/test/xc/test_wda.py
+++ gpaw/test/xc/test_wda.py
@@ -7,16 +7,21 @@ from gpaw.xc import XC
 
 def test_wda():
     a = 2.5
-    n = 4
+    n = 8
     gd = GridDescriptor((n, n, n), (a, a, a))
-    d = gd.zeros()[np.newaxis]
-    v = gd.zeros()[np.newaxis]
-    e = gd.zeros()
-    d[:] = 0.1
+    d = gd.empty()[np.newaxis]
+    v = gd.empty()[np.newaxis]
+    e = gd.empty()
+    d[:] = 0.123
     xc = XC('LDA')
+    v[:] = 0.0
     elda = xc.calculate(gd, d, v, e)
+    print(v[0, 0, 0])
     xc = XC('WDA')
+    # xc = XC('PBE')
+    v[:] = 0.0
     ewda = xc.calculate(gd, d, v, e)
+    print(v[0, 0, 0])
     print(e[0, 0], e[0, 0, 0] * a**3)
     print(elda, ewda)
     assert ewda == pytest.approx(elda)
