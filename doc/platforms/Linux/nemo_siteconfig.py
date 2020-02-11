b357e86acc05da892edf0a56ea94e412e2ca7e73
Wafa Maftuhin <wafamaftukhin@gmail.com>
2020-02-11 17:37:51 +0100

    DOC: updated nemo_siteconfig.py

--- doc/platforms/Linux/nemo_siteconfig.py
+++ doc/platforms/Linux/nemo_siteconfig.py
@@ -3,4 +3,3 @@ libraries += ['mkl_intel_lp64', 'mkl_sequential', 'mkl_core', 'mkl_avx2', 'mkl_d
 # Now add a EasyBuild "cover-all-bases" library_dirs
 library_dirs = os.getenv('LD_LIBRARY_PATH').split(':')
 include_dirs = os.getenv('INCLUDE').split(':')
-
