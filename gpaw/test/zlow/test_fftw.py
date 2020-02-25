d782dfd2fd924707fe43a258d55a153d62f857ab
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-02-25 09:31:42 +0100

    FFTW ...

--- gpaw/test/zlow/test_fftw.py
+++ gpaw/test/zlow/test_fftw.py
@@ -1,6 +1,12 @@
 import time
 
+import pytest
+
 import gpaw.fftw as fftw
+
+N = 1
+# n1, n2, n3 = 32, 28, 128
+n1, n2, n3 = 8, 6, 12
 
 
 def check(Plan, flags, input, output, sign):
@@ -8,22 +14,22 @@ def check(Plan, flags, input, output, sign):
     plan = Plan(input, output, sign, flags)
     t1 = time.time()
     t = 0.0
-    for i in range(100):
+    for i in range(N):
         input[:] = 1.3
         t2 = time.time()
         plan.execute()
         t3 = time.time()
         t += t3 - t2
-    return t1 - t0, t / 100
+    return t1 - t0, t / N
 
 
-skipif
+@pytest.mark.skipif(not fftw.have_fftw(), reason='No FFTW')
 def test_fft():
-    a1 = fftw.empty((32, 28, 128), complex)
-    a2 = fftw.empty((32, 28, 128), complex)
-    b = fftw.empty((32, 28, 65), complex)
-    c1 = b.view(dtype=float)[:, :, :128]
-    c2 = fftw.empty((32, 28, 64), complex).view(dtype=float)
+    a1 = fftw.empty((n1, n2, n3), complex)
+    a2 = fftw.empty((n1, n2, n3), complex)
+    b = fftw.empty((n1, n2, n3 // 2 + 1), complex)
+    c1 = b.view(dtype=float)[:, :, :n3]
+    c2 = fftw.empty((n1, n2, n3 // 2), complex).view(dtype=float)
     for input, output, sign in [
         (a1, a1, -1),
         (a1, a2, -1),
