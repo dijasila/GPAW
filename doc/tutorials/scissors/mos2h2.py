0c3d084ea2b9b78cd547ac30756fa0db1f16617b
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-03-17 10:53:30 +0100

    Add example

---
+++ doc/tutorials/scissors/mos2h2.py
@@ -0,0 +1,40 @@
+from ase import Atoms
+from ase.build import mx2
+from gpaw import GPAW
+from gpaw.lcao.scissors import Scissors
+
+d = 3.0
+a12 = mx2()
+a12 += Atoms('H2', positions=[(0, 0, d), (0, 0, d + 0.74)])
+a12.center(vacuum=3.0, axis=2)
+
+a1 = a12[:3]
+a2 = a12[3:]
+
+a1.calc = GPAW(mode='lcao',
+               basis='sz(dzp)',
+               kpts=(3, 3, 1),
+               txt='1.txt')
+a1.get_potential_energy()
+
+a2.calc = GPAW(mode='lcao',
+               basis='sz(dzp)',
+               kpts=(3, 3, 1),
+               txt='2.txt')
+a2.get_potential_energy()
+
+a12.calc = GPAW(mode='lcao',
+                basis='sz(dzp)',
+                kpts=(3, 3, 1),
+                txt='12.txt')
+a12.get_potential_energy()
+a12.calc.write('12.gpw')
+
+a12.calc = GPAW(mode='lcao',
+                basis='sz(dzp)',
+                kpts=(3, 3, 1),
+                eigensolver=Scissors([(-0.5, 0.5, a1.calc),
+                                      (1.0, -1.0, a2.calc)]),
+                txt='12s.txt')
+a12.get_potential_energy()
+a12.calc.write('12s.gpw')
