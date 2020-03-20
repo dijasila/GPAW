871f31a72196d5047fb49f33042c69d2a26f9261
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-03-20 17:06:40 +0100

    Refactor eigenvalues ...

--- gpaw/hybrids/eigenvalues.py
+++ gpaw/hybrids/eigenvalues.py
@@ -1,19 +1,19 @@
 import json
 from pathlib import Path
-from typing import List, Union, Tuple
+from typing import List, Union, Tuple, Generator
 
 import numpy as np
 from ase.units import Ha
 
 from gpaw import GPAW
+from gpaw.xc import XC
 from gpaw.xc.tools import vxc
-from .hybrid import HybridXC
 
 
 def non_self_consistent_eigenvalues(calc: Union[GPAW, str, Path],
                                     xcname: str,
                                     n1=0,
-                                    n2=-1,
+                                    n2=0,
                                     kpts: List[int] = None,
                                     restart=None,
                                     ftol=1e-9) -> np.ndarray:
@@ -37,15 +37,12 @@ def non_self_consistent_eigenvalues(calc: Union[GPAW, str, Path],
 
     wfs = calc.wfs
 
-    if n2 == -1:
-        n2 = wfs.bd.nbands
+    if n2 <= 0:
+        n2 += wfs.bd.nbands
 
     if kpts is None:
         kpts = np.arange(wfs.kd.nibzkpts)
 
-    nocc = max(((kpt.f_n / kpt.weight) > ftol).sum()
-               for kpt in wfs.mykpts)
-    nspins = wfs.nspins
     nkpts = len(kpts)
 
     dct = {}
@@ -54,21 +51,15 @@ def non_self_consistent_eigenvalues(calc: Union[GPAW, str, Path],
         if path.is_file():
             dct = json.loads(path.read_text())
 
-    xc = HybridXC(xcname)
+    xcname, exx_fraction, omega = parse_name(xcname)
+    xc = XC(xcname)
 
     if 'v_dft_sin' in dct:
         v_dft_sin = np.array(dct['v_dft_sin'])
         e_dft_sin = np.array(dct['e_dft_sin'])
         v_hyb_sl_sin = np.array(dct['v_hyb_sl_sin'])  # semi-local part
     else:
-        v_dft_sin = vxc(calc)[:, kpts, n1:n2]
-        e_dft_sin = np.array([[calc.get_eigenvalues(k, spin)[n1:n2]
-                               for k in kpts]
-                              for spin in range(nspins)])
-        if xc.xc:
-            v_hyb_sl_sin = vxc(calc, xc.xc)[:, kpts, n1:n2]
-        else:
-            v_hyb_sl_sin = np.zeros_like(v_dft_sin)
+        e_dft_sin, v_dft_sin, v_hyb_sl_sin = _semi_local(xc, calc)
         if wfs.world.rank == 0 and restart:
             dct = {'v_dft_sin': v_dft_sin.tolist(),
                    'e_dft_sin': e_dft_sin.tolist(),
@@ -85,33 +76,23 @@ def non_self_consistent_eigenvalues(calc: Union[GPAW, str, Path],
         i0 = 0
 
     if i0 < nkpts:
-        xc.initialize_from_calculator(calc)
-        xc.set_positions(calc.spos_ac)
-        xc._initialize()
-        VV_saii = [xc.calculate_valence_valence_paw_corrections(spin)
-                   for spin in range(nspins)]
-
-        for i, k in enumerate(kpts[i0:], i0):
-            for spin in range(nspins):
-                v_n = xc.calculate_eigenvalue_contribution(
-                    spin, k, n1, n2, nocc, VV_saii[spin]) * Ha
-                v_hyb_nl_sin[spin, i] = v_n
+        i = i0
+        for v_hyb_nl_sn in _non_local(calc, xc, n1, n2, kpts[i0:], ftol):
+            v_hyb_nl_sin[:, i] = v_hyb_nl_sn
+            i += 1
             if wfs.world.rank == 0 and restart:
-                dct = {'v_dft_sin': v_dft_sin.tolist(),
-                       'e_dft_sin': e_dft_sin.tolist(),
-                       'v_hyb_sl_sin': v_hyb_sl_sin.tolist(),
-                       'v_hyb_nl_sin': v_hyb_nl_sin[:, :i + 1].tolist()}
+                dct['v_hyb_nl_sin'] = v_hyb_nl_sin[:, :i].tolist()
                 path.write_text(json.dumps(dct))
 
     return e_dft_sin, v_dft_sin, v_hyb_sl_sin + v_hyb_nl_sin
 
 
-def _semi_local_eigenvalues(calc: GPAW,
-                            xc,
-                            n1: int,
-                            n2: int,
-                            kpts: List[int]
-                            ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+def _semi_local(calc: GPAW,
+                xc,
+                n1: int,
+                n2: int,
+                kpts: List[int]
+                ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     wfs = calc.wfs
     nspins = wfs.nspins
 
@@ -119,30 +100,26 @@ def _semi_local_eigenvalues(calc: GPAW,
                            for k in kpts]
                           for spin in range(nspins)])
     v_dft_sin = vxc(calc)[:, kpts, n1:n2]
-    if xc.xc:
-        v_hyb_sl_sin = vxc(calc, xc.xc)[:, kpts, n1:n2]
-    else:
-        v_hyb_sl_sin = np.zeros_like(v_dft_sin)
+    v_hyb_sl_sin = vxc(calc, xc)[:, kpts, n1:n2]
     return e_dft_sin, v_dft_sin, v_hyb_sl_sin
 
 
-def _non_local_eigenvalues(calc: GPAW,
-                           xc,
-                           n1: int,
-                           n2: int,
-                           kpts: List[int],
-                           ftol: float
-                           ) -> Generator[np.ndarray, None, None]:
+def _non_local(calc: GPAW,
+               xc,
+               n1: int,
+               n2: int,
+               kpts: List[int],
+               ftol: float
+               ) -> Generator[np.ndarray, None, None]:
     wfs = calc.wfs
     nocc = max(((kpt.f_n / kpt.weight) > ftol).sum()
                for kpt in wfs.mykpts)
     nspins = wfs.nspins
 
-    xc.initialize_from_calculator(calc)
-    xc.set_positions(calc.spos_ac)
-    xc._initialize()
-    VV_saii = [xc.calculate_valence_valence_paw_corrections(spin)
-               for spin in range(nspins)]
+    coulomb = coulomb_inteaction(omega, wfs.gd, kd)
+    sym = Symmetry(kd)
+
+    VV_saii, VC_aii, Delta_aiiL = calculate_paw_stuff(dens, setups)
 
     for k in kpts:
         for spin in range(nspins):
