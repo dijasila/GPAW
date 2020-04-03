9cbf715f8e61518ce7e3213324b72a2786e6f982
Asbjorn Rasmussen <asbra@fysik.dtu.dk>
2020-04-03 09:40:48 +0200

    optimization

--- gpaw/response/greensfunction.py
+++ gpaw/response/greensfunction.py
@@ -1,9 +1,15 @@
 import numpy as np
+from ase.parallel import parprint
+from time import time
+from math import pi
 
 class GreensFunction:
     def __init__(self, gw_object):
         self.gw_object = gw_object
         self.gw_object.ite = 0
+
+    def _report(self, msg):
+        parprint(f"Greensfunction: {msg}", flush=True)
 
 
     def calculate(self, domegas, k_index=None):
@@ -58,7 +64,10 @@ class GreensFunction:
             mykpts = [kpt for kpt in mykpts if gw.kpts.index(kd.bz2ibz_k[kpt.K]) == k_index]
             print("Number of kpts: ", len(mykpts))
 
-        for ie, pd0, W0, q_c, m2, W0_GW in gw.calculate_screened_potential():
+        self._report("Finished setup")
+        for iterc, (ie, pd0, W0, q_c, m2, W0_GW) in enumerate(gw.calculate_screened_potential()):
+            t1 = time()
+            self._report(f"At ie = {ie} in calc W")
             for u, kpt1 in enumerate(mykpts):
                 K2 = kd.find_k_plus_q(q_c, [kpt1.K])[0]
                 kpt2 = gw.get_k_point(kpt1.s, K2, 0, m2, block=True)
@@ -67,6 +76,10 @@ class GreensFunction:
                 i = gw.kpts.index(k1)
                 self.calculate_q(domegas, ie, i, kpt1, kpt2, pd0, W0, W0_GW)
 
+            t2 = time()
+            self._report(f"Calculation of iterc = {iterc} took {t2 - t1} seconds.")
+            
+        self._report("Finished calculation")
         gw.world.sum(self.sigma_wskn)
         gw.world.sum(self.dsigma_wskn)
 
@@ -130,7 +143,7 @@ class GreensFunction:
             nn = kpt1.n1 + n - gw.bands[0]
 
             W = Ws[0]
-            sigma_w, dsigma_w = self.calculate_sigma(n_mG, deps_wm, f_m, W)
+            sigma_w, dsigma_w = self._calculate_sigma(n_mG, deps_wm, f_m, W, full_sigma=True)
             self.sigma_wskn[:, kpt1.s, k, nn] += sigma_w
             self.dsigma_wskn[:, kpt1.s, k, nn] += dsigma_w
 
@@ -144,4 +157,56 @@ class GreensFunction:
             sigma, dsigma = gw.calculate_sigma(n_mG, deps_m, f_m, C_swGG, full_sigma=True)
             sigma_w[w_index] += -np.real(sigma)*1j + np.imag(sigma)
             dsigma_w[w_index] += dsigma
+        return sigma_w, dsigma_w
+
+    def _calculate_sigma(self, n_mG, deps_wm, f_m, C_swGG, full_sigma=False):
+        # parprint("Using optimized calculate sigma", flush=True)
+        """Calculates a contribution to the self-energy and its derivative for
+        a given (k, k-q)-pair from its corresponding pair-density and
+        energy."""
+        gw = self.gw_object
+        C_swGG = np.array(C_swGG)
+        o_wm = abs(deps_wm)
+        # Add small number to avoid zeros for degenerate states:
+        sgn_wm = np.sign(deps_wm + 1e-15)
+
+        # Pick +i*eta or -i*eta:
+        s_wm = (1 + sgn_wm * np.sign(0.5 - f_m[np.newaxis, :])).astype(int) // 2
+
+        beta = (2**0.5 - 1) * gw.domega0 / gw.omega2
+        w_wm = (o_wm / (gw.domega0 + beta * o_wm)).astype(int)
+        index_inb = np.where(w_wm < len(gw.omega_w) - 1)
+        o1_wm = np.empty(o_wm.shape)
+        o2_wm = np.empty(o_wm.shape)
+        o1_wm[index_inb] = gw.omega_w[w_wm[index_inb]]
+        o2_wm[index_inb] = gw.omega_w[w_wm[index_inb] + 1]
+
+        x = 1.0 / (gw.qd.nbzkpts * 2 * pi * gw.vol)
+        sigma_w = np.zeros(deps_wm.shape[0], dtype=np.complex128)
+        dsigma_w = np.zeros(deps_wm.shape[0], dtype=np.complex128)
+        # Performing frequency integration
+        for o_w, o1_w, o2_w, sgn_w, s_w, w_w, n_G in zip(o_wm.T, o1_wm.T, o2_wm.T,
+                                             sgn_wm.T, s_wm.T, w_wm.T, n_mG):
+
+            # if w >= len(self.omega_w) - 1:
+            #     continue
+
+            prefac_w = np.ones(len(w_w))
+            prefac_w[w_w >= len(gw.omega_w) - 1] = 0
+
+            C1_wGG = C_swGG[s_w, w_w]
+            C2_wGG = C_swGG[s_w, w_w + 1]
+            p_w = x * sgn_w
+            myn_G = n_G[gw.Ga:gw.Gb]
+            
+            if full_sigma:
+                sigma1_w = p_w * np.dot(myn_G,
+                                        (np.dot(C1_wGG, n_G.conj()).T))
+                sigma2_w = p_w * np.dot(myn_G,
+                                        (np.dot(C2_wGG, n_G.conj()).T))
+            else:
+                raise NotImplementedError
+            sigma_w += ((o_w - o1_w) * sigma2_w + (o2_w - o_w) * sigma1_w) / (o2_w - o1_w) * prefac_w
+            dsigma_w += sgn_w * (sigma2_w - sigma1_w) / (o2_w - o1_w) * prefac_w
+
         return sigma_w, dsigma_w
