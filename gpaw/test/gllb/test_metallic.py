0cdbd043dfb8222ec00f60621e6dc5125ad9be97
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2020-05-28 18:55:33 +0300

    Add test for GLLBSCM

--- gpaw/test/gllb/test_metallic.py
+++ gpaw/test/gllb/test_metallic.py
@@ -1,21 +1,40 @@
+import numpy as np
 from ase.build import bulk
 from gpaw import GPAW, PW
 
 
-def test_metallic_gllbsc_fail(in_tmp_dir):
-    repeat = 1
+def run(xc, repeat=1):
     atoms = bulk('Ag') * repeat
     k = 4 // repeat
     calc = GPAW(mode=PW(200),
                 setups={'Ag': '11'},
                 nbands=6 * repeat**3,
-                xc='GLLBSC',
+                xc=xc,
                 kpts={'size': (k, k, k), 'gamma': False},
                 txt='-')
     atoms.calc = calc
+    atoms.get_potential_energy()
+    x_i, y_i = calc.get_dos(npts=1001)
+    y_i /= repeat**3
+    return x_i, y_i
+ 
+
+def test_metallic_GLLBSC(in_tmp_dir):
     try:
-        atoms.get_potential_energy()
+        run(xc='GLLBSC')
     except RuntimeError as e:
         assert 'GLLBSCM' in str(e), 'GLLBSCM not mentioned in the error'
     else:
         raise RuntimeError('GLLBSC should fail for metallic system')
+
+
+def test_metallic_GLLBSCM(in_tmp_dir):
+    x1_i, y1_i = run(xc='GLLBSCM', repeat=1)
+    x2_i, y2_i = run(xc='GLLBSCM', repeat=2)
+    # Test that the DOSes are the same
+    assert np.allclose(x1_i, x2_i, rtol=0, atol=1e-8), \
+           "DOS energies don't match, " \
+           "error = {}".format(np.max(np.abs(x1_i - x2_i)))
+    assert np.allclose(y1_i, y2_i, rtol=0, atol=1e-6), \
+           "DOS values don't match, " \
+           "error = {}".format(np.max(np.abs(y1_i - y2_i)))
