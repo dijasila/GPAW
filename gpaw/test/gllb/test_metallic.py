1c6934b5d93dbd28f1d0cd70b651c02d90953146
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2020-05-29 09:29:53 +0300

    Fix pep8

--- gpaw/test/gllb/test_metallic.py
+++ gpaw/test/gllb/test_metallic.py
@@ -20,7 +20,7 @@ def run(xc, repeat=1):
     x_i -= calc.get_fermi_level()
     y_i /= repeat**3
     return x_i, y_i
- 
+
 
 def test_metallic_GLLBSC(in_tmp_dir):
     try:
@@ -36,8 +36,8 @@ def test_metallic_GLLBSCM(in_tmp_dir):
     x2_i, y2_i = run(xc='GLLBSCM', repeat=2)
     # Test that the DOSes are the same
     assert np.allclose(x1_i, x2_i, rtol=0, atol=1e-8), \
-           "DOS energies don't match, " \
-           "error = {}".format(np.max(np.abs(x1_i - x2_i)))
+        "DOS energies don't match, " \
+        "error = {}".format(np.max(np.abs(x1_i - x2_i)))
     assert np.allclose(y1_i, y2_i, rtol=0, atol=1e-6), \
-           "DOS values don't match, " \
-           "error = {}".format(np.max(np.abs(y1_i - y2_i)))
+        "DOS values don't match, " \
+        "error = {}".format(np.max(np.abs(y1_i - y2_i)))
