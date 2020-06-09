ed75b26f20e14cd180eb6e381af29d9475d43393
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-06-09 12:26:04 +0200

    Fix for fermi-level

--- doc/tutorials/spinorbit/plot_Bi2Se3_bands.py
+++ doc/tutorials/spinorbit/plot_Bi2Se3_bands.py
@@ -1,10 +1,12 @@
 # Creates: Bi2Se3_bands.png
 import numpy as np
 import matplotlib.pyplot as plt
-from gpaw import GPAW
-from gpaw.spinorbit import get_spinorbit_eigenvalues, set_calculator
+from ase.units import Ha
 
-#plt.rc('text', usetex=True)
+from gpaw import GPAW
+from gpaw.spinorbit import soc_eigenstates
+from gpaw.occupations import occupation_numbers
+
 
 calc1 = GPAW('Bi2Se3_bands.gpw', txt=None)
 calc2 = GPAW('gs_Bi2Se3.gpw', txt=None)
@@ -23,20 +25,23 @@ for e_k in e_nk:
     plt.plot(x, e_k, '--', c='0.5')
 
 # Spin-orbit calculation
-
-e_nk = get_spinorbit_eigenvalues(calc2)
-set_calculator(calc2, e_nk.T)
-ef = calc2.get_fermi_level()
-e_nk = get_spinorbit_eigenvalues(calc1, scale=1.0)
-e_nk -= ef
+e_kn = soc_eigenstates(calc2)['e_km']
+_, ef, _, _ = occupation_numbers({'name': 'fermi-dirac', 'width': 0.001},
+                                 e_kn[np.newaxis],
+                                 np.ones(len(e_kn)) / len(e_kn),
+                                 2 * calc2.get_number_of_electrons())
+ef *= Ha
+e_kn = soc_eigenstates(calc1, scale=1.0)['e_km']
+e_kn -= ef
 
 plt.xticks(X, [r'$\Gamma$', 'Z', 'F', r'$\Gamma$', 'L'], size=24)
 plt.yticks(size=20)
 for i in range(len(X))[1:-1]:
     plt.plot(2 * [X[i]], [1.1 * np.min(e_nk), 1.1 * np.max(e_nk)],
              c='0.5', linewidth=0.5)
-for e_k in e_nk:
+for e_k in e_kn.T:
     plt.plot(x, e_k, c='b')
+
 plt.ylabel(r'$\varepsilon_n(k)$ [eV]', size=24)
 plt.axis([0, x[-1], -1.7, 1.7])
 plt.tight_layout()
