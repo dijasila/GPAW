7da4b16ea6bf5da23c62c80efb1c27278700cabf
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-07-31 09:19:07 +0200

    TYPO

--- doc/summerschools/summerschool18/machinelearning/agts.py
+++ doc/summerschools/summerschool18/machinelearning/agts.py
@@ -7,7 +7,7 @@ from myqueue.task import task
 def create_tasks():
     if os.getenv('AGTS_FILES'):
         dir = Path(os.getenv('AGTS_FILES'))
-        file = Path('organometal.master.db')
+        file = Path('organometal.db')
         if not file.is_file():
             shutil.copyfile(dir / file, file)
     nbrun = 'gpaw.utilities.nbrun'
