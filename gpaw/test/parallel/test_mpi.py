f2715cbad06150a10b73e24e2fa5184e2c538ee9
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-08-11 10:13:26 +0200

    Add MPI test

--- gpaw/test/parallel/test_mpi.py
+++ gpaw/test/parallel/test_mpi.py
@@ -1,5 +1,8 @@
+import numpy as np
+from gpaw.mpi import world, send, receive, broadcast_array
+
+
 def test_send_receive_object():
-    from gpaw.mpi import world, send, receive
     if world.size == 1:
         return
     obj = (42, 'hello')
@@ -7,3 +10,29 @@ def test_send_receive_object():
         send(obj, 1, world)
     elif world.rank == 1:
         assert obj == receive(0, world)
+
+
+def test_bcast_array():
+    new = world.new_communicator
+
+    if world.size == 2:
+        comms = [world]
+    elif world.size == 4:
+        ranks = np.array([[0, 1], [2, 3]])
+        comms = [new(ranks[world.rank // 2]),
+                 new(ranks[:, world.rank % 2])]
+    elif world.size == 8:
+        ranks = np.array([[[0, 1], [2, 3]], [[4, 5], [6, 7]]])
+        ranks = np.arange(8).reshape((2, 2, 2))
+        comms = [new(ranks[world.rank // 4].ravel()),
+                 new(ranks[:, world.rank // 2 % 2].ravel()),
+                 new(ranks[:, :, world.rank % 2]).ravel()]
+    else:
+        return
+
+    array = np.zeros(3, int)
+    if world.rank == 0:
+        array[:] = 42
+
+    out = broadcast_array(array, comms)
+    assert (out == 42).all()
