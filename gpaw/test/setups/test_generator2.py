1394a41231d6dd3605b6fd27b69029d47e43ac8c
Jens Jørgen Mortensen <jj@smoerhul.dk>
2020-08-19 12:14:25 +0200

    Release notes

--- gpaw/test/setups/test_generator2.py
+++ gpaw/test/setups/test_generator2.py
@@ -1,7 +1,7 @@
 from gpaw.atom.generator2 import generate
 
 
-def test_lithium():
+def test_lithium(in_tmp_dir):
     G = generate('Li', 3, '2s,2p,s', [2.1, 2.1], 2.0, 0.0, 2, 'PBE', True)
     assert G.check_all()
     basis = G.create_basis_set()
@@ -10,7 +10,7 @@ def test_lithium():
     setup.write_xml()
 
 
-def test_pseudo_h():
+def test_pseudo_h(in_tmp_dir):
     G = generate('H', 1.25, '1s,s', [0.9], 0.7, 0.0, 2, 'PBE', True)
     assert G.check_all()
     basis = G.create_basis_set()
