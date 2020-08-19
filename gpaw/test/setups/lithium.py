7f655a9e0572c2360ca65343f132976ceb9569ee
Jens Jørgen Mortensen <jj@smoerhul.dk>
2020-08-19 12:08:45 +0200

    Test pseudo-H

--- gpaw/test/setups/lithium.py
+++ gpaw/test/setups/lithium.py
@@ -1,10 +1,17 @@
-import pytest
 from gpaw.atom.generator2 import generate
 
 
-@pytest.mark.skip(reason='TODO')
 def test_lithium():
-    G = generate('Li', '2s,2p,s', [2.1, 2.1], 2.0, 0.0, 2, 'PBE', True)
+    G = generate('Li', 3, '2s,2p,s', [2.1, 2.1], 2.0, 0.0, 2, 'PBE', True)
+    assert G.check_all()
+    basis = G.create_basis_set()
+    basis.write_xml()
+    setup = G.make_paw_setup('test')
+    setup.write_xml()
+
+
+def test_pseudo_h():
+    G = generate('H', 1.25, '1s,s', [0.9], 0.7, 0.0, 2, 'PBE', True)
     assert G.check_all()
     basis = G.create_basis_set()
     basis.write_xml()
