58f8710f44d57ac8377d9c207bbd770c2b78890e
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-08-26 10:42:33 +0200

    Fix new test

--- gpaw/test/cli/test_dos.py
+++ gpaw/test/cli/test_dos.py
@@ -2,28 +2,30 @@ import pytest
 from ase.build import bulk
 from gpaw import GPAW
 from gpaw.cli.main import main
+from gpaw.mpi import size
 
 
 @pytest.fixture
-def gpw(in_tmp_dir):
+def gpw(tmp_path):
     li = bulk('Li', 'bcc', 3.49)
     li.calc = GPAW(mode={'name': 'pw', 'ecut': 200},
                    kpts=(3, 3, 3))
     li.get_potential_energy()
-    li.calc.write('li.gpw')
-    return str(in_tmp_dir / 'li.gpw')
+    li.calc.write(tmp_path / 'li.gpw')
+    return str(tmp_path / 'li.gpw')
 
 
+@pytest.mark.skipif(size > 1, reason='Not serial')
 @pytest.mark.parametrize(
-    'args',
-    [[],
-     ['-t'],
-     ['-i'],
-     ['-a', 'Li-sp'],
-     ['-w', '0'],
-     ['-r', '-5', '5'],
-     ['-n', '50'],
-     ['--soc']])
-def test_dos(gpw, args):
-    args = ['dos', gpw] + args
+    'options',
+    ['',
+     '-t',
+     '-i',
+     '-a Li-sp',
+     '-w 0',
+     '-r -5 5',
+     '-n 50',
+     '--soc'])
+def test_dos(gpw, options):
+    args = ('dos ' + gpw + ' ' + options).split()
     main(args)
