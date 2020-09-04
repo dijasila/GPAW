876ca9979fdbd49504e9f35e17d89ff84bb769af
Jens Jørgen Mortensen <jj@smoerhul.dk>
2020-09-04 13:01:41 +0200

    Refactor

--- doc/exercises/wannier/wannier-si.py
+++ doc/exercises/wannier/wannier-si.py
@@ -1,14 +1,14 @@
 from ase import Atoms
 from ase.visualize import view
 from gpaw import GPAW
-from gpaw.wannier import Wannier
+from gpaw.wannier.overlaps import calculate_overlaps
+from gpaw.wannier.edmiston_ruedenberg import localize
 
 calc = GPAW('si.gpw')
 atoms = calc.get_atoms()
 
-w = Wannier(calc)
-w.localize()
-centers = w.get_centers()
+overlaps = calculate_overlaps(calc)
+wan = localize(overlaps)
 
-watoms = atoms + Atoms(symbols='X16', positions=centers)
+watoms = atoms + Atoms(symbols='X16', positions=wan.centers)
 view(watoms * (2, 2, 2))
