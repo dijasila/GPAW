edc092a7ce1f7129fc6f85fc3bae69d75662a326
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-09-28 10:56:28 +0200

    Read with new ase function

--- doc/tutorials/wannier90/agts.py
+++ doc/tutorials/wannier90/agts.py
@@ -1,4 +1,4 @@
-from typing import List
+from ase.io.wannier90 import read_wout_all
 from myqueue.task import task
 import numpy as np
 
@@ -12,24 +12,14 @@ def create_tasks():
     return [t1, t2, t3, t4, t5]
 
 
-def read(filename: str, nwan: int) -> List[float]:
-    with open(filename) as fd:
-        lines = [line.strip() for line in fd.readlines()]
-
-    for i, line in enumerate(lines):
-        if line == 'Final State':
-            break
-    line = lines[i + nwan + 1]
-    assert line.startswith('Sum of centres and spreads')
-    line = line.split('(')[1].replace(',', '').replace(')', '')
-    x, y, z, w = (float(x) for x in line.split())
-    return x, y, z, w
-
-
 if __name__ == '__main__':
-    x, y, z, w = read('GaAs.wout', 4)
+    dct = read_wout_all('GaAs.wout')
+    x, y, z = dct['centers'].sum(axis=0)
+    w = dct['spreads'].sum()
     a = 5.68
     assert abs(np.array([x, y, z, w]) - [a, a, a, 4.14]).max() < 0.01
 
-    x, y, z, w = read('Fe.wout', 18)
+    dct = read_wout_all('Fe.wout')
+    x, y, z = dct['centers'].sum(axis=0)
+    w = dct['spreads'].sum()
     assert abs(np.array([x, y, z, w]) - [0, 0, 0, 14.49]).max() < 0.01
