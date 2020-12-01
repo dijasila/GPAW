f27e5dc0e8f2e9b396e2f17703a727eb55e49e61
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-12-01 16:50:59 +0100

    NV-

---
+++ doc/tutorials/zfs/diamond_nv_minus.py
@@ -0,0 +1,25 @@
+import numpy as np
+from ase.build import bulk
+from gpaw import GPAW, PW
+
+
+def nv(n):
+    atoms = bulk('C', 'diamond') * n
+    atoms.numbers[0] = 7
+    del atoms[1]
+    magmoms = np.zeros(len(atoms))
+    magmoms[0] = 2.0
+    magmoms[:] = 1.0
+    atoms.set_initial_magnetic_moments(magmoms)
+    name = f'c{2 * n**3 - 2}n'
+    atoms.calc = GPAW(xc='PBE',
+                      mode=PW(ecut=500),
+                      charge=-1,
+                      txt=name + '.txt')
+    atoms.get_potential_energy()
+    atoms.calc.write(name + '.gpw', 'all')
+
+
+if __name__ == '__main__':
+    nv(4)
+    from gpaw.zero_field_splitting import main
