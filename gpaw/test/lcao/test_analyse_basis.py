66a6351d49f26ba18b67b88dccc7449bdb88031d
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-12-14 15:49:57 +0100

    call analyse-basis programmatically instead of via subprocess

--- gpaw/test/lcao/test_analyse_basis.py
+++ gpaw/test/lcao/test_analyse_basis.py
@@ -1,10 +1,13 @@
 from subprocess import check_output
 from pathlib import Path
+from gpaw.lcao.analyse_basis import main
 
 
-def test_analyse_basis(in_tmp_dir):
-    argv = ['gpaw-analyse-basis', 'H.dzp.basis', '--save-figs']
-    out = check_output(argv, encoding='utf-8')
+def test_analyse_basis(in_tmp_dir, capsys):
+    argv = ['H.dzp.basis', '--save-figs']
+    main(argv)
+    captured = capsys.readouterr()
+    out = captured.out
     assert '1s-sz' in out
     assert '1s-dz' in out
     assert 'p-type' in out
