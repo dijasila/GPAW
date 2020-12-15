526e81318d78d88793e891a19d76f33f1f01195c
giale <gianl.levi@gmail.com>
2020-12-15 14:23:24 +0000

    Refactoring directmin

--- gpaw/directmin/odd/fdpw/dftpzxt.py
+++ gpaw/directmin/odd/fdpw/dftpzxt.py
@@ -103,7 +103,7 @@ class DftPzSicXT:
     def get_energy_and_gradients(self, wfs, grad_knG=None,
                                  dens=None, U_k=None,
                                  add_grad=False,
-                                 ham=None, occ=None):
+                                 ham=None):
 
         wfs.timer.start('Update Kohn-Sham energy')
         # calc projectors
@@ -112,11 +112,17 @@ class DftPzSicXT:
         dens.update(wfs)
         ham.update(dens, wfs, False)
         wfs.timer.stop('Update Kohn-Sham energy')
-        energy = ham.get_energy(occ, False)
+        # TODO: We need to pass e_entropy to ham.get_energy
+        #  but e_entropy requires the calculation of the
+        #  occupation numbers. Setting e_entropy = 0.0
+        #  should work only for zero temperature calculations
+        if not hasattr(self, '_e_entropy'):
+            self._e_entropy = 0.0
+        energy = ham.get_energy(self._e_entropy, wfs, False)
         esic = 0.0
         for kpt in wfs.kpt_u:
             esic += self.get_energy_and_gradients_kpt(
-                wfs, kpt, dens, ham=ham, occ=occ)
+                wfs, kpt, dens, ham=ham)
 
             k = self.n_kps * kpt.s + kpt.q
             if U_k is not None:
@@ -131,7 +137,7 @@ class DftPzSicXT:
         return energy + esic
 
     def get_energy_and_gradients_kpt(self, wfs, kpt,
-                                     dens=None, ham=None, occ=None):
+                                     dens=None, ham=None):
 
         self.get_gradient_ks_kpt(wfs, kpt, ham=ham)
         esic = self.get_esic_add_sic_gradient_kpt(wfs, kpt)
@@ -220,13 +226,13 @@ class DftPzSicXT:
 
     def get_energy_and_gradients_inner_loop(self, wfs, a_mat,
                                             evals, evec, dens,
-                                            ham, occ):
+                                            ham):
         nbands = wfs.bd.nbands
         e_sic = self.get_energy_and_gradients(wfs,
                                               grad_knG=None,
                                               dens=dens, U_k=None,
                                               add_grad=False,
-                                              ham=ham, occ=occ)
+                                              ham=ham)
         wfs.timer.start('Unitary gradients')
         g_k = {}
         kappa_tmp = 0.0
