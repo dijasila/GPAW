ef696bdbad6f5e615b0f1df573abb445c7c8a7c3
Jens Jørgen Mortensen <jj@smoerhul.dk>
2020-12-15 18:39:50 +0100

    Finish test

--- gpaw/test/setups/test_generator2.py
+++ gpaw/test/setups/test_generator2.py
@@ -1,8 +1,12 @@
+import numpy as np
+import pytest
+
 from gpaw.atom.generator2 import generate
 from gpaw.setup import Setup
 from gpaw.xc import XC
 
 
+@pytest.mark.serial
 def test_lithium(in_tmp_dir):
     G = generate('Li', 3, '2s,2p,s', [2.1, 2.1], 2.0, None, 2, 'PBE', True)
     assert G.check_all()
@@ -12,6 +16,7 @@ def test_lithium(in_tmp_dir):
     setup.write_xml()
 
 
+@pytest.mark.serial
 def test_pseudo_h(in_tmp_dir):
     G = generate('H', 1.25, '1s,s', [0.9], 0.7, None, 2, 'PBE', True)
     assert G.check_all()
@@ -25,4 +30,6 @@ def test_pseudo_h(in_tmp_dir):
                   xc,
                   lmax=2).calculate_T_Lqp(1, 3, 2,
                                           jlL_i=[(0, 0, 0), (1, 0, 0)])
-    
+    print(T_Lqp)
+    assert (T_Lqp[1:] == 0.0).all()
+    assert T_Lqp[0] == pytest.approx(np.eye(3) / (4 * np.pi)**0.5)
