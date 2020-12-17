7e8b0c3d34831f08553fbe01378efae9e6df2557
Jens Jørgen Mortensen <jj@smoerhul.dk>
2020-12-17 23:12:56 +0100

    Fix PDOS+SOC parallelization bug ...

--- gpaw/test/dos/test_pdos.py
+++ gpaw/test/dos/test_pdos.py
@@ -9,3 +9,10 @@ def test_li_pdos_pxyz(gpw_files):
     p1 = dos.raw_pdos(energies, a=0, l=1)
     p2 = sum(dos.raw_pdos(energies, a=0, l=1, m=m) for m in range(3))
     assert abs(p1 - p2).max() < 1e-7
+
+
+def test_pdos_soc(gpw_files):
+    """Test pdos method with and without m."""
+    dos = GPAW(gpw_files['c6h12_pw']).dos(soc=True)
+    energies = dos.get_energies(npoints=10)
+    _ = dos.raw_pdos(energies, a=0, l=0)
