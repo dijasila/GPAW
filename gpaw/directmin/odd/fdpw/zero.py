3b8bf3f6971f5f063a020e455900f453e1bf31f4
giale <gianl.levi@gmail.com>
2020-12-17 14:02:25 +0000

    Fix bug

--- gpaw/directmin/odd/fdpw/zero.py
+++ gpaw/directmin/odd/fdpw/zero.py
@@ -57,7 +57,7 @@ class ZeroCorrections:
             occ_name = getattr(wfs.occupations, 'name', None)
             if occ_name == 'mom':
                 for kpt in wfs.kpt_u:
-                    wfs.occupations.sort_wavefunctions(wfs, kpt)
+                    wfs.occupations.sort_wavefunctions(kpt)
             if self.changedocc:
                 self.restart = 1
         self.momcounter += 1
