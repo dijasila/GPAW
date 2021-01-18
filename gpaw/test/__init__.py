b982050dfb5c1356fb610acf3a3346b9c001cede
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-01-18 19:18:21 +0100

    Fix findpeak() function

--- gpaw/test/__init__.py
+++ gpaw/test/__init__.py
@@ -24,12 +24,27 @@ def print_reference(data_i, name='ref_i', fmt='%.12le'):
 
 
 def findpeak(x, y):
+    """Find peak.
+
+    >>> x = np.linspace(1, 5, 10)
+    >>> y = 1 - (x - np.pi)**2
+    >>> x0, y0 = findpeak(x, y)
+    >>> f'x0={x0:.6f}, y0={y0:.6f}'
+    'x0=3.141593, y0=1.000000'
+    """
     dx = x[1] - x[0]
     i = y.argmax()
     a, b, c = np.polyfit([-1, 0, 1], y[i - 1:i + 2], 2)
     assert a < 0
-    x = -0.5 * b / a
-    return dx * (i + x), a * x**2 + b * x + c
+    x0 = -0.5 * b / a
+    print(dx * (i + x0), a * x0**2 + b * x0 + c)
+    a, b, c = np.polyfit(x[i - 1:i + 2] - x[i], y[i - 1:i + 2], 2)
+    assert a < 0
+    dx = -0.5 * b / a
+    x0 = x[i] + dx
+    # assert abs(dx * i - x[i]) < 1e-8
+    print(x0, a * dx**2 + b * dx + c)
+    return x0, a * dx**2 + b * dx + c
 
 
 def gen(symbol, exx=False, name=None, yukawa_gamma=None,
