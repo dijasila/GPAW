1cf86c09846c8ce405d2cadc3396c411658c3019
giale <gianl.levi@gmail.com>
2021-01-21 12:31:07 +0000

    PEP8

--- gpaw/mom.py
+++ gpaw/mom.py
@@ -134,8 +134,8 @@ class OccupationsMOM:
             if self.iters == 0:
                 self.width_increment_ct = 0
             if self.iters % self.niter_smearing == 0:
-                self.gauss_width = self.width + \
-                                   self.width_increment_ct * self.width_increment
+                self.gauss_width = self.width + self.width_increment_ct \
+                                   * self.width_increment
                 self.width_increment_ct += 1
 
         for kpt in self.wfs.kpt_u:
