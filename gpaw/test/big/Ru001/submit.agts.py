d640adc86f40c5fdb2f714b73d073ab6dd869d03
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-02-08 19:50:17 +0100

    Fejl40

--- gpaw/test/big/Ru001/submit.agts.py
+++ gpaw/test/big/Ru001/submit.agts.py
@@ -3,8 +3,8 @@ from myqueue.workflow import run
 
 def workflow():
     runs = [run(script='ruslab.py', cores=8, tmax='10h'),
-            run(script='ruslab.py+H', cores=8, tmax='10h'),
-            run(script='ruslab.py+N', cores=8, tmax='10h'),
-            run(script='ruslab.py+O', cores=16, tmax='15h'),
+            run(script='ruslab.py', args=['H'], cores=8, tmax='10h'),
+            run(script='ruslab.py', args=['N'], cores=8, tmax='10h'),
+            run(script='ruslab.py', args=['O'], cores=16, tmax='15h'),
             run(script='molecules.py', cores=8, tmax='20m')]
     run(script='results.py', deps=runs)
--- gpaw/test/big/scf/trouble/submit.agts.py
+++ gpaw/test/big/scf/trouble/submit.agts.py
@@ -2,8 +2,9 @@ from myqueue.workflow import run
 
 
 def workflow():
-    runs = [run(script='run.py+16', cores=16, tmax='10h'),
-            run(script='run.py+8', cores=8, tmax='12h'),
-            run(script='run.py+4', cores=4, tmax='5h'),
-            run(script='run.py+1', tmax='1h')]
+    runs = [run(script='run.py', args=[cores], cores=cores, tmax=tmax)
+            for cores, tmax in [(1, '1h'),
+                                (4, '5h'),
+                                (8, '12h'),
+                                (16, '10h')]]
     run(script='analyse.py', deps=runs)
