62da02bc11774d93ca89b79c82e5a88b414bc7ce
giale <gianl.levi@gmail.com>
2021-02-24 12:12:41 +0000

    Changes to test

--- gpaw/test/mom/test_mom_lcao.py
+++ gpaw/test/mom/test_mom_lcao.py
@@ -7,16 +7,13 @@ from gpaw.test import equal
 
 @pytest.mark.mom
 def test_mom_lcao():
-    f_n = [[1., 1., 1., 1., 0., 0.],
-           [1., 1., 1., 1., 0., 0.]]
-    dE_ref = [8.1458184518, 6.1579949366]
+    dE_ref = [8.1458184518, 7.8544875500]
 
     for s in [0, 1]:
         # Ground state calculation
-
         calc = GPAW(mode='lcao',
                     basis='dzp',
-                    nbands=6,
+                    nbands=8,
                     h=0.2,
                     xc='PBE',
                     spinpol=True,
@@ -31,10 +28,14 @@ def test_mom_lcao():
 
         E_gs = H2O.get_potential_energy()
 
+        f_n = []
+        for kpt in calc.wfs.kpt_u:
+            f_nks = calc.get_occupation_numbers(kpt.k, kpt.s)
+            f_n.append(f_nks)
+
         # Excited-state MOM calculation
-        f_n_exc = f_n.copy()
-        f_n_exc[0][3] -= 1.
-        f_n_exc[s][4] += 1.
+        f_n[0][3] -= 1.
+        f_n[s][4] += 1.
 
         mom.mom_calculation(calc, H2O, f_n)
 
@@ -42,4 +43,4 @@ def test_mom_lcao():
 
         dE = E_es - E_gs
         print(dE)
-        equal(dE, 8.1458184518, 0.011)
+        equal(dE, dE_ref[s], 0.011)
