baa987724feabc971a4baf642f6602eea30c6dd2
giale <gianl.levi@gmail.com>
2021-02-26 17:29:07 +0000

    Modify test of MOM with smearing

--- gpaw/test/mom/test_mom_smearing.py
+++ gpaw/test/mom/test_mom_smearing.py
@@ -30,15 +30,20 @@ def test_mom_smearing():
         f_ns = calc.get_occupation_numbers(spin=spin)
         f_n.append(f_ns)
 
+    ne0_gs = f_n[0].sum()
     f_n[0][3] -= 1.
     f_n[0][5] += 1.
 
     # Excited-state MOM calculation with Gaussian
     # smearing of the occupation numbers
-    mom.mom_calculation(calc, atoms, f_n,
-                        width=0.1)
+    mom.mom_calculation(calc, atoms, f_n, width=0.1)
     E_es = atoms.get_potential_energy()
 
+    f_n0 = calc.get_occupation_numbers(spin=0)
+    ne0_es = f_n0.sum()
+
     dE = E_es - E_gs
+    dne0 = ne0_es - ne0_gs
     print(dE)
     equal(dE, 9.8313927688, 0.01)
+    equal(dne0, 0.0, 1e-16)
