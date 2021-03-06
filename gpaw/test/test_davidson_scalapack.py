be95cbd24929b760f9e2c2f5b8b94990a868158c
Anubhab Haldar <ahaldar@bu.edu>
2021-03-06 16:43:05 -0500

    More formatting...

--- gpaw/test/test_davidson_scalapack.py
+++ gpaw/test/test_davidson_scalapack.py
@@ -7,23 +7,30 @@ from gpaw import GPAW, FermiDirac, PW, Davidson
 
 pytestmark = pytest.mark.skipif(
     world.size != 4 or not compiled_with_sl(),
-    reason='world.size != 4 or not compiled_with_sl()')
+    reason="world.size != 4 or not compiled_with_sl()",
+)
 
 
 def test_davidson_scalapack(in_tmp_dir):
-    atoms = bulk('Ag', cubic=True) # Need cubic so there are enough electrons...
+    atoms = bulk(
+        "Ag", cubic=True
+    )  # Need cubic so there are enough electrons...
     atoms1 = atoms.copy()
     atoms2 = atoms.copy()
-    calc_noscalapack = GPAW(mode=PW(340),
-                    kpts=[1] * 3,
-                    occupations=FermiDirac(0.1),
-                    eigensolver=Davidson(use_scalapack=False))
+    calc_noscalapack = GPAW(
+        mode=PW(340),
+        kpts=[1] * 3,
+        occupations=FermiDirac(0.1),
+        eigensolver=Davidson(use_scalapack=False),
+    )
 
-    calc_scalapack = GPAW(mode=PW(340),
-                    kpts=[1] * 3,
-                    occupations=FermiDirac(0.1),
-                    eigensolver=Davidson(use_scalapack=True))
-                    
+    calc_scalapack = GPAW(
+        mode=PW(340),
+        kpts=[1] * 3,
+        occupations=FermiDirac(0.1),
+        eigensolver=Davidson(use_scalapack=True),
+    )
+
     atoms1.calc = calc_noscalapack
     atoms2.calc = calc_scalapack
 
