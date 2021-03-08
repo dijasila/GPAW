b9354dba50c9e3fec58621d9583f80c644fce096
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2021-03-08 13:03:26 +0200

    Clean example scripts

--- doc/documentation/tddft/lcaotddft_mox/td.py
+++ doc/documentation/tddft/lcaotddft_mox/td.py
@@ -1,26 +1,10 @@
-from ase.io import read
-from gpaw import GPAW
-from gpaw import PoissonSolver
-from gpaw.occupations import FermiDirac
 from gpaw.lcaotddft import LCAOTDDFT
 from gpaw.lcaotddft.dipolemomentwriter import DipoleMomentWriter
-from gpaw.lcaotddft.wfwriter import WaveFunctionWriter
 from gpaw.lcaotddft.magneticmomentwriter import MagneticMomentWriter
-from gpaw.tddft.spectrum import photoabsorption_spectrum
-from gpaw import setup_paths
 
-
-# Time propagation
-td_calc = LCAOTDDFT('methyl-oxirane.gpw')
-
-DipoleMomentWriter(td_calc, 'rmeo003.dm')
-MagneticMomentWriter(td_calc,'cd_live003.dat')
-#WaveFunctionWriter(td_calc, 'wf003.ulm')
-
-
+td_calc = LCAOTDDFT('gs.gpw', txt='td.out')
+DipoleMomentWriter(td_calc, 'dm.dat')
+MagneticMomentWriter(td_calc, 'mm.dat')
 td_calc.absorption_kick([1e-5, 0.0, 0.0])
 td_calc.propagate(10, 1000)
-
-
-
-td_calc.write('td0.gpw', mode='all')
+td_calc.write('td.gpw', mode='all')
