50e6e9aa5f4674d40dc696cbc6795d44d67c2380
Anubhab Haldar <ahaldar@bu.edu>
2021-03-10 11:51:38 -0500

    Clean up test, prepay technical debt

--- gpaw/test/test_diagonalizer_backend.py
+++ gpaw/test/test_diagonalizer_backend.py
@@ -26,69 +26,59 @@ def prepare_eigensolver_matrices(size_of_matrices, dtype):
     return A, B
 
 
-@pytest.mark.parametrize('dtype,', [float, complex])
-def test_scipy_diagonalizer_eigenproblem_correctness(dtype):
-    is_master_rank = world.rank == 0
-    eigenproblem_size = 64
-
-    serial_diagonalizer = ScipyDiagonalizer()
-
-    a, b = prepare_eigensolver_matrices(eigenproblem_size, dtype=dtype)
-    eps = np.zeros(eigenproblem_size)
-    a_scipy_copy = a.copy()
-    b_scipy_copy = b.copy()
-
-    # a_scipy_copy contains eigenvectors after this.
-    serial_diagonalizer.diagonalize(
-        a_scipy_copy,
-        b_scipy_copy,
-        eps,
-        is_master=is_master_rank,
-        debug=False,
-    )
-
-    if is_master_rank:
-        assert np.allclose(a @ a_scipy_copy, b @ a_scipy_copy @ np.diag(eps))
-
-
-@pytest.mark.parametrize('dtype,', [float, complex])
-@pytest.mark.skipif(
-    not compiled_with_sl(),
-    reason='Not compiled with Scalapack',
-)
-def test_scalapack_diagonalizer_eigenproblem_correctness(dtype):
-
-    is_master_rank = world.rank == 0
+@pytest.fixture(params=["eigh", "blacs"])
+def backend_problemsize_kwargs(request):
+    name = request.param
     eigenproblem_size = world.size * 64
+    if name == "eigh":
+        return ScipyDiagonalizer, eigenproblem_size, {}
+    elif name == "blacs":
+        if not compiled_with_sl():
+            pytest.skip()
 
-    nrows = 2 if world.size > 1 else 1
-    ncols = world.size // 2 if nrows > 1 else 1
+        nrows = 2 if world.size > 1 else 1
+        ncols = world.size // 2 if nrows > 1 else 1
 
-    scalapack_diagonalizer = ScalapackDiagonalizer(
+        scalapack_kwargs = {
+            "arraysize": eigenproblem_size,
+            "grid_nrows": nrows,
+            "grid_ncols": ncols,
+            "scalapack_communicator": world,
+            "blocksize": 32 if world.size == 1 else 64,
+        }
+        return ScalapackDiagonalizer, eigenproblem_size, scalapack_kwargs
+
+
+@pytest.mark.parametrize("dtype,", [float, complex])
+def test_diagonalizer_eigenproblem_correctness(
+    backend_problemsize_kwargs, dtype
+):
+    is_master_rank = world.rank == 0
+
+    (
+        diagonalizer_class,
         eigenproblem_size,
-        nrows,
-        ncols,
-        dtype=dtype,
-        scalapack_communicator=world,
-        blocksize=32 if world.size == 1 else 64,
-    )
+        diagonalizer_kwargs,
+    ) = backend_problemsize_kwargs
+
+    if diagonalizer_class is ScipyDiagonalizer:
+        diagonalizer = diagonalizer_class(**diagonalizer_kwargs)
+    elif diagonalizer_class is ScalapackDiagonalizer:
+        diagonalizer = diagonalizer_class(**diagonalizer_kwargs, dtype=dtype)
 
     a, b = prepare_eigensolver_matrices(eigenproblem_size, dtype=dtype)
     eps = np.zeros(eigenproblem_size)
+    a_copy = a.copy()
+    b_copy = b.copy()
 
-    a_scalapack_copy = a.copy()
-    b_scalapack_copy = b.copy()
-
-    # a_scalapack_copy contains eigenvectors after this.
-    scalapack_diagonalizer.diagonalize(
-        a_scalapack_copy,
-        b_scalapack_copy,
+    # a_copy contains eigenvectors after this.
+    diagonalizer.diagonalize(
+        a_copy,
+        b_copy,
         eps,
         is_master=is_master_rank,
         debug=False,
     )
 
     if is_master_rank:
-        assert np.allclose(
-            a @ a_scalapack_copy, b @ a_scalapack_copy @ np.diag(eps)
-        )
+        assert np.allclose(a @ a_copy, b @ a_copy @ np.diag(eps))
