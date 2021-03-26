7d38c3277ed0724d42232af74e0eeada50f50269
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2021-03-26 18:47:02 +0200

    Rewrite tutorial with more explanations

---
+++ doc/documentation/tddft/lrtddft2/submit.agts.py
@@ -0,0 +1,11 @@
+from myqueue.workflow import run
+
+
+def workflow():
+    with run(script='gs.py', cores=8, tmax='5m'):
+        with run(script='unocc.py', cores=8, tmax='15m'):
+            with run(script='lr2.py', cores=8, tmax='5m'):
+                with run(script='lr2_restart.py', cores=8, tmax='5m'):
+                    with run(script='lr2_analyze.py', cores=1, tmax='5m'):
+                        run(script='plot_spec.py', tmax='1m')
+                        run(script='check_consistency_with_docs.py', tmax='1m')
