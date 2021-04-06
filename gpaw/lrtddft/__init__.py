b6430b494f7de3aa871ff03940887b2e3d3c1381
Michael Walter <mcoywalter@gmail.com>
2021-04-06 16:31:36 +0200

    Use weights as in mur and muv

--- gpaw/lrtddft/__init__.py
+++ gpaw/lrtddft/__init__.py
@@ -471,7 +471,7 @@ class LrTDDFTExcitation(Excitation):
             else:
                 self.muv = None
             if self.kss.magn is not None:
-                self.magn = np.dot(1. / ew_k, self.kss.magn)
+                self.magn = np.dot(ew_k, self.kss.magn)
             else:
                 self.magn = None
 
