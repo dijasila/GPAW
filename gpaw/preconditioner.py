f653a7816e13b8f76c80c9a783b8afe1cae4bc44
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-04-06 14:52:46 +0200

    Fix debug mode

--- gpaw/preconditioner.py
+++ gpaw/preconditioner.py
@@ -1,10 +1,10 @@
+import _gpaw
 import numpy as np
 
-from gpaw.transformers import Transformer
+from gpaw import debug
 from gpaw.fd_operators import Laplace
-
+from gpaw.transformers import Transformer
 from gpaw.utilities.blas import axpy
-import _gpaw
 
 
 class Preconditioner:
@@ -49,10 +49,15 @@ class Preconditioner:
         r1, d1, q1 = self.scratch1[:, :nb]
         r2, d2, q2 = self.scratch2[:, :nb]
         if self.use_c_precond:
-            _gpaw.fd_precond(self.restrictor_object0.transformer,
-                             self.restrictor_object1.transformer,
-                             self.interpolator_object1.transformer,
-                             self.interpolator_object2.transformer,
+            transformers = [self.restrictor_object0.transformer,
+                            self.restrictor_object1.transformer,
+                            self.interpolator_object1.transformer,
+                            self.interpolator_object2.transformer]
+            if debug:
+                # Unwrap wrapper:
+                transformers = [getattr(t, 'transformer', t)
+                                for t in transformers]
+            _gpaw.fd_precond(*transformers,
                              self.kin0.operator, self.kin1.operator,
                              self.kin2.operator,
                              d0, q0, r1, d1, q1, r2, d2, q2,
