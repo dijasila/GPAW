0d3fd761390100612203ace8a5cd9a355103e71c
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2021-04-08 09:51:24 +0300

    Use default maximum time for short AGTS tasks

--- doc/documentation/tddft/lrtddft2/submit.agts.py
+++ doc/documentation/tddft/lrtddft2/submit.agts.py
@@ -2,10 +2,10 @@ from myqueue.workflow import run
 
 
 def workflow():
-    with run(script='gs.py', cores=8, tmax='5m'):
+    with run(script='gs.py', cores=8):
         with run(script='unocc.py', cores=8, tmax='30m'):
-            with run(script='lr2.py', cores=8, tmax='5m'):
-                with run(script='lr2_restart.py', cores=8, tmax='5m'):
-                    with run(script='lr2_analyze.py', cores=1, tmax='5m'):
-                        run(script='plot_spec.py', tmax='1m')
-                        run(script='check_consistency_with_docs.py', tmax='1m')
+            with run(script='lr2.py', cores=8):
+                with run(script='lr2_restart.py', cores=8):
+                    with run(script='lr2_analyze.py', cores=1):
+                        run(script='plot_spec.py')
+                        run(script='check_consistency_with_docs.py')
