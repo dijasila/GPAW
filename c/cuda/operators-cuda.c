8332d612e0a16525b18aede56dea3bf9b78576c5
Martti Louhivuori <martti.louhivuori@csc.fi>
2021-04-09 17:16:02 +0300

    Move debug calls outside of the loops

--- c/cuda/operators-cuda.c
+++ c/cuda/operators-cuda.c
@@ -26,6 +26,8 @@ static int operator_buf_size = 0;
 static int operator_buf_max = 0;
 static int operator_init_count = 0;
 
+static int debug_size_arr = 0;
+static int debug_size_buf = 0;
 static double *debug_sendbuf;
 static double *debug_recvbuf;
 static double *debug_buf_cpu;
@@ -106,16 +108,24 @@ void operator_dealloc_cuda(int force)
     }
 }
 
-void debug_operator_allocate(boundary_conditions *bc, int ng, int ng2,
-                             int blocks)
+void debug_operator_allocate(OperatorObject* self, int nin, int blocks)
 {
+    boundary_conditions* bc = self->bc;
+    const int *size1 = bc->size1;
+    const int *size2 = bc->size2;
+    int ng = bc->ndouble * size1[0] * size1[1] * size1[2];
+    int ng2 = bc->ndouble * size2[0] * size2[1] * size2[2];
+
+    debug_size_arr = ng * nin;
+    debug_size_buf = ng2 * blocks;
+
     debug_sendbuf = GPAW_MALLOC(double, bc->maxsend * blocks);
     debug_recvbuf = GPAW_MALLOC(double, bc->maxrecv * blocks);
-    debug_buf_cpu = GPAW_MALLOC(double, ng2 * blocks);
-    debug_buf_gpu = GPAW_MALLOC(double, ng2 * blocks);
-    debug_out_cpu = GPAW_MALLOC(double, ng * blocks);
-    debug_out_gpu = GPAW_MALLOC(double, ng * blocks);
-    debug_in_cpu = GPAW_MALLOC(double, ng * blocks);
+    debug_buf_cpu = GPAW_MALLOC(double, debug_size_buf);
+    debug_buf_gpu = GPAW_MALLOC(double, debug_size_buf);
+    debug_out_cpu = GPAW_MALLOC(double, debug_size_arr);
+    debug_out_gpu = GPAW_MALLOC(double, debug_size_arr);
+    debug_in_cpu = GPAW_MALLOC(double, debug_size_arr);
 }
 
 void debug_operator_deallocate()
@@ -127,52 +137,56 @@ void debug_operator_deallocate()
     free(debug_out_cpu);
     free(debug_out_gpu);
     free(debug_in_cpu);
+    debug_size_arr = 0;
+    debug_size_buf = 0;
 }
 
-void debug_operator_memcpy_pre(double *out, const double *in, int ng)
+void debug_operator_memcpy_pre(double *out, const double *in)
 {
-    GPAW_CUDAMEMCPY(debug_in_cpu, in, double, ng, cudaMemcpyDeviceToHost);
-    GPAW_CUDAMEMCPY(debug_out_cpu, out, double, ng, cudaMemcpyDeviceToHost);
+    GPAW_CUDAMEMCPY(debug_in_cpu, in, double, debug_size_arr,
+                    cudaMemcpyDeviceToHost);
+    GPAW_CUDAMEMCPY(debug_out_cpu, out, double, debug_size_arr,
+                    cudaMemcpyDeviceToHost);
 }
 
-void debug_operator_memcpy_post(double *out, double *buf, int ng, int ng2)
+void debug_operator_memcpy_post(double *out, double *buf)
 {
-    GPAW_CUDAMEMCPY(debug_out_gpu, out, double, ng, cudaMemcpyDeviceToHost);
-    GPAW_CUDAMEMCPY(debug_buf_gpu, buf, double, ng2, cudaMemcpyDeviceToHost);
+    GPAW_CUDAMEMCPY(debug_out_gpu, out, double, debug_size_arr,
+                    cudaMemcpyDeviceToHost);
+    GPAW_CUDAMEMCPY(debug_buf_gpu, buf, double, debug_size_buf,
+                    cudaMemcpyDeviceToHost);
 }
 
 
 void debug_operator_relax(OperatorObject* self, int relax_method, double w,
-                          int n)
+                          int nrelax)
 {
     MPI_Request recvreq[2];
     MPI_Request sendreq[2];
     int i;
 
     boundary_conditions* bc = self->bc;
-    const int *size1 = bc->size1;
-    const int *size2 = bc->size2;
-    int ng = bc->ndouble * size1[0] * size1[1] * size1[2];
-    int ng2 = bc->ndouble * size2[0] * size2[1] * size2[2];
 
     const double_complex *ph;
     ph = 0;
 
-    for (i=0; i < 3; i++) {
-        bc_unpack1(bc, debug_out_cpu, debug_buf_cpu, i, recvreq, sendreq,
-                   debug_recvbuf, debug_sendbuf, ph + 2 * i, 0, 1);
-        bc_unpack2(bc, debug_buf_cpu, i, recvreq, sendreq,
-                   debug_recvbuf, 1);
+    for (int n=0; n < nrelax; n++ ) {
+        for (i=0; i < 3; i++) {
+            bc_unpack1(bc, debug_out_cpu, debug_buf_cpu, i, recvreq, sendreq,
+                       debug_recvbuf, debug_sendbuf, ph + 2 * i, 0, 1);
+            bc_unpack2(bc, debug_buf_cpu, i, recvreq, sendreq,
+                       debug_recvbuf, 1);
+        }
+        bmgs_relax(relax_method, &self->stencil, debug_buf_cpu, debug_out_cpu,
+                   debug_in_cpu, w);
     }
-    bmgs_relax(relax_method, &self->stencil, debug_buf_cpu, debug_out_cpu,
-               debug_in_cpu, w);
 
     double buf_err = 0;
-    for (i=0; i < ng2; i++) {
+    for (i=0; i < debug_size_buf; i++) {
         buf_err = MAX(buf_err, fabs(debug_buf_cpu[i] - debug_buf_gpu[i]));
     }
     double fun_err = 0;
-    for (i=0; i < ng; i++) {
+    for (i=0; i < debug_size_arr; i++) {
         fun_err = MAX(fun_err, fabs(debug_out_cpu[i] - debug_out_gpu[i]));
     }
     int rank = 0;
@@ -180,74 +194,13 @@ void debug_operator_relax(OperatorObject* self, int relax_method, double w,
         MPI_Comm_rank(bc->comm, &rank);
     if (buf_err > GPAW_CUDA_ABS_TOL) {
         fprintf(stderr,
-                "Debug cuda operator relax bc (n:%d rank:%d) errors: buf %g\n",
-                n, rank, buf_err);
+                "Debug cuda operator relax bc (rank:%d) errors: buf %g\n",
+                rank, buf_err);
     }
     if (fun_err > GPAW_CUDA_ABS_TOL) {
         fprintf(stderr,
-                "Debug cuda operator relax (n:%d rank:%d) errors: fun %g\n",
-                n, rank, fun_err);
-    }
-}
-
-void debug_operator_apply(OperatorObject* self, const double_complex *ph,
-                          bool real, int n, int myblocks)
-{
-    MPI_Request recvreq[2];
-    MPI_Request sendreq[2];
-    int i;
-    double err;
-
-    boundary_conditions* bc = self->bc;
-    const int *size1 = bc->size1;
-    const int *size2 = bc->size2;
-    int ng = bc->ndouble * size1[0] * size1[1] * size1[2];
-    int ng2 = bc->ndouble * size2[0] * size2[1] * size2[2];
-
-    for (i=0; i < 3; i++) {
-        bc_unpack1(bc, debug_in_cpu, debug_buf_cpu, i, recvreq, sendreq,
-                   debug_recvbuf, debug_sendbuf, ph + 2 * i, 0, myblocks);
-        bc_unpack2(bc, debug_buf_cpu, i, recvreq, sendreq,
-                   debug_recvbuf, myblocks);
-    }
-    for (int m=0; m < myblocks; m++) {
-        if (real)
-            bmgs_fd(&self->stencil, debug_buf_cpu + m * ng2,
-                    debug_out_cpu + m * ng);
-        else
-            bmgs_fdz(&self->stencil,
-                    (const double_complex*) (debug_buf_cpu + m * ng2),
-                    (double_complex*) (debug_out_cpu + m * ng));
-    }
-
-    double buf_err = 0;
-    int buf_err_n = 0;
-    for (i = 0; i < ng2 * myblocks; i++) {
-        err = fabs(debug_buf_cpu[i] - debug_buf_gpu[i]);
-        if (err > GPAW_CUDA_ABS_TOL)
-            buf_err_n++;
-        buf_err = MAX(buf_err, err);
-    }
-    double out_err = 0;
-    int out_err_n = 0;
-    for (i = 0; i < ng * myblocks; i++) {
-        err = fabs(debug_out_cpu[i] - debug_out_gpu[i]);
-        if (err > GPAW_CUDA_ABS_TOL)
-            out_err_n++;
-        out_err = MAX(out_err, err);
-    }
-    int rank = 0;
-    if (bc->comm != MPI_COMM_NULL)
-        MPI_Comm_rank(bc->comm, &rank);
-    if (buf_err > GPAW_CUDA_ABS_TOL) {
-        printf("Debug cuda operator fd bc (n:%d rank:%d) errors: buf %g count %d/%d\n",
-               n, rank, buf_err, buf_err_n, ng2 * myblocks);
-        fflush(stdout);
-    }
-    if (out_err > GPAW_CUDA_ABS_TOL) {
-        printf("Debug cuda operator fd (n:%d rank:%d) errors: out %g count %d/%d\n",
-               n, rank, out_err, out_err_n, ng * myblocks);
-        fflush(stdout);
+                "Debug cuda operator relax (rank:%d) errors: fun %g\n",
+                rank, fun_err);
     }
 }
 
@@ -266,10 +219,6 @@ static void _operator_relax_cuda_gpu(OperatorObject* self, int relax_method,
 
     const double_complex *ph;
     ph = 0;
-
-    if (gpaw_cuda_debug) {
-        debug_operator_allocate(bc, ng, ng2, 1);
-    }
 
     int blocks = 1;
     operator_alloc_buffers(self, blocks);
@@ -302,9 +251,6 @@ static void _operator_relax_cuda_gpu(OperatorObject* self, int relax_method,
         cudaEventRecord(operator_event[1], 0);
 
     for (int n=0; n < nrelax; n++ ) {
-        if (gpaw_cuda_debug) {
-            debug_operator_memcpy_pre(fun, src, ng);
-        }
         if (cuda_overlap) {
             cudaStreamWaitEvent(operator_stream[0], operator_event[1], 0);
             bc_unpack_paste_cuda_gpu(bc, fun, operator_buf_gpu, recvreq,
@@ -337,16 +283,6 @@ static void _operator_relax_cuda_gpu(OperatorObject* self, int relax_method,
                                 operator_buf_gpu, fun, src, w,
                                 GPAW_BOUNDARY_NORMAL, 0);
         }
-
-        if (gpaw_cuda_debug) {
-            cudaDeviceSynchronize();
-            debug_operator_memcpy_post(fun, operator_buf_gpu, ng, ng2);
-            debug_operator_relax(self, relax_method, w, n);
-        }
-    }
-
-    if (gpaw_cuda_debug) {
-        debug_operator_deallocate();
     }
 
     if (cuda_overlap) {
@@ -370,7 +306,19 @@ PyObject* Operator_relax_cuda_gpu(OperatorObject* self, PyObject* args)
     double *fun = (double*) func_gpu;
     const double *src = (double*) source_gpu;
 
+    if (gpaw_cuda_debug) {
+        debug_operator_allocate(self, 1, 1);
+        debug_operator_memcpy_pre(fun, src);
+    }
+
     _operator_relax_cuda_gpu(self, relax_method, fun, src, nrelax, w);
+
+    if (gpaw_cuda_debug) {
+        cudaDeviceSynchronize();
+        debug_operator_memcpy_post(fun, operator_buf_gpu);
+        debug_operator_relax(self, relax_method, w, nrelax);
+        debug_operator_deallocate();
+    }
 
     if (PyErr_Occurred())
         return NULL;
@@ -378,9 +326,75 @@ PyObject* Operator_relax_cuda_gpu(OperatorObject* self, PyObject* args)
         Py_RETURN_NONE;
 }
 
+void debug_operator_apply(OperatorObject* self, const double_complex *ph,
+                          bool real, int nin, int blocks)
+{
+    MPI_Request recvreq[2];
+    MPI_Request sendreq[2];
+    int i;
+    double err;
+
+    boundary_conditions* bc = self->bc;
+    const int *size1 = bc->size1;
+    const int *size2 = bc->size2;
+    int ng = bc->ndouble * size1[0] * size1[1] * size1[2];
+    int ng2 = bc->ndouble * size2[0] * size2[1] * size2[2];
+
+    for (int n=0; n < nin; n += blocks) {
+        const double *in = debug_in_cpu + n * ng;
+        double *out = debug_out_cpu + n * ng;
+        int myblocks = MIN(blocks, nin - n);
+        for (i=0; i < 3; i++) {
+            bc_unpack1(bc, in, debug_buf_cpu, i, recvreq, sendreq,
+                    debug_recvbuf, debug_sendbuf, ph + 2 * i, 0, myblocks);
+            bc_unpack2(bc, debug_buf_cpu, i, recvreq, sendreq,
+                    debug_recvbuf, myblocks);
+        }
+        for (int m=0; m < myblocks; m++) {
+            if (real)
+                bmgs_fd(&self->stencil, debug_buf_cpu + m * ng2,
+                        out + m * ng);
+            else
+                bmgs_fdz(&self->stencil,
+                        (const double_complex*) (debug_buf_cpu + m * ng2),
+                        (double_complex*) (out + m * ng));
+        }
+    }
+
+    double buf_err = 0;
+    int buf_err_n = 0;
+    for (i = 0; i < debug_size_buf; i++) {
+        err = fabs(debug_buf_cpu[i] - debug_buf_gpu[i]);
+        if (err > GPAW_CUDA_ABS_TOL)
+            buf_err_n++;
+        buf_err = MAX(buf_err, err);
+    }
+    double out_err = 0;
+    int out_err_n = 0;
+    for (i = 0; i < debug_size_arr; i++) {
+        err = fabs(debug_out_cpu[i] - debug_out_gpu[i]);
+        if (err > GPAW_CUDA_ABS_TOL)
+            out_err_n++;
+        out_err = MAX(out_err, err);
+    }
+    int rank = 0;
+    if (bc->comm != MPI_COMM_NULL)
+        MPI_Comm_rank(bc->comm, &rank);
+    if (buf_err > GPAW_CUDA_ABS_TOL) {
+        printf("Debug cuda operator fd bc (rank:%d) errors: buf %g count %d/%d\n",
+               rank, buf_err, buf_err_n, debug_size_buf);
+        fflush(stdout);
+    }
+    if (out_err > GPAW_CUDA_ABS_TOL) {
+        printf("Debug cuda operator fd (rank:%d) errors: out %g count %d/%d\n",
+               rank, out_err, out_err_n, debug_size_arr);
+        fflush(stdout);
+    }
+}
+
 static void _operator_apply_cuda_gpu(OperatorObject *self,
                                      const double *in, double *out,
-                                     int nin, bool real,
+                                     int nin, int blocks, bool real,
                                      const double_complex *ph)
 {
     boundary_conditions* bc = self->bc;
@@ -391,19 +405,6 @@ static void _operator_apply_cuda_gpu(OperatorObject *self,
 
     MPI_Request recvreq[3][2];
     MPI_Request sendreq[3][2];
-
-    int mpi_size = 1;
-    if ((bc->maxsend || bc->maxrecv) && bc->comm != MPI_COMM_NULL) {
-        MPI_Comm_size(bc->comm, &mpi_size);
-    }
-    int blocks = MAX(MIN(MIN(mpi_size * (GPAW_CUDA_BLOCKS_MIN),
-                             (GPAW_CUDA_BLOCKS_MAX) / bc->ndouble),
-                         nin),
-                     1);
-
-    if (gpaw_cuda_debug) {
-        debug_operator_allocate(bc, ng, ng2, blocks);
-    }
 
     operator_alloc_buffers(self, blocks);
 
@@ -438,9 +439,6 @@ static void _operator_apply_cuda_gpu(OperatorObject *self,
         const double *in2 = in + n * ng;
         double *out2 = out + n * ng;
         int myblocks = MIN(blocks, nin - n);
-        if (gpaw_cuda_debug) {
-            debug_operator_memcpy_pre(out2, in2, ng * myblocks);
-        }
         if (cuda_overlap) {
             cudaStreamWaitEvent(operator_stream[0], operator_event[1], 0);
             bc_unpack_paste_cuda_gpu(bc, in2, operator_buf_gpu, recvreq,
@@ -494,17 +492,6 @@ static void _operator_apply_cuda_gpu(OperatorObject *self,
                                   GPAW_BOUNDARY_NORMAL, myblocks, 0);
             }
         }
-
-        if (gpaw_cuda_debug) {
-            cudaDeviceSynchronize();
-            debug_operator_memcpy_post(out2, operator_buf_gpu,
-                                       ng * myblocks, ng2 * myblocks);
-            debug_operator_apply(self, ph, real, n, myblocks);
-        }
-    }
-
-    if (gpaw_cuda_debug) {
-        debug_operator_deallocate();
     }
 
     if (cuda_overlap) {
@@ -540,7 +527,25 @@ PyObject * Operator_apply_cuda_gpu(OperatorObject* self, PyObject* args)
     else
         ph = COMPLEXP(phases);
 
-    _operator_apply_cuda_gpu(self, in, out, nin, real, ph);
+    boundary_conditions* bc = self->bc;
+    int mpi_size = 1;
+    if ((bc->maxsend || bc->maxrecv) && bc->comm != MPI_COMM_NULL) {
+        MPI_Comm_size(bc->comm, &mpi_size);
+    }
+    int blocks = MAX(1, MIN(nin, MIN((GPAW_CUDA_BLOCKS_MIN) * mpi_size,
+                                     (GPAW_CUDA_BLOCKS_MAX) / bc->ndouble)));
+
+    if (gpaw_cuda_debug) {
+        debug_operator_allocate(self, nin, blocks);
+        debug_operator_memcpy_pre(out, in);
+    }
+    _operator_apply_cuda_gpu(self, in, out, nin, blocks, real, ph);
+    if (gpaw_cuda_debug) {
+        cudaDeviceSynchronize();
+        debug_operator_memcpy_post(out, operator_buf_gpu);
+        debug_operator_apply(self, ph, real, nin, blocks);
+        debug_operator_deallocate();
+    }
 
     if (PyErr_Occurred())
         return NULL;
