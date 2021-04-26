307c9d694bc89d0416558f47d4278c13eb4d49e3
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-04-26 08:42:02 +0200

    Check results ...

--- doc/tutorials/rraman/agts.py
+++ doc/tutorials/rraman/agts.py
@@ -5,3 +5,9 @@ def workflow():
     with run(script='H2O_ir.py'):
         with run(script='H2O_rraman_calc.py', cores=4, tmax='1h'):
             run(script='H2O_rraman_spectrum.py')
+            run(function=check)
+
+
+def check():
+    """Read result and make sure it's OK."""
+    ...
