5fde15b930bed4bddccda640dd4ce68b7772e7bb
Martti Louhivuori <martti.louhivuori@csc.fi>
2021-04-28 12:37:32 +0300

    Separate debug code from production code

--- c/cuda/transformers-cuda.c
+++ c/cuda/transformers-cuda.c
@@ -13,15 +13,26 @@
 #undef __TRANSFORMERS_C
 #include "gpaw-cuda.h"
 
-#ifdef DEBUG_CUDA
-#define DEBUG_CUDA_TRANSFORMER
-#endif //DEBUG_CUDA
+extern int gpaw_cuda_debug;
 
-static double *transformer_buf_gpu=NULL;
-static int transformer_buf_size=0;
-static int transformer_buf_max=0;
-static int transformer_init_count=0;
+static double *transformer_buf_gpu = NULL;
+static int transformer_buf_size = 0;
+static int transformer_buf_max = 0;
+static int transformer_init_count = 0;
 
+static int debug_size_in = 0;
+static int debug_size_out = 0;
+static int debug_size_buf = 0;
+static int debug_size_buf_out = 0;
+static double *debug_sendbuf;
+static double *debug_recvbuf;
+static double *debug_buf_cpu;
+static double *debug_buf_gpu;
+static double *debug_buf_out;
+static double *debug_out_cpu;
+static double *debug_out_gpu;
+static double *debug_in_cpu;
+static double *debug_buf_store;
 
 void transformer_init_cuda(TransformerObject *self)
 {
@@ -71,6 +82,236 @@ void transformer_dealloc_cuda(int force)
         transformer_init_count--;
 }
 
+void debug_transformer_allocate(TransformerObject* self, int nin, int blocks)
+{
+    boundary_conditions* bc = self->bc;
+    const int *size1 = bc->size1;
+    const int *size2 = bc->size2;
+    int ng = bc->ndouble * size1[0] * size1[1] * size1[2];
+    int ng2 = bc->ndouble * size2[0] * size2[1] * size2[2];
+    int out_ng = bc->ndouble * self->size_out[0] * self->size_out[1]
+               * self->size_out[2];
+
+    debug_size_in = ng * nin;
+    debug_size_out = out_ng * nin;
+    debug_size_buf = ng2 * blocks;
+    debug_size_buf_out = MAX(out_ng, ng2) * blocks;
+
+    debug_sendbuf = GPAW_MALLOC(double, bc->maxsend * blocks * bc->ndouble);
+    debug_recvbuf = GPAW_MALLOC(double, bc->maxrecv * blocks * bc->ndouble);
+    debug_buf_cpu = GPAW_MALLOC(double, debug_size_buf);
+    debug_buf_gpu = GPAW_MALLOC(double, debug_size_buf);
+    debug_buf_out = GPAW_MALLOC(double, debug_size_buf_out);
+    debug_out_cpu = GPAW_MALLOC(double, debug_size_out);
+    debug_out_gpu = GPAW_MALLOC(double, debug_size_out);
+    debug_in_cpu = GPAW_MALLOC(double, debug_size_in);
+    debug_buf_store = GPAW_MALLOC(double,
+                                  debug_size_buf * ((nin / blocks) + 1));
+}
+
+void debug_transformer_deallocate()
+{
+    free(debug_sendbuf);
+    free(debug_recvbuf);
+    free(debug_buf_cpu);
+    free(debug_buf_gpu);
+    free(debug_buf_out);
+    free(debug_out_cpu);
+    free(debug_out_gpu);
+    free(debug_in_cpu);
+    free(debug_buf_store);
+    debug_size_in = 0;
+    debug_size_out = 0;
+    debug_size_buf = 0;
+    debug_size_buf_out = 0;
+}
+
+void debug_transformer_memcpy_pre(const double *in, double *out)
+{
+    GPAW_CUDAMEMCPY(debug_in_cpu, in, double, debug_size_in,
+                    cudaMemcpyDeviceToHost);
+    GPAW_CUDAMEMCPY(debug_out_cpu, out, double, debug_size_out,
+                    cudaMemcpyDeviceToHost);
+}
+
+void debug_transformer_memcpy_post(double *out, double *buf)
+{
+    GPAW_CUDAMEMCPY(debug_out_gpu, out, double, debug_size_out,
+                    cudaMemcpyDeviceToHost);
+    GPAW_CUDAMEMCPY(debug_buf_gpu, buf, double, debug_size_buf,
+                    cudaMemcpyDeviceToHost);
+}
+
+/*
+ * Run the interpolate and restrict algorithm (see transapply_worker()
+ * in ../transformers.c) on the CPU and compare to results from the GPU.
+ */
+void debug_transformer_apply(TransformerObject* self,
+                             int nin, int blocks, bool real,
+                             const double_complex *ph)
+{
+    MPI_Request recvreq[2];
+    MPI_Request sendreq[2];
+    int i;
+    double err;
+
+    boundary_conditions* bc = self->bc;
+    const int *size1 = bc->size1;
+    const int *size2 = bc->size2;
+    int ng = bc->ndouble * size1[0] * size1[1] * size1[2];
+    int ng2 = bc->ndouble * size2[0] * size2[1] * size2[2];
+    int out_ng = bc->ndouble * self->size_out[0] * self->size_out[1]
+               * self->size_out[2];
+    int rank = 0;
+    if (bc->comm != MPI_COMM_NULL)
+        MPI_Comm_rank(bc->comm, &rank);
+
+    int last;
+    for (int n=0; n < nin; n += blocks) {
+        const double *in = debug_in_cpu + n * ng;
+        double *out = debug_out_cpu + n * out_ng;
+        int myblocks = MIN(blocks, nin - n);
+        last = myblocks;
+        for (int i=0; i < 3; i++) {
+            bc_unpack1(bc, in, debug_buf_cpu, i, recvreq, sendreq,
+                       debug_recvbuf, debug_sendbuf, ph + 2 * i, 0, myblocks);
+            bc_unpack2(bc, debug_buf_cpu, i, recvreq, sendreq,
+                       debug_recvbuf, myblocks);
+        }
+        double buf_err_un = 0;
+        double *store = debug_buf_store + n * ng2 * blocks;
+        for (int i=0; i < ng2 * myblocks; i++) {
+            buf_err_un = MAX(buf_err_un, fabs(debug_buf_cpu[i] - store[i]));
+        }
+        if (buf_err_un > GPAW_CUDA_ABS_TOL) {
+            fprintf(stderr,
+                    "[%d] Debug CUDA transformer apply (buf/unpack): error %g\n",
+                    rank, buf_err_un);
+        }
+        for (int m=0; m < myblocks; m++) {
+            if (real) {
+                if (self->interpolate)
+                    bmgs_interpolate(self->k, self->skip,
+                                     debug_buf_cpu + m * ng2,
+                                     bc->size2, debug_out_cpu + m * out_ng,
+                                     debug_buf_out + m * MAX(out_ng, ng2));
+                else
+                    bmgs_restrict(self->k, debug_buf_cpu + m * ng2,
+                                  bc->size2, debug_out_cpu + m * out_ng,
+                                  debug_buf_out + m * MAX(out_ng, ng2));
+            } else {
+                if (self->interpolate)
+                    bmgs_interpolatez(
+                            self->k, self->skip,
+                            (double_complex*) (debug_buf_cpu + m * ng2),
+                            bc->size2,
+                            (double_complex*) (debug_out_cpu + m * out_ng),
+                            (double_complex*) (debug_buf_out
+                                               + m * MAX(out_ng, ng2)));
+                else
+                    bmgs_restrictz(
+                            self->k,
+                            (double_complex*) (debug_buf_cpu + m * ng2),
+                            bc->size2,
+                            (double_complex*) (debug_out_cpu + m * out_ng),
+                            (double_complex*) (debug_buf_out
+                                               + m * MAX(out_ng, ng2)));
+            }
+        }
+    }
+
+    int buf_err_n = 0;
+    double buf_err = 0;
+    for (i=0; i < debug_size_buf; i++) {
+        err = fabs(debug_buf_cpu[i] - debug_buf_gpu[i]);
+        if (err > GPAW_CUDA_ABS_TOL) {
+            buf_err = MAX(buf_err, err);
+            buf_err_n += 1;
+        }
+    }
+    double out_err = 0;
+    for (i=0; i < debug_size_out; i++) {
+        out_err = MAX(out_err, fabs(debug_out_cpu[i] - debug_out_gpu[i]));
+    }
+    if (buf_err > GPAW_CUDA_ABS_TOL) {
+        fprintf(stderr,
+                "[%d] Debug CUDA transformer apply (buf): error %g (%d/%d)\n",
+                rank, buf_err, buf_err_n, debug_size_buf);
+    }
+    if (out_err > GPAW_CUDA_ABS_TOL) {
+        fprintf(stderr,
+                "[%d] Debug CUDA transformer apply (out): error %g\n",
+                rank, out_err);
+    }
+}
+
+/*
+ * Run the interpolate and restrict algorithm (see transapply_worker()
+ * in ../transformers.c) on the GPU.
+ */
+static void _transformer_apply_cuda_gpu(TransformerObject* self,
+                                        const double *in, double *out,
+                                        int nin, int blocks, bool real,
+                                        const double_complex *ph)
+{
+    boundary_conditions* bc = self->bc;
+    const int* size1 = bc->size1;
+    const int* size2 = self->bc->size2;
+    int ng = bc->ndouble * size1[0] * size1[1] * size1[2];
+    int ng2 = bc->ndouble * size2[0] * size2[1] * size2[2];
+    int out_ng = bc->ndouble * self->size_out[0] * self->size_out[1]
+               * self->size_out[2];
+
+    int mpi_size = 1;
+    if ((bc->maxsend || bc->maxrecv) && bc->comm != MPI_COMM_NULL)
+        MPI_Comm_size(bc->comm, &mpi_size);
+
+    MPI_Request recvreq[3][2];
+    MPI_Request sendreq[3][2];
+
+    transformer_init_buffers(self, blocks);
+
+    double* buf = transformer_buf_gpu;
+
+    for (int n = 0; n < nin; n += blocks) {
+        const double* in2 = in + n * ng;
+        double* out2 = out + n * out_ng;
+        int myblocks = MIN(blocks, nin - n);
+
+        bc_unpack_paste_cuda_gpu(bc, in2, buf, recvreq, 0, myblocks);
+        for (int i=0; i < 3; i++) {
+            bc_unpack_cuda_gpu(bc, in2, buf, i, recvreq, sendreq[i],
+                               ph + 2 * i, 0, myblocks);
+        }
+        GPAW_CUDAMEMCPY(debug_buf_store + n * ng2 * blocks, buf, double,
+                        debug_size_buf, cudaMemcpyDeviceToHost);
+        if (self->interpolate) {
+            if (real) {
+                bmgs_interpolate_cuda_gpu(self->k, self->skip, buf,
+                                          bc->size2, out2, self->size_out,
+                                          myblocks);
+            } else {
+                bmgs_interpolate_cuda_gpuz(self->k, self->skip,
+                                           (cuDoubleComplex*) (buf),
+                                           bc->size2,
+                                           (cuDoubleComplex*) (out2),
+                                           self->size_out, myblocks);
+            }
+        } else {
+            if (real) {
+                bmgs_restrict_cuda_gpu(self->k, buf, bc->size2,
+                                       out2, self->size_out, myblocks);
+            } else {
+                bmgs_restrict_cuda_gpuz(self->k,
+                                        (cuDoubleComplex*) (buf),
+                                        bc->size2,
+                                        (cuDoubleComplex*) (out2),
+                                        self->size_out, myblocks);
+            }
+        }
+    }
+}
+
 PyObject* Transformer_apply_cuda_gpu(TransformerObject *self, PyObject *args)
 {
     PyArrayObject* phases = 0;
@@ -87,176 +328,33 @@ PyObject* Transformer_apply_cuda_gpu(TransformerObject *self, PyObject *args)
     if (PyTuple_Size(shape) == 4)
         nin = (int) PyLong_AsLong(PyTuple_GetItem(shape, 0));
 
-    boundary_conditions* bc = self->bc;
-    const int* size1 = bc->size1;
-    const int* size2 = self->bc->size2;
-
-    int ng = bc->ndouble * size1[0] * size1[1] * size1[2];
-    int ng2 = bc->ndouble * size2[0] * size2[1] * size2[2];
-
     const double* in = (double*) input_gpu;
     double* out = (double*) output_gpu;
 
     bool real = (type->type_num == NPY_DOUBLE);
     const double_complex* ph = (real ? 0 : COMPLEXP(phases));
 
-    int out_ng = bc->ndouble * self->size_out[0] * self->size_out[1]
-               * self->size_out[2];
-
+    boundary_conditions* bc = self->bc;
     int mpi_size = 1;
     if ((bc->maxsend || bc->maxrecv) && bc->comm != MPI_COMM_NULL)
         MPI_Comm_size(bc->comm, &mpi_size);
 
-    int blocks = MAX(MIN(MIN(mpi_size * (GPAW_CUDA_BLOCKS_MIN),
-                             GPAW_CUDA_BLOCKS_MAX / bc->ndouble),
-                         nin), 1);
+    int blocks = MAX(1, MIN(nin, MIN((GPAW_CUDA_BLOCKS_MIN) * mpi_size,
+                                     (GPAW_CUDA_BLOCKS_MAX) / bc->ndouble)));
 
-    MPI_Request recvreq[3][2];
-    MPI_Request sendreq[3][2];
-#ifdef DEBUG_CUDA_TRANSFORMER
-    MPI_Request recvreq_cpu[2];
-    MPI_Request sendreq_cpu[2];
-
-    double* sendbuf_cpu = GPAW_MALLOC(double, bc->maxsend * blocks);
-    double* recvbuf_cpu = GPAW_MALLOC(double, bc->maxrecv * blocks);
-    double* buf_cpu = GPAW_MALLOC(double, ng2 * blocks);
-    double* buf2_cpu = GPAW_MALLOC(double, MAX(out_ng, ng2) * blocks);
-    double* in_cpu = GPAW_MALLOC(double, ng * blocks);
-    double* out_cpu = GPAW_MALLOC(double, out_ng * blocks);
-    double* buf_cpu2 = GPAW_MALLOC(double, ng2 * blocks);
-    double* out_cpu2 = GPAW_MALLOC(double, out_ng * blocks);
-#endif //DEBUG_CUDA_TRANSFORMER
-
-    transformer_init_buffers(self, blocks);
-
-    double* buf = transformer_buf_gpu;
-
-    for (int n = 0; n < nin; n += blocks) {
-        const double* in2 = in + n * ng;
-        double* out2 = out + n * out_ng;
-        int myblocks = MIN(blocks, nin - n);
-#ifdef DEBUG_CUDA_TRANSFORMER
-        GPAW_CUDAMEMCPY(in_cpu, in2, double, ng * myblocks,
-                        cudaMemcpyDeviceToHost);
-        GPAW_CUDAMEMCPY(out_cpu, out2, double, out_ng * myblocks,
-                        cudaMemcpyDeviceToHost);
-#endif //DEBUG_CUDA_TRANSFORMER
-        if (self->interpolate) {
-            bc_unpack_paste_cuda_gpu(bc, in2, buf, recvreq, 0, myblocks);
-            for (int i=0; i < 3; i++) {
-                bc_unpack_cuda_gpu(bc, in2, buf, i, recvreq, sendreq[i],
-                                   ph + 2 * i, 0, myblocks);
-            }
-#ifdef DEBUG_CUDA_TRANSFORMER
-            GPAW_CUDAMEMCPY(buf_cpu2, transformer_buf_gpu, double,
-                            ng2 * myblocks, cudaMemcpyDeviceToHost);
-#endif //DEBUG_CUDA_TRANSFORMER
-            if (real) {
-                bmgs_interpolate_cuda_gpu(self->k, self->skip, buf,
-                                          bc->size2, out2, self->size_out,
-                                          myblocks);
-            } else {
-                bmgs_interpolate_cuda_gpuz(self->k, self->skip,
-                                           (cuDoubleComplex*) (buf),
-                                           bc->size2,
-                                           (cuDoubleComplex*) (out2),
-                                           self->size_out, myblocks);
-            }
-        } else {
-            bc_unpack_paste_cuda_gpu(bc, in2, buf, recvreq, 0, myblocks);
-            for (int i=0; i < 3; i++) {
-                bc_unpack_cuda_gpu(bc, in2, buf, i, recvreq, sendreq[i],
-                        ph + 2 * i, 0, myblocks);
-            }
-#ifdef DEBUG_CUDA_TRANSFORMER
-            GPAW_CUDAMEMCPY(buf_cpu2, transformer_buf_gpu, double,
-                            ng2 * myblocks, cudaMemcpyDeviceToHost);
-#endif //DEBUG_CUDA_TRANSFORMER
-            if (real) {
-                bmgs_restrict_cuda_gpu(self->k, buf, bc->size2,
-                                       out2, self->size_out, myblocks);
-            } else {
-                bmgs_restrict_cuda_gpuz(self->k,
-                                        (cuDoubleComplex*) (buf),
-                                        bc->size2,
-                                        (cuDoubleComplex*) (out2),
-                                        self->size_out, myblocks);
-            }
-        }
-
-#ifdef DEBUG_CUDA_TRANSFORMER
-        for (int i=0; i < 3; i++) {
-            bc_unpack1(bc, in_cpu, buf_cpu, i, recvreq_cpu, sendreq_cpu,
-                       recvbuf_cpu, sendbuf_cpu, ph + 2 * i, 0, myblocks);
-            bc_unpack2(bc, buf_cpu, i, recvreq_cpu, sendreq_cpu,
-                       recvbuf_cpu, myblocks);
-        }
-        double buf_err = 0;
-        for (int i=0; i < ng2 * myblocks; i++) {
-            buf_err = MAX(buf_err, fabs(buf_cpu[i] - buf_cpu2[i]));
-        }
-        for (int m=0; m < myblocks; m++) {
-            if (real) {
-                if (self->interpolate)
-                    bmgs_interpolate(self->k, self->skip, buf_cpu2 + m * ng2,
-                                     bc->size2, out_cpu + m * out_ng,
-                                     buf2_cpu + m * MAX(out_ng, ng2));
-                else
-                    bmgs_restrict(self->k, buf_cpu2 + m * ng2,
-                                  bc->size2, out_cpu + m * out_ng,
-                                  buf2_cpu + m * MAX(out_ng, ng2));
-            } else {
-                if (self->interpolate)
-                    bmgs_interpolatez(
-                            self->k, self->skip,
-                            (double_complex*) (buf_cpu2 + m * ng2),
-                            bc->size2,
-                            (double_complex*) (out_cpu + m * out_ng),
-                            (double_complex*) (buf2_cpu + m
-                                               * MAX(out_ng, ng2)));
-                else
-                    bmgs_restrictz(
-                            self->k,
-                            (double_complex*) (buf_cpu2 + m * ng2),
-                            bc->size2,
-                            (double_complex*) (out_cpu + m * out_ng),
-                            (double_complex*) (buf2_cpu + m
-                                               * MAX(out_ng, ng2)));
-            }
-        }
-        GPAW_CUDAMEMCPY(out_cpu2, out2, double, out_ng * myblocks,
-                        cudaMemcpyDeviceToHost);
-
-        double out_err = 0;
-        for (int i=0; i < out_ng * myblocks; i++) {
-            out_err = MAX(out_err, fabs(out_cpu[i] - out_cpu2[i]));
-        }
-        int rank = 0;
-        if (bc->comm != MPI_COMM_NULL)
-            MPI_Comm_rank(bc->comm, &rank);
-        if (buf_err > GPAW_CUDA_ABS_TOL) {
-            fprintf(stderr,
-                    "Debug cuda transformer bc (n:%d rank:%d) errors: buf %g\n",
-                    n, rank, buf_err);
-        }
-        if (out_err>GPAW_CUDA_ABS_TOL) {
-            fprintf(stderr,
-                    "Debug cuda transformer (n:%d rank:%d) errors: out %g\n",
-                    n, rank, out_err);
-        }
-#endif //DEBUG_CUDA_TRANSFORMER
+    if (gpaw_cuda_debug) {
+        debug_transformer_allocate(self, nin, blocks);
+        debug_transformer_memcpy_pre(in, out);
     }
 
-#ifdef DEBUG_CUDA_TRANSFORMER
-    free(sendbuf_cpu);
-    free(recvbuf_cpu);
-    free(buf_cpu);
-    free(buf2_cpu);
-    free(in_cpu);
-    free(out_cpu);
-    free(buf_cpu2);
-    free(out_cpu2);
-#endif //DEBUG_CUDA_TRANSFORMER
+    _transformer_apply_cuda_gpu(self, in, out, nin, blocks, real, ph);
+
+    if (gpaw_cuda_debug) {
+        cudaDeviceSynchronize();
+        debug_transformer_memcpy_post(out, transformer_buf_gpu);
+        debug_transformer_apply(self, nin, blocks, real, ph);
+        debug_transformer_deallocate();
+    }
 
     if (PyErr_Occurred())
         return NULL;
