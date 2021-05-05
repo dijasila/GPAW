b58681d05a8635a2a04b99ee0ce3a581043975cc
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2021-05-05 13:11:05 +0300

    flake8 fixes

--- gpaw/test/utilities/test_elf.py
+++ gpaw/test/utilities/test_elf.py
@@ -1,26 +1,19 @@
 import numpy as np
-from ase import Atoms
 from ase.parallel import parprint
 from gpaw import GPAW, restart
 from gpaw.elf import ELF
 from gpaw.test import equal
-from gpaw.mpi import rank, world
+from gpaw.mpi import rank
 
 
 def test_utilities_elf(gpw_files):
     # Real wave functions
     atoms, calc = restart(gpw_files['h2_fd'])
     
-    energy = atoms.get_potential_energy()
     elf = ELF(calc)
     elf.update()
     elf_G = elf.get_electronic_localization_function(gridrefinement=1)
     elf_g = elf.get_electronic_localization_function(gridrefinement=2)
-
-    nt_G = calc.density.nt_sG[0]
-    taut_G = elf.taut_sG[0]
-    nt_grad2_G = elf.nt_grad2_sG[0]
-    nt_grad2_g = elf.nt_grad2_sg[0]
 
     # integrate the H2 bond
     if rank == 0:
@@ -47,7 +40,6 @@ def test_utilities_elf(gpw_files):
     #   The tested values (< r7887) do not seem to be correct
         equal(int1, 14.579199, 0.0001)
         equal(int2, 18.936101, 0.0001)
-
 
     # Complex wave functions
     calc = GPAW(gpw_files['bcc_li_fd'])
