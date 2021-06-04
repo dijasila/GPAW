d5d17ef0964f29c69fcb8a10e964415672feffb9
Robert Warmbier <rwarmbier@uj.ac.za>
2021-06-04 09:58:54 +0200

    cleanup

--- gpaw/test/lcao/test_nabla.py
+++ gpaw/test/lcao/test_nabla.py
@@ -7,6 +7,7 @@ from gpaw.fd_operators import Gradient
 
 
 DIR = 2  # z direction
+# np.set_printoptions(precision=5, suppress=1, linewidth=120)
 
 
 @pytest.fixture
@@ -35,17 +36,11 @@ def get_nabla_fd(gd, kpt, psit_nG):
 
 def get_nabla_lcao(wfs):
     gd = wfs.gd
-    kpt = wfs.kpt_u[0]
-
+    kd = wfs.kd
     dThetadR_qvMM, dTdR_qvMM = wfs._get_overlap_derivatives()
-    print(dThetadR_qvMM.shape)
-    # If using BLACS initialize lower triangle.
-    #if wfs.ksl.using_blacs:
-    #    for i in range(dThetadR_qvMM.shape[-1]):
-    #        dThetadR_qvMM[:, :, i, i:] = -dThetadR_qvMM[:, :, i:, i]
 
-    nabla_skvnm = np.zeros((wfs.nspins, wfs.kd.nibzkpts, 3, wfs.bd.nbands,
-        wfs.bd.nbands), dtype=wfs.dtype)
+    nabla_skvnm = np.zeros((wfs.nspins, kd.nibzkpts, 3, wfs.bd.nbands,
+                            wfs.bd.nbands), dtype=wfs.dtype)
 
     for kpt in wfs.kpt_u:
         C_nM = kpt.C_nM
@@ -53,38 +48,33 @@ def get_nabla_lcao(wfs):
         for v in range(3):
             # We want C^dagger · dTheta/dR · C
             dThetadRv_MM = dThetadR_qvMM[kpt.q, v]
-
-        # import numpy as np
-        # np.set_printoptions(precision=5, suppress=1, linewidth=120)
-
-        nabla_nn_lcao = np.ascontiguousarray((C_nM @ dThetadRz_MM @ C_nM.T.conj()).T)
-        gd.comm.sum(nabla_nn_lcao)
-    
-        nabla_skvnm[kpt.s, kpt.k, v] = nabla_nn_lcao
-
-    return wfs.kd.comm.sum(nabla_skvnm)
+            nabla_nn = np.ascontiguousarray((C_nM @ dThetadRv_MM @
+                                             C_nM.T.conj()).T)
+            gd.comm.sum(nabla_nn)
+            nabla_skvnm[kpt.s, kpt.k, v] = nabla_nn
+    kd.comm.sum(nabla_skvnm)
+    return nabla_skvnm
 
 
 def test_nabla_matrix(calc):
     wfs = calc.wfs
-    gd = wfs.gd
-    kpt = wfs.kpt_u[0]
-
-    mynbands = wfs.bd.mynbands
-    bfs = wfs.basis_functions
-    psit_nG = gd.zeros(mynbands, dtype=wfs.dtype)
-    bfs.lcao_to_grid(kpt.C_nM, psit_nG, q=kpt.q)
-
-    nabla_nn_lcao = get_nabla_lcao(wfs)
 
     def print0(*args, **kwargs):
         if wfs.world.rank == 0:
             print(*args, **kwargs)
 
+    nabla_nn_lcao = get_nabla_lcao(wfs)[0, 0, DIR]
+
     print0('NABLA_NN_LCAO')
     print0(nabla_nn_lcao)
     print0('biggest', np.abs(nabla_nn_lcao).max())
 
+    gd = wfs.gd
+    kpt = wfs.kpt_u[0]
+    mynbands = wfs.bd.mynbands
+    bfs = wfs.basis_functions
+    psit_nG = gd.zeros(mynbands, dtype=wfs.dtype)
+    bfs.lcao_to_grid(kpt.C_nM, psit_nG, q=kpt.q)
     nabla_fd_nn = get_nabla_fd(gd, kpt, psit_nG)
 
     print0('NABLA_FD_NN')
