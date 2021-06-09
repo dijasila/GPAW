6a869f58209446351c831b73b29852a1004a41d0
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2021-06-09 16:07:50 +0300

    Update AGTS syntax

--- doc/tutorials/circular_dichroism/fd/plot_spectrum.py
+++ doc/tutorials/circular_dichroism/fd/plot_spectrum.py
@@ -1,4 +1,4 @@
-# Creates: spectrum.png
+# web-page: spectrum.png
 import numpy as np
 import matplotlib.pyplot as plt
 
--- doc/tutorials/circular_dichroism/lcao/plot_spectra.py
+++ doc/tutorials/circular_dichroism/lcao/plot_spectra.py
@@ -1,4 +1,4 @@
-# Creates: spectra.png
+# web-page: spectra.png
 import numpy as np
 import matplotlib.pyplot as plt
 
