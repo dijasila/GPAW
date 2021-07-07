d4d27bf38f0ac78da5eb840a97934ca353832df1
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-07-07 13:45:39 +0200

    Use more time (10m not enough)

--- doc/tutorials/elphraman/agts.py
+++ doc/tutorials/elphraman/agts.py
@@ -3,10 +3,10 @@ from myqueue.workflow import run
 
 def workflow():
     ph = run(script='phonon.py')
-    el = run(script='elph.py')
+    el = run(script='elph.py', tmax='1h')
     mo = run(script='momentum_matrix.py')
     with ph, el:
-        with run(script='supercell_matrix.py'):
+        with run(script='supercell_matrix.py', tmax='1h'):
             with run(script='elph_matrix.py'), mo:
                 run(script='raman_intensities.py')
                 run(function=check)
@@ -15,6 +15,6 @@ def workflow():
 def check():
     """Read result and make sure it's OK."""
     import numpy as np
-    ri = np.load("RI_xz_632nm.npy")
+    ri = np.load('RI_xz_632nm.npy')
     assert ri[1].argmax() == 1304
     assert np.isclose(max(ri[1]), 4.8486e-5, rtol=1e-3)
