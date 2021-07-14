750fa657b4e932af85e91e5c0499cf1f3ab389a6
giale <gianl.levi@gmail.com>
2021-07-14 15:13:40 +0000

    Add test of MOM with DirectMinLCAO eigensolver

---
+++ gpaw/test/directopt/test_mom_directopt_lcao_forces.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pytest
+from ase import Atoms
+from gpaw import GPAW, restart
+from gpaw.mom import prepare_mom_calculation
+from gpaw.directmin.lcao.directmin_lcao import DirectMinLCAO
+
+
+@pytest.mark.mom
+def test_mom_directopt_lcao_forces(in_tmp_dir):
+    f_sn = []
+    for s in range(2):
+        f_sn.append([1. if i < 5 else 0. for i in range(26)])
+    f_sn[0][4] = 0.
+    f_sn[0][5] = 1.
+
+    L = 6.0
+    d = 1.13
+    delta = 0.01
+
+    atoms = Atoms('CO',
+                  [[0.5 * L, 0.5 * L, 0.5 * L - 0.5 * d],
+                   [0.5 * L, 0.5 * L, 0.5 * L + 0.5 * d]])
+    atoms.set_cell([L, L, L])
+
+    calc = GPAW(mode='lcao',
+                basis='dzp',
+                h=0.20,
+                xc='PBE',
+                spinpol=True,
+                symmetry='off',
+                occupations={'name': 'fixed-uniform'},
+                eigensolver='direct-min-lcao',
+                mixer={'backend': 'no-mixing'},
+                nbands='nao',
+                convergence={'energy': 100,
+                             'density': 1e-3,
+                             'eigenstates': 1e-3})
+    atoms.calc = calc
+    atoms.get_potential_energy()
+
+    calc.set(eigensolver=DirectMinLCAO(searchdir_algo={'name': 'LSR1P',
+                                                       'method': 'LSR1'},
+                                       linesearch_algo={'name': 'UnitStep'}))
+    prepare_mom_calculation(calc, atoms, f_sn)
+    F = atoms.get_forces()
+
+    # Test overlaps
+    calc.wfs.occupations.initialize_reference_orbitals()
+    for kpt in calc.wfs.kpt_u:
+        f_n = calc.get_occupation_numbers(spin=kpt.s)
+        unoccupied = [True for _ in range(len(f_n))]
+        P = calc.wfs.occupations.calculate_weights(kpt, 1.0, unoccupied)
+        assert (np.allclose(P, f_n))
+
+    calc.write('co.gpw', mode='all')
+
+    E = []
+    for i in [-1, 1]:
+        atoms, calc = restart('co.gpw', txt='-')
+        p = atoms.positions.copy()
+        p[0, 2] -= delta / 2. * i
+        p[1, 2] += delta / 2. * i
+        atoms.set_positions(p)
+        E.append(atoms.get_potential_energy())
+
+    f = np.sqrt(((F[1, :] - F[0, :])**2).sum()) * 0.5
+    fnum = (E[0] - E[1]) / (2. * delta)     # central difference
+
+    print(fnum)
+    assert fnum == pytest.approx(11.9364629545, 0.01)
+    assert f == pytest.approx(fnum, 0.1)
