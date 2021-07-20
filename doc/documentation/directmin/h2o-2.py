57b9f808cf8c092dcb4e4b95409405f63bc10649
alxvov <alxvov@gmail.com>
2021-07-20 11:37:29 +0000

    add documentation

---
+++ doc/documentation/directmin/h2o-2.py
@@ -0,0 +1,26 @@
+from gpaw import GPAW, LCAO
+from ase import Atoms
+import numpy as np
+from gpaw.directmin.lcao.directmin_lcao import DirectMinLCAO
+# Water molecule:
+d = 0.9575
+t = np.pi / 180 * 104.51
+H2O = Atoms('OH2',
+            positions=[(0, 0, 0),
+                       (d, 0, 0),
+                       (d * np.cos(t), d * np.sin(t), 0)])
+H2O.center(vacuum=5.0)
+
+calc = GPAW(mode=LCAO(),
+            basis='dzp',
+            eigensolver=DirectMinLCAO(
+                searchdir_algo={'name':'LBFGS_P', 'memory': 10},
+                representation='u_invar',
+                matrix_exp='egdecomp2'
+            ),
+            occupations={'name': 'fixed-uniform'},
+            mixer={'backend': 'no-mixing'},
+            nbands='nao'
+            )
+H2O.set_calculator(calc)
+H2O.get_potential_energy()
