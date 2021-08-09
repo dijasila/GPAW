5559477d6a76f842da7a6b26f5da5922738559e3
giale <gianl.levi@gmail.com>
2021-08-09 16:38:36 +0000

    PEP8

--- gpaw/test/directopt/test_mom_directopt_lcao_spinpaired.py
+++ gpaw/test/directopt/test_mom_directopt_lcao_spinpaired.py
@@ -10,11 +10,11 @@ from gpaw.directmin.lcao.tools import excite
 @pytest.mark.mom
 def test_mom_directopt_lcao_spinpaired(in_tmp_dir):
     atoms = Atoms('C2H4',
-                  [[ 6.68748500e-01,  2.00680000e-04,  5.55800000e-05],
+                  [[6.68748500e-01, 2.00680000e-04, 5.55800000e-05],
                    [-6.68748570e-01, -2.00860000e-04, -5.51500000e-05],
-                   [ 4.48890600e-01, -5.30146300e-01,  9.32670330e-01],
-                   [ 4.48878120e-01, -5.30176640e-01, -9.32674730e-01],
-                   [-1.24289513e+00,  1.46164400e-02,  9.32559990e-01],
+                   [4.48890600e-01, -5.30146300e-01, 9.32670330e-01],
+                   [4.48878120e-01, -5.30176640e-01, -9.32674730e-01],
+                   [-1.24289513e+00, 1.46164400e-02, 9.32559990e-01],
                    [-1.24286000e+00, -1.46832100e-02, -9.32554970e-01]])
     atoms.center(vacuum=4)
 
@@ -51,4 +51,3 @@ def test_mom_directopt_lcao_spinpaired(in_tmp_dir):
     # These fail if the OccupationsMOM.numbers are not updated correctly
     assert np.all(calc.get_occupation_numbers() <= 2.0)
     assert e == pytest.approx(-21.710807435576783, abs=0.01)
-
