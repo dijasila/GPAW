7579f21ec1f38413abb4b5c90b68c91a4802c2d1
Joachim Sødequist <joaso@dtu.dk>
2021-08-12 13:59:35 +0200

    Fix more paths

--- doc/tutorialsexercises/wavefunctions/wannier/agts.py
+++ doc/tutorialsexercises/wavefunctions/wannier/agts.py
@@ -2,6 +2,6 @@ def workflow():
     from myqueue.workflow import run
     with run(script='si.py', cores=8, tmax='15m'):
         run(script='wannier-si.py')
-        run(script='../dos/dos.py', args=['si.gpw'])
+        run(script='../../electronic/dos/dos.py', args=['si.gpw'])
     with run(script='benzene.py', cores=8, tmax='15m'):
         run(script='wannier-benzene.py')
