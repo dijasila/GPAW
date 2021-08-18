5b5e67e21ab90a7e1805595b84e49f3da89fc54d
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-08-18 15:47:08 +0200

    Fix small buglet

--- gpaw/test/__init__.py
+++ gpaw/test/__init__.py
@@ -57,7 +57,6 @@ def gen(symbol, exx=False, name=None, yukawa_gamma=None,
                           **tf_parameters.get(symbol, {'rcut': 0.9}))
         else:
             setup = g.run(exx=exx, name=name, yukawa_gamma=yukawa_gamma,
-                          use_restart_file=False,
                           write_xml=write_xml,
                           **parameters[symbol])
     setup = mpi.broadcast(setup, 0)
