e5f0920fc2842ccf2c40924b868406bb4a03547f
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2021-08-31 11:43:26 +0300

    Test reading response

--- gpaw/test/gllb/test_fixed_density.py
+++ gpaw/test/gllb/test_fixed_density.py
@@ -1,7 +1,8 @@
 import pytest
+import numpy as np
 from ase.build import molecule
 from gpaw import GPAW
-from gpaw.mpi import world, serial_comm
+from gpaw.mpi import world, serial_comm, broadcast_exception
 
 from gpaw.test.lcaotddft.test_molecule import only_on_master
 
@@ -11,11 +12,17 @@ pytestmark = [pytest.mark.usefixtures('module_tmp_path'),
               ]
 
 
+def check_asp(ref_asp, D_asp, atol=0):
+    assert ref_asp is not D_asp, \
+        'Trying to compare same objects. Is the test broken?'
+    assert np.allclose(ref_asp.toarray(), D_asp.toarray(), atol=atol, rtol=0)
+
+
 @pytest.fixture(scope='module')
 @only_on_master(world)
 def ground_state_calculation():
     comm = serial_comm
-    atoms = molecule('H2')
+    atoms = molecule('H2O')
     atoms.center(vacuum=4)
 
     gs_calc = GPAW(mode='lcao', basis='sz(dzp)', h=0.4,
@@ -26,6 +33,24 @@ def ground_state_calculation():
     atoms.calc = gs_calc
     atoms.get_potential_energy()
     gs_calc.write('gs.gpw', mode='all')
+    return gs_calc.hamiltonian.xc.response
+
+
+def test_read(ground_state_calculation, module_tmp_path):
+    ref_response = ground_state_calculation
+
+    # Read response and collect in master
+    gs_calc = GPAW(module_tmp_path / 'gs.gpw', txt=None)
+    response = gs_calc.hamiltonian.xc.response
+    for D_asp in [response.D_asp, response.Dresp_asp]:
+        D_asp.redistribute(D_asp.partition.as_serial())
+    response.vt_sG = gs_calc.wfs.gd.collect(response.vt_sG)
+
+    with broadcast_exception(world):
+        if world.rank == 0:
+            check_asp(ref_response.D_asp, response.D_asp)
+            check_asp(ref_response.Dresp_asp, response.Dresp_asp)
+            np.allclose(ref_response.vt_sG, response.vt_sG, rtol=0, atol=0)
 
 
 def test_fixed_density(ground_state_calculation, module_tmp_path, in_tmp_dir):
