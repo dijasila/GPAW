b8fe0e948e239028e71d8143ed627c7ce6819f01
giale <gianl.levi@gmail.com>
2021-09-19 21:53:14 +0000

    Refactor reading of saved data, introduce except with ConvergenceError, rename g2 tests script

--- doc/documentation/directmin/agts.py
+++ doc/documentation/directmin/agts.py
@@ -3,9 +3,8 @@ from myqueue.workflow import run
 
 def workflow():
     run(script='h2o.py')
-    r1 = run(script='run_g2_with_dm_ui.py', cores=8, tmax='2h')
-    r2 = run(script='run_g2_with_scf.py', cores=8, tmax='2h')
-    with r1, r2:
+    r1 = run(script='run_g2_with_dm_ui_vs_scf.py', cores=8, tmax='2h')
+    with r1:
         run(script='plot_g2.py')
     r3 = run(script='wm_dm_vs_scf.py', cores=8, tmax='30m')
     with r3:
--- doc/documentation/directmin/run_g2_with_dm_ui_vs_scf.py
+++ doc/documentation/directmin/run_g2_with_dm_ui_vs_scf.py
@@ -1,7 +1,7 @@
 import numpy as np
 from ase.collections import g2
 from doc.documentation.directmin import tests_data
-from gpaw import GPAW, LCAO, FermiDirac
+from gpaw import GPAW, LCAO, FermiDirac, ConvergenceError
 from ase.parallel import paropen
 import time
 from gpaw.directmin.etdm import ETDM
@@ -21,7 +21,8 @@ def read_saved_data(output):
 # Results (total energy, number of iterations) obtained
 # in a previous calculation. Used to compare with the
 # current results.
-saved_data_dm = read_saved_data(tests_data.output_g2_with_dm)
+saved_data = {0: read_saved_data(tests_data.output_g2_with_dm),
+              1: read_saved_data(tests_data.output_g2_with_dm)}
 
 calc_args = {'xc': 'PBE', 'h': 0.15,
              'convergence': {'density': 1.0e-6},
@@ -29,7 +30,7 @@ calc_args = {'xc': 'PBE', 'h': 0.15,
              'mode': LCAO(), 'symmetry': 'off'}
 
 with paropen('dm-g2-results.txt', 'w') as fd:
-    for name in saved_data_dm.keys():
+    for name in saved_data[0].keys():
         atoms = g2[name]
         atoms.center(vacuum=7.0)
         for dm in [0, 1]:
@@ -40,23 +41,27 @@ with paropen('dm-g2-results.txt', 'w') as fd:
                                              representation='u-invar'),
                             mixer={'backend': 'no-mixing'},
                             nbands='nao',
-                            occupations={'name': 'fixed-uniform'},
+                            occupations={'name': 'fixed-uniform'}
                             )
             else:
                 calc = GPAW(**calc_args,
                             txt=name + '_scf.txt',
-                            occupations=FermiDirac(width=0.0, fixmagmom=True),
+                            occupations=FermiDirac(width=0.0, fixmagmom=True)
                             )
             atoms.calc = calc
 
-            t1 = time.time()
-            e = atoms.get_potential_energy()
-            t2 = time.time()
-            assert abs(saved_data_dm[name][0] - e) < 1.0e-2
+            try:
+                t1 = time.time()
+                e = atoms.get_potential_energy()
+                t2 = time.time()
+                assert abs(saved_data[dm][name][0] - e) < 1.0e-2
 
-            steps = atoms.calc.get_number_of_iterations()
-            iters = atoms.calc.wfs.eigensolver.eg_count
-            assert abs(saved_data_dm[name][1] - iters) < 3
+                steps = atoms.calc.get_number_of_iterations()
+                iters = atoms.calc.wfs.eigensolver.eg_count
+                assert abs(saved_data[dm][name][1] - iters) < 3
 
-            print(name + "\t{}".format(iters),
-                  file=fd, flush=True)
+                print(name + "\t{}".format(iters),
+                      file=fd, flush=True)
+            except ConvergenceError:
+                print(name + "\t{}".format(None),
+                      file=fd, flush=True)
