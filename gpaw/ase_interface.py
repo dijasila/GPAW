6210c284c9cf5353f5c7112b7083dfaf12b8c924
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-09-20 17:19:47 +0200

    ...

--- gpaw/ase_interface.py
+++ gpaw/ase_interface.py
@@ -1,11 +1,24 @@
-from typing import Dict, Any
-from gpaw.mpi import MPIComm, world, Parallelization
-from pathlib import Path
+from __future__ import annotations
+
+import os
+import sys
 from contextlib import contextmanager
+from pathlib import Path
+from typing import Any
+from ase.units import Bohr
+
+import numpy as np
+from gpaw.core import UniformGrid
+from gpaw.hybrids import HybridXC
+from gpaw.mpi import MPIComm, Parallelization, world
+from gpaw.new.input_parameters import (InputParameters,
+                                       create_default_parameters)
 
 
 class Logger:
-    def __init__(self, filename='-', comm=None):
+    def __init__(self,
+                 filename='-',
+                 comm: MPIComm = None):
         comm = comm or world
 
         if comm.rank > 0 or filename is None:
@@ -51,8 +64,11 @@ def GPAW(filename=None,
     else:
         txt = '-'
 
+    communicator = communicator or world
+
     log = Logger(txt, communicator)
 
+    parallel = parallel or {}
     parallel = {**parallel, 'world': communicator}
 
     if filename:
@@ -60,6 +76,7 @@ def GPAW(filename=None,
         calculation = Calculation.read(filename, log, parallel)
         return calculation.ase_calculator()
 
+    log('GPAW')
     return ASECalculator(parameters, parallel, log)
 
 
@@ -71,103 +88,167 @@ class ASECalculator:
                  log,
                  calculation=None):
         self.parameters = parameters
+        self.parallel = parallel
+        self.log = log
         self.calculation = calculation
 
     def calculate_property(self, atoms, prop: str):
         if self.calculation is None:
             self.calculation = calculate_ground_state(
                 atoms, self.parameters, self.parallel, self.log)
-        return self.calculation.calculate_property(atoms, prop)
+        try:
+            return self.calculation.calculate_property(atoms, prop)
+        except DrasticChangesError:
+            self.calculation = None
+            return self.calculate_property(atoms, prop)
 
     def get_potential_energy(self, atoms):
-        return self.calculate_property('energy')
+        return self.calculate_property(atoms, 'energy')
+
+
+def create_communicators(world: MPIComm,
+                         nibzkpts: int,
+                         domain: int | tuple[int, int, int] = None,
+                         kpt: int = None,
+                         band: int = None) -> dict[str, MPIComm]:
+    parallelization = Parallelization(world, nibzkpts)
+    if domain is not None:
+        domain = np.prod(domain)
+    parallelization.set(kpt=kpt,
+                        domain=domain,
+                        band=band)
+    comms = parallelization.build_communicators()
+    return comms
+
+
+class FDMode:
+    name = 'fd'
+
+    def create_uniform_grid(self, h, gpts, cell, pbc, symmetry, comm):
+        return UniformGrid(cell=cell, pbc=pbc, size=gpts, comm=comm)
+
+
+class Symmetry:
+    pass
+
+
+class KPoints:
+    def __init__(self, points):
+        self.points = points
+
+
+class MonkhorstPackKPoints(KPoints):
+    def __init__(self, size, shift=(0, 0, 0)):
+        self.size = size
+        self.shift = shift
+        KPoints.__init__(self, np.zeros((1, 3)))
 
 
 def calculate_ground_state(atoms, parameters, parallel, log):
-    params = InputParameters(**paramaters)
+    default_parameters = create_default_parameters()
+    params = InputParameters(parameters, default_parameters)
 
     world = parallel['world']
 
-    xc = params.xc
+    mode = params.mode()
 
-    d = parallel['domain']
-    k = parallel['kpt']
-    b = parallel['band']
+    xc = params.xc()
+
+    setups = params.setups(atoms.numbers,
+                           params.basis.value,
+                           xc.get_setup_name(),
+                           world)
+
+    magmoms = params.magmoms(atoms)
+    collinear = magmoms.ndim == 1
+    if collinear:
+        zmagmoms = magmoms
+        magmoms = np.zeros((len(atoms), 3))
+        magmoms[:, 2] = zmagmoms
+
+    symmetry = params.symmetry(atoms, setups, magmoms)
+
+    kpts = params.kpts(atoms)
+    ibz = 'G'  # symmetry.reduce(kpts)
+
+    d = parallel.pop('domain', None)
+    k = parallel.pop('kpt', None)
+    b = parallel.pop('band', None)
 
     if isinstance(xc, HybridXC):
         d = world.size
 
-    communicators = create_communicators(d, k, b, world)
+    communicators = create_communicators(world, len(ibz), d, k, b)
 
-    mode = params.mode
+    grid = mode.create_uniform_grid(params.h.value,
+                                    params.gpts.value,
+                                    atoms.cell / Bohr,
+                                    atoms.pbc,
+                                    symmetry,
+                                    comm=communicators['d'])
 
-    grid = mode.create_uniform_grid(params.h, params.gpts, communicators['d'])
+    if mode.name == 'fd':
+        filter = create_fourier_filter(grid)
+        # setups = setups.filter(filter)
+
+    # density = ...
 
     if mode.name == 'pw':
-        pw = mode.create_plane_waves(grid)
-        filter = None
-    else:
-        filter = mode.create_filter(grid)
+        _ = mode.create_plane_waves(grid)
 
-    setups = Setups(atoms.numbers,
-                    params.setups,
-                    params.basis,
-                    xc, filter, self.world)
+
+class DrasticChangesError(Exception):
+    """Atoms have changed so much that a fresh start is needed."""
+
+
+def create_fourier_filter(grid):
+    gamma = 1.6
+
+    h = ((grid.icell**2).sum(1)**-0.5 / grid.size).max()
+
+    def filter(rgd, rcut, f_r, l=0):
+        gcut = np.pi / h - 2 / rcut / gamma
+        ftmp = rgd.filter(f_r, rcut * gamma, gcut, l)
+        f_r[:] = ftmp[:len(f_r)]
+
+    return filter
+
+
+def compare_atoms(a1, a2):
+    if len(a1.numbers) != len(a2.numbers) or (a1.numbers != a2.numbers).any():
+        raise DrasticChangesError
+    if (a1.cell - a2.cell).abs().max() > 0.0:
+        raise DrasticChangesError
+    if (a1.pbc != a2.pbc).any():
+        raise DrasticChangesError
+    if (a1.positions - a2.positions).abs().max() > 0.0:
+        return {'positions'}
+    return set()
 
 
 class Calculation:
     def __init__(self, atoms, parameters):
         self.atoms = atoms
-        self.parameters = parameters
+        # self.parameters = parameters
         self.results = {}
-
 
     def calculate_property(self, atoms, prop):
         changes = compare_atoms(self.atoms, atoms)
-        if not changes:
-            if prop in self.results:
-                return self.results[prop]
-            self.
+        if changes:
+            self.recompute_ground_state()
+
+        if prop in self.results:
+            return self.results[prop]
+
+        if prop == 'forces':
+            self.calculate_forces()
+        else:
+            1 / 0
+
+        return self.results[prop]
 
 
-default_parameters: Dict[str, Any] = {
-    'mode': 'fd',
-    'h': None,  # Angstrom
-    'gpts': None,
-    'kpts': [(0.0, 0.0, 0.0)],
-    'nbands': None,
-    'charge': 0,
-    'magmoms': None,
-    'symmetry': {'point_group': True,
-                 'time_reversal': True,
-                 'symmorphic': True,
-                 'tolerance': 1e-7,
-                 'do_not_symmetrize_the_density': None},  # deprecated
-    'soc': None,
-    'background_charge': None,
-    'setups': {},
-    'basis': {},
-    'spinpol': None,
-    'xc': 'LDA',
-
-    'occupations': None,
-    'poissonsolver': None,
-    'mixer': None,
-    'eigensolver': None,
-    'reuse_wfs_method': 'paw',
-    'external': None,
-    'random': False,
-    'hund': False,
-    'maxiter': 333,
-    'idiotproof': True,
-    'convergence': {'energy': 0.0005,  # eV / electron
-                    'density': 1.0e-4,  # electrons / electron
-                    'eigenstates': 4.0e-8,  # eV^2 / electron
-                    'bands': 'occupied'},
-    'verbose': 0}  # deprecated
-
-
-default_parallel: Dict[str, Any] = {
+default_parallel: dict[str, Any] = {
     'kpt': None,
     'domain': None,
     'band': None,
@@ -183,5 +264,3 @@ default_parallel: Dict[str, Any] = {
     'use_elpa': False,
     'elpasolver': '2stage',
     'buffer_size': None}
-
-
---
+++ gpaw/new/input_parameters.py
@@ -0,0 +1,147 @@
+import numpy as np
+import functools
+
+
+default_parameters = {
+    'h': None,  # Angstrom
+    'gpts': None,
+    'kpts': [(0.0, 0.0, 0.0)],
+    'nbands': None,
+    'charge': 0,
+    'magmoms': None,
+    'soc': None,
+    'background_charge': None,
+    'setups': {},
+    'basis': {},
+    'spinpol': None,
+    'xc': 'LDA',
+
+    'occupations': None,
+    'poissonsolver': None,
+    'mixer': None,
+    'eigensolver': None,
+    'reuse_wfs_method': 'paw',
+    'external': None,
+    'random': False,
+    'hund': False,
+    'maxiter': 333,
+    'idiotproof': True,
+    'convergence': {'energy': 0.0005,  # eV / electron
+                    'density': 1.0e-4,  # electrons / electron
+                    'eigenstates': 4.0e-8,  # eV^2 / electron
+                    'bands': 'occupied'},
+    'verbose': 0}  # deprecated
+
+
+class InputParameter:
+    def __init__(self, value=None):
+        self.value = value
+
+
+class XC(InputParameter):
+    def __init__(self, value='LDA'):
+        if isinstance(value, str):
+            value = {'name': value}
+        self.value = value
+
+    def __call__(self):
+        from gpaw.xc import XC
+        return XC(self.value)
+
+
+class Mode(InputParameter):
+    def __init__(self, value='fd'):
+        self.value = value
+
+    def __call__(self):
+        from gpaw.ase_interface import FDMode
+        return FDMode()
+
+
+class Setups(InputParameter):
+    def __init__(self, value='paw'):
+        self.value = value
+
+    def __call__(self, atomic_numbers, basis, xc_name, world):
+        from gpaw.setup import Setups
+        return Setups(atomic_numbers,
+                      self.value,
+                      basis,
+                      xc_name,
+                      world)
+
+
+class Symmetry(InputParameter):
+    def __init__(self, value=None):
+        if value in {None, 'off'}:
+            value = {}
+        self.value = value
+
+    def __call__(self, atoms, setups, magmoms):
+        from gpaw.symmetry import Symmetry as OldSymmetry
+        ids = [id + tuple(m) for id, m in zip(setups.id_a, magmoms)]
+        symmetry = OldSymmetry(ids, atoms.cell, atoms.pbc, **self.value)
+        symmetry.analyze(atoms.get_scaled_positions())
+        return symmetry
+
+
+class Basis(InputParameter):
+    def __init__(self, value=None):
+        self.value = value or {}
+
+
+class Magmoms(InputParameter):
+    def __call__(self, atoms):
+        if self.value is None:
+            return atoms.get_initial_magnetic_moments()
+
+        if isinstance(self.value, float):
+            return np.zeros(len(atoms)) + self.value
+
+        return np.array(self.value)
+
+
+class KPts(InputParameter):
+    def __init__(self, value=None):
+        if value is None:
+            value = {'size': (1, 1, 1)}
+        elif not isinstance(value, dict):
+            if len(value) == 3 and isinstance(value[0], int):
+                value = {'size': value}
+            else:
+                value = {'points': np.array(value)}
+        self.value = value
+
+    def __call__(self, atoms):
+        from gpaw.ase_interface import KPoints, MonkhorstPackKPoints
+        if 'points' in self.value:
+            return KPoints(self.values['points'])
+        return MonkhorstPackKPoints(self.value['size'])
+
+
+class H(InputParameter):
+    pass
+
+
+class GPts(InputParameter):
+    pass
+
+
+class InputParameters:
+    def __init__(self, params, defaults):
+        self.defaults = defaults
+        self.params = {key: defaults[key](value)
+                       for key, value in params.items()}
+
+    def __getattr__(self, name):
+        param = self.params.get(name)
+        if param is not None:
+            return param
+        return self.defaults[name]()
+
+
+@functools.lru_cache
+def create_default_parameters():
+    return {param.__name__.lower(): param for param in
+            (cls for cls in globals().values()
+             if isinstance(cls, type) and issubclass(cls, InputParameter))}
