9dc1b05642740ddeca7afd1b73dbb21bfbba18ba
giale <gianl.levi@gmail.com>
2021-09-22 10:37:05 +0000

    Typo

--- doc/documentation/directmin/agts.py
+++ doc/documentation/directmin/agts.py
@@ -6,6 +6,6 @@ def workflow():
     r1 = run(script='g2_dm_ui_vs_scf.py', cores=8, tmax='4h')
     with r1:
         run(script='plot_g2.py')
-    r3 = run(script='wm_dm_vs_scf.py', cores=8, tmax='1h')
-    with r3:
+    r2 = run(script='wm_dm_vs_scf.py', cores=8, tmax='1h')
+    with r2:
         run(script='plot_h2o.py')
