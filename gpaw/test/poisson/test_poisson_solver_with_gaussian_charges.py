ac5325e391a70726355f4cf0f156a4edba81913a
Mejdeddine Mokhtar <mokhtar.mejdeddine@gmail.com>
2021-09-24 10:33:10 +0200

    cleaning the code

--- gpaw/test/poisson/test_poisson_solver_with_gaussian_charges.py
+++ gpaw/test/poisson/test_poisson_solver_with_gaussian_charges.py
@@ -6,30 +6,35 @@ from gpaw.poisson_with_gaussian_charges import PoissonSolverWithGaussianCharges
 
 def test_PoissonSolverWithGaussianCharges():
     a = 20.0  # Size of cell
-    Nc = (100, 100, 100)  # Number of grid points along each axis
-    center_of_charge_1, center_of_charge_2 = [
-        [a / 2, a / 2, a / 4], [a / 2, a / 2, a * 3 / 4]]
-    gd = GridDescriptor(Nc, (a, a, a), 0)    # Grid-descriptor object
+    Nc = 20  # Number of grid points along each axis
+    Ncenter = int(Nc / 2)
+    Nc_c = (Nc, Nc, Nc)
+    center_of_charge_1, center_of_charge_2 = (
+        [a / 2, a / 2, a / 4], [a / 2, a / 2, a * 3 / 4])
+    gd = GridDescriptor(Nc_c, (a, a, a), 0)    # Grid-descriptor object
     gaussian_1 = Gaussian(gd, a=19.0, center=center_of_charge_1)
     gaussian_2 = Gaussian(gd, a=19.0, center=center_of_charge_2)
     rho_1 = gaussian_1.get_gauss(0)
     rho_2 = -1 * gaussian_2.get_gauss(0)
+
     # Total density of two gaussian densities of different sign
     total_density = rho_1 + rho_2
     analytical_potential = gaussian_1.get_gauss_pot(
         0) + (-1 * gaussian_2.get_gauss_pot(0))  # Get analytic potential
+
     # Test agreement with standard PoissonSolver
+    # There is a difference in values on borders
     phi_num = gd.zeros()  # Array for storing the potential
     solver = PoissonSolver(nn=3, use_charge_center=True)
     solver.set_grid_descriptor(gd)
     solver.solve(phi_num, total_density, zero_initial_phi=True)
-    # Check the agreement between the analytical and numerical potentials
+    # Check the disagreement between the analytical and numerical potentials
     # at the boundaries
-    assert abs(analytical_potential[50, 50, :]
-               [0] - phi_num[50, 50, :][0]) > 0.44
-    assert abs(analytical_potential[50, 50, :]
-               [-1] - phi_num[50, 50, :][-1]) > 0.44
-    # There is a difference in values on borders
+    assert abs(analytical_potential[Ncenter, Ncenter, :]
+               [0] - phi_num[Ncenter, Ncenter, :][0]) > 0.3
+    assert abs(analytical_potential[Ncenter, Ncenter, :]
+               [-1] - phi_num[Ncenter, Ncenter, :][-1]) > 0.3
+
     # Test agreement with Gaussian charges
     poisson = PoissonSolverWithGaussianCharges()
     poisson.set_grid_descriptor(gd)
@@ -37,9 +42,9 @@ def test_PoissonSolverWithGaussianCharges():
     poisson.positions = [center_of_charge_1, center_of_charge_2]
     phi_gaussian = gd.zeros()  # Array for storing the potential
     poisson.solve(phi_gaussian, total_density)
+
     # Check the agreement at the boundaries
-    assert abs(analytical_potential[50, 50, :]
-               [0] - phi_gaussian[50, 50, :][0]) < 0.07
-    assert abs(analytical_potential[50, 50, :]
-               [-1] - phi_gaussian[50, 50, :][-1]) < 0.07
-    # The difference is quite small
+    assert abs(analytical_potential[Ncenter, Ncenter, :]
+               [0] - phi_gaussian[Ncenter, Ncenter, :][0]) < 0.07
+    assert abs(analytical_potential[Ncenter, Ncenter, :]
+               [-1] - phi_gaussian[Ncenter, Ncenter, :][-1]) < 0.07
