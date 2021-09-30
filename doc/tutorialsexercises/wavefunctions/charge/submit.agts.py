37f4c83a27eb06ffae2ccecffe87fe8ec240857e
Michael Walter <mcoywalter@gmail.com>
2021-09-30 16:29:40 +0200

    Check that the trajectory file is there

--- doc/tutorialsexercises/wavefunctions/charge/submit.agts.py
+++ doc/tutorialsexercises/wavefunctions/charge/submit.agts.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from myqueue.workflow import run
 
 
@@ -5,3 +6,4 @@ def workflow():
     with run(script='h2o.py'):
         with run(script='bader.py'):
             run(script='plot.py')
+            assert Path('Hirshfeld.traj').is_file()
