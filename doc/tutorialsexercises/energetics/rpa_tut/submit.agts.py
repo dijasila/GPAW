9e9f46ead002bd2ab3b6b5a75f6345d8e219afbd
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-10-14 13:02:12 +0200

    Simplify

--- doc/tutorialsexercises/energetics/rpa_tut/submit.agts.py
+++ doc/tutorialsexercises/energetics/rpa_tut/submit.agts.py
@@ -1,5 +1,6 @@
 from myqueue.workflow import run
 
+
 def workflow():
     with run(script='gs_N2.py', cores=16):
         r1 = run(script='frequency.py', tmax='3h')
@@ -15,9 +16,6 @@ def workflow():
     with r1, r2, r3:
         run(shell='rm', args=['N.gpw', 'N2.gpw'])  # clean up
 
-    ds = [1.75, 2.0, 2.25, 2.5, 2.75, 3.0, 3.25, 3.5, 3.75, 4.0, 5.0, 6.0, 10.0]
-    r4 = run(script='gs_graph_Co.py', cores=16)
-    with r4:
-        run(script='rpa_graph_Co.py', cores=16)
-        run(shell='rm', args=[f'gs_{d}.gpw' for d in ds])  # clean up
-
+    with run(script='gs_graph_Co.py', cores=16):
+        with run(script='rpa_graph_Co.py', cores=16, tmax='10h'):
+            run(shell='rm', args=['gs_*.gpw'])  # clean up
