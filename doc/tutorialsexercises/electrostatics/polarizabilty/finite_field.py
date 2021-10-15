11ca794f48232444c4dc242e19b30b1efc03a40e
Michael Walter <mcoywalter@gmail.com>
2021-10-15 07:25:53 +0200

    Forgotten comment and flakes

--- doc/tutorialsexercises/electrostatics/polarizabilty/finite_field.py
+++ doc/tutorialsexercises/electrostatics/polarizabilty/finite_field.py
@@ -2,13 +2,12 @@ import numpy as np
 from ase.units import Bohr, Ha
 from ase.build import molecule
 from gpaw.cluster import Cluster
-from gpaw import GPAW, FermiDirac
+from gpaw import GPAW
 from gpaw.external import static_polarizability
 
 
 atoms = Cluster(molecule('H2O'))
 atoms.minimal_box(3)
-#atoms.edit()
 atoms.calc = GPAW(txt=None)
 
 alpha_cc = static_polarizability(atoms)
