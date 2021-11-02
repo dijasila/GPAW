2d99f0c2c513140e331041e3d99667d106732123
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-11-02 11:57:28 +0100

    TYPO

--- gpaw/test/big/gw/nio_gw.py
+++ gpaw/test/big/gw/nio_gw.py
@@ -3,7 +3,7 @@ from gpaw.response.g0w0 import G0W0
 gw = G0W0('nio.gpw',
           nbands=100,
           ecut=100,
-          wstc=True,
+          truncation='wigner-seitz',
           kpts=[(0, 0, 0)],
           relbands=(-1, 1))
 result = gw.calculate()
