f35e738106c6aef3c0d49c76f7463ee9a4f78a88
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-11-02 14:13:34 +0100

    Add test

--- gpaw/test/big/gw/agts.py
+++ gpaw/test/big/gw/agts.py
@@ -2,5 +2,5 @@ from myqueue.workflow import run
 
 
 def workflow():
-    with run(script='nio_pbe.py'):
-        run(script='nio_gw.py', tmax='1d', cores=16)
+    with run(script='nio_pbe.py', cores=16):
+        run(script='nio_gw.py', tmax='1h', cores=24)
--- gpaw/test/big/gw/nio_pbe.py
+++ gpaw/test/big/gw/nio_pbe.py
@@ -1,4 +1,10 @@
-"""NiO in an antiferromagnetic configuration."""
+"""NiO in an antiferromagnetic configuration.
+
+See:
+    Quasiparticle energy bands of NiO in the GW approximation
+    Je-Luen Li, G.-M. Rignanese, and Steven G. Louie
+    10.1103/PhysRevB.71.193102
+"""
 from ase import Atoms
 from gpaw import GPAW
 
@@ -16,13 +22,14 @@ atoms = Atoms('Ni2O2',
                          (b, 0, 0),
                          (a, 0, b)],
               magmoms=[m, -m, 0, 0])
+atoms.write('nio.json')
 
 atoms.calc = GPAW(mode={'name': 'pw', 'ecut': 600},
                   txt='nio.txt',
                   kpts={'density': 3.0, 'gamma': True},
                   xc='PBE')
+
 e = atoms.get_potential_energy()
-atoms.write('nio.json')
 
 calc = atoms.calc.fixed_density(nbands=100)
 calc.write('nio.gpw', mode='all')
