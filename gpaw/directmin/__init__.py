65166f18e10b04c8fda8cd06750c95f51187b450
Yorick Schmerwitz <yla1@hi.is>
2021-11-10 17:12:52 +0000

    Allow capital letters and missing hyphens

--- gpaw/directmin/__init__.py
+++ gpaw/directmin/__init__.py
@@ -4,7 +4,7 @@ Exponential Transformation Direct Minimization
 
 from gpaw.xc import xc_string_to_dict
 from gpaw.directmin.sd_etdm import SteepestDescent, FRcg, QuickMin, LBFGS, \
-    LBFGS_P, LSR1P
+    LBFGS_P, LSR1P, ModeFollowing
 from gpaw.directmin.ls_etdm import MaxStep, StrongWolfeConditions, Parabola
 
 
@@ -14,14 +14,14 @@ def search_direction(method):
 
     if isinstance(method, dict):
         kwargs = method.copy()
-        name = kwargs.pop('name')
+        name = kwargs.pop('name').replace('-', '').lower()
 
         searchdir = {'sd': SteepestDescent,
-                     'fr-cg': FRcg,
-                     'quick-min': QuickMin,
-                     'l-bfgs': LBFGS,
-                     'l-bfgs-p': LBFGS_P,
-                     'l-sr1p': LSR1P
+                     'frcg': FRcg,
+                     'quickmin': QuickMin,
+                     'lbfgs': LBFGS,
+                     'lbfgsp': LBFGS_P,
+                     'lsr1p': LSR1P
                      }[name](**kwargs)
 
         return searchdir
@@ -35,16 +35,16 @@ def line_search_algorithm(method, objective_function, searchdir_algo):
 
     if isinstance(method, dict):
         kwargs = method.copy()
-        name = kwargs.pop('name')
-        if name == 'swc-awc':
+        name = kwargs.pop('name').replace('-', '').lower()
+        if name == 'swcawc':
             # for swc-awc we need to know
             # what search. dir. algo is used
             if 'searchdirtype' not in kwargs:
                 kwargs['searchdirtype'] = searchdir_algo.type
 
-        ls_algo = {'max-step': MaxStep,
+        ls_algo = {'maxstep': MaxStep,
                    'parabola': Parabola,
-                   'swc-awc': StrongWolfeConditions
+                   'swcawc': StrongWolfeConditions
                    }[name](objective_function, **kwargs)
 
         return ls_algo
