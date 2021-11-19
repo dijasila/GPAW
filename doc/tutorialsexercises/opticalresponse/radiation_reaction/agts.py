410eb357b8ac772bbab3475591eabf590fed167d
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-11-19 08:49:55 +0100

    Give scripts more time

--- doc/tutorialsexercises/opticalresponse/radiation_reaction/agts.py
+++ doc/tutorialsexercises/opticalresponse/radiation_reaction/agts.py
@@ -1,7 +1,7 @@
 def workflow():
     from myqueue.workflow import run
-    td_jobs = [run(script='lcao_linres_NaD.py'),
-               run(script='lcao_linres_NaD2chain.py')]
+    td_jobs = [run(script='lcao_linres_NaD.py', tmax='1h'),
+               run(script='lcao_linres_NaD2chain.py', tmax='1h')]
 
     # Plot spectra
     run(script='plot_spectra.py', deps=td_jobs)
