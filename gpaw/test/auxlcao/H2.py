e6717525e7b71436b87925dafecc06f954c5f10b
Mikael Kuisma <kuisma@dtu.dk>
2021-11-22 22:15:56 +0100

    Add first real test

--- gpaw/test/auxlcao/H2.py
+++ gpaw/test/auxlcao/H2.py
@@ -1,22 +1,23 @@
 from gpaw import GPAW
 from gpaw.poisson import PoissonSolver
-from ase.build import molecule
+from ase.build import molecule, bulk
 from ase import Atoms
 from sys import argv
 from os.path import exists
-
 from ase.collections import g2
+import numpy as np
+from gpaw.test import equal
 
-#atoms = molecule('H2')
-atoms = Atoms('H2', positions = ((0,0,0),(0.3,0.8,1.0)))
-atoms.center(vacuum=5.5)
-
-#algs = [ 'RIVFullBasisDebug', 'RI-MPV' ]
-algs = [ 'RI-MPV' ]
-
-for alg in algs:
-    calc = GPAW(h=0.2,
-                poissonsolver=PoissonSolver(remove_moment=1+3+5),
-                mode='lcao', xc='EXX:backend=aux-lcao:algorithm=%s' % alg, basis='dzp')
+for R in [ 0.7 ]:
+    atoms = molecule('H2')
+    alg = 'RI-MPV'
+    atoms.center(vacuum=2)
+    calc = GPAW(h=0.20,
+                mode='lcao', 
+                xc='PBE0:backend=aux-lcao:algorithm=%s' % alg, 
+                basis='dzp')
     atoms.set_calculator(calc)
     atoms.get_potential_energy()
+    equal([-11.99158365,   3.27369088], calc.get_eigenvalues(), 1e-6)
+    equal(-6.554025635093129, calc.get_potential_energy())
+
