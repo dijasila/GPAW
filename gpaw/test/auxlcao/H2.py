333cf90b0ebbfd14926b96a7e48695dbf1acf160
Mikael Kuisma <kuisma@dtu.dk>
2021-11-22 22:46:02 +0100

    Make test silent and pass

--- gpaw/test/auxlcao/H2.py
+++ gpaw/test/auxlcao/H2.py
@@ -15,9 +15,9 @@ for R in [ 0.7 ]:
     calc = GPAW(h=0.20,
                 mode='lcao', 
                 xc='PBE0:backend=aux-lcao:algorithm=%s' % alg, 
-                basis='dzp')
+                basis='dzp', txt='None')
     atoms.calc = calc
     atoms.get_potential_energy()
     equal([-11.99158365,   3.27369088], calc.get_eigenvalues(), 1e-6)
-    equal(-6.554025635093129, calc.get_potential_energy())
+    equal(-6.554025635093129, calc.get_potential_energy(), 1e-5)
 
