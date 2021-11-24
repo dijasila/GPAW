20c19396b1e8a6821bbbcc18ff4c280eefe22992
Anubhab Haldar <ahaldar@bu.edu>
2021-11-24 02:17:14 -0500

    Slight speedup of LCAO eigensolver for large periodic systems

--- gpaw/lcao/eigensolver.py
+++ gpaw/lcao/eigensolver.py
@@ -57,8 +57,11 @@ class DirectLCAO(object):
             yy = 0.5
             k_c = wfs.kd.ibzk_qc[kpt.q]
             H_MM = (0.5 + 0.0j) * Vt_xMM[0]
-            for sdisp_c, Vt_MM in zip(bfs.sdisp_xc[1:], Vt_xMM[1:]):
-                H_MM += np.exp(2j * np.pi * np.dot(sdisp_c, k_c)) * Vt_MM
+
+            H_MM += np.einsum('x,xMN->MN',
+                              np.exp(2j * np.pi * bfs.sdisp_xc[1:] @ k_c),
+                              Vt_xMM[1:],
+                              optimize=True)
             wfs.timer.stop('Sum over cells')
 
         # Add atomic contribution
