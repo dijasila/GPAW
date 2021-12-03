f4cb718b23223261c15b2a38f2b9fda494591066
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-12-03 10:28:56 +0100

    Fix debug mode check

--- gpaw/utilities/blas.py
+++ gpaw/utilities/blas.py
@@ -100,7 +100,7 @@ def gemm(alpha, a, b, beta, c, transa='n'):
         assert a.size == 0 or a[0].flags.contiguous
         assert c.flags.contiguous or c.ndim == 2 and c.strides[1] == c.itemsize
         assert b.ndim == 2
-        assert b.strides[1] == b.itemsize
+        assert b.size == 0 or b.strides[1] == b.itemsize
         assert a.shape[0] == b.shape[1]
         assert c.shape == b.shape[0:1] + a.shape[1:]
     else:
