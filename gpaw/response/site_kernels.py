33c5aaadf7582fe51c124dd12b31d5ab363eb9a5
Frederik Durhuus <fladu@dtu.dk>
2021-12-22 16:07:29 +0100

    Create site_kernel script with explanation

---
+++ gpaw/response/site_kernels.py
@@ -0,0 +1,4 @@
+"""Compute site-kernels. Used for computing Heisenberg exchange.
+Specifically, one maps DFT calculations onto a Heisenberg lattice model,
+where the site-kernels define the lattice sites and magnetic moments."""
+
