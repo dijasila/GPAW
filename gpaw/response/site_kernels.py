c2c6e6c987b70abb64c361027c3458f2c0d78a68
Frederik Durhuus <fladu@dtu.dk>
2021-12-22 16:14:09 +0100

    Add sinc function

--- gpaw/response/site_kernels.py
+++ gpaw/response/site_kernels.py
@@ -2,3 +2,8 @@
 Specifically, one maps DFT calculations onto a Heisenberg lattice model,
 where the site-kernels define the lattice sites and magnetic moments."""
 
+import numpy as np
+
+def sinc(x):
+    """np.sinc(x) = sin(pi*x) / (pi*x), hence the division by pi"""
+    return np.sinc(x / np.pi)
