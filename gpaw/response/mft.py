4e81e1b22a6882f8e287308416404ac864d21752
Frederik Durhuus <fladu@dtu.dk>
2021-12-23 11:24:43 +0100

    Description of IsotropicExchangeCalculator

--- gpaw/response/mft.py
+++ gpaw/response/mft.py
@@ -12,3 +12,31 @@ from gpaw.xc import XC
 
 # ASE modules
 from ase.units import Hartree
+
+
+class IsotropicExchangeCalculator():
+    """Class calculating the isotropic Heisenberg exchange, J.
+
+    J describes the exchange interactions between magnetic moments on a
+    discrete lattice.
+
+    J can be related to the static, transverse magnetic susceptibility of
+    the Kohn-Sham system. This can be computed ab-initio by way of
+    linear response theory.
+
+    The magnetic moments are defined as integrals of the
+    magnetisation density centered on the lattice sites. Both the shape
+    and size of these integration regions can be varied.
+    All the information about positions and integration regions for
+    the magnetic sites are encoded in the wavevector dependent
+    site-kernels, K_m(q).
+
+    The central formula for computing J is
+    J(q) = sum_{G1,G2,G3,G4} Bxc_G1 Kn_G1G2(q) chiks_G2G3^{-+}(q)
+                             X Km_G3G4^* Bxc_G4^*
+
+    Note that the response function, chiks, is computed with opposite sign
+    relative to the implementation papers. If this is fixed, then the formula
+    for J should also get a minus sign.
+
+    """
