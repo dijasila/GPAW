61f30ab140fbb5442e2b9e757355b951c009c95e
Frederik Durhuus <fladu@dtu.dk>
2021-12-23 13:27:16 +0100

    Added exchange calculation itself

--- gpaw/test/response/test_mft_Febcc.py
+++ gpaw/test/response/test_mft_Febcc.py
@@ -49,3 +49,40 @@ def test_Fe_bcc():
     # Do ground state calculation
     atoms.set_calculator(calc)
     atoms.get_potential_energy()
+
+    # Prepare exchange calculator
+    exchCalc = IsotropicExchangeCalculator(calc,
+                                           sitePos_mv,
+                                           shapes_m=shapes_m,
+                                           ecut=ecut,
+                                           nbands=nbands_response)
+
+    # Do calculation for each q-point
+    J_rmnq = np.zeros([1, 1, 1, 4], dtype=np.complex128)
+    for q, q_c in enumerate(q_qc):
+        J_rmnq[:, :, :, q] = exchCalc(q_c, rc_rm=1)
+    J_q = J_rmnq[0, 0, 0, :]
+
+    # Compare with expected result (previous calculation with working code)
+    Jexp_q = np.array([0.06138644+0.0j, 0.03409565+0.0j,
+                       0.04244621+0.0j, 0.04533697+0.0j])
+    equal(J_q, Jexp_q, 1e-6)
+
+    # Test Bxc and chiks calculators individually
+    Bxc_G = exchCalc.Bxc_G
+    chiks_GGq = []
+    for q_c in q_qc:
+        _, chiks_GG = exchCalc.chiksf('-+', q_c, txt=None)
+        chiks_GGq += [chiks_GG]
+    chiks_GGq = np.dstack(chiks_GGq)
+    chiksexp_q = np.array([0.36527329, 0.21086173, 0.2479018, 0.26542496])
+    equal(chiksexp_q, chiks_GGq[0, 0, :], 1e-6)
+    Bxcexp_G = np.array([-0.82801687, -0.28927704, -0.28927704, -0.28927704])
+    equal(Bxcexp_G, Bxc_G[:4], 1e-6)
+
+    # Test computation of magnon energies
+    Efm_mq = compute_magnon_energy_FM(J_rmnq[0, :, :, :], q_qc, mm)
+    Esimple_q = compute_magnon_energy_simple(J_q, q_qc, mm)
+    equal(Efm_mq[0, :], Esimple_q, 1e-10)
+    Eexp_q = np.array([0., 0.02469755, 0.01714048, 0.01452441])
+    equal(Eexp_q, Efm_mq[0, :], 1e-6)
