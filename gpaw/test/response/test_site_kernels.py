5aedc78f5c44a736e4a934a8c9810ea08e3bd994
Frederik Durhuus <fladu@dtu.dk>
2021-12-23 13:04:24 +0100

    Fixed import error

--- gpaw/test/response/test_site_kernels.py
+++ gpaw/test/response/test_site_kernels.py
@@ -4,8 +4,8 @@
 # Import modules
 from gpaw import GPAW, PW
 from ase.units import Bohr
-from My_classes.Calculator_classes import StaticChiKSFactory
-from My_functions.Calc_site_kernels import calc_K_mixed_shapes
+from gpaw.response.mft import StaticChiKSFactory
+from gpaw.response.site_kernels import calc_K_mixed_shapes
 from gpaw.response.susceptibility import get_pw_coordinates
 from gpaw.test import equal
 from ase.build import bulk
