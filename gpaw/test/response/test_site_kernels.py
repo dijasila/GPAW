392b0f0f870b9364ac2314f3352af193f7694984
Frederik Durhuus <fladu@dtu.dk>
2021-12-23 10:45:14 +0100

    Co_hcp define system

--- gpaw/test/response/test_site_kernels.py
+++ gpaw/test/response/test_site_kernels.py
@@ -10,3 +10,22 @@ from gpaw.response.susceptibility import get_pw_coordinates
 from gpaw.test import equal
 from ase.build import bulk
 import numpy as np
+
+
+def test_Co_hcp():
+    # Use Co(hcp) as test system
+    atoms = bulk('Co', 'hcp', a=2.5071, c=4.0695)
+    calc = GPAW(xc='LDA',
+                spinpol=True,
+                mode=PW(100),
+                kpts={'size': (2, 2, 2),
+                      'gamma': True}
+                )
+    atoms.set_calculator(calc)
+    atoms.get_potential_energy()
+    chiksf = StaticChiKSFactory(calc, ecut=50)
+
+    pd0 = chiksf.get_PWDescriptor([0, 0, 0])
+    Omega_cell = atoms.get_volume()     # Volume of unit cell in Bohr^3
+    sitePose_mv = atoms.positions
+    NG = len(get_pw_coordinates(pd0))
