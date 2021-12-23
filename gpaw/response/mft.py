cc97d3528081ebab8f2de6992be8cbcac9eef78f
Frederik Durhuus <fladu@dtu.dk>
2021-12-23 11:26:39 +0100

    __init__ for IsotropicExchangeCalculator

--- gpaw/response/mft.py
+++ gpaw/response/mft.py
@@ -40,3 +40,63 @@ class IsotropicExchangeCalculator():
     for J should also get a minus sign.
 
     """
+
+    def __init__(self, gs, sitePos_mv, shapes_m='sphere', ecut=100,
+                 nbands=None, world=mpi.world):
+        """Construct the IsotropicExchangeCalculator object
+
+        Parameters
+        ----------
+        gs : str or gpaw calculator
+            Calculator with converged ground state as input to the linear
+            response calculation.
+        sitePos_mv : nd.array, str or tuple
+            Positions of magnetic sites.
+            Options are : array of positions, 'atoms'
+                or ('some atoms', [names of elements])
+            E.g. ('some atoms', ['Fe', 'Co', 'Ni']) will use the positions
+                of all Fe, Co and Ni atoms.
+        shapes_m : str or list of str
+            Shapes of the integration regions used to define magnetic moments.
+            Options are 'sphere', 'cylinder' and 'unit cell'
+        ecut : number
+            Cutoff energy in eV
+            In response calculation, include all G-vectors with G^2/2 < ecut
+        nbands : int
+            Maximum band index to include in response calculation.
+        world : obj
+            MPI communicator.
+
+        """
+
+        # Determine positions of magnetic sites
+        atoms = gs.atoms
+        if type(sitePos_mv) is str and sitePos_mv == 'atoms':
+            sitePos_mv = atoms.get_positions()  # Absolute coordinates
+        elif type(sitePos_mv) == tuple and sitePos_mv[0] == 'some atoms':
+            # Which atomic sites to include
+            siteFilter = np.array([x in sitePos_mv[1]
+                                   for x in atoms.get_chemical_symbols()])
+            sitePos_mv = atoms.get_positions()   # Absolute coordinates
+            sitePos_mv = sitePos_mv[siteFilter]  # Filter for relevant atoms
+        self.sitePos_mv = sitePos_mv
+        self.N_sites = len(sitePos_mv)   # Number of magnetic sites
+
+        # Determine shapes of integration regions
+        if type(shapes_m) is str:
+            shapes_m = [shapes_m]*self.N_sites
+        self.shapes_m = shapes_m
+
+        # Calculator for response function
+        self.chiksf = StaticChiKSFactory(gs,
+                                         ecut=ecut,
+                                         nblocks=1,
+                                         eta=0,
+                                         nbands=nbands,
+                                         world=world)
+
+        # Calculator for xc-kernel
+        self.Bxc_calc = AdiabaticBXC(self.chiksf.calc, world=world)
+
+        # Make empty object for Bxc field
+        self.Bxc_G = None
