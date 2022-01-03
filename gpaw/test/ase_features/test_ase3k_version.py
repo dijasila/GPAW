3e8a26ef847f9a5fd8948529b01ca734bad21bfa
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-01-03 11:35:07 +0100

    Fix test_ase_features_ase3k_version()

--- gpaw/test/ase_features/test_ase3k_version.py
+++ gpaw/test/ase_features/test_ase3k_version.py
@@ -4,4 +4,4 @@ from ase import __version__
 
 
 def test_ase_features_ase3k_version():
-    assert Version(__version__) >= __ase_version_required__
+    assert Version(__version__) >= Version(__ase_version_required__)
