d2271c6d6b51eb495d542e521ed2ffb7424cf33d
Frederik Durhuus <fladu@dtu.dk>
2022-01-04 16:12:39 +0100

    magnon_energy_plot.py : load results

--- doc/tutorialsexercises/magnetic/mft_Co_hcp/magnon_energy_plot.py
+++ doc/tutorialsexercises/magnetic/mft_Co_hcp/magnon_energy_plot.py
@@ -5,3 +5,15 @@ high-symmetry points of Co(hcp)"""
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+
+# ----- Load results ----- #
+
+# From 'high_sym_pts.py'
+rc_r = np.load('rc_r.npy')
+E_rmq = np.load('high_sym_pts_E_rmq.npy')
+with open('spts.json') as file:
+    spts = json.load(file)
+
+# Get info
+Nr, N_sites, Nq = E_rmq.shape
+qnames = list(spts.keys())
