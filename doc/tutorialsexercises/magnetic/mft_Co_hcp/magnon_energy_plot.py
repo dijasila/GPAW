eedf8bf4276b9b82f8543ac49a59502c17645473
Frederik Durhuus <fladu@dtu.dk>
2022-01-04 16:13:02 +0100

    magnon_energy_plot.py : plot results

--- doc/tutorialsexercises/magnetic/mft_Co_hcp/magnon_energy_plot.py
+++ doc/tutorialsexercises/magnetic/mft_Co_hcp/magnon_energy_plot.py
@@ -17,3 +17,26 @@ with open('spts.json') as file:
 # Get info
 Nr, N_sites, Nq = E_rmq.shape
 qnames = list(spts.keys())
+
+# ----- Plot results ----- #
+
+# Increase plot font
+plt.rcParams['font.size'] = 16
+
+# Convert from eV to meV
+E_rmq = E_rmq * 1000
+
+# Plot magnon energies vs. integration sphere radii (rc)
+plt.figure()
+# Define colours for different q-points
+cols_q = ['blue', 'red', 'green', 'purple', 'magenta', 'orange']
+for q in range(Nq):
+    # Plot as low energy magnon band with solid lines
+    plt.plot(rc_r, E_rmq[:, 0, q], label=qnames[q], linestyle='-',
+             color=cols_q[q])
+    # Plot high energy magnon band with dashed lines
+    plt.plot(rc_r, E_rmq[:, 1, q], linestyle='--', color=cols_q[q])
+plt.xlabel('rc [Å]')
+plt.ylabel('Magnon energy [meV]')
+plt.legend(prop={'size':14})
+plt.savefig('high_sym_pts_energy_vs_rc.png', bbox_inches='tight')
