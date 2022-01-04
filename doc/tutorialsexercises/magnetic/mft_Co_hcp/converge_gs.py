b180fc0af7b6bcabe0edc5dc92e3d8d48e8fbcb1
Frederik Durhuus <fladu@dtu.dk>
2022-01-04 16:06:28 +0100

    converge_gs.py : Select gs settings

--- doc/tutorialsexercises/magnetic/mft_Co_hcp/converge_gs.py
+++ doc/tutorialsexercises/magnetic/mft_Co_hcp/converge_gs.py
@@ -16,3 +16,13 @@ import numpy as np
 # CAMD modules
 from gpaw import GPAW, PW, FermiDirac
 from ase.build import bulk
+
+# ----- Select ground state simulation settings ----- #
+xc = 'LDA'              # Exchange-correlation potential
+k = 12                  # kpts in each direction of simulation grid
+pw = 1200               # Plane wave energy cutoff in eV
+nbands_gs = 21          # Bands for ground state calculation
+nbands_response = 18    # Bands for response calculation
+conv = {'density': 1.e-8,
+        'forces': 1.e-8,
+        'bands': nbands_response}  # Converge bands used in response calc
