0e0471409da0bcaac74581cdc81601fd25cb0a86
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-02-28 09:30:59 +0100

    Skip test early

--- gpaw/test/new/code_graph_test.py
+++ gpaw/test/new/code_graph_test.py
@@ -5,7 +5,7 @@ from gpaw.doctools.codegraph import main
 @pytest.mark.ci
 @pytest.mark.serial
 def test_code_graph(monkeypatch):
+    pytest.importorskip('graphviz')
     monkeypatch.setattr('graphviz.Digraph.render',
                         lambda self, name, format: None)
-    pytest.importorskip('graphviz')
     main()
