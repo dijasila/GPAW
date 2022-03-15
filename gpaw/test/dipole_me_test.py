3e583ec4797286026ba2d58ffdc2dba07b1278be
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-03-15 09:12:26 +0100

    Dipole sign

--- gpaw/test/dipole_me_test.py
+++ gpaw/test/dipole_me_test.py
@@ -12,11 +12,13 @@ def test_dipole_me(gpw_files):
         calc.calculation.state.ibzwfs.
         get_all_electron_wave_function(n)
         for n in [0, 1])
-    d1_v = (psi0 * psi1).moment() * Bohr
+    d1_v = -(psi0 * psi1).moment() * Bohr
 
     # Method 2: use pseudo wave function + PAW corrections:
     d2_nnv = dipole_matrix_elements_from_calc(calc, n1=0, n2=2)[0]
 
+    print(d1_v)
+    print(d2_nnv)
     assert abs(d2_nnv[0, 0] - calc.atoms.cell.sum(0) / 2).max() < 0.04
     assert abs(d2_nnv[1, 1] - calc.atoms.cell.sum(0) / 2).max() < 0.04
     assert abs(d2_nnv[0, 1] - d1_v).max() < 1e-3
