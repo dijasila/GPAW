425daef938fee8f4604ed3b5620f5daa4cfd5493
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-04-12 20:03:06 +0200

    TYPO

--- gpaw/test/fuzz_test.py
+++ gpaw/test/fuzz_test.py
@@ -2,9 +2,10 @@ import pytest
 from gpaw.test.fuzz import main
 
 
+@pytest.mark.serial
 @pytest.mark.ci
 @pytest.mark.parametrize('mode', ['pw', 'lcao', 'fd'])
 @pytest.mark.parametrize('pbc', [0, 1])
-def test_fuzz(tmp_path, mode, pbc):
+def test_fuzz(in_tmp_dir, mode, pbc):
     error = main(f'h2 -m {mode} -c new,old -v 3.0 -p {pbc}')
     assert error == 0, mode
