0916bf72563e864f22a98aee5715e892e5b5c0b0
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-04-26 14:06:00 +0200

    Small fix

--- gpaw/core/matrix.py
+++ gpaw/core/matrix.py
@@ -241,14 +241,14 @@ class Matrix:
         M, N = self.shape
         assert M == N
         dist = self.dist
-        bc, br = dist.desc[4:6]
-        assert bc == br
         if dist.comm.size == 1:
             self.tril2full()
             self.data[:] = linalg.inv(self.data,
                                       overwrite_a=True,
                                       check_finite=debug)
             return
+        bc, br = dist.desc[4:6]
+        assert bc == br
         info = _gpaw.scalapack_inverse(self.data, dist.desc, 'U')
         if info != 0:
             raise ValueError(f'scalapack_inverse error: {info}')
