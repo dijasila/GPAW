61d6f26f4e6a918e618735c305c83499a60b8e9f
Ask Hjorth Larsen <asklarsen@gmail.com>
2022-04-29 16:52:37 +0200

    commit due to tests passing.  Cleanup pending

--- gpaw/response/g0w0.py
+++ gpaw/response/g0w0.py
@@ -5,6 +5,8 @@ import pickle
 import warnings
 from math import pi
 
+def is_symmetric(arr):
+    return np.allclose(arr, arr.T.conj())
 
 class Dyson:
     def __init__(self):
@@ -19,7 +21,8 @@ class Dyson:
         #   ii) Calculate epsinv_(gg) = eps_(gg)^-1
         #   iii) Calcualte W_GG = vsqr_G (epsinv - I_GG) vsqr_G
 
-        return htm(W_WgG), htp(W_WgG)
+        # return htm(W_WgG), htp(W_WgG)
+        pass
 
 import numpy as np
 from ase.dft.kpoints import monkhorst_pack
@@ -784,6 +787,8 @@ class G0W0(PairDensity):
         htp = HilbertTransform(self.omega_w, self.eta, gw=True)
         htm = HilbertTransform(self.omega_w, -self.eta, gw=True)
 
+        # htp = htm = lambda x: x
+
         # Find maximum size of chi-0 matrices:
         gd = self.calc.wfs.gd
         nGmax = max(count_reciprocal_vectors(self.ecut, gd, q_c)
@@ -975,6 +980,12 @@ class G0W0(PairDensity):
             pdi, Wm2_wGG, Wp2_wGG = self.dyson_and_W_new(wstc, iq, q_c, chi0, chi0_wvv, chi0_wxvG, chi02_wGG, A1_2_x, A2_2_x, pd, ecut, htp, htm)
             self.timer.stop('new non gamma')
 
+
+            print('Wm_wGG')
+            print(Wm_wGG[0])
+            print('Wm2_wGG')
+            print(Wm2_wGG[0])
+
             assert np.allclose(Wm_wGG, Wm2_wGG)
             assert np.allclose(Wp_wGG, Wp2_wGG)
 
@@ -1007,7 +1018,7 @@ class G0W0(PairDensity):
     #############
     #### NEW ####
     #############
-    def dyson_and_W_new(self, wstc, iq, q_c, chi0, chi0_wvv, chi0_wxvG, chi0_wGG, A1_x, A2_x, pd, ecut, htp, htm):
+    def dyson_and_W_new(self, wstc, iq, q_c, chi0, chi0_wvv, chi0_wxvG, chi0_WgG, A1_x, A2_x, pd, ecut, htp, htm):
         assert self.ppa == False
         assert self.do_GW_too == False
         assert ecut == pd.ecut
@@ -1018,67 +1029,108 @@ class G0W0(PairDensity):
         #print(chi0_wGG.shape)
         #step1_matrix = Matrix(nw, nG*nG, dist=(self.blockcomm, 1, self.blockcomm.size))
 
-        nw = len(self.omega_w)
+        nW = len(self.omega_w)
         nG = pd.ngmax
 
-        mynw = (nw + self.blockcomm.size - 1) // self.blockcomm.size
-        if self.blockcomm.size > 1:
-            chi0_wGG = chi0.redistribute(chi0_wGG, A2_x)
-            wa = min(self.blockcomm.rank * mynw, nw)
-            wb = min(wa + mynw, nw)
-        else:
-            wa = 0
-            wb = nw
+        from gpaw.response.wgg import Grid
+        from gpaw.matrix import suggest_blocking
+        # chi0 is currently WgG
 
-        if ecut == pd.ecut:
-            # nG = len(chi0_wGG[0])
-            pdi = pd
-            G2G = None
+        from gpaw.utilities.tools import tri2full
 
-        if self.integrate_gamma != 0:
-            if self.integrate_gamma == 2:
-                reduced = True
-            else:
-                reduced = False
-            V0, sqrV0 = get_integrated_kernel(pdi,
-                                              self.calc.wfs.kd.N_c,
-                                              truncation=self.truncation,
-                                              reduced=reduced,
-                                              N=100)
-        elif self.integrate_gamma == 0 and np.allclose(q_c, 0):
-            bzvol = (2 * np.pi)**3 / self.vol / self.qd.nbzkpts
-            Rq0 = (3 * bzvol / (4 * np.pi))**(1. / 3.)
-            V0 = 16 * np.pi**2 * Rq0 / bzvol
-            sqrV0 = (4 * np.pi)**(1.5) * Rq0**2 / bzvol / 2
-        else:
-            pass
+        #for chi_gG in chi0_WgG:
+        #    tri2full(chi_gG, 'U')
+
+        def issymmetric(mat):
+            return np.allclose(mat.T.conj(), mat)
 
 
-        self.timer.start('Dyson eq.')
-        #W_wGG = redistribute_and_calculate_Dyson(sqrV_G, chi0_wGG, self.blockcomm)
-        for iw, chi0_GG in enumerate(chi0_wGG):
-            sqrV_G = get_coulomb_kernel(pdi,
+        gsize1, gsize2, _ = suggest_blocking(nG, self.blockcomm.size)
+        WGG = (nW, nG, nG)
+        WgG_grid = Grid(
+            comm=self.blockcomm,
+            shape=WGG,
+            cpugrid=(1, self.blockcomm.size, 1))
+        assert chi0_WgG.shape == WgG_grid.myshape
+
+        my_gslice = WgG_grid.myslice[1]
+
+        dielectric_WgG = chi0_WgG  # XXX
+        for iw, chi0_GG in enumerate(chi0_WgG):
+            sqrV_G = get_coulomb_kernel(pd,  # XXX was: pdi
                                         self.calc.wfs.kd.N_c,
                                         truncation=self.truncation,
                                         wstc=wstc)**0.5
             e_GG = np.eye(nG) - chi0_GG * sqrV_G * sqrV_G[:, np.newaxis]
-            einv_GG = np.linalg.inv(e_GG)
-            W_GG = chi0_GG
-            W_GG[:] = (einv_GG - np.eye(nG)) * sqrV_G * sqrV_G[:, np.newaxis]
+            e_gG = e_GG[my_gslice]
 
-        if self.blockcomm.size > 1:
-            Wm_wGG = chi0.redistribute(chi0_wGG, A1_x)
-        else:
-            Wm_wGG = chi0_wGG
+            dielectric_WgG[iw, :, :] = e_gG
 
-        Wp_wGG = A2_x[:Wm_wGG.size].reshape(Wm_wGG.shape)
-        Wp_wGG[:] = Wm_wGG
+        wgg_grid = Grid(
+            comm=self.blockcomm,
+            shape=WGG,
+            cpugrid=(1, gsize1, gsize2))  # TODO: actual 3D cpu grid
+
+        dielectric_wgg = wgg_grid.zeros(dtype=complex)
+        WgG_grid.redistribute(wgg_grid, dielectric_WgG, dielectric_wgg)
+
+        assert np.allclose(dielectric_wgg, dielectric_WgG)
+        #from gpaw.utilities.tools import tri2full
+        #for x in dielectric_wgg:
+        #    tri2full(x, 'U')
+        #    x += x.T
+
+        wgg_grid.invert_inplace(dielectric_wgg)
+
+        wgg_grid.redistribute(WgG_grid, dielectric_wgg, dielectric_WgG)
+        inveps_WgG = dielectric_WgG
+
+        # HERE: invert
+
+        #mynw = (nw + self.blockcomm.size - 1) // self.blockcomm.size
+        #if self.blockcomm.size > 1:
+        #    chi0_wGG = chi0.redistribute(chi0_wGG, A2_x)
+        #    wa = min(self.blockcomm.rank * mynw, nw)
+        #    wb = min(wa + mynw, nw)
+        #else:
+        #    wa = 0
+        #    wb = nw
+
+        self.timer.start('Dyson eq.')
+        #W_wGG = redistribute_and_calculate_Dyson(sqrV_G, chi0_wGG, self.blockcomm)
+        for iw, inveps_gG in enumerate(inveps_WgG):
+            #sqrV_G = get_coulomb_kernel(pdi,
+            #                            self.calc.wfs.kd.N_c,
+            #                            truncation=self.truncation,
+            #                            wstc=wstc)**0.5
+            # e_GG = np.eye(nG) - chi0_GG * sqrV_G * sqrV_G[:, np.newaxis]
+            # einv_GG = np.linalg.inv(e_GG)
+            #W_GG = chi0_GG
+            inveps_gG -= np.identity(nG)[my_gslice]
+            thing_GG = sqrV_G * sqrV_G[:, np.newaxis]
+            inveps_gG *= thing_GG[my_gslice]
+
+
+        W_WgG = inveps_WgG
+            #W_GG = (einv_GG - np.eye(nG)) * sqrV_G * sqrV_G[:, np.newaxis]
+            #W_gG = W_GG[
+            #W_GG[:] = 
+
+        #if self.blockcomm.size > 1:
+        #    Wm_wGG = chi0.redistribute(chi0_wGG, A1_x)
+        #else:
+        #    Wm_wGG = chi0_wGG
+
+        #Wp_wGG = A2_x[:Wm_wGG.size].reshape(Wm_wGG.shape)
+        #Wp_wGG[:] = Wm_wGG
+        Wp_wGG = W_WgG.copy()
+        Wm_wGG = W_WgG.copy()
 
         with self.timer('Hilbert transform'):
             htp(Wp_wGG)
             htm(Wm_wGG)
         self.timer.stop('Dyson eq.')
-        return pdi, Wm_wGG, Wp_wGG
+        return pd, Wm_wGG, Wp_wGG
 
     def dyson_and_W_old(self, wstc, iq, q_c, chi0, chi0_wvv, chi0_wxvG, chi0_wGG, A1_x, A2_x, pd, ecut, htp, htm):
         nw = len(self.omega_w)
@@ -1267,6 +1319,11 @@ class G0W0(PairDensity):
                             np.dot(chi0_GG * sqrV_G *
                                    sqrV_G[:, np.newaxis], fv))
 
+                assert np.allclose(fv, np.identity(len(fv))), 'fv bad'
+
+                #assert is_symmetric(chi0_GG), 'chi0_GG before inv'
+                #assert is_symmetric(e_GG), 'e_GG'
+
                 einv_GG = np.linalg.inv(e_GG)
 
                 if self.do_GW_too:
@@ -1278,7 +1335,13 @@ class G0W0(PairDensity):
                 einv_wGG.append(einv_GG - delta_GG)
             else:
                 W_GG = chi0_GG
+
+                # assert is_symmetric(W_GG), 'chi0/W after inv'
+                
                 W_GG[:] = (einv_GG - delta_GG) * sqrV_G * sqrV_G[:, np.newaxis]
+
+
+                # assert is_symmetric(W_GG), 'W_GG'
 
                 if self.do_GW_too:
                     W_GW_GG = chi0_GW_GG
