7deaa2e7e13d1b126f86fb4cfcdbfce0b73636ad
Ask Hjorth Larsen <asklarsen@gmail.com>
2022-05-02 15:50:13 +0200

    simplify dielectric function calculator a bit

--- gpaw/response/temp.py
+++ gpaw/response/temp.py
@@ -6,22 +6,25 @@ class DielectricFunctionCalculator:
         self.sqrV_G = sqrV_G
         self.chiVV_GG = chi0_GG * sqrV_G * sqrV_G[:, np.newaxis]
 
-        if mode != 'GW':
-            assert fv is not None, 'Must supply fv_GG'
-            self.chiVVfv_GG = self.chiVV_GG @ fv_GG
-
         self.I_GG = np.eye(len(sqrV_G))
 
         self.chi0_GG = chi0_GG
         self.mode = mode
 
+    def _chiVVfv(self):
+        assert self.mode != 'GW'
+        assert self.fv_GG is not None
+        return self.chiVV_GG @ self.fv_GG
+
     def e_GG_gwp(self):
-        gwp_inverse_GG = np.linalg.inv(self.I_GG - self.chiVVfv_GG + self.chiVV_GG)
-        return self.I_GG - gwp_inverse_GG @ self.chiVV_GG
+        gwp_inv_GG = np.linalg.inv(self.I_GG - self._chiVVfv_GG() + self.chiVV_GG)
+        return self.I_GG - gwp_inv_GG @ self.chiVV_GG
 
     def e_GG_gws(self):
-        gws_inverse_GG = np.linalg.inv(self.I_GG + self.chiVVfv_GG - self.chiVV_GG)
-        return gws_inverse_GG @ (self.I_GG - self.chiVV_GG)
+        # Note how the signs are different wrt. gwp.
+        # Nobody knows why.
+        gws_inv_GG = np.linalg.inv(self.I_GG + self._chiVVfv_GG() - self.chiVV_GG)
+        return gws_inv_GG @ (self.I_GG - self.chiVV_GG)
 
     def e_GG_plain(self):
         return self.I_GG - self.chiVV_GG
