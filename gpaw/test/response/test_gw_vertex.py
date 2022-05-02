196975a0d4c3ad37b773e70882c13b7c800d3c2c
Fredrik Nilsson <fanni@dtu.dk>
2022-05-02 19:03:13 +0200

    Fixed according to comments in MR

--- gpaw/test/response/test_gw_vertex.py
+++ gpaw/test/response/test_gw_vertex.py
@@ -13,7 +13,12 @@ pytestmark = pytest.mark.skipif(
 
 
 @pytest.mark.response
-def test_vertex_GWP_hBN(in_tmp_dir):
+@pytest.mark.parametrize('fxc_mode, ref_gap', [
+    ('GWP', 4.6672015836973255),
+    ('GWS', 4.988260472871817),
+])
+
+def test_fxc_mode(in_tmp_dir,fxc_mode, ref_gap):
     atoms = bulk('BN', 'zincblende', a=3.615)
 
     calc = GPAW(mode=PW(400),
@@ -36,52 +41,9 @@ def test_vertex_GWP_hBN(in_tmp_dir):
               xc='rALDA',
               method='G0W0',
               ecut=40,
-              fxc_mode='GWP')
+              fxc_mode=fxc_mode)
 
     result = gw.calculate()
 
-    gap = 4.67
-    
-    equal(np.min(result['qp'][0, :, 1]) -
-          np.max(result['qp'][0, :, 0]), gap, 0.01)
-
-
-pytestmark = pytest.mark.skipif(
-    world.size != 1 and not compiled_with_sl(),
-    reason='world.size != 1 and not compiled_with_sl()')
-
-
-@pytest.mark.response
-def test_vertex_GWS_hBN(in_tmp_dir):
-    atoms = bulk('BN', 'zincblende', a=3.615)
-
-    calc = GPAW(mode=PW(400),
-                kpts={'size': (2, 2, 2), 'gamma': True},
-                xc='LDA',
-                eigensolver='rmm-diis',
-                parallel={'domain': 1},
-                occupations=FermiDirac(0.001))
-
-    atoms.calc = calc
-    atoms.get_potential_energy()
-
-    calc.diagonalize_full_hamiltonian(scalapack=True)
-    calc.write('BN_bulk_k2_ecut400_allbands.gpw', mode='all')
-
-    gw = G0W0('BN_bulk_k2_ecut400_allbands.gpw',
-              bands=(3, 5),
-              nbands=9,
-              nblocks=1,
-              xc='rALDA',
-              method='G0W0',
-              ecut=40,
-              fxc_mode='GWS')
-
-    result = gw.calculate()
-
-    gap = 4.99
-    print(np.min(result['qp'][0, :, 1]) -
-          np.max(result['qp'][0, :, 0]))
-    
-    equal(np.min(result['qp'][0, :, 1]) -
-          np.max(result['qp'][0, :, 0]), gap, 0.01)
+    calculated_gap=np.min(result['qp'][0, :, 1]) - np.max(result['qp'][0, :, 0])
+    assert calculated_gap == pytest.approx(ref_gap)
