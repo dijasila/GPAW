aed724cf98939b24cf52f711cfc39dd199047ae1
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-05-03 08:54:57 +0200

    Use gpw_files fixture for new test

--- gpaw/test/response/test_gw_vertex.py
+++ gpaw/test/response/test_gw_vertex.py
@@ -2,8 +2,6 @@ import pytest
 from gpaw.mpi import world
 from gpaw.utilities import compiled_with_sl
 import numpy as np
-from ase.build import bulk
-from gpaw import GPAW, FermiDirac, PW
 from gpaw.response.g0w0 import G0W0
 
 pytestmark = pytest.mark.skipif(
@@ -12,27 +10,10 @@ pytestmark = pytest.mark.skipif(
 
 
 @pytest.mark.response
-@pytest.mark.parametrize('fxc_mode, ref_gap', [
-    ('GWP', 4.6672015836973255),
-    ('GWS', 4.988260472871817),
-])
-def test_fxc_mode(in_tmp_dir, fxc_mode, ref_gap):
-    atoms = bulk('BN', 'zincblende', a=3.615)
-
-    calc = GPAW(mode=PW(400),
-                kpts={'size': (2, 2, 2), 'gamma': True},
-                xc='LDA',
-                eigensolver='rmm-diis',
-                parallel={'domain': 1},
-                occupations=FermiDirac(0.001))
-
-    atoms.calc = calc
-    atoms.get_potential_energy()
-
-    calc.diagonalize_full_hamiltonian(scalapack=True)
-    calc.write('BN_bulk_k2_ecut400_allbands.gpw', mode='all')
-
-    gw = G0W0('BN_bulk_k2_ecut400_allbands.gpw',
+@pytest.mark.parametrize('fxc_mode, ref_gap', [('GWP', 4.6672),
+                                               ('GWS', 4.9882)])
+def test_fxc_mode(in_tmp_dir, fxc_mode, ref_gap, gpw_files):
+    gw = G0W0(gpw_files['bn_pw_wfs'],
               bands=(3, 5),
               nbands=9,
               nblocks=1,
@@ -40,9 +21,7 @@ def test_fxc_mode(in_tmp_dir, fxc_mode, ref_gap):
               method='G0W0',
               ecut=40,
               fxc_mode=fxc_mode)
-
     result = gw.calculate()
-
-    calculated_gap = np.min(result['qp'][0, :, 1])\
-        - np.max(result['qp'][0, :, 0])
-    assert calculated_gap == pytest.approx(ref_gap)
+    calculated_gap = (np.min(result['qp'][0, :, 1]) -
+                      np.max(result['qp'][0, :, 0]))
+    assert calculated_gap == pytest.approx(ref_gap, abs=0.001)
