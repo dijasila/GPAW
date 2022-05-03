924da4ab600674fe61a70fae51955d592b1d580d
Fredrik Nilsson <fanni@dtu.dk>
2022-05-03 14:26:53 +0200

    Now testing all qp energies

--- gpaw/test/response/test_do_GW_too.py
+++ gpaw/test/response/test_do_GW_too.py
@@ -4,6 +4,7 @@ from gpaw.utilities import compiled_with_sl
 import numpy as np
 from gpaw.response.g0w0 import G0W0
 import pickle
+import os
 
 pytestmark = pytest.mark.skipif(
     world.size != 1 and not compiled_with_sl(),
@@ -12,7 +13,16 @@ pytestmark = pytest.mark.skipif(
 
 @pytest.mark.response
 def test_do_GW_too(in_tmp_dir, gpw_files):
-    ref_gap = 4.7747
+    gw0 = G0W0(gpw_files['bn_pw_wfs'],
+               bands=(3, 5),
+               nbands=9,
+               nblocks=1,
+               method='G0W0',
+               ecut=40,
+               restartfile=None)
+
+    results0 = gw0.calculate()
+    
     gw = G0W0(gpw_files['bn_pw_wfs'],
               bands=(3, 5),
               nbands=9,
@@ -21,12 +31,14 @@ def test_do_GW_too(in_tmp_dir, gpw_files):
               method='G0W0',
               ecut=40,
               fxc_mode='GWP',
-              do_GW_too=True)
+              do_GW_too=True,
+              restartfile=None)
 
     gw.calculate()
 
     with open('gw_results_GW.pckl', 'rb') as handle:
         results_GW = pickle.load(handle)
-    calculated_gap = np.min(results_GW['qp'][0, :, 1])\
-        - np.max(results_GW['qp'][0, :, 0])
-    assert calculated_gap == pytest.approx(ref_gap, abs=0.001)
+   
+    np.testing.assert_array_equal(results0['qp'], results_GW['qp'], 
+                                  err_msg='G0W0 and do_GW_too not equivalent', 
+                                  verbose=True)    
