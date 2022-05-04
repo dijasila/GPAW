cb0efb7b756304efb32e6a942012360830e75330
Fredrik Nilsson <fanni@dtu.dk>
2022-05-04 08:51:11 +0200

    and here is the file

---
+++ gpaw/test/response/test_ppa.py
@@ -0,0 +1,32 @@
+import pytest
+from gpaw.mpi import world
+from gpaw.utilities import compiled_with_sl
+import numpy as np
+from gpaw.response.g0w0 import G0W0
+import pickle
+
+pytestmark = pytest.mark.skipif(
+    world.size != 1 and not compiled_with_sl(),
+    reason='world.size != 1 and not compiled_with_sl()')
+
+
+@pytest.mark.response
+def test_do_GW_too(in_tmp_dir, gpw_files):
+    ref_result = np.asarray([[[11.23361945, 21.66191905],
+                              [ 5.26494026, 16.11789189],
+                              [ 8.68826104, 22.54448065]]])
+    gw = G0W0(gpw_files['bn_pw_wfs'],
+              bands=(3, 5),
+              nbands=9,
+              nblocks=1,
+              xc='rALDA',
+              method='G0W0',
+              ecut=40,
+              ppa=True)
+
+    results=gw.calculate()
+    print(results['qp'])
+    np.testing.assert_array_equal(results['qp'], ref_result, 
+                                  err_msg='error in ppa', 
+                                  verbose=True)
+
