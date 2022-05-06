dc6d5d43853a77ae12b839eee31ab11d050a20db
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-05-06 14:51:50 +0200

    Simplify things

--- doc/tutorialsexercises/magnetic/spinspiral/plot.py
+++ doc/tutorialsexercises/magnetic/spinspiral/plot.py
@@ -1,39 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from ase.io import read
+from ase.spectrum.band_structure import BandStructure
 
-plt.rcParams['mathtext.fontset'] = 'cm'
-plt.rcParams['mathtext.rm'] = 'serif'
+energies = []
+magmoms = []
+for i in range(31):
+    atoms = read(f'gs-{i:02}.txt', format='gpaw-out')
+    energy = atoms.get_potential_energy()
+    energies.append(energy)
 
-# Input file has format ([BandPath, ndarray, ndarray])
-fil = 'spiral_1000pw18k.npy'
-print(f'Plotting file: {fil}')
+energies = np.array(energies) * 1000
+energies -= energies[0]
 
-with open(fil, 'rb') as f:
-    path = np.load(f, allow_pickle=True)[0]
-    e = np.load(f)
-    mT = np.load(f)
+path = atoms.cell.bandpath('GXW', npoints=31)
 
-q, x, X = path.get_linear_kpoint_axis()
+bs = BandStructure(path, energies[np.newaxis, :, np.newaxis])
+bs.plot(emin=0, emax=31, label='q', ylabel='Energy [meV per atom]')
+plt.savefig('espiral.png')
 
-p = np.polyfit(q[:-2], e[:-2], 2)
-e = (e - e[0]) * 1000
-fig, ax1 = plt.subplots()
-ax1.plot(q, e, c='r', marker='.')
-ax1.set_xlabel('$|q|$', size=20)
-ax1.set_xticks(x)
-ax1.set_xticklabels(X, size=16)
-ax1.set_yticks([0, -20, -40, -60])
-ax1.set_yticklabels([0, -20, -40, -60], size=16)
-ax1.set_ylabel('Energy [meV per atom]', size=16, c='r')
-ax1.set_ylim([-70, 10])
-
-ax2 = ax1.twinx()
-mT = np.linalg.norm(mT, axis=-1)
-ax2.plot(q, mT, 'b')
-ax2.set_ylabel(r'Total magnetic moment [$\mu_B$]', size=16, c='b')
-ax2.set_ylim([0, 1.4])
-ax2.set_yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4])
-ax2.set_yticklabels([0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4], size=16)
-plt.axis([0, max(q), None, None])
-plt.tight_layout()
-plt.show()
+bs = BandStructure(path, magmoms[np.newaxis, :, np.newaxis])
+bs.plot(emin=0, emax=1.5, label='q', ylabel=r'Total magnetic moment [$\mu_B$]')
+plt.savefig('mspiral.png')
