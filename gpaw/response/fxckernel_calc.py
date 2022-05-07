73cab8295e04da46675b989bd4b985ac6979b7cf
Ask Hjorth Larsen <asklarsen@gmail.com>
2022-05-08 00:22:52 +0200

    move stuff to function

--- gpaw/response/fxckernel_calc.py
+++ gpaw/response/fxckernel_calc.py
@@ -6,16 +6,43 @@ from gpaw.xc.fxc import KernelWave
 from ase.io.aff import affopen
 
 
+def actually_calculate_kernel(self, q_empty, tag, ecut_max):
+    kd = self.calc.wfs.kd
+    bzq_qc = kd.get_bz_q_points(first=True)
+    U_scc = kd.symmetry.op_scc
+    ibzq_qc = kd.get_ibz_q_points(bzq_qc, U_scc)[0]
+
+    l_l = np.array([1.0])
+
+    if self.linear_kernel:
+        l_l = None
+        omega_w = None
+    elif not self.dyn_kernel:
+        omega_w = None
+    else:
+        omega_w = self.wd.omega_w
+
+    kernel = KernelWave(
+        l_l=l_l,
+        omega_w=omega_w,
+        calc=self.calc,
+        xc=self.xc,
+        ibzq_qc=ibzq_qc,
+        fd=self.fd,
+        q_empty=q_empty,
+        Eg=self.Eg,
+        ecut=ecut_max,
+        tag=tag,
+        timer=self.timer)
+
+    kernel.calculate_fhxc()
+
+
 def calculate_kernel(self, nG, ns, iq, cut_G=None):
     tag = self.calc.atoms.get_chemical_formula(mode='hill')
 
     if self.av_scheme is not None:
         tag += '_' + self.av_scheme + '_nspins' + str(self.nspins)
-
-    kd = self.calc.wfs.kd
-    bzq_qc = kd.get_bz_q_points(first=True)
-    U_scc = kd.symmetry.op_scc
-    ibzq_qc = kd.get_ibz_q_points(bzq_qc, U_scc)[0]
 
     ecut = self.ecut * Ha
     if isinstance(ecut, (float, int)):
@@ -27,39 +54,14 @@ def calculate_kernel(self, nG, ns, iq, cut_G=None):
 
     def bloody_filename():
         return ('fhxc_%s_%s_%s_%s.ulm'
-                % (tag, self.xc,
-                   ecut_max, iq))
+                % (tag, self.xc, ecut_max, iq))
 
     if not os.path.isfile(bloody_filename()):
         q_empty = iq
 
     if self.xc not in ('RPA'):
         if q_empty is not None:
-            l_l = np.array([1.0])
-
-            if self.linear_kernel:
-                l_l = None
-                omega_w = None
-            elif not self.dyn_kernel:
-                omega_w = None
-            else:
-                omega_w = self.wd.omega_w
-
-            kernel = KernelWave(
-                l_l=l_l,
-                omega_w=omega_w,
-                calc=self.calc,
-                xc=self.xc,
-                ibzq_qc=ibzq_qc,
-                fd=self.fd,
-                q_empty=q_empty,
-                Eg=self.Eg,
-                ecut=ecut_max,
-                tag=tag,
-                timer=self.timer)
-
-            kernel.calculate_fhxc()
-            del kernel
+            actually_calculate_kernel(self, q_empty, tag, ecut_max)
 
         mpi.world.barrier()
 
