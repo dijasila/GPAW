bd0b9fbe3fa522875ee875f7d61ad09c8da6c44e
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-05-12 10:27:27 +0200

    yml

--- gpaw/test/fileio/yaml_test.py
+++ gpaw/test/fileio/yaml_test.py
@@ -4,4 +4,5 @@ from ase.io import read
 
 def test_yaml(gpw_files):
     pytest.importorskip('yaml')
-    read(gpw_files['h2_pw'].with_name('h2_pw.txt'))
+    a = read(gpw_files['h2_pw'].with_name('h2_pw.txt'))
+    print(a)
