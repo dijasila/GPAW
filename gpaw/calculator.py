f6ed9aede737af78b47686a9197af030babe3a5a
Yorick Schmerwitz <yla1@hi.is>
2022-05-12 16:19:30 +0000

    Fix for spin-paired calculations.

--- gpaw/calculator.py
+++ gpaw/calculator.py
@@ -413,8 +413,10 @@ class GPAW(Calculator):
 
             occ_name = getattr(self.wfs.occupations, "name", None)
             if occ_name == 'mom':
-                occ = [self.wfs.kpt_u[x].f_n.copy() for x in
-                       range(len(self.wfs.kpt_u))]
+                fac = 2 if self.wfs.collinear and self.wfs.nspins == 1 else 1
+                occ = [self.wfs.kpt_u[x].f_n.copy()
+                       / (self.wfs.kpt_u[x].weightk * fac)
+                       for x in range(len(self.wfs.kpt_u))]
                 if type(self.parameters.occupations) == dict:
                     self.parameters.occupations['numbers'] = occ
                 else:
