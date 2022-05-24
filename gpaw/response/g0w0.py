ea495cca51e5911b24deedcc8af81bb294b9e7d7
Ask Hjorth Larsen <asklarsen@gmail.com>
2022-05-24 10:01:41 +0200

    extract q0 correction to separate module

--- gpaw/response/g0w0.py
+++ gpaw/response/g0w0.py
@@ -1471,114 +1471,12 @@ class G0W0:
 
     def add_q0_correction(self, pd, W_GG, einv_GG, chi0_xvG, chi0_vv,
                           sqrtV_G, print_ac=False):
-        from ase.dft import monkhorst_pack
-        cell_cv = self.gd.cell_cv
-        qpts_qc = self.kd.bzk_kc
-        L = cell_cv[2, 2]
-        vc_G0 = sqrtV_G[1:]**2
+        from gpaw.response.q0_correction import Q0Correction
+        q0c = Q0Correction(x0density=self.x0density,
+                           cell_cv=self.gd.cell_cv, bzk_kc=self.kd.bzk_kc,
+                           N_c=self.qd.N_c)
 
-        B_GG = einv_GG[1:, 1:]
-        u_v0G = vc_G0[np.newaxis, :]**0.5 * chi0_xvG[0, :, 1:]
-        u_vG0 = vc_G0[np.newaxis, :]**0.5 * chi0_xvG[1, :, 1:]
-        U_vv = -chi0_vv
-        a_v0G = -np.dot(u_v0G, B_GG)
-        a_vG0 = -np.dot(u_vG0, B_GG.T)
-        A_vv = U_vv - np.dot(a_v0G, u_vG0.T)
-        S_v0G = a_v0G
-        S_vG0 = a_vG0
-        L_vv = A_vv
-
-        # Get necessary G vectors.
-        G_Gv = pd.get_reciprocal_vectors()[1:]
-        G_Gv += np.array([1e-14, 1e-14, 0])
-        G2_G = np.sum(G_Gv**2, axis=1)
-        Gpar_G = np.sum(G_Gv[:, 0:2]**2, axis=1)**0.5
-
-        # Generate numerical q-point grid
-        rcell_cv = 2 * pi * np.linalg.inv(cell_cv).T
-        N_c = self.qd.N_c
-
-        iq = np.argmin(np.sum(qpts_qc**2, axis=1))
-        assert np.allclose(qpts_qc[iq], 0)
-        q0cell_cv = np.array([1, 1, 1])**0.5 * rcell_cv / N_c
-        q0vol = abs(np.linalg.det(q0cell_cv))
-
-        x0density = self.x0density
-        q0density = 2. / L * x0density
-        npts_c = np.ceil(np.sum(q0cell_cv**2, axis=1)**0.5 /
-                         q0density).astype(int)
-        npts_c[2] = 1
-        npts_c += (npts_c + 1) % 2
-        if print_ac:
-            print('Applying analytical 2D correction to W:',
-                  file=self.fd)
-            print('    Evaluating Gamma point contribution to W on a ' +
-                  '%dx%dx%d grid' % tuple(npts_c), file=self.fd)
-
-        qpts_qc = monkhorst_pack(npts_c)
-        qgamma = np.argmin(np.sum(qpts_qc**2, axis=1))
-
-        qpts_qv = np.dot(qpts_qc, q0cell_cv)
-        qpts_q = np.sum(qpts_qv**2, axis=1)**0.5
-        qpts_q[qgamma] = 1e-14
-        qdir_qv = qpts_qv / qpts_q[:, np.newaxis]
-        qdir_qvv = qdir_qv[:, :, np.newaxis] * qdir_qv[:, np.newaxis, :]
-        nq = len(qpts_qc)
-        q0area = q0vol / q0cell_cv[2, 2]
-        dq0 = q0area / nq
-        dq0rad = (dq0 / pi)**0.5
-        R = L / 2.
-        x0area = q0area * R**2
-        dx0rad = dq0rad * R
-
-        exp_q = 4 * pi * (1 - np.exp(-qpts_q * R))
-        dv_G = ((pi * L * G2_G * np.exp(-Gpar_G * R) * np.cos(G_Gv[:, 2] * R) -
-                 4 * pi * Gpar_G * (1 - np.exp(-Gpar_G * R) *
-                                    np.cos(G_Gv[:, 2] * R))) /
-                (G2_G**1.5 * Gpar_G *
-                 (4 * pi * (1 - np.exp(-Gpar_G * R) *
-                            np.cos(G_Gv[:, 2] * R)))**0.5))
-
-        dv_Gv = dv_G[:, np.newaxis] * G_Gv
-
-        # Add corrections
-        W_GG[:, 0] = 0.0
-        W_GG[0, :] = 0.0
-
-        A_q = np.sum(qdir_qv * np.dot(qdir_qv, L_vv), axis=1)
-        frac_q = 1. / (1 + exp_q * A_q)
-
-        # HEAD:
-        w00_q = -(exp_q / qpts_q)**2 * A_q * frac_q
-        w00_q[qgamma] = 0.0
-        W_GG[0, 0] = w00_q.sum() / nq
-        Axy = 0.5 * (L_vv[0, 0] + L_vv[1, 1])  # in-plane average
-        a0 = 4 * pi * Axy + 1
-
-        W_GG[0, 0] += -((a0 * dx0rad - np.log(a0 * dx0rad + 1)) /
-                        a0**2 / x0area * 2 * np.pi * (2 * pi * L)**2 * Axy)
-
-        # WINGS:
-        u_q = -exp_q / qpts_q * frac_q
-        W_GG[1:, 0] = 1. / nq * np.dot(
-            np.sum(qdir_qv * u_q[:, np.newaxis], axis=0),
-            S_vG0 * sqrtV_G[np.newaxis, 1:])
-
-        W_GG[0, 1:] = 1. / nq * np.dot(
-            np.sum(qdir_qv * u_q[:, np.newaxis], axis=0),
-            S_v0G * sqrtV_G[np.newaxis, 1:])
-
-        # BODY:
-        # Constant corrections:
-        W_GG[1:, 1:] += 1. / nq * sqrtV_G[1:, None] * sqrtV_G[None, 1:] * \
-            np.tensordot(S_v0G, np.dot(S_vG0.T,
-                                       np.sum(-qdir_qvv *
-                                              exp_q[:, None, None] *
-                                              frac_q[:, None, None],
-                                              axis=0)), axes=(0, 1))
-        u_vvv = np.tensordot(u_q[:, None] * qpts_qv, qdir_qvv, axes=(0, 0))
-        # Gradient corrections:
-        W_GG[1:, 1:] += 1. / nq * np.sum(
-            dv_Gv[:, :, None] * np.tensordot(
-                S_v0G, np.tensordot(u_vvv, S_vG0 * sqrtV_G[None, 1:],
-                                    axes=(2, 0)), axes=(0, 1)), axis=1)
+        q0c.add_q0_correction(
+            pd, W_GG, einv_GG, chi0_xvG, chi0_vv,
+            sqrtV_G,
+            fd=self.fd if print_ac else None)
