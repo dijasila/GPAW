2d12e9443f11894deb4e4a0ed7af735d6b0c6a3c
Mikael Kuisma <kuisma@dtu.dk>
2022-05-24 16:09:01 +0200

    Break the calculation deliberately to test restart in real life situation

--- gpaw/test/response/test_restart_file.py
+++ gpaw/test/response/test_restart_file.py
@@ -1,19 +1,44 @@
 import pytest
+import numpy as np
 from gpaw.response.g0w0 import G0W0
+from os.path import exists
+import os
 
+class FragileG0W0(G0W0):
+    def calculate_q(self, *args, **kwargs):
+        if not hasattr(self, 'doom'):
+            self.doom = 0
+        self.doom += 1 # Advance doom
+        if self.doom == 12:
+            raise ValueError
+        G0W0.calculate_q(self, *args, **kwargs)
 
 @pytest.mark.response
-def test_do_GW_too(in_tmp_dir, gpw_files):
-    restart = False
-    gw = G0W0(gpw_files['bn_pw_wfs'],
-              bands=(3, 5),
-              nbands=9,
-              nblocks=1,
-              ecut=40,
-              kpts=[0, 1],
-              restartfile='restartfile')
+def test_restart_file(in_tmp_dir, gpw_files):
+    kwargs =dict(bands=(3, 5),
+                 nbands=9,
+                 nblocks=1,
+                 ecut=40,
+                 kpts=[0, 1],
+                 restartfile='restartfile')
+    gw = FragileG0W0(gpw_files['bn_pw_wfs'], **kwargs)
+    failed = False
+    try:
+        gw.calculate()
+    except ValueError:
+        failed = True
+    assert failed
 
+    assert exists('restartfile.sigma.pckl')
+
+    gw = G0W0(gpw_files['bn_pw_wfs'], **kwargs)
     gw.calculate()
-    gw.save_restart_file(3)
-    restart = gw.load_restart_file()
-    assert restart
+    results = gw.calculate()
+
+    kwargs.pop('restartfile')
+    gw = G0W0(gpw_files['bn_pw_wfs'], **kwargs)
+    gw.calculate()
+    results2 = gw.calculate()
+
+    assert np.allclose(results['qp'], results2['qp'])
+
