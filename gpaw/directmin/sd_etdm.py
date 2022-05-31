986c34f4824c4adeaeca550dfb2ca3f1ec9dfecc
Yorick Schmerwitz <yla1@hi.is>
2022-05-31 01:01:33 +0000

    Test

--- gpaw/directmin/sd_etdm.py
+++ gpaw/directmin/sd_etdm.py
@@ -85,8 +85,10 @@ class ModeFollowingBase(object):
                     + 1.0j * self.eigvec[i][dimtot:]
             self.eigvec = eigvec
         self.fixed_sp_order = self.partial_diagonalizer.sp_order
+        parprint('Davidson')
         parprint('Eigenvalues:')
         parprint(self.eigv[: 4])
+        parprint('Eigenvectors:')
         parprint(self.eigvec[: 4])
 
     def negate_parallel_grad(self, g_k1):
