19c0fcb129cdc455dcdd50e70b23639037ad0103
Thorbjørn Skovhus <s134264@student.dtu.dk>
2022-06-01 10:59:54 +0200

    test the J values with different spherical radii

--- gpaw/test/response/test_mft_Febcc.py
+++ gpaw/test/response/test_mft_Febcc.py
@@ -70,11 +70,13 @@ def test_Fe_bcc():
     J_rmnq = np.zeros([len(rc_rm), 1, 1, len(q_qc)], dtype=np.complex128)
     for q, q_c in enumerate(q_qc):
         J_rmnq[:, :, :, q] = exchCalc(q_c, rc_rm=rc_rm)
-    J_q = J_rmnq[0, 0, 0, :]  # The different r values are untested? XXX
+    J_rq = J_rmnq[:, 0, 0, :]
 
     # Calculate the magnon energies
+    # Could be unit tested? XXX
+    # Should be vectorized, so we can do all integration domains at once? XXX
     mw_mq = compute_magnon_energy_FM(J_rmnq[0, :, :, :], q_qc, mm)
-    mw_q = compute_magnon_energy_simple(J_q, q_qc, mm)
+    mw_q = compute_magnon_energy_simple(J_rq[0, :], q_qc, mm)
 
     # Run the chiks calculator individually
     chiks_GGq = []  # Could be tested elsewhere? XXX
@@ -84,21 +86,25 @@ def test_Fe_bcc():
     chiks_GGq = np.dstack(chiks_GGq)
 
     # Part 3: compare new results to test values
-    test_J_q = np.array([1.61643955 + 0.0j, 0.88155322 + 0.0j,
-                         1.10019274 + 0.0j, 1.18879169 + 0.0j])
+    test_J_rq = np.array([[1.61643955, 0.88155322, 1.10019274, 1.18879169],
+                          [1.8678718, 0.93756859, 1.23108965, 1.33281237],
+                          [4.67944783, 0.20054973, 1.28535702, 1.30257353]])
     test_chiks_q = np.array([0.36507507, 0.19186653, 0.23056424, 0.24705505])
     test_Bxc_G = np.array([-0.82801687, -0.28927704, -0.28927704, -0.28927704])
     test_mw_q = np.array([0., 0.6650555, 0.46719168, 0.38701164])
 
     # Exchange constants
-    equal(J_q, test_J_q, 1e-3)
+    assert np.allclose(J_rq.imag, 0.)
+    equal(J_rq.real, test_J_rq, 1e-3)
 
     # Bxc field
     Bxc_G = exchCalc.Bxc_G  # Could be tested elsewhere? XXX
-    equal(Bxc_G[:4], test_Bxc_G, 1e-3)
+    assert np.allclose(Bxc_G.imag, 0.)
+    equal(Bxc_G[:4].real, test_Bxc_G, 1e-3)
 
     # Static reactive part of chiks
-    equal(chiks_GGq[0, 0, :], test_chiks_q, 1e-3)
+    assert np.allclose(chiks_GGq[0, 0, :].imag, 0.)
+    equal(chiks_GGq[0, 0, :].real, test_chiks_q, 1e-3)
     
     # Magnon energies
     equal(mw_mq[0, :], mw_q, 1e-10)  # Check for self-consistency
