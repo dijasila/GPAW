9a42c67b7820ff11e16724ddf44d71b43ff02976
Thorbjørn Skovhus <s134264@student.dtu.dk>
2022-06-07 11:11:34 +0200

    only initialize the ground state calculator

--- gpaw/test/response/test_site_kernels.py
+++ gpaw/test/response/test_site_kernels.py
@@ -1,6 +1,5 @@
 """Test the site kernel calculation functionality of the response code"""
 from gpaw import GPAW, PW
-from gpaw.response.mft import StaticChiKSFactory
 from gpaw.response.site_kernels import site_kernel_interface
 from gpaw.response.susceptibility import get_pw_coordinates
 from ase.build import bulk
@@ -20,28 +19,29 @@ def test_site_kernels():
 def Co_hcp_test():
     # ---------- Inputs ---------- #
 
-    # Part 1: Perform ground state calculation
-    # This step should be made redudant! XXX
-    xc = 'LDA'
-    kpts = 4
-    pw = 200
+    # Part 1: Generate plane wave representation (PWDescriptor)
+    # Atomic configuration
     a = 2.5071
     c = 4.0695
     mm = 1.6
+    # Ground state settings
+    xc = 'LDA'
+    kpts = 4
+    pw = 200
+    # Response settings
+    ecut = 50.
+    gammacentered = False
+    q_c = [0., 0., 0.]
 
-    # Part 2: Generate PWDescriptor
-    ecut = 50
-
-    # Part 3: Calculate site kernels
+    # Part 2: Calculate site kernels
     rc_m = np.array([2.0, 3.0])  # radii in Aa
     zc_m = ['diameter', 'unit cell']
 
-    # Part 4: Check the calculated kernels
+    # Part 3: Check the calculated kernels
 
     # ---------- Script ---------- #
 
-    # Part 1: Perform ground state calculation to get PWDescriptor
-
+    # Part 1: Generate plane wave representation (PWDescriptor)
     atoms = bulk('Co', 'hcp', a=a, c=c)
     atoms.set_initial_magnetic_moments([mm, mm])
 
@@ -52,16 +52,12 @@ def Co_hcp_test():
                       'gamma': True}
                 )
 
-    atoms.calc = calc
-    atoms.get_potential_energy()
+    pd0 = get_PWDescriptor(atoms, calc, q_c,
+                           ecut=ecut,
+                           gammacentered=gammacentered)
 
-    # Part 2: Generate PWDescriptor
-    chiksf = StaticChiKSFactory(calc, ecut=ecut)
-    pd0 = chiksf.get_PWDescriptor([0, 0, 0])
-
-    # Part 3: Calculate site kernels
+    # Part 2: Calculate site kernels
     siteposition_mv = atoms.get_positions()
-    nG = len(get_pw_coordinates(pd0))
 
     # Compute site-kernels
     Kuc_GGm = site_kernel_interface(pd0, siteposition_mv,
@@ -72,6 +68,7 @@ def Co_hcp_test():
                                      shapes_m='cylinder', rc_m=rc_m, zc_m=zc_m)
 
     # Part 4: Check the calculated kernels
+    nG = len(get_pw_coordinates(pd0))
     V0 = atoms.get_volume()  # Volume of unit cell in Aa^3
 
     # Calculate integration volumes in Aa^3
@@ -88,3 +85,25 @@ def Co_hcp_test():
     assert abs(Kuc_GGm[0, 0, 0] - 1.) < 1.e-8
     assert np.allclose(Ksph_GGm[0, 0, :], Vsphere_m / V0)
     assert np.allclose(Kcyl_GGm[0, 0, :], Vcylinder_m / V0)
+
+
+# ---------- Test functionality ---------- #
+
+
+def get_PWDescriptor(atoms, calc, q_c, ecut=50., gammacentered=False):
+    """Mock-up of PlaneWaveKSLRF.get_PWDescriptor working on a bare calculator
+    instance without any actual data in it."""
+    from ase.units import Ha
+    from gpaw.pw.descriptor import PWDescriptor
+    from gpaw.kpt_descriptor import KPointDescriptor
+
+    # Perform inexpensive calculator initialization
+    calc.initialize(atoms)
+
+    # Create the plane wave descriptor
+    q_c = np.asarray(q_c, dtype=float)
+    qd = KPointDescriptor([q_c])
+    pd = PWDescriptor(ecut / Ha, calc.wfs.gd,
+                      complex, qd, gammacentered=gammacentered)
+
+    return pd
