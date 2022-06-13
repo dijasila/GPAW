55112b75f2a15b9bd5ab74dd6696ee8aa307cd36
Fredrik Andreas Nilsson <fanni@DTU-FVFGC14Q05G.local>
2022-06-13 17:16:13 +0200

    cleanup

--- gpaw/test/response/test_qeh.py
+++ gpaw/test/response/test_qeh.py
@@ -7,32 +7,38 @@ from gpaw.response.df import DielectricFunction
 from gpaw.response.qeh import BuildingBlock, check_building_blocks
 from gpaw.response.qeh import interpolate_building_blocks
 
-#cmr=pytest.importskip
 """
 xxx QEH module seem to require at least 6x6x1 kpoints.
     -this should be investigated
-xxxx Often fails with unreadable errors in interpolation.
+xxx Often fails with unreadable errors in interpolation.
     -arrays should be checked with assertions and readable errors
+xxx add_intraband fails with NotImplementedError in dielctric 
+    function. -Implement or remove option????
+xxx isotropic_q = False is temporarily turned off. However, 
+    most features require isotropic_q = True anyway.
+    Should we remove the option or should we expand QEH to handle
+    non-isotropic q?
 """
+
+def dielectric(calc,domega,omega2):
+    diel = DielectricFunction(calc=calc,
+                              frequencies = {'type': 'nonlinear',
+                                             'omegamax': 10,
+                                             'domega0': domega,
+                                             'omega2': omega2},
+                              nblocks=1,
+                              ecut=10,
+                              truncation='2D')
+    return diel
+
+
 @pytest.mark.response
-def test_qeh(in_tmp_dir, gpw_files):
-    df = DielectricFunction(calc=gpw_files['graphene_pw_wfs'],
-                            frequencies = {'type': 'nonlinear',
-                                           'omegamax': 10,
-                                           'domega0': 0.2,
-                                           'omega2': 0.6},
-                            nblocks=1,
-                            ecut=10,
-                            truncation='2D')
-    
-    df2 = DielectricFunction(calc=gpw_files['mos2_pw_wfs'],
-                            frequencies = {'type': 'nonlinear',
-                                           'omegamax': 10,
-                                           'domega0': 0.1,
-                                           'omega2': 0.5},
-                             nblocks=1,
-                             ecut=10,
-                             truncation='2D')
+def test_basics(in_tmp_dir, gpw_files):
+    qeh = pytest.importorskip('qeh')
+    Heterostructure = qeh.Heterostructure    
+
+    df = dielectric(gpw_files['graphene_pw_wfs'], 0.2, 0.6)
+    df2 = dielectric(gpw_files['mos2_pw_wfs'], 0.1, 0.5)
 
     #Testing to compute building block
     bb1 = BuildingBlock('graphene', df)
@@ -49,16 +55,10 @@ def test_qeh(in_tmp_dir, gpw_files):
     are_equal=check_building_blocks(['mos2_int','graphene_int'])
     assert are_equal
 
-    #testing so that building blocks can be read by qeh module
-    #and produce the correct result for a single testcase
-    try:
-        from qeh import Heterostructure
-    except:
-        print('QEH module not found. Skipping some tests...')
-    else:
-        HS = Heterostructure(structure=['mos2_int', 'graphene_int'],d=[5],wmax=0,d0=5)
-        chi = HS.get_chi_matrix()
-        assert np.amax(chi) == pytest.approx(0.019456648867161096-0.00023954749821020183j)
+    #test qeh interface
+    HS = Heterostructure(structure=['mos2_int', 'graphene_int'],d=[5],wmax=0,d0=5)
+    chi = HS.get_chi_matrix()
+    assert np.amax(chi) == pytest.approx(0.019456648867161096-0.00023954749821020183j)
         
     #test to interpolate to grid and actual numbers
     q_grid = np.array([0, 0.1])
