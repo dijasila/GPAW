ee97e5ddad0c4e2470af660bd737f67567fb2187
Jens Jørgen Mortensen <jjmo@dtu.dk>
2022-06-14 10:09:58 +0200

    Use He xtal

--- gpaw/test/ralda/test_ralda_energy_Si.py
+++ gpaw/test/ralda/test_ralda_energy_Si.py
@@ -1,38 +1,28 @@
 import pytest
-import numpy as np
 from ase import Atoms
-from ase.build import bulk
-from ase.dft.kpoints import monkhorst_pack
 
 from gpaw import GPAW, FermiDirac
 from gpaw.mpi import serial_comm
-from gpaw.test import equal
-from gpaw.xc.rpa import RPACorrelation
 from gpaw.xc.fxc import FXCCorrelation
+from gpaw.xc.rpa import RPACorrelation
 
 
-@pytest.mark.slow
 @pytest.mark.response
 def test_ralda_ralda_energy_Si(in_tmp_dir, scalapack):
-    a0 = 5.43
-    cell = bulk('Si', 'fcc', a=a0).get_cell()
-    Si = Atoms('Si2', cell=cell, pbc=True,
-               scaled_positions=((0, 0, 0), (0.25, 0.25, 0.25)))
-
-    kpts = monkhorst_pack((2, 2, 2))
-    kpts += np.array([1 / 4, 1 / 4, 1 / 4])
-
-    calc = GPAW(mode=dict(name='pw', ecut=250),
-                kpts=kpts,
+    a = 3.0
+    atoms = Atoms('He', cell=[a, a, a], pbc=True)
+    calc = GPAW(mode=dict(name='pw', ecut=200),
+                kpts=dict(size=(2, 2, 2), gamma=True),
+                nbands=2,
                 occupations=FermiDirac(0.001),
                 communicator=serial_comm)
-    Si.calc = calc
-    Si.get_potential_energy()
-    calc.diagonalize_full_hamiltonian(nbands=50)
+    atoms.calc = calc
+    atoms.get_potential_energy()
+    calc.diagonalize_full_hamiltonian(nbands=20)
 
     ecuts = [20, 30]
     rpa = RPACorrelation(calc, nfrequencies=8)
-    E_rpa1 = rpa.calculate(ecut=ecuts)
+    E_rpa1 = rpa.calculate(ecut=ecuts)[-1]
 
     def fxc(xc, nfrequencies=8, **kwargs):
         return FXCCorrelation(calc, xc=xc, **kwargs).calculate(ecut=ecuts)[-1]
@@ -46,21 +36,15 @@ def test_ralda_ralda_energy_Si(in_tmp_dir, scalapack):
         fxc('JGMs', av_scheme='wavevector', Eg=3.1, nlambda=2),
         fxc('CP_dyn', av_scheme='wavevector', nfrequencies=2, nlambda=2)]
 
-    equal(E_rpa1[-1], energies[0], 0.01)
+    assert E_rpa1 == pytest.approx(energies[0], abs=0.01)
 
-    refs = [
-        -9.5303,
-        -8.9431,
-        -8.8272,
-        -8.7941,
-        -8.6809,
-        -8.8596,
-        -4.6787]
-    tols = [0.002] * 5 + [0.001] * 2
+    refs = [-0.1054,
+            -0.0560,
+            -0.0523,
+            -0.0241,
+            -0.0288,
+            -0.0263,
+            -0.0275]
 
-    for val, ref, tol in zip(enerigies, refs, tols):
-        assert val == pytest.approx(ref, abs=tol)
-
-
-if __name__ == '__main__':
-    test_ralda_ralda_energy_Si(1, 2)
+    for val, ref in zip(energies, refs):
+        assert val == pytest.approx(ref, abs=0.001)
