6721f18ab8b9b7ca69d12152dcd5b0868e389b34
Ask Hjorth Larsen <asklarsen@gmail.com>
2022-06-14 16:42:53 +0200

    move gamma integrator to standalone module

--- gpaw/response/g0w0.py
+++ gpaw/response/g0w0.py
@@ -30,6 +30,7 @@ from gpaw.xc.tools import vxc
 from gpaw.response.temp import DielectricFunctionCalculator
 from gpaw.response.q0_correction import Q0Correction
 from gpaw.response.hilbert import GWHilbertTransforms
+from gpaw.response.gamma_int import GammaIntegrator
 
 
 class Sigma:
@@ -235,27 +236,6 @@ def get_qdescriptor(kd, atoms):
     qd = KPointDescriptor(bzq_qc)
     qd.set_symmetry(atoms, kd.symmetry)
     return qd
-
-
-class GammaIntegrator:
-    def __init__(self, truncation, kd, pd, chi0_wvv, chi0_wxvG):
-        N = 4
-        N_c = np.array([N, N, N])
-        if truncation is not None:
-            # Only average periodic directions if trunction is used
-            N_c[np.where(kd.N_c == 1)[0]] = 1
-        qf_qc = monkhorst_pack(N_c) / kd.N_c
-        qf_qc *= 1.0e-6
-        U_scc = kd.symmetry.op_scc
-        qf_qc = kd.get_ibz_q_points(qf_qc, U_scc)[0]
-        self.weight_q = kd.q_weights
-        self.qf_qv = 2 * np.pi * (qf_qc @ pd.gd.icell_cv)
-        self.a_wq = np.sum([chi0_vq * self.qf_qv.T
-                            for chi0_vq in
-                            np.dot(chi0_wvv, self.qf_qv.T)],
-                           axis=1)
-        self.a0_qwG = np.dot(self.qf_qv, chi0_wxvG[:, 0])
-        self.a1_qwG = np.dot(self.qf_qv, chi0_wxvG[:, 1])
 
 
 def choose_ecut_things(ecut, ecut_extrapolation):
