8f318d9cf43e4485758b8ebb1b2bd03534d6ed5c
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-06-16 15:30:53 +0200

    Add test ...

--- gpaw/test/ext_potential/test_harmonic.py
+++ gpaw/test/ext_potential/test_harmonic.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ase import Atoms
 from ase.units import Ha
 
@@ -41,3 +42,37 @@ def test_ext_potential_harmonic(in_tmp_dir):
     calc.write('harmonic.gpw')
     known_potentials['HarmonicPotential'] = HarmonicPotential
     GPAW('harmonic.gpw')
+
+
+class PöchelTellerPotential(ExternalPotential):
+    def calculate_potential(self, gd):
+        a = gd.cell_cv[2, 2]
+        r_vg = gd.get_grid_point_coordinates()
+        lam = 1
+        self.vext_g = -lam * (lam + 1) / 2 * np.cosh(r_vg[2] - a / 2)**-2
+
+    def todict(self):
+        return {'name': 'HarmonicPotential'}
+
+
+def test_pt_potential(in_tmp_dir):
+    """Test againts analytic result (no xc, no Coulomb)."""
+    d = 6.0
+    a = 2
+    x = Atoms(cell=(a, a, d), pbc=[1, 1, 0])  # no atoms
+
+    calc = GPAW(charge=-2,
+                nbands=1,
+                mode=dict(name='pw', ecut=500),
+                xc={'name': 'null'},
+                external=PöchelTellerPotential(),
+                poissonsolver=NoInteractionPoissonSolver(),
+                #eigensolver='cg'
+                )
+
+    x.calc = calc
+    x.get_potential_energy()
+
+    (eig,) = calc.get_eigenvalues()
+    print(eig)
+    assert eig == -0.5 * Ha
