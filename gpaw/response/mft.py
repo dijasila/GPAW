93053f45278ddf689e7f89fd007f14bb0beeb731
Thorbjørn Skovhus <s134264@student.dtu.dk>
2022-06-21 09:21:15 +0200

    update docstring

--- gpaw/response/mft.py
+++ gpaw/response/mft.py
@@ -14,31 +14,26 @@ from ase.units import Hartree
 
 
 class IsotropicExchangeCalculator:
-    """Class calculating the isotropic Heisenberg exchange, J.
+    """Calculator class for the Heisenberg exchange constants
 
-    J describes the exchange interactions between magnetic moments on a
-    discrete lattice.
+    _           2
+    J^ab(q) = - ‾‾ B^(xc†) K^(a†)(q) χ_KS^('+-)(q) K^b(q) B^(xc)
+                V0
 
-    J can be related to the static, transverse magnetic susceptibility of
-    the Kohn-Sham system. This can be computed ab-initio by way of
-    linear response theory.
+    calculated for an isotropic system in a plane wave representation using
+    the magnetic force theorem within second order perturbation theory, see
+    [arXiv:2204.04169].
 
-    The magnetic moments are defined as integrals of the
-    magnetisation density centered on the lattice sites. Both the shape
-    and size of these integration regions can be varied.
-    All the information about positions and integration regions for
-    the magnetic sites are encoded in the wavevector dependent
-    site-kernels, K_m(q).
+    Entering the formula for the isotropic exchange constant at wave vector q
+    between sublattice a and b is the unit cell volume V0, the functional
+    derivative of the (LDA) exchange-correlation energy with respect to the
+    magnitude of the magnetization B^(xc), the sublattice site kernels K^a(q)
+    and K^b(q) as well as the reactive part of the static transverse magnetic
+    susceptibility of the Kohn-Sham system χ_KS^('+-)(q).
 
-    The central formula for computing J is
-    J(q) = sum_{G1,G2,G3,G4} Bxc_G1 Kn_G1G2(q) chiks_G2G3^{-+}(q)
-                             X Km_G3G4^* Bxc_G4^*
-
-    Note that the response function, chiks, is computed with opposite sign
-    relative to the implementation papers. If this is fixed, then the formula
-    for J should also get a minus sign.
-
-    """
+    The site kernels encode the partitioning of real space into sites of the
+    Heisenberg model. This is not a uniquely defined procedure, why the user
+    has to define them externally through the SiteKernels interface."""
 
     def __init__(self, chiks):
         """Construct the IsotropicExchangeCalculator object
