4fc93067ddcfa14a557c97b148bfa03aca739a07
Thorbjørn Skovhus <s134264@student.dtu.dk>
2022-06-23 09:37:15 +0200

    write a solid script to compute and save the Fe KS orbitals

--- doc/tutorialsexercises/magnetic/mft/Fe_es.py
+++ doc/tutorialsexercises/magnetic/mft/Fe_es.py
@@ -1,55 +1,68 @@
-"""Converge ground state of Co(hcp) and save with all plane-wave components
+"""DFT ground state calculation of Fe(bcc), storing occupied and unoccupied
+Kohn-Sham orbitals in a file."""
 
-A number of unoccupied bands are converged for the sake of subsequent
-response calculations.
-
-Note : k must be a multiple of 6 in order to do computations at all the
-high-symmetry points of the hcp lattice.
-
-Note : Co(hcp) has 2 atoms in the unit cell, each with 9 valence electrons
-which fully or partially occupy 6 bands per atom. Hence there are 12 occupied
-bands, so nbands_gs >= nbands_response > 12 is required"""
-
-# General modules
-import numpy as np
-
-# CAMD modules
+# Script modules
 from gpaw import GPAW, PW, FermiDirac
 from ase.build import bulk
 
-# ----- Select ground state simulation settings ----- #
-xc = 'LDA'              # Exchange-correlation potential
-k = 12                  # kpts in each direction of simulation grid
-pw = 1200               # Plane wave energy cutoff in eV
-nbands_gs = 21          # Bands for ground state calculation
-nbands_response = 18    # Bands for response calculation
+# ---------- Inputs ---------- #
+
+# Crystal structure
+a = 2.867  # Lattice constant
+mm = 2.21  # Initial magnetic moment
+
+# The derivation of the Heisenberg exchange constants based on the magnetic
+# force theorem takes an outset in the L(S)DA exchange-correlation functional.
+# Thus, to be formally consistent, we need to use the LDA functional for the
+# ground state.
+xc = 'LDA'
+# The plane wave energy cutoff of ground state calculation is chosen: 1) high
+# enough to provide well converged Kohn-Sham orbitals, 2) larger or equal to
+# the energy cutoff of the mft response calculation. In a benchmark study, the
+# latter will typically be the stricter requirement.
+pw = 800  # eV
+# When specifying the k-point grid, one should keep in mind that the q-points
+# of the mft response calculation are required to be commensurate with the
+# grid. For a bcc crystal, a k-point grid that samples a multiple of 4 k-points
+# along all axes will support calculations at the high-symmetry points N, H and
+# P. We choose N_k^(1/3)=24, which should yield converged magnon energies
+# according to the convergence study of [arXiv:2204.04169].
+kpts = 24  # final grid is (kpts, kpts, kpts)
+# In the original paper [arXiv:2204.04169], it was shown that the mft response
+# calculations are well converged when including only bands corresponding to
+# shells with partial or full occupation.
+nbands_mft = 6  # 4s + 3d
+# To stabilize the ground state calculation, we add some extra empty bands
+nbands_gs = nbands_mft + 4  # 4p + 5s
+# For response calculations, we typically need a more strict convergence of the
+# ground state density, magnetization and Kohn-Sham orbitals than usual.
 conv = {'density': 1.e-8,
         'forces': 1.e-8,
-        'bands': nbands_response}  # Converge bands used in response calc
+        'bands': nbands_mft}  # Converge only the bands needed subsequently
+occw = 0.001  # Fermi temperature in eV
 
-# ----- Ground state simulation ----- #
+# ---------- Script ---------- #
 
-# Setup material
-Co = bulk('Co','hcp',a=2.5071, c=4.0695)
-mm = np.array([1.67, 1.67])
-Co.set_initial_magnetic_moments(magmoms=mm)
+# Set up crystal structure
+atoms = bulk('Fe', 'bcc', a=a)
+atoms.set_initial_magnetic_moments([mm])
 
-# Prepare ground state calculator
-kpts = {'size': (k, k, k), 'gamma': True}
+# Construct the ground state calculator
+kpt_grid = {'size': (kpts, kpts, kpts),
+            'gamma': True},  # When converged, the grid offset shouldn't matter
 calc = GPAW(xc='LDA',
             mode=PW(pw),
-            kpts=kpts,
+            kpts=kpt_grid,
             nbands=nbands_gs,
             convergence=conv,
-            occupations=FermiDirac(0.01),
-            symmetry={'point_group': False},
-            parallel={'domain': 1},
-            spinpol=True,
+            occupations=FermiDirac(occw),
             )
 
-# Converge ground state
-Co.set_calculator(calc)
-Co.get_potential_energy()
+# DFT ground state calculation
+atoms.calc = calc
+atoms.get_potential_energy()
 
-# Save converged ground state with all plane-wave components
-calc.write(f'converged_gs_calc.gpw', mode='all')
+# Save the ground state and Kohn-Sham orbitals
+# WARNING: This will generally leave large files on your computer. It is wise
+# to remove them, once all subsequent calculations have finished.
+calc.write('Fe_all.gpw', mode='all')
