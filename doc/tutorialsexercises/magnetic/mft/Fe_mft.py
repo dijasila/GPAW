1a59804eab01223fbbd0ccb610b7916d6b084987
Thorbjørn Skovhus <s134264@student.dtu.dk>
2022-06-23 16:15:57 +0200

    make a mft script for Fe

--- doc/tutorialsexercises/magnetic/mft/Fe_mft.py
+++ doc/tutorialsexercises/magnetic/mft/Fe_mft.py
@@ -1,69 +1,92 @@
-"""Compute magnon dispersion on band path between all the high-symmetry
-points of the hcp lattice. The resulting magnon energies are saved."""
+"""Compute the isotropic exchange constants of Fe on a band path between all
+the high-symmetry points of the bcc lattice."""
 
 # General modules
 import numpy as np
 
-# CAMD modules
-from gpaw import GPAW
-from gpaw.response.mft import IsotropicExchangeCalculator, \
-    compute_magnon_energy_FM
+# Script modules
+from gpaw.response.site_kernels import (SphericalSiteKernels,
+                                        ParallelepipedicSiteKernels)
+from gpaw.response.chiks import ChiKS
+from gpaw.response.mft import IsotropicExchangeCalculator
 
-# ----- Setup exchange calculator ----- #
 
-# Settings specific to exchange calculator
-ecut = 400  # Energy cutoff in eV for response calc (number of G-vectors)
-# How to map onto discrete lattice of magnetic moments
-sitePos_mv = 'atoms'  # Magnetic sites at all atoms
-shapes_m = ['sphere', 'sphere']  # Use spheres to define magnetic sites
-rcBest = 1.3  # Pick optimal rc value
+# ---------- Inputs ---------- #
 
-# Load converged ground state
-calc = GPAW(f'converged_gs_calc.gpw', parallel={'domain': 1})
-Co = calc.get_atoms()
-mm = calc.get_magnetic_moments()
+# Parameters reused from the ground state
+gs = 'Fe_all.gpw'
+kpts = 24
+nbands = 6
 
-# Get number of converged bands
-nbands_response = calc.parameters['convergence']['bands']
+# We choose the plane wave energy cutoff of the mft response calculation
+# so as to provide magnon energies converged within 10%, based on the
+# convergence study in [arXiv:2204.04169]
+ecut = 750  # eV
+# We compute the transverse magnetic susceptibility without broadening
+eta = 0.
 
-# Get size of k-space simulation grid
-k = calc.parameters['kpts']['size'][0]
+# We map out the high-symmetry path G-N-P-G-H, by generating all commensurate
+# q-vectors on the path
+qGN_qc = np.array([[0., 0., x / kpts]
+                   for x in range(kpts // 2 + 1)])
+qNP_qc = np.array([[x / kpts, x / kpts, 1 / 2. - x / kpts]
+                   for x in range(kpts // 4 + 1)])
+qPG_qc = np.array([[x / kpts, x / kpts, x / kpts]
+                   for x in reversed(range(kpts // 4 + 1))])
+qGH_qc = np.array([[x / kpts, -x / kpts, x / kpts]
+                   for x in range(kpts // 2 + 1)])
+q_qc = np.vstack([qGN_qc, qNP_qc[1:], qPG_qc[1:], qGH_qc[1:]])
 
-# Setup exchange calculator
-exchCalc = IsotropicExchangeCalculator(calc,
-                                       sitePos_mv,
-                                       shapes_m=shapes_m,
-                                       ecut=ecut,
-                                       nbands=nbands_response)
+# Define the Fe site radii to try for the spherical site kernels
+rc_r = np.linspace(0.5, 1.25, 31)
 
-# ----- Magnon dispersion between high symmetry points ----- #
+# ---------- Script ---------- #
 
-# Construct bandpath between all the high-symmetry points
-# G -> K -> H -> A -> L -> M -> G
-# where G is the Gamma point.
-# Note that the response calculation only works for points of the form
-# q = (n_x/k, n_y/k, n_z/k) where n_i is an integer and (k, k, k) is the
-# simulation grid
-GK = np.array([[x, x, 0] for x in np.arange(0, 1/3+0.01, 1/k)])
-KH = np.array([[1/3, 1/3, x] for x in np.arange(1/k, 1/2+0.01, 1/k)])
-HA = np.array([[1/3-x, 1/3-x, 1/2] for x in np.arange(1/k, 1/3+0.01, 1/k)])
-AL = np.array([[x, 0, 1/2] for x in np.arange(1/k, 1/2+0.01, 1/k)])
-LM = np.array([[1/2, 0, 1/2-x] for x in np.arange(1/k, 1/2+0.01, 1/k)])
-MG = np.array([[1/2-x, 0, 0] for x in np.arange(1/k, 1/2+0.01, 1/k)])
-q_qc = np.vstack([GK, KH, HA, AL, LM, MG])
+# Initialize the ChiKS calculator, which is responsible for computing the
+# transverse magnetic susceptibility of the Kohn-Sham system
+chiks = ChiKS(gs,
+              ecut=ecut, nbands=nbands, eta=eta)
+# When initialized from a file, the ChiKS calculator has a serial copy of
+# the ground state calculator. From it, we extract the atoms
+atoms = chiks.calc.atoms
 
-# Compute exchange coupling along bandpath
-Nq = len(q_qc)
-N_sites = len(exchCalc.sitePos_mv)
-J_rmnq = np.empty([1, N_sites, N_sites, Nq], dtype=complex)
+# Initialize the exchange calculator
+isoexch_calc = IsotropicExchangeCalculator(chiks)
+
+# Initialize the site kernels
+positions = atoms.positions  # sublattice positions
+# We give multiple spherical radii to the site kernels instance by specifying
+# different partitionings of space into magnetic sublattices. In the case of
+# Fe, we only have a single sublattice
+rc_pa = [[rc] for rc in rc_r]  # p: partition, a: sublattice index
+sitekernels = SphericalSiteKernels(positions, rc_pa)
+# We may also try to use the entire unit cell for the sublattice site kernel.
+# To do this, we use the parallelepipedic site kernel and use the bcc unit
+# cell as the parallelepipedic cell
+cell_pav = [[atoms.get_cell()]]
+ucsitekernels = ParallelepipedicSiteKernels(positions, cell_pav)
+
+# Allocate arrays for the exchange constants
+nq = len(q_qc)
+nsites = sitekernels.nsites
+npartitions = sitekernels.npartitions
+J_qabp = np.empty((nq, nsites, nsites, npartitions), dtype=complex)
+Juc_qabp = np.empty((nq, 1, 1, 1), dtype=complex)
+
+# Compute the isotropic exchange coupling along the chosen bandpath
 for q, q_c in enumerate(q_qc):
-    J_rmn = exchCalc(q_c, rc_rm=rcBest)
-    J_rmnq[:, :, :, q] = J_rmn
-J_mnq = J_rmnq[0, :, :, :]
+    # The IsotropicExchangeCalculator will keep a buffer with the transverse
+    # magnetic susceptibility untill we ask for a new q-vector. Thus, we may
+    # compute the exchange constants with multiple different site kernels
+    # instances, virtually without any computational overhead.
+    J_qabp[q] = isoexch_calc(q_c, sitekernels)
+    Juc_qabp[q] = isoexch_calc(q_c, ucsitekernels)
+# Since we only have a single site, reduce the arrays
+J_qr = J_qabp[:, 0, 0, :]
+Juc_q = Juc_qabp[:, 0, 0, 0]
 
-# Compute magnon energies along bandpath
-E_mq = compute_magnon_energy_FM(J_mnq, q_qc, mm)
-
-# Save energies and q-points
-np.save('q_qc.npy', q_qc)
-np.save('high_sym_path_E_mq.npy', E_mq)
+# Save the bandpath, spherical radii and computed exchange constants
+np.save('Fe_q_qc.npy', q_qc)
+np.save('Fe_rc_r.npy', rc_r)
+np.save('Fe_J_qr.npy', J_qr)
+np.save('Fe_Juc_q.npy', Juc_q)
