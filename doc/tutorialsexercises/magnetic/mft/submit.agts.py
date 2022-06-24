983bce7ccbaef776d4009df2dc6c3bc04208061c
Thorbjørn Skovhus <s134264@student.dtu.dk>
2022-06-24 15:27:59 +0200

    update the tutorial workflow

--- doc/tutorialsexercises/magnetic/mft/submit.agts.py
+++ doc/tutorialsexercises/magnetic/mft/submit.agts.py
@@ -2,8 +2,10 @@ from myqueue.workflow import run
 
 
 def workflow():
-    with run(script='converge_Fe_gs.py', cores=16, tmax='2h'):
-        with run(script='high_sym_pts.py', cores=16, tmax='2h'):
-            run(script='Fe_magnon_energy_plot.py')
-        with run(script='high_sym_path.py', cores=16, tmax='4h'):
-            run(script='Fe_magnon_dispersion_plot.py')
+    with run(script='Fe_gs.py', cores=40, tmax='1h'):
+        with run(script='Fe_mft.py', cores=40, tmax='2h'):
+            run(script='Fe_plot_magnons_vs_rc.py')
+            run(script='Fe_plot_magnon_dispersion.py')
+    with run(script='Co_gs.py', cores=40, tmax='1h'):
+        with run(script='Co_mft.py', cores=40, tmax='6h'):
+            run(script='Co_plot_magnon_dispersion.py')
