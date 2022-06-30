18fbde69bb6744086ea02bb897e40a2b66c7c9e0
Thorbjørn Skovhus <s134264@student.dtu.dk>
2022-06-30 16:49:15 +0200

    parametrize test in terms of gammacentering and symmetries

--- gpaw/test/response/test_chiks.py
+++ gpaw/test/response/test_chiks.py
@@ -26,7 +26,7 @@ def test_Fe_chiks(in_tmp_dir):
 
     Unfortunately, there will always be random noise in the wave functions,
     such that this symmetry is not fulfilled exactly. However, we should be
-    able to fulfill it within 1%, which is tested here. Generally speaking,
+    able to fulfill it within 2%, which is tested here. Generally speaking,
     the "symmetry" noise can be reduced making running with symmetry='off' in
     the ground state calculation."""
     # ---------- Inputs ---------- #
@@ -47,6 +47,10 @@ def test_Fe_chiks(in_tmp_dir):
     ecut = 50
     eta_e = [0., 0.1]
     frequencies = [0., 0.05, 0.1, 0.2]
+    sym_settings = [(True, True),  # gammacentered, disable symmetries
+                    (True, False),
+                    (False, True),
+                    (False, False)]
     # Do some q-points on the -N..G..N path
     q_qc = np.array([[0., 0., -0.5],      # -N
                      [0., 0., -0.25],     # -N/2
@@ -81,53 +85,62 @@ def test_Fe_chiks(in_tmp_dir):
     atoms.get_potential_energy()
 
     # Part 2: ChiKS calculation
-    chiks_eqwGG = []
-    pd_eq = []
-    for eta in eta_e:
-        chiks = ChiKS(calc,
-                      ecut=ecut, nbands=nbands, eta=eta,
-                      gammacentered=True,
-                      disable_time_reversal=False,  # Make this the default XXX
-                      disable_point_group=False)
+    chiks_seqwGG = []
+    pd_seq = []
+    for gammacentered, disable_syms in sym_settings:
+        chiks_eqwGG = []
+        pd_eq = []
+        for eta in eta_e:
+            chiks = ChiKS(calc,
+                          ecut=ecut, nbands=nbands, eta=eta,
+                          gammacentered=gammacentered,
+                          disable_time_reversal=disable_syms,
+                          disable_point_group=disable_syms)
 
-        chiks_qwGG = []
-        pd_q = []
-        for q_c in q_qc:
-            pd, chiks_wGG = chiks.calculate(q_c, frequencies,
-                                            spincomponent='+-')
-            pd_q.append(pd)
-            chiks_qwGG.append(chiks_wGG)
+            chiks_qwGG = []
+            pd_q = []
+            for q_c in q_qc:
+                pd, chiks_wGG = chiks.calculate(q_c, frequencies,
+                                                spincomponent='+-')
+                pd_q.append(pd)
+                chiks_qwGG.append(chiks_wGG)
 
-        chiks_eqwGG.append(chiks_qwGG)
-        pd_eq.append(pd_q)
+            chiks_eqwGG.append(chiks_qwGG)
+            pd_eq.append(pd_q)
+        chiks_seqwGG.append(chiks_eqwGG)
+        pd_seq.append(pd_eq)
 
     # Part 3: Check reciprocity
 
-    for pd_q, chiks_qwGG in zip(pd_eq, chiks_eqwGG):  # One eta at a time
-        for q1, q2 in zip([0, 1, 2], [4, 3, 2]):  # q and -q pairs
-            invmap_GG = get_inverted_pw_mapping(pd_q[q1], pd_q[q2])
+    for pd_eq, chiks_eqwGG in zip(pd_seq, chiks_seqwGG):
+        for pd_q, chiks_qwGG in zip(pd_eq, chiks_eqwGG):  # One eta at a time
+            for q1, q2 in zip([0, 1, 2], [4, 3, 2]):  # q and -q pairs
+                invmap_GG = get_inverted_pw_mapping(pd_q[q1], pd_q[q2])
 
-            # Check reciprocity of the reactive part of the static
-            # susceptibility. This specific check makes sure that the exchange
-            # constants calculated within the MFT remains reciprocal.
-            # Calculate the reactive part
-            chi1_GG, chi2_GG = chiks_qwGG[q1][0], chiks_qwGG[q2][0]
-            chi1r_GG = 1 / 2. * (chi1_GG + np.conj(chi1_GG).T)
-            chi2r_GG = 1 / 2. * (chi2_GG + np.conj(chi2_GG).T)
+                # Check reciprocity of the reactive part of the static
+                # susceptibility. This specific check makes sure that the
+                # exchange constants calculated within the MFT remains
+                # reciprocal.
+                # Calculate the reactive part
+                chi1_GG, chi2_GG = chiks_qwGG[q1][0], chiks_qwGG[q2][0]
+                chi1r_GG = 1 / 2. * (chi1_GG + np.conj(chi1_GG).T)
+                chi2r_GG = 1 / 2. * (chi2_GG + np.conj(chi2_GG).T)
 
-            # err = np.conj(chi1r_GG) - chi2r_GG[invmap_GG]
-            # is_bad = err > 1.e-8 + 1.e-2 * np.absolute(chi1r_GG)
-            # print(np.absolute(err[is_bad]) / np.absolute(chi1r_GG[is_bad]))
-            assert np.allclose(np.conj(chi1r_GG), chi2r_GG[invmap_GG],
-                               rtol=1.e-2)
+                # err = np.absolute(np.conj(chi2r_GG[invmap_GG]) - chi1r_GG)
+                # is_bad = err > 1.e-8 + 2.e-2 * np.absolute(chi1r_GG)
+                # print(np.absolute(err[is_bad])
+                #       / np.absolute(chi1r_GG[is_bad]))
+                assert np.allclose(np.conj(chi2r_GG[invmap_GG]), chi1r_GG,
+                                   rtol=2.e-2)
 
-            # Check the reciprocity of the full susceptibility
-            for chi1_GG, chi2_GG in zip(chiks_qwGG[q1], chiks_qwGG[q2]):
-                # err = np.absolute(chi1_GG - chi2_GG[invmap_GG].T)
-                # is_bad = err > 1.e-8 + 1.e-2 * np.absolute(chi1_GG)
-                # print(np.absolute(err[is_bad]) / np.absolute(chi1_GG[is_bad]))
-                assert np.allclose(chi1_GG, chi2_GG[invmap_GG].T,
-                                   rtol=1.e-2)
+                # Check the reciprocity of the full susceptibility
+                for chi1_GG, chi2_GG in zip(chiks_qwGG[q1], chiks_qwGG[q2]):
+                    # err = np.absolute(chi1_GG - chi2_GG[invmap_GG].T)
+                    # is_bad = err > 1.e-8 + 2.e-2 * np.absolute(chi1_GG)
+                    # print(np.absolute(err[is_bad])
+                    #       / np.absolute(chi1_GG[is_bad]))
+                    assert np.allclose(chi2_GG[invmap_GG].T, chi1_GG,
+                                       rtol=2.e-2)
 
 
 # ---------- Test functionality ---------- #
