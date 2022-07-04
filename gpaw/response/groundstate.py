1dfa8063ad5a4861a3837a28edf49683fb505d1a
Ask Hjorth Larsen <asklarsen@gmail.com>
2022-07-04 11:32:36 +0200

    use ground state adapter with pair object

--- gpaw/response/groundstate.py
+++ gpaw/response/groundstate.py
@@ -1,3 +1,6 @@
+from ase.units import Ha
+
+
 class ResponseGroundStateAdapter:
     def __init__(self, calc):
         wfs = calc.wfs
@@ -14,4 +17,18 @@ class ResponseGroundStateAdapter:
 
         self.wfs = wfs
         self.kpt_u = wfs.kpt_u
+        self.kpt_qs = wfs.kpt_qs
         self.setups = wfs.setups
+
+        self.fermi_level = wfs.fermi_level
+
+    def get_occupations_width(self):
+        # Ugly hack only used by pair.intraband_pair_density I think.
+
+        # Only works with Fermi-Dirac distribution
+        occs = self.wfs.occupations
+        assert occs.name in {'fermi-dirac', 'zero-width'}
+
+        # No carriers when T=0
+        width = getattr(occs, '_width', 0.0) / Ha
+        return width
