8bd2aff8e5e9531c6970b635bead8b1d15ae4ccf
Thorbjørn Skovhus <s134264@student.dtu.dk>
2022-07-14 13:39:15 +0200

    add Co magnons vs rc plot to workflow

--- doc/tutorialsexercises/magnetic/mft/submit.agts.py
+++ doc/tutorialsexercises/magnetic/mft/submit.agts.py
@@ -8,4 +8,5 @@ def workflow():
             run(script='Fe_plot_magnon_dispersion.py')
     with run(script='Co_gs.py', cores=40, tmax='1h'):
         with run(script='Co_mft.py', cores=40, tmax='6h'):
+            run(script='Co_plot_magnons_vs_rc.py')
             run(script='Co_plot_magnon_dispersion.py')
