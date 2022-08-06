bfe6546c4506213dd1c504e12170d527c76f959d
Mikael Kuisma <kuisma.mikael.juhani@gmail.com>
2022-08-06 12:34:55 +0200

    pep8 fixes

--- gpaw/xc/ri/spherical_hse_kernel.py
+++ gpaw/xc/ri/spherical_hse_kernel.py
@@ -17,13 +17,13 @@ from gpaw.sphere.lebedev import weight_n, Y_nL, R_nv
 from gpaw.spherical_harmonics import Y
 
 
-"""
-
-    Explicit implementation as given by the article Eqs. A1, A2 and A3.
-    These are compared to the official implementation in the test suite.
-
-"""
 def Phiold(n, mu, R, r):
+    """
+
+        Explicit implementation as given by the article Eqs. A1, A2 and A3.
+        These are compared to the official implementation in the test suite.
+
+    """
     Rg = np.maximum.reduce([R, r])
     Rl = np.minimum.reduce([R, r])
     Xi = mu*Rg
@@ -44,22 +44,25 @@ def Phiold(n, mu, R, r):
         return mu*prefactor*A
     raise NotImplementedError
 
-"""
 
-Helper function (Eq. 24)
-
-"""
 def Hn(n, Xi, xi):
+    """
+
+    Helper function (Eq. 24)
+
+    """
     return 1 / (2*(xi*Xi)**(n+1)) * ((Xi**(2*n+1)+xi**(2*n+1))*erfc(Xi+xi) - (Xi**(2*n+1)-xi**(2*n+1))*erfc(Xi-xi))  # noqa: E501
 
-"""
 
-    Helper function (Eq. 22).
-
-    It appears, that the article has a typo, because the summation starts at p=1, but correct results require to start at p=0.
-
-"""
 def Fn(n, Xi, xi):
+    """
+
+        Helper function (Eq. 22).
+
+        It appears, that the article has a typo, because the summation
+        starts at p=1, but correct results require to start at p=0.
+
+    """
     prefactor = 2 / np.pi**0.5
     result = 0.0
     for p in range(0, n+1):
@@ -67,12 +70,12 @@ def Fn(n, Xi, xi):
     return prefactor * result
 
 
-"""
-
-    The official spherical kernel expansion
-
-"""
 def Phi(n, mu, R, r):
+    """
+
+        The official spherical kernel expansion
+
+    """
     Rg = np.maximum.reduce([R, r])
     Rl = np.minimum.reduce([R, r])
 
@@ -86,28 +89,29 @@ def Phi(n, mu, R, r):
         result += Fn(n-m, Xi, xi)*(Xi**(2*m)+xi**(2*m))/(xi*Xi)**m
     return result * mu
 
+
 class ScreenedCoulombKernelDebug:
     def __init__(self, rgd, omega):
         self.rgd = rgd
         h = 0.01
-        r1_gg = np.zeros( (rgd.N, rgd.N) )
-        r2_gg = np.zeros( (rgd.N, rgd.N) )
-        d_gg = np.zeros( (rgd.N, rgd.N) )
+        r1_gg = np.zeros((rgd.N, rgd.N))
+        r2_gg = np.zeros((rgd.N, rgd.N))
+        d_gg = np.zeros((rgd.N, rgd.N))
         r_g = rgd.r_g.copy()
         r_g[0] = r_g[1]
-        r1_gg[:] = r_g[None, :] 
-        r2_gg[:] = r_g[:, None] 
-        rmin_gg = np.where(r1_gg<r2_gg, r1_gg, r2_gg)
-        rmax_gg = np.where(r1_gg<r2_gg, r2_gg, r1_gg)
-        d_gg[:] = rgd.dr_g[None ,:] * rgd.r_g[None,:]**2 * 4*np.pi
+        r1_gg[:] = r_g[None, :]
+        r2_gg[:] = r_g[:, None]
+        rmin_gg = np.where(r1_gg < r2_gg, r1_gg, r2_gg)
+        rmax_gg = np.where(r1_gg < r2_gg, r2_gg, r1_gg)
+        d_gg[:] = rgd.dr_g[None, :] * rgd.r_g[None, :]**2 * 4*np.pi
         self.V_lgg = []
 
-        for l in range(3):
-            kernel_gg = Phiold(l, rmax_gg, rmin_gg, omega) / (2*l+1)
+        for ll in range(3):
+            kernel_gg = Phiold(ll, rmax_gg, rmin_gg, omega) / (2*ll+1)
             self.V_lgg.append(d_gg * kernel_gg)
 
-        Rdir_v = np.mean(R_nv[[23,31,16],:],axis=0)
-        Q_vv, _ = np.linalg.qr(np.array([ Rdir_v]).T,'complete')
+        Rdir_v = np.mean(R_nv[[23, 31, 16], :], axis=0)
+        Q_vv, _ = np.linalg.qr(np.array([Rdir_v]).T, 'complete')
         R2_nv = R_nv @ Q_vv
 
         Y2_nL = np.zeros((50, 25))
@@ -116,7 +120,7 @@ class ScreenedCoulombKernelDebug:
             Y2_nL[n] = [Y(L, x, y, z) for L in range(25)]
             n += 1
 
-        def solve(r, L=0, L2 =0):
+        def solve(r, L=0, L2=0):
             v_g = np.zeros((rgd.N,))
             w_n = (weight_n * Y_nL[:, L]).copy()
             for n2, R2_v in enumerate(R2_nv):
@@ -124,15 +128,16 @@ class ScreenedCoulombKernelDebug:
                     D_n = np.sum((R_nv*r - r2*R2_v[None, :])**2, axis=1)**0.5
                     D2_n = np.where(D_n < h, h, D_n)
                     V_n = erfc(D_n*omega) / D2_n
-                    v_g[g] += weight_n[n2] * Y2_nL[n2, L2] * np.sum(w_n  * V_n)
+                    v_g[g] += weight_n[n2] * Y2_nL[n2, L2] * np.sum(w_n * V_n)
             return 4*np.pi*v_g
 
         import hashlib
         import pickle
 
-
         try:
-            f = open('screened_kernel' + hashlib.md5(rgd.xml().encode('utf-8')).hexdigest()+'.pckl','rb')
+            f = open('screened_kernel'+
+                    hashlib.md5(rgd.xml().encode('utf-8')).hexdigest()+
+                    '.pckl','rb')
             self.V_lgg = pickle.load(f)
         except IOError:
             pass
@@ -145,7 +150,7 @@ class ScreenedCoulombKernelDebug:
 
             with open('screened_kernel' + hashlib.md5(rgd.xml().encode('utf-8')).hexdigest()+'.pckl','wb') as f:
                 pickle.dump(self.V_lgg, f)
-        
+
     def screened_coulomb(self, n_g, l):
         v_g = self.V_lgg[l] @ n_g
         return v_g
@@ -158,13 +163,13 @@ if __name__ == '__main__':
     sck = ScreenedCoulombKernelDebug(rgd, 0.11)
     import matplotlib.pyplot as plt
     n_g = rgd.r_g*0
-    n_g[40] = 1.0 / rgd.dv_g[40]  
+    n_g[40] = 1.0 / rgd.dv_g[40]
     v_g = sck.screened_coulomb(n_g, l)
     plt.plot(rgd.r_g, v_g,'x-',label='hse',linewidth=4)
     plt.plot(rgd.r_g, n_g,'o',label='density')
     v2_g = rgd.poisson(n_g,l)
     v2_g[1:] /= rgd.r_g[1:]
-    v2_g[0] = v2_g[1]  
+    v2_g[0] = v2_g[1]
     #plt.plot(rgd.r_g, v2_g,'+',label='poisson')
     #plt.plot(rgd.r_g, (v2_g-v_g),'-',label='diff x 100')
     plt.show()
