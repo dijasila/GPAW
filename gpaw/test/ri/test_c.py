dc16e7cc48eb93a12a98795cae3974cf5d1c47f1
Mikael Kuisma <kuisma.mikael.juhani@gmail.com>
2022-08-12 14:15:30 +0200

    flake8

--- gpaw/test/ri/test_c.py
+++ gpaw/test/ri/test_c.py
@@ -7,26 +7,18 @@ from gpaw.atom.generator2 import generate
 @pytest.mark.skipif(world.size > 1, reason='Not parallelized')
 def test_diamond(in_tmp_dir, add_cwd_to_setup_paths):
 
-
-
     from gpaw.atom.generator import Generator
-    from gpaw.atom.configurations import parameters_extra, parameters
+    from gpaw.atom.configurations import parameters
     from gpaw.atom.basis import BasisMaker
 
-    args = parameters['C']  # Dictionary of default setup parameters
+    args = parameters['C']
     generator = Generator('C', 'PBE', configuration='1s2')
-    generator.N *= 2  # Increase grid resolution
+    generator.N *= 2
     generator.run(write_xml=True, exx=True, **args)
     bm = BasisMaker(generator, run=False)
-
-    # Create double-zeta RPBE basis where p orbital is considered a valence state
-    # (ordinary dzp basis would use a smaller p-type Gaussian for polarization)
-    # The list jvalues indicates which states should be cincluded, and the
-    # ordering corresponds to the valence states in the setup.
     basis = bm.generate(zetacount=4, polarizationcount=2,
                         energysplit=0.0001, rcutmax=12.0)
-
-    basis.write_xml()  # Dump to file 'Au.special.dz.basis'
+    basis.write_xml()
 
     parameters = {'symbol': 'C', 'Z': 6, 'xc': 'PBE',
                   'projectors': '2s,s,2p,p,d', 'radii': [1.2],
@@ -39,7 +31,7 @@ def test_diamond(in_tmp_dir, add_cwd_to_setup_paths):
     setup = gen.make_paw_setup()
     setup.write_xml()
 
-    from gpaw import GPAW, PW
+    from gpaw import GPAW
     from ase import Atoms
     from ase.build import bulk
     from ase.units import Hartree
@@ -50,16 +42,18 @@ def test_diamond(in_tmp_dir, add_cwd_to_setup_paths):
     # atoms.calc = calc
     # atoms.get_potential_energy()
 
-    calc = GPAW(h=0.12, mode='lcao', basis='qzdp', xc='HSE06WIP:backend=ri', charge=4)
+    calc = GPAW(h=0.12, mode='lcao', basis='qzdp',
+                xc='HSE06WIP:backend=ri', charge=4)
     atoms.calc = calc
     atoms.get_potential_energy()
 
     # TURBOMOLE C^4+ calculation aug-cc-pV5Z
-    tm_eigs = [ -68.5345, -61.6901 ]
-    tm_e = -32.35980873243*Hartree
+    tm_eigs = [-68.5345, -61.6901]
+    tm_e = -32.35980873243 * Hartree
 
     assert np.allclose(tm_eigs, calc.get_eigenvalues()[:2], atol=0.1)
-    assert np.allclose(tm_e, -1027.171946+atoms.get_potential_energy(), atol=0.1)
+    assert np.allclose(tm_e, -1027.171946 + atoms.get_potential_energy(),
+                       atol=0.1)
 
     atoms = bulk('C', 'diamond')
     atoms.calc = GPAW(kpts={'size': (2, 2, 2), 'gamma': True},
@@ -78,5 +72,3 @@ def test_diamond(in_tmp_dir, add_cwd_to_setup_paths):
                        rtol=1e-2, atol=1e-4)
 
     assert np.allclose(setup.X_p, setup.X_wp[0.11], atol=1e-5)
-
-
