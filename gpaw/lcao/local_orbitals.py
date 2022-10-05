bac34599c2a98cde529e8b700a0984baa75ea431
ggandus <ggandus@student.ethz.ch>
2022-10-05 13:01:47 +0200

    enh: take model from energies and fermi

--- gpaw/lcao/local_orbitals.py
+++ gpaw/lcao/local_orbitals.py
@@ -1,4 +1,8 @@
-from typing import List, Union
+import stat
+from collections import defaultdict
+from curses import wrapper
+from functools import wraps
+from typing import List, Tuple, Union
 
 import numpy as np
 import numpy.typing as npt
@@ -6,11 +10,10 @@ from ase.units import Hartree
 from gpaw import GPAW
 from gpaw.lcao.pwf2 import LCAOwrap
 from gpaw.lcao.tightbinding import TightBinding  # as LCAOTightBinding
-from gpaw.lcao.tools import get_lcao_hamiltonian
+from gpaw.lcao.tools import get_bfi, get_lcao_hamiltonian
 from gpaw.utilities.blas import r2k
 from gpaw.utilities.tools import lowdin, tri2full
 from scipy.linalg import eigh
-from typing_extensions import Self
 
 # Numeric type
 Numeric = Union[int, float, complex]
@@ -126,123 +129,77 @@ class BasisTransform:
 
     Attributes
     ----------
+    U_MM : np.ndarray
+        Rotation matrix between 2 basis.
+    indices : array_like, optional
+        Subset of indices in the new basis.
     U_Mw : np.ndarray
-        Rotation matrix between the two basis.
+        Same as `U_MM` but the include only `indices` of the new basis.
 
     Methods
     -------
-    rotate_matrx(A_MM)
+    rotate_matrx(A_MM, keep_rest=False)
         Rotate a matrix.
-    rotate_projections(P_aMi)
+    rotate_projections(P_aMi, keep_rest=False)
+        Rotate PAW atomic projects.
+    rotate_function(P_aMi, keep_rest=False)
         Rotate PAW atomic projects.
 
     """
 
-    def __init__(self, U_Mw: npt.NDArray, S_MM: npt.NDArray = None) -> None:
-        """
-
-        Parameters
-        ----------
-        U_Mw : (M,w) array_like
-            Rotation matrix between the two basis.
-        S_MM : (M,M) array_like, optional
-            LCAO overlap matrix. If provided, the resulting basis
-            is orthogonalized.
-        """
-        self.U_Mw = np.ascontiguousarray(U_Mw)
-
-        if S_MM is not None:
-            lowdin(self.U_Mw, self.rotate_matrix(S_MM))
-            np.testing.assert_allclose(self.rotate_matrix(
-                S_MM), np.eye(self.U_Mw.shape[1]))
-
-    def rotate_matrix(self, A_MM: npt.NDArray):
-        """Rotate a matrix from AOs to LOs."""
-        if A_MM.ndim == 1:
-            return np.dot(self.U_Mw.T.conj() * A_MM, self.U_Mw)
+    def __init__(self, U_MM: npt.NDArray, indices: npt.ArrayLike = None) -> None:
+        self.U_MM = U_MM
+        self.indices = indices
+        if indices is not None:
+            self.U_Mw = np.ascontiguousarray(U_MM[:, indices])
         else:
-            return dots(self.U_Mw.T.conj(), A_MM, self.U_Mw)
+            self.U_Mw = None
 
-    def rotate_projections(self, P_aMi):
-        """Rotate atomic projections from AOs to LOs."""
+    def get_rotation(self, keep_rest=False):
+        if keep_rest or self.U_Mw is None:
+            return self.U_MM
+        return self.U_Mw
+
+    def rotate_matrix(self, A_MM, keep_rest=False):
+        U_Mx = self.get_rotation(keep_rest)
+        return dots(U_Mx.T.conj(), A_MM, U_Mx)
+
+    def rotate_projections(self, P_aMi, keep_rest=False):
+        U_Mx = self.get_rotation(keep_rest)
         P_awi = {}
         for a, P_Mi in P_aMi.items():
-            P_awi[a] = np.tensordot(self.U_Mw, P_Mi, axes=[[0], [0]])
+            P_awi[a] = np.tensordot(U_Mx, P_Mi, axes=[[0], [0]])
         return P_awi
 
-    def rotate_function(self, Psi_MG):
-        return np.tensordot(self.U_Mw, Psi_MG, axes=[[0], [0]])
+    def rotate_function(self, Psi_MG, keep_rest=False):
+        U_Mx = self.get_rotation(keep_rest)
+        return np.tensordot(U_Mx, Psi_MG, axes=[[0], [0]])
 
 
-class EffectiveModel:
-    """Class to handle an embedded effective model.
+class EffectiveModel(BasisTransform):
+    """Class for an effective model."""
 
-    Rotates to a new base with the same size as the old one, but where the 
-    active model is simply a subset of the new basis.
-
-
-    Attributes
-    ----------
-    model
-        A BasisTransformation object to the effective model.
-    model_plus_rest
-        A BasisTransformation object to the effective model embedded in the rest of the new basis.
-    index_list : array_like
-        Indices of the effective model in the new basis
-
-    Methods
-    -------
-    (rotate_matrx, rotate_projections, rotate_function)(*args, keep_rest=True)
-        See BasisTransform. The additional keyword `keep_rest` allows to leave 
-        the rest of the new basis.
-    get_static_correction(H_MM, S_MM, z):
-        Get the Hybridization with the rest of the system evaluated at the complex energy `z`.
-
-    """
-
-    def __init__(self, U_MM: npt.NDArray, index_list: npt.ArrayLike, S_MM: npt.NDArray = None) -> None:
+    def __init__(self, U_MM: npt.NDArray, indices: npt.ArrayLike, S_MM: npt.NDArray = None) -> None:
         """
 
         Parameters:
         ----------
-        U_MM : (M,M) array_like
-            Rotation matrix from LCAO to LOs representation.
-        index_list : array_like
-            List of orbitals for this model.
-        S_MM : (M,M) array_like
-            Overlap matrix in the M representation.
+        S_MM : (M,M) array_like, optional
+            LCAO overlap matrix. If provided, the resulting basis
+            is orthogonalized.
         """
-        self.model = BasisTransform(U_MM[:, index_list], S_MM)
-        # Maybe lowdin LOs
         if S_MM is not None:
-            U_MM = U_MM.copy()
-            U_MM[:, index_list] = self.model.U_Mw
-        self.model_plus_rest = BasisTransform(U_MM)
-        self.index_list = index_list
+            lowdin(self.U_Mw, self.rotate_matrix(S_MM))
+            np.testing.assert_allclose(self.rotate_matrix(
+                S_MM), np.eye(self.U_Mw.shape[1]))
+            U_MM = U_MM[:]
+            U_MM = U_MM[:, indices] = self.U_Mw
 
-    def __len__(self):
-        """Number of orbitals."""
-        return len(self.index_list)
-
-    def _pick_rotation(self, method_name, keep_rest, *args, **kwargs):
-        if keep_rest:
-            method = getattr(self.model_plus_rest, method_name)
-        else:
-            method = getattr(self.model, method_name)
-        return method(*args, **kwargs)
-
-    def rotate_matrix(self, A_MM: npt.NDArray, keep_rest=False):
-        return self._pick_rotation('rotate_matrix', keep_rest, A_MM)
-
-    def rotate_projections(self, P_aMi, keep_rest=False):
-        return self._pick_rotation('rotate_projections', keep_rest, P_aMi)
-
-    def rotate_function(self, Psi_MG, keep_rest=False):
-        return self._pick_rotation('rotate_function', keep_rest, Psi_MG)
+        super().__init__(U_MM, indices)
 
     def get_static_correction(self, H_MM: npt.NDArray, S_MM: npt.NDArray, z: Numeric = 0. + 1e-5j):
         """Get static correction to model Hamiltonian."""
-        w = self.index_list  # Alias
+        w = self.indices  # Alias
 
         Hp_MM = self.rotate_matrix(H_MM, keep_rest=True)
         Sp_MM = self.rotate_matrix(S_MM, keep_rest=True)
@@ -261,12 +218,12 @@ class EffectiveModel:
         return D0.real
 
 
-class Subdiagonalization:
+class Subdiagonalization(BasisTransform):
     """Class to perform a subdiagonalization of the Hamiltonian.
 
     Attributes
     ----------
-    H_MM. S_MMM : np.ndarray
+    H_MM, S_MMM : np.ndarray
         LCAO Hamiltonian and overlap matrices.
     U_MM : np.ndarray
         Rotation matrix that subdiagonalizes the LCAO Hamiltonian.
@@ -278,7 +235,7 @@ class Subdiagonalization:
 
     """
 
-    def __init__(self, H_MM: npt.NDArray, S_MM: npt.NDArray, subdiag_lists: List[List]) -> None:
+    def __init__(self, H_MM: npt.NDArray, S_MM: npt.NDArray, blocks: List[List]) -> None:
         """
         Parameters
         ----------
@@ -286,18 +243,19 @@ class Subdiagonalization:
             LCAO Hamiltonian.
         S_MM : (M,M) NDArray
             LCAO Overlap.
-        block_lists : list of lists
+        blocks : list of lists
             Subdiagonalization blocks given as a list of list indices.
         """
-        self.subdiag_lists = subdiag_lists
+        self.blocks = blocks
         self.H_MM = H_MM
         self.S_MM = S_MM
-        self.eps_M, self.U_MM = subdiagonalize(
-            self.H_MM, self.S_MM, subdiag_lists)
+        self.eps_M, U_MM = subdiagonalize(
+            self.H_MM, self.S_MM, blocks)
+        super().__init__(U_MM)
 
-    def get_effective_model(self, index_list, ortho=False):
+    def get_model(self, indices, ortho=False):
         """Get a local orbital model for the subdiagonalized space."""
-        return EffectiveModel(self.U_MM, index_list, self.S_MM if ortho else None)
+        return EffectiveModel(self.U_MM, indices, self.S_MM if ortho else None)
 
 
 class LocalOrbitals(TightBinding):
@@ -305,14 +263,13 @@ class LocalOrbitals(TightBinding):
 
     Attributes
     ----------
-    subdiag
-        A Subdiagonalization object constructed from the input subdiag_lists.
-    effmodel
-        An EffectiveModel object.
-    H_NMM, S_NMM : np.ndarray
-        Real-space Hamiltonian and overlap matrices in eV.
-    N0 : int
-        Home unit cell index
+    subdiag : Subdiagonalization
+    model : EffectiveModel
+
+    H_NMM, S_NMM: np.ndarray
+        LCAO Hamiltonian and overlap matrices.
+    N0: int
+        Home unit cell index.
 
     Methods:
     --------
@@ -335,9 +292,10 @@ class LocalOrbitals(TightBinding):
 
     """
 
-    def __init__(self, calc: GPAW, subdiag_lists: List[List], spin=0):
+    def __init__(self, calc: GPAW):
 
-        self.gamma = calc.wfs.kd.gamma
+        self.calc = calc
+        self.gamma = calc.wfs.kd.gamma  # Gamma point calculation
 
         if self.gamma:
             self.calc = calc
@@ -365,34 +323,88 @@ class LocalOrbitals(TightBinding):
                 raise RuntimeError(
                     "Must include central unit cell, i.e. R=[0,0,0].") from exc
 
-        # Subdiagonalization Object
+    def subdiagonalize(self, symbols=None, blocks=None):
+        """Subdiagonalize Hamiltonian and overlap matrices."""
+        if symbols is not None:
+            atoms = self.calc.atoms.symbols.search(symbols)
+            blocks = [get_bfi(self.calc, [c]) for c in atoms]
+        if blocks is None:
+            raise RuntimeError("""User must provide either the element(s)
+                               or a list of blocks to subdiagonalize.""")
+        self.blocks = blocks
         self.subdiag = Subdiagonalization(
-            self.H_NMM[self.N0], self.S_NMM[self.N0], subdiag_lists)
-        # Effective model
-        self.effmodel = None
+            self.H_NMM[self.N0], self.S_NMM[self.N0], blocks)
 
-    def set_effmodel(self, index_list, ortho=False):
-        self.effmodel = self.subdiag.get_effective_model(index_list, ortho)
+        # Group orbitals with same symmetry.
+        eps = self.subdiag.eps_M.round(1)
+        show = np.where(~eps.mask)[0]
+        groups = defaultdict(list)
+        for index in show:
+            groups[eps[index]].append(index)
+
+        self.groups = groups
+
+    def take_model(self, indices=None, minimal=True, cutoff=1e-3, ortho=False):
+        """Build an effective model."""
+        if self.subdiag is None:
+            raise RuntimeError("""Not yet subdiagonalized.""")
+
+        eps = self.subdiag.eps_M.round(1)
+
+        if indices is None:
+            # Find active orbitals with energy closest to Fermi.
+
+            fermi = round(self.calc.get_fermi_level(), 1)
+            # diffs = [] # Min distance from fermi for each block
+            indices = []  # Min distance index for each block
+            for block in self.blocks:
+                eb = eps[block]
+                ib = np.abs(eb - fermi).argmin()
+                indices.append(block[ib])
+                # diffs.append(abs(eb[ib]))
+
+        if not minimal:
+            # Find orbitals that connect to active with a matrix element larger than cutoff
+
+            # Look at gamma of 1st neighbor
+            H_MM = self.H_NMM[(self.N0 + 1) % len(self.H_NMM)]
+            H_MM = self.subdiag.rotate_matrix(H_MM)
+            # H_MM = dots(self.subdiag.U_MM.T.conj(), H_MM, self.subdiag.U_MM)
+
+            extend = []
+            for group in self.groups.values():
+                if np.isin(group, indices).any():
+                    continue
+                if np.abs(H_MM[np.ix_(indices, group)]).max() > cutoff:
+                    extend += group
+
+            # Expand model
+            indices += extend
+
+        self.indices = indices
+        self.model = self.subdiag.get_model(indices)
 
         if self.gamma:
-            H_Nww = self.effmodel.rotate_matrix(self.H_NMM[0])[None, ...]
-            S_Nww = self.effmodel.rotate_matrix(self.S_NMM[0])[None, ...]
+            H_Nww = self.model.rotate_matrix(self.H_NMM[0])[None, ...]
+            S_Nww = self.model.rotate_matrix(self.S_NMM[0])[None, ...]
 
         else:
             # Bypass parent's LCAO construction.
-            shape = (self.R_cN.shape[1],) + 2 * (len(self.effmodel),)
+            shape = (self.R_cN.shape[1],) + 2 * (len(self.indices),)
             dtype = self.H_NMM.dtype
             H_Nww = np.empty(shape, dtype)
             S_Nww = np.empty(shape, dtype)
 
             for N, (H_MM, S_MM) in enumerate(zip(self.H_NMM, self.S_NMM)):
-                H_Nww[N] = self.effmodel.rotate_matrix(H_MM)
-                S_Nww[N] = self.effmodel.rotate_matrix(S_MM)
+                H_Nww[N] = self.model.rotate_matrix(H_MM)
+                S_Nww[N] = self.model.rotate_matrix(S_MM)
         self.H_Nww = H_Nww
         self.S_Nww = S_Nww
 
-        self.H_Nww[self.N0] += self.effmodel.get_static_correction(
-            self.H_NMM[self.N0], self.S_NMM[self.N0])
+        if minimal:
+            # Add static correction of hybridization to minimal model.
+            self.H_Nww[self.N0] += self.model.get_static_correction(
+                self.H_NMM[self.N0], self.S_NMM[self.N0])
 
     def h_and_s(self):
         """Returns the Hamiltonian and overlap in Hartree units."""
@@ -408,11 +420,11 @@ class LocalOrbitals(TightBinding):
         return self.S_Nww[self.N0]
 
     def get_orbitals(self):
-        return get_orbitals(self.calc, self.effmodel.model.U_Mw)
+        return get_orbitals(self.calc, self.model.U_Mw)
 
     def get_projections(self, q=0):
         P_aMi = {a: P_aqMi[q] for a, P_aqMi in self.calc.wfs.P_aqMi.items()}
-        return self.effmodel.rotate_projections(P_aMi)
+        return self.model.rotate_projections(P_aMi)
 
     def get_xc(self):
         return get_xc(self.calc, self.get_orbitals(), self.get_projections())
