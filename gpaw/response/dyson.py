4594bb628c7a5128759f23312ae286562e788e11
Mikael Kuisma <kuisma@dtu.dk>
2022-10-10 11:03:20 +0300

    May work

--- gpaw/response/dyson.py
+++ gpaw/response/dyson.py
@@ -1,5 +1,43 @@
 import numpy as np
 
+from gpaw.gpu.arrays import CuPyArrayInterface
+from gpaw import gpu
+import cupy
+import os
+
+GPAW_CUPY = bool(int(os.environ.get('GPAW_CUPY', False)))
+
+print('GPAW_CUPY:', GPAW_CUPY)
+
+if GPAW_CUPY:
+    gpu.setup(cuda=True)
+
+def CUPYBridge(f):
+    def identity(*args, **kwargs):
+        f(*args, **kwargs)
+
+    # If GPAW_CUPY environment variavble is not set or
+    # is set to 0, we will just run the normal function.
+    if not GPAW_CUPY:
+        return identity
+
+    # If GPAW_CUPY=1, we will convert arrays to cupy-arrays.
+    # If they already are cupy arrays, this won't do anything.
+    # We do not convert output, and we assign to output, thus
+    # cupy will automatically convert to correct output upon
+    # assignment (if done properly).
+    def bridge(sqrV_g, chi0_GG, mode, fv_GG, weight=1.0, out_GG=None, lib=np):
+        if not gpu.is_device_array(out_GG):
+            gpu_out_GG = cupy.asarray(out_GG)
+        else:
+            gpu_out_GG = out_GG
+        f(cupy.asarray(sqrV_g), cupy.asarray(chi0_GG), mode, weight=weight, out_GG=gpu_out_GG, lib=cupy)
+        if not gpu.is_device_array(out_GG):
+            out_GG[:] = cupy.asnumpy(gpu_out_GG)
+    
+    return bridge
+
+@CUPYBridge
 def calculate_inveps(sqrV_G, chi0_GG, mode, fv_GG=None, weight=1.0, out_GG=None, lib=np):
     """
 
