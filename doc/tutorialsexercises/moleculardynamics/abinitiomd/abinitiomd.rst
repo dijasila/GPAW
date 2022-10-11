5ce10e88fa9839065492939dc2c43916af3b5049
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-10-11 16:07:54 +0200

    Use new ase.build.graphene() function

--- doc/tutorialsexercises/moleculardynamics/abinitiomd/abinitiomd.rst
+++ doc/tutorialsexercises/moleculardynamics/abinitiomd/abinitiomd.rst
@@ -4,28 +4,30 @@
 ab initio molecular dynamics (DFT/MD)
 =====================================
 
-Ab initio molecular dynamics uses DFT to calculate the forces between the atoms 
-at each time step. While computationally expensive, prohibiting simulations longer
-than a few ps, DFT/MD can be directly applied to any system that DFT can describe.
+Ab initio molecular dynamics uses DFT to calculate the forces between the
+atoms at each time step. While computationally expensive, prohibiting
+simulations longer than a few ps, DFT/MD can be directly applied to any system
+that DFT can describe.
+
 
 -----------------------------------------
 Transmission of hydrogen through graphene
 -----------------------------------------
 
-Neutral atom transmission through a graphene target can be simulated with DFT/MD.
-However, note that TDDFT/MD is required to account for electronic stopping 
-(Ref. [#Brand2019]_).
+Neutral atom transmission through a graphene target can be simulated with
+DFT/MD. However, note that TDDFT/MD is required to account for electronic
+stopping (Ref. [#Brand2019]_).
 
 The following script simulates the impact of a hydrogen atom with an initial
 velocity corresponding to a kinetic energy of 40 keV, transmitting through the
-center of a hexagon in a graphene target. The impact point in this case is the 
-center of a carbon hexagon, but this can be modified by changing the x-y position 
-of the H atom (``projpos``).
+center of a hexagon in a graphene target. The impact point in this case is the
+center of a carbon hexagon, but this can be modified by changing the x-y
+position of the H atom (``projpos``).
 
-In a realistic calculation, one should consider the choice of the exchange-
-correlation functional, and verify the convergence of the results with respect to 
-the grid spacing, timestep and *k*-points. Here, less accurate parameters are used 
-so that the calculation can be run on 4-8 CPU cores. 
+In a realistic calculation, one should consider the choice of the XC
+functional, and verify the convergence of the results with respect to the grid
+spacing, timestep and *k*-points. Here, less accurate parameters are used so
+that the calculation can be run on 4-8 CPU cores.
 
 .. literalinclude:: atomtransmission.py
 
@@ -35,6 +37,6 @@ References
 ----------
 
 .. [#Brand2019] C. Brand, M. Debiossac, T. Susi, F. Aguillon, J. Kotakoski,
-                P. Roncin, M. Arndt, "Coherent diffraction of hydrogen 
+                P. Roncin, M. Arndt, "Coherent diffraction of hydrogen
                 through the 246 pm lattice of graphene",
                 *New J. Phys.* **21**, 033004 (2019).
--- doc/tutorialsexercises/moleculardynamics/abinitiomd/atomtransmission.py
+++ doc/tutorialsexercises/moleculardynamics/abinitiomd/atomtransmission.py
@@ -1,67 +1,55 @@
-from ase.lattice.hexagonal import Graphene
-from gpaw import GPAW
+import ase.units as units
 from ase import Atoms
+from ase.build import graphene
 from ase.io import Trajectory
-from ase.units import fs
 from ase.md.verlet import VelocityVerlet
+from gpaw import GPAW
 from gpaw.utilities import h2gpts
 
 name = 'graphene_h'
 
 # 5 x 5 supercell of graphene
-index1 = 5
-index2 = 5
 a = 2.45
-c = 3.355
-
-gra = Graphene(symbol='C',
-               latticeconstant={'a': a, 'c': c},
-               size=(index1, index2, 1),
-               )
-
-gra.center(vacuum=10.0, axis=2)
+gra = graphene(a=a, size=(5, 5, 1), vacuum=10)
 gra.center()
 
-# Projectile impact point at the above a hexagon center
-projpos = [[gra[15].position[0], gra[15].position[1] + 1.41245, 15.0]]
-
-H = Atoms('H', cell=gra.cell, positions=projpos)
-H.mass = 1.0  # Set mass to one atomic mass unit to avoid isotope average
-
-# Combine target and projectile
-atoms = gra + H
-atoms.set_pbc(True)
+# Starting position of the projectile with an impact point at the
+# center of a hexagon.
+# Set mass to one atomic mass unit to avoid isotope average.
+atoms = gra + Atoms('H', mass=1.0)
+d = a / 3**0.5
+atoms.positions[-1] = atoms.positions[22] + (0, d, 5)
+atoms.pbc = (True, True, True)
 
 calc = GPAW(gpts=h2gpts(0.2, gra.get_cell(), idiv=8),
             nbands=110,
             xc='LDA',
-            txt=name + '_gs.txt',
-            )
+            txt=f'{name}_gs.txt')
 
 atoms.calc = calc
 atoms.get_potential_energy()
 
 # Moving to the MD part
-Ekin = 100  # Kinetic energy of the ion (in eV)
-timestep = 0.1  # Timestep in fs
+ekin = 100  # kinetic energy of the ion (in eV)
+timestep = 0.1  # timestep in fs
 
 # Filename for saving trajectory
-ekin_str = '_ek' + str(int(Ekin)) + 'eV'
-traj_file = name + ekin_str + '.traj'
-
-proj_idx = 50  # atomic index of the projectile
+ekin_str = '_ek' + str(int(ekin / 1000)) + 'keV'
+strbody = name + ekin_str
+traj_file = f'{name}_ek_{ekin}.traj'
 
 # Integrator for the equations of motion, timestep depends on system
-dyn = VelocityVerlet(atoms, timestep * fs)
+dyn = VelocityVerlet(atoms, timestep * units.fs)
 
 # Saving the positions of all atoms after every time step
-traj = Trajectory(traj_file, 'w', atoms)
-dyn.attach(traj.write, interval=1)
+with Trajectory(traj_file, 'w', atoms) as traj:
+    dyn.attach(traj.write, interval=1)
 
-# Giving the target atom a kinetic energy of ene in the -z direction
-atoms[proj_idx].momentum[2] = -(2 * Ekin * atoms[proj_idx].mass)**0.5
+    # Running one timestep before impact
+    dyn.run(1)
 
-# Running the simulation for 80 timesteps
-dyn.run(80)
+    # Giving the target atom a kinetic energy of ene in the -z direction
+    atoms[-1].momentum[2] = -(2 * ekin * atoms[-1].mass)**0.5
 
-traj.close()
+    # Running the simulation for 80 timesteps
+    dyn.run(80)
