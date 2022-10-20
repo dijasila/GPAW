816455ccb28a5a0d7e8b29528a66bb8cf9f14725
Michael Walter <mcoywalter@gmail.com>
2022-10-20 12:57:29 +0200

    flakes

--- doc/tutorialsexercises/vibrational/rraman/H2O_static_raman.py
+++ doc/tutorialsexercises/vibrational/rraman/H2O_static_raman.py
@@ -9,7 +9,7 @@ xc = 'PBE'
 atoms = Cluster('relaxed.traj')
 atoms.minimal_box(4., h=h)
 
-atoms.calc = GPAW(xc=xc, h=h, 
+atoms.calc = GPAW(xc=xc, h=h,
                   occupations=FermiDirac(width=0.1),
                   symmetry={'point_group': False})
 atoms.get_potential_energy()
@@ -18,6 +18,7 @@ atoms.get_potential_energy()
 class Polarizability:
     def __call__(self, atoms):
         return static_polarizability(atoms)
+
 
 name = exname = 'static_raman'
 ram = StaticRamanCalculator(
