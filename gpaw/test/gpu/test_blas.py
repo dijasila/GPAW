2bfefd571670cfbf79738816557b378d1bb77071
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2022-10-21 12:28:19 +0300

    Unit test for GPU blas

---
+++ gpaw/test/gpu/test_blas.py
@@ -0,0 +1,50 @@
+import pytest
+import numpy as np
+from gpaw.utilities.blas import axpy, mmm, rk, r2k
+
+def test_blas(gpu):
+
+    rng = np.random.RandomState(42)
+    a = rng.random((100, 100))
+    b = rng.random((100, 100))
+    c = np.zeros_like(a)
+
+    a_gpu = gpu.copy_to_device(a)
+    b_gpu = gpu.copy_to_device(b)
+    c_gpu = gpu.array.zeros_like(a_gpu)
+
+    # axpy
+    axpy(0.5, a, c)
+    check_cpu = c.sum()
+    
+    axpy(0.5, a_gpu, c_gpu, cuda=True)
+    check_gpu = gpu.copy_to_host(c_gpu.sum())
+
+    assert check_cpu == pytest.approx(check_gpu, abs=1e-14) 
+
+    # mmm
+    mmm(0.5, a, 'N', b, 'N', 0.2, c)
+    check_cpu = c.sum()
+
+    mmm(0.5, a_gpu, 'N', b_gpu, 'N', 0.2, c_gpu, cuda=True)
+    check_gpu = gpu.copy_to_host(c_gpu.sum())
+
+    assert check_cpu == pytest.approx(check_gpu, rel=1e-14) 
+
+    # rk
+    rk(0.5, a, 0.2, c)
+    check_cpu = c.sum()
+    
+    rk(0.5, a_gpu, 0.2, c_gpu, cuda=True)
+    check_gpu = gpu.copy_to_host(c_gpu.sum())
+
+    assert check_cpu == pytest.approx(check_gpu, rel=1e-14) 
+ 
+    # r2k
+    r2k(0.5, a, b, 0.2, c)
+    check_cpu = c.sum()
+    
+    r2k(0.5, a_gpu, b_gpu, 0.2, c_gpu, cuda=True)
+    check_gpu = gpu.copy_to_host(c_gpu.sum())
+
+    assert check_cpu == pytest.approx(check_gpu, rel=1e-14) 
