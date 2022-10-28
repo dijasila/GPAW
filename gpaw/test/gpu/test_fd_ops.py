d960bf3b74e78726d610d3c156fe6a4423575029
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2022-10-28 10:34:35 +0300

    Test also periodic / complex case

--- gpaw/test/gpu/test_fd_ops.py
+++ gpaw/test/gpu/test_fd_ops.py
@@ -16,43 +16,54 @@ def test_fd_ops(gpu):
         domain_comm = world
 
     lat = 8.0
-    gd = GridDescriptor((32, 32, 32), (lat, lat, lat), comm=domain_comm)
+    for pbc in (True, False):
+        gd = GridDescriptor((32, 32, 32), (lat, lat, lat), pbc_c=pbc, comm=domain_comm)
 
-    # Use Gaussian as input
-    x, y, z = gd.get_grid_point_coordinates()
-    sigma = 1.5
-    mu = lat / 2.0
-    a = np.exp(-((x-mu)**2 + (y-mu)**2 + (z-mu)**2) / (2.0*sigma))
-    # analytic solution
-    b_analytic = (((x-mu)**2 + (y-mu)**2 + (z-mu)**2)/sigma**2 - 3.0/sigma) * a
+        if pbc:
+            dtype = complex
+            phase = np.ones((3, 2), complex)
+        else:
+            dtype = float
+            phase = None
 
-    b = np.zeros_like(a)
-    a_gpu = gpu.copy_to_device(a)
-    b_gpu = gpu.array.zeros_like(a_gpu)
+        # Use Gaussian as input
+        x, y, z = gd.get_grid_point_coordinates()
+        sigma = 1.5
+        mu = lat / 2.0
+     
+        a = gd.zeros(dtype=dtype)
+        a[:] = np.exp(-((x-mu)**2 + (y-mu)**2 + (z-mu)**2) / (2.0*sigma))
+        # analytic solution
+        b_analytic = np.zeros_like(a)
+        b_analytic[:] = (((x-mu)**2 + (y-mu)**2 + (z-mu)**2)/sigma**2 - 3.0/sigma) * a
 
-    # Laplace
-    Laplace(gd, 1.0, 3).apply(a, b)
-    Laplace(gd, 1.0, 3, cuda=True).apply(a_gpu, b_gpu)
-    b_ref = gpu.copy_to_host(b_gpu)
+        b = np.zeros_like(a)
+        a_gpu = gpu.copy_to_device(a)
+        b_gpu = gpu.array.zeros_like(a_gpu)
 
-    assert b == pytest.approx(b_ref, abs=1e-12)
-    # Neglect boundaries in check to analytic solution
-    assert b_analytic[2:-2, 2:-2, 2:-2] == pytest.approx(b_ref[2:-2, 2:-2, 2:-2], abs=1e-2)
+        # Laplace
+        Laplace(gd, 1.0, 3, dtype=dtype).apply(a, b, phase_cd=phase)
+        Laplace(gd, 1.0, 3, dtype=dtype, cuda=True).apply(a_gpu, b_gpu, phase_cd=phase)
+        b_ref = gpu.copy_to_host(b_gpu)
 
-    # Transformers
-    coarsegd = gd.coarsen()
-    a_coarse = coarsegd.zeros()
-    a_coarse_gpu = gpu.array.zeros_like(a_coarse)
+        assert b == pytest.approx(b_ref, abs=1e-12)
+        # Neglect boundaries in check to analytic solution
+        assert b_analytic[2:-2, 2:-2, 2:-2] == pytest.approx(b_ref[2:-2, 2:-2, 2:-2], abs=1e-2)
 
-    # Restrict
-    Transformer(gd, coarsegd, 1).apply(a, a_coarse)
-    # Interpolate
-    Transformer(coarsegd, gd, 1).apply(a_coarse, a)
+        # Transformers
+        coarsegd = gd.coarsen()
+        a_coarse = coarsegd.zeros(dtype=dtype)
+        a_coarse_gpu = gpu.array.zeros_like(a_coarse)
 
-    # Restrict
-    Transformer(gd, coarsegd, 1, cuda=True).apply(a_gpu, a_coarse_gpu)
-    # Interpolate
-    Transformer(coarsegd, gd, 1, cuda=True).apply(a_coarse_gpu, a_gpu)
-    a_ref = gpu.copy_to_host(a_gpu)
+        # Restrict
+        Transformer(gd, coarsegd, 1, dtype=dtype).apply(a, a_coarse, phases=phase)
+        # Interpolate
+        Transformer(coarsegd, gd, 1, dtype=dtype).apply(a_coarse, a, phases=phase)
+
+        # Restrict
+        Transformer(gd, coarsegd, 1, dtype=dtype, cuda=True).apply(a_gpu, a_coarse_gpu, phases=phase)
+        # Interpolate
+        Transformer(coarsegd, gd, 1, dtype=dtype, cuda=True).apply(a_coarse_gpu, a_gpu, phases=phase)
+        a_ref = gpu.copy_to_host(a_gpu)
     
-    assert a == pytest.approx(a_ref, abs=1e-14)
+        assert a == pytest.approx(a_ref, abs=1e-14)
