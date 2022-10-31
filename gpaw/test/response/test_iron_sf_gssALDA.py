28bf9590fa835da4dde0e3605ce55b4d5d58dec3
Jens Jørgen Mortensen <jjmo@dtu.dk>
2022-10-31 12:18:47 +0100

    Revert

--- gpaw/test/response/test_iron_sf_gssALDA.py
+++ gpaw/test/response/test_iron_sf_gssALDA.py
@@ -11,8 +11,11 @@ import numpy as np
 # Script modules
 import time
 
+from ase.build import bulk
+from ase.dft.kpoints import monkhorst_pack
 from ase.parallel import parprint
 
+from gpaw import GPAW, PW
 from gpaw.test import findpeak, equal
 from gpaw.mpi import world
 
@@ -23,8 +26,18 @@ from gpaw.response.susceptibility import read_macroscopic_component
 
 @pytest.mark.kspair
 @pytest.mark.response
-def test_response_iron_sf_gssALDA(in_tmp_dir, gpw_files):
+def test_response_iron_sf_gssALDA(in_tmp_dir):
     # ------------------- Inputs ------------------- #
+
+    # Part 1: ground state calculation
+    xc = 'LDA'
+    kpts = 4
+    nb = 6
+    pw = 300
+    a = 2.867
+    mm = 2.21
+
+    # Part 2: magnetic response calculation
     q_qc = [[0.0, 0.0, 0.0], [0.0, 0.0, 1. / 4.]]  # Two q-points along G-N
     frq_qw = [np.linspace(-0.080, 0.120, 26), np.linspace(0.100, 0.300, 26)]
     fxc = 'ALDA'
@@ -37,10 +50,29 @@ def test_response_iron_sf_gssALDA(in_tmp_dir, gpw_files):
         nblocks = 1
 
     # ------------------- Script ------------------- #
+
+    # Part 1: ground state calculation
+
     t1 = time.time()
+
+    Febcc = bulk('Fe', 'bcc', a=a)
+    Febcc.set_initial_magnetic_moments([mm])
+
+    calc = GPAW(xc=xc,
+                mode=PW(pw),
+                kpts=monkhorst_pack((kpts, kpts, kpts)),
+                nbands=nb,
+                symmetry={'point_group': False},
+                parallel={'domain': 1})
+
+    Febcc.calc = calc
+    Febcc.get_potential_energy()
+    calc.write('Fe', 'all')
+    t2 = time.time()
+
+    # Part 2: magnetic response calculation
     context = ResponseContext()
-    gs = ResponseGroundStateAdapter.from_gpw_file(gpw_files['fe_pw_wfs'],
-                                                  context=context)
+    gs = ResponseGroundStateAdapter.from_gpw_file('Fe', context=context)
     fxckwargs = {'rshelmax': None, 'fxc_scaling': fxc_scaling}
     tms = TransverseMagneticSusceptibility(gs,
                                            context=context,
@@ -57,9 +89,10 @@ def test_response_iron_sf_gssALDA(in_tmp_dir, gpw_files):
             filename='iron_dsus' + '_%d.csv' % (q + 1))
         tms.context.write_timer()
 
-    t2 = time.time()
+    t3 = time.time()
 
-    parprint('Excited state calculation took', (t2 - t1) / 60, 'minutes')
+    parprint('Ground state calculation took', (t2 - t1) / 60, 'minutes')
+    parprint('Excited state calculation took', (t3 - t2) / 60, 'minutes')
 
     world.barrier()
 
@@ -77,20 +110,19 @@ def test_response_iron_sf_gssALDA(in_tmp_dir, gpw_files):
     mw2 = wpeak2 * 1000
 
     # Part 4: compare new results to test values
-    test_fxcs = 1.011
+    test_fxcs = 1.033
     test_mw1 = -0.03  # meV
     test_mw2 = 176.91  # meV
-    test_mw2 = 260  # meV
     test_Ipeak1 = 71.20  # a.u.
-    test_Ipeak2 = 35  # a.u.
+    test_Ipeak2 = 44.46  # a.u.
 
     # fxc_scaling:
     equal(fxc_scaling[1], test_fxcs, 0.005)
 
     # Magnon peak:
     equal(mw1, test_mw1, 0.1)
-    assert mw2 == pytest.approx(test_mw2, abs=eta * 650)
+    equal(mw2, test_mw2, eta * 650)
 
     # Scattering function intensity:
-    assert Ipeak1 == pytest.approx(test_Ipeak1, abs=5)
-    assert Ipeak2 == pytest.approx(test_Ipeak2, abs=5)
+    equal(Ipeak1, test_Ipeak1, 5)
+    equal(Ipeak2, test_Ipeak2, 5)
