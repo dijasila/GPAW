90caec82ee21e02829eec59aaf372ece0437b64b
ggandus <ggandus@student.ethz.ch>
2022-11-02 16:08:35 +0100

    enh: tests accept arbitrary vacuum

--- gpaw/test/lcao/test_local_orbitals.py
+++ gpaw/test/lcao/test_local_orbitals.py
@@ -9,7 +9,7 @@ pytestmark = pytest.mark.skipif(world.size > 1,
 
 
 def test_gamma_point_calculation():
-    atoms = molecule('C6H6', vacuum=5)
+    atoms = molecule('C6H6', vacuum=2)
 
     calc = GPAW(mode='lcao', xc='LDA', basis='szp(dzp)', txt=None)
     calc.atoms = atoms
@@ -18,22 +18,26 @@ def test_gamma_point_calculation():
     los = LocalOrbitals(calc)
 
     los.subdiagonalize('C', groupby='energy')
+    group_energies = sorted(los.groups.keys())
 
     # Assert minimal model contains only pz-LOs
     los.take_model(minimal=True)
-    assert los.indices == los.groups[-6.9]
+    assert los.indices == los.groups[group_energies[3]]
     assert len(los.indices) == 6
 
     # Assert extended model also contains +2 d-LOs
     los.take_model(minimal=False)
     assert los.indices == (
-        los.groups[-6.9] + los.groups[20.2] + los.groups[21.6])
+        los.groups[group_energies[3]]
+        + los.groups[group_energies[-3]]
+        + los.groups[group_energies[-1]]
+    )
     assert len(los.indices) == (6 * 3)
 
 
 def test_k_point_calculation():
     atoms = graphene_nanoribbon(2, 1, type='zigzag', saturated=True,
-                                C_H=1.1, C_C=1.4, vacuum=5.0)
+                                C_H=1.1, C_C=1.4, vacuum=2)
 
     calc = GPAW(mode='lcao', xc='LDA', basis='szp(dzp)', txt=None, kpts={'size': (1, 1, 11), 'gamma': True},
                 symmetry={'point_group': False, 'time_reversal': True})
@@ -43,14 +47,18 @@ def test_k_point_calculation():
     los = LocalOrbitals(calc)
 
     los.subdiagonalize('C', groupby='symmetry')
+    group_energies = sorted(los.groups.keys())
 
     # Assert minimal model contains only pz-LOs
     los.take_model(minimal=True)
-    assert los.indices == los.groups[-7.2]
+    assert los.indices == los.groups[group_energies[3]]
     assert len(los.indices) == 4
 
     # Assert extended model also contains d-LOs
     los.take_model(minimal=False)
     assert los.indices == (
-        los.groups[-7.2] + los.groups[19.9] + los.groups[21.3])
+        los.groups[group_energies[3]]
+        + los.groups[group_energies[-3]]
+        + los.groups[group_energies[-1]]
+    )
     assert len(los.indices) == (4 * 3)
