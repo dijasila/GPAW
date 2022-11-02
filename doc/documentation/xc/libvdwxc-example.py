758bbc6ba2a4d6e59eb139f4f36616de8324656b
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-11-02 11:11:19 +0100

    FLAKE8

--- doc/documentation/xc/libvdwxc-example.py
+++ doc/documentation/xc/libvdwxc-example.py
@@ -3,8 +3,9 @@ from gpaw import GPAW
 
 atoms = molecule('H2O')
 atoms.center(vacuum=3.0)
-# There are functionals: vdW-DF, vdW-DF2, vdW-DF-cx, optPBE-vdW, optB88-vdW, C09-vdW, BEEF-vdW, and mBEEF-vdW
+# There are these functionals: vdW-DF, vdW-DF2, vdW-DF-cx, optPBE-vdW,
+# optB88-vdW, C09-vdW, BEEF-vdW, and mBEEF-vdW.
 # There are three modes: serial, mpi, and pfft. Default is auto.
-calc = GPAW(xc={'name':'BEEF-vdW', 'backend':'libvdwxc', 'mode': 'mpi'})
+calc = GPAW(xc={'name': 'BEEF-vdW', 'backend': 'libvdwxc', 'mode': 'mpi'})
 atoms.calc = calc
 atoms.get_potential_energy()
