fec32cb499125f7e2a281a4e7b9b68083628c0fd
madkru <madkru@dtu.dk>
2022-11-10 13:05:16 +0100

    Added optical_conductivity_test.py which causes MPIBLAST error when calculator is not loaded in serially.

--- gpaw/response/optical_conductivity_test.py
+++ gpaw/response/optical_conductivity_test.py
@@ -16,42 +16,28 @@ def parallelisation_sizes(kpts_k, rank=None):
     myKsize = len(myKrange)
     return myKrange, myKsize
 
-#def get_berrycurvature(calc, kpts_k=None, n1=0, n2=None, mi=None, mf=None, width=None, theta=0.0, phi=0.0, scale=1.0):
+
 def get_berrycurvature(calc_file, kpts_k=None, n1=0, n2=None, mi=None, mf=None, width=None, theta=0.0, phi=0.0, scale=1.0):
-    #calc1 = GPAW(calc_file, txt=None)
-    calc1 = GPAW(calc_file, txt=None, communicator=serial_comm)
-    
-    #calc2 = GPAW(calc_file, txt=None, communicator = serial_comm)
-    #calc2 = GPAW(calc_file, txt=None, world = serial_comm)
-    #calc = GPAW(calc, txt=None, communicator = serial_comm)
-
-
-    #kpts_kc = calc.get_bz_k_points()
+    calc1 = GPAW(calc_file, txt=None)
+        
     kpts_kc = calc1.get_bz_k_points()
     if kpts_k is None:
         kpts_k = range(len(kpts_kc))
 
     if width is not None:
-        #calc.wfs.occupations._width = width
         calc1.wfs.occupations._width = width
 
     if n2 is None:
-        #n2 = calc.get_number_of_bands()
         n2 = calc1.get_number_of_bands()
     bands_n = range(n1, n2)
 
-    #pdensity = PairDensity(calc, txt=None, communicator=serial_comm)
-    #pdensity = PairDensity(calc2, txt=None, world=serial_comm)
-
     myKrange, myKsize = parallelisation_sizes(kpts_k)
 
-    #soc = soc_eigenstates(calc, scale=scale, theta=theta, phi=phi, n1=n1, n2=n2)
     soc = soc_eigenstates(calc1, scale=scale, theta=theta, phi=phi, n1=n1, n2=n2)
     v_kmsn = soc.eigenvectors()
     f_km = soc.occupation_numbers()
     e_km = soc.eigenvalues() / Hartree
     Nn = len(bands_n)
-    #Ns = calc.get_number_of_spins()
     Ns = calc1.get_number_of_spins()
 
     berryc_xy_k = np.zeros(len(kpts_k))
@@ -65,10 +51,8 @@ def get_berrycurvature(calc_file, kpts_k=None, n1=0, n2=None, mi=None, mf=None, 
 
     for i in myKrange:
         calc2 = GPAW(calc_file, txt=None, communicator = serial_comm)
-        #pdensity = PairDensity(calc2, txt=None, communicator=serial_comm)
         pdensity = PairDensity(calc2, txt=None, world=serial_comm)
-        print('pdensity line passed')
-        
+
         ik = kpts_k[i]
         k_c = kpts_kc[ik]
 
@@ -92,10 +76,6 @@ def get_berrycurvature(calc_file, kpts_k=None, n1=0, n2=None, mi=None, mf=None, 
         frac_mm[inds_mm] = 0
         frac_mm = np.nan_to_num(frac_mm)
 
-        del deps_mm
-        del f_m
-        del f_mm
-
         rho_snnv = np.zeros((2, Nn, Nn, 3), complex)
         for s in range(Ns):
             kpt = pdensity.get_k_point(s, k_c, n1, n2)
@@ -110,8 +90,6 @@ def get_berrycurvature(calc_file, kpts_k=None, n1=0, n2=None, mi=None, mf=None, 
                            np.dot(v_msn[:, 0], rho_snnv[0]))                             
         rho_mmv += np.dot(v_msn[:, 1].conj(),                                       
                           np.dot(v_msn[:, 1], rho_snnv[1]))                             
-        del rho_snnv
-        del v_msn
 
         A_mm = rho_mmv[:, :, 0].conj() * rho_mmv[:, :, 1]
         berryc_xy_k[i] = np.einsum('mn, mn', A_mm, frac_mm).imag
@@ -121,8 +99,6 @@ def get_berrycurvature(calc_file, kpts_k=None, n1=0, n2=None, mi=None, mf=None, 
         
         A_mm = rho_mmv[:, :, 2].conj() * rho_mmv[:, :, 0]
         berryc_zx_k[i] = np.einsum('mn, mn', A_mm, frac_mm).imag
-
-        print(berryc_xy_k[i])
 
     berryc_xy_k = berryc_xy_k * Bohr**2
     berryc_yz_k = berryc_yz_k * Bohr**2
@@ -135,8 +111,6 @@ def get_berrycurvature(calc_file, kpts_k=None, n1=0, n2=None, mi=None, mf=None, 
     return berryc_xy_k, berryc_yz_k, berryc_zx_k
 
 
-#def get_optical_conductivity(calc, kpts_k=None, n1=0, n2=None, mi=None, mf=None, width=None, theta=0.0, phi=0.0, scale=1.0):
-#    berryc_xy_k, berryc_yz_k, berryc_zx_k = get_berrycurvature(calc, kpts_k=None, n1=0, n2=None, mi=None, mf=None, width=None, theta=0.0, phi=0.0, scale=1.0)
 def get_optical_conductivity(*args, **kwargs):
     berryc_xy_k, berryc_yz_k, berryc_zx_k = get_berrycurvature(*args, **kwargs)
 
