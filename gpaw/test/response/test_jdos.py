850220035290bf933ae57a064d196291666de44f
Thorbjørn Skovhus <s134264@student.dtu.dk>
2022-11-29 09:58:36 +0100

    calculate jdos in test

---
+++ gpaw/test/response/test_jdos.py
@@ -0,0 +1,40 @@
+# General modules
+import pytest
+import numpy as np
+from itertools import product
+
+# Script modules
+from gpaw import GPAW
+from gpaw.response import ResponseGroundStateAdapter
+from gpaw.response.frequencies import FrequencyDescriptor
+from gpaw.response.jdos import JDOSCalculator
+
+import matplotlib.pyplot as plt
+
+
+def test_iron_jdos(in_tmp_dir, gpw_files):
+    # ---------- Inputs ---------- #
+
+    q_qc = [[0.0, 0.0, 0.0], [0.0, 0.0, 1. / 4.]]  # Two q-points along G-N
+    wd = FrequencyDescriptor.from_array_or_dict(np.linspace(0.0, 4.0, 161))
+    eta = 0.2
+
+    spincomponent_s = ['00', '+-']
+
+    # ---------- Script ---------- #
+
+    # Get the ground state calculator from the fixture
+    calc = GPAW(gpw_files['fe_pw_wfs'], parallel=dict(domain=1))
+    nbands = calc.parameters.convergence['bands']
+
+    # Set up the JDOSCalculator
+    gs = ResponseGroundStateAdapter(calc)
+    jdos_calc = JDOSCalculator(gs)
+
+    for q_c, spincomponent in product(q_qc, spincomponent_s):
+        jdos_w = jdos_calc.calculate(q_c, wd,
+                                     eta=eta,
+                                     spincomponent=spincomponent)
+        plt.subplot()
+        plt.plot(wd.omega_w, jdos_w)
+        plt.show()
