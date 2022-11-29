c3e6605d0e879df460b5ba1b4123e18e6672766b
Thorbjørn Skovhus <s134264@student.dtu.dk>
2022-11-29 11:52:00 +0100

    add a manual calculator of the jdos for reference

--- gpaw/test/response/test_jdos.py
+++ gpaw/test/response/test_jdos.py
@@ -1,7 +1,12 @@
 # General modules
 import pytest
-import numpy as np
+
 from itertools import product
+
+import numpy as np
+from scipy.spatial import cKDTree
+
+import matplotlib.pyplot as plt
 
 # Script modules
 from ase.units import Hartree
@@ -10,8 +15,6 @@ from gpaw import GPAW
 from gpaw.response import ResponseGroundStateAdapter
 from gpaw.response.frequencies import FrequencyDescriptor
 from gpaw.response.jdos import JDOSCalculator
-
-import matplotlib.pyplot as plt
 
 
 def test_iron_jdos(in_tmp_dir, gpw_files):
@@ -33,12 +36,98 @@ def test_iron_jdos(in_tmp_dir, gpw_files):
     gs = ResponseGroundStateAdapter(calc)
     jdos_calc = JDOSCalculator(gs)
 
+    # Set up reference MyManualJDOS
+    jdos_refcalc = MyManualJDOS(calc)
+
     for q_c, spincomponent in product(q_qc, spincomponent_s):
         jdos_w = jdos_calc.calculate(q_c, wd,
                                      eta=eta,
                                      spincomponent=spincomponent,
                                      nbands=nbands)
+        jdosref_w = jdos_refcalc.calculate(q_c, wd.omega_w,
+                                           eta=eta,
+                                           spincomponent=spincomponent,
+                                           nbands=nbands)
         plt.subplot()
         plt.plot(wd.omega_w * Hartree, jdos_w)
+        plt.plot(wd.omega_w * Hartree, jdosref_w)
         plt.title(f'{q_c} {spincomponent}')
         plt.show()
+
+
+class MyManualJDOS:
+    def __init__(self, calc):
+        self.calc = calc
+
+        kd = calc.wfs.kd
+        gd = calc.wfs.gd
+        self.kd = kd
+        self.kdtree = cKDTree(np.mod(np.mod(kd.bzk_kc, 1).round(6), 1))
+        self.kweight = 1 / (gd.volume * len(kd.bzk_kc))
+
+    def calculate(self, q_c, omega_w, eta=0.05, spincomponent='00', nbands=None):
+        # Allocate array
+        jdos_w = np.zeros_like(omega_w)
+        
+        for K1, k1_c in enumerate(self.kd.bzk_kc):
+            # Get de = e2 - e1 and df = f2 - f1 of relevant transitions
+            de_t, df_t = self.get_transitions(K1, k1_c, q_c, spincomponent, nbands)
+
+            # Set up jdos
+            delta_wt = self.delta(omega_w, eta, de_t)
+            jdos_wt = - df_t[np.newaxis] * delta_wt  # weight δ-function by (f1 - f2)
+
+            # Sum over transitions
+            jdos_w += self.kweight * np.sum(jdos_wt, axis=1)
+
+        return jdos_w
+
+    @staticmethod
+    def delta(omega_w, eta, de_t):
+        """Create lorentzian delta-functions
+
+                ~ 1        η
+        δ(ω-Δε) = ‾ ‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+                  π (ω-Δε)^2 + η^2
+        """
+        x_wt = omega_w[:, np.newaxis] - de_t[np.newaxis]
+        return eta / np.pi / (x_wt**2. + eta**2.)
+
+    def get_transitions(self, K1, k1_c, q_c, spincomponent, nbands):
+        assert isinstance(nbands, int)
+        if spincomponent == '00':
+            s1_s = [0, 0]
+            s2_s = [1, 1]
+        elif spincomponent == '+-':
+            s1_s = [0]
+            s2_s = [1]
+        else:
+            raise ValueError(spincomponent)
+
+        # Find k_c + q_c
+        K2 = self.kdtree.query(np.mod(np.mod(k1_c + q_c, 1).round(6), 1))[1]
+
+        de_t = []
+        df_t = []
+        kd = self.kd
+        calc = self.calc
+        for s1, s2 in zip(s1_s, s2_s):
+            # Get composite u=(s,k) indices and KPoint objects
+            u1 = kd.bz2ibz_k[K1] + s1 * kd.nibzkpts
+            u2 = kd.bz2ibz_k[K2] + s2 * kd.nibzkpts
+            kpt1, kpt2 = calc.wfs.kpt_u[u1], calc.wfs.kpt_u[u2]
+
+            # Extract eigenenergies and occupation numbers
+            eps1_n, eps2_n = kpt1.eps_n[:nbands], kpt2.eps_n[:nbands]
+            f1_n, f2_n = kpt1.f_n[:nbands], kpt2.f_n[:nbands]
+
+            # Append data
+            de_nm = eps2_n[:, np.newaxis] - eps1_n[np.newaxis]
+            df_nm = f2_n[:, np.newaxis] - f1_n[np.newaxis]
+            de_t += list(de_nm.flatten())
+            df_t += list(df_nm.flatten())
+        de_t = np.array(de_t)
+        df_t = np.array(df_t)
+
+        return de_t, df_t
+        
