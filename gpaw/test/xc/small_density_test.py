2a86bf21d67c89c993c25a5939671551c04c0b8e
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-11-30 14:06:14 +0100

    ...

---
+++ gpaw/test/xc/small_density_test.py
@@ -0,0 +1,24 @@
+import pytest
+import numpy as np
+from gpaw.xc.libxc import LibXC
+from gpaw.xc.kernel import XCKernel
+
+
+@pytest.mark.libxc
+def test_xc_xc():
+    name = 'LDA'
+    for xc in [LibXC(name), XCKernel(name)]:
+        n0 = 1e-10
+        n_sr = np.array([[-2, -0.5, 0.0, 0.45, 0.55, 1.5]]) * n0
+        n_sr = np.linspace(-15, -9, 101)[np.newaxis]
+        s_xr = np.zeros_like(n_sr)
+        e_r = np.empty_like(n_sr[0])
+        v_sr = np.zeros_like(n_sr)
+        w_xr = np.zeros_like(n_sr)
+        # xc.calculate(e_r, 10**n_sr, v_sr, s_xr, w_xr)
+        xc.calculate(e_r, 10**n_sr, v_sr)
+        import matplotlib.pyplot as plt
+        plt.plot(n_sr[0], v_sr[0])
+    plt.show()
+    print(e_r)
+    print(v_sr[0])
