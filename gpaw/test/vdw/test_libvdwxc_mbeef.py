0b2290fe386527ffdd9481348c348a72b244e60e
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-12-06 08:17:50 +0100

    Adjust values to match fixed MGGA implementation

--- gpaw/test/vdw/test_libvdwxc_mbeef.py
+++ gpaw/test/vdw/test_libvdwxc_mbeef.py
@@ -26,7 +26,7 @@ def test_vdw_libvdwxc_mbeef():
                 setups={'Si': setup})
     system.calc = calc
     e = system.get_potential_energy()
-    ref = -60.544411820017906
+    ref = -60.53662105617721
     err = abs(e - ref)
     print('e=%r ref=%r err=%r' % (e, ref, err))
     # It would be reasonable to put 1e-6 as tolerance,
