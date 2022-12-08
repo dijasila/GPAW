f92be85a2896f55773c436728cd3df4cc9151e39
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2022-12-08 14:14:16 +0100

    Collect tests systems

---
+++ gpaw/test/big/test_systems/create.py
@@ -0,0 +1,263 @@
+from math import sqrt
+
+import ase.io
+import numpy as np
+from ase import Atoms
+from ase.build import add_adsorbate, fcc100, fcc111, molecule
+from ase.io import read
+from ase.lattice.cubic import FaceCenteredCubic
+from gpaw import PW, FermiDirac
+from gpaw.utilities import h2gpts
+
+
+def _create_test_systems():
+    systems = {}
+    for name, obj in globals().items():
+        if callable(obj) and not name.startswith('_'):
+            atoms, params = obj()
+            systems[name] = (atoms, params)
+    return systems
+
+
+def a5():
+    atoms = ase.io.read('alpra_Mg.CONTCAR')
+    atoms.pbc = True
+    return atoms, dict(xc='vdW-DF',
+                       occupations={'name': 'fermi-dirac', 'width': 0.1})
+
+
+def ni100():
+    atoms = fcc100(symbol='Ni', size=(1, 1, 9), a=3.52, vacuum=5.5)
+    atoms.set_initial_magnetic_moments([0.6] * len(atoms))
+    gpts = h2gpts(0.18, atoms.cell, idiv=8)
+    return atoms, dict(gpts=gpts,
+                       kpts=(8, 8, 1),
+                       xc='PBE')
+
+
+def biimtf():
+    atoms = read('biimtf.xyz')
+    atoms.center(vacuum=5)
+    return atoms, dict(h=0.16,
+                       charge=+1,
+                       occupations=FermiDirac(0.05),
+                       xc='RPBE')
+
+
+def opt111b():
+    atoms = fcc111('Pt', (2, 2, 6), a=4.00, vacuum=10.0)
+    add_adsorbate(atoms, 'O', 2.0, 'fcc')
+    return atoms, dict(mode='pw',
+                       kpts=(8, 8, 1),
+                       xc='RPBE')
+
+
+def scsz():
+    # the system losing magnetic moment
+    atoms = read('ScSZ.xyz')
+    atoms.set_cell([[7.307241, 0., 0.],
+                   [0., 12.656514, 0.],
+                   [0., 0., 19.]],
+                   scale_atoms=False)
+    atoms.center(axis=2)
+    magmoms = [0.0 for n in range(len(atoms))]
+    for n, a in enumerate(atoms):
+        if a.symbol == 'Ni':
+            magmoms[n] = 0.6
+    atoms.set_initial_magnetic_moments(magmoms)
+
+    atoms.pbc = (True, True, False)
+
+    return atoms, dict(h=0.2,
+                       kpts=(2, 1, 1),
+                       xc='PBE',
+                       maxiter=100,
+                       txt='ScSZ.txt')
+
+
+def tio2v2o5():
+    atoms = Atoms('(O2Ti4O6)3V2O5',
+                  [(8.3411, 1.9309, 9.2647),
+                   (3.2338, 0.0854, 9.4461),
+                   (1.5783, 0.1417, 8.4327),
+                   (6.6126, 2.0588, 8.2126),
+                   (9.4072, 2.0891, 7.6857),
+                   (4.2748, 0.1256, 7.9470),
+                   (9.9477, 0.2283, 7.3281),
+                   (4.7391, 2.0618, 7.6111),
+                   (7.7533, 2.1354, 6.7529),
+                   (2.6347, 0.2585, 6.9403),
+                   (6.2080, 0.1462, 8.6280),
+                   (0.9517, 1.9798, 8.9832),
+                   (8.5835, 6.2280, 9.4350),
+                   (3.1930, 3.9040, 9.7886),
+                   (1.4899, 4.0967, 8.8898),
+                   (6.6167, 5.8865, 8.8463),
+                   (9.3207, 5.9258, 7.7482),
+                   (4.1984, 3.9386, 8.2442),
+                   (9.9075, 3.9778, 7.6337),
+                   (4.7626, 5.8322, 8.0051),
+                   (7.6143, 5.6963, 7.1276),
+                   (2.5760, 3.9661, 7.3115),
+                   (6.2303, 3.8223, 8.8067),
+                   (1.1298, 5.9913, 8.6968),
+                   (8.3845, 9.7338, 9.1214),
+                   (3.1730, 7.9593, 9.3632),
+                   (1.5914, 7.8120, 8.2310),
+                   (6.7003, 9.7064, 8.1528),
+                   (9.3943, 9.7202, 7.6037),
+                   (4.3168, 7.7857, 7.9666),
+                   (9.9045, 7.7968, 7.2716),
+                   (4.7772, 9.7015, 7.4648),
+                   (7.7314, 9.7221, 6.6253),
+                   (2.7673, 7.6929, 6.8222),
+                   (6.2358, 7.8628, 8.6557),
+                   (1.0528, 9.7017, 8.5919),
+                   (8.4820, 5.0952, 11.4981),
+                   (9.7787, 2.0447, 11.0800),
+                   (6.4427, 5.6315, 10.7415),
+                   (10.7389, 0.4065, 11.8697),
+                   (8.3109, 3.0048, 12.4083),
+                   (10.4702, 4.1612, 10.6543),
+                   (8.9827, 6.3884, 13.0109)],
+                  cell=[10.152054, 11.430000, 18.295483],
+                  pbc=[1, 1, 0])
+
+    return atoms, dict(h=0.20,
+                       kpts=(2, 2, 1),
+                       xc='RPBE')
+
+
+def pt_h2o():
+    atoms = read('Pt_H2O.xyz')
+    atoms.set_cell([[8.527708, 0, 0],
+                   [0, 4.923474, 0],
+                   [0, 0, 16]],
+                   scale_atoms=False)
+    atoms.center(axis=2)
+
+    atoms.pbc = (True, True, False)
+
+    return atoms, dict(h=0.20,
+                       kpts=(2, 4, 1),
+                       xc='RPBE',
+                       poissonsolver={'dipolelayer': 'xy'},
+                       basis='dzp',
+                       maxiter=200,
+                       txt='Pt_H2O.txt')
+
+
+def lih():
+    atoms = molecule('LiH')
+    atoms.cell = [12, 12.01, 12.02]
+    atoms.center()
+    return atoms, dict(mode=PW(400),
+                       xc='PBE')
+
+
+def gs_small():
+    NBN = 7
+    NGr = 7
+    a = 2.5
+    c = 3.22
+
+    GR = Atoms(symbols='C2',
+               positions=[(0.5 * a, -sqrt(3) / 6 * a, 0.0),
+                          (0.5 * a, +sqrt(3) / 6 * a, 0.0)],
+               cell=[(0.5 * a, -0.5 * 3**0.5 * a, 0),
+                     (0.5 * a, +0.5 * 3**0.5 * a, 0), (0.0, 0.0, c * 2.0)])
+    GR.set_pbc((True, True, True))
+
+    GR2 = GR.copy()
+    cell = GR2.get_cell()
+    uv = cell[0] - cell[1]
+    uv = uv / np.sqrt(np.sum(uv**2.0))
+    dist = np.array([0.5 * a, -sqrt(3) / 6 * a]) - np.array(
+        [0.5 * a, +sqrt(3) / 6 * a])
+    dist = np.sqrt(np.sum(dist**2.0))
+    GR2.translate(uv * dist)
+
+    BN = Atoms(symbols='BN',
+               positions=[(0.5 * a, -sqrt(3) / 6 * a, 0.0),
+                          (0.5 * a, +sqrt(3) / 6 * a, 0.0)],
+               cell=[(0.5 * a, -0.5 * 3**0.5 * a, 0),
+                     (0.5 * a, +0.5 * 3**0.5 * a, 0), (0.0, 0.0, c * 2.0)])
+    BN.set_pbc((True, True, True))
+
+    NB = Atoms(symbols='NB',
+               positions=[(0.5 * a, -sqrt(3) / 6 * a, 0.0),
+                          (0.5 * a, +sqrt(3) / 6 * a, 0.0)],
+               cell=[(0.5 * a, -0.5 * 3**0.5 * a, 0),
+                     (0.5 * a, +0.5 * 3**0.5 * a, 0), (0.0, 0.0, c * 2.0)])
+    NB.set_pbc((True, True, True))
+
+    GR2.translate([0, 0, c])
+    NB.translate([0, 0, (NGr + 1.0 * (1 - NGr % 2)) * c] +
+                 uv * dist * (NGr % 2))
+    BN.translate([0, 0, (NGr + 1.0 * (NGr % 2)) * c] + uv * dist * (NGr % 2))
+
+    GRBN = (GR * (1, 1, (NGr % 2 + NGr // 2)) + GR2 * (1, 1, (NGr // 2)) + NB *
+            (1, 1, (NBN // 2 + (NBN % 2) * (NGr % 2))) + BN *
+            (1, 1, (NBN // 2 + (NBN % 2) * (1 - NGr % 2))))
+    BNNB = BN + NB
+    Graphite = GR + GR2
+
+    Graphite.set_pbc((True, True, True))
+    old_cell = GR.get_cell()
+    old_cell[2, 2] = 2 * c
+    Graphite.set_cell(old_cell)
+
+    BNNB.set_pbc((True, True, True))
+    old_cell = BN.get_cell()
+    old_cell[2, 2] = 2 * c
+    BNNB.set_cell(old_cell)
+    BNNB.center()
+
+    GRBN.set_pbc((True, True, True))
+    old_cell = BN.get_cell()
+    old_cell[2, 2] = (NGr + NBN) * c
+    GRBN.set_cell(old_cell)
+
+    atoms = GRBN
+
+    return atoms, dict(h=0.18,
+                       mode=PW(600),
+                       kpts=(29, 29, 1),
+                       xc='PBE',
+                       occupations=FermiDirac(0.01),
+                       parallel={'band': 1})
+
+
+def na2():
+    atoms = molecule('Na2')
+    atoms.cell = [12, 12.01, 12.02]
+    atoms.center()
+    return atoms, dict(mode=PW(400),
+                       xc='PBE')
+
+
+def opt111():
+    atoms = fcc111('Pt', (2, 2, 6), a=4.00, vacuum=10.0)
+    add_adsorbate(atoms, 'O', 2.5, 'fcc')
+    return atoms, dict(mode='pw',
+                       kpts=(8, 8, 1),
+                       xc='RPBE')
+
+
+def pt13():
+    element = 'Pt'
+    atoms = FaceCenteredCubic(symbol=element,
+                              size=(2, 2, 2),
+                              directions=[[1, 1, 0],
+                                          [-1, 1, 0],
+                                          [0, 0, 1]])
+    del atoms[4]
+    del atoms[3]
+    del atoms[2]
+
+    kpts = (8, 8, 4)
+    ecut = 800
+    xc1 = 'PBE'
+    return atoms, dict(mode=PW(ecut=ecut),
+                       kpts=kpts,
+                       xc=xc1)
