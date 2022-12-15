74befab87b1ccf2f68af8296854be8e1330021b7
Julian <juhe@debian-BULLSEYE-live-builder-AMD64>
2022-12-15 15:29:12 +0100

    remove line

--- gpaw/test/response/test_bse_MoS2_cut.py
+++ gpaw/test/response/test_bse_MoS2_cut.py
@@ -11,34 +11,32 @@ from gpaw.response.df import read_response_function
 
 @pytest.mark.response
 def test_response_bse_MoS2_cut(in_tmp_dir, scalapack):
-    if 1:
-        calc = GPAW(mode=PW(180),
-                    xc='PBE',
-                    nbands='nao',
-                    setups={'Mo': '6'},
-                    parallel={'band': 1, 'domain': 1},
-                    occupations=FermiDirac(0.001),
-                    convergence={'bands': -5},
-                    kpts=(5, 5, 1))
+    calc = GPAW(mode=PW(180),
+                xc='PBE',
+                nbands='nao',
+                setups={'Mo': '6'},
+                occupations=FermiDirac(0.001),
+                convergence={'bands': -5},
+                kpts=(5, 5, 1))
 
-        a = 3.1604
-        c = 10.0
+    a = 3.1604
+    c = 10.0
 
-        cell = Hexagonal(symbol='Mo',
-                         latticeconstant={'a': a, 'c': c}).get_cell()
-        layer = Atoms(symbols='MoS2', cell=cell, pbc=(1, 1, 0),
-                      scaled_positions=[(0, 0, 0),
-                                        (2 / 3, 1 / 3, 0.3),
-                                        (2 / 3, 1 / 3, -0.3)])
+    cell = Hexagonal(symbol='Mo',
+                     latticeconstant={'a': a, 'c': c}).get_cell()
+    layer = Atoms(symbols='MoS2', cell=cell, pbc=(1, 1, 0),
+                  scaled_positions=[(0, 0, 0),
+                                    (2 / 3, 1 / 3, 0.3),
+                                    (2 / 3, 1 / 3, -0.3)])
 
-        pos = layer.get_positions()
-        pos[1][2] = pos[0][2] + 3.172 / 2
-        pos[2][2] = pos[0][2] - 3.172 / 2
-        layer.set_positions(pos)
-        layer.center(axis=2)
-        layer.calc = calc
-        layer.get_potential_energy()
-        calc.write('MoS2.gpw', mode='all')
+    pos = layer.get_positions()
+    pos[1][2] = pos[0][2] + 3.172 / 2
+    pos[2][2] = pos[0][2] - 3.172 / 2
+    layer.set_positions(pos)
+    layer.center(axis=2)
+    layer.calc = calc
+    layer.get_potential_energy()
+    calc.write('MoS2.gpw', mode='all')
 
     bse = BSE('MoS2.gpw',
               spinors=True,
