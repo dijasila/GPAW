a8f18175d344d99123d20280eb318ff66d8e3db6
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-01-09 14:35:39 +0100

    Guard against too old scipy

--- gpaw/test/gpu/pw_test.py
+++ gpaw/test/gpu/pw_test.py
@@ -1,10 +1,12 @@
 import pytest
 from ase import Atoms
 from gpaw.new.calculation import DFTCalculation
+from gpaw import SCIPY_VERSION
 
 
 @pytest.mark.gpu
 @pytest.mark.serial
+@pytest.mark.skipif(SCIPY_VERSION < (1, 6), 'Too old scipy')
 def test_gpu_pw():
     atoms = Atoms('H2')
     atoms.positions[1, 0] = 0.75
