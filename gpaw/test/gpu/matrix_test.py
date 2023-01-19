4ff2913d5cf44fe0e25b3e53eb6aaf4f839e6790
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-01-19 15:58:43 +0100

    Test that eigenvectors are parallel

--- gpaw/test/gpu/matrix_test.py
+++ gpaw/test/gpu/matrix_test.py
@@ -22,11 +22,22 @@ def test_zyrk():
 @pytest.mark.gpu
 @pytest.mark.serial
 def test_eigh():
-    H = Matrix(2, 2, data=np.array([[2, 42.1 + 42.1j], [0.1 - 0.1j, 3]]))
-    S = Matrix(2, 2, data=np.array([[1, 42.1 + 42.2j], [0.1 - 0.2j, 0.9]]))
-    h = Matrix(2, 2, data=cp.asarray(H.data))
-    s = Matrix(2, 2, data=cp.asarray(S.data))
-    E = H.eigh(S)
-    e = h.eigh(s)
-    assert as_xp(e, np) == pytest.approx(E)
-    assert h.to_cpu().data == pytest.approx(H.data)
+    H1 = Matrix(2, 2, data=np.array([[2, 42.1 + 42.1j], [0.1 - 0.1j, 3]]))
+    S1 = Matrix(2, 2, data=np.array([[1, 42.1 + 42.2j], [0.1 - 0.2j, 0.9]]))
+    H2 = Matrix(2, 2, data=cp.asarray(H1.data))
+    S2 = Matrix(2, 2, data=cp.asarray(S1.data))
+
+    E1 = H1.eigh(S1)
+
+    S0 = S1.copy()
+    S0.tril2full()
+
+    E2 = H2.eigh(S2)
+    assert as_xp(E2, np) == pytest.approx(E1)
+
+    C1 = H1.data
+    C2 = H2.to_cpu().data
+
+    # Check that eigenvectors are parallel:
+    X = C1.conj() @ S0.data @ C2.T
+    assert abs(X) == pytest.approx(np.eye(2))
