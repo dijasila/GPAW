36371ef763dbef5f01420bb4822fbe267458444e
Julian <juhe@debian-BULLSEYE-live-builder-AMD64>
2023-01-23 11:53:10 +0100

    decrease some indices of the items in the results list, because CP_dyn was removed.

--- doc/tutorialsexercises/energetics/fxc_correlation/diam_kern.ralda_09_compare.py
+++ doc/tutorialsexercises/energetics/fxc_correlation/diam_kern.ralda_09_compare.py
@@ -6,8 +6,8 @@ results = np.loadtxt('diam_kern.ralda_kernel_comparison.dat')
 ralda_wave = results[0]
 raldac = results[1]
 JGMs = results[2]
-range_RPA = results[4]
-RPA = results[5]
+range_RPA = results[3]
+RPA = results[4]
 
 equal(ralda_wave, -9.08, 0.01)
 equal(raldac, -9.01, 0.01)
