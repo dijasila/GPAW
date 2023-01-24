7e0732422b9ac200387a5b0b4d6a770eda12725f
Julian Heske <juhe@slid.fysik.dtu.dk>
2023-01-24 16:05:45 +0100

    updating doc/tutorialsexercises/energetics/fxc_correlation/submit.agts.py and the corresponding scripts trying to make it work again

--- doc/tutorialsexercises/energetics/fxc_correlation/submit.agts.py
+++ doc/tutorialsexercises/energetics/fxc_correlation/submit.agts.py
@@ -1,32 +1,19 @@
+from myqueue.workflow import run
+
 def workflow():
-    # Most of these time out at the moment ...
-    return []
-
-
-"""
-    gs_H_lda = queue.add('H.ralda_01_lda.py', ncpus=2, walltime=5)
-    queue.add('H.ralda_02_rpa_at_lda.py', deps=gs_H_lda, ncpus=16, walltime=20)
-    queue.add('H.ralda_03_ralda.py', deps=gs_H_lda, ncpus=16, walltime=200)
-    gs_H_pbe = queue.add('H.ralda_04_pbe.py', ncpus=2, walltime=5)
-    queue.add('H.ralda_05_rpa_at_pbe.py', deps=gs_H_pbe, ncpus=16, walltime=20)
-    queue.add('H.ralda_06_rapbe.py', deps=gs_H_pbe, ncpus=16, walltime=200)
-    gs_CO = queue.add('CO.ralda_01_pbe+exx.py', ncpus=1, walltime=1000)
-    queue.add('CO.ralda_02_CO_rapbe.py', deps=gs_CO, ncpus=16, walltime=2000)
-    queue.add('CO.ralda_03_C_rapbe.py', deps=gs_CO, ncpus=16, walltime=2000)
-    queue.add('CO.ralda_04_O_rapbe.py', deps=gs_CO, ncpus=16, walltime=2000)
-    gs_diamond = queue.add('diamond.ralda_01_pbe.py', deps=gs_CO,
-                           ncpus=1, walltime=100)
-    queue.add('diamond.ralda_02_rapbe_rpa.py', deps=gs_diamond,
-              ncpus=16, walltime=1200)
-    diamkern_gs = queue.add('diam_kern.ralda_01_lda.py', ncpus=8, walltime=2)
-    diamkern_ralda = queue.add('diam_kern.ralda_03_ralda_wave.py',
-                               deps=diamkern_gs, ncpus=8, walltime=5)
-    diamkern_raldac = queue.add('diam_kern.ralda_04_raldac.py',
-                                deps=diamkern_ralda, ncpus=8, walltime=5)
-    diamkern_rangeRPA = queue.add('diam_kern.ralda_07_range_rpa.py',
-                                  deps=diamkern_CPdyn, ncpus=8, walltime=5)
-    diamkern_RPA = queue.add('diam_kern.ralda_08_rpa.py',
-                             deps=diamkern_rangeRPA, ncpus=8, walltime=5)
-    queue.add('diam_kern.ralda_09_compare.py',
-              deps=diamkern_RPA, ncpus=1, walltime=5)
-"""
+    with run(script='H.ralda_01_lda.py', cores=2, tmax='5m'):
+        run(script='H.ralda_02_rpa_at_lda.py', cores=16, tmax='20m')
+        run(script='H.ralda_03_ralda.py', cores=16, tmax='200m')
+    with run(script='H.ralda_04_pbe.py', cores=2, tmax='5m'):
+        run(script='H.ralda_05_rpa_at_pbe.py', cores=16, tmax='20m')
+        run(script='H.ralda_06_rapbe.py', cores=16, tmax='200m')
+    with run(script='CO.ralda_01_pbe_exx.py', cores=16, tmax='1000m'):
+        run(script='CO.ralda_02_CO_rapbe.py', cores=16, tmax='2000m')
+        run(script='CO.ralda_03_C_rapbe.py', cores=16, tmax='2000m')
+        run(script='CO.ralda_04_O_rapbe.py', cores=16, tmax='2000m')
+    with run(script='diamond.ralda_01_pbe.py', cores=1, tmax='100m'):
+        run(script='diamond.ralda_02_rapbe_rpa.py', cores=16, tmax='1200m')
+    with run(script='diam_kern.ralda_01_lda.py', cores=8, tmax='2m'):
+        with run(script='diam_kern.ralda_03_ralda_wave.py', cores=8, tmax='5m'):
+            with run(script='diam_kern.ralda_08_rpa.py', cores=8, tmax='5m'):
+                run(script='diam_kern.ralda_09_compare.py', cores=1, tmax='5m')
