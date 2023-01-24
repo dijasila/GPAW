40c02db2d8204c5c475f1a3efb2df2d1f1359cca
Julian Heske <juhe@slid.fysik.dtu.dk>
2023-01-24 17:00:34 +0100

    flake8

--- doc/tutorialsexercises/energetics/fxc_correlation/submit.agts.py
+++ doc/tutorialsexercises/energetics/fxc_correlation/submit.agts.py
@@ -1,5 +1,6 @@
 from myqueue.workflow import run
 
+
 def workflow():
     with run(script='H.ralda_01_lda.py', cores=2, tmax='5m'):
         run(script='H.ralda_02_rpa_at_lda.py', cores=16, tmax='20m')
@@ -14,6 +15,7 @@ def workflow():
     with run(script='diamond.ralda_01_pbe.py', cores=1, tmax='2h'):
         run(script='diamond.ralda_02_rapbe_rpa.py', cores=16, tmax='20h')
     with run(script='diam_kern.ralda_01_lda.py', cores=8, tmax='2m'):
-        with run(script='diam_kern.ralda_03_ralda_wave.py', cores=8, tmax='5m'):
+        with run(script='diam_kern.ralda_03_ralda_wave.py',
+                 cores=8, tmax='5m'):
             with run(script='diam_kern.ralda_08_rpa.py', cores=8, tmax='5m'):
                 run(script='diam_kern.ralda_09_compare.py', cores=1, tmax='5m')
