9e454f41d0c6ec4df726e31ba0af82fa495bc231
Martin Ovesen <martin.ovesen@outlook.dk>
2023-01-24 14:25:13 +0100

    Dummy commit

--- doc/tutorialsexercises/opticalresponse/gw_tutorial/check_gw.py
+++ doc/tutorialsexercises/opticalresponse/gw_tutorial/check_gw.py
@@ -6,6 +6,7 @@ g0w0g = 'MoS2_g0w0g_40_results_GWG.pckl'
 res_g0w0 = pickle.load(open(g0w0, 'rb'), encoding='bytes')
 res_g0w0g = pickle.load(open(g0w0g, 'rb'), encoding='bytes')
 
+
 assert abs(res_g0w0['qp'][0, 0, 3:7] -
            [0.765, 2.248, 5.944, 5.944]).max() < 0.01
 assert abs(res_g0w0g['qp'][0, 0, 3:7] -
