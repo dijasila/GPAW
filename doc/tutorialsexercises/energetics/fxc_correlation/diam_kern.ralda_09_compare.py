1aaa850018fa0e6de3e1cca866603ceded4d417e
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-01-24 13:48:18 +0100

    remove range stuff from tutorials

--- doc/tutorialsexercises/energetics/fxc_correlation/diam_kern.ralda_09_compare.py
+++ doc/tutorialsexercises/energetics/fxc_correlation/diam_kern.ralda_09_compare.py
@@ -4,9 +4,7 @@ from gpaw.test import equal
 results = np.loadtxt('diam_kern.ralda_kernel_comparison.dat')
 
 ralda_wave = results[0]
-range_RPA = results[1]
-RPA = results[2]
+RPA = results[1]
 
 equal(ralda_wave, -9.08, 0.01)
-equal(range_RPA, -13.84, 0.01)
 equal(RPA, -11.17, 0.01)
