d33f67c5964dedcb45a819c92be1a2aa950050d9
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-02-01 08:56:41 +0100

    Add GPU-fft test

---
+++ gpaw/test/gpu/fft_test.py
@@ -0,0 +1,14 @@
+from gpaw
+
+def test_fft():
+    L = 5
+    n = 20
+    grid = UniformGrid(cell=[L, L, L], size=(n, n, n), comm=world)
+    f = grid.zeros()
+
+
+    pw = PlaneWaves(cell=grid.cell, ecut=700, comm=world)
+    f2 = f.fft(pw=pw)
+
+    assert abs(f2.integrate()) < 1e-14
+    assert f2.moment() == pytest.approx([0, moment, 0], abs=1e-5)
