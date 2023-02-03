a0e65d827366b9169935ab122d79b7857c202259
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-02-03 16:35:31 +0100

    Test ACF on GPU

--- gpaw/test/core/acf_test.py
+++ gpaw/test/core/acf_test.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 from gpaw.core import PlaneWaves, UniformGrid
 from gpaw.mpi import world
-from gpaw.gpu import cupy
+from gpaw.gpu import cupy as cp
 
 
 a = 2.5
@@ -16,13 +16,14 @@ def grid():
                        dtype=complex)
 
 
+# Gussian:
 alpha = 4.0
 s = (0, 3.0, lambda r: np.exp(-alpha * r**2))
-I = np.pi / 2 / alpha**1.5
+gauss_integral = np.pi / 2 / alpha**1.5
 
 
 @pytest.mark.ci
-@pytest.mark.parametrize('xp', [np])
+@pytest.mark.parametrize('xp', [np, cp])
 def test_acf_fd(grid, xp):
 
     basis = grid.atom_centered_functions(
@@ -32,27 +33,27 @@ def test_acf_fd(grid, xp):
     if 0 in coefs:
         print(coefs[0])
         coefs[0] = [1.0]
-    f1 = grid.zeros()
-    basis.add_to(f1, coefs)
+    f1 = grid.zeros(xp=xp)
+    basis.add_to(f1, coefs.to_xp(xp))
 
     if 0:
         from sympy import integrate, exp, oo, var
         a0, r = var('a, r')
         integrate(exp(-a0 * r**2) * r**2, (r, 0, oo))
 
-    assert f1.integrate() == pytest.approx(I)
+    assert f1.integrate() == pytest.approx(gauss_integral)
 
     f2 = f1.gather(broadcast=True)
-    x, y = f2.xy(10, 10, ...)
+    x, y = f2.xy(n // 2, n // 2, ...)
     y0 = np.exp(-alpha * (x - a / 2)**2) / (4 * np.pi)**0.5
     assert abs(y - y0).max() == pytest.approx(0.0, abs=0.001)
 
 
 @pytest.mark.ci
 @pytest.mark.gpu
-@pytest.mark.parametrize('xp', [np, cupy])
+@pytest.mark.parametrize('xp', [np, cp])
 def test_acf_pw(grid, xp):
-    if world.size > 1 and xp is cupy:
+    if world.size > 1 and xp is cp:
         pytest.skip()
 
     pw = PlaneWaves(ecut=50, cell=grid.cell, dtype=complex, comm=world)
@@ -68,7 +69,7 @@ def test_acf_pw(grid, xp):
 
     f1 = pw.zeros(xp=xp)
     basis.add_to(f1, coefs)
-    assert f1.integrate().item() == pytest.approx(I)
+    assert f1.integrate().item() == pytest.approx(gauss_integral)
     f2 = f1.gather(broadcast=True)
     r2 = f2.ifft(grid=grid.new(comm=None))
     x, y = r2.xy(10, 10, ...)
