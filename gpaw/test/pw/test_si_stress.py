4d75bbb070ca30a2eeb734ae94ab69388c7b6b1c
varunrp <vrapa@dtu.dk>
2023-02-07 12:52:45 +0100

    speed up test silicon stress

--- gpaw/test/pw/test_si_stress.py
+++ gpaw/test/pw/test_si_stress.py
@@ -2,13 +2,14 @@ import numpy as np
 from ase.build import bulk
 from gpaw import GPAW, PW, Mixer
 from gpaw.mpi import world
+from ase.calculators.test import numeric_force
 
 
 def test_pw_si_stress(in_tmp_dir):
     xc = 'PBE'
     si = bulk('Si')
-    k = 3
-    si.calc = GPAW(mode=PW(250),
+    k = 2
+    si.calc = GPAW(mode=PW(200),
                    mixer=Mixer(0.7, 5, 50.0),
                    xc=xc,
                    kpts=(k, k, k),
@@ -26,12 +27,32 @@ def test_pw_si_stress(in_tmp_dir):
 
     # Trigger nasty bug (fixed in !486):
     si.calc.wfs.pt.blocksize = si.calc.wfs.pd.maxmyng - 1
-
+    
+    # compute error in stress as numerical - analytical
     s_analytical = si.get_stress()
     s_numerical = si.calc.calculate_numerical_stress(si, 1e-5)
     s_err = s_numerical - s_analytical
 
-    print('Analytical stress:\n', s_analytical)
-    print('Numerical stress:\n', s_numerical)
-    print('Error in stress:\n', s_err)
     assert np.all(abs(s_err) < 1e-4)
+
+
+def test_pw_si_force(in_tmp_dir):
+    xc = 'PBE'
+    si = bulk('Si')
+    k = 1
+    si.calc = GPAW(mode=PW(200),
+                   mixer=Mixer(0.7, 5, 50.0),
+                   xc=xc,
+                   kpts=(k, k, k),
+                   convergence={'energy': 1e-8},
+                   parallel={'domain': min(2, world.size)},
+                   symmetry='off',  # needed for set_positions
+                   txt='si.txt')
+
+    si.get_potential_energy()
+
+    # compute error in force as numerical-analytical
+    f_analytical = si.get_forces()
+    f_numerical = numeric_force(si, 1, 0, 0.001)
+
+    assert abs(f_analytical[1, 0] - f_numerical) < 0.0005
