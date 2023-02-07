1806ed15fc9cff507a80fc9b2a762f9da49c7f98
Julian <juhe@debian-BULLSEYE-live-builder-AMD64>
2023-02-07 15:26:12 +0100

    hardcoding the force check of the HSE calculation instead of comparing with numerical calculation to save calculation time

--- gpaw/test/exx/test_double_cell.py
+++ gpaw/test/exx/test_double_cell.py
@@ -23,6 +23,13 @@ def test_exx_double_cell(in_tmp_dir):
     e1 = a.get_potential_energy()
     eps1 = a.calc.get_eigenvalues(1)[0]
     f1 = a.get_forces()
+    assert abs(f1[1, 0] - 9.60644) < 0.0005
+    if 0:
+        # To check against numeric calculation of the forces, but it takes
+        # more time
+        from ase.calculators.test import numeric_force
+        f1n = numeric_force(a, 1, 0, 0.001)
+        assert abs(f1[1, 0] - f1n) < 0.0005
 
     a *= (1, 1, 2)
     a.calc = GPAW(
