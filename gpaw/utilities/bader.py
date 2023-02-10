4391fbabc6ceb5130f91718d00704ff5ff8dbb82
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-02-10 11:50:57 +0100

    ...

--- gpaw/utilities/bader.py
+++ gpaw/utilities/bader.py
@@ -25,13 +25,13 @@ if __name__ == '__main__':
     calc = GPAW(sys.argv[1])
     dens = calc.calculation.densities()
     n_sR = dens.all_electron_densities(grid_spacing=0.05)
-    ne = n_sR.integrate().sum()
-    print(ne, 'electrons')
     # NOTE: Ignoring ASE's hint for **kwargs in write() because it is wrong:
     write('density.cube',
           calc.atoms,
           data=n_sR.data.sum(axis=0) * Bohr**3)  # type: ignore
     subprocess.run('bader -p all_atom density.cube'.split())
+    ne = n_sR.integrate().sum()
+    print(ne, 'electrons')
     charges = calc.atoms.numbers - read_bader_charges()
     for symbol, charge in zip(calc.atoms.symbols, charges):
         print(f'{symbol:2} {charge:10.6f} |e|')
