bfa088b26e5bc40d978055c22bc0829d9d7a58f5
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-02-13 16:21:27 +0100

    split wavefunctions reuse test up in smaller bits and test explicitly that the initial eigenstates tolerance after we begin the second scf step is better than if we had started from scratch

--- gpaw/test/test_reuse_wfs.py
+++ gpaw/test/test_reuse_wfs.py
@@ -2,39 +2,86 @@ import pytest
 from ase.build import bulk
 
 from gpaw import GPAW, PW, Mixer
+from gpaw.convergence_criteria import Eigenstates
 
 
-@pytest.mark.later
-def test_reuse_wfs():
-    atoms0 = bulk('Si')
-    atoms0.rattle(stdev=0.01, seed=17)  # Break symmetry
+class MyConvergenceCriterion(Eigenstates):
+    def __init__(self, tol):
+        super().__init__(tol)
+        self.history = []
 
-    results = []
+    def get_error(self, context):
+        value = super().get_error(context)
+        self.history.append(value)
+        return value
 
-    for mode in ['fd', 'pw']:
-        for method in [None, 'paw', 'lcao']:
-            atoms = atoms0.copy()
 
-            kwargs = {}
-            if mode == 'pw':
-                if method == 'lcao':
-                    continue  # Not implemented yet
-                kwargs.update(mode=PW(400.0))
+#@pytest.mark.later
+#@pytest.mark.parametrize('mode', ['fd', 'pw'])
+#@pytest.mark.parametrize('method', [None, 'paw'])
+#@pytest.mark.parametrize('mode', '
+def run(mode, method, setups):
+    atoms = bulk('Si')
+    atoms.rattle(stdev=0.01, seed=17)  # Break symmetry
 
-            calc = GPAW(mixer=Mixer(0.4, 5, 20.0),
-                        basis='dzp' if method == 'lcao' else {},
-                        experimental={'reuse_wfs_method': method},
-                        xc='oldLDA',
-                        kpts=[2, 2, 2],
-                        **kwargs)
-            atoms.calc = calc
-            E1 = atoms.get_potential_energy()
-            niter1 = calc.scf.niter
-            atoms.rattle(stdev=0.05)
-            E2 = atoms.get_potential_energy()
-            niter2 = calc.scf.niter
-            results.append([mode, method, E1, E2, niter1, niter2])
+    #for mode in ['fd', 'pw']:
+    #    for method in [None, 'paw', 'lcao']:
+            # atoms = atoms0.copy()
+    #wfs_convergence = []
 
-    if calc.wfs.world.rank == 0:
-        for result in results:
-            print(' '.join(str(x) for x in result))
+    kwargs = {}
+    if mode == 'pw':
+        kwargs.update(mode=PW(200.0))
+
+    conv_tol = 1e-9
+    conv = MyConvergenceCriterion(conv_tol)
+
+    calc = GPAW(mixer=Mixer(0.4, 5, 20.0),
+                txt=None,
+                setups=setups,
+                convergence={'custom': [conv]},
+                experimental={'reuse_wfs_method': method},
+                xc='PBE',
+                kpts=[2, 2, 2],
+                **kwargs)
+
+
+    atoms.calc = calc
+    E1 = atoms.get_potential_energy()
+    assert conv.history[-1] < conv_tol
+    niter1 = len(conv.history)
+    del conv.history[:]
+
+    atoms.rattle(stdev=0.001)
+
+    E2 = atoms.get_potential_energy()
+    niter2 = len(conv.history)
+    reuse_error = conv.history[0]
+
+    # If the energy is exactly or suspiciously close to zero, it's because
+    # nothing was done at all (something was cached but shouldn't have been)
+    delta_e = abs(E2 - E1)
+    assert delta_e > 1e-6, delta_e
+    return niter1, niter2, reuse_error
+
+
+@pytest.mark.parametrize('mode, reuse_type, setups, max_reuse_error', [
+    ('pw', 'paw', 'paw', 1e-5),
+    ('pw', None, 'paw', 1e-4),
+    ('fd', 'paw', 'paw', 1e-4),
+    ('fd', None, 'paw', 1e-3),
+    # ('pw', 'paw', 'sg15', 0)
+])
+def test_reuse_wfs(mode, reuse_type, setups, max_reuse_error):
+    """Check that wavefunctions are meaningfully reused.
+
+    For a different modes and parameters, this test asserts that the
+    initial wavefunction error in the second scf step is below a
+    certain threshold, indicating that we are doing better than if
+    we started from scratch."""
+
+    niter_first, niter_second, reuse_error = run(mode, reuse_type, setups)
+
+    # It should at the very least be faster to do the second step:
+    assert niter_second < niter_first
+    assert reuse_error < max_reuse_error
