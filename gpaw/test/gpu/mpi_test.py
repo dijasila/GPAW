2ead33998a758c3cd592593de5d08916e6b75df0
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-02-22 10:20:04 +0100

    Try array.ctypes.data

--- gpaw/test/gpu/mpi_test.py
+++ gpaw/test/gpu/mpi_test.py
@@ -4,5 +4,6 @@ from gpaw.mpi import world
 
 def test_mpi():
     a = cp.ones(1)
+    print(a, world, a.shape, a.nbytes, a.size, a.dtype, a.data.ptr)
     world.sum(a)
     assert a[0].get() == world.size
