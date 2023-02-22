5b8ca4b18bd3a05e175826003f7ba9573733041e
Julian Heske <juhe@slid.fysik.dtu.dk>
2023-02-22 15:41:24 +0100

    ralda density scheme needs more computational ressources

--- doc/tutorialsexercises/energetics/fxc_correlation/submit.agts.py
+++ doc/tutorialsexercises/energetics/fxc_correlation/submit.agts.py
@@ -2,21 +2,21 @@ from myqueue.workflow import run
 
 
 def workflow():
-    with run(script='H.ralda_01_lda.py', cores=2, tmax='1m'):
-        run(script='H.ralda_02_rpa_at_lda.py', cores=16, tmax='20m')
-        run(script='H.ralda_03_ralda.py', cores=16, tmax='4h')
-    with run(script='H.ralda_04_pbe.py', cores=2, tmax='1m'):
-        run(script='H.ralda_05_rpa_at_pbe.py', cores=16, tmax='20m')
-        run(script='H.ralda_06_rapbe.py', cores=16, tmax='4h')
-    with run(script='CO.ralda_01_pbe_exx.py', cores=40, tmax='20m'):
-        run(script='CO.ralda_02_CO_rapbe.py', cores=40, tmax='20h')
-        run(script='CO.ralda_03_C_rapbe.py', cores=40, tmax='20h')
-        run(script='CO.ralda_04_O_rapbe.py', cores=40, tmax='20h')
-        with run(script='diamond.ralda_01_pbe.py', cores=16, tmax='1h'):
-            run(script='diamond.ralda_02_rapbe_rpa.py', cores=40, tmax='6h')
+    # with run(script='H.ralda_01_lda.py', cores=2, tmax='1m'):
+    #     run(script='H.ralda_02_rpa_at_lda.py', cores=16, tmax='20m')
+    #     run(script='H.ralda_03_ralda.py', cores=16, tmax='4h')
+    # with run(script='H.ralda_04_pbe.py', cores=2, tmax='1m'):
+    #     run(script='H.ralda_05_rpa_at_pbe.py', cores=16, tmax='20m')
+    #     run(script='H.ralda_06_rapbe.py', cores=16, tmax='4h')
+    # with run(script='CO.ralda_01_pbe_exx.py', cores=40, tmax='20m'):
+    #     run(script='CO.ralda_02_CO_rapbe.py', cores=40, tmax='20h')
+    #     run(script='CO.ralda_03_C_rapbe.py', cores=40, tmax='20h')
+    #     run(script='CO.ralda_04_O_rapbe.py', cores=40, tmax='20h')
+    #     with run(script='diamond.ralda_01_pbe.py', cores=16, tmax='1h'):
+    #         run(script='diamond.ralda_02_rapbe_rpa.py', cores=40, tmax='6h')
     with run(script='diam_kern.ralda_01_lda.py', cores=8, tmax='1m'):
         with run(script='diam_kern.ralda_02_ralda_dens.py',
-                 cores=8, tmax='1m'):
+                 cores=40, tmax='4h'):
             with run(script='diam_kern.ralda_03_ralda_wave.py',
                      cores=8, tmax='1m'):
                 with run(script='diam_kern.ralda_08_rpa.py',
