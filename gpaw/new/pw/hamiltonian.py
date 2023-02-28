ceda34bf89aac5300827b5676cde389917df369c
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-02-28 16:53:27 +0100

    ...

--- gpaw/new/pw/hamiltonian.py
+++ gpaw/new/pw/hamiltonian.py
@@ -10,12 +10,12 @@ class PWHamiltonian(Hamiltonian):
     def apply(self,
               vt_sR: UniformGridFunctions,
               psit_nG: PlaneWaveExpansions,
-              out_nG: PlaneWaveExpansions,
+              out: PlaneWaveExpansions,
               spin: int):
+        out_nG = out
         vt_R = vt_sR[spin].gather(broadcast=True)
         xp = psit_nG.xp
         e_kin_G = xp.asarray(psit_nG.desc.ekin_G)
-        xp.multiply(, psit_nG.data, out_nG.data)
         grid = vt_R.desc.new(comm=None, dtype=psit_nG.desc.dtype)
         tmp_R = grid.empty(xp=xp)
         psit_G = psit_nG.desc.new(comm=None).empty()
@@ -33,34 +33,6 @@ class PWHamiltonian(Hamiltonian):
 
     def create_preconditioner(self, blocksize):
         return precondition
-
-
-# Old optimized code:
-"""
-        N = len(psit_xG)
-        S = self.gd.comm.size
-
-        vt_R = self.gd.collect(ham.vt_sG[kpt.s], broadcast=True)
-        Q_G = self.pd.Q_qG[kpt.q]
-        T_G = 0.5 * self.pd.G2_qG[kpt.q]
-
-        for n1 in range(0, N, S):
-            n2 = min(n1 + S, N)
-            psit_G = self.pd.alltoall1(psit_xG[n1:n2], kpt.q)
-            with self.timer('HMM T'):
-                np.multiply(T_G, psit_xG[n1:n2], Htpsit_xG[n1:n2])
-            if psit_G is not None:
-                psit_R = self.pd.ifft(psit_G, kpt.q, local=True, safe=False)
-                psit_R *= vt_R
-                self.pd.fftplan.execute()
-                vtpsit_G = self.pd.tmp_Q.ravel()[Q_G]
-            else:
-                vtpsit_G = self.pd.tmp_G
-            self.pd.alltoall2(vtpsit_G, kpt.q, Htpsit_xG[n1:n2])
-
-        ham.xc.apply_orbital_dependent_hamiltonian(
-            kpt, psit_xG, Htpsit_xG, ham.dH_asp)
-"""
 
 
 def precondition(psit_nG, residual_nG, out):
