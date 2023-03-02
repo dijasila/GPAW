6e116ee954a5fa724f318cf67174cf00d2eab3db
Julian Heske <juhe@slid.fysik.dtu.dk>
2023-03-02 11:50:05 +0100

    adding the hf + c values to the check for diamond and CO

--- doc/tutorialsexercises/energetics/fxc_correlation/CO.ralda_05_extrapolate.py
+++ doc/tutorialsexercises/energetics/fxc_correlation/CO.ralda_05_extrapolate.py
@@ -14,6 +14,7 @@ a = CO_rpa
 a[:, 1] -= (C_rpa[:, 1] + O_rpa[:, 1])
 ext, A, B, sigma = extrapolate(a[:, 0], a[:, 1], reg=3, plot=False)
 equal(A, -3.27, 0.01)
+equal(E_hf + A, -10.64, 0.01)
 
 CO_rapbe = np.loadtxt('CO.ralda_rapbe_CO.dat')
 C_rapbe = np.loadtxt('CO.ralda_rapbe_C.dat')
@@ -23,3 +24,4 @@ a = CO_rapbe
 a[:, 1] -= (C_rapbe[:, 1] + O_rapbe[:, 1])
 ext, A, B, sigma = extrapolate(a[:, 0], a[:, 1], reg=3, plot=False)
 equal(A, -3.51, 0.01)
+equal(E_hf + A, -10.88, 0.01)
--- doc/tutorialsexercises/energetics/fxc_correlation/diamond.ralda_03_extrapolate.py
+++ doc/tutorialsexercises/energetics/fxc_correlation/diamond.ralda_03_extrapolate.py
@@ -11,9 +11,11 @@ b = np.loadtxt('CO.ralda_rpa_C.dat')
 ext, A, B, sigma = extrapolate(a[:, 0], a[:, 1] / 2 - b[:, 1],
                                reg=3, plot=False)
 equal(A, -1.89, 0.01)
+equal(E_hf + A, -7.06, 0.01)
 
 a = np.loadtxt('diamond.ralda.rapbe.dat')
 b = np.loadtxt('CO.ralda_rapbe_C.dat')
 ext, A, B, sigma = extrapolate(a[:, 0], a[:, 1] / 2 - b[:, 1],
                                reg=3, plot=False)
 equal(A, -1.48, 0.01)
+equal(E_hf + A, -6.65, 0.01)
