4eb6c865aecbdde1eff893e684d129f4d117ac74
Yorick Schmerwitz <yla1@hi.is>
2023-03-02 15:21:57 +0000

    Git nonsense

---
+++ gpaw/test/directopt/test_hess 2.py
@@ -0,0 +1,93 @@
+import pytest
+
+from ase import Atoms
+from gpaw import GPAW, LCAO
+from gpaw.directmin.derivatives import Derivatives
+import numpy as np
+
+
+def test_directmin_lcao_numerical_hessian(in_tmp_dir):
+    """
+    Test complex numerical Hessian
+    w.r.t rotation parameters in LCAO
+
+    :param in_tmp_dir:
+    :return:
+    """
+
+    calc = GPAW(xc='PBE',
+                mode=LCAO(force_complex_dtype=True),
+                h=0.25,
+                basis='dz(dzp)',
+                spinpol=False,
+                eigensolver={'name': 'etdm',
+                             'representation': 'u-invar'},
+                occupations={'name': 'fixed-uniform'},
+                mixer={'backend': 'no-mixing'},
+                nbands='nao',
+                symmetry='off',
+                )
+
+    atoms = Atoms('H', positions=[[0, 0, 0]])
+    atoms.center(vacuum=5.0)
+    atoms.set_pbc(False)
+    atoms.calc = calc
+    atoms.get_potential_energy()
+
+    numder = Derivatives(calc.wfs.eigensolver, calc.wfs)
+
+    hess_n = numder.get_numerical_derivatives(
+        calc.wfs.eigensolver,
+        calc.hamiltonian,
+        calc.wfs,
+        calc.density,
+        what2calc='hessian'
+    )
+    hess_a = numder.get_analytical_derivatives(
+        calc.wfs.eigensolver,
+        calc.hamiltonian,
+        calc.wfs,
+        calc.density,
+        what2calc='hessian'
+    )
+    hess_nt = np.asarray([[1.32720630e+00, -1.93947467e-11],
+                         [3.95786680e-09, 1.14599176e+00]])
+    assert hess_n == pytest.approx(hess_nt, abs=1e-4)
+    assert hess_a == pytest.approx(hess_nt, abs=0.2)
+
+    a_mat_u = {0: [np.sqrt(2) * np.pi / 4.0 + 1.0j * np.sqrt(2) * np.pi / 4.0]}
+    c_nm_ref = calc.wfs.eigensolver.dm_helper.reference_orbitals
+    calc.wfs.eigensolver.rotate_wavefunctions(calc.wfs,
+                                              a_mat_u,
+                                              {0: calc.wfs.bd.nbands},
+                                              c_nm_ref
+                                              )
+    calc.wfs.eigensolver.update_ks_energy(calc.hamiltonian,
+                                          calc.wfs, calc.density)
+    calc.wfs.eigensolver.get_canonical_representation(
+        calc.hamiltonian, calc.wfs, calc.density, sort_eigenvalues=True)
+    c_nm = {x: calc.wfs.kpt_u[x].C_nM.copy()
+            for x in range(len(calc.wfs.kpt_u))}
+
+    numder = Derivatives(calc.wfs.eigensolver, calc.wfs,
+                         c_ref=c_nm)
+
+    hess_n = numder.get_numerical_derivatives(
+        calc.wfs.eigensolver,
+        calc.hamiltonian,
+        calc.wfs,
+        calc.density,
+        what2calc='hessian'
+    )
+
+    hess_a = numder.get_analytical_derivatives(
+        calc.wfs.eigensolver,
+        calc.hamiltonian,
+        calc.wfs,
+        calc.density,
+        what2calc='hessian'
+    )
+    hess_nt = np.asarray([[-1.08209601e+00, -1.11022302e-09],
+                         [8.50014503e-10, -9.37664521e-01]])
+    assert hess_n == pytest.approx(hess_nt, abs=1e-4)
+    assert hess_a == pytest.approx(hess_nt, abs=0.2)
---
+++ gpaw/test/directopt/test_mom_directopt_lcao 2.py
@@ -0,0 +1,60 @@
+import pytest
+
+from gpaw import GPAW, LCAO
+from gpaw.directmin.tools import excite
+from gpaw.mom import prepare_mom_calculation
+from gpaw.directmin.etdm import ETDM
+
+from ase import Atoms
+import numpy as np
+
+
+def test_mom_directopt_lcao(in_tmp_dir):
+    # Water molecule:
+    d = 0.9575
+    t = np.pi / 180 * 104.51
+    H2O = Atoms('OH2',
+                positions=[(0, 0, 0),
+                           (d, 0, 0),
+                           (d * np.cos(t), d * np.sin(t), 0)])
+    H2O.center(vacuum=4.0)
+
+    calc = GPAW(mode=LCAO(),
+                basis='dzp',
+                h=0.22,
+                occupations={'name': 'fixed-uniform'},
+                eigensolver='etdm',
+                mixer={'backend': 'no-mixing'},
+                nbands='nao',
+                symmetry='off',
+                spinpol=True,
+                convergence={'density': 1.0e-4,
+                             'eigenstates': 4.0e-8})
+    H2O.calc = calc
+    H2O.get_potential_energy()
+
+    calc.set(eigensolver=ETDM(searchdir_algo={'name': 'l-sr1p'},
+                              linesearch_algo={'name': 'max-step'},
+                              need_init_orbs=False))
+    # Ground-state occupation numbers
+    f_sn = excite(calc, 0, 0, spin=(0, 0))
+    prepare_mom_calculation(calc, H2O, f_sn)
+
+    def rotate_homo_lumo(calc=calc):
+        a = 70 / 180 * np.pi
+        iters = calc.get_number_of_iterations()
+        if iters == 3:
+            c = calc.wfs.kpt_u[0].C_nM.copy()
+            calc.wfs.kpt_u[0].C_nM[3] = np.cos(a) * c[3] + np.sin(a) * c[4]
+            calc.wfs.kpt_u[0].C_nM[4] = np.cos(a) * c[4] - np.sin(a) * c[3]
+            counter = calc.wfs.eigensolver.update_ref_orbs_counter
+            calc.wfs.eigensolver.update_ref_orbs_counter = iters + 1
+            calc.wfs.eigensolver.update_ref_orbitals(calc.wfs,
+                                                     calc.hamiltonian,
+                                                     calc.density)
+            calc.wfs.eigensolver.update_ref_orbs_counter = counter
+
+    calc.attach(rotate_homo_lumo, 1)
+    e = H2O.get_potential_energy()
+
+    assert e == pytest.approx(-4.854496813259008, abs=1.0e-4)
