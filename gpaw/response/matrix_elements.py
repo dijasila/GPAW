b7746cad08f12e571c7e3417916bb89082e974e7
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-03-13 15:03:00 +0100

    make sure not to overwrite the input projections until the very end

--- gpaw/response/matrix_elements.py
+++ gpaw/response/matrix_elements.py
@@ -188,7 +188,7 @@ class NewPairDensityCalculator:
 
         ut_symmetrizer = T
         Ph_symmetrizer = partial(symmetrize_projections,
-                                 a_a=a_a, U_aii=U_aii,
+                                 a1_a2=a_a, U_aii=U_aii,
                                  time_reversal=time_reversal)
 
         return ut_symmetrizer, Ph_symmetrizer, shift_c
@@ -198,19 +198,22 @@ class NewPairDensityCalculator:
         return fft_indices(self.gs.kd, K1, K2, qpd, dshift_c)
 
 
-def symmetrize_projections(Ph, a_a, U_aii, time_reversal):
-    """Symmetrize the PAW projections."""
-    P_ahi = []
-    for a1, U_ii in zip(a_a, U_aii):
-        P_hi = np.ascontiguousarray(Ph[a1])
-        # Apply symmetry operations. This will map a1 onto a2
+def symmetrize_projections(Ph, a1_a2, U_aii, time_reversal):
+    """Symmetrize the PAW projections.
+
+    NB: The projections of atom a1 are mapped onto a *different* atom a2
+    according to the input map of atomic indices a1_a2."""
+    # First, we apply the symmetry operations to the projections one at a time
+    P_a2hi = []
+    for a1, U_ii in zip(a1_a2, U_aii):
+        P_hi = Ph[a1].copy(order='C')
         np.dot(P_hi, U_ii, out=P_hi)
         if time_reversal:
             np.conj(P_hi, out=P_hi)
-        P_ahi.append(P_hi)
+        P_a2hi.append(P_hi)
 
-    # Store symmetrized projectors
-    for a2, P_hi in enumerate(P_ahi):
+    # Then, we store the symmetry mapped projectors in the projections object
+    for a2, P_hi in enumerate(P_a2hi):
         I1, I2 = Ph.map[a2]
         Ph.array[..., I1:I2] = P_hi
 
