3406d5d25379a3df9219c79a66564d4f717bc23c
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-03-14 11:29:28 +0100

    test that the output logging works as desired

--- gpaw/test/test_new_calculator.py
+++ gpaw/test/test_new_calculator.py
@@ -1,7 +1,10 @@
+# General modules
+import sys
+from pathlib import Path
 import pytest
 
+# Script modules
 from ase.build import bulk
-
 from gpaw import GPAW, PW
 
 
@@ -32,15 +35,40 @@ def test_new_calculator(in_tmp_dir):
 
     atoms = bulk('Si')
 
-    calc = GPAW(**params)
+    calc = GPAW(**params, txt='calc0.txt')
     atoms.calc = calc
 
-    for modification in modification_m:
-        atoms.calc = calc.new(**modification)
+    for m, modification in enumerate(modification_m):
+        if m == 0:
+            # Don't give a new txt file
+            atoms.calc = calc.new(**modification)
+            check_file_handles(calc, atoms)
+        else:
+            txt = f'calc{m}.txt'
+            atoms.calc = calc.new(**modification, txt=txt)
+            check_file_handles(calc, atoms, txt=txt)
+
         check_calc(atoms, params, modification, world=calc.world)
 
 
 # ---------- Test functionality ---------- #
+
+
+def check_file_handles(calc, atoms, txt=None):
+    assert calc.log.world.rank == atoms.calc.log.world.rank
+
+    if atoms.calc.log.world.rank == 0:
+        # We never want to reuse the output file
+        assert atoms.calc.log._fd is not calc.log._fd
+
+        if txt is None:
+            # When no txt is specified, the new calculator should log its
+            # output in stdout
+            assert atoms.calc.log._fd is sys.stdout
+        else:
+            # Check that the new calculator log file handle was updated
+            # appropriately
+            assert Path(atoms.calc.log._fd.name).name == txt
 
 
 def check_calc(atoms, params, modification, *, world):
