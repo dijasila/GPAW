65ce7fa40d4f06b8226b6cc45a4586c17641168a
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-03-14 10:49:30 +0100

    add the basic test layout

---
+++ gpaw/test/test_new_calculator.py
@@ -0,0 +1,40 @@
+from ase.build import bulk
+
+from gpaw import GPAW, PW
+
+
+# ---------- Actual tests ---------- #
+
+
+def test_new_calculator(in_tmp_dir):
+    """Test the GPAW.new() method."""
+
+    # ---------- Inputs ---------- #
+
+    params = dict(
+        mode=PW(200),
+        xc='LDA',
+        nbands=8,
+        kpts={'size': (4, 4, 4), 'gamma': True})
+
+    modification_m = [
+        dict(xc='PBE'),
+    ]
+
+    # ---------- Script ---------- #
+
+    atoms = bulk('Si')
+
+    calc = GPAW(**params)
+    atoms.calc = calc
+
+    for modification in modification_m:
+        atoms.calc = calc.new(**modification)
+        check_calc(atoms)
+
+
+# ---------- Test functionality ---------- #
+
+
+def check_calc(atoms):
+    pass
