c43884cf4f6602ce73c0d87241739cd556fad755
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-03-16 11:33:18 +0100

    initialize the kptpair extractors

--- gpaw/test/response/test_parallel_kptpair_extraction.py
+++ gpaw/test/response/test_parallel_kptpair_extraction.py
@@ -3,6 +3,9 @@ import pytest
 from gpaw import GPAW
 from gpaw.mpi import world
 from gpaw.response import ResponseContext, ResponseGroundStateAdapter
+from gpaw.response.pw_parallelization import block_partition
+from gpaw.response.kspair import KohnShamKPointPairExtractor
+
 from gpaw.test.response.test_chiks import generate_system_s
 
 pytestmark = pytest.mark.skipif(world.size == 1, reason='world.size == 1')
@@ -20,7 +23,7 @@ def test_parallel_extract_kptdata(in_tmp_dir, gpw_files, system):
     # ---------- Inputs ---------- #
 
     wfs, spincomponent, _, _, _ = system
-    # nblocks XXX
+    nblocks = 1  # Introduce parametrization XXX
 
     # ---------- Script ---------- #
 
@@ -32,3 +35,14 @@ def test_parallel_extract_kptdata(in_tmp_dir, gpw_files, system):
     calc = GPAW(gpw_files[wfs], parallel=dict(domain=1))
     nbands = calc.parameters.convergence['bands']
     parallel_gs = ResponseGroundStateAdapter(calc)
+
+    # Initialize kptpair extractors
+    transitions_blockcomm, kpts_blockcomm = block_partition(context.comm, nblocks)
+    serial_kptpair_extractor = KohnShamKPointPairExtractor(
+        serial_gs, context,
+        transitions_blockcomm=transitions_blockcomm,
+        kpts_blockcomm=kpts_blockcomm)
+    parallel_kptpair_extractor = KohnShamKPointPairExtractor(
+        serial_gs, context,
+        transitions_blockcomm=transitions_blockcomm,
+        kpts_blockcomm=kpts_blockcomm)
