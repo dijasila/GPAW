e1fff8041577a89115adeee76e415a04250cb81d
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-03-16 10:03:27 +0100

    parametrize the test of chiks systems

--- gpaw/test/response/test_parallel_kptpair_extraction.py
+++ gpaw/test/response/test_parallel_kptpair_extraction.py
@@ -1,9 +1,15 @@
 import pytest
 
 from gpaw.mpi import world
+from gpaw.test.response.test_chiks import generate_system_s
 
 pytestmark = pytest.mark.skipif(world.size == 1, reason='world.size == 1')
 
 
-def test_parallel_extract_kptdata():
+# ---------- Actual tests ---------- #
+
+
+@pytest.mark.response
+@pytest.mark.parametrize('system', generate_system_s())
+def test_parallel_extract_kptdata(in_tmp_dir, gpw_files, system):
     pass
