41ac3abe83b5e0f015a65ed977fc75b2dde8ec10
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-03-16 10:14:05 +0100

    initialize serial and parallel ground state adapters

--- gpaw/test/response/test_parallel_kptpair_extraction.py
+++ gpaw/test/response/test_parallel_kptpair_extraction.py
@@ -1,6 +1,8 @@
 import pytest
 
+from gpaw import GPAW
 from gpaw.mpi import world
+from gpaw.response import ResponseContext, ResponseGroundStateAdapter
 from gpaw.test.response.test_chiks import generate_system_s
 
 pytestmark = pytest.mark.skipif(world.size == 1, reason='world.size == 1')
@@ -12,4 +14,21 @@ pytestmark = pytest.mark.skipif(world.size == 1, reason='world.size == 1')
 @pytest.mark.response
 @pytest.mark.parametrize('system', generate_system_s())
 def test_parallel_extract_kptdata(in_tmp_dir, gpw_files, system):
-    pass
+    """Test that the KohnShamKPointPair data extracted from a serial and a
+    parallel calculator object is identical."""
+
+    # ---------- Inputs ---------- #
+
+    wfs, spincomponent, _, _, _ = system
+    # nblocks XXX
+
+    # ---------- Script ---------- #
+
+    # Initialize serial ground state adapter
+    context = ResponseContext()
+    serial_gs = ResponseGroundStateAdapter.from_gpw_file(gpw_files[wfs], context)
+    
+    # Initialize parallel ground state adapter
+    calc = GPAW(gpw_files[wfs], parallel=dict(domain=1))
+    nbands = calc.parameters.convergence['bands']
+    parallel_gs = ResponseGroundStateAdapter(calc)
