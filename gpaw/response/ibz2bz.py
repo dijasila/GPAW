db410f1999e7d15a80b5233237115fcb2e0bd0fc
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-03-17 10:56:53 +0100

    generate projections rotations ahead of time

--- gpaw/response/ibz2bz.py
+++ gpaw/response/ibz2bz.py
@@ -8,62 +8,72 @@ class IBZ2BZMaps(Sequence):
 
     The sequence is indexed by the BZ index K."""
 
-    def __init__(self, kd, R_asii, spos_ac):
+    def __init__(self, kd, spos_ac, R_asii):
         """Construct the IBZ2BZMapper.
 
         Parameters
         ----------
         kd : KPointDescriptor
-        R_asii : list
-            Atomic rotations under the symmetry transformations
         spos_ac : np.array
             Scaled atomic positions
+        R_asii : list
+            Atomic rotations under the symmetry transformations
         """
         self.kd = kd
-        self.spos_ac = spos_ac
-        self.R_asii = R_asii
+        self.U_kaii = self.generate_projections_rotation_matrices(
+            spos_ac, R_asii)
 
     @classmethod
     def from_calculator(cls, calc):
         R_asii = [setup.R_sii for setup in calc.setups]
-        return cls(calc.wfs.kd, R_asii, calc.spos_ac)
+        return cls(calc.wfs.kd, calc.spos_ac, R_asii)
+
+    def generate_projections_rotation_matrices(self, spos_ac, R_asii):
+        """Rotation matrices for the PAW projections of every IBZ -> K map."""
+        U_kaii = []
+        for K in range(len(self)):
+            s = self.kd.sym_k[K]
+            ik_c = self.get_ik_c(K)
+            U_cc = self.get_rotation_matrix(K)
+            b_a = self.get_atomic_permutations(K)
+            U_aii = []
+            for a, R_sii in enumerate(R_asii):
+                b = b_a[a]
+                S_c = np.dot(spos_ac[a], U_cc) - spos_ac[b]
+                x = np.exp(2j * np.pi * np.dot(ik_c, S_c))
+                U_ii = R_sii[s].T * x
+                U_aii.append(U_ii)
+            U_kaii.append(U_aii)
+
+        return U_kaii
 
     def __len__(self):
         return len(self.kd.bzk_kc)
 
     def __getitem__(self, K):
         return IBZ2BZMap(self.get_ik_c(K),
-                         *self.get_unitary_symmetry_transformations(K),
-                         self.get_time_reversal(K),
-                         self.spos_ac)
+                         self.get_rotation_matrix(K),
+                         self.get_atomic_permutations(K),
+                         self.U_kaii[K],
+                         self.get_time_reversal(K))
 
     def get_ik_c(self, K):
         ik = self.kd.bz2ibz_k[K]
         ik_c = self.kd.ibzk_kc[ik]
         return ik_c
 
-    def get_unitary_symmetry_transformations(self, K):
-        s = self.kd.sym_k[K]
-        U_cc = self.get_rotation_matrix(s)
-        b_a = self.get_atomic_permutations(s)
-        R_aii = self.get_atomic_rotations(s)
-        return U_cc, b_a, R_aii
-
-    def get_rotation_matrix(self, s):
+    def get_rotation_matrix(self, K):
         """Coordinate rotation matrix, mapping IBZ -> K."""
+        s = self.kd.sym_k[K]
         U_cc = self.kd.symmetry.op_scc[s]
         return U_cc
 
-    def get_atomic_permutations(self, s):
+    def get_atomic_permutations(self, K):
         """Permutations of atomic indices in the IBZ -> K map."""
+        s = self.kd.sym_k[K]
         # Atom a is mapped onto atom b.
         b_a = self.kd.symmetry.a_sa[s]
         return b_a
-
-    def get_atomic_rotations(self, s):
-        """Rotations of the individual atoms in the IBZ -> K map."""
-        R_aii = [R_sii[s] for R_sii in self.R_asii]
-        return R_aii
 
     def get_time_reversal(self, K):
         """Does the mapping IBZ -> K involve time reversal?"""
@@ -76,30 +86,14 @@ class IBZ2BZMap:
     Some documentation here! XXX
     """
 
-    def __init__(self, ik_c, U_cc, b_a, R_aii, time_reversal, spos_ac):
+    def __init__(self, ik_c, U_cc, b_a, U_aii, time_reversal):
         """Construct the IBZ2BZMap."""
         self.ik_c = ik_c
 
         self.U_cc = U_cc
         self.b_a = b_a
-        self.R_aii = R_aii
+        self.U_aii = U_aii
         self.time_reversal = time_reversal
-
-        self.spos_ac = spos_ac
-
-    @property
-    def U_aii(self):
-        """Atomic rotation matrices of the PAW projections in the IBZ -> K map.
-        """
-        U_aii = []
-        for a, R_ii in enumerate(self.R_aii):
-            b = self.b_a[a]
-            S_c = np.dot(self.spos_ac[a], self.U_cc) - self.spos_ac[b]
-            x = np.exp(2j * np.pi * np.dot(self.ik_c, S_c))
-            U_ii = R_ii.T * x
-            U_aii.append(U_ii)
-
-        return U_aii
 
     def map_kpoint(self):
         """Get the relative k-point coordinates after the IBZ -> K mapping.
