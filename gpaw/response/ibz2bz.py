dcc740bf5a5781d506ebb1d63ec9a40d50f08af6
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-03-17 11:24:40 +0100

    create a cache for the projections rotation matrices instead of generating one for every K

--- gpaw/response/ibz2bz.py
+++ gpaw/response/ibz2bz.py
@@ -1,4 +1,5 @@
 from collections.abc import Sequence
+from functools import lru_cache
 
 import numpy as np
 
@@ -17,44 +18,26 @@ class IBZ2BZMaps(Sequence):
         spos_ac : np.array
             Scaled atomic positions
         R_asii : list
-            Atomic rotations under the symmetry transformations
+            Rotations of the PAW projections under symmetry transformations
         """
         self.kd = kd
-        self.U_kaii = self.generate_projections_rotation_matrices(
-            spos_ac, R_asii)
+        self.spos_ac = spos_ac
+        self.R_asii = R_asii
 
     @classmethod
     def from_calculator(cls, calc):
         R_asii = [setup.R_sii for setup in calc.setups]
         return cls(calc.wfs.kd, calc.spos_ac, R_asii)
 
-    def generate_projections_rotation_matrices(self, spos_ac, R_asii):
-        """Rotation matrices for the PAW projections of every IBZ -> K map."""
-        U_kaii = []
-        for K in range(len(self)):
-            s = self.kd.sym_k[K]
-            ik_c = self.get_ik_c(K)
-            U_cc = self.get_rotation_matrix(K)
-            b_a = self.get_atomic_permutations(K)
-            U_aii = []
-            for a, R_sii in enumerate(R_asii):
-                b = b_a[a]
-                S_c = np.dot(spos_ac[a], U_cc) - spos_ac[b]
-                x = np.exp(2j * np.pi * np.dot(ik_c, S_c))
-                U_ii = R_sii[s].T * x
-                U_aii.append(U_ii)
-            U_kaii.append(U_aii)
-
-        return U_kaii
-
     def __len__(self):
         return len(self.kd.bzk_kc)
 
     def __getitem__(self, K):
+        s = self.kd.sym_k[K]
         return IBZ2BZMap(self.get_ik_c(K),
-                         self.get_rotation_matrix(K),
-                         self.get_atomic_permutations(K),
-                         self.U_kaii[K],
+                         self.get_rotation_matrix(s),
+                         self.get_atomic_permutations(s),
+                         self.get_projections_rotation_matrices(K),
                          self.get_time_reversal(K))
 
     def get_ik_c(self, K):
@@ -62,18 +45,46 @@ class IBZ2BZMaps(Sequence):
         ik_c = self.kd.ibzk_kc[ik]
         return ik_c
 
-    def get_rotation_matrix(self, K):
+    def get_rotation_matrix(self, s):
         """Coordinate rotation matrix, mapping IBZ -> K."""
-        s = self.kd.sym_k[K]
         U_cc = self.kd.symmetry.op_scc[s]
         return U_cc
 
-    def get_atomic_permutations(self, K):
+    def get_atomic_permutations(self, s):
         """Permutations of atomic indices in the IBZ -> K map."""
-        s = self.kd.sym_k[K]
         # Atom a is mapped onto atom b.
         b_a = self.kd.symmetry.a_sa[s]
         return b_a
+
+    def get_projections_rotation_matrices(self, K):
+        """PAW projections rotation matrices for the IBZ -> K mapping."""
+        ik = self.kd.bz2ibz_k[K]
+        s = self.kd.sym_k[K]
+        return self._get_projections_rotation_matrices(ik, s)
+
+    @lru_cache
+    def _get_projections_rotation_matrices(self, ik, s):
+        """Correct the phase of the rotations of PAW projections.
+
+        The rotation for symmetry s is corrected by a phase factor depending on
+        the irreducible k-point ik, corresponding to the Bloch phase associated
+        to the atomic permutations of augmentation spheres.
+
+        Since ik and s are integers, we can easily keep a cache of the phase
+        corrected rotation matrices.
+        """
+        ik_c = self.kd.ibzk_kc[ik]
+        U_cc = self.get_rotation_matrix(s)
+        b_a = self.get_atomic_permutations(s)
+        U_aii = []
+        for a, R_sii in enumerate(self.R_asii):
+            b = b_a[a]
+            S_c = np.dot(self.spos_ac[a], U_cc) - self.spos_ac[b]
+            x = np.exp(2j * np.pi * np.dot(ik_c, S_c))
+            U_ii = R_sii[s].T * x
+            U_aii.append(U_ii)
+
+        return U_aii
 
     def get_time_reversal(self, K):
         """Does the mapping IBZ -> K involve time reversal?"""
