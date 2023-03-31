3b8ec464864ee4ced1fae75e32e109bf1368083a
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-03-31 21:00:05 +0200

    Ignore chnages in initial magmoms

--- gpaw/new/ase_interface.py
+++ gpaw/new/ase_interface.py
@@ -64,10 +64,12 @@ def compare_atoms(a1: Atoms, a2: Atoms) -> set[str]:
     if abs(a1.cell - a2.cell).max() > 0.0:
         return {'cell'}
 
+    """
     magnetic1 = a1.get_initial_magnetic_moments().any()
     magnetic2 = a2.get_initial_magnetic_moments().any()
     if magnetic1 != magnetic2:
         return {'magmoms'}
+    """
 
     if abs(a1.positions - a2.positions).max() > 0.0:
         return {'positions'}
@@ -114,9 +116,12 @@ class ASECalculator:
         * magmoms
         * dipole
         """
+        from gpaw.mpi import rank
+        print('PROP', rank, prop)
         if self.calculation is not None:
             changes = compare_atoms(self.atoms, atoms)
-            if changes & {'numbers', 'pbc', 'cell', 'magmoms'}:
+            print('CHANGES', rank, changes)
+            if changes & {'numbers', 'pbc', 'cell'}:
                 if 'numbers' not in changes:
                     # Remember magmoms if there are any:
                     magmom_a = self.calculation.results.get('magmoms')
@@ -200,6 +205,9 @@ class ASECalculator:
         Will also calculate "cheap" properties: energy, magnetic moments
         and dipole moment.
         """
+        from gpaw.mpi import rank
+        print('CONV', rank)
+
         with self.timer('SCF'):
             self.calculation.converge(calculate_forces=self._calculate_forces)
 
