56a68481aea884129e6c78da8684449905a27739
Yorick Schmerwitz <yla1@hi.is>
2023-04-28 15:31:11 +0000

    Remove constrained optimization content

--- gpaw/directmin/etdm.py
+++ gpaw/directmin/etdm.py
@@ -457,8 +457,7 @@ class ETDM:
                     continue
                 g_vec_u[k], error = self.dm_helper.calc_grad(
                     wfs, ham, kpt, self.func, self.evecs[k], self.evals[k],
-                    self.matrix_exp, self.representation, self.ind_up[k],
-                    self.constraints[k])
+                    self.matrix_exp, self.representation, self.ind_up[k])
 
                 self.error += error
             self.error = wfs.kd.comm.sum(self.error)
