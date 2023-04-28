7f01f8d9856aa4917895b19b8389dd92d4642ef7
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-04-28 09:31:40 +0200

    extract the magnon eigenmodes to test magnon frequency consistency

--- gpaw/test/response/test_cobalt_sf_gssALDA.py
+++ gpaw/test/response/test_cobalt_sf_gssALDA.py
@@ -54,7 +54,9 @@ def test_response_cobalt_sf_gssALDA(in_tmp_dir, gpw_files):
                              fxc=fxc, hxc_scaling=hxc_scaling)
         chi = chi.copy_with_reduced_ecut(reduced_ecut)
         chi.write_diagonal(f'chiwG_q{q}.pckl')
-        Amaj, Amin = spectral_decomposition(chi, pos_eigs=2, neg_eigs=0)
+        Amaj, _ = spectral_decomposition(chi,
+                                         pos_eigs=pos_eigs,
+                                         neg_eigs=neg_eigs)
         Amaj.write(f'Amaj_q{q}.pckl')
         assert f'{fxc},+-' in chi_factory.fxc_kernel_cache
 
@@ -64,8 +66,8 @@ def test_response_cobalt_sf_gssALDA(in_tmp_dir, gpw_files):
     # Read data
     w0_w, _, chi0_wG = read_susceptibility_array('chiwG_q0.pckl')
     w1_w, _, chi1_wG = read_susceptibility_array('chiwG_q1.pckl')
-    # Amaj0 = EigendecomposedSpectrum.from_file('Amaj_q0.pckl')
-    # Amaj1 = EigendecomposedSpectrum.from_file('Amaj_q1.pckl')
+    Amaj0 = EigendecomposedSpectrum.from_file('Amaj_q0.pckl')
+    Amaj1 = EigendecomposedSpectrum.from_file('Amaj_q1.pckl')
 
     # Find acoustic magnon mode
     wpeak00, Ipeak00 = findpeak(w0_w, -chi0_wG[:, 0].imag)
@@ -73,6 +75,21 @@ def test_response_cobalt_sf_gssALDA(in_tmp_dir, gpw_files):
     # Find optical magnon mode
     wpeak10, Ipeak10 = findpeak(w0_w, -chi0_wG[:, 1].imag)
     wpeak11, Ipeak11 = findpeak(w1_w, -chi1_wG[:, 1].imag)
+
+    # Use the eigenvectors at the acoustic magnon peaks to distinguish the
+    # two magnon modes
+    w0 = np.argmin(np.abs(Amaj0.omega_w - wpeak00))
+    s00_w = extract_magnon_eigenmodes(Amaj0, w0, 0)
+    s10_w = extract_magnon_eigenmodes(Amaj0, w0, 1)
+    w1 = np.argmin(np.abs(Amaj1.omega_w - wpeak01))
+    s01_w = extract_magnon_eigenmodes(Amaj1, w1, 0)
+    s11_w = extract_magnon_eigenmodes(Amaj1, w1, 1)
+
+    # Find peaks in eigenmodes
+    mpeak00, Speak00 = findpeak(Amaj0.omega_w, s00_w)
+    mpeak01, Speak01 = findpeak(Amaj1.omega_w, s01_w)
+    mpeak10, Speak10 = findpeak(Amaj0.omega_w, s10_w)
+    mpeak11, Speak11 = findpeak(Amaj1.omega_w, s11_w)
 
     if context.comm.rank == 0:
         # # Plot the magnons
@@ -85,6 +102,8 @@ def test_response_cobalt_sf_gssALDA(in_tmp_dir, gpw_files):
         # plt.axvline(wpeak10, c='0.5', linewidth=0.8)
         # plt.plot(Amaj0.omega_w, Amaj0.s_we[:, 0])
         # plt.plot(Amaj0.omega_w, Amaj0.s_we[:, 1])
+        # plt.plot(Amaj0.omega_w, s00_w)
+        # plt.plot(Amaj0.omega_w, s10_w)
         # # q=1
         # plt.subplot(1, 2, 2)
         # plt.plot(w1_w, -chi1_wG[:, 0].imag)
@@ -93,12 +112,16 @@ def test_response_cobalt_sf_gssALDA(in_tmp_dir, gpw_files):
         # plt.axvline(wpeak11, c='0.5', linewidth=0.8)
         # plt.plot(Amaj1.omega_w, Amaj1.s_we[:, 0])
         # plt.plot(Amaj1.omega_w, Amaj1.s_we[:, 1])
+        # plt.plot(Amaj1.omega_w, s01_w)
+        # plt.plot(Amaj1.omega_w, s11_w)
         # plt.show()
 
         # Print values
         print(hxc_scaling.lambd)
         print(wpeak00, wpeak01, wpeak10, wpeak11)
         print(Ipeak00, Ipeak01, Ipeak10, Ipeak11)
+        print(mpeak00, mpeak01, mpeak10, mpeak11)
+        print(Speak00, Speak01, Speak10, Speak11)
 
     # Test kernel scaling
     assert hxc_scaling.lambd == pytest.approx(0.9859, abs=0.005)
@@ -114,3 +137,22 @@ def test_response_cobalt_sf_gssALDA(in_tmp_dir, gpw_files):
     assert Ipeak01 == pytest.approx(2.209, abs=0.01)
     assert Ipeak10 == pytest.approx(1.013, abs=0.01)
     assert Ipeak11 == pytest.approx(0.934, abs=0.01)
+
+    # Test magnon frequency consistency
+    assert mpeak00 == pytest.approx(wpeak00, abs=0.005)
+    assert mpeak01 == pytest.approx(wpeak01, abs=0.01)
+    assert mpeak10 == pytest.approx(wpeak10, abs=0.01)
+    assert mpeak11 == pytest.approx(wpeak11, abs=0.01)
+
+    # Test magnon mode eigenvalues at extrema
+    assert Speak00 == pytest.approx(6.085, abs=0.01)
+    assert Speak01 == pytest.approx(4.796, abs=0.01)
+    assert Speak10 == pytest.approx(2.587, abs=0.01)
+    assert Speak11 == pytest.approx(2.426, abs=0.01)
+
+
+def extract_magnon_eigenmodes(Amaj, windex, eindex):
+    v_G = Amaj.v_wGe[windex, :, eindex]
+    s_w = np.conj(v_G) @ Amaj.A_wGG @ v_G
+    assert np.allclose(s_w.imag, 0.)
+    return s_w.real
