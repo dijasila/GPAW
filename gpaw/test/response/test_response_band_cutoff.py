d53264ee0ddd6dbeca0b54ff09f4217ac348547e
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-05-15 13:34:11 +0200

    use the ulmreader to avoid reading wave functions

--- gpaw/test/response/test_response_band_cutoff.py
+++ gpaw/test/response/test_response_band_cutoff.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
+from ase.io.ulm import ulmopen
 from gpaw.test.conftest import response_band_cutoff
-from gpaw import GPAW
 import gpaw.mpi as mpi
 
 
@@ -9,30 +9,27 @@ import gpaw.mpi as mpi
 def test_response_band_cutoff(in_tmp_dir, gpw_files):
 
     for gs, nbands in response_band_cutoff.items():
-        calc = GPAW(gpw_files[gs],
-                    communicator=mpi.serial_comm)
-        nconv = calc.parameters.convergence['bands']
+        with ulmopen(gpw_files[gs]) as reader:
+            eps_skn = reader.wave_functions.eigenvalues
+            nconv = reader.parameters.convergence['bands']
         print(gs)
         assert nbands < nconv
-        possible_cutoffs = get_nbands_cutoff_list(calc.wfs,
-                                                  nconv)
+        possible_cutoffs = get_nbands_cutoff_list(eps_skn, nconv)
         assert nbands in possible_cutoffs
 
         
-def get_nbands_cutoff_list(wfs, nconv, atol=1e-4):
+def get_nbands_cutoff_list(eps_skn, nconv, atol=1e-4):
     """ Possible cutoffs for response calc
     Returns the set all allowed band cutoffs in a response calculation.
     Assures that there are no  degeneracies at the edge of the cutoff
     """
-    nibzk = wfs.kd.nibzkpts
     allset = set(range(nconv + 1))
     # Loop over spins and k-points
-    for s in range(wfs.nspins):
-        for ik in range(nibzk):
-            kpt = wfs.kpt_qs[ik][s]
-            eps_n = kpt.eps_n
+    for eps_kn in eps_skn:
+        for eps_n in eps_kn:
             # check degenerate eigenvalues
-            cutlist = np.isclose(eps_n[:nconv - 1], eps_n[1:nconv])
+            cutlist = np.isclose(eps_n[:nconv - 1], eps_n[1:nconv],
+                                 atol=atol)
             cutlist = np.argwhere(~cutlist)
             # cutoff is allowed index + 1
             cutlist += 1
