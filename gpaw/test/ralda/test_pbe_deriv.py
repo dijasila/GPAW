49f7801cea0fc8bd267d4cb3c0f4b9dfb2193d45
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-05-15 14:26:28 +0200

    try to get proper tolerances for small and large densities

--- gpaw/test/ralda/test_pbe_deriv.py
+++ gpaw/test/ralda/test_pbe_deriv.py
@@ -7,21 +7,23 @@ def eps_unif_x(kf_g):
     return -3 * kf_g / (4.0 * np.pi)
 
 
-def test_pbe_x_derivs():
+# (We test "small" and "large" densities separately)
+@pytest.mark.parametrize(
+    'n_g', [
+        np.linspace(0.001, 0.02, 10000),
+        np.linspace(0.02, 0.5, 1000)
+    ])
+def test_pbe_x_derivs(n_g):
     # Here we test d(fxc(n))/dn.  We create a linspace of n and
     # calculate all the derivatives.  However we put a dummy (constant)
-    # value for s2_g (the gradient) which makes the numerical expression
-    # inaccurate (I think) for small densities.
-    #
-    # Therefore, we do not test for very small densities.
-    # However in principle it would be wise to check that these numbers
-    # are still kind of good.
+    # value for s2_g (the gradient) which makes the numerics sensitive
+    # (I think) for small densities.
     #
     # This test guards against the problem observed in #723.
-    n_g = np.linspace(0, 1, 1000)[100:]
     dn = n_g[1] - n_g[0]
+    # (assuming np.linspace)
 
-    gradn2 = np.ones_like(n_g) * 0.001
+    gradn2 = np.ones_like(n_g)
     kf_g = (3. * np.pi**2 * n_g)**(1 / 3.)
     s2_g = gradn2 / (4 * kf_g**2 * n_g**2)
 
@@ -33,6 +35,30 @@ def test_pbe_x_derivs():
 
     E_g = n_g * eps_unif_x(kf_g) * F_g
     d2Edn2_num = numderiv(numderiv(E_g))
+
+    if 1:
+        import matplotlib.pyplot as plt
+        ax = plt.gca()
+
+        Fn_num_g = numderiv(F_g)
+        #Fnn_num_g = numderiv(Fn_g)
+        ax.plot(n_g, Fn_g)
+        nhalf_g = n_g[:-1] + dn / 2
+        ax.plot(nhalf_g, Fn_num_g, ls='--')
+
+        Fnn_num_g = numderiv(Fn_g)
+        ax.plot(n_g, Fnn_g)
+        #ax.plot(nhalf_g, Fnn_num_g, ls='--')
+
+        Fnn_num2_g = numderiv(numderiv(F_g))
+        ax.plot(n_g[1:-1], Fnn_num2_g, ls='--')
+        errs = np.abs(Fnn_num2_g - Fnn_g[1:-1])
+        ax.plot(n_g[1:-1], errs)
+
+        ax.plot(n_g, fxc_g)
+        ax.plot(n_g[1:-1], d2Edn2_num, ls='--')
+
+        plt.show()
 
     assert fxc_g[1:-1] == pytest.approx(d2Edn2_num, abs=1e-4, rel=1e-4)
     # We could also test the other derivatives (Fn, Fnn)
