4b6b2bb87a9a3586c82969769ac9b08b9db9b84b
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-05-15 11:15:40 +0200

    unduplicate duplicated code

--- gpaw/test/conftest.py
+++ gpaw/test/conftest.py
@@ -667,28 +667,15 @@ class GPWFiles:
         return atoms.calc
 
     def h_pw210_rmmdiis(self):
-        H = Atoms('H')
-        H.set_pbc(True)
-        H.set_cell((2., 2., 3.))
-        H.center()
-        calc = GPAW(mode=PW(210, force_complex_dtype=True),
-                    eigensolver='rmm-diis',
-                    xc='LDA',
-                    basis='dzp',
-                    nbands=4,
-                    hund=True,
-                    parallel={'domain': 1},
-                    convergence={'density': 1.e-6})
-        H.calc = calc
-        H.get_potential_energy()
-        calc.diagonalize_full_hamiltonian(nbands=80)
-        return calc
+        return self._pw_210_rmmdiis(Atoms('H'))
 
     def h2_pw210_rmmdiis(self):
-        H2 = Atoms('H2', [(0, 0, 0), (0, 0, 0.7413)])
-        H2.set_pbc(True)
-        H2.set_cell((2., 2., 3.))
-        H2.center()
+        return self._pw_210_rmmdiis(Atoms('H2', [(0, 0, 0), (0, 0, 0.7413)]))
+
+    def _pw_210_rmmdiis(self, atoms):
+        atoms.set_pbc(True)
+        atoms.set_cell((2., 2., 3.))
+        atoms.center()
         calc = GPAW(mode=PW(210, force_complex_dtype=True),
                     eigensolver='rmm-diis',
                     xc='LDA',
@@ -696,10 +683,12 @@ class GPWFiles:
                     nbands=8,
                     parallel={'domain': 1},
                     convergence={'density': 1.e-6})
-        H2.calc = calc
-        H2.get_potential_energy()
+        atoms.calc = calc
+        atoms.get_potential_energy()
         calc.diagonalize_full_hamiltonian(nbands=80)
         return calc
+
+
 
 
 class GPAWPlugin:
