f8646c92f751fcc707db6dd873175eb6bd0d6bfb
Joachim Sødequist <joaso@dtu.dk>
2023-05-17 17:14:07 +0200

    Update test

--- gpaw/test/noncollinear/test_rad_pot.py
+++ gpaw/test/noncollinear/test_rad_pot.py
@@ -2,7 +2,7 @@ import pytest
 
 from gpaw.new.density import atomic_occupation_numbers
 from gpaw.setup import create_setup
-from gpaw.spinorbit import soc
+from gpaw.spinorbit import soc, projected_soc
 from gpaw.xc import XC
 
 
@@ -11,7 +11,7 @@ def test_rad_pot():
     setup = create_setup('Cr', xc=xc)
     mz = 1.0
     f_si = atomic_occupation_numbers(setup, [0, 0, mz], 4)
-    #print(f_si)
+    print(f_si)
     D_sp = setup.initialize_density_matrix(f_si)
     dv_vii = soc(setup, xc, D_sp)
     # Reversing magmom should give same potential
@@ -26,9 +26,9 @@ def test_proj_soc():
     mz = 1.0
     f_si = atomic_occupation_numbers(setup, [0, 0, mz], 4)
     D_sp = setup.initialize_density_matrix(f_si)
-    dv_vii = soc(setup, xc, D_sp,
-                 projected=True, theta=0, phi=0)
+    dv_vii = projected_soc(setup, xc, D_sp,
+                           theta=0, phi=0)
     # Reversing normal plane direction should give same potential
-    dv_vii -= soc(setup, xc, D_sp,
-                  projected=True, theta=180, phi=0)
+    dv_vii -= projected_soc(setup, xc, D_sp,
+                            theta=180, phi=0)
     assert abs(dv_vii).max() == pytest.approx(0.0, abs=1e-12)
