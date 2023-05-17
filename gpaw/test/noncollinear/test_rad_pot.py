273a2900b93c8d83343cf869dd892f1bbdbea94a
Joachim Sødequist <joaso@dtu.dk>
2023-05-17 15:10:45 +0200

    Add test for projected soc

--- gpaw/test/noncollinear/test_rad_pot.py
+++ gpaw/test/noncollinear/test_rad_pot.py
@@ -11,10 +11,24 @@ def test_rad_pot():
     setup = create_setup('Cr', xc=xc)
     mz = 1.0
     f_si = atomic_occupation_numbers(setup, [0, 0, mz], 4)
-    print(f_si)
+    #print(f_si)
     D_sp = setup.initialize_density_matrix(f_si)
     dv_vii = soc(setup, xc, D_sp)
     # Reversing magmom should give same potential
     D_sp[3] *= -1
     dv_vii -= soc(setup, xc, D_sp)
+    assert abs(dv_vii).max() == pytest.approx(0.0, abs=1e-12)
+
+
+def test_proj_soc():
+    xc = XC('LDA', collinear=False)
+    setup = create_setup('Cr', xc=xc)
+    mz = 1.0
+    f_si = atomic_occupation_numbers(setup, [0, 0, mz], 4)
+    D_sp = setup.initialize_density_matrix(f_si)
+    dv_vii = soc(setup, xc, D_sp,
+                 projected=True, theta=0, phi=0)
+    # Reversing normal plane direction should give same potential
+    dv_vii -= soc(setup, xc, D_sp,
+                  projected=True, theta=180, phi=0)
     assert abs(dv_vii).max() == pytest.approx(0.0, abs=1e-12)
