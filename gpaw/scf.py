5a5deab56a00cbddc4fbe22bb39c503a48a687f6
alxvov <alxvov@gmail.com>
2023-05-21 23:00:31 +0100

    move dm specifc function to dm folder

--- gpaw/scf.py
+++ gpaw/scf.py
@@ -1,13 +1,11 @@
 import time
-import warnings
 
 import numpy as np
-from ase.units import Ha
 
 from gpaw import KohnShamConvergenceError
 from gpaw.convergence_criteria import check_convergence
 from gpaw.forces import calculate_forces
-
+from gpaw.directmin.scf_helper import do_if_converged, check_eigensolver_state
 
 class SCFLoop:
     """Self-consistent field loop."""
@@ -48,7 +46,7 @@ class SCFLoop:
     def irun(self, wfs, ham, dens, log, callback):
 
         self.eigensolver_name = getattr(wfs.eigensolver, "name", None)
-        self.check_eigensolver_state(wfs, ham, dens, log)
+        check_eigensolver_state(self.eigensolver_name, wfs, ham, dens, log)
         self.niter = 1
         converged = False
 
@@ -62,7 +60,9 @@ class SCFLoop:
             converged = (self.converged and
                          self.niter >= self.niter_fixdensity)
             if converged:
-                self.do_if_converged(wfs, ham, dens, log)
+                do_if_converged(
+                    self.eigensolver_name,wfs, ham, dens, log
+                )
                 break
 
             self.update_ham_and_dens(wfs, ham, dens)
@@ -106,29 +106,6 @@ class SCFLoop:
         raise KohnShamConvergenceError(
             'Did not converge!  See text output for help.')
 
-    def check_eigensolver_state(self, wfs, ham, dens, log):
-
-        solver = wfs.eigensolver
-        name = self.eigensolver_name
-        if name == 'etdm' or name == 'directmin':
-            solver.eg_count = 0
-            solver.globaliters = 0
-
-            if hasattr(solver, 'iloop'):
-                if solver.iloop is not None:
-                    solver.iloop.total_eg_count = 0
-            if hasattr(solver, 'iloop_outer'):
-                if solver.iloop_outer is not None:
-                    solver.iloop_outer.total_eg_count = 0
-
-            wfs.eigensolver.check_assertions(wfs, dens)
-            if name == 'etdm':
-                if wfs.eigensolver.dm_helper is None:
-                    wfs.eigensolver.initialize_dm_helper(wfs, ham, dens, log)
-            else:
-                if not solver.initialized:
-                    solver.init_me(wfs, ham, dens, log)
-
     def iterate_eigensolver(self, wfs, ham, dens, log):
 
         if self.eigensolver_name == 'etdm':
@@ -160,99 +137,6 @@ class SCFLoop:
         ham.get_energy(
             e_entropy, wfs, kin_en_using_band=kin_en_using_band, e_sic=e_sic)
 
-    def do_if_converged(self, wfs, ham, dens, log):
-
-        if self.eigensolver_name == 'etdm':
-            if hasattr(wfs.eigensolver, 'e_sic'):
-                e_sic = wfs.eigensolver.e_sic
-            else:
-                e_sic = 0.0
-            energy = ham.get_energy(
-                0.0, wfs, kin_en_using_band=False, e_sic=e_sic)
-            wfs.calculate_occupation_numbers(dens.fixed)
-            wfs.eigensolver.get_canonical_representation(
-                ham, wfs, dens, sort_eigenvalues=True)
-            wfs.eigensolver.update_ks_energy(ham, wfs, dens)
-            energy_converged = ham.get_energy(
-                0.0, wfs, kin_en_using_band=False, e_sic=e_sic)
-            energy_diff_after_scf = abs(energy - energy_converged) * Ha
-            if energy_diff_after_scf > 1.0e-6:
-                warnings.warn('Jump in energy of %f eV detected at the end of '
-                              'SCF after getting canonical orbitals, SCF '
-                              'might have converged to the wrong solution '
-                              'or achieved energy convergence to the correct '
-                              'solution above 1.0e-6 eV'
-                              % (energy_diff_after_scf))
-
-            log('\nOccupied states converged after'
-                ' {:d} e/g evaluations'.format(wfs.eigensolver.eg_count))
-        elif self.eigensolver_name == 'directmin':
-            solver = wfs.eigensolver
-            occ_name = getattr(wfs.occupations, 'name', None)
-            if wfs.mode == 'fd' or wfs.mode == 'pw':
-                solver.choose_optimal_orbitals(
-                    wfs, ham, dens)
-                niter1 = solver.eg_count
-                niter2 = 0
-                niter3 = 0
-
-                iloop1 = solver.iloop is not None
-                iloop2 = solver.iloop_outer is not None
-                if iloop1:
-                    niter2 = solver.total_eg_count_iloop
-                if iloop2:
-                    niter3 = solver.total_eg_count_iloop_outer
-
-                if iloop1 and iloop2:
-                    log(
-                        '\nOccupied states converged after'
-                        ' {:d} KS and {:d} SIC e/g '
-                        'evaluations'.format(niter3,
-                                             niter2 + niter3))
-                elif not iloop1 and iloop2:
-                    log(
-                        '\nOccupied states converged after'
-                        ' {:d} e/g evaluations'.format(niter3))
-                elif iloop1 and not iloop2:
-                    log(
-                        '\nOccupied states converged after'
-                        ' {:d} KS and {:d} SIC e/g '
-                        'evaluations'.format(niter1, niter2))
-                else:
-                    log(
-                        '\nOccupied states converged after'
-                        ' {:d} e/g evaluations'.format(niter1))
-                if solver.convergelumo:
-                    log('Converge unoccupied states:')
-                    max_er = wfs.eigensolver.error
-                    max_er *= Ha ** 2 / wfs.nvalence
-                    solver.run_lumo(ham, wfs, dens, max_er, log)
-                else:
-                    solver.initialized = False
-                    log('Unoccupied states are not converged.')
-                rewrite_psi = True
-                sic_calc = 'SIC' in solver.odd_parameters['name']
-                if sic_calc:
-                    rewrite_psi = False
-                solver.get_canonical_representation(
-                    ham, wfs, dens, rewrite_psi)
-                solver._e_entropy = \
-                    wfs.calculate_occupation_numbers(dens.fixed)
-                if not sic_calc and occ_name:
-                    for kpt in wfs.kpt_u:
-                        solver.sort_wavefunctions(wfs, kpt)
-                    solver._e_entropy = \
-                        wfs.calculate_occupation_numbers(dens.fixed)
-                solver.get_energy_and_tangent_gradients(
-                    ham, wfs, dens)
-            elif wfs.mode == 'lcao':
-                # Do we need to calculate the occupation numbers here?
-                wfs.calculate_occupation_numbers(dens.fixed)
-                solver.get_canonical_representation(ham, wfs, dens)
-                niter = solver.eg_count
-                log(
-                    '\nOccupied states converged after'
-                    ' {:d} e/g evaluations'.format(niter))
 
     def update_ham_and_dens(self, wfs, ham, dens):
 
