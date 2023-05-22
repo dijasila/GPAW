e8ac234c60e754787a6319de35018b6db07a9795
tboland1 <tmabo@dtu.dk>
2023-05-22 14:05:55 +0200

    clean up unused variables and consolidate plotting codes

--- doc/tutorialsexercises/opticalresponse/dielectric_response/graphene_dielectric_function.py
+++ doc/tutorialsexercises/opticalresponse/dielectric_response/graphene_dielectric_function.py
@@ -66,18 +66,14 @@ df1, df2 = df.get_dielectric_function(q_c=[0, 0, 0],
                                       filename='df_point.csv')
 omega_w = df.get_frequencies()
 
-# Find the peak and assert
-w_w = df.wd.omega_w[200:450]
-df2_point = -(1. / df2.imag)[200:450]
-df2_tetra = -(1. / df2tetra.imag)[200:450]
-# convolve with gaussian to smooth the curve
-df2_wimag = gaussian_filter1d(df2.imag, 7)
-
 
 if world.rank == 0:
     plt.figure(figsize=(6, 6))
     plt.plot(omega_w, df2.imag * 2, label='Point sampling')
     plt.plot(omega_w, df2tetra.imag * 2, label='Tetrahedron')
+
+    # convolve with gaussian to smooth the curve
+    df2_wimag = gaussian_filter1d(df2.imag, 7)
     plt.plot(omega_w, df2_wimag * 2, 'magenta', label='Smoothed')
 
     # Analytical result for graphene
