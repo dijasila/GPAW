06fdb0f4dc6e35eeb8eaa5346fa7c2833eb3943b
Joachim Sødequist <joaso@dtu.dk>
2023-05-22 11:13:43 +0200

    Update to findpeak and include other PR changes

--- doc/tutorialsexercises/opticalresponse/dielectric_response/al-plasmon-peak.py
+++ doc/tutorialsexercises/opticalresponse/dielectric_response/al-plasmon-peak.py
@@ -1,26 +1,27 @@
 from gpaw import GPAW
 from gpaw.response.df import DielectricFunction
+from gpaw.test import findpeak
 from ase.build import bulk
 import numpy as np
 
 
 def get_plasmon_peak(df, q_c):
     _, eels_w = df.get_eels_spectrum(q_c=q_c, filename=None)
-    plasmonpeak = np.argmax(eels_w)
-    omega = df.get_frequencies()[plasmonpeak]
-    return omega
+    omega_w = df.get_frequencies()
+    wpeak, _ = findpeak(omega_w, eels_w)
+    return wpeak
 
 
 # Part 1: Ground state calculation
 atoms = bulk('Al', 'fcc', a=4.043)  # generate fcc crystal structure
 
 # GPAW calculator initialization:
-kpts = [8, 8, 8]
+kpts = [16, 16, 16]
 calc = GPAW(mode='pw', kpts={'size': kpts, 'gamma': True}, txt=None)
 
 atoms.calc = calc
 atoms.get_potential_energy()  # ground state calculation is performed
-calc.write('gs-al-peak.gpw')  # use 'all' option to write wavefunctions
+calc.write('gs-al-peak.gpw')
 
 peak_ki = []
 N_k = []
@@ -28,11 +29,11 @@ for i in range(1, 11, 1):
     N_k.append(k := i * 8)
     kpts = {'size': [k, k, k], 'gamma': True}
     responseGS = GPAW('gs-al-peak.gpw').fixed_density(kpts=kpts, txt=None)
-    responseGS.write('res-al-peak.gpw', 'all')
-    
+    responseGS.write('res-al-peak.gpw', 'all')  # 'all' to write wavefunctions
+
     # Momentum transfer, must be the difference between two kpoints:
     q_c = [0., 0., 0.]
-    
+
     # Part 2: Spectrum calculation
     df_tetra = DielectricFunction(calc='res-al-peak.gpw',
                                   rate=0.2,
@@ -41,7 +42,7 @@ for i in range(1, 11, 1):
 
     df = DielectricFunction(calc='res-al-peak.gpw', rate=0.2)
     point_peak = get_plasmon_peak(df, q_c)
-    
+
     peak_ki.append([tetra_peak, point_peak])
 
 peak_ik = np.array(peak_ki).T
