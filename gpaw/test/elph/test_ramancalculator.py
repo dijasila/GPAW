8fec3fce537e7dc65e2ad22a72bfde82e7f727d0
Robert Warmbier <robert.warmbier@gmx.de>
2023-05-24 14:47:43 +0200

    fixed raman test to life with non-symmetic moments tensor

--- gpaw/test/elph/test_ramancalculator.py
+++ gpaw/test/elph/test_ramancalculator.py
@@ -42,6 +42,14 @@ def test_ramancalculator(gpw_files, tmp_path_factory):
         np.save("gsqklnn.npy", g_sqklnn)
 
     rrc = ResonantRamanCalculator(calc, wph_w)
+    assert rrc.mom_skvnm == pytest.approx(np.transpose(rrc.mom_skvnm,
+                                                       (0, 1, 2, 4, 3)).conj())
+    # Force momentum matrix elements to be the same in all directions
+    # else R^ab won't be R^{ba*}
+    # This is a bit of a dirty hack I guess. Ideally we need a test systm with
+    # equivalent axes but no degenerate bands... so yeah
+    rrc.mom_skvnm[0, :, 1] = rrc.mom_skvnm[0, :, 0]
+    rrc.mom_skvnm[0, :, 2] = rrc.mom_skvnm[0, :, 0]
 
     # check reading of file cache
     check_cache = MultiFileJSONCache("Rlab")
@@ -59,5 +67,6 @@ def test_ramancalculator(gpw_files, tmp_path_factory):
         if j > i:
             # need to make sure momentum matrix is perfectly hermitian too
             Rother_l = check_cache[f"{'xyz'[j]}{'xyz'[i]}"]
-            assert R_l[2].real == pytest.approx(Rother_l[2].real, rel=0.1)
-            assert R_l[2].imag == pytest.approx(-Rother_l[2].imag, rel=1.0)
+            print(i, j, R_l[2], Rother_l[2])
+            assert R_l[2].real == pytest.approx(Rother_l[2].real)
+            assert R_l[2].imag == pytest.approx(Rother_l[2].imag)
