dc6323263a83c881b0252182b737fb353af3b9b7
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-05-31 09:23:48 +0200

    make the rshe reduction functionality stand-alone functions

--- gpaw/response/rshe.py
+++ gpaw/response/rshe.py
@@ -94,94 +94,6 @@ class RealSphericalHarmonicsExpansion:
         Y_nM = self.Y_nL[:, self.L_M]
         return Y_nM @ self.f_gM.T
 
-    def reduce_expansion(self, fns_g, lmax=-1, wmin=None):
-        """Reduce the composite index L=(l,m) to M, which indexes coefficients
-        contributing with a weight larger than wmin to the surface norm square
-        on average.
-        Remember to adjust documentation XXX
-
-        Parameters
-        ----------
-        Some documentation here! XXX
-        """
-        # Redo me XXX
-        # We do not expand beyond l=5
-        if lmax == -1:
-            lmax = 5
-        assert lmax in range(6)
-        wmin = wmin if wmin is not None else 0.
-        assert isinstance(wmin, float) and wmin >= 0.
-
-        # We assume to start with a full expansion
-        assert self.f_gM.shape[1] == self.nL
-        f_gL = self.f_gM
-
-        # Filter away (l,m)-coefficients based on their average weight in
-        # completing the surface norm square of df
-        fw_gL = self._calculate_ns_weights(self.nL, f_gL, fns_g)
-        rshew_L = np.average(fw_gL, axis=0)  # Average over the radial grid
-
-        # Take rshe coefficients up to l <= lmax (<= 5) which contribute with
-        # at least wmin to the surface norm square on average
-        nL = min(self.nL, (lmax + 1)**2)
-        L_L = np.arange(nL)
-        L_M = np.where(rshew_L[L_L] >= wmin)[0]
-        f_gM = f_gL[:, L_M]
-        reduced_rshe = RealSphericalHarmonicsExpansion(
-            f_gM, self.Y_nL, L_M=L_M)
-
-        # Construct info string about the reduced expansion
-        info_string = self.get_reduction_info_string(nL, wmin, fw_gL, rshew_L)
-
-        return reduced_rshe, info_string
-
-    @staticmethod
-    def _calculate_ns_weights(nL, df_gL, dfSns_g):
-        """Calculate the weighted contribution of each rsh coefficient to the
-        surface norm square of df as a function of radial grid index g."""
-        # This function is in dire need of clean-up! XXX
-        nallL = df_gL.shape[1]
-        dfSns_gL = np.repeat(dfSns_g, nallL).reshape(dfSns_g.shape[0], nallL)
-        dfSw_gL = df_gL ** 2 / dfSns_gL
-
-        return dfSw_gL
-
-    def get_reduction_info_string(self, nL, wmin, fw_gL, rshew_L):
-        """
-        Some documentation here! XXX
-        """
-        info_string = '{0:6}  {1:10}  {2:10}  {3:8}'.format('(l,m)',
-                                                            'max weight',
-                                                            'avg weight',
-                                                            'included')
-        for L, (fw_g, rshew) in enumerate(zip(fw_gL.T, rshew_L)):
-            info_string += '\n' + self.get_rshe_coefficient_info_string(
-                L, nL, rshew, wmin, fw_g)
-
-        tot_avg_cov = np.average(np.sum(fw_gL, axis=1))
-        avg_cov = np.average(
-            np.sum(fw_gL[:, :nL][:, rshew_L[:nL] > wmin], axis=1))
-        info_string += f'\nIn total: {avg_cov} of the surface norm square is '\
-            'covered on average'
-        info_string += f'\nIn total: {tot_avg_cov} of the surface norm '\
-            'square could be covered on average'
-
-        return info_string
-
-    @staticmethod
-    def get_rshe_coefficient_info_string(L, nL, rshew, wmin, fw_g):
-        """
-        Some documentation here! XXX
-        """
-        l = int(np.sqrt(L))
-        m = L - l * (l + 1)
-        included = 'yes' if (rshew > wmin and L < nL) else 'no'
-        info_string = '{0:6}  {1:1.8f}  {2:1.8f}  {3:8}'.format(f'({l},{m})',
-                                                                np.max(fw_g),
-                                                                rshew,
-                                                                included)
-        return info_string
-
 
 def calculate_reduced_rshe(f_ng, Y_nL, lmax=-1, wmin=None):
     """
@@ -189,4 +101,94 @@ def calculate_reduced_rshe(f_ng, Y_nL, lmax=-1, wmin=None):
     """
     rshe = RealSphericalHarmonicsExpansion.from_spherical_grid(f_ng, Y_nL)
     dfns_g = integrate_lebedev(f_ng ** 2)
-    return rshe.reduce_expansion(dfns_g, lmax=lmax, wmin=wmin)
+    return reduce_expansion(rshe, dfns_g, lmax=lmax, wmin=wmin)
+
+
+def reduce_expansion(rshe, fns_g, lmax=-1, wmin=None):
+    """Reduce the composite index L=(l,m) to M, which indexes coefficients
+    contributing with a weight larger than wmin to the surface norm square
+    on average.
+    Remember to adjust documentation XXX
+
+    Parameters
+    ----------
+    Some documentation here! XXX
+    """
+    # Redo me XXX
+    # We do not expand beyond l=5
+    if lmax == -1:
+        lmax = 5
+    assert lmax in range(6)
+    wmin = wmin if wmin is not None else 0.
+    assert isinstance(wmin, float) and wmin >= 0.
+
+    # We assume to start with a full expansion
+    assert rshe.f_gM.shape[1] == rshe.nL
+    f_gL = rshe.f_gM
+
+    # Filter away (l,m)-coefficients based on their average weight in
+    # completing the surface norm square of df
+    fw_gL = _calculate_ns_weights(rshe.nL, f_gL, fns_g)
+    rshew_L = np.average(fw_gL, axis=0)  # Average over the radial grid
+
+    # Take rshe coefficients up to l <= lmax (<= 5) which contribute with
+    # at least wmin to the surface norm square on average
+    nL = min(rshe.nL, (lmax + 1)**2)
+    L_L = np.arange(nL)
+    L_M = np.where(rshew_L[L_L] >= wmin)[0]
+    f_gM = f_gL[:, L_M]
+    reduced_rshe = RealSphericalHarmonicsExpansion(
+        f_gM, rshe.Y_nL, L_M=L_M)
+
+    # Construct info string about the reduced expansion
+    info_string = get_reduction_info_string(nL, wmin, fw_gL, rshew_L)
+
+    return reduced_rshe, info_string
+
+
+def _calculate_ns_weights(nL, df_gL, dfSns_g):
+    """Calculate the weighted contribution of each rsh coefficient to the
+    surface norm square of df as a function of radial grid index g."""
+    # This function is in dire need of clean-up! XXX
+    nallL = df_gL.shape[1]
+    dfSns_gL = np.repeat(dfSns_g, nallL).reshape(dfSns_g.shape[0], nallL)
+    dfSw_gL = df_gL ** 2 / dfSns_gL
+
+    return dfSw_gL
+
+
+def get_reduction_info_string(nL, wmin, fw_gL, rshew_L):
+    """
+    Some documentation here! XXX
+    """
+    info_string = '{0:6}  {1:10}  {2:10}  {3:8}'.format('(l,m)',
+                                                        'max weight',
+                                                        'avg weight',
+                                                        'included')
+    for L, (fw_g, rshew) in enumerate(zip(fw_gL.T, rshew_L)):
+        info_string += '\n' + get_rshe_coefficient_info_string(
+            L, nL, rshew, wmin, fw_g)
+
+    tot_avg_cov = np.average(np.sum(fw_gL, axis=1))
+    avg_cov = np.average(
+        np.sum(fw_gL[:, :nL][:, rshew_L[:nL] > wmin], axis=1))
+    info_string += f'\nIn total: {avg_cov} of the surface norm square is '\
+        'covered on average'
+    info_string += f'\nIn total: {tot_avg_cov} of the surface norm '\
+        'square could be covered on average'
+
+    return info_string
+
+
+def get_rshe_coefficient_info_string(L, nL, rshew, wmin, fw_g):
+    """
+    Some documentation here! XXX
+    """
+    l = int(np.sqrt(L))
+    m = L - l * (l + 1)
+    included = 'yes' if (rshew > wmin and L < nL) else 'no'
+    info_string = '{0:6}  {1:1.8f}  {2:1.8f}  {3:8}'.format(f'({l},{m})',
+                                                            np.max(fw_g),
+                                                            rshew,
+                                                            included)
+    return info_string
