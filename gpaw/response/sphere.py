f5b4a3519b499ca14f9511d56781a4c97c3f479b
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-05-31 14:32:45 +0200

    add integrate_radial_grid function

--- gpaw/response/sphere.py
+++ gpaw/response/sphere.py
@@ -9,3 +9,45 @@ def integrate_lebedev(f_nx):
     """
     from gpaw.sphere.lebedev import weight_n
     return 4. * np.pi * np.tensordot(weight_n, f_nx, axes=([0], [0]))
+
+
+def integrate_radial_grid(f_gx, r_g, rcut=None):
+    """
+    Some documentation here! XXX
+    """
+    if rcut is not None:
+        assert rcut > 0.
+        assert np.any(r_g >= rcut)
+        if rcut not in r_g:
+            # Find the two points closest to rcut and interpolate between them
+            # to get the value at rcut
+            g1, g2 = find_two_closest_grid_points(r_g, rcut)
+            r1 = r_g[g1]
+            r2 = r_g[g2]
+            lambd = (rcut - r1) / (r2 - r1)
+            f_interpolated_x = (1 - lambd) * f_gx[g1] + lambd * f_gx[g2]
+            # Add rcut as a grid point
+            r_g = np.append(r_g, np.array([rcut]))
+            f_gx = np.append(f_gx, np.array([f_interpolated_x]), axis=0)
+        # Integrate only grid points inside rcut
+        mask_g = r_g <= rcut
+        r_g = r_g[mask_g]
+        f_gx = f_gx[mask_g]
+    # Perform actual integration using the trapez method
+    f_xg = np.moveaxis(f_gx, 0, -1)
+    f_x = np.trapz(f_xg * r_g**2., x=r_g)
+
+    return f_x
+
+
+def find_two_closest_grid_points(r_g, rcut):
+    """Find the two closest grid point to a specified rcut."""
+    # Find the two smallest absolute differences
+    abs_diff_g = abs(r_g - rcut)
+    ad1, ad2 = np.partition(abs_diff_g, 1)[:2]
+
+    # Identify the corresponding indices
+    g1 = np.where(abs_diff_g == ad1)[0][0]
+    g2 = np.where(abs_diff_g == ad2)[0][0]
+
+    return g1, g2
