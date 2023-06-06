fbeef58ea0cb0752ee3f3a83938f08a02c07a104
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-06-06 14:30:31 +0200

    Timing ...

--- gpaw/test/core/test_abs_square.py
+++ gpaw/test/core/test_abs_square.py
@@ -1,24 +1,32 @@
-from math import pi
+from time import time
 
 import numpy as np
 import pytest
-from gpaw import SCIPY_VERSION
+
 from gpaw.core import PlaneWaves, UniformGrid
-from gpaw.core.plane_waves import find_reciprocal_vectors
 from gpaw.gpu import cupy as cp
-from gpaw.mpi import world
 
 
 @pytest.mark.parametrize('xp', [np, cp])
 def test_pw_abs_square(xp):
-    a = 2.5
-    N = 10
-    B = 17
+    abs_square(a=2.5, N=10, B=17, xp=xp)
+
+
+def abs_square(a, N, B, xp):
     grid = UniformGrid(cell=[a, a, a], size=[N, N, N])
-    pw = PlaneWaves(ecut=10, cell=grid.cell, dtype=complex)
+    ecut = 0.5 * (np.pi * N / a)
+    pw = PlaneWaves(ecut=ecut, cell=grid.cell, dtype=complex)
     psit_nG = pw.zeros(B, xp=xp)
     psit_nG.data[:, 0] = 1.0
     weight_n = np.linspace(1, 0, B)
     nt_R = grid.zeros(xp=xp)
+    t = time()
     psit_nG.abs_square(weight_n, nt_R)
+    t = time() - t
     assert nt_R.integrate() == pytest.approx(a**3 * weight_n.sum())
+    return t
+
+
+if __name__ == '__main__':
+    t = abs_square(6.0, 32, 100, cp)
+    print(t)
