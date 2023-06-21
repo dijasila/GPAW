a13a11850ca01e295174192798e094192951a0a2
giale <gianl.levi@gmail.com>
2023-06-21 17:12:23 +0000

    Speed up lcao gmf sic test

--- gpaw/test/sic/test_lcaosic_gmf_h2o.py
+++ gpaw/test/sic/test_lcaosic_gmf_h2o.py
@@ -4,6 +4,7 @@ from gpaw import GPAW, LCAO
 from gpaw.directmin.etdm import ETDM
 from gpaw.directmin.tools import excite
 from gpaw.directmin.derivatives import Davidson
+from gpaw.mom import prepare_mom_calculation
 from ase import Atoms
 import numpy as np
 
@@ -23,20 +24,13 @@ def test_lcaosic_h2o(in_tmp_dir):
                 positions=[(0, 0, 0),
                            (d, 0, 0),
                            (d * np.cos(t), d * np.sin(t), 0)])
-    H2O.center(vacuum=4.0)
+    H2O.center(vacuum=3.0)
 
-    f_sn = [[1,1,1,1,0,0], [1,1,1,1,0,0]]
-
-    calc = GPAW(mode=LCAO(force_complex_dtype=True),
-                h=0.22,
-                occupations={'name': 'mom', 'numbers': f_sn,
-                             'use_fixed_occupations': True},
-                eigensolver=ETDM(localizationtype='PM_PZ',
-                                 localizationseed=42,
-                                 functional_settings={
-                                     'name': 'PZ-SIC',
-                                     'scaling_factor': \
-                                         (0.5, 0.5)}),  # SIC/2
+    calc = GPAW(mode=LCAO(),
+                basis='sz(dzp)',
+                h=0.24,
+                occupations={'name': 'fixed-uniform'},
+                eigensolver='etdm',
                 convergence={'eigenstates': 1e-4},
                 mixer={'backend': 'no-mixing'},
                 nbands='nao',
@@ -46,30 +40,39 @@ def test_lcaosic_h2o(in_tmp_dir):
     H2O.calc = calc
     H2O.get_potential_energy()
 
+    calc.set(eigensolver=ETDM(searchdir_algo={'name': 'l-sr1p'},
+                              linesearch_algo={'name': 'max-step'},
+                              need_init_orbs=False))
     f_sn = excite(calc, 0, 0, spin=(0, 0))
-    for k, kpt in enumerate(calc.wfs.kpt_u):
-        kpt.f_n = f_sn[k]
+    prepare_mom_calculation(calc, H2O, f_sn)
+    H2O.get_potential_energy()
 
     dave = Davidson(calc.wfs.eigensolver, None)
     appr_sp_order = dave.estimate_sp_order(calc)
+    print(appr_sp_order)
 
     calc.set(eigensolver=ETDM(
         partial_diagonalizer={
-            'name': 'Davidson', 'logfile': None, 'seed': 42, 'm': 30,
-            'remember_sp_order': True, 'sp_order': appr_sp_order},
+            'name': 'Davidson', 'logfile': 'test.txt', 'seed': 42,
+            'm': 20, 'eps': 5e-3, 'remember_sp_order': True,
+            'sp_order': appr_sp_order},
         linesearch_algo={'name': 'max-step'},
         searchdir_algo={'name': 'LBFGS-P_GMF'},
-        functional_settings={'name': 'PZ-SIC', 'scaling_factor': (0.5, 0.5)},
+        localizationtype='PM',
+        functional_settings={'name': 'PZ-SIC',
+                             'scaling_factor': (0.5, 0.5)},
         need_init_orbs=False),
         occupations={'name': 'mom', 'numbers': f_sn,
                      'use_fixed_occupations': True})
 
-    H2O.get_potential_energy()
+    e = H2O.get_potential_energy()
+    assert e == pytest.approx(-2.007241, abs=1.0e-3)
+
     f = H2O.get_forces()
 
-    f_num = [[-1.16916945e+01, -1.27929188e+01, 1.04419787e-02],
-             [1.64474334e+01, -1.25908321e+00, -3.04315451e-03],
-             [-4.99662063e+00, 1.40785094e+01, -1.13466702e-03]]
+    f_num = np.array([[-8.01206297e+00, -1.51553367e+01,  3.60670227e-03],
+                      [1.42287594e+01, -9.81724693e-01, -5.09333905e-04],
+                      [-4.92299436e+00,  1.55306540e+01,  2.12438557e-03]])
 
     numeric = True
     if numeric:
@@ -81,5 +84,4 @@ def test_lcaosic_h2o(in_tmp_dir):
         print(f_num)
         print(f - f_num, np.abs(f - f_num).max())
 
-
-    assert f == pytest.approx(f_num, abs=0.1)
+    assert f == pytest.approx(f_num, abs=0.75)
