6aa4e03a59882e16903381f3068c800f9788f01a
Fredrik Andreas Nilsson <fanni@DTU-FVFGC14Q05G.lan>
2023-06-27 15:12:40 +0200

    added iron test

---
+++ gpaw/test/wannier/test_wannier90.py
@@ -0,0 +1,82 @@
+import pytest
+import os
+import numpy as np
+from gpaw import GPAW
+import gpaw.wannier90 as w90
+from gpaw.wannier.w90 import read_wout_all
+from pathlib import Path
+from gpaw.spinorbit import soc_eigenstates
+"""
+@pytest.mark.wannier
+@pytest.mark.serial
+def test_wannier90(gpw_files, in_tmp_dir):
+    o_ai = [[], [0, 1, 2, 3]]
+    bands = range(4)
+    calc = GPAW(gpw_files['gaas_pw_nosym_wfs'])
+    seed = 'GaAs'
+    assert calc.wfs.kd.nbzkpts == calc.wfs.kd.nibzkpts
+
+    w90.write_input(calc, orbitals_ai=o_ai,
+                    bands=bands,
+                    seed=seed,
+                    num_iter=1000,
+                    plot=False)
+    try:
+        os.system('wannier90.x -pp ' + seed)
+    except FileNotFoundError:
+        return  # no wannier90.x executable
+    w90.write_projections(calc, orbitals_ai=o_ai, seed=seed)
+    w90.write_eigenvalues(calc, seed=seed)
+    w90.write_overlaps(calc, seed=seed)
+    os.system('wannier90.x ' + seed)
+    with (Path('GaAs.wout')).open() as fd:
+        w = read_wout_all(fd)
+    centers = np.sum(np.array(w['centers']), axis=1)
+    print('centers:', centers)
+    centers_correct = np.array([5.68, 5.68, 5.68])
+    assert np.allclose(centers, centers_correct)
+    spreads = np.sum(np.array(w['spreads']))
+    assert spreads == pytest.approx(4.4999, abs=0.002)
+
+"""
+@pytest.mark.wannier
+@pytest.mark.serial
+def test_wannier90_soc(gpw_files, in_tmp_dir):
+    o_ai = [[], [0, 1, 2, 3]]
+    bands = range(4)
+    calc = GPAW(gpw_files['fe_pw_nosym_wfs'])
+    soc = soc_eigenstates(calc)
+    seed = 'Fe'
+    assert calc.wfs.kd.nbzkpts == calc.wfs.kd.nibzkpts
+    
+    w90.write_input(calc,
+                bands=range(9),
+                spinors=True,
+                num_iter=200,
+                dis_num_iter=500,
+                dis_mix_ratio=1.0,
+                seed=seed)
+    try:
+        os.system('wannier90.x -pp ' + seed)
+    except FileNotFoundError:
+        return  # no wannier90.x executable
+    w90.write_projections(calc,
+                      seed=seed, soc=soc)
+    w90.write_eigenvalues(calc, seed=seed, soc=soc)
+    w90.write_overlaps(calc, seed=seed, soc=soc)
+
+    os.system('wannier90.x ' + seed)
+    
+    with (Path('Fe.wout')).open() as fd:
+        w = read_wout_all(fd)
+    centers = np.sum(np.array(w['centers']), axis=1)
+    centers_correct = np.array([2.142001, 2.206456, 2.150292,
+                                2.202362, 2.150252, 2.155269,
+                                2.150239, 2.158398, 2.150542,
+                                2.148504, 2.150135, 2.150721,
+                                2.150219, 2.15022,  2.151031,
+                                2.150383, 2.150193, 2.15023])
+    assert np.allclose(centers, centers_correct)
+    spreads = np.sum(np.array(w['spreads']))
+    assert spreads == pytest.approx(19.69782, abs=0.002)
+
