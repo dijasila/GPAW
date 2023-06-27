220368b0dbe42f7707ad495ac50e340bac5fc2ed
Fredrik Andreas Nilsson <fanni@DTU-FVFGC14Q05G.lan>
2023-06-27 15:34:48 +0200

    flakes

--- gpaw/test/wannier/test_wannier90.py
+++ gpaw/test/wannier/test_wannier90.py
@@ -7,6 +7,7 @@ from gpaw.wannier.w90 import read_wout_all
 from pathlib import Path
 from gpaw.spinorbit import soc_eigenstates
 
+
 @pytest.mark.wannier
 @pytest.mark.serial
 def test_wannier90(gpw_files, in_tmp_dir):
@@ -48,18 +49,18 @@ def test_wannier90_soc(gpw_files, in_tmp_dir):
     assert calc.wfs.kd.nbzkpts == calc.wfs.kd.nibzkpts
     
     w90.write_input(calc,
-                bands=range(9),
-                spinors=True,
-                num_iter=200,
-                dis_num_iter=500,
-                dis_mix_ratio=1.0,
-                seed=seed)
+                    bands=range(9),
+                    spinors=True,
+                    num_iter=200,
+                    dis_num_iter=500,
+                    dis_mix_ratio=1.0,
+                    seed=seed)
     try:
         os.system('wannier90.x -pp ' + seed)
     except FileNotFoundError:
         return  # no wannier90.x executable
     w90.write_projections(calc,
-                      seed=seed, soc=soc)
+                          seed=seed, soc=soc)
     w90.write_eigenvalues(calc, seed=seed, soc=soc)
     w90.write_overlaps(calc, seed=seed, soc=soc)
 
@@ -68,8 +69,7 @@ def test_wannier90_soc(gpw_files, in_tmp_dir):
     with (Path('Fe.wout')).open() as fd:
         w = read_wout_all(fd)
     centers = np.sum(np.array(w['centers']), axis=0)
-    centers_correct = [12.9034  , 12.981219, 12.932828]
+    centers_correct = [12.9034, 12.981219, 12.932828]
     assert np.allclose(centers, centers_correct)
     spreads = np.sum(np.array(w['spreads']))
     assert spreads == pytest.approx(19.69782, abs=0.002)
-
