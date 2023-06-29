b9a376d26d2b71bd9e3345fb6a9e5d6136aa326c
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-06-29 13:34:49 +0200

    Run test in CI

--- gpaw/test/typos/test_typo.py
+++ gpaw/test/typos/test_typo.py
@@ -1,6 +1,8 @@
 from gpaw.lcaotddft.dipolemomentwriter import DipoleMomentWriter
+import pytest
 
 
+@pytest.mark.ci
 def test_typo():
     assert 'xisting' not in DipoleMomentWriter.__doc__.split()
     
