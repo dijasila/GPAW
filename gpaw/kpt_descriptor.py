543db6de1544b72d54df933298b65bf2c26e66ea
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-06-30 14:12:07 +0200

    No threshold

--- gpaw/kpt_descriptor.py
+++ gpaw/kpt_descriptor.py
@@ -145,10 +145,7 @@ class KPointDescriptor:
         self.nbzkpts = len(self.bzk_kc)
 
         # Gamma-point calculation?
-        self.gamma = self.nbzkpts == 1 and np.allclose(self.bzk_kc, 0)
-        if self.gamma:
-            self.bzk_kc[:] = 0.0
-            self.offset_c = np.zeros(3)
+        self.gamma = self.nbzkpts == 1 and not self.bzk_kc.any()
 
         # Point group and time-reversal symmetry neglected:
         self.weight_k = np.ones(self.nbzkpts) / self.nbzkpts
