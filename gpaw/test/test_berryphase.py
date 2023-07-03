d69cda7ab0347e1c15b7b7b4310357a1ab3ee297
Fredrik Andreas Nilsson <fanni@DTU-FVFGC14Q05G.local>
2023-07-03 16:42:02 +0200

    test berryphase as well

--- gpaw/test/test_berryphase.py
+++ gpaw/test/test_berryphase.py
@@ -1,6 +1,7 @@
 import numpy as np
 from gpaw import GPAW
 from gpaw.berryphase import get_polarization_phase, parallel_transport
+from gpaw.berryphase import get_berry_phases
 import gpaw.mpi as mpi
 
 
@@ -43,3 +44,20 @@ def test_parallel_transport(in_tmp_dir, gpw_files):
     # Stest = [0.99938, 0.99874]
     # Sval = [S_km[1, 12], S_km[0, 0]]
     # assert np.allclose(Sval, Stest, atol=1e-3)
+
+
+def test_berry_phases(in_tmp_dir, gpw_files):
+    calc = GPAW(gpw_files['mos2_pw_nosym_wfs'],
+                communicator=mpi.serial_comm)
+    ind, phases = get_berry_phases(calc)
+
+    indtest = [[0, 6, 12, 18, 24, 30],
+               [1, 7, 13, 19, 25, 31],
+               [2, 8, 14, 20, 26, 32],
+               [3, 9, 15, 21, 27, 33],
+               [4, 10, 16, 22, 28, 34],
+               [5, 11, 17, 23, 29, 35]]
+
+    phasetest = [1.66179, 2.54985, 3.10069, 2.54985, 1.66179, 0.92385]
+    assert np.allclose(ind, indtest)
+    assert np.allclose(phases, phasetest, atol=1e-3)
