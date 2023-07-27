fefe0be9b1392156dbd4a3e8340f38d08182df21
giale <gianl.levi@gmail.com>
2023-07-27 15:51:09 +0000

    no need to check for mode in scf_helper

--- gpaw/directmin/scf_helper.py
+++ gpaw/directmin/scf_helper.py
@@ -33,60 +33,59 @@ def do_if_converged(eigensolver_name, wfs, ham, dens, log):
     elif eigensolver_name == 'etdm-fdpw':
         solver = wfs.eigensolver
         occ_name = getattr(wfs.occupations, 'name', None)
-        if wfs.mode == 'fd' or wfs.mode == 'pw':
-            solver.choose_optimal_orbitals(wfs)
-            niter1 = solver.eg_count
-            niter2 = 0
-            niter3 = 0
+        solver.choose_optimal_orbitals(wfs)
+        niter1 = solver.eg_count
+        niter2 = 0
+        niter3 = 0
 
-            iloop1 = solver.iloop is not None
-            iloop2 = solver.outer_iloop is not None
-            if iloop1:
-                niter2 = solver.total_eg_count_iloop
-            if iloop2:
-                niter3 = solver.total_eg_count_outer_iloop
+        iloop1 = solver.iloop is not None
+        iloop2 = solver.outer_iloop is not None
+        if iloop1:
+            niter2 = solver.total_eg_count_iloop
+        if iloop2:
+            niter3 = solver.total_eg_count_outer_iloop
 
-            if iloop1 and iloop2:
-                log(
-                    '\nOccupied states converged after'
-                    ' {:d} KS and {:d} SIC e/g '
-                    'evaluations'.format(niter3,
-                                         niter2 + niter3))
-            elif not iloop1 and iloop2:
-                log(
-                    '\nOccupied states converged after'
-                    ' {:d} e/g evaluations'.format(niter3))
-            elif iloop1 and not iloop2:
-                log(
-                    '\nOccupied states converged after'
-                    ' {:d} KS and {:d} SIC e/g '
-                    'evaluations'.format(niter1, niter2))
-            else:
-                log(
-                    '\nOccupied states converged after'
-                    ' {:d} e/g evaluations'.format(niter1))
-            if solver.converge_unocc:
-                log('Converge unoccupied states:')
-                max_er = wfs.eigensolver.error
-                max_er *= Ha ** 2 / wfs.nvalence
-                solver.run_unocc(ham, wfs, dens, max_er, log)
-            else:
-                solver.initialized = False
-                log('Unoccupied states are not converged.')
-            rewrite_psi = True
-            sic_calc = 'SIC' in solver.func_settings['name']
-            if sic_calc:
-                rewrite_psi = False
-            solver.get_canonical_representation(ham, wfs, rewrite_psi)
+        if iloop1 and iloop2:
+            log(
+                '\nOccupied states converged after'
+                ' {:d} KS and {:d} SIC e/g '
+                'evaluations'.format(niter3,
+                                     niter2 + niter3))
+        elif not iloop1 and iloop2:
+            log(
+                '\nOccupied states converged after'
+                ' {:d} e/g evaluations'.format(niter3))
+        elif iloop1 and not iloop2:
+            log(
+                '\nOccupied states converged after'
+                ' {:d} KS and {:d} SIC e/g '
+                'evaluations'.format(niter1, niter2))
+        else:
+            log(
+                '\nOccupied states converged after'
+                ' {:d} e/g evaluations'.format(niter1))
+        if solver.converge_unocc:
+            log('Converge unoccupied states:')
+            max_er = wfs.eigensolver.error
+            max_er *= Ha ** 2 / wfs.nvalence
+            solver.run_unocc(ham, wfs, dens, max_er, log)
+        else:
+            solver.initialized = False
+            log('Unoccupied states are not converged.')
+        rewrite_psi = True
+        sic_calc = 'SIC' in solver.func_settings['name']
+        if sic_calc:
+            rewrite_psi = False
+        solver.get_canonical_representation(ham, wfs, rewrite_psi)
+        solver._e_entropy = \
+            wfs.calculate_occupation_numbers(dens.fixed)
+        if not sic_calc and occ_name:
+            for kpt in wfs.kpt_u:
+                sort_orbitals_according_to_occ_kpt(wfs, kpt)
             solver._e_entropy = \
                 wfs.calculate_occupation_numbers(dens.fixed)
-            if not sic_calc and occ_name:
-                for kpt in wfs.kpt_u:
-                    sort_orbitals_according_to_occ_kpt(wfs, kpt)
-                solver._e_entropy = \
-                    wfs.calculate_occupation_numbers(dens.fixed)
-            solver.get_energy_and_tangent_gradients(
-                ham, wfs, dens)
+        solver.get_energy_and_tangent_gradients(
+            ham, wfs, dens)
 
 
 def check_eigensolver_state(eigensolver_name, wfs, ham, dens, log):
