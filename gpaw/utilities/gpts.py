258af8257d877c5ea54ee17edffbe30e0727f682
Robert Warmbier <robert.warmbier@gmx.de>
2023-07-28 09:56:46 +0200

    move it all to utilities - not working for some rason

--- gpaw/utilities/gpts.py
+++ gpaw/utilities/gpts.py
@@ -1,6 +1,8 @@
 import warnings
 
 import numpy as np
+from typing import List
+
 from ase.units import Bohr, Hartree
 
 from gpaw.fftw import get_efficient_fft_size
@@ -80,6 +82,11 @@ def get_number_of_grid_points(cell_cv,
 def obtain_gpts_suggestion(cell_cv, ecut, h, print_suggestions=False):
     """Compare PW and LCAO gpts and returns tighter one.
 
+    When two separate simulations using different calculators need to use the
+    same real space grid, like in the electron-phonon coupling case, it might
+    be necessary to specify gpts=(...) explicitly. This tool helps with finding
+    the correct values for a combination of PW and LCAO mode calculations.
+
     Parameters
     ----------
     cell_cv: np.ndarray
@@ -110,3 +117,40 @@ def obtain_gpts_suggestion(cell_cv, ecut, h, print_suggestions=False):
                   "in LCAO calculator.")
 
     return Nopt_c
+
+
+def main(argv: List[str] = None) -> None:
+    import argparse
+    from ase.io import read
+
+    parser = argparse.ArgumentParser(
+        prog='python3 -m gpaw.utilities.gpts',
+        description='Calculate optimal gpts size between PW and LCAO mode.')
+    add = parser.add_argument
+    add('file', metavar='input-file',
+        help='ASE readable structure file.')
+    add('-e', '--ecut', type=float,
+        help='Cutoff "ecut" used in PW mode.')
+    add('-g', '--grid-spacing', type=float,
+        help='Maximal grid spacing "h" used in LCAO mode.')
+    add('-s', '--super-cell', default='1,1,1',
+        help='Supercell size.  Example: "-s 2,2,2".')
+
+    args = parser.parse_intermixed_args(argv)
+    if not args.ecut or not args.grid_spacing:
+        parser.print_help()
+        raise SystemExit
+
+    sc = []
+    for value in args.super_cell.split(','):
+        sc.append(int(value))
+
+    atoms = read(args.file)
+    atoms_sc = atoms * sc
+    cell_cv = atoms_sc.get_cell()
+    obtain_gpts_suggestion(cell_cv, args.ecut, args.grid_spacing, True)
+
+
+if __name__ == "__main__":
+    main()
+
