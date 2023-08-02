93019001525a1d2df8cbdd5d834e9ea2b3f39639
Martin Ovesen <martin.ovesen@outlook.dk>
2023-08-02 14:45:33 +0200

    Add test

---
+++ gpaw/test/noncollinear/test_Ga.py
@@ -0,0 +1,24 @@
+import pytest
+
+import numpy as np
+from ase import Atoms
+
+from gpaw.new.ase_interface import GPAW
+from gpaw.spinorbit import soc_eigenstates
+
+
+def test_noncollinear_Ga():
+
+    # Parameters
+
+    atom = Atoms('Ga', positions = [[3, 3, 3]],
+                cell = [6, 6, 6], pbc = True)
+
+    calc = GPAW(mode = {'name': 'pw', 'ecut':  400}, xc = 'LDA', nbands = 13,
+                kpts = {'size': (1, 1, 1), 'gamma': True}, symmetry = 'off',
+                soc = True, magmoms = [[0, 0, 1]], dtype = complex)
+
+    atom.calc = calc
+    e = atom.get_potential_energy()
+
+    assert e == pytest.approx(-0.0779024429, abs=1.0e-6)
