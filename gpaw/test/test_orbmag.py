977cdf5b402499c215ec42faa7844816b4741b53
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-08-03 22:08:01 +0200

    try to fix weird misbehaviours

--- gpaw/test/test_orbmag.py
+++ gpaw/test/test_orbmag.py
@@ -6,7 +6,7 @@ from gpaw.new.ase_interface import GPAW
 from gpaw.spinorbit import soc_eigenstates
 
 
-@pytest.mark.xfail('something with gpaw new?')
+@pytest.mark.skip(reason='something with gpaw new?')
 def test_orbmag_Ni(gpw_files):
 
     # Parameters
