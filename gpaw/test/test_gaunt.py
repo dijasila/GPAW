0b6263b53ad42e9d57e1ded601f3d291874b00fc
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-08-08 14:15:59 +0200

    test gaunt coefficient permutation symmetry

--- gpaw/test/test_gaunt.py
+++ gpaw/test/test_gaunt.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import permutations
 
 from gpaw.gaunt import gaunt
 
@@ -25,6 +26,21 @@ def test_selection_rules(lmax: int = 3):
                         m1 + m2 - m3 == 0 or \
                         m3 + m1 - m2 == 0 or \
                         m2 + m3 - m1 == 0, f'{m1, m2, m3}'
+
+
+def test_permutation_symmetry(lmax: int = 3):
+    """Test that the Gaunt coefficients are permutationally invariant"""
+    G_LLL = gaunt(lmax)
+    Lmax = G_LLL.shape[0]
+    L2max = G_LLL.shape[1]
+    for L1 in range(Lmax):
+        for L2 in range(Lmax):
+            for L3 in range(Lmax):
+                for Lp1, Lp2, Lp3 in permutations([L1, L2, L3]):
+                    abs(G_LLL[L1, L2, L3] - G_LLL[Lp1, Lp2, Lp3]) < 1e-10
+        for L2 in range(L2max):
+            for L3 in range(L2max):
+                abs(G_LLL[L1, L2, L3] - G_LLL[L1, L3, L2]) < 1e-10
 
 
 def lm_indices(L):
