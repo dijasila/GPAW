1e25caf8c4653b542cc08c24c5fb00b44d851383
Thorbjørn Skovhus <s134264@student.dtu.dk>
2023-08-15 14:26:23 +0200

    cache and reuse the pair density paw corrections

--- gpaw/test/response/test_chiks.py
+++ gpaw/test/response/test_chiks.py
@@ -244,6 +244,9 @@ def test_chiks(in_tmp_dir, gpw_files, system, qrel, gammacentered):
         cross_tabulation=dict(distribution=distribution_d,
                               nblocks=nblocks_n))
 
+    # Make it possible to check timings for the test
+    chiks_testing_factory.context.write_timer()
+
 
 @pytest.mark.response
 @pytest.mark.parametrize(
@@ -300,6 +303,9 @@ def test_chiks_vs_chi0(in_tmp_dir, gpw_files, system, qrel):
     # Part 3: Check chiks vs. chi0
     assert chiks.array == pytest.approx(chi0_wGG, rel=1e-3, abs=1e-5)
 
+    # Make it possible to check timings for the test
+    context.write_timer()
+
 
 # ---------- Test functionality ---------- #
 
@@ -311,6 +317,7 @@ class ChiKSTestingFactory:
                  spincomponent, q_c, zd,
                  nbands, ecut, gammacentered):
         self.gs = GSAdapterWithPAWCache(calc)
+        self.context = ResponseContext()
         self.spincomponent = spincomponent
         self.q_c = q_c
         self.zd = zd
@@ -334,7 +341,8 @@ class ChiKSTestingFactory:
             return self.cached_chiks[cache_string]
 
         chiks_calc = ChiKSCalculator(
-            self.gs, ecut=self.ecut, nbands=self.nbands,
+            self.gs, context=self.context,
+            ecut=self.ecut, nbands=self.nbands,
             gammacentered=self.gammacentered,
             disable_time_reversal=disable_syms,
             disable_point_group=disable_syms,
@@ -394,7 +402,7 @@ class GSAdapterWithPAWCache(ResponseGroundStateAdapter):
     """Add a PAW correction cache to the ground state adapter.
 
     WARNING: Use with care! The cache is only valid, when the plane-wave
-    representations are identical.
+    representations are identical and the functional f[n](r) is not changed.
     """
 
     def __init__(self, calc):
@@ -403,16 +411,16 @@ class GSAdapterWithPAWCache(ResponseGroundStateAdapter):
         self._cached_corrections = []
         self._cached_parameters = []
 
-    def pair_density_paw_corrections(self, qpd):
+    def matrix_element_paw_corrections(self, qpd, rshe_a):
         """Overwrite method with a cached version."""
         cache_index = self._cache_lookup(qpd)
         if cache_index is not None:
             return self._cached_corrections[cache_index]
 
-        return self._calculate_correction(qpd)
+        return self._calculate_correction(qpd, rshe_a)
 
-    def _calculate_correction(self, qpd):
-        correction = super().pair_density_paw_corrections(qpd)
+    def _calculate_correction(self, qpd, rshe_a):
+        correction = super().matrix_element_paw_corrections(qpd, rshe_a)
 
         self._cached_corrections.append(correction)
         self._cached_parameters.append((qpd.q_c, qpd.ecut, qpd.gammacentered))
