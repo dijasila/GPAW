80935371daf04f785f1330bf57ddf1dbf545eaf3
Jakob Schiotz <schiotz@fysik.dtu.dk>
2023-08-22 11:21:12 +0200

    Stop blocking Intel due to bug #241 as that is fixed in EasyBuild (I hope)

--- doc/platforms/Linux/Niflheim/gpaw_venv.py
+++ doc/platforms/Linux/Niflheim/gpaw_venv.py
@@ -96,8 +96,8 @@ def main():
                         'exising venv.')
     args = parser.parse_args()
 
-    if args.toolchain == 'intel':
-        raise ValueError('See: https://gitlab.com/gpaw/gpaw/-/issues/241')
+    #if args.toolchain == 'intel':
+    #    raise ValueError('See: https://gitlab.com/gpaw/gpaw/-/issues/241')
 
     venv = Path(args.venv).absolute()
     activate = venv / 'bin/activate'
