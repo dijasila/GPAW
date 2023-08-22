3a98d5853e94456cba79ecd1f603c41d477940fc
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-08-22 11:07:11 +0200

    Read old gpw-files

--- gpaw/new/gpw.py
+++ gpaw/new/gpw.py
@@ -146,7 +146,6 @@ def read_gpw(filename: Union[str, Path, IO[str]],
     if comm.rank == 0:
         nt_sR_array = reader.density.density * bohr**3
         vt_sR_array = reader.hamiltonian.potential / ha
-        vHt_x_array = reader.hamiltonian.electrostatic_potential / ha
         if builder.xc.type == 'MGGA':
             taut_sR_array = reader.density.ked * (bohr**3 / ha)
             dedtaut_sR_array = reader.hamiltonian.mgga_potential * bohr**-3
@@ -156,7 +155,6 @@ def read_gpw(filename: Union[str, Path, IO[str]],
     else:
         nt_sR_array = None
         vt_sR_array = None
-        vHt_x_array = None
         taut_sR = None
         dedtaut_sR = None
         D_sap_array = None
@@ -178,7 +176,6 @@ def read_gpw(filename: Union[str, Path, IO[str]],
 
     nt_sR = builder.grid.empty(builder.ncomponents)
     vt_sR = builder.grid.empty(builder.ncomponents)
-    vHt_x = builder.electrostatic_potential_desc.empty()
     if builder.xc.type == 'MGGA':
         taut_sR = builder.grid.empty(builder.ncomponents)
         dedtaut_sR = builder.grid.empty(builder.ncomponents)
@@ -196,7 +193,6 @@ def read_gpw(filename: Union[str, Path, IO[str]],
     if kpt_band_comm.rank == 0:
         nt_sR.scatter_from(nt_sR_array)
         vt_sR.scatter_from(vt_sR_array)
-        vHt_x.scatter_from(vHt_x_array)
         if builder.xc.type == 'MGGA':
             taut_sR.scatter_from(taut_sR_array)
             dedtaut_sR.scatter_from(dedtaut_sR_array)
@@ -209,12 +205,23 @@ def read_gpw(filename: Union[str, Path, IO[str]],
 
     kpt_band_comm.broadcast(nt_sR.data, 0)
     kpt_band_comm.broadcast(vt_sR.data, 0)
-    kpt_band_comm.broadcast(vHt_x.data, 0)
     if builder.xc.type == 'MGGA':
         kpt_band_comm.broadcast(taut_sR.data, 0)
         kpt_band_comm.broadcast(dedtaut_sR.data, 0)
     kpt_band_comm.broadcast(D_asp.data, 0)
     kpt_band_comm.broadcast(dH_asp.data, 0)
+
+    if reader.version >= 4:
+        if comm.rank == 0:
+            vHt_x_array = reader.hamiltonian.electrostatic_potential / ha
+        else:
+            vHt_x_array = None
+        vHt_x = builder.electrostatic_potential_desc.empty()
+        if kpt_band_comm.rank == 0:
+            vHt_x.scatter_from(vHt_x_array)
+        kpt_band_comm.broadcast(vHt_x.data, 0)
+    else:
+        vHt_x = None
 
     density = Density.from_data_and_setups(
         nt_sR, taut_sR, D_asp.to_full(),
