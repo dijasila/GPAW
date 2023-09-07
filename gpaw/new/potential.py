4fe0c4910db72f8213209e20d2077461b7164bfa
Mikael Kuisma <kuisma@dtu.dk>
2023-09-07 15:11:21 +0300

    remove debug

--- gpaw/new/potential.py
+++ gpaw/new/potential.py
@@ -48,24 +48,18 @@ class Potential:
                 assert ni_a.flags.c_contiguous
                 assert P_ani.data.flags.c_contiguous
                 assert out_ani.data.flags.c_contiguous
-                print(type(self.dH_asii.data[spin]))
-                print(type(P_ani.data))
-                print(type(out_ani.data))
-                #for a, P_ni in out_ani.items():
-                #    out_ani[a][:] = a
-                #self.dH_asii.data[spin][:] = 0.0
                 ni_a = xp.asarray(ni_a, dtype=xp.int32)
                 xp.cuda.runtime.deviceSynchronize()
                 _gpaw.dH_aii_times_P_ani_gpu(self.dH_asii.data[spin], ni_a,
                                              P_ani.data, out_ani.data)
 
-                out2_ani = out_ani.new()
+                """out2_ani = out_ani.new()
                 for (a, P_ni), out_ni in zips(P_ani.items(), out2_ani.values()):
                     dH_ii = self.dH_asii[a][spin]
                     out2_ani[a][:] = xp.einsum('ni, ij -> nj', P_ni, dH_ii)
                 print('out_ani', out_ani.data)
                 print('out2_ani', out2_ani.data)
-                assert xp.allclose(out_ani.data, out2_ani.data)
+                assert xp.allclose(out_ani.data, out2_ani.data)"""
             return
 
         # Non-collinear wave functions:
