8fa9cf63b489169d5cb5c217d707c5992eda6ee3
Mikael Kuisma <kuisma@dtu.dk>
2023-09-13 22:42:52 +0200

    Use sparse matrices

--- gpaw/new/symmetry.py
+++ gpaw/new/symmetry.py
@@ -7,6 +7,7 @@ from gpaw.new.brillouin import IBZ, BZPoints
 from gpaw.rotation import rotation
 from gpaw.symmetry import Symmetry as OldSymmetry
 
+
 class SymmetrizationPlan:
     def __init__(self, xp, rotations, a_sa, l_aj, layout):
         if xp is np:
@@ -33,21 +34,14 @@ class SymmetrizationPlan:
             a = coset[0]  # Representative atom for coset
             R_sii = xp.asarray(rotations(l_aj[a], xp)) / ns**0.5
             i2 = R_sii.shape[1]**2
-            for R_ii in R_sii:
-                print(type(R_ii), R_ii)
-            print(xp)
-            R_sii = [sparse.csc_matrix(R_ii) for R_ii in R_sii]
+            R_sii = [sparse.csr_matrix(R_ii) for R_ii in R_sii]
             # The atomic density matrices transform as
             # ρ'_ii = R_sii ρ_ii R^T_sii
             # Which equals to vec(ρ'_ii) = (R^s_ii ⊗  R^s_ii) vec(ρ_ii)
             # Here we to the Kronecker product for each of the
             # symmetry transformations.
             R_sPP = [sparse.kron(R_ii, R_ii) for R_ii in R_sii]
-            #R_sPP = xp.einsum('sab,scd->sacbd', R_sii, R_sii)
-            #R_sPP = R_sPP.reshape((ns, i2, i2)) / ns
-            S_ZZ = sparse.csc_matrix(xp.zeros((nA * i2,) * 2))
-            #print(xp.count_nonzero(R_sPP) / R_sPP.size * 100,
-            #      '% of non zeros R_sPP')
+            S_ZZ = sparse.csr_matrix(xp.zeros((nA * i2,) * 2))
 
             # For each orbit, the symetrization operation is represented by
             # a full matrix operating on a subset of indices to the full array.
@@ -59,8 +53,6 @@ class SymmetrizationPlan:
                     Z3 = loca2 * i2
                     Z4 = Z3 + i2
                     S_ZZ[Z1:Z2, Z3:Z4] += R_sPP[s]
-            #print(xp.count_nonzero(S_ZZ) / S_ZZ.size * 100,
-            #      '% of non zeros S_ZZ')
             S_aZZ[a] = S_ZZ
             indices = []
             for loca1, a1 in enumerate(coset):
