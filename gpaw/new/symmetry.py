8273c6b46c9fffce5d68009b460b9d7ad4a30709
Mikael Kuisma <kuisma@dtu.dk>
2023-09-14 00:08:22 +0300

    Use sparse wisely

--- gpaw/new/symmetry.py
+++ gpaw/new/symmetry.py
@@ -10,15 +10,15 @@ from gpaw.symmetry import Symmetry as OldSymmetry
 
 class SymmetrizationPlan:
     def __init__(self, xp, rotations, a_sa, l_aj, layout):
+        ns = a_sa.shape[0]  # Number of symmetries
+        na = a_sa.shape[1]  # Number of atoms
+
         if xp is np:
             import scipy
             sparse = scipy.sparse
         else:
             from gpaw.gpu import cupyx
             sparse = cupyx.scipy.sparse
-        
-        ns = a_sa.shape[0]  # Number of symmetries
-        na = a_sa.shape[1]  # Number of atoms
 
         # Find orbits, i.e. point group action,
         # which also equals to set of all cosets.
@@ -32,16 +32,18 @@ class SymmetrizationPlan:
         for coset in map(list, cosets):
             nA = len(coset)  # Number of atoms in this orbit
             a = coset[0]  # Representative atom for coset
-            R_sii = xp.asarray(rotations(l_aj[a], xp)) / ns**0.5
-            i2 = R_sii.shape[1]**2
-            R_sii = [sparse.csr_matrix(R_ii) for R_ii in R_sii]
+
             # The atomic density matrices transform as
             # ρ'_ii = R_sii ρ_ii R^T_sii
             # Which equals to vec(ρ'_ii) = (R^s_ii ⊗  R^s_ii) vec(ρ_ii)
             # Here we to the Kronecker product for each of the
             # symmetry transformations.
-            R_sPP = [sparse.kron(R_ii, R_ii) for R_ii in R_sii]
-            S_ZZ = sparse.csr_matrix(xp.zeros((nA * i2,) * 2))
+            R_sii = xp.asarray(rotations(l_aj[a], xp))
+            i2 = R_sii.shape[1]**2
+            R_sPP = xp.einsum('sab,scd->sacbd', R_sii, R_sii)
+            R_sPP = R_sPP.reshape((ns, i2, i2)) / ns
+
+            S_ZZ = xp.zeros((nA * i2,) * 2)
 
             # For each orbit, the symetrization operation is represented by
             # a full matrix operating on a subset of indices to the full array.
@@ -53,6 +55,10 @@ class SymmetrizationPlan:
                     Z3 = loca2 * i2
                     Z4 = Z3 + i2
                     S_ZZ[Z1:Z2, Z3:Z4] += R_sPP[s]
+            # Utilize sparse matrices if sizes get out of hand
+            # Limit is hard coded to 100MB per orbit
+            if S_ZZ.nbytes > 100*1024**2:
+                S_ZZ = sparse.csr_matrix(S_ZZ)
             S_aZZ[a] = S_ZZ
             indices = []
             for loca1, a1 in enumerate(coset):
