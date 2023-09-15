ab2d8423f77cd41e33c47ee234735a10638ee482
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-09-15 12:10:56 +0200

    Clean up siteconfig.py

--- doc/platforms/Cray/siteconfig-lumi-gpu.py
+++ doc/platforms/Cray/siteconfig-lumi-gpu.py
@@ -1,6 +1,4 @@
-# Custom GPAW siteconfig for LUMI-G
-
-parallel_python_interpreter = True
+"""Custom GPAW siteconfig for LUMI-G."""
 
 mpi = True
 compiler = 'cc'
@@ -16,9 +14,8 @@ extra_compile_args = [
     '-fPIC',
     '-Wall',
     '-Wno-stringop-overflow',  # suppress warnings from MPI_STATUSES_IGNORE
-    #'-DNDEBUG',
-    '-g',
-    ]
+    '-DNDEBUG',
+    '-g']
 extra_link_args = ['-fopenmp']
 
 # hip
@@ -28,7 +25,7 @@ gpu_compiler = 'hipcc'
 gpu_include_dirs = []
 gpu_compile_args = ['--offload-arch=gfx90a', '-O3', '-g']
 libraries += ['amdhip64', 'hipblas']
-define_macros += [('GPAW_GPU_AWARE_MPI', None)]
+# define_macros += [('GPAW_GPU_AWARE_MPI', None)]
 
 # ELPA
 elpa = True
@@ -44,9 +41,5 @@ scalapack = True
 
 # Libxc
 libraries += ['xc']
-#dpath = '/appl/lumi/spack/22.08/0.18.1/opt/spack/libxc-5.1.7-c55ppsw'
-#include_dirs += [f'{dpath}/include']
-#library_dirs += [f'{dpath}/lib']
-#extra_link_args += [f'-Wl,-rpath,{dpath}/lib']
 
 define_macros += [('GPAW_ASYNC', 1)]
