100c33d57d5e2dbb4bd250c9a8f09f9f7684ea93
tboland1 <tmabo@dtu.dk>
2023-09-25 15:41:21 +0200

    flake8

--- gpaw/test/response/test_gw_restart.py
+++ gpaw/test/response/test_gw_restart.py
@@ -1,7 +1,7 @@
-import pytest
-import numpy as np
 from gpaw.response.g0w0 import G0W0
-from gpaw.mpi import world
+from gpaw import GPAW, PW
+from ase import Atoms
+
 
 class FragileG0W0(G0W0):
     def calculate_q(self, *args, **kwargs):
@@ -12,10 +12,10 @@ class FragileG0W0(G0W0):
             raise ValueError('Cthulhu awakens')
         G0W0.calculate_q(self, *args, **kwargs)
 
-from gpaw import GPAW, PW
-from ase import Atoms
-atoms = Atoms('He', cell=[3,3,3], pbc=1)
-calc = GPAW(mode=PW(ecut=150), kpts=dict(size=[2,2,2], gamma=True), nbands=10)
+
+atoms = Atoms('He', cell=[3, 3, 3], pbc=1)
+calc = GPAW(mode=PW(ecut=150), kpts=dict(size=[2, 2, 2], gamma=True),
+            nbands=10)
 atoms.calc = calc
 atoms.get_potential_energy()
 calc.write('wfs.gpw', mode='all')
@@ -32,11 +32,9 @@ gw.calculate()
 # # Do it two calls to calculate
 # while True:
 #     try:
-#         gw = FragileG0W0('wfs.gpw', 'restarted',  **kwargs)
+#         gw = FragileG0W0('wfs.gpw', 'restarted', **kwargs)
 #         gw.calculate()
 #         break
 #     except ValueError as e:
 #         print(e)
 #         assert str(e) == 'Cthulhu awakens'
-
-
