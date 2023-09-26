6bf677dee20bcb1c3478bc81f834a9cf9b7a591d
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-09-26 10:14:41 +0200

    nitpick

--- gpaw/response/kpoints.py
+++ gpaw/response/kpoints.py
@@ -5,4 +5,4 @@ class ResponseKPointGrid:
     def __init__(self, icell_cv, bzk_kc):
         self.icell_cv = icell_cv
         self.bzk_kc = bzk_kc
-        self.bzk_kv = np.dot(bzk_kc, icell_cv * 2 * np.pi)
+        self.bzk_kv = bzk_kc @ (2 * np.pi * icell_cv)
