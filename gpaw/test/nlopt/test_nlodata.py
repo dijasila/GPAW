44f9a11f0ce8d61d01126ee599740e427a4b0c66
Martin Ovesen <martin.ovesen@outlook.dk>
2023-09-27 18:57:49 +0200

    Internal communicator on NLOData, remove importing mpi.world

--- gpaw/test/nlopt/test_nlodata.py
+++ gpaw/test/nlopt/test_nlodata.py
@@ -5,20 +5,26 @@ from gpaw.mpi import world
 from gpaw.nlopt.basic import NLOData
 
 
+def generate_testing_data(ns, nk, nb, seed=42):
+    rng = np.random.default_rng(seed=42)
+
+    w_sk = rng.random((ns, nk))
+    f_skn = rng.random((ns, nk, nb))
+    E_skn = rng.random((ns, nk, nb))
+    p_skvnn = rng.random((ns, nk, 3, nb, nb)) \
+        + 1j * rng.random((ns, nk, 3, nb, nb))
+
+    return w_sk, f_skn, E_skn, p_skvnn
+
+
 @pytest.mark.skipif(world.size > 1, reason='Serial only')
 def test_write_load_serial(in_tmp_dir):
-    rng = np.random.default_rng(seed=42)
-    w_sk = rng.random((1, 8))
-    f_skn = rng.random((1, 8, 20))
-    E_skn = rng.random((1, 8, 20))
-    p_skvnn = rng.random((1, 8, 3, 20, 20)) \
-        + 1j * rng.random((1, 8, 3, 20, 20))
+    w_sk, f_skn, E_skn, p_skvnn = generate_testing_data(1, 8, 20)
 
-    nlo = NLOData(w_sk, f_skn, E_skn, p_skvnn)
+    nlo = NLOData(w_sk, f_skn, E_skn, p_skvnn, world)
     nlo.write('nlodata.npz')
-    del nlo
 
-    newdata = NLOData.load('nlodata.npz')
+    newdata = NLOData.load('nlodata.npz', world)
     assert np.all(newdata.w_sk == w_sk)
     assert np.all(newdata.f_skn == f_skn)
     assert np.all(newdata.E_skn == E_skn)
@@ -27,26 +33,19 @@ def test_write_load_serial(in_tmp_dir):
 
 def test_serial_file_parallel_data(in_tmp_dir):
     # Random data only on rank = 0
-    rng = np.random.default_rng(seed=42)
     if world.rank == 0:
-        w_sk = rng.random((1, 8))
-        f_skn = rng.random((1, 8, 20))
-        E_skn = rng.random((1, 8, 20))
-        p_skvnn = rng.random((1, 8, 3, 20, 20)) \
-            + 1j * rng.random((1, 8, 3, 20, 20))
+        w_sk, f_skn, E_skn, p_skvnn = generate_testing_data(1, 8, 20)
     else:
         w_sk = None
         f_skn = None
         E_skn = None
         p_skvnn = None
 
-    nlo = NLOData(w_sk, f_skn, E_skn, p_skvnn)
+    nlo = NLOData(w_sk, f_skn, E_skn, p_skvnn, world)
     nlo.write('nlodata.npz')
     k_info_original = nlo.distribute()
 
-    del nlo
-
-    newdata = NLOData.load('nlodata.npz')
+    newdata = NLOData.load('nlodata.npz', world)
     k_info = newdata.distribute()
     for (k, data), (_, original_data) in zip(k_info.items(),
                                              k_info_original.items()):
@@ -58,18 +57,12 @@ def test_serial_file_parallel_data(in_tmp_dir):
 
 def test_write_load_parallel(in_tmp_dir):
     # Same random data array on each core
-    rng = np.random.default_rng(seed=42)
-    w_sk = rng.random((1, 8))
-    f_skn = rng.random((1, 8, 20))
-    E_skn = rng.random((1, 8, 20))
-    p_skvnn = rng.random((1, 8, 3, 20, 20)) \
-        + 1j * rng.random((1, 8, 3, 20, 20))
+    w_sk, f_skn, E_skn, p_skvnn = generate_testing_data(1, 8, 20)
 
-    nlo = NLOData(w_sk, f_skn, E_skn, p_skvnn)
+    nlo = NLOData(w_sk, f_skn, E_skn, p_skvnn, world)
     nlo.write('nlodata.npz')
-    del nlo
 
-    newdata = NLOData.load('nlodata.npz')
+    newdata = NLOData.load('nlodata.npz', world)
     k_info = newdata.distribute()
 
     # Compare the distributed data with original data
