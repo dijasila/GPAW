bca0509bfd7751af6de2064fc4e8890513888f3b
Martin Ovesen <martin.ovesen@outlook.dk>
2023-09-28 20:50:23 +0200

    Remove hbn test from shg testing suite

--- gpaw/test/nlopt/test_shg.py
+++ gpaw/test/nlopt/test_shg.py
@@ -82,36 +82,3 @@ def test_shg_spinpol(gpw_files, in_tmp_dir):
         np.max(np.abs(shg_xyz_rerr_real))
     assert shg_xyz_rerr_imag == pytest.approx(0, abs=2e-2), \
         np.max(np.abs(shg_xyz_rerr_imag))
-
-
-def test_shg_hBN(gpw_files, in_tmp_dir):
-    # SHG spectrum of h-BN as previously calculated.
-    # We should be aware of any changes to this.
-    shg_values = np.array([-498.32624277 - 69.02559975j,
-                           -652.74329198 - 122.25657802j,
-                           -939.87916222 - 269.17682137j,
-                           -1539.46588908 - 919.4495916j,
-                           391.40378777 - 3400.02524452j,
-                           1408.14833639 - 749.79781681j,
-                           859.34871229 - 237.75920108j,
-                           597.45469429 - 112.27235265j,
-                           453.42533737 - 64.8513409j,
-                           363.29204261 - 42.20753326j])
-    freqs = np.linspace(2, 2.4, 10)
-
-    # Get nlodata from pre-calculated SiC fixtures
-    calc = gpw_files['hbn_pw_nopg']
-    make_nlodata(calc, out_name='mml.npz')
-    world.barrier()
-
-    # Calculate 'xyz' tensor element of SHG spectra
-    get_shg(freqs=freqs, eta=0.025, pol='xyz',
-            out_name='shg_xyz.npy',
-            mml_name='mml.npz')
-    world.barrier()
-
-    # Load the calculated SHG spectra (in units of nm/V)
-    shg_xyz = np.load('shg_xyz.npy')[1] * 1e15
-
-    assert shg_xyz.real == pytest.approx(shg_values.real, abs=1e-4)
-    assert shg_xyz.imag == pytest.approx(shg_values.imag, abs=1e-4)
