773ea136d733bbbe1bb88acdf01b357ae557efed
Dario A. Leon <dalv.cuba@gmail.com>
2023-10-01 09:01:34 +0200

    partial commit

--- gpaw/response/mpa_sampling.py
+++ gpaw/response/mpa_sampling.py
@@ -1,29 +1,14 @@
-# -------------------------------------------------------------------
-# Pair sampling of a positive unidimentional (frequency) domain as a
-# function of a scale parameter and the exponent of the distribution
-#
-#                               to be used in the MPA interpolation*
-#
-# *DA. Leon et al, PRB 104, 115157 (2021)
-#
-# Notes:
-#
-#   1) Homogeneous (homo)
-#   2) Linear Partition Pair Sampling (lPPS)
-#   3) Quadratic Partition Pair Sampling (qPPS)
-#   4) Cubic Partition Pair Sampling (cPPS)
-#   5) Alpha Partition Pair Sampling (aPPS)
-#
-#   The samplings do not depend on the sampled function
-# -------------------------------------------------------------------
-
 import numpy as np
-# from cmath import *
 
 
-def mpa_frequency_sampling(npol, w0, d, ps='2l', alpha=1):  # , w_grid
+def mpa_frequency_sampling(npol, w0, d, ps='2l', alpha=1):
     """
-    This function creates a frequency grid in the complex plane
+    This function creates a frequency grid in the complex plane.
+    The grid can have 1 or 2 branches with points non homogeneously
+    distributed along the real frequency axis.
+    See Fig. 1 and Eq. (18) of Ref. [1], and Eq. (11) of Ref. [2]
+    [1] DA. Leon et al, PRB 104, 115157 (2021)
+    [2] DA. Leon et al, PRB 107, 155130 (2023)
 
     Parameters
     ----------
@@ -32,11 +17,21 @@ def mpa_frequency_sampling(npol, w0, d, ps='2l', alpha=1):  # , w_grid
     d : array of two real numbers defining the damping range
     ps : string of length 2 defining a sampling with 1 or 2 lines
     alpha : exponent of the distribution of points along the real axis
+    ______________________________________________________________________
+                  Example: double parallel sampling with 9 poles
+    ----------------------------------------------------------------------
+                            complex frequency plane
+    ----------------------------------------------------------------------
+    |(w0[0].real, w0[0].imag) .. . . . . . .   . (w0[1].real, w0[1].imag)|
+    |                                                                    |
+    |     (w0[0].real, d[0])  .. . . . . . .   . (w0[1].real, d[1])      |
+    ______________________________________________________________________
     """
  
     w0 = np.array(w0)
     assert (w0.real >= 0).all()
     assert (w0.imag >= 0).all()
+    assert w0[1].real >= w0[0].real # positive interval
 
     if npol == 1:
         w_grid = np.array(w0, dtype=complex)
@@ -48,23 +43,24 @@ def mpa_frequency_sampling(npol, w0, d, ps='2l', alpha=1):  # , w_grid
         raise ValueError("If ps = '1l', only alpha = 0 is implemented")
 
     if ps == '2l':
-        if alpha == 0:
+        assert (w0.imag > d).all() # two branches
+        if alpha == 0: # homogeneous distribution
             w_grid = np.concatenate((np.linspace(complex(np.real(w0[0]),
                                      d[1]), complex(np.real(w0[1]), d[1]),
                                     npol), np.linspace(w0[0], w0[1],
                                                        npol)))
             w_grid[0] = complex(np.real(w0[0]), d[0])
             return w_grid
-
-        ws = w0[1] - w0[0]
-        w_grid = np.ones(2 * npol, dtype=complex)
+        # inhomogeneous distribution in powers of 1/2^alpha
+        ws = w0[1] - w0[0] # complex length
+        w_grid = np.ones(2 * npol, dtype=complex) # number of freqs = 2 npol
         w_grid[0] = complex(np.real(w0[0]), d[0])
         w_grid[npol - 1] = complex(np.real(w0[1]), d[1])
         w_grid[npol] = w0[0]
         w_grid[2 * npol - 1] = w0[1]
         lp = int(np.log(npol - 1) / np.log(2))
         r = int((npol - 1) % (2**lp))
-        # print(r)
+
         if r > 0:
             for i in range(1, 2 * r):
                 w_grid[npol + i] = w0[0] + ws * (i / 2.**(lp + 1)
