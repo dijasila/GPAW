8669e21288bc3c01c65d0f0a566f0c8721ec159d
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-10-06 11:56:51 +0200

    No need for txt=None when reading a gpw-file

--- gpaw/test/rpa/test_rpa_energy_Na.py
+++ gpaw/test/rpa/test_rpa_energy_Na.py
@@ -8,7 +8,7 @@ from gpaw.test import equal
 @pytest.mark.rpa
 @pytest.mark.response
 def test_rpa_rpa_energy_Na(in_tmp_dir, gpw_files):
-    calc = GPAW(gpw_files['na_pw'], communicator=serial_comm, txt=None)
+    calc = GPAW(gpw_files['na_pw'], communicator=serial_comm)
     ecut = 120
     rpa = RPACorrelation(calc, txt=f'rpa_{ecut}s.txt', ecut=[ecut])
     E = rpa.calculate()
