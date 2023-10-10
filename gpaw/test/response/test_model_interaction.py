af766fbd4ee7f9a50c3168b45e287970a9f9bf61
Fredrik Andreas Nilsson <fanni@DTU-FVFGC14Q05G.lan>
2023-10-10 14:13:15 +0200

    tests pass

--- gpaw/test/response/test_model_interaction.py
+++ gpaw/test/response/test_model_interaction.py
@@ -1,18 +1,44 @@
 import numpy as np
 import pytest
-from gpaw import GPAW
+from gpaw import GPAW, PW, FermiDirac, Mixer
 from gpaw.response.modelinteraction import initialize_w_model
 from gpaw.response.chi0 import Chi0
 from gpaw.wannier90 import Wannier90
 import os
+from ase.build import bulk
+from ase import Atoms
 
 @pytest.mark.serial  # W90 does not work in parallel
 @pytest.mark.parametrize('symm', [True, False])
 def test_w90(in_tmp_dir, gpw_files, symm):
+    k = 4
+    cell = bulk('Ga', 'fcc', a=5.68).cell
+    a = Atoms('GaAs', cell=cell, pbc=True,
+              scaled_positions=((0, 0, 0), (0.25, 0.25, 0.25)))
+
     if symm:
-            calc = GPAW(gpw_files['gaas_pw'])
+        #calc = GPAW(gpw_files['gaas_pw'])
+        calc = GPAW(mode=PW(200),
+                    xc='LDA',
+                    occupations=FermiDirac(width=0.01),
+                    convergence={'bands': -1},
+                    kpts={'size': (k, k, k), 'gamma': True},
+                    txt='gs_GaAs.txt')
+
     else:
-        calc = GPAW(gpw_files['gaas_pw_nosym'])
+        #calc = GPAW(gpw_files['gaas_pw_nosym'])
+        calc = GPAW(mode=PW(200),
+                    xc='LDA',
+                    occupations=FermiDirac(width=0.01),
+                    convergence={'bands': -1},
+                    symmetry='off',
+                    kpts={'size': (k, k, k), 'gamma': True},
+                    txt='gs_GaAs_symmOff.txt')
+
+    a.calc = calc
+    a.get_potential_energy()
+    calc.write('GaAs.gpw', mode='all')
+
     
     seed = 'GaAs'
     w90 = Wannier90(calc, orbitals_ai=[[], [0, 1, 2, 3]],
@@ -38,9 +64,9 @@ def test_w90(in_tmp_dir, gpw_files, symm):
     Wm = initialize_w_model(calc, chi0calc)
     Wwann = Wm.calc_in_Wannier(chi0calc,Uwan=seed,bandrange=[0,4])
 
-    assert Wwann[0, 0, 0, 0, 0] == pytest.approx(1.2308, abs=0.002)
-    assert Wwann[0, 1, 1, 1, 1] == pytest.approx(0.8031, abs=0.002)
-    assert Wwann[0, 2, 2, 2, 2] == pytest.approx(0.8031, abs=0.002)
-    assert Wwann[0, 3, 3, 3, 3] == pytest.approx(0.8031, abs=0.002)
-    assert Wwann[0, 3, 3, 0, 0] == pytest.approx(0.4327, abs=0.002)
-    assert Wwann[0, 3, 0, 3, 0] == pytest.approx(0.8314, abs=0.002)
+    assert Wwann[0, 0, 0, 0, 0] == pytest.approx(1.13031, abs=0.003)
+    assert Wwann[0, 1, 1, 1, 1] == pytest.approx(0.80556, abs=0.003)
+    assert Wwann[0, 2, 2, 2, 2] == pytest.approx(0.80556, abs=0.003)
+    assert Wwann[0, 3, 3, 3, 3] == pytest.approx(0.80556, abs=0.003)
+    assert Wwann[0, 3, 3, 0, 0] == pytest.approx(0.42424, abs=0.003)
+    assert Wwann[0, 3, 0, 3, 0] == pytest.approx(0.79786, abs=0.003)
