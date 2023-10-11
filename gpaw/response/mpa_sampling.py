ad7c17f135f2daf83d845ce4a9ba77e424fcfa2d
Mikael Kuisma <kuisma@dtu.dk>
2023-10-11 15:09:19 +0200

    fix flake8

--- gpaw/response/mpa_sampling.py
+++ gpaw/response/mpa_sampling.py
@@ -24,7 +24,8 @@ def sampling_branches(w_dist: Array1D,
     if len(w_dist) == 1:
         assert eta0 >= 0
         assert parallel_lines == 2
-        w_grid = np.array([w_dist + 1j * eta0, w_dist + 1j * varpi], dtype=complex)
+        w_grid = np.array([w_dist + 1j * eta0, w_dist + 1j * varpi],
+                          dtype=complex)
         return w_grid
 
     if parallel_lines == 1:  # only one branch
