2eaaf235648994afb0ec296bdc415c3b33b9ccda
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-10-18 16:42:26 +0200

    test the davidson scalapack diagonalizer also with elpa

--- gpaw/test/parallel/test_davidson_scalapack.py
+++ gpaw/test/parallel/test_davidson_scalapack.py
@@ -6,13 +6,15 @@ from ase.parallel import world
 from gpaw import GPAW, FermiDirac, PW
 
 
-def get_calculator(sl_auto, kpoint_gamma):
+def get_calculator(sl_auto, kpoint_gamma, use_elpa=False):
     calculator = GPAW(
         mode=PW(100),
         nbands='400%',
         basis='szp(dzp)',
+        txt=None,
         occupations=FermiDirac(0.1),
-        parallel={'sl_auto': sl_auto, 'band': 2 if world.size == 8 else 1},
+        parallel={'sl_auto': sl_auto, 'band': 2 if world.size == 8 else 1,
+                  'use_elpa': use_elpa},
         kpts=[1, 1, 1] if kpoint_gamma else [1, 1, 2],
         symmetry='off',
         convergence={'maximum iterations': 1})
@@ -25,10 +27,18 @@ def test_davidson_scalapack_eigenvalues(scalapack, kpoint_gamma):
 
     atoms1 = atoms.copy()
     atoms2 = atoms.copy()
+    atoms3 = atoms.copy()
 
     atoms1.calc = get_calculator(sl_auto=True, kpoint_gamma=kpoint_gamma)
     atoms2.calc = get_calculator(sl_auto=False, kpoint_gamma=kpoint_gamma)
+    atoms3.calc = get_calculator(sl_auto=True, kpoint_gamma=kpoint_gamma,
+                                 use_elpa=True)
 
+    assert_energies_equal(atoms1, atoms2)
+    assert_energies_equal(atoms2, atoms3)
+
+
+def assert_energies_equal(atoms1, atoms2):
     e1 = atoms1.get_potential_energy()
     e2 = atoms2.get_potential_energy()
 
