26d72b7a07cc539aef567423f27e67ec2041f4cc
Martin Ovesen <martin.ovesen@outlook.dk>
2023-10-20 12:23:45 +0200

    Add comments and move caller facing methods to base class

--- gpaw/nlopt/adapters.py
+++ gpaw/nlopt/adapters.py
@@ -1,5 +1,16 @@
 
+
 class GSInfo:
+    """
+    This is the base class for the ground state adapters in the non-linear
+    optics module. It is only compatible with GPAW_NEW.
+
+    The class should never be called directly, but should instead be called
+    through the CollinearGSInfo and NoncollinearGSInfo classes.
+
+    These subclasses are necessary due to the different ways which the spin
+    index is handled in collinear and noncollinear ground state calculations.
+    """
     def __init__(self, calc, comm):
         self.comm = comm  # This will eventually just become kptcomm from calc
 
@@ -17,17 +28,41 @@ class GSInfo:
         self.collinear = density.collinear
         self.grid = density.nt_sR.desc.new(dtype=complex)
         self.ndens = density.ndensities
+        self.ns = None
 
         wfs = calc.wfs
         self.gd = wfs.gd
         self.nabla_aiiv = [setup.nabla_iiv for setup in wfs.setups]
 
-        self.ns = None
-
     def get_pseudo_wave_function(self, ni, nf, k_ind, spin):
-        raise NotImplementedError
+        """
+        Returns the periodic part of the real-space pseudo wfs
+        for the given k-point, spin index and slice of band indices.
+
+        Output is an array with shape (slice of band indices, 3D grid indices)
+        """
+        wfs = self._get_wfs(k_ind, spin)
+        psit_nX = wfs.psit_nX[ni:nf].ifft(grid=self.grid, periodic=True)
+        return self._wfs_data(psit_nX, spin)
 
     def get_wave_function_projections(self, ni, nf, k_ind, spin):
+        """
+        Returns the projections of the pseudo wfs onto the partial waves
+        for the given k-point, spin index and slice of band indices.
+
+        Output is a dictionary with atom index keys and array values
+        with shape (slice of band indices, partial wave index)
+        """
+        wfs = self._get_wfs(k_ind, spin)
+        return self._proj_data(wfs.P_ani, ni, nf, spin)
+
+    def _get_wfs(self, k_ind, spin):
+        raise NotImplementedError
+
+    def _wfs_data(self, psit, spin):
+        raise NotImplementedError
+
+    def _proj_data(self, P, ni, nf, spin):
         raise NotImplementedError
 
 
@@ -36,14 +71,14 @@ class CollinearGSInfo(GSInfo):
         super().__init__(calc, comm)
         self.ns = self.ndens
 
-    def get_pseudo_wave_function(self, ni, nf, k_ind, spin):
-        psit_nX = self.ibzwfs.wfs_qs[k_ind][spin].psit_nX[ni:nf]
-        return psit_nX.ifft(grid=self.grid, periodic=True).data
+    def _get_wfs(self, k_ind, spin):
+        return self.ibzwfs.wfs_qs[k_ind][spin]
 
-    def get_wave_function_projections(self, ni, nf, k_ind, spin):
-        P_ani = {a: P_ni[ni:nf] for a, P_ni in
-                 self.ibzwfs.wfs_qs[k_ind][spin].P_ani.items()}
-        return P_ani
+    def _wfs_data(self, psit_nR, _=None):
+        return psit_nR.data
+
+    def _proj_data(self, P_ani, ni, nf, _=None):
+        return {a: P_ni[ni:nf] for a, P_ni in P_ani.items()}
 
 
 class NoncollinearGSInfo(GSInfo):
@@ -51,11 +86,11 @@ class NoncollinearGSInfo(GSInfo):
         super().__init__(calc, comm)
         self.ns = 2
 
-    def get_pseudo_wave_function(self, ni, nf, k_ind, spin):
-        psit_nX = self.ibzwfs.wfs_qs[k_ind][0].psit_nX[ni:nf]
-        return psit_nX.ifft(grid=self.grid, periodic=True).data[:, spin]
+    def _get_wfs(self, k_ind, _=None):
+        return self.ibzwfs.wfs_qs[k_ind][0]
 
-    def get_wave_function_projections(self, ni, nf, k_ind, spin):
-        P_ani = {a: P_ni[ni:nf, spin] for a, P_ni in
-                 self.ibzwfs.wfs_qs[k_ind][0].P_ani.items()}
-        return P_ani
+    def _wfs_data(self, psit_nsR, spin):
+        return psit_nsR.data[:, spin]
+
+    def _proj_data(self, P_ansi, ni, nf, spin):
+        return {a: P_nsi[ni:nf, spin] for a, P_nsi in P_ansi.items()}
