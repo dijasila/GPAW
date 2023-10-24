7fa5cf5854e1312d3bfd7594693c61c4c13166ff
Mikael Kuisma <kuisma@dtu.dk>
2023-10-24 18:29:58 +0200

    restore test

--- gpaw/test/gpu/test_lfc.py
+++ gpaw/test/gpu/test_lfc.py
@@ -12,33 +12,30 @@ from gpaw.core.atom_arrays import AtomArraysLayout
 @pytest.mark.gpu
 @pytest.mark.parametrize('dtype', [float, complex])
 def test_lfc(gpu, dtype):
-    s = Spline(0, 5, np.exp(-0.3*np.arange(0, 5, 0.001)**2))
+    s = Spline(0, 1.0, [1.0, 0.5, 0.0])
     n = 40
-    a = 8
-    gd = GridDescriptor((64, 64, 64), (a, a, a), comm=mpi.world)
+    a = 8.0
+    gd = GridDescriptor((n, n, n), (a, a, a), comm=mpi.world)
 
-    c = LFC(gd, [[s]], dtype=dtype, integral=100000.0)
-    c_gpu = LFC(gd, [[s]], dtype=dtype, xp=cp, integral=100000.0)
-    c.set_positions([(0, 0, 0 + 0.25 * i) for i in [0]])
-    c_gpu.set_positions([(0, 0, 0 + 0.25 * i) for i in [0]])
+    c = LFC(gd, [[s], [s], [s]], dtype=dtype)
+    c_gpu = LFC(gd, [[s], [s], [s]], dtype=dtype, xp=cp)
+    c.set_positions([(0.5, 0.5, 0.25 + 0.25 * i) for i in [0, 1, 2]])
+    c_gpu.set_positions([(0.5, 0.5, 0.25 + 0.25 * i) for i in [0, 1, 2]])
 
     P_ani = {}
     for a in c.my_atom_indices:
         P_ani[a] = np.ones((1,), dtype=dtype)
 
-    P_gpu_ani = AtomArraysLayout([1], dtype=dtype, xp=cp).empty()
+    P_gpu_ani = AtomArraysLayout([1, 1, 1], dtype=dtype, xp=cp).empty()
     P_gpu_ani.data[:] = 1.0
 
     b = gd.zeros(dtype=dtype)
     b_gpu = cp.zeros_like(b)
-    q = {float: 0, complex: 0}[dtype]
+    q = {float: -1, complex: 0}[dtype]
     c.add(b, P_ani, q=q)
     c_gpu.add(b_gpu, P_gpu_ani, q=q)
     b_ref = b_gpu.get()
-    from matplotlib import pyplot as plt
-    plt.plot(b_ref[0,0,:])
-    plt.plot(b[0,0,:])
-    plt.show()
+
     assert b == pytest.approx(b_ref, abs=1e-12)
 
     c.integrate(b, P_ani, q=q)
