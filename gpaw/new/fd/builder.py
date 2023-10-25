2a6a4d23d177c635282b08d7856228385f5d34ea
Tuomas Rossi <tuomas.rossi@csc.fi>
2023-10-25 16:46:04 +0300

    Enable tri-quintic interpolation with a workaround

--- gpaw/new/fd/builder.py
+++ gpaw/new/fd/builder.py
@@ -16,7 +16,7 @@ from gpaw.poisson import PoissonSolver as make_poisson_solver
 
 
 class FDDFTComponentsBuilder(PWFDDFTComponentsBuilder):
-    def __init__(self, atoms, params, *, comm, nn=3, interpolation=1):
+    def __init__(self, atoms, params, *, comm, nn=3, interpolation=3):
         super().__init__(atoms,
                          params,
                          comm=comm)
