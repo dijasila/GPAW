51617a76e0d50eec3e4bb6c293eeacda3d00edeb
Martti Louhivuori <martti.louhivuori@csc.fi>
2023-10-26 12:20:13 +0300

    Use new style in LFC test

--- gpaw/test/gpu/test_lfc.py
+++ gpaw/test/gpu/test_lfc.py
@@ -1,45 +1,39 @@
 import pytest
 import numpy as np
-from gpaw.grid_descriptor import GridDescriptor
-from gpaw.spline import Spline
-import gpaw.mpi as mpi
-from gpaw.lfc import LocalizedFunctionsCollection as LFC
+
+from gpaw.core import UGDesc
 from gpaw.gpu import cupy as cp
-from gpaw.core.atom_arrays import AtomArraysLayout
+from gpaw.mpi import world
+from gpaw.spline import Spline
 
 
-@pytest.mark.serial
-@pytest.mark.gpu
 @pytest.mark.parametrize('dtype', [float, complex])
-def test_lfc(gpu, dtype):
+def test_lfc(dtype):
     s = Spline(0, 1.0, [1.0, 0.5, 0.0])
     n = 40
     a = 8.0
-    gd = GridDescriptor((n, n, n), (a, a, a), comm=mpi.world)
+    fracpos_ac = [(0.5, 0.5, 0.25 + 0.25 * i) for i in [0, 1, 2]]
 
-    c = LFC(gd, [[s], [s], [s]], dtype=dtype)
-    c_gpu = LFC(gd, [[s], [s], [s]], dtype=dtype, xp=cp)
-    c.set_positions([(0.5, 0.5, 0.25 + 0.25 * i) for i in [0, 1, 2]])
-    c_gpu.set_positions([(0.5, 0.5, 0.25 + 0.25 * i) for i in [0, 1, 2]])
+    grid = UGDesc(cell=[a, a, a], size=(n, n, n), comm=world, dtype=dtype)
+    basis_cpu = grid.atom_centered_functions([[s],[s],[s]],
+                                             positions=fracpos_ac, xp=np)
+    basis_gpu = grid.atom_centered_functions([[s],[s],[s]],
+                                             positions=fracpos_ac, xp=cp)
 
-    P_ani = {}
-    for a in c.my_atom_indices:
-        P_ani[a] = np.ones((1,), dtype=dtype)
-
-    P_gpu_ani = AtomArraysLayout([1, 1, 1], dtype=dtype, xp=cp).empty()
+    P_cpu_ani = basis_cpu.layout.empty()
+    P_gpu_ani = basis_gpu.layout.empty()
+    P_cpu_ani.data[:] = 1.0
     P_gpu_ani.data[:] = 1.0
 
-    b = gd.zeros(dtype=dtype)
-    b_gpu = cp.zeros_like(b)
-    q = {float: -1, complex: 0}[dtype]
-    c.add(b, P_ani, q=q)
-    c_gpu.add(b_gpu, P_gpu_ani, q=q)
-    b_ref = b_gpu.get()
+    b_cpu = grid.zeros(xp=np)
+    b_gpu = grid.zeros(xp=cp)
+    basis_cpu.add_to(b_cpu, P_cpu_ani)
+    basis_gpu.add_to(b_gpu, P_gpu_ani)
 
-    assert b == pytest.approx(b_ref, abs=1e-12)
+    assert b_cpu.data == pytest.approx(b_gpu.data.get(), abs=1e-12)
 
-    c.integrate(b, P_ani, q=q)
-    c_gpu.integrate(b_gpu, P_gpu_ani, q=q)
+    out_cpu_ani = basis_cpu.integrate(b_cpu)
+    out_gpu_ani = basis_gpu.integrate(b_gpu)
 
-    for a, P_ni in P_ani.items():
-        assert P_ni == pytest.approx(P_gpu_ani[a].get(), abs=1e-12)
+    for a, out_cpu_ni in out_cpu_ani.items():
+        assert out_cpu_ni == pytest.approx(out_gpu_ani[a].get(), abs=1e-12)
