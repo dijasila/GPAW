8bd8d6eefd8944794335c1aaf064210e23487704
Mikael Kuisma <kuisma@dtu.dk>
2023-11-07 12:35:38 +0100

    remove accidental upf file, update test

--- gpaw/test/response/test_gw_sg15.py
+++ gpaw/test/response/test_gw_sg15.py
@@ -1,24 +1,23 @@
 from ase.build import molecule
 from gpaw import GPAW, PW
 from gpaw.response.g0w0 import G0W0
-from gpaw import setup_paths
-setup_paths.insert(0,'.')
+from gpaw.mpi import world
 
-sys = molecule('H2',pbc=True)
-sys.center(vacuum=2.5)
 
-def test_gw_sg15(sg15_hydrogen):
-
+def test_gw_sg15(in_tmp_dir, add_cwd_to_setup_paths):
     from gpaw.test.pseudopotential.H_sg15 import pp_text
-    with open('H_ONCV_PBE-1.0.upf','w') as pp_file:
-        print(pp_text,file=pp_file)
+    if world.rank == 0:
+        with open('H_ONCV_PBE-1.0.upf','w') as pp_file:
+            print(pp_text,file=pp_file)
+    world.barrier()
+    sys = molecule('H2',pbc=True)
+    sys.center(vacuum=2.5)
 
     calc = GPAW(setups='sg15', 
                 xc='PBE', mode=PW(ecut=300), nbands=50, kpts=(2,2,2))
     sys.calc = calc
     sys.get_potential_energy()
     calc.write('gs.gpw', mode='all')
-    GPAW('gs.gpw')
 
     gw = G0W0(calc='gs.gpw',
                   bands=(1, 6),
@@ -26,3 +25,4 @@ def test_gw_sg15(sg15_hydrogen):
                   nblocksmax=True,
                   filename=f'H2_g0w0_b11-15')
     gw.calculate()
+    # todo: add an assert of some kind
