5794d5ea5b954369ba6bbaaff8b627012cb268d5
Tuomas Rossi <tuomas.rossi@csc.fi>
2023-11-07 17:40:56 +0200

    Update siteconfig for LUMI-G

--- doc/platforms/Cray/siteconfig-lumi-gpu.py
+++ doc/platforms/Cray/siteconfig-lumi-gpu.py
@@ -1,35 +1,22 @@
 """Custom GPAW siteconfig for LUMI-G."""
 
+parallel_python_interpreter = True
+
 mpi = True
 compiler = 'cc'
+compiler_args = []  # remove all default args
 libraries = []
 library_dirs = []
 include_dirs = []
 extra_compile_args = [
     '-O3',
-    '-march=native',
-    '-mtune=native',
-    '-mavx2',
     '-fopenmp',
     '-fPIC',
     '-Wall',
     '-Wno-stringop-overflow',  # suppress warnings from MPI_STATUSES_IGNORE
-    '-DNDEBUG',
+    '-Werror',
     '-g']
 extra_link_args = ['-fopenmp']
-
-# hip
-gpu = True
-gpu_target = 'hip-amd'
-gpu_compiler = 'hipcc'
-gpu_include_dirs = []
-gpu_compile_args = ['--offload-arch=gfx90a', '-O3', '-g']
-libraries += ['amdhip64', 'hipblas']
-# define_macros += [('GPAW_GPU_AWARE_MPI', None)]
-
-# ELPA
-elpa = True
-libraries += ['elpa']
 
 # FFTW
 fftw = True
@@ -43,3 +30,11 @@ scalapack = True
 libraries += ['xc']
 
 define_macros += [('GPAW_ASYNC', 1)]
+
+# hip
+gpu = True
+gpu_target = 'hip-amd'
+gpu_compiler = 'hipcc'
+gpu_include_dirs = []
+gpu_compile_args = ['--offload-arch=gfx90a', '-O3', '-g']
+libraries += ['amdhip64', 'hipblas']
