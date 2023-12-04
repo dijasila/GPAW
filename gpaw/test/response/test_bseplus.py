7dbd91caabf9d881b16df482539b81313d862d9b
Mikael Kuisma <kuisma@dtu.dk>
2023-12-04 15:20:24 +0100

    fix test

--- gpaw/test/response/test_bseplus.py
+++ gpaw/test/response/test_bseplus.py
@@ -13,7 +13,7 @@ def test_bse_plus(in_tmp_dir, scalapack):
                 occupations=FermiDirac(0.01),
                 nbands=8,
                 symmetry='off',
-                convergence={'bands': -4})
+                convergence={'bands': -4, 'density':1e-7, 'eigenstates':1e-10})
 
     a = 5.431
 
@@ -24,23 +24,20 @@ def test_bse_plus(in_tmp_dir, scalapack):
 
     bse = BSE('Si.gpw',
             ecut=20, spinors=False,
-            valence_bands=range(2,4),
-            conduction_bands=range(4,6),
+            valence_bands=range(4),
+            conduction_bands=range(4,8),
             eshift=0,
             mode = 'BSE',
             nbands=8,
+            q_c=[0.0, 0.0, 0.0],
             write_h=False,
             write_v=False)
     
     
-    chi_irr_bse = bse.get_vchi(eta=0.2,optical = True, q_c = [0,0,0], hybrid = True,
-                    w_w=np.array([-3,0,6]))
- 
-    assert np.allclose(chi_irr_bse[0,:3,:3],np.array([[ 2.76303362+1.41542559j, -0.06519706-0.01796091j,  0.01792886-0.06572987j],
-  [-0.01769456+0.06515327j, -0.01168023+0.01055983j, -0.00986236-0.01076832j],
- [ 0.06568665+0.01766283j,  0.00986239+0.0107683j,  -0.01166505+0.01061083j]]), atol = 1e-2, rtol = 1e-2)
-
- #   chi_irr_BSE_correct = np.load('chi_irr_BSE_correct.npy') 
-    
- #   assert np.allclose(chi_irr_BSE_new.real, chi_irr_BSE_correct.real, atol=1e-5, rtol=1e-4)
- #   assert np.allclose(chi_irr_BSE_new.imag, chi_irr_BSE_correct.imag, atol=1e-5, rtol=1e-4)
+    chi_irr_bse = bse.get_chi_GG(eta=0.2,optical = True, hybrid = True,
+                                 w_w=np.array([-3,0,6]))
+    ref = [(-0.12319305784052169-0.005900101520066767j),
+           (-3.519763508433997e-10-0.035759806607911705j),
+           (3.031540803184087e-05-0.0004896329314266086j)]
+    for i, r in enumerate(ref):
+        assert np.allclose(chi_irr_bse[i,i,i+1], r)
