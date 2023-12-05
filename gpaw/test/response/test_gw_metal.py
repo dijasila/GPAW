45a656f94e600c29f8299e0d2c2bcebba7ce1bd6
Fredrik Andreas Nilsson <fanni@DTU-FVFGC14Q05G.local>
2023-12-05 11:26:38 +0100

    flakes

--- gpaw/test/response/test_gw_metal.py
+++ gpaw/test/response/test_gw_metal.py
@@ -1,11 +1,12 @@
 from gpaw.response.g0w0 import G0W0
 import pytest
 
+
 @pytest.mark.response
 def test_gw_metal(gpw_files):
     gpwfile = gpw_files['srvo3_pw']
     with pytest.raises(NotImplementedError) as excinfo:
-        gw = G0W0(gpwfile, nbands=15)
+        _ = G0W0(gpwfile, nbands=15)
 
-    errormsg = 'Intraband screening is not implemented for metals' 
+    errormsg = 'Intraband screening is not implemented for metals'
     assert str(excinfo.value) == errormsg
