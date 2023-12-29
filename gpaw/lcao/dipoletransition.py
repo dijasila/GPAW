de3c684765d0328197baf5bfe2e829ae63055de1
Anubhab Haldar <ahaldar@bu.edu>
2023-12-29 05:54:37 -0500

    Memory improvements and degeneracy handling for dipole moments

--- gpaw/lcao/dipoletransition.py
+++ gpaw/lcao/dipoletransition.py
@@ -25,8 +25,11 @@ def get_dipole_transitions(wfs) -> ArrayND:
     for kpt in wfs.kpt_u:
         # NOTE: Check whether it's this way or other way around.
         deltaE = kpt.eps_n[:, None] - kpt.eps_n[None, :]
-        np.fill_diagonal(deltaE, np.inf)
-        r_skvnm[kpt.s, kpt.k, :] = -1j * p_skvnm[kpt.s, kpt.k, :] / deltaE
+        # The treatment of the energy difference in the denominator is
+        # from https://doi.org/10.1103/PhysRevB.52.14636 eq 7 and note 18
+        # therein
+        r_skvnm[kpt.s, kpt.k, :] = -1j * p_skvnm[kpt.s, kpt.k, :] * \
+            np.reciprocal(deltaE, where=~np.isclose(deltaE, 0.0))
     wfs.kd.comm.sum(r_skvnm)
     return r_skvnm
 
@@ -57,12 +60,12 @@ def get_momentum_transitions(wfs, savetofile: bool = True) -> ArrayND:
 
     mom_skvnm = np.zeros((nspins, nk, 3, nbands, nbands), dtype=complex)
 
-    dThetadR_qvMM, dTdR_qvMM = wfs.manytci.O_qMM_T_qMM(gd.comm, ksl.Mstart,
+    dThetadR_qvMM, _ = wfs.manytci.O_qMM_T_qMM(gd.comm, ksl.Mstart,
                                                        ksl.Mstop, False,
                                                        derivative=True)
 
     mome_skvnm = np.zeros((nspins, nk, 3, nbands, nbands), dtype=dtype)
-    momd_skvnm = np.zeros((nspins, nk, 3, nbands, nbands), dtype=dtype)
+    momd_skv = np.zeros((nspins, nk, 3), dtype=dtype)
     moma_skvnm = np.zeros((nspins, nk, 3, nbands, nbands), dtype=dtype)
 
     B_cv = 2.0 * np.pi * gd.icell_cv
@@ -79,16 +82,20 @@ def get_momentum_transitions(wfs, savetofile: bool = True) -> ArrayND:
         for a, P_ni in kpt.P_ani.items():
             nabla_iiv = wfs.setups[a].nabla_iiv
             moma_vnm += np.einsum('ni,ijv,mj->vnm',
-                                  P_ni.conj(), nabla_iiv, P_ni)
+                                  P_ni.conj(), nabla_iiv, P_ni, optimize=True)
         gd.comm.sum(moma_vnm)
         moma_skvnm[kpt.s, kpt.k] = moma_vnm
 
         # diagonal term
         k_v = np.dot(wfs.kd.ibzk_kc[kpt.k], B_cv)
-        momd_skvnm[kpt.s, kpt.k] = np.einsum("k,nm->knm",
-                                             k_v, np.identity(nbands))
+        momd_skv[kpt.s, kpt.k, :] = k_v
 
-    mom_skvnm = momd_skvnm - 1j * (mome_skvnm + moma_skvnm)
+    mom_skvnm = - 1j * (mome_skvnm + moma_skvnm)
+    # Extract a view of the diagonal elements out
+    # Einsum with no optimization since that is empirically faster here
+    mom_diag = np.einsum('skvnn->skvn', mom_skvnm)
+    # Add the diagonal term. Since mom_diag is a view, this changes mom_skvnm
+    mom_diag += momd_skv[..., None]
     wfs.kd.comm.sum(mom_skvnm)
 
     if world.rank == 0 and savetofile:
