d3e0fb044586220e2f62b8f7f631bbb4b4a3a83a
Aleksei Ivanov <alxvov@gmail.com>
2024-01-13 22:29:07 +0000

    make eigensolver abstract

--- gpaw/new/eigensolver.py
+++ gpaw/new/eigensolver.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
+from abc import ABC, abstractmethod
+
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from gpaw.new.calculation import DFTState
 
 
-class Eigensolver:
+class Eigensolver(ABC):
+    @abstractmethod
     def iterate(self, state: DFTState, hamiltonian) -> float:
-        raise NotImplementedError
+        pass
