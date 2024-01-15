2bed156469084d6c9fb5246410a886098e088caf
Aleksei Ivanov <alxvov@gmail.com>
2024-01-15 22:14:26 +0000

    use one index instead of qs

--- gpaw/new/pwfd/_searchdir.py
+++ gpaw/new/pwfd/_searchdir.py
@@ -17,17 +17,17 @@ _TArray_co = TypeVar("_TArray_co", bound=DistributedArrays, covariant=True)
 class LBFGS:
     def __init__(
         self,
-        grad_cur_qs: ArrayCollection,
+        grad_cur_u: ArrayCollection,
         memory: int = 2,
         kpt_comm: MPIComm = serial_comm,
         xp: ModuleType = np,
     ):
 
-        self.grad_old_qs: ArrayCollection = grad_cur_qs.make_copy()
-        self.search_dir_qs: ArrayCollection = -grad_cur_qs
+        self.grad_old_u: ArrayCollection = grad_cur_u.make_copy()
+        self.search_dir_u: ArrayCollection = -grad_cur_u
 
-        self.ds_mqs: list[ArrayCollection] = [grad_cur_qs.empty()] * memory
-        self.dy_mqs: list[ArrayCollection] = [grad_cur_qs.empty()] * memory
+        self.ds_mqs: list[ArrayCollection] = [grad_cur_u.empty()] * memory
+        self.dy_mqs: list[ArrayCollection] = [grad_cur_u.empty()] * memory
         self.rho_m: list[float] = [0] * memory
 
         self._local_iter: int = 1
@@ -37,14 +37,14 @@ class LBFGS:
         self.xp = xp
 
     def update(
-        self, grad_cur_qs: ArrayCollection[_TArray_co]
+        self, grad_cur_u: ArrayCollection[_TArray_co]
     ) -> "ArrayCollection[_TArray_co]":
 
         m = self._local_iter % self._memory
         # ds = a_cur - a_old, which is search dir
         # but in some cases only the difference is known
-        self.ds_mqs[m] = self.search_dir_qs.make_copy()
-        self.dy_mqs[m] = grad_cur_qs - self.grad_old_qs
+        self.ds_mqs[m] = self.search_dir_u.make_copy()
+        self.dy_mqs[m] = grad_cur_u - self.grad_old_u
         dyds = self.ds_mqs[m].dot(self.dy_mqs[m])
         dyds = self.kpt_comm.sum_scalar(dyds)
         if abs(dyds) > 1.0e-20:
@@ -54,15 +54,15 @@ class LBFGS:
 
         if self.rho_m[m] < 0:
             # reset the optimizer
-            self.grad_old_qs = grad_cur_qs.make_copy()
-            self.search_dir_qs = -grad_cur_qs
-            self.ds_mqs = [grad_cur_qs.empty()] * self._memory
-            self.dy_mqs = [grad_cur_qs.empty()] * self._memory
+            self.grad_old_u = grad_cur_u.make_copy()
+            self.search_dir_u = -grad_cur_u
+            self.ds_mqs = [grad_cur_u.empty()] * self._memory
+            self.dy_mqs = [grad_cur_u.empty()] * self._memory
             self.rho_m = [0] * self._memory
             self._local_iter = 1
-            return self.search_dir_qs
+            return self.search_dir_u
 
-        q = grad_cur_qs.make_copy()
+        q = grad_cur_u.make_copy()
         k = self._memory - 1
         alpha = [0.0] * self._memory
 
@@ -78,7 +78,7 @@ class LBFGS:
         yy = self.kpt_comm.sum_scalar(yy)
 
         devis: float = self.xp.maximum(self.rho_m[m] * yy, 1.0e-20)
-        self.search_dir_qs = q.multiply_by_number(1 / devis)
+        self.search_dir_u = q.multiply_by_number(1 / devis)
 
         for k in range(self._memory):
             if self._local_iter < self._memory:
@@ -86,15 +86,18 @@ class LBFGS:
             else:
                 c_ind = (k + m + 1) % self._memory
 
-            yr = self.search_dir_qs.dot(self.dy_mqs[c_ind])
+            yr = self.search_dir_u.dot(self.dy_mqs[c_ind])
             yr = self.kpt_comm.sum_scalar(yr)
 
             beta = self.rho_m[c_ind] * yr
-            self.search_dir_qs += self.ds_mqs[c_ind].multiply_by_number(
+            self.search_dir_u += self.ds_mqs[c_ind].multiply_by_number(
                 alpha[c_ind] - beta
             )
 
-        self.grad_old_qs = grad_cur_qs.make_copy()
-        self.search_dir_qs.multiply_by_number_in_place(-1)
+        self.grad_old_u = grad_cur_u.make_copy()
+        self.search_dir_u.multiply_by_number_in_place(-1)
         self._local_iter += 1
-        return self.search_dir_qs
+        return self.search_dir_u
+
+    def rescale_searchdir_vector(self, alpha: float) -> None:
+        self.search_dir_u.multiply_by_number_in_place(alpha)
--- gpaw/test/new/test_searchdir.py
+++ gpaw/test/new/test_searchdir.py
@@ -3,10 +3,11 @@ from gpaw.new.pwfd import ArrayCollection, LBFGS
 
 
 def test_initialize_and_update():
-    wfs_qs: list[list[PWArray]] = []
+    wfs_u: list[PWArray] = []
     for kpt in [(1, 1, 1), (2, 2, 2)]:
         pwdesc = PWDesc(ecut=5, cell=[1, 1, 1], kpt=kpt, dtype=complex)
-        wfs_qs.append([pwdesc.empty(), pwdesc.empty()])
-    arrcol_qs = ArrayCollection(wfs_qs)
-    optimizer = LBFGS(arrcol_qs)
-    optimizer.update(arrcol_qs)
+        wfs_u.append(pwdesc.empty())
+
+    arrcol_u = ArrayCollection(wfs_u)
+    optimizer = LBFGS(arrcol_u)
+    optimizer.update(arrcol_u)
