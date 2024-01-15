dcf3f8632d1d16b01391722da640c32537da5272
Aleksei Ivanov <alxvov@gmail.com>
2024-01-15 22:09:00 +0000

    restructure array_collection, use just list instead of neasted lists.

--- gpaw/new/pwfd/_array_collection.py
+++ gpaw/new/pwfd/_array_collection.py
@@ -1,53 +1,46 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Generator, Generic, TypeVar
+from typing import TYPE_CHECKING, Generic, Generator, TypeVar
+
 from typing_extensions import Self
 
 if TYPE_CHECKING:
     from gpaw.new.ibzwfs import IBZWaveFunctions, PWFDWaveFunctions
 
 from gpaw.core.arrays import DistributedArrays
-from gpaw.new import zips
 
 _TArray_co = TypeVar("_TArray_co", bound=DistributedArrays, covariant=True)
 
 
 class ArrayCollection(Generic[_TArray_co]):
-    def __init__(self, data_qs: list[list[_TArray_co]]):
-        self.array_qs: list[list[_TArray_co]] = data_qs
+    def __init__(self, data_u: list[_TArray_co]):
+        self.array_u: list[_TArray_co] = data_u
 
     def __iter__(self) -> Generator[_TArray_co, None, None]:
-        for array_s in self.array_qs:
-            yield from array_s
+        for array in self.array_u:
+            yield array
 
     def __neg__(self) -> "ArrayCollection[_TArray_co]":
-        new_array_qs: list[list[_TArray_co]] = []
-        for array_s in self.array_qs:
-            new_array_qs.append([])
-            for array in array_s:
-                new_array_qs[-1].append(array.new(-array.data))
-
-        return ArrayCollection(new_array_qs)
+        new_array_u: list[_TArray_co] = [
+            array.new(-array.data) for array in self
+        ]
+        return ArrayCollection(new_array_u)
 
     def __sub__(self, other: Self) -> "ArrayCollection[_TArray_co]":
 
-        new_array_qs: list[list[_TArray_co]] = []
-        for a_s, b_s in zips(self.array_qs, other.array_qs):
-            new_array_qs.append([])
-            for a, b in zips(a_s, b_s):
-                new_array_qs[-1].append(a.new(a.data - b.data))
+        new_array_u: list[_TArray_co] = []
+        for a, b in zip(self, other):
+            new_array_u.append(a.new(a.data - b.data))
 
-        return ArrayCollection(new_array_qs)
+        return ArrayCollection(new_array_u)
 
     def __add__(self, other: Self) -> "ArrayCollection[_TArray_co]":
 
-        new_array_qs: list[list[_TArray_co]] = []
-        for a_s, b_s in zips(self.array_qs, other.array_qs):
-            new_array_qs.append([])
-            for a, b in zips(a_s, b_s):
-                new_array_qs[-1].append(a.new(a.data + b.data))
+        new_array_u: list[_TArray_co] = []
+        for a, b in zip(self, other):
+            new_array_u.append(a.new(a.data + b.data))
 
-        return ArrayCollection(new_array_qs)
+        return ArrayCollection(new_array_u)
 
     def multiply_by_number_in_place(self, number: complex) -> None:
         for array in self:
@@ -56,22 +49,19 @@ class ArrayCollection(Generic[_TArray_co]):
     def multiply_by_number(
         self, number: complex
     ) -> "ArrayCollection[_TArray_co]":
-        new_array_qs: list[list[_TArray_co]] = []
-        for array_s in self.array_qs:
-            new_array_qs.append([])
-            for array in array_s:
-                new_array_qs[-1].append(array.new(number * array.data))
 
-        return ArrayCollection(new_array_qs)
+        new_array_u: list[_TArray_co] = [
+            array.new(number * array.data) for array in self
+        ]
+
+        return ArrayCollection(new_array_u)
 
     def make_copy(self) -> "ArrayCollection[_TArray_co]":
-        new_array_qs: list[list[_TArray_co]] = []
-        for array_s in self.array_qs:
-            new_array_qs.append([])
-            for array in array_s:
-                new_array_qs[-1].append(array.copy())
+        new_array_u: list[_TArray_co] = [
+            array.copy() for array in self
+        ]
 
-        return ArrayCollection(new_array_qs)
+        return ArrayCollection(new_array_u)
 
     def dot(self, other: Self) -> float:
         """dot product between self and complex conjugate other
@@ -81,7 +71,7 @@ class ArrayCollection(Generic[_TArray_co]):
         unique products between arrays.
         """
         result = 0
-        for a, b in zips(self, other):
+        for a, b in zip(self, other):
             tmp_nn = a.integrate(b)
             xp = a.xp
             if not isinstance(tmp_nn, complex):
@@ -95,21 +85,15 @@ class ArrayCollection(Generic[_TArray_co]):
     def copy_from_ibzwfs(
         ibzwfs: IBZWaveFunctions[PWFDWaveFunctions],
     ) -> "ArrayCollection[_TArray_co]":
-        new_array_qs: list[list[_TArray_co]] = []
-        for wfs_s in ibzwfs.wfs_qs:
-            new_array_qs.append([])
-            for wfs in wfs_s:
-                new_array_qs[-1].append(wfs.psit_nX.copy())
+        new_array_u: list[_TArray_co] = []
+        for wfs in ibzwfs:
+            new_array_u.append(wfs.psit_nX.copy())
 
-        return ArrayCollection(new_array_qs)
+        return ArrayCollection(new_array_u)
 
     def empty(self) -> "ArrayCollection[_TArray_co]":
-        new_array_qs: list[list[_TArray_co]] = []
-        for array_s in self.array_qs:
-            new_array_qs.append([])
-            for array in array_s:
-                new_array_qs[-1].append(
-                    array.desc.empty(array.dims, array.comm, array.xp)
-                )
-
-        return ArrayCollection(new_array_qs)
+        new_array_u: list[_TArray_co] = [
+            array.desc.empty(array.dims, array.comm, array.xp)
+            for array in self
+        ]
+        return ArrayCollection(new_array_u)
