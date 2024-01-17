58081bde8245f1d39db0328155989de8879847fe
Aleksei Ivanov <alxvov@gmail.com>
2024-01-17 10:44:48 +0000

    modify the test to check parallelization over kpts and domain

--- gpaw/test/new/test_directopt.py
+++ gpaw/test/new/test_directopt.py
@@ -4,18 +4,22 @@ from gpaw.new.ase_interface import GPAW
 
 
 def test_direct_optimization():
+    """test with 4 cores to check parallelization over
+     both kpts and domain"""
+
     a = 2.0
     atoms = Atoms('Li',
                   cell=[a, a, a],
                   pbc=True)
     atoms.calc = GPAW(
         mode='pw',
-        kpts=(2, 2, 2),
+        kpts=(1, 1, 2),
         nbands=-2,
+        symmetry="off",
         mixer={'backend': 'no-mixing'},
         occupations={"name": "fixed-uniform"},
         eigensolver={"name": "lbfgs", "memory": 2},
         convergence={"bands": "occupied"},
     )
     energy = atoms.get_potential_energy()
-    assert energy == pytest.approx(0.480154, rel=0.05)
+    assert energy == pytest.approx(-2.3329, rel=0.05)
