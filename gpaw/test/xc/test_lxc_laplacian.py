e5f82bc8ab2d6d1f287a05d3262bde7ed0d8a1a9
Rolf Wuerdemann <rowue@digitalis.org>
2024-02-24 18:09:21 +0100

    ENH: shorter test for laplacian in MGGAs

--- gpaw/test/xc/test_lxc_laplacian.py
+++ gpaw/test/xc/test_lxc_laplacian.py
@@ -1,28 +1,15 @@
 """check if an error is raised if the laplacian is needed (mgga)"""
 import pytest
-from ase.build import molecule
-from gpaw import GPAW
-from gpaw.cluster import Cluster
-
-vacuum = 4.0
-h = 0.3
+from gpaw.xc import LibXC
 
 
 @pytest.mark.mgga
 @pytest.mark.libxc
 def test_mgga_lxc_laplacian():
-    cluster = Cluster(molecule('CO'))
-    cluster.minimal_box(border=vacuum, h=h)
-    calc = GPAW(xc='MGGA_X_BR89+MGGA_C_TPSS', mode='fd',
-                h=h, maxiter=14,
-                convergence={
-                    'energy': 0.5,
-                    'density': 1.0e-1,
-                    'eigenstates': 4.0e-1})
-    cluster.calc = calc
+    """Check for raised error."""
     laplacian_test = False
     try:
-        cluster.get_potential_energy()
+        LibXC('MGGA_X_BR89+MGGA_C_TPSS')
     except ValueError:
         laplacian_test = True
     assert laplacian_test
