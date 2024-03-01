6fe1921eeac2dfd67ad4be2f442565c295cac4d7
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2024-03-01 10:18:40 +0100

    Parametrize test

--- gpaw/test/generic/test_move_across_cell.py
+++ gpaw/test/generic/test_move_across_cell.py
@@ -8,44 +8,42 @@ from gpaw import GPAW, Davidson, MixerSum
 # which compensates for discontinuity of phases is probably broken.
 
 
-@pytest.mark.later
-def test_generic_move_across_cell(gpaw_new):
-    def test(calc):
-        atoms = molecule('H2O', vacuum=2.5)
-        atoms.pbc = 1
+# @pytest.mark.later
+@pytest.mark.parametrize(
+    'params',
+    [dict(mode='pw', eigensolver=Davidson(3),
+          experimental={'reuse_wfs_method': 'paw'}),
+     # pw + lcao extrapolation is currently broken (PWLFC lacks integrate2):
+     # dict(mode='pw', experimental={'reuse_wfs_method': 'lcao'}),
+     dict(mode='fd', h=0.3,
+          experimental={'reuse_wfs_method': 'lcao'}),
+     dict(mode='lcao', basis='sz(dzp)', h=0.3)])
+def test_generic_move_across_cell(gpaw_new, params):
+    params.update(
+        xc='oldLDA',
+        # make sure MixerSum works for spin-paired system also:
+        mixer=MixerSum(0.7),
+        kpts=[1, 1, 2])
+    calc = GPAW(**params)
+    atoms = molecule('H2O', vacuum=2.5)
+    atoms.pbc = 1
 
-        # Translate O to corner:
-        atoms.positions -= atoms.positions[0, None, :]
+    # Translate O to corner:
+    atoms.positions -= atoms.positions[0, None, :]
 
-        # Be sure that we are on the positive axis:
+    # Be sure that we are on the positive axis:
 
-        atoms.calc = calc
+    atoms.calc = calc
 
-        eps = 1e-12
-        atoms.positions[0, :] = eps
-        atoms.get_potential_energy()
-        atoms.positions[0, 2] -= 2 * eps
-        atoms.get_potential_energy()
+    eps = 1e-12
+    atoms.positions[0, :] = eps
+    atoms.get_potential_energy()
+    atoms.positions[0, 2] -= 2 * eps
+    atoms.get_potential_energy()
 
-        # We should be within the convergence criterion.
-        # It runs a minimum of three iterations:
-        if gpaw_new:
-            print(calc.calculation.scf_loop.niter)
-            assert calc.calculation.scf_loop.niter == 3
-        else:
-            print(calc.scf.niter)
-            assert calc.scf.niter == 3
-
-    # Make sure MixerSum works for spin-paired system also:
-    kwargs = dict(xc='oldLDA', mixer=MixerSum(0.7), kpts=[1, 1, 2])
-
-    test(GPAW(mode='pw', eigensolver=Davidson(3),
-              experimental={'reuse_wfs_method': 'paw'}, **kwargs))
-    test(GPAW(mode='fd', h=0.3,
-              experimental={'reuse_wfs_method': 'lcao'}, **kwargs))
-    test(GPAW(mode='lcao', basis='sz(dzp)', h=0.3))
-
-    # pw + lcao extrapolation is currently broken (PWLFC lacks integrate2):
-    # test(GPAW(mode='pw',
-    #           experimental={'reuse_wfs_method': 'lcao'},
-    #           **kwargs()))
+    # We should be within the convergence criterion.
+    # It runs a minimum of three iterations:
+    if gpaw_new:
+        assert calc.calculation.scf_loop.niter == 3
+    else:
+        assert calc.scf.niter == 3
