f2efbe3534a578615bc692a5af2d1a50a73b0344
Ask Hjorth Larsen <asklarsen@gmail.com>
2024-03-11 16:56:50 +0100

    remove unnecessary parentheses

--- gpaw/response/df.py
+++ gpaw/response/df.py
@@ -378,7 +378,7 @@ class DielectricFunctionCalculator:
 
         # Calculate eels = -Im 4 \pi / q^2  \chi
         eels_NLFC_w = -(1. / (1. - Vchi0_wGG[:, 0, 0])).imag
-        eels_LFC_w = - (Vchi_wGG[:, 0, 0]).imag
+        eels_LFC_w = -Vchi_wGG[:, 0, 0].imag
 
         # Collect frequencies
         eels_NLFC_w = self.collect(eels_NLFC_w)
@@ -446,8 +446,8 @@ class DielectricFunctionCalculator:
             qpd, chi0_wGG, chi_wGG = self.get_chi(xc=xc,
                                                   q_c=q_c,
                                                   direction=direction)
-            alpha_w = -V * (chi_wGG[:, 0, 0]) / (4 * pi)
-            alpha0_w = -V * (chi0_wGG[:, 0, 0]) / (4 * pi)
+            alpha_w = -V / (4 * pi) * chi_wGG[:, 0, 0]
+            alpha0_w = -V / (4 * pi) * chi0_wGG[:, 0, 0]
 
             alpha_w = self.collect(alpha_w)
             alpha0_w = self.collect(alpha0_w)
