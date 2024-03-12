8586e15c3ac792553e7caae9d2f0d546c5cdbe72
Martin Ovesen <martin.ovesen@outlook.dk>
2024-03-12 14:01:52 +0100

    Remove hydrogen tests, add proper shift current test

--- gpaw/test/nlopt/test_shg.py
+++ gpaw/test/nlopt/test_shg.py
@@ -1,48 +1,9 @@
-from ase import Atoms
 import numpy as np
 import pytest
 
-from gpaw.mpi import serial_comm, world
-from gpaw.new.ase_interface import GPAW
+from gpaw.mpi import world
 from gpaw.nlopt.basic import NLOData
-from gpaw.nlopt.matrixel import make_nlodata
 from gpaw.nlopt.shg import get_shg
-
-
-@pytest.mark.skipif(world.size > 4, reason='System too small')
-def test_shg(in_tmp_dir):
-    # Check for Hydrogen atom
-    atoms = Atoms('H', cell=(3 * np.eye(3)), pbc=True)
-
-    # Do a GS and save it
-    calc = GPAW(mode={'name': 'pw', 'ecut': 600},
-                communicator=serial_comm,
-                symmetry={'point_group': False},
-                kpts={'size': (2, 2, 2)},
-                nbands=5,
-                txt=None)
-    atoms.calc = calc
-    atoms.get_potential_energy()
-
-    # Get the mml
-    nlodata = make_nlodata(calc, comm=world)
-
-    # Do a SHG
-    freqs = np.linspace(0, 5, 101)
-    get_shg(nlodata, freqs=freqs)
-    get_shg(nlodata, freqs=freqs, gauge='vg', out_name='shg2.npy')
-
-    # Check it
-    if world.rank == 0:
-        shg = np.load('shg.npy')
-        shg2 = np.load('shg2.npy')
-        # Check for nan's
-        assert not np.isnan(shg).any()
-        assert not np.isnan(shg2).any()
-        # Check the two gauges
-        assert np.all(np.abs(shg2[1] - shg[1]) < 1e-3)
-        # It should be zero (small) since H2 is centro-symm.
-        assert np.all(np.abs(shg[1]) < 1e-8)
 
 
 def test_shg_spinpol(mme_files):
--- gpaw/test/nlopt/test_shift.py
+++ gpaw/test/nlopt/test_shift.py
@@ -1,37 +1,58 @@
 import pytest
 import numpy as np
-from ase import Atoms
 
-from gpaw import GPAW, PW
 from gpaw.mpi import world
-from gpaw.nlopt.matrixel import make_nlodata
+from gpaw.nlopt.basic import NLOData
 from gpaw.nlopt.shift import get_shift
 
 
-@pytest.mark.skipif(world.size > 4, reason='System too small')
-def test_shift(in_tmp_dir):
-    # Check for Hydrogen atom
-    atoms = Atoms('H', cell=(3 * np.eye(3)), pbc=True)
+def test_shift_spinpol(mme_files):
+    shift_values = np.array([98.0118706 + 0.j,
+                             98.89282526 + 0.j,
+                             99.78333556 + 0.j,
+                             100.68356521 + 0.j,
+                             101.59368155 + 0.j,
+                             102.51385566 + 0.j,
+                             103.44426247 + 0.j,
+                             104.38508086 + 0.j,
+                             105.33649376 + 0.j,
+                             106.29868833 + 0.j,
+                             107.271856 + 0.j])
 
-    # Do a GS and save it
-    calc = GPAW(
-        mode=PW(600), symmetry={'point_group': False},
-        kpts={'size': (2, 2, 2)}, nbands=5, txt=None)
-    atoms.calc = calc
-    atoms.get_potential_energy()
-    calc.write('gs.gpw', 'all')
+    freqs = np.linspace(2.3, 2.4, 11)
+    shift_xyz = {}
+    for spinpol in ['spinpaired', 'spinpol']:
+        tag = '_spinpol' if spinpol == 'spinpol' else ''
 
-    # Get the mml
-    nlodata = make_nlodata('gs.gpw', world)
+        # Get pre-calculated nlodata from SiC fixtures
+        nlodata = NLOData.load(mme_files[f'sic_pw{tag}'], comm=world)
 
-    # Do a shift current caclulation
-    get_shift(nlodata, freqs=np.linspace(0, 5, 100))
+        # Calculate 'xyz' tensor element of shift spectra
+        get_shift(nlodata, freqs=freqs, eta=0.025, pol='xyz',
+                  out_name=f'shift_xyz{tag}.npy')
+        world.barrier()
 
-    # Check it
-    if world.rank == 0:
-        shift = np.load('shift.npy')
+        # Load the calculated SHG spectra (in units of nm/V)
+        shift_xyz[spinpol] = np.load(f'shift_xyz{tag}.npy')[1] * 1e9
+        assert shift_xyz[spinpol] == pytest.approx(shift_values, abs=5e-2), \
+            np.max(np.abs(shift_xyz[spinpol] - shift_values))
 
-        # Check for nan's
-        assert not np.isnan(shift).any()
-        # It should be zero (small) since H2 is centro-symm.
-        assert np.all(np.abs(shift[1]) < 2e-8)
+    # import matplotlib.pyplot as plt
+    # plt.plot(freqs, shift_xyz['spinpaired'])
+    # plt.plot(freqs, shift_xyz['spinpol'])
+    # plt.show()
+
+    # Assert that the difference between spectra from spinpaired and
+    # spinpolarised calculations is small
+
+    # Absolute error
+    shift_xyz_diff = shift_xyz['spinpaired'] - shift_xyz['spinpol']
+    assert shift_xyz_diff.real == pytest.approx(0, abs=2e-2)
+    assert shift_xyz_diff.imag == pytest.approx(0, abs=1e-10), \
+        'Imaginary part should be removed when determining shift current.'
+
+    # Relative error
+    shift_xyz_avg = (shift_xyz['spinpaired'] + shift_xyz['spinpol']) / 2
+    shift_xyz_rerr_real = shift_xyz_diff.real / shift_xyz_avg.real
+    assert shift_xyz_rerr_real == pytest.approx(0, abs=1e-4), \
+        np.max(np.abs(shift_xyz_rerr_real))
