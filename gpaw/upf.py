bcdda4cc26bfc82f0a405dc06414ee9d1c6f7bad
Jens Jørgen Mortensen <jj@smoerhul.dk>
2024-03-13 13:28:00 +0100

    FLAKE8

--- gpaw/upf.py
+++ gpaw/upf.py
@@ -515,7 +515,8 @@ class UPFSetupData:
             phit_g = divrl(phit_g, 1, rgd.r_g)
             icut = len(phit_g) - 1  # XXX correct or off-by-one?
             rcut = rgd.r_g[icut]
-            bf = BasisFunction(self.n_j[j], state.l, rcut, phit_g, 'pregenerated')
+            bf = BasisFunction(self.n_j[j], state.l, rcut, phit_g,
+                               'pregenerated')
             b.bf_j.append(bf)
         return b
 
