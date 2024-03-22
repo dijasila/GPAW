733cd553d07f853c7ac6057aedcdbf3700b0afb5
Tuomas Rossi <tuomas.rossi@csc.fi>
2024-03-22 15:41:56 +0200

    Increase time for induced density calculation

--- doc/tutorialsexercises/opticalresponse/tddft/lcaotddft_Na8/run.agts.py
+++ doc/tutorialsexercises/opticalresponse/tddft/lcaotddft_Na8/run.agts.py
@@ -10,7 +10,7 @@ def workflow():
                 with run(script='spectrum.py') as spec:
                     run(script='spec_plot.py')
                 with run(script='td_fdm_replay.py', cores=8):
-                    with run(script='fdm_ind.py', cores=8):
+                    with run(script='fdm_ind.py', cores=8, tmax='30m'):
                         run(script='ind_plot.py')
                     run(script='tcm_plot.py', deps=[ksd, spec])
         with run(script='td_pulse.py', cores=8, tmax='30m'):
