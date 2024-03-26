2098c736174577986c1022c44933f67ca1768ed5
JakobKSvaneborg <jakobksvaneborg@protonmail.com>
2024-03-26 12:07:10 +0100

    implemented assertion and new test

---
+++ gpaw/test/symmetry/test_fractional_translation_grid_compatibility.py
@@ -0,0 +1,33 @@
+from ase.spacegroup import crystal
+from gpaw import GPAW
+from gpaw import PW
+import pytest
+
+
+def test_symmetry_fractional_translations_grid():
+    'cristobalite'
+    # no. 92 - tetragonal
+
+    a = 5.0833674
+    c = 7.0984738
+    p0 = (0.2939118, 0.2939118, 0.0)
+    p1 = (0.2412656, 0.0931314, 0.1739217)
+
+    atoms = crystal(['Si', 'O'], basis=[p0, p1],
+                    spacegroup=92, cellpar=[a, a, c, 90, 90, 90])
+
+    with pytest.raises(ValueError) as errmsg:
+        failcalc = GPAW(mode=PW(),
+                        xc='LDA',
+                        kpts=(3, 3, 2),
+                        nbands=40,
+                        symmetry={'symmorphic': False},
+                        gpts=(23, 23, 32),
+                        eigensolver='rmm-diis')
+
+        atoms.calc = failcalc
+        atoms.get_potential_energy()
+
+    errstr = str(errmsg.value)
+    compat_grids = ['[22 22 32]', '[22 24 32]', '[24 22 32]', '[24 24 32]']
+    assert any(grid in errstr for grid in compat_grids)
