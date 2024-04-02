51da9d386eb969cac4c70856274773de32afd3df
Robert Warmbier <robert.warmbier@gmx.de>
2024-04-02 15:51:39 +0200

    add check of deformation potential value to elph agts

--- doc/tutorialsexercises/vibrational/elph/agts.py
+++ doc/tutorialsexercises/vibrational/elph/agts.py
@@ -7,4 +7,18 @@ def workflow():
     with el:
         sc = run(script='supercell.py', cores=4, tmax='1h')
     with el, sc, scf:
-        run(script='gmatrix.py')
+        with run(script='gmatrix.py', cores=1):
+            run(function=check)
+
+
+def check():
+    """Read result and make sure it's OK."""
+    import numpy as np
+
+    g_sqklnn = np.load("gsqklnn.npy")
+    # Deformation potential at VBM, Gamma, LO phonons is
+    # about 3.6 eV/A. 3.47 eV/A with these parameters
+
+    g_lnn = g_sqklnn[0, 0, 665, 3:6, 1:4, 1:4]
+    M = np.abs(g_lnn.sum())
+    assert np.isclose(M, 3.4736929, rtol=1e-4)
