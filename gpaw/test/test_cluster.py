29040bac193ccb63b7889ce5d7593a6ded82a5e4
Esther Johnsen <estherandrea99@hotmail.com>
2024-04-05 11:56:32 +0200

    test_cluster test warnings

--- gpaw/test/test_cluster.py
+++ gpaw/test/test_cluster.py
@@ -3,10 +3,14 @@ import pytest
 
 from ase import Atoms
 
+from gpaw.cluster import Cluster
 
-def test_CO(in_tmp_dir):
+
+def test_CO(recwarn):
     R = 2.0
-    CO = Atoms('CO', [(1, 0, 0), (1, 0, R)])
+    box = 4.
+    h = 0.2
+    CO = Cluster(Atoms('CO', [(1, 0, 0), (1, 0, R)]))
 
     CO.rotate(90, 'y')
     assert CO.positions[1, 0] == pytest.approx(R, abs=1e-10)
@@ -23,3 +27,15 @@ def test_CO(in_tmp_dir):
     for c in range(3):
         assert q[0, c] == pytest.approx(p[0, 0] / sqrt(3), abs=1e-10)
         assert q[1, c] == pytest.approx(p[1, 0] / sqrt(3), abs=1e-10)
+
+    CO.minimal_box(box, h)
+    w = recwarn.pop(FutureWarning)
+
+    assert (str(w.message) ==
+            'Please use adjust_cell from gpaw.utilities.adjust_cell instead.')
+
+    CO.find_connected(0)
+    w = recwarn.pop(FutureWarning)
+
+    assert (str(w.message) ==
+            'Please use connected_indices from ase.build.connected instead.')
