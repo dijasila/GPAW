d2ca723f40bb27174d077f18a510b5385e8d110b
Ask Hjorth Larsen <asklarsen@gmail.com>
2024-04-08 16:27:48 +0200

    fix test

--- gpaw/test/response/test_bse_h_spinpol.py
+++ gpaw/test/response/test_bse_h_spinpol.py
@@ -31,4 +31,5 @@ def test_bse_spinpol(in_tmp_dir):
               conduction_bands=[[1], [1]])
 
     bse.get_bse_matrix()
-    assert bse._diag.w_T[0] == pytest.approx(0.013, abs=0.001)
+    w_T = bse.diagonalize().w_T
+    assert w_T[0] == pytest.approx(0.013, abs=0.001)
