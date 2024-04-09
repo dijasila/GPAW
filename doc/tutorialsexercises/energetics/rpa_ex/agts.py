4f566cc096e81f9238d9c8d4a4dfc0ed1e4551c2
tboland1 <tmabo@dtu.dk>
2024-04-09 14:29:12 +0200

    add atom to agts and test the output remains the same

--- doc/tutorialsexercises/energetics/rpa_ex/agts.py
+++ doc/tutorialsexercises/energetics/rpa_ex/agts.py
@@ -1,5 +1,9 @@
 def workflow():
     from myqueue.workflow import run
+    with run(script='atoms/si.atom.pbe+exx.py', cores=24, tmax='20m'):
+        run(script='atom/si.atom.rpa_init_pbe.py', cores=24, tmax='15m')
+        run(script='atom/si.atom.rpa.py', cores=24, tmax='15m')
+        run(script='atom/test_pbe_isolated_output.py', cores=1, tmax='5m')
     with run(script='si.pbe.py'):
         run(script='si_pbe_exx.py', cores=4, tmax='15m')
         run(script='test_pbe_output.py', cores=1, tmax='5m')
