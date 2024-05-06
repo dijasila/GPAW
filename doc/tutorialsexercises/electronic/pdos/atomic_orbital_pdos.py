bb21353c4fa548b242e1372c8c29ceb470ab1e99
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2024-05-06 22:43:01 +0200

    np.trapz -> scipy.integrate.trapezoid

--- doc/tutorialsexercises/electronic/pdos/atomic_orbital_pdos.py
+++ doc/tutorialsexercises/electronic/pdos/atomic_orbital_pdos.py
@@ -1,14 +1,15 @@
 # web-page: ag-ddos.png
 import numpy as np
+from scipy.integrate import trapezoid
 import matplotlib.pyplot as plt
 from gpaw import GPAW
 
 calc = GPAW('au.gpw', txt=None)
 energy, pdos = calc.get_orbital_ldos(a=0, angular='d')
 energy -= calc.get_fermi_level()
-I = np.trapz(pdos, energy)
-center = np.trapz(pdos * energy, energy) / I
-width = np.sqrt(np.trapz(pdos * (energy - center)**2, energy) / I)
+I = trapezoid(pdos, energy)
+center = trapezoid(pdos * energy, energy) / I
+width = np.sqrt(trapezoid(pdos * (energy - center)**2, energy) / I)
 plt.plot(energy, pdos)
 plt.xlabel('Energy (eV)')
 plt.ylabel('d-projected DOS on atom 0')
