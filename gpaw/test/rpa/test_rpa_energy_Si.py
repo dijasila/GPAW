2479805371440a49b9ab881cdf043ca39ca2c877
Mikael Kuisma <kuisma@dtu.dk>
2024-05-13 12:07:40 +0200

    Add a test for q0 being [1e-17,0,0]

--- gpaw/test/rpa/test_rpa_energy_Si.py
+++ gpaw/test/rpa/test_rpa_energy_Si.py
@@ -3,6 +3,26 @@ from gpaw.mpi import serial_comm
 from gpaw import GPAW
 from gpaw.xc.rpa import RPACorrelation
 
+@pytest.mark.rpa
+@pytest.mark.response
+def test_qpt_rounding_error(in_tmp_dir):
+    from ase.build import bulk
+    atoms = bulk('Si')
+    calc = GPAW(mode='pw', kpts={'gamma':True, 'size':(6,1,1)})
+    atoms.calc = calc
+    atoms.get_potential_energy()
+
+    calc.diagonalize_full_hamiltonian(nbands=50)
+
+    ecut = 50
+    rpa = RPACorrelation(calc, qsym=False, nfrequencies=8, ecut=[ecut])
+    E_rpa_noqsym = rpa.calculate()
+
+    rpa = RPACorrelation(calc, qsym=True, nfrequencies=8, ecut=[ecut])
+    E_rpa_qsym = rpa.calculate()
+
+    assert E_rpa_qsym == pytest.approx(-9.753, abs=0.01)
+    assert E_rpa_qsym == pytest.approx(E_rpa_noqsym)
 
 @pytest.mark.rpa
 @pytest.mark.response
